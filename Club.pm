982988965452ec7c86b7d58ae46a5d67b9f88b61
Rob Nagler <github@q33.us>
1999-03-30 07:54:57 +0000

    Removed content_type as separate method, so can deal with directories. BROKE directories.  URI = /cosmic isn't really handled properly. Should be redirected to /cosmic/.  However redirects cause SEGV in mod_perl.  Need to generate own headers.

--- Club.pm
+++ Club.pm
@@ -34,27 +34,28 @@ sub _request ($) {
     my($user) = Bivio::User->authenticate($br);
     my($r) = $br->r;
     my($path);
-    ($path = $r->uri()) =~ s<^/([^/]+)/?><>;
+    ($path = $r->uri()) =~ s<^/([^/]+)><>;
     my($name) = $1;
     my($self) = &Bivio::Data::lookup($_HOME . $name, Bivio::Club::, $br);
     defined($self) || $br->not_found($name, ': no such club');
     $br->set_club($self);
     $self->{members}->authenticate($br);
-    $r->content_type(&_content_type($path, $br));
-    $r->send_http_header();
-    open('Bivio::Club::IN', $r->document_root . '/' . $path)
+# Causes SEGV
+#    length($path) || $br->redirect($r->uri() . '/');
+    my($ct) = "text/html";
+    {
+	$path =~ /.html$/ && last;
+	$path =~ /^[^.]*$/ && ($path .= '/index.html', last);  	    # no suffix
+	$path =~ /.gif$/ && ($ct = "image/gif", last);
+	$br->not_found("bad suffix");
+    }
+    open('Bivio::Club::IN', $r->document_root . '/' . $name . '/' . $path)
 	|| $br->not_found("file not found");
+    $r->content_type($ct);
+    $r->send_http_header();
     $r->send_fd('Bivio::Club::IN');
     close('Bivio::Club::IN');
     return &Apache::Constants::OK;
-}
-
-sub _content_type ($)
-{
-    local($_) = shift;
-    /.gif$/ && return "image/gif";
-    /.html$/ && return "text/html";
-    shift->not_found("bad suffix");
 }
 
 
