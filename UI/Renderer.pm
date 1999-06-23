3964a63c7ffeb4f8793f8ad568c26ce44ad7f0d5
Paul Moeller <moeller@bivio.biz>
1999-06-23 02:46:57 +0000

    initial revision

---
+++ UI/Renderer.pm
@@ -0,0 +1,60 @@
+# Copyright (c) 1999 bivio, LLC.  All rights reserved.
+# $Id$
+package Bivio::UI::Renderer;
+use strict;
+$Bivio::UI::Renderer::VERSION = sprintf('%d.%02d', q$Revision$ =~ /+/g);
+
+=head1 NAME
+
+Bivio::UI::Renderer - abstract renderer
+
+=cut
+
+@Bivio::UI::Renderer::ISA = qw(Bivio::UNIVERSAL);
+
+=head1 DESCRIPTION
+
+C<Bivio::UI::Renderer>
+Parent interface of all output renderers.
+
+=cut
+
+=for html <a name="new"></a>
+
+=head2 static new() : Bivio::UI::Renderer
+
+=cut
+
+sub new {
+    return &Bivio::UNIVERSAL::new(@_);
+}
+
+=head1 METHODS
+
+=cut
+
+=for html <a name="render"></a>
+
+=head2 abstract render(UNIVERSAL target, Request req)
+
+Renders the target onto the Request's print stream.
+
+=cut
+
+sub render {
+    die( "abstract method Renderer::render invoked!\n" );
+}
+
+#=PRIVATE METHODS
+
+=head1 COPYRIGHT
+
+Copyright (c) 1999 bivio, LLC.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
