d18aa84a3fb9cf361cf81f1d53d4866607b987e3
Rob Nagler <github@q33.us>
1999-07-02 05:36:55 +0000

    *** empty log message ***

---
+++ Ext/DBI.pm
@@ -0,0 +1,116 @@
+# Copyright (c) 1999 bivio, LLC.  All rights reserved.
+# $Id$
+package Bivio::Ext::DBI;
+use strict;
+$Bivio::Ext::DBI::VERSION = sprintf('%d.%02d', q$Revision$ =~ /+/g);
+
+=head1 NAME
+
+Bivio::Ext::DBI - configuration wrapper around DBI
+
+=head1 SYNOPSIS
+
+    use Bivio::Ext::DBI;
+    Bivio::Ext::DBI->connect();
+    Bivio::Ext::DBI->connect("my_db_cfg");
+
+=cut
+
+=head1 EXTENDS
+
+L<DBI>
+
+=cut
+
+use DBI;
+@Bivio::Ext::DBI::ISA = qw(DBI);
+
+=head1 DESCRIPTION
+
+C<Bivio::Ext::DBI> is a simple wrapper around the standard L<DBI>.  Instead of
+specifying the configuration explicitly, the caller specifies a configuration
+name which is used to connect to.
+
+=cut
+
+#=IMPORTS
+use Bivio::IO::Config;
+#use Bivio::IO::Trace;
+
+#=VARIABLES
+#use vars ($_TRACE);
+#Bivio::IO::Trace->register;
+#my($_PACKAGE) = __PACKAGE__;
+Bivio::IO::Config->register({
+    'ORACLE_HOME' => Bivio::IO::Config->REQUIRED,
+    Bivio::IO::Config->NAMED => {
+	'database' => Bivio::IO::Config->REQUIRED,
+	'user' => Bivio::IO::Config->REQUIRED,
+	'pass' => Bivio::IO::Config->REQUIRED,
+    },
+});
+my($_DEFAULT_OPTIONS) = {
+    'AutoCommit' => 0,
+    'RaiseError' => 1,
+};
+
+=head1 FACTORIES
+
+=cut
+
+=for html <a name="connect"></a>
+
+=head2 static connect() : Bivio::Ext::DBI
+
+=head2 static connect(string database) : Bivio::Ext::DBI
+
+Connect to the default or the specfied database.  Returns a handle that
+can be used just like DBI.
+
+If an error is encountered, die is called.
+
+=cut
+
+sub connect {
+    my($proto, $database) = @_;
+    my($cfg) = Bivio::IO::Config->get($database);
+    my($self) = &DBI::connect("dbi:Oracle:$cfg->{database}",
+	    $cfg->{user}, $cfg->{password}, $_DEFAULT_OPTIONS);
+    return $self;
+}
+
+=head1 METHODS
+
+=cut
+
+=for html <a name="configure"></a>
+
+=head2 static configure(hash cfg)
+
+=over 4
+
+=item name : type [default]
+
+=back
+
+=cut
+
+sub configure {
+    my(undef, $cfg) = @_;
+    $ENV{ORACLE_HOME} = $cfg->{ORACLE_HOME};
+    return;
+}
+
+#=PRIVATE METHODS
+
+=head1 COPYRIGHT
+
+Copyright (c) 1999 bivio, LLC.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
---
+++ IO/t/Config.t
@@ -0,0 +1,81 @@
+# -*-perl-*-
+#
+# $Id$
+#
+use strict;
+
+BEGIN { $| = 1; print "1..8\n"; }
+my($loaded) = 0;
+END {print "not ok 1\n" unless $loaded;}
+use Bivio::IO::Config;
+use Bivio::IO::Alert;
+$loaded = 1;
+print "ok 1\n";
+
+######################### End of black magic.
+
+my($T) = 1;
+sub dev_init {
+    ++$T;
+    print eval {
+	Bivio::IO::Config->initialize(@_);
+	1;
+    } ? "not ok $T\n" : "ok $T\n";
+}
+
+sub conf_init {
+    ++$T;
+    print eval {
+	Bivio::IO::Config->initialize(@_);
+	1;
+    } ? "ok $T\n" : "not ok $T\n";
+}
+
+sub dev_get {
+    ++$T;
+    my($caller) = caller;
+    print eval "
+        package $caller;
+	Bivio::IO::Config->get(\@_);
+	1;
+    " ? "$@not ok $T\n" : "ok $T\n";
+}
+
+sub conf_get {
+    ++$T;
+    my($caller) = caller;
+    print eval "
+        package $caller;
+	Bivio::IO::Config->get(\@_);
+	1;
+    " ? "ok $T\n" : "$@not ok $T\n";
+}
+
+package Bivio::IO::Config::T1;
+sub configure {}
+Bivio::IO::Config->register({
+    'p1' => Bivio::IO::Config->REQUIRED,
+    'p2' => 'p2',
+    Bivio::IO::Config->NAMED => {
+       p3 => Bivio::IO::Config->REQUIRED,
+       p4 => undef,
+       p5 => 39
+    }
+});
+
+&main::dev_init({});
+&main::conf_init({
+    'Bivio::IO::Config::T1' => {
+	'p1' => 'p1',
+    }
+});
+&main::conf_get();
+&main::dev_get('hello');
+&main::dev_get(undef);
+&main::conf_init({
+    'Bivio::IO::Config::T1' => {
+	'p1' => 'p1',
+	'p3' => 'p3',
+    }
+});
+&main::conf_get(undef);
