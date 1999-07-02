0691a76a3dafa36c8a5626d33d990cba5d329ac5
Rob Nagler <github@q33.us>
1999-07-02 05:32:48 +0000

    Added named and required configuration since that seemed to be the pattern.

--- IO/Config.pm
+++ IO/Config.pm
@@ -46,17 +46,51 @@ upcalled with their configuration.
 
 =cut
 
+=head1 CONSTANTS
+
+=cut
+
+=for html <a name="NAMED"></a>
+
+=head2 NAMED : string
+
+Identifies the named configuration specification, see L<register|"register">.
+
+=cut
+
+sub NAMED {
+    return \&NAMED;
+}
+
+=for html <a name="REQUIRED"></a>
+
+=head2 REQUIRED : string
+
+Returns a unique value which passed in spec (see L<get|"get">)
+will indicate the configuration parameter is required.
+
+=cut
+
+sub REQUIRED {
+    return \&REQUIRED;
+}
+
 #=IMPORTS
 # Do not use explicitly, to ensure this module initialized first
 # use Bivio::IO::Alert;
 
 #=VARIABLES
 my($_PACKAGE) = __PACKAGE__;
-# So we behave nicely before we are initialized
-my($_DATA) = {};
+# The configuration read off disk or passed in
+my($_ACTUAL) = {};
+# Was "initialized" called once?
 my($_INITIALIZED) = 0;
 # List of packages registered
 my(@_REGISTERED) = ();
+# Configuration specifications for registered packages
+my(%_SPEC) = ();
+# Has a package been configured?
+my(%_CONFIGURED) = ();
 
 =head1 METHODS
 
@@ -64,12 +98,38 @@ my(@_REGISTERED) = ();
 
 =for html <a name="get"></a>
 
-=head2 static get() : hash_ref
+=head2 static get(string name) : hash
 
-=head2 static get(string package) : hash_ref
+Looks up configuration for the caller's package.  If name is provided, returns
+the configuration hash bound to I<name> within the package's configuration
+space, e.g. given the config:
 
-If no package is supplied, uses caller's package.  Looks up configuration for
-the package.  If none is found, returns an empty hash_ref.
+    'Bivio::IPC::Server' => {
+        'listen' => 35,
+        'my_server' => {
+            'port' => 1234,
+            'timeout' => 60_000,
+        },
+        'my_other_server' => {
+            'port' => 9999,
+        },
+    }
+
+C<get('my_server')> will return the following hash:
+
+    {
+        'listen' => 35,
+        'port' => 1234,
+        'timeout' => 60_000,
+    }
+
+Required configuration is checked during this call.
+
+If I<name> is passed but is undefined, then only the named configuration
+parameters will be returned.
+
+If I<name> is not passed, then the entire configuration will be returned,
+including specific named sections.
 
 NOTE: Should not be called from a package body, because
 L<initialize|"initialize"> should be called by main.
@@ -77,9 +137,27 @@ L<initialize|"initialize"> should be called by main.
 =cut
 
 sub get {
-    my($proto, $package) = @_;
-    defined($package) || ($package = caller);
-    return ref($_DATA->{$package}) eq 'HASH' ? $_DATA->{$package} : {};
+    my($proto, $name) = @_;
+    my($pkg);
+    my($i) = 0;
+    0 while ($pkg = caller($i++)) eq __PACKAGE__;
+    my($pkg_cfg) = &_get_pkg($pkg);
+    int(@_) < 2 && return $pkg_cfg;
+    my($spec) = $_SPEC{$pkg};
+    defined($spec) && defined($spec->{&NAMED})
+	    || die("$pkg: named config not specified");
+    if (defined($name)) {
+	defined($pkg_cfg->{$name})
+		|| die("$pkg $name: named config not found");
+	return $pkg_cfg->{$name};
+    }
+    # Retrieve the "undef" config, see _get_pkg
+    my($cfg) = $pkg_cfg->{&NAMED};
+    my(@bad) = grep(defined($cfg->{$_}) && $cfg->{$_} eq &REQUIRED,
+	    keys(%$cfg));
+    @bad || return;
+    @bad = sort @bad;
+    die("$pkg @bad: named config required");
 }
 
 =for html <a name="initialize"></a>
@@ -99,12 +177,13 @@ Calls registrants if configuration is valid.
 sub initialize {
     my(undef, $arg) = @_;
     $_INITIALIZED = 1;
+    %_CONFIGURED = ();
     # On failure, we have no configuration.
-    $_DATA = {};
+    $_ACTUAL = {};
     my($file);
     if (defined($arg)) {
 	if (ref($arg) eq 'HASH') {
-	    $_DATA = $arg;
+	    $_ACTUAL = $arg;
 	}
 	else {
 	    -r $arg || Bivio::IO::Alert->die(
@@ -124,19 +203,19 @@ sub initialize {
 	ref($data) eq 'HASH' || Bivio::IO::Alert->die(
 		"$file: config parse failed: ",
 		$@ ? $@ : "empty or not a hash_ref");
-	$_DATA = $data;
+	$_ACTUAL = $data;
     }
     # Call registrants in FIFO
-    my($r);
-    foreach $r (@_REGISTERED) {
-	&_configure($r);
+    my($pkg);
+    foreach $pkg (@_REGISTERED) {
+	&{\&{$pkg . '::configure'}}($pkg, &_get_pkg($pkg));
     }
     return;
 }
 
 =for html <a name="register"></a>
 
-=head2 register()
+=head2 register(hash spec)
 
 Calling package will be put in the list of packages to be configured.  A
 callback may happen immediately, if L<initialize|"initialize"> was called
@@ -145,25 +224,109 @@ already.
 The calling package must define a C<configure> method which takes two
 arguments, the class and the configuration as a hash.
 
+If I<spec> is supplied, the values will be filled in when
+L<get|"get"> is called or the values are upcalled to I<configure>.
+
+A configuration I<spec> looks like:
+
+    {
+	'my_optional_param' => 35,
+        'my_required_param' => Bivio::IO::Config->REQUIRED,
+        Bivio::IO::Config->NAMED => {
+            'my_named_optional_param' => 'hello',
+            'my_named_required_param' => Bivio::IO::Config->REQUIRED,
+        }
+    }
+
+Named configuration allows the package's configuration to be separately
+named.  For example, you might have several named databases you want
+to configure.  Named configuration is initialized from three locations:
+
+=over 4
+
+=item *
+
+A specifically named configuration section, e.g. C<my_server>.
+
+=item *
+
+The parameters found in the (unnamed) common part of the configuration
+using the names found in the L<NAMED|"NAMED"> part of the specification.
+
+=item *
+
+Lastly, the default values specified in the L<NAMED|"NAMED"> specification.
+
+=back
+
+All configuration names must be fully specified.
+
 =cut
 
 sub register {
-    my($proto) = @_;
+    my($proto, $spec) = @_;
     my($pkg) = caller;
     defined(&{$pkg . '::configure'}) || Bivio::IO::Alert->die(
 	    "&$pkg\::configure not defined");
     push(@_REGISTERED, $pkg);
+    $_SPEC{$pkg} = $spec;
     $_INITIALIZED && &_configure($pkg);
     return;
 }
 
 #=PRIVATE METHODS
 
-sub _configure {
+sub _get_pkg {
     my($pkg) = @_;
-    Bivio::IO::Alert->eval_or_warn(sub {
-	&{\&{$pkg . '::configure'}}($pkg, &get(undef, $pkg))
-    });
+    $_CONFIGURED{$pkg} && return $_ACTUAL->{$pkg};
+    my($cfg) = ref($_ACTUAL->{$pkg}) ? $_ACTUAL->{$pkg} : {};
+    if ($_SPEC{$pkg}) {
+	# Set the defaults for the common configuration
+	my($spec) = $_SPEC{$pkg};
+	while (my($k, $v) = each(%$spec)) {
+	    # Have an actual value for specified config?
+	    exists($cfg->{$k}) && next;
+	    # Is the named spec?
+	    $k eq &NAMED && next;
+	    # If it is required, then it is an error
+	    $v eq &REQUIRED
+		    && die("$pkg $k: config parameter required");
+	    # Assign the default value
+	    $cfg->{$k} = $v;
+	    # Set the defaults for all named configuration
+	    if (defined($spec->{&NAMED})) {
+		my($named_spec) = $spec->{&NAMED};
+		# Fill in the cfg for the "undef" case of &get
+		my($undef_cfg) = {%$named_spec};
+		while (my($k, $v) = each(%$cfg)) {
+		    # Does a spec exist for this param?
+		    exists($spec->{$k}) && next;
+		    # Does a named spec exist?
+		    if (exists($named_spec->{$k})) {
+			# Override named default with actual config
+			$undef_cfg->{$k} = $v;
+			next;
+		    }
+		    # Must be a named configuration section
+		    ref($v) || die("$pkg $k: invalid config parameter");
+		    while (my($nk, $nv) = each(%$named_spec)) {
+			# Have an actual value for specified named config?
+			exists($v->{$nk}) && next;
+			# If it is required, then it is an error
+			$nv eq &REQUIRED && die(
+				"$pkg $nk: named config parameter required");
+			# Assign the default value
+			$v->{$nk} = $nv;
+		    }
+		}
+		# Overload the use of "NAMED" to mean undef named cfg
+		# in actual configuration.
+		$cfg->{&NAMED} = $undef_cfg;
+	    }
+	}
+    }
+    $_CONFIGURED{$pkg} = 1;
+    return $_ACTUAL->{$pkg} = $cfg;
 }
 
 =head1 ENVIRONMENT
