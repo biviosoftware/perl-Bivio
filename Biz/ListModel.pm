e6bb170e2d26102c6ee752fdeb6033a905759b3e
Paul Moeller <moeller@bivio.biz>
1999-07-05 18:06:46 +0000

    ctor uses field-descriptors, added internal_get_rows

--- Biz/ListModel.pm
+++ Biz/ListModel.pm
@@ -42,15 +42,24 @@ my($_PACKAGE) = __PACKAGE__;
 
 =for html <a name="new"></a>
 
-=head2 static new(string name) : Bivio::Biz::ListModel
+=head2 static new(string name, array column_info) : Bivio::Biz::ListModel
 
-Creates a new ListModel with the specified name.
+Creates a new ListModel with the specified name and column information.
+column_info should have the format:
+    [
+        ['heading', field-descriptor]
+        ...
+    }
 
 =cut
 
 sub new {
-    my($proto, $name) = @_;
+    my($proto, $name, $column_info) = @_;
     my($self) = &Bivio::Biz::Model::new($proto, $name);
+    $self->{$_PACKAGE} = {
+	column_info => $column_info,
+	rows => []
+    };
     return $self;
 }
 
@@ -60,62 +69,89 @@ sub new {
 
 =for html <a name="get_column_count"></a>
 
-=head2 abstract get_column_count() : int
+=head2 get_column_count() : int
 
 Returns the number of columns in the model.
 
 =cut
 
 sub get_column_count {
-    die("abstract method");
+    my($self, $col) = @_;
+    my($fields) = $self->{$_PACKAGE};
+    return scalar(@{$fields->{column_info}});
 }
 
 =for html <a name="get_column_descriptor"></a>
 
-=head2 abstract get_column_descriptor(int col) : FieldDescriptor
+=head2 get_column_descriptor(int col) : FieldDescriptor
 
 Returns the descriptor for the indexed column.
 
 =cut
 
 sub get_column_descriptor {
-    die("abstract method");
+    my($self, $col) = @_;
+    my($fields) = $self->{$_PACKAGE};
+    return $fields->{column_info}->[1];
 }
 
 =for html <a name="get_column_heading"></a>
 
 =head2 get_column_heading(int col) : string
 
-Returns the column heading for the specified column.
+Returns the heading for the specified column.
 
 =cut
 
 sub get_column_heading {
-    die("abstract method");
+    my($self, $col) = @_;
+    my($fields) = $self->{$_PACKAGE};
+    return $fields->{column_info}->[0];
 }
 
 =for html <a name="get_row_count"></a>
 
-=head2 abstract get_row_count() : int
+=head2 get_row_count() : int
 
 Returns the number of rows in the model result set.
 
 =cut
 
 sub get_row_count {
-    die("abstract method");
+    my($self) = @_;
+    my($fields) = $self->{$_PACKAGE};
+    return scalar(@{$fields->{rows}});
 }
 
 =for html <a name="get_value_at"></a>
 
-=head2 abstract get_value_at(int row, int col) : scalar or CompoundField
+=head2 get_value_at(int row, int col) : scalar or CompoundField
 
 Returns the simple or complex value at the specified coordinates.
 
 =cut
 
 sub get_value_at {
-    die("abstract method");
+    my($self, $row, $col) = @_;
+    my($fields) = $self->{$_PACKAGE};
+    return $fields->{rows}->[$row]->[$col];
+}
+
+=for html <a name="internal_get_rows"></a>
+
+=head2 protected internal_get_rows() : array
+
+Returns the contents of the rows array. Only subclasses may call this
+method (enforced).
+
+=cut
+
+sub internal_get_rows {
+    my($self) = @_;
+    my($fields) = $self->{$_PACKAGE};
+
+    caller(0)->isa($_PACKAGE) || Carp::croak("protected method");
+    return $fields->{rows};
 }
 
 #=PRIVATE METHODS
