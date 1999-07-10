4a271b412802fc516490e991ad1fb9c6f483868b
Paul Moeller <moeller@bivio.biz>
1999-07-10 04:46:01 +0000

    user creation handling

--- Agent/HTTP/AdminController.pm
+++ Agent/HTTP/AdminController.pm
@@ -36,6 +36,9 @@ C<Bivio::Agent::HTTP::AdminController>
 
 #=IMPORTS
 use Bivio::Agent::Request;
+use Bivio::Biz::Club;
+use Bivio::Biz::ClubUser;
+use Bivio::Biz::User;
 use Bivio::IO::Trace;
 
 #=VARIABLES
@@ -82,6 +85,13 @@ sub handle_request {
     my($self, $req) = @_;
     my($fields) = $self->{$_PACKAGE};
 
+    my($ret, $club, $club_user) = &_authorize_admin($req);
+
+    if (! $ret) {
+	$req->set_state(Bivio::Agent::Request::AUTH_REQUIRED);
+	return;
+    }
+
     # set the default view if necessary
     unless ($req->get_view_name()) {
 	$req->set_view_name($fields->{default_view});
@@ -91,23 +101,75 @@ sub handle_request {
     if (defined($view)) {
 
 	my($model) = $view->get_default_model();
-	$model->find($req->get_model_args());
+	if (scalar(%{$req->get_model_args()})) {
+	    $model->find($req->get_model_args());
+	}
+	elsif ($view->get_name() eq 'users') {
+	    $model->find({'club' => $club->get('id')});
+	}
+
 	if ($req->get_action_name()) {
-	    $model->get_action($req->get_action_name())->execute(
-		    $model, $req);
+	    $model->get_action($req->get_action_name())->execute($model, $req);
+
+	    if ($model->get_status()->is_OK()
+		    && $req->get_action_name() eq 'add'
+		    && $req->get_view_name() eq 'user') {
+
+		# successful user add
+		# need to change to the user list
+		$req->set_view_name('users');
+		$view = $self->get_view('users');
+		$model = $view->get_default_model();
+		$model->find({'club' => $club->get('id')});
+	    }
 	}
 
 	$view->activate()->render($model, $req);
 	$req->set_state(Bivio::Agent::Request::OK);
     }
     else {
-	$req->log_error("\n".__PACKAGE__.' couldn\'t find view '.
-		$req->get_view_name()."\n\n");
-	$req->set_state(Bivio::Agent::Request::NOT_HANDLED);
+	&_trace('couldn\'t find view '.$req->get_view_name());
     }
 }
 
 #=PRIVATE METHODS
+
+# _authorize_admin(Request req) : (boolean, Club, ClubUser)
+#
+# Determines if the request is the authorized club administrator.
+# Returns (1, club, club_user) if successful, (0) otherwise.
+
+sub _authorize_admin {
+    my($req) = @_;
+
+    my($user) = $req->get_user();
+
+    # has the user logged in?
+    return (0) if ! $user;
+
+    # do the password's match?
+    unless($req->get_password()
+	    && $req->get_password() eq $user->get('password')) {
+	return (0);
+    }
+
+    my($club) = Bivio::Biz::Club->new();
+    $club->find({name => $req->get_target_name()});
+
+    # does the club exist?
+    return (0) if ! $club->get_status()->is_OK();
+
+    my($club_user) = Bivio::Biz::ClubUser->new();
+    $club_user->find({club => $club->get('id'), user => $user->get('id')});
+
+    # is the user a member of the club?
+    return (0) if ! $club_user->get_status()->is_OK();
+
+    # is the user an admin of the club?
+    return (0) if $club_user->get('role') != 0;
+
+    return (1, $club, $club_user);
+}
 
 =head1 COPYRIGHT
 
