8121d73f22a386561a89604e7baaa86171ec72b3
Rob Nagler <github@q33.us>
1999-09-10 03:56:38 +0000

    Needs documentation

---
+++ TypeError.pm
@@ -0,0 +1,137 @@
+# Copyright (c) 1999 bivio, LLC.  All rights reserved.
+# $Id$
+package Bivio::TypeError;
+use strict;
+$Bivio::TypeError::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+=head1 NAME
+
+Bivio::TypeError - enum of errors in converting values
+
+=head1 SYNOPSIS
+
+    use Bivio::TypeError;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio::Type::Enum>
+
+=cut
+
+use Bivio::Type::Enum;
+@Bivio::TypeError::ISA = ('Bivio::Type::Enum');
+
+=head1 DESCRIPTION
+
+C<Bivio::TypeError> is a list of problems converting types.
+
+#TODO: Make a list in doc here
+
+=cut
+
+#=IMPORTS
+use Bivio::TypeError;
+
+#=VARIABLES
+
+__PACKAGE__->compile(
+    UNKNOWN => [
+	0,
+    ],
+    INTEGER => [
+	1,
+	undef,
+	'expecting a number without a decimal point',
+    ],
+    NUMBER_RANGE => [
+	2,
+	undef,
+	'number outside of expected range',
+    ],
+    NAME => [
+	3,
+	undef,
+	'name is not valid',
+    ],
+    DATE_TIME => [
+	4,
+	undef,
+	'date time must be number of seconds since Jan 1, 1970',
+    ],
+    DATE => [
+	5,
+	undef,
+	'invalid date format, must be mm/dd/yyyy',
+    ],
+    TIME => [
+	6,
+	undef,
+	'invalid time format, must be hh:mm, hh:mm a, hh:mm:ss p',
+    ],
+    NUMBER => [
+	7,
+	undef,
+	'expecting a number',
+    ],
+    DATE_RANGE => [
+	8,
+	undef,
+	'date outside of valid range (1970 to 2038)',
+    ],
+    TIME_RANGE => [
+	9,
+	undef,
+	'date outside of valid range (years 1970 to 2037)',
+    ],
+    MONTH => [
+	10,
+	undef,
+	'invalid month',
+    ],
+    DAY_OF_MONTH => [
+	11,
+	undef,
+	'invalid day for this month',
+    ],
+    HOUR => [
+	12,
+	undef,
+	'invalid hour',
+    ],
+    MINUTE => [
+	13,
+	undef,
+	'invalid minute',
+    ],
+    SECOND => [
+	14,
+	undef,
+	'invalid second',
+    ],
+    NULL => [
+	15,
+	undef,
+	'field may not be empty',
+    ],
+    CONFIRM_PASSWORD => [
+	16,
+	undef,
+	'password and confirm password fields do not match',
+    ],
+);
+
+#=PRIVATE METHODS
+
+=head1 COPYRIGHT
+
+Copyright (c) 1999 bivio, LLC.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
