59a1f807e1ad35de811bc5c9bf96162ce6114640
Paul Moeller <moeller@bivio.biz>
1999-09-23 17:21:09 +0000

    first pass at supporting list models as choices

--- UI/HTML/Widget/Select.pm
+++ UI/HTML/Widget/Select.pm
@@ -45,6 +45,22 @@ Which form are we dealing with.
 
 List of choices will be constructed from the Enum's values.
 
+=item choices : string (required)
+
+Name of the Bivio::Biz::ListModel returned from source->get_widget_value().
+The values are looked up dynamically during render.
+
+=item list_display_field : string (required if 'choices' is a list)
+
+Name of the list field used for display.
+
+=item list_id_field : string (required if 'choices' is a list)
+
+Name of the list field used as the item id.
+
+TODO: this attribute shouldn't exist - it should use the primary key
+      fields of the list model.
+
 =back
 
 =cut
@@ -92,19 +108,17 @@ sub initialize {
     my($fields) = $self->{$_PACKAGE};
     return if $fields->{model};
     $fields->{model} = $self->ancestral_get('form_model');
-    ($fields->{field}, $fields->{enum}) = $self->get('field', 'choices');
-    my(@list) = sort {
-	# Always put "0" (unknown) first.
-	return -1 if $a->as_int == 0;
-	return 1 if $b->as_int == 0;
-	$a->get_name cmp $b->get_name
-    } $fields->{enum}->get_list;
-    my(@choices);
-    foreach my $choice (@list) {
-	push(@choices, $choice->as_int,
-		Bivio::Util::escape_html($choice->get_short_desc));
+    $fields->{field} = $self->get('field');
+
+    my($choices) = $self->get('choices');
+    if ($choices->isa('Bivio::Type::Enum')) {
+	_load_items_from_enum($self, $choices);
+	$fields->{list_source} = 0;
     }
-    $fields->{choices} = \@choices;
+    else {
+	$fields->{list_source} = 1;
+	# load it dynamically during render
+    }
     return;
 }
 
@@ -130,14 +144,18 @@ sub render {
 	$fields->{initialized} = 1;
     }
     $$buffer .= $fields->{prefix};
-    my($choices) = $fields->{choices};
+    _load_items_from_list($self, $source) if $fields->{list_source};
+    my($items) = $fields->{items};
     my($field_value) = $form->get($field);
-    $field_value = defined($field_value) ? $field_value->as_int : 'x';
-    for (my($i) = 0; $i < int(@$choices); $i += 2) {
-	my($v) = $choices->[$i];
+
+    $field_value = '' unless defined($field_value);
+    $field_value = $field_value->as_int if ref($field_value);
+
+    for (my($i) = 0; $i < int(@$items); $i += 2) {
+	my($v) = $items->[$i];
 	$$buffer .= '<option value='.$v;
 	$$buffer .= ' selected' if $field_value eq $v;
-	$$buffer .= '>'.$choices->[$i+1]."\n";
+	$$buffer .= '>'.$items->[$i+1]."\n";
     }
     # No newline, don't know what follows.
     $$buffer .= '</select>';
@@ -145,6 +163,56 @@ sub render {
 }
 
 #=PRIVATE METHODS
+
+# _load_items_from_enum(Bivio::Type::Enum enum)
+#
+# Loads items from the enum choices attribute. Enum values are static
+# so this is called during initialize.
+#
+sub _load_items_from_enum {
+    my($self, $enum) = @_;
+    my($fields) = $self->{$_PACKAGE};
+
+    my(@values) = sort {
+	# Always put "0" (unknown) first.
+	return -1 if $a->as_int == 0;
+	return 1 if $b->as_int == 0;
+	$a->get_name cmp $b->get_name
+    } $enum->get_list;
+
+    # id, display pairs
+    my(@items);
+    foreach my $item (@values) {
+	push(@items, $item->as_int,
+		Bivio::Util::escape_html($item->get_short_desc));
+    }
+    $fields->{items} = \@items;
+
+    return;
+}
+
+# _load_items_from_list(any source)
+#
+# Loads items from the list choices attribute. List values are
+# dynamic so this is called during render.
+#
+sub _load_items_from_list {
+    my($self, $source) = @_;
+    my($fields) = $self->{$_PACKAGE};
+
+    my($list) = $source->get_widget_value($self->get('choices'));
+    my($display_name) = $self->get('list_display_field');
+    my($id_name) = $self->get('list_id_field');
+ 
+    # id, display pairs
+    my(@items);
+    while($list->next_row) {
+	push(@items, $list->get($id_name), $list->get($display_name));
+    }
+    $fields->{items} = \@items;
+
+    return;
+}
 
 =head1 COPYRIGHT
 
