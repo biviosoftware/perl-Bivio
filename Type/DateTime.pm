37fdf421a2207d80417190170732ea7247c0206f
Rob Nagler <github@q33.us>
1999-09-24 00:14:13 +0000

    DateTimes are stored in 'J SSSSS' format internally.  Unix time was too restrictive.

--- Type/DateTime.pm
+++ Type/DateTime.pm
@@ -31,14 +31,30 @@ L<Bivio::Type::Date|Bivio::Type::Date>
 and L<Bivio::Type::Time|Bivio::Type::Time>.
 This allows for some common code.
 
-Although a C<DateTime> is represented in perl as an int,
-it is not a L<Bivio::Type::Number|Bivio::Type::Number>.
+Although a C<DateTime> is represented as the number of
+julian days separated by the number of seconds in the day,
+i.e. same as C<TO_CHAR('J SSSSS')> in SQL.
+A C<DateTime> is not a L<Bivio::Type::Number|Bivio::Type::Number>.
 
 =cut
 
 =head1 CONSTANTS
 
 =cut
+
+=for html <a name="DEFAULT_DATE"></a>
+
+=head2 DEFAULT_DATE : string
+
+Returns L<FIRST_YEAR_IN_JULIAN_DAYS|"FIRST_YEAR_IN_JULIAN_DAYS">.
+Used when there is only a time value.  See
+L<Bivio::Type::Time|Bivio::Type::Time>.
+
+=cut
+
+sub DEFAULT_DATE {
+    return FIRST_YEAR_IN_JULIAN_DAYS();
+}
 
 =for html <a name="DEFAULT_TIME"></a>
 
@@ -63,6 +79,54 @@ This is a compromise until we have more time work on DateTime.
 
 sub DEFAULT_TIME {
     return 79199;
+}
+
+=for html <a name="FIRST_YEAR"></a>
+
+=head2 FIRST_YEAR : int
+
+Returns 1800.
+
+=cut
+
+sub FIRST_YEAR {
+    return 1800;
+}
+
+=for html <a name="FIRST_YEAR_IN_JULIAN_DAYS"></a>
+
+=head2 FIRST_YEAR_IN_JULIAN_DAYS : int
+
+Returns 2378497.
+
+=cut
+
+sub FIRST_YEAR_IN_JULIAN_DAYS {
+    return 2378497;
+}
+
+=for html <a name="LAST_YEAR"></a>
+
+=head2 LAST_YEAR : int
+
+Returns 2199.
+
+=cut
+
+sub LAST_YEAR {
+    return 2199;
+}
+
+=for html <a name="LAST_DATE_IN_JULIAN_DAYS"></a>
+
+=head2 LAST_DATE_IN_JULIAN_DAYS : int
+
+Returns 1/1/2199 in julian.
+
+=cut
+
+sub LAST_DATE_IN_JULIAN_DAYS {
+    return 2524593;
 }
 
 =for html <a name="SECONDS_IN_DAY"></a>
@@ -104,6 +168,20 @@ sub UNIX_EPOCH_IN_JULIAN_DAYS {
 #=IMPORTS
 use Bivio::TypeError;
 
+#=VARIABLES
+my($_MIN) = FIRST_YEAR_IN_JULIAN_DAYS().' 0';
+my($_MAX) = LAST_DATE_IN_JULIAN_DAYS().' '.(SECONDS_IN_DAY() - 1);
+# Is this year (- FIRST_YEAR) a leap year?  Returns 0 or 1.
+my(@_IS_LEAP_YEAR);
+# First index is "is_leap_year", next is month - 1.
+# Returns days in month and days in year up to month.
+my(@_MONTH_DAYS, @_MONTH_BASE);
+# Index is year - FIRST_YEAR.  Returns number of days up to this year.
+my(@_YEAR_BASE);
+my($_TIME_SUFFIX) = ' '.DEFAULT_TIME();
+my($_DATE_PREFIX) = FIRST_YEAR_IN_JULIAN_DAYS().' ';
+_initialize();
+
 =head1 METHODS
 
 =cut
@@ -136,12 +214,102 @@ sub can_be_positive {
 
 =head2 static can_be_zero : boolean
 
-Returns true.
+Returns false.
 
 =cut
 
 sub can_be_zero {
-    return 1;
+    return 0;
+}
+
+=for html <a name="compare"></a>
+
+=head2 compare(string left, string right) : int
+
+Returns 1 if I<left> is greater than I<right>.
+Returns 0 if I<left> is equal to I<right>.
+Returns -1 if I<left> is less than I<right>.
+
+=cut
+
+sub compare {
+    my(undef, $left, $right) = @_;
+    my($ld, $lt) = split(/\s+/, $left);
+    my($rd, $rt) = split(/\s+/, $right);
+    return 1 if $ld > $rd;
+    return -1 if $ld < $rd;
+    return 1 if $lt > $rt;
+    return -1 if $lt < $rt;
+    return 0;
+}
+
+=for html <a name="date_from_parts"></a>
+
+=head2 date_from_parts(int mday, int mon, int year) : array
+
+Returns the date/time value comprising the parts.  If there is an
+error converting, returns undef and L<Bivio::TypeError|Bivio::TypeError>.
+
+=cut
+
+sub date_from_parts {
+    my(undef, $mday, $mon, $year) = @_;
+    return (undef, Bivio::TypeError::YEAR_RANGE())
+	    unless FIRST_YEAR() <= $year && $year <= LAST_YEAR();
+    return (undef, Bivio::TypeError::MONTH()) unless 1 <= $mon && $mon <= 12;
+    $mon--;
+    $year -= Bivio::Type::DateTime::FIRST_YEAR();
+    my($ly) = $_IS_LEAP_YEAR[$year];
+    return (undef, Bivio::TypeError::DAY_OF_MONTH())
+	    unless 1 <= $mday && $mday <= $_MONTH_DAYS[$ly]->[$mon];
+    return ($_YEAR_BASE[$year] + $_MONTH_BASE[$ly]->[$mon] + --$mday)
+	    .$_TIME_SUFFIX;
+}
+
+=for html <a name="from_unix"></a>
+
+=head2 from_unix(int unix_time) : string
+
+Returns date/time for I<unix_time>.
+
+=cut
+
+sub from_unix {
+    my(undef, $unix_time) = @_;
+    return undef unless defined($unix_time);
+    my($s) = int($unix_time % SECONDS_IN_DAY() + 0.5);
+    my($j) = int(($unix_time - $s)/SECONDS_IN_DAY() + 0.5)
+	    + UNIX_EPOCH_IN_JULIAN_DAYS();
+    return $j . ' ' . $s;
+}
+
+=for html <a name="now"></a>
+
+=head2 now() : string
+
+Returns date/time for now.
+
+=cut
+
+sub now {
+    return from_unix(__PACKAGE__, time);
+}
+
+=for html <a name="date_from_parts"></a>
+
+=head2 time_from_parts(int sec, int min, int hour) : array
+
+Returns the date/time value comprising the parts.  If there is an
+error converting, returns undef and L<Bivio::TypeError|Bivio::TypeError>.
+
+=cut
+
+sub time_from_parts {
+    my(undef, $sec, $min, $hour) = @_;
+    return (undef, Bivio::TypeError::HOUR) if $hour > 23 || $hour < 0;
+    return (undef, Bivio::TypeError::MINUTE) if $min > 59 || $min < 0;
+    return (undef, Bivio::TypeError::SECOND) if $sec > 59 || $sec < 0;
+    return $_DATE_PREFIX.(($hour * 60 + $min) * 60 + $sec);
 }
 
 =for html <a name="from_literal"></a>
@@ -157,31 +325,14 @@ sub from_literal {
     return undef unless defined($value) && $value =~ /\S/;
     # Get rid of all blanks to be nice to user
     return (undef, Bivio::TypeError::DATE_TIME())
-	    unless $value =~ /^\s*(\d+)\s*$/;
-    $value = $1;
-    # Get rid of leading zeroes for range check
-    $value =~ s/^0+/0/g;
+	    unless $value =~ /^\s*(\d+)\s+(\d+)\s*$/;
+    $value = "$1 $2";
     # Compare as strings for date range, because don't want integer
     # conversion to wrap
-    return (undef, Bivio::TypeError::DATE_RANGE())
-	    unless length($value) < $proto->get_width
-		    || $value le $proto->get_max;
-    return int($value);
-}
-
-=for html <a name="from_sql_column"></a>
-
-=head2 from_sql_column(string result) : string
-
-Converts the string in 'J SSSSS' form to a unix time (integer).
-
-=cut
-
-sub from_sql_column {
-    my(undef, $result) = @_;
-    return undef unless defined($result);
-    my($j, $s) = split(/ /, $result);
-    return ($j - UNIX_EPOCH_IN_JULIAN_DAYS()) * SECONDS_IN_DAY() + $s;
+#    return (undef, Bivio::TypeError::NUMBER_RANGE())
+#	    unless length($value) < $proto->get_width
+#		    || $value le $proto->get_max;
+    return $value;
 }
 
 =for html <a name="from_sql_value"></a>
@@ -211,69 +362,95 @@ sub get_decimals {
 
 =for html <a name="get_max"></a>
 
-=head2 get_max() : int
+=head2 get_max() : string
 
-Maximum date: Jan 19 03:14:07 2038 GMT.
+Maximum date: 12/31/2199 23:59:59
 
 =cut
 
 sub get_max {
-    return 0x7fffffff;
+    return $_MAX;
 }
 
 =for html <a name="get_min"></a>
 
-=head2 get_min() : int
+=head2 get_min() : string
 
-Returns 0.
+Returns 1/1/1800 0:0:0.
 
 =cut
 
 sub get_min {
-    return 0;
-}
-
-=for html <a name="get_precision"></a>
-
-=head2 static get_precision : int
-
-Maximum number of digits in a value of this type.
-
-=cut
-
-sub get_precision {
-    return 10;
+    return $_MIN;
 }
 
 =for html <a name="get_width"></a>
 
 =head2 static get_width : int
 
-Returns 10.  This is the largest string representation of the maximum
-perl integer (2^31).
+Returns 13.
 
 =cut
 
 sub get_width {
-    return 10;
+    return 13;
 }
 
-=for html <a name="to_sql_param"></a>
+=for html <a name="to_parts"></a>
 
-=head2 to_sql_param(string param_value) : string
+=head2 to_parts(string value) : array
 
-Returns string form of unix time (integer) which is acceptable
-to a positional parameter generated by L<to_sql_value|"to_sql_value">.
+Returns the date/time in parts in the same order as C<gmtime>.
+
+Handles BOTH unix and date/time formats (for convenience).
 
 =cut
 
-sub to_sql_param {
-    my(undef, $param_value) = @_;
-    return undef unless defined($param_value);
-    my($s) = $param_value % SECONDS_IN_DAY();
-    my($j) = int($param_value / SECONDS_IN_DAY())
-	    + UNIX_EPOCH_IN_JULIAN_DAYS();
-    return $j . ' ' . $s;
+sub to_parts {
+    my(undef, $value) = @_;
+    my($date, $time) = split(/\s+/, $value);
+
+    # Unix time doesn't have a "$time" component
+    unless (defined($time)) {
+	my($sec, $min, $hour, $mday, $mon, $year) = localtime($value);
+	$year += 1900;
+	$mon++;
+	return ($sec, $min, $hour, $mday, $mon, $year);
+    }
+
+    # Parse time component
+    my($sec) = int($time % 60 + 0.5);
+    $time = int(($time - $sec)/ 60 + 0.5);
+    my($min) = int($time % 60 + 0.5);
+    my($hour) = int(($time - $min)/ 60 + 0.5);
+
+    # Search for $date in julian tables
+    my($exact, $i) = Bivio::Util::bsearch_numeric($date, \@_YEAR_BASE);
+    my($year) = FIRST_YEAR() + $i;
+    return ($sec, $min, $hour, 1, 1, $year) if $exact;
+
+    # Make sure within range
+    if ($i == 0) {
+	Carp::croak("$value: time less than first year")
+		    if FIRST_YEAR_IN_JULIAN_DAYS > $date;
+    }
+    elsif ($i >= $#_YEAR_BASE) {
+	Carp::croak("$value: time greater than last year")
+		    if LAST_DATE_IN_JULIAN_DAYS() < $date;
+    }
+
+    # Adjust year if base is after $date
+    $year--, $i-- if $_YEAR_BASE[$i] > $date;
+    $date -= $_YEAR_BASE[$i];
+    my($month_base) = $_MONTH_BASE[$_IS_LEAP_YEAR[$i]];
+
+    # Search for month (always in range)
+    ($exact, $i) = Bivio::Util::bsearch_numeric($date, $month_base);
+    # Adjust month if base is after $date
+    $i-- if $month_base->[$i] > $date;
+    my($mon) = $i + 1;
+    my($mday) = $date - $month_base->[$i] + 1;
+    return ($sec, $min, $hour, $mday, $mon, $year);
 }
 
 =for html <a name="to_sql_value"></a>
@@ -290,6 +467,38 @@ sub to_sql_value {
 }
 
 #=PRIVATE METHODS
+
+# _initialize()
+#
+# Initializes year and month tables.
+#
+sub _initialize {
+    # 0th index is non-leap year
+    @_MONTH_DAYS = [(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)];
+    # 1th index is leap-year
+    $_MONTH_DAYS[1] = [@{$_MONTH_DAYS[0]}];
+    $_MONTH_DAYS[1]->[1] = 29;
+
+    # Create month bases from month days
+    foreach my $ly (0..1) {
+	$_MONTH_BASE[$ly] = [0];
+	foreach my $m (1..11) {
+	    $_MONTH_BASE[$ly]->[$m] = $_MONTH_BASE[$ly]->[$m-1]
+		    + $_MONTH_DAYS[$ly]->[$m-1];
+	}
+    }
+    # 1800 is a leap year and is julian 2378497
+    $_IS_LEAP_YEAR[0] = 0;
+    $_YEAR_BASE[0] = Bivio::Type::DateTime::FIRST_YEAR_IN_JULIAN_DAYS();
+    foreach my $y (Bivio::Type::DateTime::FIRST_YEAR()+1
+	    ..Bivio::Type::DateTime::LAST_YEAR()) {
+	my($yy) = $y - Bivio::Type::DateTime::FIRST_YEAR();
+	$_IS_LEAP_YEAR[$yy] = ($y % 4 == 0 && ($y % 100 != 0 || $y == 2000))
+		? 1 : 0;
+	$_YEAR_BASE[$yy] = $_YEAR_BASE[$yy-1]
+		+ ($_IS_LEAP_YEAR[$yy-1] ? 366 : 365);
+    }
+}
 
 =head1 COPYRIGHT
 
--- Type/t/DateTime.t
+++ Type/t/DateTime.t
@@ -17,27 +17,39 @@ print "ok 1\n";
 
 my($T) = 2;
 
+sub make_date {
+    return shift(@_).' '.Bivio::Type::DateTime::DEFAULT_TIME();
+}
+sub make_time {
+    return Bivio::Type::DateTime::DEFAULT_DATE().' '.shift(@_);
+}
 my(@tests) = (
     'Bivio::Type::DateTime', {
-    	get_min => 0,
-	get_max => 2147483647,
-	get_precision => 10,
-	get_width => 10,
+    	get_min => '2378497 0',
+	get_max => '2524593 86399',
+	get_precision => undef,
+	get_width => 13,
 	get_decimals => 0,
-	can_be_zero => 1,
+	can_be_zero => 0,
 	can_be_positive => 1,
 	can_be_negative => 0,
 	from_literal => [
 	    undef, undef,
-	    '9' => 9,
-	    '1000000000' => 1000000000,
+	    '2378497 9' => '2378497 9',
 	    '-9' => undef,
+	],
+	to_parts => [
+	    Bivio::Type::DateTime->get_min => '0 0 0 1 1 1800',
+	    Bivio::Type::DateTime->get_max => '59 59 23 31 12 2199',
+	    '2440588 0' => '0 0 0 1 1 1970',
+	    '2441377 0' => '0 0 0 29 2 1972',
+	    '2451604 47593' => '13 13 13 29 2 2000',
 	],
     },
     'Bivio::Type::Date', {
-    	get_min => Bivio::Type::DateTime::DEFAULT_TIME(),
-	get_max => 2147472000 + Bivio::Type::DateTime::DEFAULT_TIME(),
-	get_precision => 10,
+    	get_min => '2378497 '.Bivio::Type::DateTime::DEFAULT_TIME(),
+	get_max => (2524594 - 1).' '.Bivio::Type::DateTime::DEFAULT_TIME(),
+	get_precision => undef,
 	get_width => 10,
 	get_decimals => 0,
 	can_be_zero => 0,
@@ -45,39 +57,40 @@ my(@tests) = (
 	can_be_negative => 0,
 	from_literal => [
 	    undef, undef,
-	    '1/1/1970' => Bivio::Type::DateTime::DEFAULT_TIME(),
+	    '1/1/1800' => make_date(2378497),
 	    '1/1/1970 x' => undef,
-	    '1/2/1970' => Bivio::Type::DateTime::SECONDS_IN_DAY()
-	        + Bivio::Type::DateTime::DEFAULT_TIME(),
-	    '12/31/2037' => 2145830400
-	        + Bivio::Type::DateTime::DEFAULT_TIME(),
-	    '12/32/2037' => undef,
-	    '0/1/2037' => undef,
-	    '1/1/2038' => undef,
-	    '1/1/1969' => undef,
+	    '1/1/1850' => make_date(2396759),
+	    '1/2/1800' => make_date(2378498),
+	    '1/1/1900' => make_date(2415021),
+	    '1/1/1970' => make_date(
+		    Bivio::Type::DateTime::UNIX_EPOCH_IN_JULIAN_DAYS()),
+	    '1/1/1900' => make_date(2415021),
+	    '1/1/2000' => make_date(2451545),
+	    '1/1/2100' => make_date(2488070),
+	    '12/31/2199' => make_date(2524593),
 	],
     },
     'Bivio::Type::Time', {
-    	get_min => 0,
-	get_max => (23 * 60 + 59) * 60 + 59,
-	get_precision => 5,
+    	get_min => '2378497 0',
+	get_max => '2378497 '.((23 * 60 + 59) * 60 + 59),
+	get_precision => undef,
 	get_width => 13,
 	get_decimals => 0,
-	can_be_zero => 1,
+	can_be_zero => 0,
 	can_be_positive => 1,
 	can_be_negative => 0,
 	from_literal => [
 	    undef, undef,
-	    '1:1:1' => (1 * 60 + 1) * 60 + 1,
-	    '24:0:0' => 0,
+	    '1:1:1' => make_time((1 * 60 + 1) * 60 + 1),
+	    '24:0:0' => make_time(0),
 	    '24:0:0 x' => undef,
 	    '24:0:0 ax' => undef,
 	    '24:0:1' => undef,
 	    '24:1:0' => undef,
-	    '12:59:0 p.m.' => (12 * 60 + 59) * 60 + 0,
-	    '12:59:0  a' => (0 * 60 + 59) * 60 + 0,
-	    '1:0:0  a' => (1 * 60 + 0) * 60 + 0,
-	    '1:0:1  p' => (13 * 60 + 0) * 60 + 1,
+	    '12:59:0 p.m.' => make_time((12 * 60 + 59) * 60 + 0),
+	    '12:59:0  a' => make_time((0 * 60 + 59) * 60 + 0),
+	    '1:0:0  a' => make_time((1 * 60 + 0) * 60 + 0),
+	    '1:0:1  p' => make_time((13 * 60 + 0) * 60 + 1),
 	],
     },
 );
@@ -100,7 +113,9 @@ while (@tests) {
 
 sub t {
     my($class, $method, $case, $expected) = @_;
-    my($actual, $error) = $class->$method($case);
+    my($actual, $error) =  $method eq 'to_parts'
+	    ? join(' ', $class->$method($case))
+		    : $class->$method($case);
     (print "ok ", $T++, "\n"), return if defined($actual) == defined($expected)
 	    && (!defined($actual) || $actual eq $expected);
     print "not ok ", $T++,  " $class\->$method\(",
