5321e248b608e4e462f80180cdba97884609e765
Rob Nagler <github@q33.us>
1999-12-05 04:49:46 +0000

    Added die_actions.

--- Agent/Task.pm
+++ Agent/Task.pm
@@ -35,6 +35,11 @@ The following fields are returned by L<get|"get">:
 
 The task_id to go to in other cases. Not required.
 
+=item die_actions
+
+The map of die codes (any enums, actually) to tasks executed when
+the die code is encountered for this task.
+
 =item form_model
 
 The first form model in I<items> or C<undef>.
@@ -45,7 +50,9 @@ L<Bivio::Agent::TaskId|Bivio::Agent::TaskId> for this task.
 
 =item items
 
-A list of classes which have an C<execute> method.
+A list of actions.  An action is the tuple (singleton instance,
+method name).  When the task is executed, the methods are
+called on the singletons.
 
 =item next
 
@@ -65,6 +72,8 @@ L<Bivio::Auth::RealmType|Bivio::Auth::RealmType> for this task.
 =cut
 
 #=IMPORTS
+use Bivio::IO::Alert;
+use Bivio::IO::Trace;
 use Bivio::Agent::TaskId;
 use Bivio::Collection::SingletonMap;
 use Bivio::DieCode;
@@ -74,9 +83,15 @@ use Carp ();
 
 
 #=VARIABLES
+use vars ('$_TRACE');
+Bivio::IO::Trace->register;
 my($_PACKAGE) = __PACKAGE__;
 my(%_ID_TO_TASK) = ();
 my($_INITIALIZED);
+my(%_REDIRECT_DIE_CODES) = (
+    Bivio::DieCode::CLIENT_REDIRECT_TASK() => 1,
+    Bivio::DieCode::SERVER_REDIRECT_TASK() => 1,
+);
 
 =head1 FACTORIES
 
@@ -89,18 +104,46 @@ my($_INITIALIZED);
 Creates a new task for I<id> with I<perm> and I<realm_type>.
 A task must not already be
 bound to the I<id>.   The rest of the arguments are
-items to be executed (in order).
+items to be executed (in order) or mapped.  An executable item must be a
+class with an C<execute> method or of the form C<class-E<gt>method>.
+
+A mapping item is of the form I<name>=I<action>, where I<name>
+and I<action> are mapped as follows:
+
+=over 4
+
+=item next
+
+identifies "OK" task of a form.  All tasks which have an
+L<Bivio::Biz::FormModel|Bivio::Biz::FormModel> as an executable
+item, must have a I<next>.
+
+=item cancel
+
+the "Cancel" task of a form.  If not specified, defaults to
+the I<next> task.
+
+=item I<DIE_CODE>
+
+The name of a L<Bivio::DieCode|Bivio::DieCode> or a fully
+specified enum, e.g. C<Bivio::TypeError::EXISTS>.  The action
+will be executed if this enum is thrown.
+
+=back
 
 =cut
 
 sub new {
     my($proto, $id, $realm_type, $perm, @items) = @_;
     my($self) = &Bivio::UNIVERSAL::new($proto);
+
+    # Validate $id
     die("id invalid") unless $id->isa('Bivio::Agent::TaskId');
     die("realm_type invalid")
 	    unless $realm_type->isa('Bivio::Auth::RealmType');
     die($id->as_string, ': id already defined') if $_ID_TO_TASK{$id};
     my($i, $next, @new_items);
+
     # If there is an error, we'll be caching instances in one of the
     # hashes which may never be used.  Unlikely we'll be continuing after
     # the error anyway...
@@ -108,38 +151,36 @@ sub new {
 	id => $id,
 	realm_type => $realm_type,
 	permission_set => $perm,
+	die_actions => {},
+	form_model => undef,
     };
-    my($have_form) = 0;
     foreach $i (@items) {
-	if ($i =~ /^(next)=(\w+)$/) {
-	    $fields->{$1} = Bivio::Agent::TaskId->from_any($2);
+	if ($i =~ /=/) {
+	    # Map item
+	    _parse_map_item($fields, split(/=/, $i, 2));
+	    next;
 	}
-	elsif ($i =~ /^(cancel)=(\w+)$/) {
-	    $fields->{$1} = Bivio::Agent::TaskId->from_any($2);
-	}
-	else {
-	    my($class, $method) = split(/->/, $i, 2);
-	    my($c) = Bivio::Collection::SingletonMap->get($class);
-	    $method ||= 'execute';
-	    Carp::croak($i, ": can't be executed (missing $method method)")
-			unless $c->can($method);
-	    if ($c->isa('Bivio::Biz::FormModel')) {
-		$fields->{form_model} = $class
-			unless $fields->{form_model};
-		$have_form++;
-	    }
-	    push(@new_items, [$c, $method]);
-	}
+
+	# Executable item
+	my($class, $method) = split(/->/, $i, 2);
+	my($c) = Bivio::Collection::SingletonMap->get($class);
+	$method ||= 'execute';
+	Carp::croak($i, ": can't be executed (missing $method method)")
+		unless $c->can($method);
+	$fields->{form_model} = $class
+		unless $fields->{form_model}
+			|| !$c->isa('Bivio::Biz::FormModel');
+	push(@new_items, [$c, $method]);
     }
-    if ($have_form) {
+
+    # Set form
+    if ($fields->{form_model}) {
 	Carp::croak($id->as_string, ": FormModels require \"next=\" item")
-		    if !$fields->{next};
+		    unless $fields->{next};
 	# default cancel to next unless present
 	$fields->{cancel} = $fields->{next} unless $fields->{cancel};
     }
-    else {
-	$fields->{form_model} = undef;
-    }
+
     # If there is an error, we'll be caching instances in one of the
     # hashes which may never be used.  Unlikely we'll be continuing after
     # the error anyway...
@@ -212,30 +253,48 @@ Returns the task associated with the id.
 
 sub get_by_id {
     my(undef, $id) = @_;
-    Carp::croak($id->as_string, ": no task associated with id")
+    Bivio::IO::Alert->die($id, ": no task associated with id")
 	    unless $_ID_TO_TASK{$id};
     return $_ID_TO_TASK{$id};
 }
 
 =for html <a name="handle_die"></a>
 
-=head2 handle_die(Bivio::Die die)
+=head2 static handle_die(Bivio::Die die)
 
 Something happened while executing a request, so we have to rollback
-and discard the mail queue unless is a CLIENT_REDIRECT_TASK.
+and discard the mail queue unless is a <tt>CLIENT_REDIRECT_TASK</tt>
+or <tt>SERVER_REDIRECT_TASK</tt>.
+
+If I<proto> is a reference which can map the I<die> code in
+one of its I<die_actions> (cannot be redirect code).
+The die code is converted to <tt>SERVER_REDIRECT_TASK</tt>
+with the mapped die_action set as its I<task_id> attribute.
 
 =cut
 
 sub handle_die {
     my($proto, $die) = @_;
     my($die_code) = $die->get('code');
-    if ($die_code == Bivio::DieCode::CLIENT_REDIRECT_TASK()) {
+    if ($_REDIRECT_DIE_CODES{$die_code}) {
 	# commit redirects: current task is completed
 	_commit();
+	return;
     }
-    else {
-	$proto->rollback;
-    }
+
+    # Some type of unhandled error.  Rollback and check die_actions
+    $proto->rollback;
+    return unless ref($proto);
+
+    # Mapped?
+    my($new_task_id) = $proto->{$_PACKAGE}->{die_actions}->{$die_code};
+    return unless defined($new_task_id);
+
+    # Redirected
+    # This is enough tracing, because the dispatcher describes the transition
+    _trace('mapped server redirect from ', $die_code) if $_TRACE;
+    $die->set_code(Bivio::DieCode::SERVER_REDIRECT_TASK(),
+	    task_id => $new_task_id);
     return;
 }
 
@@ -304,6 +363,35 @@ sub _commit {
     Bivio::Mail::Common->send_queued_messages;
 #TODO: Garbage collect state that doesn't agree with SQL DB
     # Note: rollback is in handle_die
+    return;
+}
+
+# _parse_map_item(hash_ref fields, string cause, string action)
+#
+# Parses a new map item for this task.
+#
+sub _parse_map_item {
+    my($fields, $cause, $action) = @_;
+    $action = Bivio::Agent::TaskId->from_any($action);
+    if ($cause =~ /^(?:next|cancel)$/) {
+	# Special cases (non-enums)
+	$fields->{$cause} = $action;
+    }
+    elsif ($cause =~ /(.+)::(.+)/) {
+	# Fully specified enum
+	my($class, $method) = ($1, $2);
+	Carp::croak("$cause: not an enum (", $fields->{id}->as_string, ')')
+		    unless UNIVERSAL::isa($class, 'Bivio::Type::Enum');
+	$cause = $class->from_name($method);
+    }
+    else {
+	# Must be a DieCode
+	$cause = Bivio::DieCode->from_name($cause);
+    }
+    Carp::croak($cause->get_name, ': cannot be a mapped item (',
+	    $fields->{id}->as_string, ')')
+		if $_REDIRECT_DIE_CODES{$cause};
+    $fields->{die_actions}->{$cause} = $action;
     return;
 }
 
