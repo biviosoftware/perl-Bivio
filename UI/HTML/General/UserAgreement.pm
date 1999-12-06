0fb7e1c467f886e5f2b610bb7859c0deebf67a69
Rob Nagler <github@q33.us>
1999-12-06 06:35:23 +0000

    Made DescriptivePage.  Reads in text once.

--- UI/HTML/General/UserAgreement.pm
+++ UI/HTML/General/UserAgreement.pm
@@ -17,12 +17,12 @@ Bivio::UI::HTML::General::UserAgreement - renders the user agreement
 
 =head1 EXTENDS
 
-L<Bivio::UI::HTML::Widget::Template>
+L<Bivio::UI::HTML::DescriptivePage>
 
 =cut
 
-use Bivio::UI::HTML::Widget::Template;
-@Bivio::UI::HTML::General::UserAgreement::ISA = ('Bivio::UI::HTML::Widget::Template');
+use Bivio::UI::HTML::DescriptivePage;
+@Bivio::UI::HTML::General::UserAgreement::ISA = ('Bivio::UI::HTML::DescriptivePage');
 
 =head1 DESCRIPTION
 
@@ -31,48 +31,81 @@ with an included form.
 
 =cut
 
+
+=head1 CONSTANTS
+
+=cut
+
+=for html <a name="PAGE_HEADING"></a>
+
+=head2 PAGE_HEADING : string
+
+Returns 'Terms of Service'
+
+=cut
+
+sub PAGE_HEADING {
+    return 'Terms of Service';
+}
+
+=for html <a name="PAGE_TOPIC"></a>
+
+=head2 PAGE_TOPIC : string
+
+Returns 'Terms of Service'
+
+=cut
+
+sub PAGE_TOPIC {
+    return PAGE_HEADING();
+}
+
 #=IMPORTS
-use Bivio::UI::HTML::Widget::StandardSubmit;
-use Bivio::UI::HTML::Widget::Form;
+use Bivio::Agent::HTTP::Location;
+use Bivio::Agent::Request;
+use Bivio::Agent::TaskId;
+use Bivio::Biz::Action::HTTPDocument;
+use Bivio::UI::HTML::DescriptivePageForm;
 
 #=VARIABLES
-
-
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new() : Bivio::UI::HTML::General::UserAgreement
-
-Creates and initializes this widget
-
-=cut
-
-sub new {
-    my($self) = Bivio::UI::HTML::Widget::Template::new(@_);
-    $self->put(
-	uri => ['->format_stateless_uri',
-	    Bivio::Agent::TaskId::USER_AGREEMENT_TEXT()],
-	map => {
-	    FORM => Bivio::UI::HTML::Widget::Form->new({
-		form_model => ['Bivio::Biz::Model::UserAgreementForm'],
-		value => Bivio::UI::HTML::Widget::StandardSubmit->new({
-		    align => 'center',
-		    separation => 100,
-		}),
-	    }),
-	},
-    );
-    # This is the top level widget
-    $self->initialize;
-    return $self;
-}
+my($_P) = __PACKAGE__;
 
 =head1 METHODS
 
 =cut
+
+=for html <a name="create_content"></a>
+
+=head2 create_content()
+
+Returns the form.
+
+=cut
+
+sub create_content {
+    my($self) = @_;
+    my($req) = Bivio::Agent::Request->new();
+
+    # Read in terms of service
+    my($fh) = Bivio::Biz::Action::HTTPDocument->open($req,
+	    Bivio::Agent::HTTP::Location->format(
+		    Bivio::Agent::TaskId::USER_AGREEMENT_TEXT()));
+    local($_);
+    0 while defined($_ = <$fh>) && !/<td>.*Terms of Service/;
+    my($text) = '';
+    $text .= $_ while defined($_ = <$fh>) && !/^\<\!--FORM--/;
+    close($fh);
+    die('unable to open hm/user.html') unless $text;
+
+    my($form) = Bivio::UI::HTML::DescriptivePageForm->new({
+	form_class => 'Bivio::Biz::Model::UserAgreementForm',
+	header => $_P->join([$text]),
+    });
+
+    # No fields, just header
+    $form->put(value => $form->create_fields([]));
+    return $form;
+}
 
 #=PRIVATE METHODS
 
