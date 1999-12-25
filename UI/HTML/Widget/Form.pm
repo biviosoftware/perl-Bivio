d5abfa9ada8e53265af5ce27165218eba7f51ed3
Rob Nagler <github@q33.us>
1999-12-25 05:19:16 +0000

    Deprecated form_model in favor of form_class FileUploadNameHandler: Added form_name so can reference forms easily

--- UI/HTML/Widget/Form.pm
+++ UI/HTML/Widget/Form.pm
@@ -54,14 +54,25 @@ used as the C<ACTION> attribute of the C<FORM> tag.
 Will be passed to L<Bivio::Util::escape_html|Bivio::Util/"escape_html">
 before rendering.
 
+=item form_class : Bivio::Biz::FormModel [*computed*] (inherited)
+
+This value is computed from I<form_model> if it can be.  It
+also be set.
+
 =item form_method : string [POST] (inherited)
 
 The value to be passed to the C<METHOD> attribute of the C<FORM> tag.
 
 =item form_model : array_ref (required, inherited)
 
-Which form are we dealing with.  Will call
-L<Bivio::Biz::FormModel::get_hidden_field_values|Bivio::Biz::FormModel/"get_hidden_field_values">
+B<DEPRECATED>. Which form are we dealing with.
+Use I<form_class>.
+
+=item form_name : string [fnNNN] (inherited)
+
+Name of the form which can be used within JavaScript.  Set dynamically
+to C<fn>I<NNN> where I<NNN> is globally assigned starting at 1.
+The value is set on the I<self>, so it can be used by fields.
 
 =item value : Bivio::UI::Widget (required)
 
@@ -80,6 +91,7 @@ use Bivio::UI::HTML::Widget::TimezoneField;
 
 #=VARIABLES
 my($_PACKAGE) = __PACKAGE__;
+my($_FORM_NAME_INDEX) = 0;
 
 =head1 FACTORIES
 
@@ -115,23 +127,61 @@ sub initialize {
     my($self) = @_;
     my($fields) = $self->{$_PACKAGE};
     return if $fields->{prefix};
+
+    # Compute form_class from form_model or vice-versa
+    my($class) = $self->ancestral_get('form_class', undef);
+    my($model) = $self->ancestral_get('form_model', undef);
+    if ($class && $model) {
+	# fall through
+    }
+    elsif ($model) {
+	# DEPRECATED
+	$class = $model->[0];
+	$self->put(form_class => $class);
+    }
+    elsif ($class) {
+	# DEPRECATED
+	$model = [$class];
+	$self->put(form_model => $model);
+    }
+    else {
+	die('form_class not set');
+    }
+    die($class, ': invalid or not set form_class')
+	    unless UNIVERSAL::isa($class, 'Bivio::Biz::FormModel');
+    $fields->{class} = $class;
+
+    # Compute form_name
+    my($name) = $self->ancestral_get('form_name', undef);
+    if ($name) {
+	# Should be at least two chars starting with a letter
+	die($name, ': invalid form_name') unless $name =~ /^[a-z]\w+$/i;
+    }
+    else {
+	$name = 'fn'.$_FORM_NAME_INDEX++;
+	$self->put(form_name => $name);
+    }
+
+    # Initialize prefix
     my($p) = '<form method=';
     $p .= $self->ancestral_get('form_method', 'POST');
-    ($fields->{model}, $fields->{value}) = $self->get('form_model', 'value');
+    $p .= ' name="'.$name.'"';
     my($action) = $self->unsafe_get('action');
     $p .= ' action="';
     if (ref($action)) {
 	$fields->{action} = $action;
     }
     elsif (defined($action)) {
-	$p .= Bivio::Util::escape_html($action) . "\">\n";
+	$p .= Bivio::Util::escape_html($action);
     }
     else {
 	$fields->{action} = 1;
     }
     $fields->{prefix} = $p;
     $fields->{end_tag} = $self->get_or_default('end_tag', 1);
-    $fields->{model} = $self->get('form_model');
+
+    # Initialize renderer
+    $fields->{value} = $self->get('value');
     $fields->{value}->put(parent => $self);
     $fields->{value}->initialize;
     return;
@@ -148,6 +198,7 @@ Render the form.
 sub render {
     my($self, $source, $buffer) = @_;
     my($fields) = $self->{$_PACKAGE};
+    my($model) = $source->get_widget_value($fields->{class});
 
     # Method
     $$buffer .= $fields->{prefix};
@@ -164,14 +215,23 @@ sub render {
 #      context when you have a form to store the context in fields.
 #      Context management is hard....
 	$action =~ s/[&?]fc=\w+//;
-	$$buffer .= $action . "\">\n"
+	$$buffer .= $action;
     }
+
+    # Set encoding type if form has a file field
+    unless ($fields->{middle}) {
+	$fields->{middle} = '"';
+	$fields->{middle} .= ' enctype="multipart/form-data"'
+		if $model->get_info('file_fields');
+	$fields->{middle} .= ">\n";
+    }
+    $$buffer .= $fields->{middle};
+
     # Timezone is computed on every form
     Bivio::UI::HTML::Widget::TimezoneField->render($source, $buffer);
 
     # Hidden fields (if any)
-    my($hidden) = $source->get_widget_value(@{$fields->{model}})
-	    ->get_hidden_field_values();
+    my($hidden) = $model->get_hidden_field_values();
     while (@$hidden) {
 	# hidden fields have been converted to literal, but not  escaped.
 	$$buffer .= '<input type=hidden name='.shift(@$hidden).' value="'
