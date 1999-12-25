0d0ac47b30b10171d4357c6e3a6eb18e72f6a665
Rob Nagler <github@q33.us>
1999-12-25 05:00:43 +0000

    Added BLOB handling

--- SQL/Connection.pm
+++ SQL/Connection.pm
@@ -30,6 +30,39 @@ which modifies the database.>
 
 =cut
 
+
+=head1 CONSTANTS
+
+=cut
+
+=for html <a name="MAX_BLOB"></a>
+
+=head2 MAX_BLOB : int
+
+Maximum length of a blob.  You cannot retrieve blobs larger than this.
+You can only have one blob per record.
+
+=cut
+
+sub MAX_BLOB {
+    return 0x400_000;
+}
+
+=for html <a name="MAX_PARAMETERS"></a>
+
+=head2 MAX_PARAMETERS : int
+
+Maximum number of '?' parameters on a single statement.
+
+=cut
+
+sub MAX_PARAMETERS {
+    # This value is somewhat arbitrary, but we've tested this up to
+    # 230.  The problem is it depends on the statement complexity...
+    # Deleting 100 at a time seems like it gets the biggest impact.
+    return 100;
+}
+
 #=IMPORTS
 use Bivio::Die;
 use Bivio::DieCode;
@@ -37,6 +70,7 @@ use Bivio::Ext::DBI;
 use Bivio::IO::Trace;
 use Bivio::Util;
 use Bivio::TypeError;
+use DBD::Oracle qw(:ora_types);
 use Carp ();
 
 #=VARIABLES
@@ -58,6 +92,10 @@ my(%_ERR_TO_DIE_CODE) = (
 #	die('required value missing') if $err == 1400;
 #    die('invalid number') if $err == 1722;
 );
+# Allow for a bit larger space than maximum blob
+my($_MAX_BLOB) = int(MAX_BLOB() * 1.1);
+
+
 =head1 METHODS
 
 =cut
@@ -86,6 +124,8 @@ sub commit {
 
 =head2 execute(string sql, array_ref params, ref die)
 
+=head2 execute(string sql, array_ref params, ref die, boolean has_blob)
+
 Executes the specified statement and dies with an appropriate error
 if it fails.
 
@@ -93,10 +133,15 @@ B<NOTE: All calls must go through this
 
 I<die> must implement L<Bivio::Die::die|Bivio::Die/"die">.
 
+I<has_blob> is specified, the arguments are scanned for a scalar_ref.
+If found, the positional parameter is bound properly.  If no scalar_ref
+is found, then the BLOB is assumed to be an output parameter and
+I<LongReadLen> and I<LongTruncOk> are set accordingly.
+
 =cut
 
 sub execute {
-    my($self, $sql, $params, $die) = @_;
+    my($self, $sql, $params, $die, $has_blob) = @_;
 
     my($statement);
     Bivio::Die->eval(sub {
@@ -106,7 +151,26 @@ sub execute {
 #TODO: If do cache, then make sure not "active" when making call.
 	$statement = _get_connection()->prepare($sql);
 	# Only need a commit if there has been data modification language
-	$_NEED_COMMIT = 1 if $sql !~ /^\s*select/i;
+	my($is_select) = $sql =~ /^\s*select/i;
+	$_NEED_COMMIT = 1 unless $is_select;
+	if ($has_blob) {
+	    if ($is_select) {
+		# Returns a value
+		$statement->{LongReadLen} = $_MAX_BLOB;
+		$statement->{LongTruncOk} = 0;
+	    }
+	    else {
+		# Passing a value, possibly
+		my($i) = 1;
+		foreach my $p (@$params) {
+		    $statement->bind_param($i++, $p), next unless ref($p);
+		    # I wonder if it stores a reference or a copy?
+		    $statement->bind_param($i++,  $$p, {ora_type => ORA_BLOB});
+		}
+		# Parameters are bound, so don't pass below
+		$params = undef;
+	    }
+	}
 	ref($params) ? $statement->execute(@$params)
 		: $statement->execute();
 #TODO: need to inc when a failure
@@ -127,8 +191,8 @@ sub execute {
 	sql_params => $params,
     };
     my($die_code);
-    if ($err == 1 && $errstr =~ /unique constraint \((\w+)\.(\w+)\)/i) {
-	$die_code = _find_constraint_columns($attrs, uc($1), uc($2));
+    if ($errstr =~ /constraint \((\w+)\.(\w+)\) violated/i) {
+	$die_code = _intrepret_constraint_violation($attrs, uc($1), uc($2));
     }
     Bivio::Die->eval(sub {
 	# Clean up just in case statement is cached
@@ -199,45 +263,79 @@ sub rollback {
 
 #=PRIVATE METHODS
 
-# _find_constraint_columns(hash_ref attrs, string owner, string constraint) : Bivio::Type::Enum
+# _intrepret_constraint_violation(hash_ref attrs, string owner, string constraint) : Bivio::Type::Enum
 #
-# Will set "columns" and "table" in attrs.  Returns die code.
+# Will set "columns" and "table" in attrs.  Returns die code that is
+# appropriate for the constraint violation.
 #
-sub _find_constraint_columns {
+sub _intrepret_constraint_violation {
     my($attrs, $owner, $constraint) = @_;
     my($die_code);
+
+    # Ignore errors, die_code will be undef in this case and result in a
+    # server error
     Bivio::Die->eval(sub {
+
+	# Try to find the constraint columns
 	my($statement) = _get_connection()->prepare(<<"EOF");
 	    SELECT user_cons_columns.table_name,
 		    user_cons_columns.column_name
 	    FROM user_cons_columns, user_constraints
-	    WHERE user_constraints.constraint_name = '$constraint'
-	    AND user_constraints.owner = '$owner'
+	    WHERE user_constraints.constraint_name = ?
+	    AND user_constraints.owner = ?
 	    AND user_constraints.constraint_name
 	    = user_cons_columns.constraint_name
 EOF
-	$statement->execute();
+	$statement->execute($constraint, $owner);
 	my($row);
 	my($cols) = [];
 	my($table);
 	while ($row = $statement->fetchrow_arrayref) {
+# TODO: table must always be the same(?)
 	    $table = lc($row->[0]);
 	    push(@$cols, lc($row->[1]));
 	}
+
+	# This is an operation error, not db error.  Don't need to ping.
 	$_NEED_PING = 0;
+
+	# Found the constraint?
 	if ($table) {
+	    # Save the state for the die message
 	    $attrs->{columns} = $cols, $attrs->{table} = $table;
 	    _trace($owner, '.', $constraint, ': found ', $table, '.', $cols)
 		    if $_TRACE;
-	    $die_code = Bivio::TypeError::EXISTS();
+
+	    if (1 == $attrs->{dbi_err}) {
+		# unique constraint violated (ORA-00001)
+		$die_code = Bivio::TypeError::EXISTS();
+	    }
+	    elsif (2290 == $attrs->{dbi_err}) {
+		# check constraint violated (ORA-02290)
+		# We understand only one type of check constraint:
+		# max_* exceeded.  This will back all the way to
+		# the Task level and it will map to a different
+		# task.
+		if (int(@$cols) == 2 && grep(/max_/, @$cols)) {
+		    $die_code = Bivio::DieCode::NO_RESOURCES();
+		}
+	    }
+	    elsif (2292 == $attrs->{dbi_err}) {
+		# integrity constraint violated (ORA-02292)
+		# child record not found
+		if (int(@$cols) == 2 && grep(/max_/, @$cols)) {
+		    $die_code = Bivio::DieCode::NO_RESOURCES();
+		}
+	    }
 	}
 	else {
+	    # returns undef for die_code
 	    _trace($owner, '.', $constraint,
 		    ': constraint query returned nothing') if $_TRACE;
 	}
-	# Connection works, no need to ping
 	1;
     });
+
     _trace($owner, '.', $constraint, ':', $@) if $_TRACE && $@;
     return $die_code;
 }
