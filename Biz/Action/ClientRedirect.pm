bf760a53d591ee5eb385c8c3e246b3a8e7f14bd7
Rob Nagler <github@q33.us>
1999-12-31 17:43:51 +0000

    Added ->SOME_TASK form

--- Biz/Action/ClientRedirect.pm
+++ Biz/Action/ClientRedirect.pm
@@ -6,13 +6,14 @@ $Bivio::Biz::Action::ClientRedirect::VERSION = sprintf('%d.%02d', q$Revision$ =~
 
 =head1 NAME
 
-Bivio::Biz::Action::ClientRedirect - redirects to next or cancel tasks
+Bivio::Biz::Action::ClientRedirect - client redirect to specific task
 
 =head1 SYNOPSIS
 
     use Bivio::Biz::Action::ClientRedirect;
     Bivio::Biz::Action::ClientRedirect->execute_next($req);
     Bivio::Biz::Action::ClientRedirect->execute_cancel($req);
+    Bivio::Biz::Action::ClientRedirect->SOME_TASK($req);
 
 =cut
 
@@ -24,11 +25,20 @@ use Bivio::UNIVERSAL;
 C<Bivio::Biz::Action::ClientRedirect> redirects to the cancel or
 next task values.  There is no I<execute>, you must be explicit.
 
+You may also redirect to a specific task:
+
+    Bivio::Biz::Action::ClientRedirect->SOME_TASK($req);
+
+The subroutines are dynamically compiled from the lists of
+tasks in L<Bivio::Agent::TaskId|Bivio::Agent::TaskId>.
+
 =cut
 
 #=IMPORTS
+use Bivio::Agent::TaskId;
 
 #=VARIABLES
+_compile();
 
 =head1 METHODS
 
@@ -63,6 +73,30 @@ sub execute_next {
 }
 
 #=PRIVATE METHODS
+
+# _compile()
+#
+# Create autoredirect functions for all the tasks in TaskId.
+#
+sub _compile {
+    foreach my $t (Bivio::Agent::TaskId->get_list) {
+#TODO: Would like to not define subs for for that don't have URIs,
+#      but can't do this because Tasks is not fully initialized by
+#      the time this module has been called.  It is called during
+#      initialization and Location hasn't fully initialized either.
+#      Probably want to do two passes in Location.  First setting
+#      URIs and second initializing the tasks.
+	my($n) = $t->get_name;
+	eval(<<"EOF") || die($@);
+	    sub $n {
+                my(undef, \$req) = \@_;
+                \$req->client_redirect(Bivio::Agent::TaskId::$n());
+            }
+            1;
+EOF
+    }
+    return;
+}
 
 =head1 COPYRIGHT
 
