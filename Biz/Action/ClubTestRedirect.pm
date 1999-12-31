40072841902f03a73d9b76bcb8490d84d241fcf9
Martin Lichtin <lichtin@bivio.biz>
1999-12-31 13:15:04 +0000

    satisfy pod2html, add title desc

--- Biz/Action/ClubTestRedirect.pm
+++ Biz/Action/ClubTestRedirect.pm
@@ -6,7 +6,7 @@ $Bivio::Biz::Action::ClubTestRedirect::VERSION = sprintf('%d.%02d', q$Revision$ 
 
 =head1 NAME
 
-Bivio::Biz::Action::ClubTestRedirect - 
+Bivio::Biz::Action::ClubTestRedirect - testing club redirects
 
 =head1 SYNOPSIS
 
