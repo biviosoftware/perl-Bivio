ee0237478a85c7cd55b272ecaf8dbf5611980384
Paul Moeller <moeller@bivio.biz>
2000-01-21 18:10:16 +0000

    *** empty log message ***

--- Biz/Model/Instrument.pm
+++ Biz/Model/Instrument.pm
@@ -79,7 +79,7 @@ sub internal_initialize {
             name => ['Bivio::Type::Line',
     		Bivio::SQL::Constraint::NOT_NULL()],
             ticker_symbol => ['Bivio::Type::Name',
-    		Bivio::SQL::Constraint::NOT_NULL()],
+    		Bivio::SQL::Constraint::NONE()],
             exchange_name => ['Bivio::Type::Name',
     		Bivio::SQL::Constraint::NONE()],
             instrument_type => ['Bivio::Type::InstrumentType',
