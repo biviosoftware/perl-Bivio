44e3fee96ebd5ef2a7cdf2e52fd9b51818f134ee
yates <yates@bivio.biz>
2000-03-11 23:38:58 +0000

    Moved f1065.pdf to Bivio/UI/PDF/Form/f1065/y1999 from Bivio/UI/PDF/Form.

--- UI/PDF/Form/buildIt.sh
+++ UI/PDF/Form/buildIt.sh
@@ -4,5 +4,5 @@
 	f1065/y1999/Form.pm \
 	Bivio::UI::PDF::Form::f1065::y1999::Form \
 	Bivio::UI::PDF::Form::Form \
-	f1065.pdf \
+	f1065/y1999/f1065.pdf \
 	Bivio::UI::PDF::Form::f1065::y1999::XlatorSet
