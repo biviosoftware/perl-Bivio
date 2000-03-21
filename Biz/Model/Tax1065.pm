36f5f21f6b03d75519373a01693bdd0fcd296566
Paul Moeller <moeller@bivio.biz>
2000-03-21 00:26:29 +0000

    added field 'draft', default club's irs center using address

--- Biz/Model/Tax1065.pm
+++ Biz/Model/Tax1065.pm
@@ -31,7 +31,10 @@ C<Bivio::Biz::Model::Tax1065> IRS 1065 tax parameters
 =cut
 
 #=IMPORTS
+use Bivio::Biz::Model::Address;
 use Bivio::Type::AllocationMethod;
+use Bivio::Type::F1065IRSCenter;
+use Bivio::Type::Location;
 
 #=VARIABLES
 my($_PACKAGE) = __PACKAGE__;
@@ -66,6 +69,7 @@ sub internal_initialize {
 	    return_type => ['F1065Return', 'NOT_NULL'],
 	    irs_center => ['F1065IRSCenter', 'NOT_NULL'],
 	    allocation_method => ['AllocationMethod', 'NOT_NULL'],
+	    draft => ['Boolean', 'NOT_NULL'],
         },
 	auth_id => 'realm_id',
     };
@@ -82,10 +86,6 @@ Loads or creates the 1065 model for the previous tax year.
 sub load_or_default {
     my($self, $fiscal_end_date) = @_;
     unless ($self->unsafe_load(fiscal_end_date => $fiscal_end_date)) {
-
-#TODO: determine irs center from club address
-	my($irs_center) = Bivio::Type::F1065IRSCenter->UNKNOWN;
-
 	$self->create({
 	    realm_id => $self->get_request->get('auth_id'),
 	    fiscal_end_date => $fiscal_end_date,
@@ -98,10 +98,23 @@ sub load_or_default {
 	    foreign_account_country => undef,
 	    foreign_trust => 0,
 	    return_type => Bivio::Type::F1065Return::UNKNOWN(),
-	    irs_center => $irs_center,
+	    irs_center => Bivio::Type::F1065IRSCenter->UNKNOWN,
 	    allocation_method => Bivio::Type::AllocationMethod::TIME_BASED(),
+	    draft => 1,
 	});
     }
+
+    # determine irs center based on club address
+    if ($self->get('irs_center') == Bivio::Type::F1065IRSCenter::UNKNOWN()) {
+	# get the current realm's address
+	my($address) = Bivio::Biz::Model::Address->new($self->get_request);
+	$address->load(location => Bivio::Type::Location::HOME());
+	$self->update({
+	    irs_center => Bivio::Type::F1065IRSCenter->get_irs_center(
+		    $address->get('state'), $address->get('zip'))
+	});
+    }
+
     return;
 }
 
