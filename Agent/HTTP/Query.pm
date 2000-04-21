1e654055b9dffd48e8df268ae0866722bc9a634c
Rob Nagler <github@q33.us>
2000-04-21 15:30:46 +0000

    Added parse

--- Agent/HTTP/Query.pm
+++ Agent/HTTP/Query.pm
@@ -35,7 +35,7 @@ use Bivio::Util;
 
 =for html <a name="format"></a>
 
-=head2 format(hash_ref query) : string
+=head2 static format(hash_ref query) : string
 
 Returns the string version of the query.
 Returns C<undef> if I<query> is C<undef>.
@@ -52,6 +52,31 @@ sub format {
     }
     chop($res);
     return $res;
+}
+
+=for html <a name="parse"></a>
+
+=head2 static parse(string query) : hash_ref
+
+Returns a hash_ref for the query string.  Returns C<undef> if
+string not defined.
+
+=cut
+
+sub parse {
+    my(undef, $string) = @_;
+    # Nothing
+    return undef unless defined($string);
+
+    my(@v);
+    foreach my $v (split(/[=&]/, $string)) {
+	push(@v, Bivio::Util::unescape_uri($v));
+    }
+    return undef unless @v;
+    push(@v, '') if int(@v) % 2;
+
+    # Return the hash
+    return {@v};
 }
 
 #=PRIVATE METHODS
