10dbc207145f49c43ed1c977f0639ba857d46527
Rob Nagler <github@q33.us>
2000-05-31 09:44:03 +0000

    auth_code on invites now explicit Try to find invite of user.

--- Biz/Model/RealmInvite.pm
+++ Biz/Model/RealmInvite.pm
@@ -51,6 +51,7 @@ sub EXPIRE_DAYS {
 #=IMPORTS
 use Bivio::Agent::HTTP::Cookie;
 use Bivio::Auth::Role;
+use Bivio::Biz::Model::Email;
 use Bivio::DieCode;
 use Bivio::IO::Trace;
 use Bivio::SQL::Constraint;
@@ -71,6 +72,7 @@ my($_COOKIE_FIELD) = Bivio::Agent::HTTP::Cookie->REALM_INVITE_FIELD;
 my($_QUERY_FIELD) = 'x';
 my($_MIN_PRIMARY_ID) = Bivio::Type::PrimaryId->get_min;
 my($_MAX_INTEGER) = Bivio::Type::Integer->get_max;
+my($_SEPARATOR) = '!';
 
 =head1 METHODS
 
@@ -92,7 +94,97 @@ sub check_accept {
 
     my($self) = _check_cookie($proto, $req);
     _set_state($self, $req) if ref($self);
+
     return;
+}
+
+=for html <a name="check_accept_after_login"></a>
+
+=head2 static check_accept_after_login(Bivio::Biz::Model::LoginForm login)
+
+B<Only called by L<Bivio::Biz::Model::LoginForm|Bivio::Biz::Model::LoginForm>.>
+
+If there is an outstanding invitation on the request or in the
+cookie, do nothing.
+Check to see if the auth_user's email matches an outstanding invitation.
+If so redirect to the invite task.
+
+=cut
+
+sub check_accept_after_login {
+    my($proto, $login) = @_;
+    my($req) = $login->get_request;
+
+    # Is there an existing invite on the request?
+    my($invite) = $req->unsafe_get('Bivio::Biz::Model::RealmInvite');
+    if ($invite) {
+
+	# New User registering via an invite URL
+	if ($req->get('Bivio::Biz::Model::CreateUserForm')) {
+	    RealmInviteAcceptForm->execute($req, {});
+	    $req->client_redirect(Bivio::Agent::TaskId::CLUB_USER_NEW());
+	    # DOES NOT RETURN
+	}
+
+#TODO: What case is this?
+	_trace($invite, ': returning to task') if $_TRACE;
+	return;
+    }
+
+    # Don't do anything if already unwinding to an invite task
+    my($task) = $login->unsafe_get_context('unwind_task');
+    if ($task == Bivio::Agent::TaskId::REALM_INVITE_ACCEPT()
+	    || $task == Bivio::Agent::TaskId::CLUB_GUEST_2_MEMBER_ACCEPT()) {
+	_trace('unwinding to ', $task) if $_TRACE;
+	return;
+    }
+
+    # Find an invite by the user's email
+    my($email) = Bivio::Biz::Model::Email->new($req);
+    my($auth_user) = $req->get('auth_user');
+    my($auth_user_id) = $auth_user->get('realm_id');
+    $invite = Bivio::Biz::Model::RealmInvite->new($req);
+
+    # Find by oldest invitation which matches email
+    $email->unauth_load_or_die(realm_id => $auth_user_id);
+    my($it) = $invite->unauth_iterate_start('creation_date_time asc',
+	    {email => $email->get('email')});
+
+    my($ok) = $invite->iterate_next_and_load($it);
+    $invite->iterate_end($it);
+
+    # Try to find by other realm_name@bivio.com email
+    unless ($ok) {
+	$it = $invite->unauth_iterate_start('creation_date_time asc',
+		{email => $auth_user->format_email});
+	$ok = $invite->iterate_next_and_load($it);
+	$invite->iterate_end($it);
+    }
+    _trace($invite, ': ok=', $ok) if $_TRACE;
+
+    # No invites?
+    return unless $ok;
+
+    # Do not use local format_query, because it includes the auth_code
+    # which we want from the user.
+    my($query) = $invite->SUPER::format_query();
+
+    # Have an invite figure out what type of invite it is
+    my($realm_user_id) = $invite->get('realm_user_id');
+    if ($realm_user_id eq $auth_user_id) {
+	# Guest 2 Member (needs a realm)
+	$req->client_redirect(
+		Bivio::Agent::TaskId::CLUB_GUEST_2_MEMBER_ACCEPT(),
+		Bivio::Auth::Realm->new($invite->get('realm_id'), $req),
+		$query, undef);
+	# DOES NOT RETURN
+    }
+
+    # Normal invitation
+    $req->client_redirect(
+	    Bivio::Agent::TaskId::REALM_INVITE_ACCEPT(),
+	    undef, $query, undef);
+    # DOES NOT RETURN
 }
 
 =for html <a name="create"></a>
@@ -147,10 +239,12 @@ sub delete_cookie {
     my($cookie) = $self->get_request->get('cookie');
     my($c) = $cookie->delete($_COOKIE_FIELD);
 
+    return unless defined($c);
+
     # Don't actually delete the cookie if the user was editing the
-    # invite list.
-    $cookie->put($_COOKIE_FIELD, $c)
-	    if defined($c) && $c ne $self->get('realm_invite_id');
+    # invite list.  User may have only submitted the id, not the auth_code.
+    my($id, $ac) = split(' ', $c);
+    $cookie->put($_COOKIE_FIELD, $c) if $c ne $self->get('realm_invite_id');
     return;
 }
 
@@ -176,9 +270,8 @@ sub execute_accept {
     # Did the user just click on the "accept" link
     my($q) = $req->unsafe_get('query');
     if ($req->unsafe_get('query')) {
-#TODO: Really want unauth_load_from_query
-	my($lq) = Bivio::SQL::ListQuery->new({%$q, auth_id => 1, count => 1},
-		$self->internal_get_sql_support, $self);
+	my($lq) = Bivio::SQL::ListQuery->unauth_new({%$q},
+		$self, $self->internal_get_sql_support);
 	my($id) = $lq->unsafe_get('this');
 
 	# User hacked the query?
@@ -191,18 +284,20 @@ sub execute_accept {
 		unless $self->unauth_load(realm_invite_id => $id->[0]);
 
 	# Compare actual and expected
-	my($actual) = $q->{$_QUERY_FIELD};
-	my($expected) = _magic($self);
-	$self->die(Bivio::DieCode::NOT_FOUND(),
-		'missing magic query field') unless $actual;
-	$self->die(Bivio::DieCode::NOT_FOUND(),
-		{actual => $actual, expected => $expected,
-		    message => 'magic field mismatch'})
-		unless $actual eq $expected;
+	my($actual) = $lq->unsafe_get($_QUERY_FIELD) || '';
+	my($expected) = $self->get_auth_code();
+	_trace('actual=', $actual, '; expected=', $expected) if $_TRACE;
+	if (defined($actual) && length($actual)) {
+	    $self->die(Bivio::DieCode::NOT_FOUND(),
+		    {actual => $actual, expected => $expected,
+			message => 'auth_code field mismatch'})
+		    unless $actual eq $expected;
+	    $req->put(realm_invite_auth_code => $actual);
+	}
 
-	# Loaded ok, so set in the cookie
+	# Loaded ok, so set in the cookie with $actual, if it was passed in
 	$req->get('cookie')->put($_COOKIE_FIELD,
-		$self->get('realm_invite_id'));
+		$self->get('realm_invite_id').$_SEPARATOR.$actual);
     }
     else {
 	# If there is a valid cookie, try to load it
@@ -222,13 +317,44 @@ sub execute_accept {
 
 =head2 format_query() : string
 
-Formats the query string with I<this> and I<magic>.
+Formats the query string with I<this> and I<auth_code>.
 
 =cut
 
 sub format_query {
     my($self) = @_;
-    return $self->SUPER::format_query().'&'.$_QUERY_FIELD.'='._magic($self);
+    return $self->SUPER::format_query().'&'.$_QUERY_FIELD
+	    .'='.$self->get_auth_code;
+}
+
+=for html <a name="get_auth_code"></a>
+
+=head2 get_auth_code() : Bivio::Type::Integer
+
+=head2 static get_auth_code(Bivio::Biz::ListModel list_model, string model_prefix) : boolean
+
+Returns the authorization code for this invite.
+
+=cut
+
+sub get_auth_code {
+    my($self, $m, $p) = @_;
+    $p ||= '';
+    $m ||= $self;
+
+    # RealmInviteList assumes that only "get" is called.
+    my($id, $realm_id, $date_time) = $m->get(
+	    $p.'realm_invite_id', $p.'realm_id', $p.'creation_date_time');
+
+    # None of this is critical except that the algorithm can't change
+    # without changing the version.  We don't really take advantage
+    # of the internals.
+    $id = int($id/$_MIN_PRIMARY_ID % $_MAX_INTEGER);
+    $realm_id = int($realm_id/$_MIN_PRIMARY_ID % $_MAX_INTEGER);
+    my($date, $time) = split(/\s+/, $date_time);
+
+    # XOR all these values
+    return $id ^ $realm_id ^ $date ^ $time;
 }
 
 =for html <a name="internal_initialize"></a>
@@ -292,35 +418,26 @@ sub _check_cookie {
     # Coming in from another class.  Can we load from the cookie?
     my($cookie) = $req->get('cookie');
     my($c) = $cookie->unsafe_get($_COOKIE_FIELD);
-    # Nope, fall through to normal business logic
+    # Nope, return; else fall through to normal business logic
     return undef unless $c;
 
+    my($id, $ac) = split(/$_SEPARATOR/o, $c);
+
     my($self) = $proto->new($req);
-    return $self if $self->unauth_load(realm_invite_id => $c);
+    if ($self->unauth_load(realm_invite_id => $id)) {
+#TODO: DELETE THIS LINE after 6/15/2000.
+	$ac = $self->get_auth_code() unless $c =~ /$_SEPARATOR/o;
+
+	_trace('actual=', $ac, '; expected=', $self->get_auth_code) if $_TRACE;
+	# If the auth_code was in the cookie, set it on the request.
+	if (defined($ac) && $ac eq $self->get_auth_code()) {
+	    $req->put(realm_invite_auth_code => $ac);
+	}
+	return $self;
+    }
     _trace($c, ': not found, deleting cookie') if $_TRACE;
     $cookie->delete($_COOKIE_FIELD);
     return $c;
-}
-
-# _magic(Bivio::Biz::Model::RealmInvite self) : string
-#
-# Returns a unique string (trap door) from this invite which is
-# checked when execute_accept is called.
-#
-sub _magic {
-    my($self) = @_;
-    my($id, $realm_id, $date_time) = $self->get(
-	    qw(realm_invite_id realm_id creation_date_time));
-
-    # None of this is critical except that the algorithm can't change
-    # without changing the version.  We don't really take advantage
-    # of the internals.
-    $id = int($id/$_MIN_PRIMARY_ID % $_MAX_INTEGER);
-    $realm_id = int($realm_id/$_MIN_PRIMARY_ID % $_MAX_INTEGER);
-    my($date, $time) = split(/\s+/, $date_time);
-
-    # XOR all these values
-    return $id ^ $realm_id ^ $date ^ $time;
 }
 
 # _set_state(Bivio::Biz::Model::RealmInvite self, Bivio::Agent::Request req)
