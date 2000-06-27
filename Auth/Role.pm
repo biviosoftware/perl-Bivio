8974d20ac3254c077818052dbce9a5d4e5a9a825
Martin Lichtin <lichtin@bivio.biz>
2000-06-27 04:16:36 +0000

    update comments

--- Auth/Role.pm
+++ Auth/Role.pm
@@ -11,7 +11,7 @@ Bivio::Auth::Role - authorized roles enum
 =head1 SYNOPSIS
 
     use Bivio::Auth::Role;
-    Bivio::Auth::Role->ANON;
+    Bivio::Auth::Role->ANONYMOUS();
 
 =cut
 
@@ -30,7 +30,7 @@ C<Bivio::Auth::Role> defines the roles users play in a
 L<Bivio::Auth::Realm|Bivio::Auth::Realm>.
 A role is a collection of privileges.  A privilege is a the ability
 to execute a particular L<Bivio::Agent::Task|Bivio::Agent::Task>
-within a realm.  For example, an C<ADMIN> may be granted the
+within a realm.  For example, an C<ADMINISTRATOR> may be granted the
 ability to execute
 L<Bivio::Agent::TaskId::CLUB_ADMIN_ADD_MEMBER|Bivio::Agent::TaskId/"CLUB_ADMIN_ADD_MEMBER">
 in the L<Bivio::Auth::Realm::CLUB|Bivio::Auth::Realm/"CLUB">.
