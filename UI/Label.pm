f17425b0839bf71af0cf3af26a2414014f516591
Paul Moeller <moeller@bivio.biz>
2000-07-14 16:26:28 +0000

    added Return To

--- UI/Label.pm
+++ UI/Label.pm
@@ -179,6 +179,7 @@ _compile(
     WITHDRAWAL_REALIZED_GAIN => ["Gain/(Loss) Realized on Withdrawal (B)-(A)"],
     BASIS_AFTER_WITHDRAWAL => ["Member's Basis After Withdrawal"],
     RETURN_SINCE => ['Return Since'],
+    RETURN_TO => ['Return To'],
     PREPARATION_DATE => ['Report Preparation Date'],
 
     # Accounting Reports (Tasks sorted alphabetically)
