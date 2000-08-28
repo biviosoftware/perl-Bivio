79165cb01ce6bb8ed66a205d84d0bac94d0128ad
Paul Moeller <moeller@bivio.biz>
2000-08-28 21:25:34 +0000

    form buttons

---
+++ Type/CancelButton.pm
@@ -0,0 +1,52 @@
+# Copyright (c) 2000 bivio, Inc.  All rights reserved.
+# $Id$
+package Bivio::Type::CancelButton;
+use strict;
+$Bivio::Type::CancelButton::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+=head1 NAME
+
+Bivio::Type::CancelButton - cancel button
+
+=head1 SYNOPSIS
+
+    use Bivio::Type::CancelButton;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio::Type::FormButton>
+
+=cut
+
+use Bivio::Type::FormButton;
+@Bivio::Type::CancelButton::ISA = ('Bivio::Type::FormButton');
+
+=head1 DESCRIPTION
+
+C<Bivio::Type::CancelButton> cancel button
+
+=cut
+
+#=IMPORTS
+
+#=VARIABLES
+
+=head1 METHODS
+
+=cut
+
+#=PRIVATE METHODS
+
+=head1 COPYRIGHT
+
+Copyright (c) 2000 bivio, Inc.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
---
+++ Type/OKButton.pm
@@ -0,0 +1,52 @@
+# Copyright (c) 2000 bivio, Inc.  All rights reserved.
+# $Id$
+package Bivio::Type::OKButton;
+use strict;
+$Bivio::Type::OKButton::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+=head1 NAME
+
+Bivio::Type::OKButton - OK button
+
+=head1 SYNOPSIS
+
+    use Bivio::Type::OKButton;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio::Type::FormButton> OK button
+
+=cut
+
+use Bivio::Type::FormButton;
+@Bivio::Type::OKButton::ISA = ('Bivio::Type::FormButton');
+
+=head1 DESCRIPTION
+
+C<Bivio::Type::OKButton> OK button
+
+=cut
+
+#=IMPORTS
+
+#=VARIABLES
+
+=head1 METHODS
+
+=cut
+
+#=PRIVATE METHODS
+
+=head1 COPYRIGHT
+
+Copyright (c) 2000 bivio, Inc.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
