0b4be9ae04e5889d71f1bf65489320a66d7dec9d
akaba <akaba@bivio.biz>
2000-10-06 23:28:03 +0000

    First version.  Run this script to run the testsuite.

---
+++ Test/b-testsuite.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+# Copyright (c) 2000 bivio, Inc.  All rights reserved.
+# $Id$
+use strict;
+
+=head1 NAME
+
+b-testsuite - call Bivio::Test::Engine to run site tests
+
+=head1 SYNOPSIS
+
+b-testsuite
+b-testsuite [test name]
+
+=head1 DESCRIPTION
+
+see L<Bivio::Test::Engine>
+
+=cut
+
+#=IMPORTS
+use Bivio::Test::Engine;
+
+#=VARIABLES
+
+Bivio::Test::Engine->main(@ARGV);
+
+=head1 SEE ALSO
+
+=head1 COPYRIGHT
+
+Copyright (c) 2000 Bivio, Inc.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+#Local Variables:
+#mode:cperl
+#End:
