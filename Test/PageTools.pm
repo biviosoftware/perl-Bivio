0c1c2771fc86a7db7de89a645fee305380d7ac05
akaba <akaba@bivio.biz>
2000-10-19 21:25:44 +0000

    Deprecated quiet visit.  Now uses the new HTTPUtil file to send http requests and get a response.

--- Test/PageTools.pm
+++ Test/PageTools.pm
@@ -27,7 +27,7 @@ C<Bivio::Test::PageTools> contains all the methods invoked by the test scripts t
 #=IMPORTS
 use Bivio::IO::Trace;
 use Bivio::Test::BulletinBoard;
-use Bivio::Test::HTTPTools;
+use Bivio::Test::HTTPUtil;
 
 #=VARIABLES
 use vars ('$_TRACE');
@@ -38,42 +38,11 @@ my($_PACKAGE) = __PACKAGE__;
 
 =cut
 
-=for html <a name="quiet_visit"></a>
-
-=head2 quiet_visit() : Page visitor with less output that returns the response.
-
-This version of visit is meant to be used by modules that need to visit a page as part of a longer process.  It returns a reference to the response so it doesn't need to be gotten again by the caller.  It also checks the response for errors.
-
-=cut
-
-sub quiet_visit {
-    my($proto, $uri) = @_;
-    _trace("Current method is quiet_visit(). Current url is:", $uri, "\n")
-	    if $_TRACE;
-
-    my($response) = HTTPTools::http_href('URL', $uri);
-
-    if (!defined $response) {
-	_trace("Failed to get response from server for $uri.\n") if $_TRACE;
-    }
-    elsif ($response->{HTTP_RESPONSE}->is_success) {
-	my ($board) = Bivio::Test::BulletinBoard->get_current();
-	$board->put(response => $response);
-	_trace("Visiting page: " , ($board->get('response'))->{TITLE} , "\n")
-		if $_TRACE;
-    }
-    else {
-	_trace("Error: Page did not load successfully.\n
-                Text gotten was:\n$response\n") if $_TRACE;
-    }
-    return $response;
-}
-
 =for html <a name="visit"></a>
 
 =head2 visit(Bivio::Test::BulletinBoard board, hash_ref uri) 
 
-Visits the page specified by url (which may be dynamically passed) and returns the BulletinBoard object with new entries for response and current_url.
+Visits the page specified by url (which may be dynamically passed) and puts the response in the current BulletinBoard object.
 
 =cut
 
@@ -82,22 +51,23 @@ sub visit {
     _trace("Current method is visit(). Current url is:", $uri, "\n")
 	    if $_TRACE;
 
-    my($response) = HTTPTools::http_href('URL', $uri);
+    my($util) = (Bivio::Test::BulletinBoard->get_current)->get('HTTPUtil');
+    my($http_res) = $util->get_response($uri);
+    _trace("****Content = ". $http_res->content) if $_TRACE;
 
-    if ($response = undef) {
-	Bivio::Test::Engine->print_later(
-		"Failed to get response from server for $uri.\n");
+    if (!defined $http_res) {
+	_trace("Failed to get HTTP Response from server for $uri.\n")
+		if $_TRACE;
     }
-    elsif ($response->{HTTP_RESPONSE}->is_success) {
+    elsif ($http_res->{HTTP_RESPONSE}->is_success) {
 	my ($board) = Bivio::Test::BulletinBoard->get_current();
-	$board->put(response => $response);
-	Bivio::Test::Engine->print_later(
-		"Visiting page: " , ($board->get('response'))->{TITLE} , "\n");
+	$board->put(response => $http_res);
+	_trace("PageVisit Response is:\n*****\n*****" . $http_res->content)
+		if $_TRACE;
     }
     else {
-	Bivio::Test::Engine->print_later(
-		"Error: Page did not load successfully.\n");
-      	_trace("Text gotten was: ", $response, "\n");
+      	_trace("Error: Page did not load successfully.
+                \nText gotten was: ", $http_res, "\n");
     }
     return;
 }
