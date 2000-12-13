e4b607165f03e8ed6b97c3840c21856a67532cc4
Martin Lichtin <lichtin@bivio.biz>
2000-12-13 07:06:16 +0000

    Support more states and accessor, state-transition functions

--- Type/ECPaymentStatus.pm
+++ Type/ECPaymentStatus.pm
@@ -31,15 +31,23 @@ a payment can be associated with. The current choices are:
 
 =over 4
 
-=item PROCESSING
-
-=item FAILED
+=item TRY_CAPTURE
 
 =item CAPTURED
 
-=item REFUND
+=item DECLINED
 
-=item VOID
+=item FAILED
+
+=item CANCELLED
+
+=item TRY_VOID
+
+=item VOIDED
+
+=item TRY_CREDIT
+
+=item CREDITED
 
 =back
 
@@ -51,26 +59,110 @@ a payment can be associated with. The current choices are:
 my($_PACKAGE) = __PACKAGE__;
 
 __PACKAGE__->compile([
-    PROCESSING => [
+    TRY_CAPTURE => [
 	1,
     ],
-    FAILED => [
+    CAPTURED => [
 	2,
     ],
-    CAPTURED => [
+    DECLINED => [
 	3,
     ],
-    REFUND => [
+    FAILED => [
 	4,
     ],
-    VOID => [
+    CANCELLED => [
 	5,
+    ],
+    TRY_VOID => [
+	6,
+    ],
+    VOIDED => [
+	7,
+    ],
+    TRY_CREDIT => [
+	8,
+    ],
+    CREDITED => [
+	9,
     ],
 ]);
 
 =head1 METHODS
 
 =cut
+
+=for html <a name="get_authorize_net_type"></a>
+
+=head2 get_authorize_net_type(Bivio::Type::ECPaymentStatus state) : string
+
+Return the appropriate Authorize.Net transaction type
+
+=cut
+
+sub get_authorize_net_type {
+    my(undef, $state) = @_;
+    if ($state == Bivio::Type::ECPaymentStatus::TRY_CAPTURE()) {
+        return 'AUTH_CAPTURE';
+    } elsif ($state == Bivio::Type::ECPaymentStatus::TRY_VOID()) {
+        return 'VOID';
+    } elsif ($state == Bivio::Type::ECPaymentStatus::TRY_CREDIT()) {
+        return 'CREDIT';
+    } else {
+        Bivio::Die->die('Status not appropriate: ', $state);
+    }
+}
+
+=for html <a name="get_success_state"></a>
+
+=head2 get_success_state(Bivio::Type::ECPaymentStatus state) : Bivio::Type::ECPaymentStatus
+
+From any TRY_* state, change to the corresponding success state.
+
+=cut
+
+sub get_success_state {
+    my(undef, $state) = @_;
+    my($new_state) = $state;
+    if ($state == Bivio::Type::ECPaymentStatus::TRY_CAPTURE()) {
+        $new_state = Bivio::Type::ECPaymentStatus::CAPTURED();
+    } elsif ($state == Bivio::Type::ECPaymentStatus::TRY_VOID()) {
+        $new_state = Bivio::Type::ECPaymentStatus::VOIDED();
+    } elsif ($state == Bivio::Type::ECPaymentStatus::TRY_CREDIT()) {
+        $new_state = Bivio::Type::ECPaymentStatus::CREDITED();
+    }
+    return $new_state;
+}
+
+=for html <a name="is_approved"></a>
+
+=head2 is_approved(Bivio::Type::ECPaymentStatus state) : boolean
+
+Return TRUE if state is one of the approved states
+
+=cut
+
+sub is_approved {
+    my($self, $state) = @_;
+    return $state == Bivio::Type::ECPaymentStatus::CAPTURED()
+            || $state == Bivio::Type::ECPaymentStatus::VOIDED()
+                    || $state == Bivio::Type::ECPaymentStatus::CREDITED();
+}
+
+=for html <a name="needs_processing"></a>
+
+=head2 needs_processing(Bivio::Type::ECPaymentStatus state) : boolean
+
+Return TRUE if <I>state needs further processing
+
+=cut
+
+sub needs_processing {
+    my(undef, $state) = @_;
+    return $state == Bivio::Type::ECPaymentStatus::TRY_CAPTURE()
+            || $state == Bivio::Type::ECPaymentStatus::TRY_VOID()
+                    || $state == Bivio::Type::ECPaymentStatus::TRY_CREDIT();
+}
 
 #=PRIVATE METHODS
 
