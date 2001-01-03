44bbab75d4155abbbc7e1e2cc99850d7e214a29a
Paul Moeller <moeller@bivio.biz>
2001-01-03 18:40:57 +0000

    now it is a ListFormModel, with list fields for foreign tax investment countries

--- Biz/Model/F1065ParametersForm.pm
+++ Biz/Model/F1065ParametersForm.pm
@@ -18,12 +18,12 @@ Bivio::Biz::Model::F1065ParametersForm - IRS 1065 parameters
 
 =head1 EXTENDS
 
-L<Bivio::Biz::FormModel>
+L<Bivio::Biz::ListFormModel>
 
 =cut
 
-use Bivio::Biz::FormModel;
-@Bivio::Biz::Model::F1065ParametersForm::ISA = ('Bivio::Biz::FormModel');
+use Bivio::Biz::ListFormModel;
+@Bivio::Biz::Model::F1065ParametersForm::ISA = ('Bivio::Biz::ListFormModel');
 
 =head1 DESCRIPTION
 
@@ -48,15 +48,30 @@ my($_SQL_DATE_VALUE) = Bivio::Type::DateTime->to_sql_value('?');
 
 =cut
 
-=for html <a name="execute_empty"></a>
+=for html <a name="execute_empty_row"></a>
 
-=head2 execute_empty()
+=head2 execute_empty_row()
+
+Loads country codes.
+
+=cut
+
+sub execute_empty_row {
+    my($self) = @_;
+    $self->internal_put_field('RealmInstrument.country'
+	    => $self->get_list_model->get('RealmInstrument.country'));
+    return;
+}
+
+=for html <a name="execute_empty_start"></a>
+
+=head2 execute_empty_start()
 
 Loads current settings.
 
 =cut
 
-sub execute_empty {
+sub execute_empty_start {
     my($self) = @_;
     my($tax) = Bivio::Biz::Model::Tax1065->new($self->get_request)
 	    ->load_or_default(_get_end_date($self));
@@ -75,15 +90,33 @@ sub execute_empty {
     return;
 }
 
-=for html <a name="execute_ok"></a>
+=for html <a name="execute_ok_row"></a>
 
-=head2 execute_ok()
+=head2 execute_ok_row()
+
+Saves the foreign tax country code into the RealmInstrument.
+
+=cut
+
+sub execute_ok_row {
+    my($self) = @_;
+
+    my($realm_inst) = $self->get_list_model->get_model('RealmInstrument');
+    $realm_inst->update({
+	country => $self->get('RealmInstrument.country'),
+    });
+    return;
+}
+
+=for html <a name="execute_ok_start"></a>
+
+=head2 execute_ok_start()
 
 Saves new settings.
 
 =cut
 
-sub execute_ok {
+sub execute_ok_start {
     my($self) = @_;
 
     # save values into related models
@@ -104,7 +137,8 @@ B<FOR INTERNAL USE ONLY>
 sub internal_initialize {
     my($self) = @_;
     my($info) = {
-	version => 3,
+	version => 4,
+	list_class => 'ForeignTaxCountryList',
 	require_context => 1,
 	visible => [
 	    'Tax1065.partnership_type',
@@ -125,7 +159,13 @@ sub internal_initialize {
 	    Address.state
 	    Address.zip
 	    TaxId.tax_id
-	)],
+	),
+	    {
+		name => 'RealmInstrument.country',
+		constraint => 'NOT_NULL',
+		in_list => 1,
+	    },
+	],
 	hidden => [qw(
             Address.location
 	    Tax1065.realm_id
@@ -143,15 +183,35 @@ sub internal_initialize {
 	    $self->SUPER::internal_initialize, $info);
 }
 
-=for html <a name="validate"></a>
+=for html <a name="internal_initialize_list"></a>
 
-=head2 validate()
+=head2 internal_initialize_list() : Bivio::Biz::ListModel
+
+Loads and returns the MemberTaxList using the date from the
+form context.
+
+=cut
+
+sub internal_initialize_list {
+    my($self) = @_;
+
+    Bivio::Biz::Action::ReportDate->set_report_date(
+	    _get_end_date($self), $self->get_request);
+    Bivio::Biz::Model::ForeignTaxCountryList->execute_load_all(
+	    $self->get_request);
+    # use the super class to get the list from the request
+    return $self->SUPER::internal_initialize_list();
+}
+
+=for html <a name="validate_start"></a>
+
+=head2 validate_start()
 
 Ensures fields are valid.
 
 =cut
 
-sub validate {
+sub validate_start {
     my($self) = @_;
 
     if ($self->get('Tax1065.partnership_type')
