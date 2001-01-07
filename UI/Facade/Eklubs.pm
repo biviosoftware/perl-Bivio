a3964ee529c88cafe39f201b20d0a83603eda58e
Rob Nagler <github@q33.us>
2001-01-07 21:24:41 +0000

    Copy of AllWomenInvest, simple

--- UI/Facade/Eklubs.pm
+++ UI/Facade/Eklubs.pm
@@ -7,7 +7,7 @@ $_ = $Bivio::UI::Facade::Eklubs::VERSION;
 
 =head1 NAME
 
-Bivio::UI::Facade::Eklubs - eklubs full co-brand
+Bivio::UI::Facade::Eklubs - www.eklubs.com
 
 =head1 SYNOPSIS
 
@@ -26,83 +26,41 @@ use Bivio::UI::Facade;
 
 =head1 DESCRIPTION
 
-C<Bivio::UI::Facade::Eklubs> is the main eklubsuction and default Facade.
+C<Bivio::UI::Facade::Eklubs> is a full co-brand for
+www.eklubs.com.
 
 =cut
 
 #=IMPORTS
+use Bivio::UI::Facade::Eklubs::Home;
 
 #=VARIABLES
+my($_W) = 'Bivio::UI::HTML::Widget';
 __PACKAGE__->new({
     clone => 'Prod',
-    is_production => 0,
-    uri => 'cimo',
-    'Bivio::UI::Color' => {
-	initialize => sub {
-	    my($fc) = @_;
-	    $fc->regroup(footer_line => 0x000000);
-            $fc->value(page_link_hover => 0xc0c0c0);
-	    $fc->value(page_link => 0x336699);
-	    $fc->value(page_vlink => 0x336699);
-	    $fc->value(page_heading => 0x336699);
-            $fc->value(realm_name => 0xFF0300);
-            $fc->value(image_menu_bg => 0x84a4c4);
-            $fc->value(summary_line => 0x6600CC);
-	    return;
-	},
-    },
-    'Bivio::UI::Font' => {
-	initialize => sub {
-	    my($fc) = @_;
-	    $fc->value(default => [
-		'family=arial', 'size=xx-small',
-	    ]);
-	    $fc->group(help_log_button => ['bold']);
-	    return;
-	}
-    },
+    is_production => 1,
+    uri => 'eklubs',
     'Bivio::UI::HTML' => {
 	clone => undef,
 	initialize => sub {
 	    my($fc) = @_;
 
 	    # Some required strings and values
-	    $fc->group(logo_icon => 'logo');
-	    $fc->group(site_name => 'eklubs');
-	    $fc->group(home_alt_text => 'eklubs home');
+	    $fc->group(logo_icon => 'dot');
+	    $fc->group(site_name => 'Eklubs');
+	    $fc->group(home_alt_text => 'Eklubs - '
+		    .'Real Investors, Real Investments, Real Returns');
 
 	    $fc->initialize_standard_support;
-
-	    # This one is used dynamically by ImageMenu in header_widget
-	    # widget.  It is not a required field.  Only if you are using
-	    # ImageMenu.
-	    my($icon) = $fc->get_facade->get('Bivio::UI::Icon');
-	    $fc->value(text_menu_base_offset =>
-		    $icon->get_width('logo_full') + $icon->get_width('grad'));
-
-	    $fc->value(image_menu_left_cell =>
-		    Bivio::UI::HTML::Widget->image('grad', ''));
-
-	    # Used by standard header
-	    $fc->value(logo_icon_width_as_html =>
-		    $icon->get_width_as_html('logo_full'));
+	    $fc->value(want_secure => 0);
+	    # Home page is special
+	    $fc->value(home_page =>
+		    Bivio::UI::Facade::Eklubs::Home->new);
 
 	    # These are required names, which are checked by page.
-	    Bivio::UI::HTML::Widget->load_class('Page');
-	    $fc->group(page_widget => Bivio::UI::HTML::Widget::Page->new({
-		head => $fc->get_standard_head(),
-		style => $fc->get_standard_style(),
-		body => Bivio::UI::HTML::Widget->join([
-		    _header(),
-		    Bivio::UI::HTML::Widget->indirect(['page_scene']),
-		    _footer(),
-		]),
-	    }));
-	    $fc->group(header_widget => _header());
-	    $fc->group(header_height => $icon->get_height('logo_full')
-		    + $icon->get_height('tag')
-		    + $icon->get_height('power_grad') + 100
-		   );
+	    _header($fc);
+	    _footer($fc);
+	    $fc->group(page_widget => $fc->get_standard_page);
 	    $fc->group(logo_widget => $fc->get_standard_logo);
 	    $fc->group(head_widget => $fc->get_standard_head);
 	    return;
@@ -116,177 +74,106 @@ __PACKAGE__->new({
 
 #=PRIVATE METHODS
 
-# _footer() : Bivio::UI::HTML::Widget
+# _footer(Bivio::UI::HTML html) : Bivio::UI::HTML::Widget
 #
-# Returns footer widget.
+# Sets footer_widget.
 #
 sub _footer {
+    my($html) = @_;
     # Create list of links
     my($links) = [];
-    foreach my $t ('HOME:HTTP_DOCUMENT',
-#	    'ABOUT EKLUBS:ABOUT_US',
-	    'SAFE & PRIVATE:GENERAL_PRIVACY',
-#	    'REGISTER:USER_CREATE',
-#	    'START A CLUB:GENERAL_CONTACT'
-#	    'TOUR:TOUR'
-	    'CONTACT:GENERAL_CONTACT',
+    foreach my $t (
+	    'HOME:http://www.eklubs.com/eklubs.htm',
+	    'ABOUT EKLUBS:http://www.eklubs.com/eklubs.htm',
+	    'SAFE & PRIVATE:http://www.eklubs.com/safe.htm',
+	    'REGISTER:/pub/register',
+	    'START A CLUBS:http://www.eklubs.com/start.htm',
 	   ) {
 	my($label, $task) = split(/:/, $t, 2);
-	push(@$links, Bivio::UI::HTML::Widget->link(
-		$label, $task, 'footer_menu'),
+	push(@$links, $_W->link($label, $task, 'footer_menu'),
 		'&nbsp;|&nbsp;');
     }
-    push(@$links, Bivio::UI::HTML::Widget->mailto(['support_email'])->put(
-	    string_font => 'footer_menu',
-	   ));
+    # Delete last separator
+    pop(@$links);
 
     # Create grid
-    Bivio::UI::HTML::Widget->load_class('Grid');
-    return Bivio::UI::HTML::Widget::Grid->new({
+    $_W->load_class('Grid');
+    $html->group(footer_widget => Bivio::UI::HTML::Widget::Grid->new({
 	expand => 1,
 	values => [
 	    [
-		' ',
-	    ],
+		Bivio::UI::HTML::Widget->clear_dot(1, 10),
+		],
 	    [
-		Bivio::UI::HTML::Widget->clear_dot(undef, 3)->put(
+		Bivio::UI::HTML::Widget->clear_dot(undef, 1)->put(
 			cell_expand => 1,
 			cell_bgcolor => 'footer_line',
 		       ),
 	    ],
 	    [
-		Bivio::UI::HTML::Widget->toggle_secure(),
+		# Match the top
+		'&nbsp;&nbsp;&nbsp;',
 		Bivio::UI::HTML::Widget::Grid->new({
 		    cell_align => 'center',
+		    cell_expand => 1,
 		    values => [
 			$links,
 		    ],
 		}),
-		Bivio::UI::HTML::Widget->link('top', '#top', 'footer_menu'),
+		$_W->link('top', '#top', 'footer_menu'),
 	    ],
 	    [
 		' ',
 	    ],
 	    [
-		Bivio::UI::HTML->get_standard_copyright->put(
-		    cell_align => 'right',
+		Bivio::UI::HTML::Widget::Grid->new({
+		    expand => 1,
 		    cell_expand => 1,
-		),
+		    values => [
+			[
+			    $_W->link($_W->image('bivio_power'),
+				    'http://www.bivio.com')
+				    ->put(cell_align => 'sw'),
+			    $html->get_standard_copyright->put(
+				    cell_align => 'right',
+				    cell_expand => 1,
+				   ),
+			],
+		    ],
+		}),
 	    ],
 	],
-    });
+    }));
     return;
 }
 
-# _header() : Bivio::UI::HTML::Widget
+# _header(Bivio::UI::HTML html) : Bivio::UI::HTML::Widget
 #
-# Returns the header widget.
+# Sets header_widget and header_height.
 #
 sub _header {
-    # The header is different for each realm type
-    Bivio::UI::HTML::Widget->load_class(qw(RealmChooser Grid));
-    my($realm_info) = Bivio::UI::HTML::Widget::RealmChooser->new({
-	pad_left => 0,
-	cell_rowspan => 1,
-    });
-    $realm_info->put(cell_nowrap => 1, cell_align => 'left',
-	    cell_colspan => 2, cell_expand => 1);
-    my($top_menu) = Bivio::UI::HTML::Widget->indirect(
-	    ['page_image_menu'])->put(
-		    cell_align => 'nw',
-		    cell_bgcolor => 'image_menu_bg',
-		    cell_colspan => 2,
-		    cell_align => 'left',
-		   );
-    my($sub_menu) = Bivio::UI::HTML::Widget->indirect(
-	    ['page_text_menu']);
-    my($top_part) = Bivio::UI::HTML::Widget::Grid->new({
-	cell_expand => 1,
-	expand => 1,
-	values => [
-	    [
-		Bivio::UI::HTML::Widget->director(
-			['super_user_id'],
-			{},
-			Bivio::UI::HTML::Widget->string(['auth_user', 'name'],
-				'substitute_user'),
-			Bivio::UI::HTML::Widget->link(
-				Bivio::UI::HTML::Widget->image(
-				    'logo_full',
-				    ['Bivio::UI::HTML', '->get_value',
-					'home_alt_text'],
-				       ),
-				'/'))->put(
-				cell_rowspan => 2,
-				cell_align => 'sw',
-				cell_width_as_html => [
-				    'Bivio::UI::Icon', '->get_width_as_html',
-				    'logo_full'],
-			       ),
-		$realm_info,
-	    ],
-	    [
-		Bivio::UI::HTML::Widget::Grid->new({
-		    cell_align => 'sw',
-		    values => [[
-			Bivio::UI::HTML::Widget->image('dashed_line', '')
-				    ->put(
-					    cell_align => 'sw',
-					   ),
-			Bivio::UI::HTML::Widget->join([
-			    '&nbsp;',
-			    Bivio::UI::HTML::Widget->link(
-				    'help',
-				    ['->format_help_uri'],
-				    'help_log_button',
-				   ),
-			    '&nbsp;',
-			    '&nbsp;',
-			    Bivio::UI::HTML::Widget->director(
-				    ['auth_user'],
-				    {
-				    },
-				    Bivio::UI::HTML::Widget->link(
-					    'logout',
-					    'LOGOUT',
-					    'help_log_button',
-					   ),
-				    Bivio::UI::HTML::Widget->link(
-					    'login',
-					    'LOGIN',
-					    'help_log_button',
-					   ),
-				   ),
-			    '&nbsp;',
-			])->put(
-				cell_align => 'sw',
-				cell_nowrap => 1,
-			       ),
-			   ]],
-		    }),
-	    ],
-	    [
-		Bivio::UI::HTML::Widget->image('tag',
-			'The Easy way to invest'
-		       )->put(
-			       cell_colspan => 2,
-			      ),
-	    ],
-	    [
-		Bivio::UI::HTML::Widget->link(
-			Bivio::UI::HTML::Widget->image('power_grad',
-				'powered by bivio'),
-			'http://www.bivio.com'),
-		$top_menu,
-	    ],
-	],
-    });
+    my($html) = @_;
+    my($hdr) = $html->get_standard_header();
+    # Insert before <a name=top>...
+    splice(@{$hdr->get('values')}, 1, 0, _logo($html));
+    $html->group(header_widget => $hdr);
+    $html->group(header_height =>
+	    $html->get_standard_header_height
+	    + $html->get_facade->get('Bivio::UI::Icon')
+	    ->get_height('eklubs_header'));
+    return;
+}
 
-    # The top is used by the standard_footer.
-#TODO: Make link '_top' dynamic.
-    # We set _top, because the header is used in a frame.
-    return Bivio::UI::HTML::Widget->join('<a name="top"></a>',
-	    $top_part, $sub_menu);
+# _logo(Bivio::UI::HTML html) : Bivio::UI::HTML::Widget
+#
+# Returns the logo widget which points to eklubs home page.
+#
+sub _logo {
+    my($html) = @_;
+    return $_W->link(
+	    $_W->image('eklubs_header', $html->get_value('home_alt_text')),
+	        'http://www.eklubs.com');
+
 }
 
 =head1 COPYRIGHT
