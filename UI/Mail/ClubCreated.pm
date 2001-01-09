96608494d3a254ca2a3afcb1ba556d22044e2ed0
Rob Nagler <github@q33.us>
2001-01-09 20:54:57 +0000

    New text with premium services

--- UI/Mail/ClubCreated.pm
+++ UI/Mail/ClubCreated.pm
@@ -1,8 +1,9 @@
-# Copyright (c) 1999 bivio, LLC.  All rights reserved.
+# Copyright (c) 1999,2000 bivio Inc.  All rights reserved.
 # $Id$
 package Bivio::UI::Mail::ClubCreated;
 use strict;
 $Bivio::UI::Mail::ClubCreated::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+$_ = $Bivio::UI::Mail::ClubCreated::VERSION;
 
 =head1 NAME
 
@@ -11,7 +12,6 @@ Bivio::UI::Mail::ClubCreated - sends a mail message to the newly created club
 =head1 SYNOPSIS
 
     use Bivio::UI::Mail::ClubCreated;
-    Bivio::UI::Mail::ClubCreated->execute();
 
 =cut
 
@@ -26,8 +26,10 @@ C<Bivio::UI::Mail::ClubCreated>
 
 #=IMPORTS
 use Bivio::UI::Mail::SupportAuthor;
+use Bivio::UI::HTML::Widget;
 
 #=VARIABLES
+my($_W) = 'Bivio::UI::HTML::Widget';
 
 =head1 METHODS
 
@@ -47,27 +49,37 @@ sub execute {
     my($club) = $req->get('Bivio::Biz::Model::RealmOwner');
     my($url) = $club->format_http();
     my($recipient) = $club->format_email();
+    my($http) = $req->format_http_prefix;
+    my($ak) = $http
+	    .$_W->format_uri_static_site($req, 'hm/account-keeper.html');
+    my($ps) = $http
+	    .$_W->format_uri_static_site($req, 'hm/premium-support.html');
     my($msg) = Bivio::UI::Mail::SupportAuthor->enqueue_message($req,
 	    $recipient,
 	    # For syntax see Common::_text_to_html
 	    "Welcome to bivio", <<"EOF");
-Thank you for bringing your club to bivio.  You may access your
-club's space at the following URL:
+Thank you for bringing your club to bivio.  Your private club homepage is:
 
     $url
 
-Your club's area is accessible only to members and guests of your
-club.  This area provides you with a host of functions including:
-revise and view your books, send to and view your club's message
-board, and download financial data.
+Use bivio to:
 
-In addition, you can reach your club members by mailing to the
-following address:
+    * Keep your club books.
+    * Generate financial and performance reports.
+    * Share documents and exchange messages.
+
+Sign your whole club up for a full 12 months of Premium Support for only \$39:
+
+    $ps
+
+Promote yourself to CFO.  Let your bivio AccountKeeper do all the work for
+only \$195 per year, including year-end tax forms:
+
+    $ak
+
+A final tip.  To send a message to your club, use this email address:
 
     $recipient
-
-Messages sent to this address will also appear in your club's
-message board.
 EOF
     return;
 }
@@ -76,7 +88,7 @@ EOF
 
 =head1 COPYRIGHT
 
-Copyright (c) 1999 bivio, LLC.  All rights reserved.
+Copyright (c) 1999,2000 bivio Inc.  All rights reserved.
 
 =head1 VERSION
 
