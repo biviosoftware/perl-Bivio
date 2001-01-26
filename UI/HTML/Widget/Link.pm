acbb258f564d0f1fc615bbbc258fd71fc18bfaeb
Rob Nagler <github@q33.us>
2001-01-26 19:36:51 +0000

    Made AbstractControl

--- UI/HTML/Widget/Link.pm
+++ UI/HTML/Widget/Link.pm
@@ -17,12 +17,12 @@ Bivio::UI::HTML::Widget::Link - renders a URI link
 
 =head1 EXTENDS
 
-L<Bivio::UI::HTML::Widget>
+L<Bivio::UI::HTML::Widget::AbstractControl>
 
 =cut
 
-use Bivio::UI::HTML::Widget;
-@Bivio::UI::HTML::Widget::Link::ISA = qw(Bivio::UI::HTML::Widget);
+use Bivio::UI::HTML::Widget::AbstractControl;
+@Bivio::UI::HTML::Widget::Link::ISA = ('Bivio::UI::HTML::Widget::AbstractControl');
 
 =head1 DESCRIPTION
 
@@ -38,14 +38,9 @@ an C<HREF> attribute.
 Arbitrary HTML attributes to be applied to the begin tag.  Must begin
 with leading space.
 
-=item control : string []
+=item control : any
 
-=item control : Bivio::Agent::TaskId [] (get_request)
-
-=item control : array_ref []
-
-Don't make a link if doesn't return true.
-If string or task, will generate the appropriate control.
+See L<Bivio::UI::HTML::Widget::AbstractControl|Bivio::UI::HTML::Widget::AbstractControl>.
 
 =item href : array_ref (required)
 
@@ -64,11 +59,6 @@ The value to be passed to the C<TARGET> attribute of C<A> tag.
 
 Anchor name.
 
-=item off_value : any [value]
-
-The value to use when the control returns false.
-If the off_value is 0, then nothing will be rendered.
-
 =item value : widget (required)
 
 The value between the C<A> tags aka the label.
@@ -80,7 +70,6 @@ The value between the C<A> tags aka the label.
 #=IMPORTS
 use Bivio::Die;
 use Bivio::HTML;
-use Carp ();
 
 #=VARIABLES
 my($_PACKAGE) = __PACKAGE__;
@@ -98,7 +87,7 @@ Creates a new Link widget.
 =cut
 
 sub new {
-    my($self) = &Bivio::UI::HTML::Widget::new(@_);
+    my($self) = Bivio::UI::HTML::Widget::AbstractControl::new(@_);
     $self->{$_PACKAGE} = {};
     return $self;
 }
@@ -106,6 +95,27 @@ sub new {
 =head1 METHODS
 
 =cut
+
+=for html <a name="control_on_render"></a>
+
+=head2 control_on_render(any source, string_ref buffer)
+
+Render the link.
+
+=cut
+
+sub control_on_render {
+    my($self, $source, $buffer) = @_;
+    my($fields) = $self->{$_PACKAGE};
+
+    $$buffer .= $fields->{prefix};
+    $$buffer .= ' href="'.$source->get_widget_value(@{$fields->{href}}).'"'
+	    if $fields->{href};
+    $$buffer .= '>';
+    $fields->{value}->render($source, $buffer);
+    $$buffer .= '</a>';
+    return;
+}
 
 =for html <a name="initialize"></a>
 
@@ -119,8 +129,9 @@ It is fully initialized after first render.
 sub initialize {
     my($self) = @_;
     my($fields) = $self->{$_PACKAGE};
-    return if exists($fields->{value});
-    # Both must be defined
+    return if $fields->{value};
+
+    # href and value both must be defined
     my($href);
     ($fields->{value}, $href) = $self->get('value', 'href');
     my($p, $s) = ('<a'.$self->link_target_as_html, '');
@@ -135,104 +146,12 @@ sub initialize {
     else {
 	$p .= ' href="'.$href.'"';
     }
-    # We assume is not a constant and on first rendering, may be set to true
-    $fields->{is_constant} = 0;
-    $fields->{is_initialized} = 0;
     $fields->{prefix} = $p;
     $fields->{value}->put(parent => $self);
 
-    # check control and off_value
-    $fields->{control} = $self->unsafe_get('control');
-    $fields->{control} = [['->get_request'], '->can_user_execute_task',
-	Bivio::Agent::TaskId->from_any($fields->{control})]
-	    if $fields->{control} && ref($fields->{control}) ne 'ARRAY';
-    if ($fields->{control}) {
-	$fields->{off_value} = $self->unsafe_get('off_value');
-	if (ref($fields->{off_value})) {
-	    $fields->{off_value}->put(parent => $self);
-	    $fields->{off_value}->initialize;
-	}
-	elsif ($fields->{off_value}) {
-	    Bivio::Die->die($fields->{off_value}, ': invalid off value');
-	}
-	elsif (!defined($fields->{off_value})) {
-	    $fields->{off_value} = $fields->{value};
-	}
-    }
-
-    # Child initializations happen last.
+    # Child initializations happen last.  Parent happens after that.
     $fields->{value}->initialize;
-    return;
-}
-
-=for html <a name="is_constant"></a>
-
-=head2 is_constant : boolean
-
-Is this instance a constant?
-
-=cut
-
-sub is_constant {
-    my($fields) = shift->{$_PACKAGE};
-    Carp::croak('can only be called after first render')
-		unless $fields->{is_initialized};
-    return $fields->{is_constant};
-}
-
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Render the link.  Most of the code is involved in avoiding unnecessary method
-calls.  If I<value> is a constant, then it will be rendered only once.
-
-=cut
-
-sub render {
-    my($self, $source, $buffer) = @_;
-    my($fields) = $self->{$_PACKAGE};
-    $$buffer .= $fields->{value}, return if $fields->{is_constant};
-
-    if ($fields->{control}
-	    && !$source->get_widget_value(@{$fields->{control}})) {
-	$fields->{off_value}->render($source, $buffer)
-		if $fields->{off_value};
-	# Make sure we don't initialize twice.
-	$fields->{is_initialized} = 1 unless $fields->{is_initialized};
-	return;
-    }
-
-    # Used only at initialization
-    my($start) = length($$buffer);
-
-    # Render href
-    $$buffer .= $fields->{prefix};
-    $$buffer .= ' href="'.
-	    $source->get_widget_value(@{$fields->{href}}).'"'
-		    if $fields->{href};
-
-    # Render value
-    $$buffer .= '>';
-    $fields->{value}->render($source, $buffer);
-    $$buffer .= '</a>';
-
-    # Initialize?
-    return if $fields->{is_initialized};
-    Carp::croak($self, '->initialize: not called')
-		unless $fields->{value};
-    $fields->{is_initialized} = 1;
-
-    # Can't be constant if control or href
-    return if $fields->{control} || $fields->{href}
-	    || !$fields->{value}->is_constant;
-
-    # Everything is constant.  Delete other fields and store constant
-    $fields->{value} = substr($$buffer, $start);
-    $fields->{is_constant} = 1;
-    delete($fields->{prefix});
-    delete($fields->{suffix});
-    return;
+    return $self->SUPER::initialize();
 }
 
 #=PRIVATE METHODS
