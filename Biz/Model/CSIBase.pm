71312056e561dec0dfbe4a66b8b193a823d74a32
Martin Lichtin <lichtin@bivio.biz>
2001-02-12 23:55:55 +0000

    moved audit_clubs into Biz/Accounting/Util

--- Biz/Model/CSIBase.pm
+++ Biz/Model/CSIBase.pm
@@ -41,14 +41,12 @@ the records can be properly dispatched for processing.
 #=IMPORTS
 use Bivio::Biz::Model::RealmOwner;
 use Bivio::Data::CSI::RecordType;
-use Bivio::SQL::Connection;
 use Bivio::Type::DateTime;
 
 #=VARIABLES
 my($_PACKAGE) = __PACKAGE__;
 use vars qw($_TRACE);
 Bivio::IO::Trace->register;
-my($_SQL_DATE_VALUE) = Bivio::Type::DateTime->to_sql_value('?');
 # keyed by Data::CSI::RecordType
 my(%_RECORD_HANDLERS);
 
@@ -60,46 +58,7 @@ my(%_RECORD_HANDLERS);
 
 =cut
 
-=for html <a name="audit_clubs"></a>
-
-=head2 static audit_clubs(Bivio::Agent::Request req, Bivio::Type::Date date)
-
-Audits any clubs which have used a day between I<date> and now
-as a valuation date.
-
-=cut
-
-sub audit_clubs {
-    my($self, $req, $date) = @_;
-
-    # find all clubs using that date or any later date for valuations
-    my($sth) = Bivio::SQL::Connection->execute("
-        select to_char(min(member_entry_t.valuation_date), 'j sssss'),
-            realm_transaction.realm_id
-        from realm_transaction_t, entry_t, member_entry_t
-        where realm_transaction_t.realm_transaction_id
-            =entry_t.realm_transaction_id
-        and entry_t.entry_id=member_entry_t.entry_id
-        realm_transaction_t.modified_date_time >= $_SQL_DATE_VALUE
-        group by realm_transaction_t.realm_id", [$date]);
-
-    # audit that books from that date forward
-    my($realm) = Bivio::Biz::Model::RealmOwner->new($req);
-    while (my $row = $sth->fetchrow_arrayref) {
-	my($val_date, $realm_id) = @$row;
-	$realm->unauth_load_or_die(realm_id => $realm_id);
-	$req->set_realm(Bivio::Auth::Realm->new($realm));
-
-	# print a message to the logs. Don't use warn, because we
-	# don't know how many of these messages we might get.
-	Bivio::IO::Alert->info('auditing club ', $realm->get('name'),
-                ', starting on ', $val_date->to_string);
-	$realm->audit_units($val_date);
-    }
-    return;
-}
-
-=for html <a name="create_or_update"></a>
+for html <a name="create_or_update"></a>
 
 =head2 create_or_update(hash_ref new_values, Bivio::Data::CSI::RecordType record_type) : Bivio::Biz::Model::CSIBase
 
