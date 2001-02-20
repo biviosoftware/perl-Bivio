38d356ea7049a609463932967eb8ece03b2796b1
Rob Nagler <github@q33.us>
2001-02-20 19:00:01 +0000

    running

--- Biz/Model/RealmNotice.pm
+++ Biz/Model/RealmNotice.pm
@@ -41,7 +41,7 @@ my($_PACKAGE) = __PACKAGE__;
 
 =for html <a name="create"></a>
 
-=head2 create(hash_ref new_values)
+=head2 create(hash_ref new_values) : self
 
 Sets I<creation_date_time>, I<realm_id> (auth_id), and
 I<at_least_role> (ACCOUNTANT)
@@ -53,47 +53,11 @@ sub create {
     my($self, $values) = @_;
     $values->{creation_date_time} = Bivio::Type::DateTime->now()
 	    unless $values->{creation_date_time};
-    $values->{realm_id} = $self->get_request('auth_id')
+    $values->{realm_id} = $self->get_request->get('auth_id')
 	    unless $values->{realm_id};
     $values->{at_least_role} = Bivio::Auth::Role::ACCOUNTANT()
 	    unless $values->{at_least_role};
     return $self->SUPER::create($values);
-}
-
-=for html <a name="create_template"></a>
-
-=head2 create_template(string template, hash_ref values) : Bivio::Biz::PropertyModel
-
-Creates a TEMPLATE type notice.  I<values> may be undef.
-
-=cut
-
-sub create_template {
-    my($self, $template, $values) = @_;
-    $values ||= {};
-    $values->{notice_type} = Bivio::Type::Notice::TEMPLATE();
-    # Make sure the template compiles
-    Bivio::UI::HTML::Widget->template($template);
-    $values->{template_or_args} = $template;
-    return $self->create($values);
-}
-
-=for html <a name="create_standard"></a>
-
-=head2 create_standard(string type, array_ref args, hash_ref values) : Bivio::Biz::PropertyModel
-
-Creates a notice of I<type>.  I<args> may be undef.  I<values> may be
-undef.
-
-=cut
-
-sub create_standard {
-    my($self, $type, $args, $values) = @_;
-    $values ||= {};
-    $args ||= [];
-    $values->{notice_type} = Bivio::Type::Notice->from_name($type);
-    $values->{template_or_args} = join($;, @$args);
-    return $self->create($values);
 }
 
 =for html <a name="internal_initialize"></a>
@@ -113,11 +77,56 @@ sub internal_initialize {
             realm_id => ['PrimaryId', 'NOT_NULL'],
             at_least_role => ['Bivio::Auth::Role', 'NOT_ZERO_ENUM'],
 	    creation_date_time => ['DateTime', 'NOT_NULL'],
-            notice_type => ['Notice', 'NOT_NULL'],
-	    template_or_args => ['LongText', 'NONE'],
+            realm_notice_type => ['RealmNotice', 'NOT_ZERO_ENUM'],
+	    template_params => ['Array', 'NONE'],
         },
 	auth_id => 'realm_id',
     };
+}
+
+=for html <a name="unauth_create_unless_type_exists"></a>
+
+=head2 unauth_create_unless_type_exists(string realm_id, any type, array_ref params, hash_ref values) : self
+
+Creates a notice of I<type> only if one doesn't exist for this realm.
+I<params> may be undef.  I<values> may be undef.
+
+=cut
+
+sub unauth_create_unless_type_exists {
+    my($self, $realm_id, $type, $params, $values) = @_;
+    $values ||= {};
+    $values->{realm_id} = $realm_id;
+    $values->{realm_notice_type} = Bivio::Type::RealmNotice->from_any($type);
+    $values->{template_params} = $params || $params;
+    return $self
+	    if $self->unauth_load(realm_id => $realm_id,
+		    realm_notice_type => $values->{realm_notice_type});
+    return $self->create($values);
+}
+
+=for html <a name="unauth_delete_by_type"></a>
+
+=head2 unauth_delete_by_type(string realm_id, any type) : boolean
+
+Deletes the notice for I<realm_id> if I<type> exists.  May delete more than
+one record.
+
+Returns true if it deleted one or more records.
+
+=cut
+
+sub unauth_delete_by_type {
+    my($self, $realm_id, $type) = @_;
+    my($it) = $self->unauth_iterate_start('realm_notice_id', {
+	realm_id => $realm_id,
+	realm_notice_type => Bivio::Type::RealmNotice->from_any($type)
+    });
+    my($res) = 0;
+    while ($self->iterate_next_and_load($it)) {
+	$res = 1 if $self->unauth_delete;
+    }
+    return $res;
 }
 
 #=PRIVATE METHODS
