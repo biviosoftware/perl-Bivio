4d06c85aa1ef46e5174946faa0266949d2f2dd00
Rob Nagler <github@q33.us>
2001-04-03 04:56:34 +0000

    Added _site_advertizement Allow "tag" on goto

--- UI/HTML/Format/Link.pm
+++ UI/HTML/Format/Link.pm
@@ -41,27 +41,67 @@ links.
 
 =for html <a name="get_widget_value"></a>
 
-=head2 static get_widget_value(string href) : string
+=head2 static get_widget_value(any href) : string
 
-=head2 static get_widget_value(any href, string site) : string
+=head2 static get_widget_value(any href, string site, any site_args, ...) : string
 
 Returns an href, possibly as an /goto link.  If I<site> is specified,
 must match one of the site implementations below.
 
+href may be a L<Bivio::Agent::TaskId|Bivio::Agent::TaskId> or it
+may be a name of TaskId.
+
 =cut
 
 sub get_widget_value {
-    my(undef, $href, $site) = @_;
-    $href = &{\&{'_site_'.$site}}($href) if $site;
-    return $href unless $href =~ /^\w+:/;
+    my(undef, $href, $site) = (shift, shift, shift);
+    my($tag);
+    ($href, $tag) = &{\&{'_site_'.$site}}($href, @_)
+	    if $site;
+    return $href if _format_task(\$href);
+
+    # Not an offsite uri?  Just return $href.
+    return $href unless $href =~ /^\w+:/ || $tag;
+
     return Bivio::Agent::HTTP::Location->format_realmless(
-	    Bivio::Agent::TaskId::CLIENT_REDIRECT())
+	    Bivio::Agent::TaskId::CLIENT_REDIRECT(),
+	   $tag)
 	    .'?'
 	    .Bivio::Biz::Action::ClientRedirect->QUERY_TAG()
 	    .'='.Bivio::HTML->escape_query($href);
 }
 
 #=PRIVATE METHODS
+
+# _format_task(string_ref href) : boolean
+#
+# Returns true if formatted as a task uri.
+#
+sub _format_task {
+    my($href) = @_;
+    if (ref($$href)) {
+	Bivio::Die->die($$href, ': ref is not a TaskId')
+		    unless UNIVERSAL::isa($$href, 'Bivio::Agent::TaskId');
+    }
+    elsif (Bivio::Agent::TaskId->is_valid_name($$href)) {
+	$$href = Bivio::Agent::TaskId->$$href();
+    }
+    else {
+	return 0;
+    }
+    # This will result in an "onsite" uri, but may have https:/ in front
+    $$href = Bivio::Agent::Request->get_current->format_stateless_uri($$href);
+    return 1;
+}
+
+# _site_advertizement(any uri, string tag) : string
+#
+# Returns its arguments.  You may pass a TASK as the uri.  If there
+# is a tag, it will be return and used.
+#
+sub _site_advertizement {
+    return @_;
+}
 
 # _site_yahoo_quotes(array_ref ticker) : string
 #
