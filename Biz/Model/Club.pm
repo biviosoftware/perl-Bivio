a0d787eeae34e04108d57629ec28a02decd5e699
Paul Moeller <moeller@bivio.biz>
2001-04-24 17:20:17 +0000

    moved has_transactions to RealmTransaction removed delete_member_by_name, should use UI instead, didn't handle files

--- Biz/Model/Club.pm
+++ Biz/Model/Club.pm
@@ -33,16 +33,21 @@ and delete interface to the C<club_t> table.
 =cut
 
 #=IMPORTS
+# also uses RealmUserList
 use Bivio::Auth::RealmType;
 use Bivio::Auth::RoleSet;
 use Bivio::Biz::Accounting::Ratio;
+use Bivio::Biz::Model::RealmOwner;
 use Bivio::Biz::Model::RealmUser;
+use Bivio::IO::Trace;
+use Bivio::SQL::Connection;
+use Bivio::Type::Amount;
 use Bivio::Type::DateTime;
+use Bivio::Type::RealmName;
 
 #=VARIABLES
 use vars qw($_TRACE);
 Bivio::IO::Trace->register;
-my($_SQL_DATE_VALUE) = Bivio::Type::DateTime->to_sql_value('?');
 my($_COUNT_ALL_WHERE_CLAUSE) =
         "name NOT LIKE '%"
 	.Bivio::Type::RealmName::DEMO_CLUB()
@@ -116,84 +121,6 @@ sub count_all_members {
 			    "SELECT count(user_id)
                             FROM realm_user_t")->[0]),
 	    0);
-}
-
-=for html <a name="delete_member_by_name"></a>
-
-=head2 delete_member_by_name(string member) : boolean
-
-Returns true if could delete.  Returns false if the member has
-transactions associated, can't be .
-
-=cut
-
-sub delete_member_by_name {
-    my($self, $member) = @_;
-    my($id) = $self->get('club_id');
-    my($req) = $self->get_request;
-
-    # Find user
-    my($user) = Bivio::Biz::Model::RealmOwner->new($req);
-    $user->unauth_load_or_die(name => $member,
-	   realm_type => Bivio::Auth::RealmType::USER());
-    my($user_id) = $user->get('realm_id');
-
-    # If has txns, can't delete.
-    my($txn_list) = Bivio::Biz::Model::MemberTransactionList->new($req);
-#TODO: This is broken unless $id is $auth_id.  Don't have (or want)
-#      unauth_iterate_start.
-    die('trying to delete outside auth_realm')
-	    unless $id eq $req->get('auth_id');
-    my($it) = $txn_list->iterate_start({parent_id => $user_id});
-    my(%row);
-    my($has_txn) = $txn_list->iterate_next($it, \%row);
-    $txn_list->iterate_end($it);
-    $user->throw_die('user has transactions') if $has_txn;
-
-#TODO: Need to check for owner of Files and admin of Transactions
-
-    # Delete RealmUser first to auth and lock record
-    my($realm_user) = Bivio::Biz::Model::RealmUser->new($req);
-    $realm_user->load(
-	    realm_id => $id, user_id => $user_id);
-    $realm_user->delete();
-    return;
-}
-
-=for html <a name="has_transactions"></a>
-
-=head2 has_transactions() : boolean
-
-=head2 has_transactions(string start_date, string end_date) : boolean
-
-Returns 1 if the club has any accounting transactions.
-
-#TODO: move somewhere else, Accounting::Util?
-
-=cut
-
-sub has_transactions {
-    my($self, $start_date, $end_date) = @_;
-
-    my($sql) = "
-            SELECT COUNT(*)
-            FROM realm_transaction_t
-            WHERE realm_id=?";
-    my($params) = [$self->get('club_id')];
-
-    if (defined($start_date)) {
-	$sql .= "
-            AND realm_transaction_t.date_time
-                BETWEEN $_SQL_DATE_VALUE AND $_SQL_DATE_VALUE";
-	push(@$params, $start_date, $end_date);
-    }
-
-    my($sth) = Bivio::SQL::Connection->execute($sql, $params);
-    my($count) = 0;
-    while (my $row = $sth->fetchrow_arrayref) {
-	$count = $row->[0] || 0;
-    }
-    return $count ? 1 : 0;
 }
 
 =for html <a name="internal_initialize"></a>
