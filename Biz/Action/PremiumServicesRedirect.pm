78a0554223941fc30473b54752384d7d049f979c
akaba <akaba@bivio.biz>
2001-05-16 17:02:32 +0000

    redirects to /hm/services.html

---
+++ Biz/Action/PremiumServicesRedirect.pm
@@ -0,0 +1,69 @@
+# Copyright (c) 2000 bivio Inc.  All rights reserved.
+# $Id$
+package Bivio::Biz::Action::PremiumServicesRedirect;
+use strict;
+$Bivio::Biz::Action::PremiumServicesRedirect::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+$_ = $Bivio::Biz::Action::PremiumServicesRedirect::VERSION;
+
+=head1 NAME
+
+Bivio::Biz::Action::PremiumServicesRedirect - redirects to the premium services page
+
+=head1 SYNOPSIS
+
+    use Bivio::Biz::Action::PremiumServicesRedirect;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio::Biz::Action>
+
+=cut
+
+use Bivio::Biz::Action;
+@Bivio::Biz::Action::PremiumServicesRedirect::ISA = ('Bivio::Biz::Action');
+
+=head1 DESCRIPTION
+
+C<Bivio::Biz::Action::PremiumServicesRedirect> redirects to the premium services page
+
+=cut
+
+#=IMPORTS
+
+#=VARIABLES
+my($_PACKAGE) = __PACKAGE__;
+
+=head1 METHODS
+
+=cut
+
+=for html <a name="execute"></a>
+
+=head2 execute(Bivio::Agent::Request)
+
+Redirects to the taxes page.
+
+=cut
+
+sub execute {
+    my($proto, $req) = @_;
+    $req->client_redirect(Bivio::UI::HTML::Widget->format_uri_static_site(
+	   $req, 'hm/services.html'));
+    # DOES NOT RETURN
+}
+
+#=PRIVATE METHODS
+
+=head1 COPYRIGHT
+
+Copyright (c) 2000 bivio Inc.  All rights reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
