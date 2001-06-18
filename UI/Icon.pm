f96711af9a523c84d913e93682d634d06408e2de
Rob Nagler <github@q33.us>
2001-06-18 18:18:28 +0000

    config now all defaulted no longer a FacadeComponent isa WidgteValueSource

--- UI/Icon.pm
+++ UI/Icon.pm
@@ -17,12 +17,12 @@ Bivio::UI::Icon - returns widget image values
 
 =head1 EXTENDS
 
-L<Bivio::UI::FacadeComponent>
+L<Bivio::UI::WidgetValueSource>
 
 =cut
 
-use Bivio::UI::FacadeComponent;
-@Bivio::UI::Icon::ISA = ('Bivio::UI::FacadeComponent');
+use Bivio::UI::WidgetValueSource;
+@Bivio::UI::Icon::ISA = ('Bivio::UI::WidgetValueSource');
 
 =head1 DESCRIPTION
 
@@ -36,61 +36,49 @@ L<Bivio::UI::HTML::Widget::Image|Bivio::UI::HTML::Widget::Image>.
 See also
 L<Bivio::UI::HTML::Widget::ClearDot|Bivio::UI::HTML::Widget::ClearDot>.
 
-There are two retrieval methods:  L<get_value|"get_value"> and
-L<format_html|"format_html">.
+There are several retrieval methods, but the main ones are
+L<get_value|"get_value"> and L<format_html|"format_html">.
 
-There is no Facade configuration.  All images are stored in the
-root icon directory or sub-directories identified by the
-Facade I<uri>.
+Bivio::UI::Icon is not a Facade component, but icons are facade-based.
+Icons are found with
+L<Bivio::UI::Facade::get_local_file_name|Bivio::UI::Facade/"get_local_file_name">
+in the L<Bivio::UI::LocalFileType-E<gt>PLAIN|Bivio::UI::LocalFileType>
 
-=cut
+See L<handle_config|"handle_config"> for configuration values.
 
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="UNDEF_CONFIG"></a>
-
-=head2 UNDEF_CONFIG : array_ref
-
-Returns config for missing-image.
+This class is initialized by L<Bivio::UI::Facade|Bivio::UI::Facade>.
 
 =cut
-
-sub UNDEF_CONFIG {
-    return '';
-}
 
 #=IMPORTS
-use Bivio::UI::HTML::ViewShortcuts;
 use Bivio::IO::Alert;
 use Bivio::IO::Config;
 use Bivio::IO::Trace;
 use Bivio::UI::Facade;
+use Bivio::UI::LocalFileType;
 use Image::Size ();
+# Bivio::UI::Facade imports this class
 
 #=VARIABLES
-my($_VS) = 'Bivio::UI::HTML::ViewShortcuts';
 use vars qw($_TRACE);
 Bivio::IO::Trace->register;
 my($_PACKAGE) = __PACKAGE__;
-my($_DIR);
-my($_URI);
+my($_URI) = '/i';
 my($_CLEAR_DOT) = {
     uri => '/i/dot.gif',
     height => 1,
     width => 1,
 };
 my($_MISSING) = '/missing-image';
+my($_FILE_SUFFIX_SEARCH_LIST) = ['.gif', '.jpg', '.jpeg'];
 Bivio::IO::Config->register({
-    'uri' => Bivio::IO::Config->REQUIRED,
-    'directory' => Bivio::IO::Config->REQUIRED,
-    'missing_uri' => $_MISSING,
-    'clear_dot_uri' => $_CLEAR_DOT->{uri},
+    uri => $_URI,
+    missing_uri => $_MISSING,
+    clear_dot_uri => $_CLEAR_DOT->{uri},
+    file_suffix_search_list => $_FILE_SUFFIX_SEARCH_LIST,
 });
-# We keep a cache of all values, because FacadeComponent's don't share
-# information--avoids bugs when there are dependencies.  In this case,
-# we cache to avoid repeating not-found errors for each icon and for
+# We keep a cache of all values if Facade.want_local_file_cache is true. In
+# this case, we cache to avoid repeating not-found errors for each icon and for
 # performance (avoids (N-1)xM file reads).
 my(%_CACHE);
 
@@ -102,8 +90,6 @@ my(%_CACHE);
 
 =head2 static format_html(string name, Bivio::Collection::Attributes req_or_facade) : string
 
-=head2 format_html(string name) : string
-
 Returns the image formated for an an C<IMG> tag, e.g.
 
      src="uri" width=W height=H
@@ -113,43 +99,36 @@ Value contains a I<leading space>.
 =cut
 
 sub format_html {
-    return shift->internal_get_value(@_)->{html};
+    return _find(@_)->{html};
 }
 
 =for html <a name="get_clear_dot"></a>
 
-=head2 get_clear_dot() : hash_ref
+=head2 static get_clear_dot() : hash_ref
 
-Please use
-L<Bivio::UI::HTML::Widget::vs_clear_dot|Bivio::UI::Widget/vs_"clear_dot">
-or
-L<Bivio::UI::HTML::Widget::vs_clear_dot_as_html|Bivio::UI::Widget/vs_"clear_dot_as_html">
-to get a clear dot of arbitrary width/height.
+Please use L<Bivio::UI::HTML::Widget::ClearDot|Bivio::UI::HTML::Widget::ClearDot>.
 
-Returns single pixel transparent gif.  Value must be treated as
+Returns single pixel transparent gif.  Value should be treated as
 read-only and is constant.
 
 =cut
 
 sub get_clear_dot {
-    return $_CLEAR_DOT;
+    # Make a copy just in case
+    return {%$_CLEAR_DOT};
 }
 
 =head2 static get_height(string name, Bivio::Collection::Attributes req_or_facade) : int
-
-=head2 get_height(string name) : int
 
 Returns the height of the icon.
 
 =cut
 
 sub get_height {
-    return shift->internal_get_value(@_)->{value}->{height};
+    return _find(@_)->{value}->{height};
 }
 
 =head2 static get_height_as_html(string name, Bivio::Collection::Attributes req_or_facade) : string
-
-=head2 get_height_as_html(string name) : string
 
 Returns the height of the icon in the form of an " height=N" attribute
 to an HTML tag.
@@ -157,7 +136,7 @@ to an HTML tag.
 =cut
 
 sub get_height_as_html {
-    return ' height='.shift->internal_get_value(@_)->{value}->{height};
+    return ' height='._find(@_)->{value}->{height};
 }
 
 =for html <a name="get_value"></a>
@@ -188,24 +167,21 @@ The width of the image.
 =cut
 
 sub get_value {
-    return shift->internal_get_value(@_)->{value};
+    # Make a copy for safety reasons
+    return {%{_find(@_)->{value}}};
 }
 
 =head2 static get_width(string name, Bivio::Collection::Attributes req_or_facade) : int
-
-=head2 get_width(string name) : int
 
 Returns the width of the icon.
 
 =cut
 
 sub get_width {
-    return shift->internal_get_value(@_)->{value}->{width};
+    return _find(@_)->{value}->{width};
 }
 
 =head2 static get_width_as_html(string name, Bivio::Collection::Attributes req_or_facade) : string
-
-=head2 get_width_as_html(string name) : string
 
 Returns the width of the icon in the form of an " width=N" attribute
 to an HTML tag.
@@ -213,7 +189,7 @@ to an HTML tag.
 =cut
 
 sub get_width_as_html {
-    return ' width='.shift->internal_get_value(@_)->{value}->{width};
+    return ' width='._find(@_)->{value}->{width};
 }
 
 =for html <a name="handle_config"></a>
@@ -227,17 +203,20 @@ sub get_width_as_html {
 URI of single pixel transparent gif.
 See L<get_clear_dot|"get_clear_dot">.
 
-=item directory : string (required)
+=item file_suffix_search_list : array_ref [['.gif', '.jpg', '.jpeg']]
 
-Directory in which icons reside.
+Ordered list of file suffices to search for when trying to find an icon.
+Two icons cannot share the same base name, e.g. only one of
+my_icon.gif and my_icon.jpg will be found when looking for I<my_icon>.
 
 =item missing_uri : string [/missing-image]
 
 URI to be used when an icon could not be found.
 
-=item uri : string (required)
+=item uri : string [/i]
 
-URI prefix for icons.
+URI prefix for icons.  The uniquely short name allows for simple
+configuration of URI-based front-end icon serving.
 
 =back
 
@@ -248,132 +227,98 @@ sub handle_config {
     $_URI = $cfg->{uri};
     Bivio::IO::Alert->warn("$_URI: is not absolute") unless $_URI =~ m!^/!;
     $_URI =~ s!([^/])$!$1/!;
-    $_DIR = $cfg->{directory};
-    Bivio::IO::Alert->warn("$_DIR: not a directory") unless -d $_DIR;
-    $_DIR =~ s!([^/])$!$1/!;
     $_MISSING = $cfg->{missing_uri};
     $_CLEAR_DOT->{uri} = $cfg->{clear_dot_uri};
+    $_FILE_SUFFIX_SEARCH_LIST = [map {
+	/^\w+$/ ? '.'.$_ : /^\.\w+$/ ? $_
+		: Bivio::Die->die($_,
+			': bad file_suffix_search_list value (not a word)');
+    } @{$cfg->{file_suffix_search_list}}];
     return;
 }
 
-=for html <a name="handle_register"></a>
+=for html <a name="initialize_by_facade"></a>
 
-=head2 static handle_register()
+=head2 static initialize_by_facade(Bivio::UI::Facade facade)
 
-Registers with Facade.
-
-=cut
-
-sub handle_register {
-    my($proto) = @_;
-    Bivio::UI::Facade->register($proto, ['Text']);
-    return;
-}
-
-=for html <a name="initialization_complete"></a>
-
-=head2 initialization_complete()
-
-Does the initialization from the Facade's URI.
+Initializes all views in a facade, if caching turned on.
 
 =cut
 
-sub initialization_complete {
-    my($self) = @_;
-    my(%map);
-
-    # First find all the files by searching "root" directory then
-    # facade sub directory.
-    foreach my $subdir ('', $self->get_facade->get('uri')) {
-	my($d) = $subdir ? $_DIR.'/'.$subdir : $_DIR;
-	unless (opendir(IN, $d)) {
-	    _trace($d, ": opendir($d): $! (OK to not be found)")
-		    if $_TRACE;
-	    next;
-	}
-
-	# Only look for names that have suffixes and avoid dot files.
-	my($prefix) = $subdir ? $subdir.'/' : '';
-	foreach my $file (grep(/[^\.].*\.\w+$/, readdir(IN))) {
-	    my($name) = $file;
-	    $name =~ s/\.\w+$//;
-	    $map{lc($name)} = $prefix.$file;
-	}
+sub initialize_by_facade {
+    my($proto, $facade) = @_;
+    my($d) = $facade->get_local_file_name(
+	    Bivio::UI::LocalFileType->PLAIN, $_URI);
+    Bivio::Die->die($d, ": $!")
+		unless opendir(IN, $d);
+    foreach my $file (grep(/[^\.].*\.\w+$/, readdir(IN))) {
+	# Only look up files which match search prefixes
+	next unless $file =~ s/(\.\w+)$//
+		&& grep($1 eq $_, @$_FILE_SUFFIX_SEARCH_LIST);
+	_find($proto, $file, $facade);
     }
-    close(IN);
-
-    # Initialize each file.  If we can't get the image, print a
-    # warning.  Only reinitialize those images with different config,
-    # i.e. different file names.
-    while (my($name, $file) = each(%map)) {
-	my($v) = $self->internal_unsafe_lc_get_value($name);
-	unless (defined($v)) {
-	    $self->group($name, $file);
-	}
-	elsif ($v->{config} ne $file) {
-	    $self->regroup($name, $file);
-	}
-    }
-    $self->SUPER::initialization_complete();
-    return;
-}
-
-=for html <a name="internal_initialize_value"></a>
-
-=head2 internal_initialize_value(hash_ref value)
-
-Initializes the internal value from the configuration.
-
-=cut
-
-sub internal_initialize_value {
-    my($self, $value) = @_;
-    my($file) = $value->{config};
-
-    my($f) = $_DIR.'/'.$file;
-    if ($file ne UNDEF_CONFIG()) {
-
-	# Is it in the cache?
-	if ($_CACHE{$f}) {
-	    my($cv) = $_CACHE{$f};
-	    $value->{value} = $cv->{value};
-	    $value->{html} = $cv->{html};
-	    return;
-	}
-
-	# Try to read the file
-	my($w, $h, $err) = Image::Size::imgsize($f);
-	if (defined($w)) {
-	    # Valid image.  Save values.
-	    my($u) = $_URI.$file;
-	    $value->{value} = {
-		uri => $u,
-		width => $w,
-		height => $h,
-	    };
-	    $value->{html} = qq! src="$u" width=$w height=$h!;
-	    $_CACHE{$f} = {%{$value}};
-	    return;
-	}
-
-	# Let 'em know and then put undef in the table.
-	$self->bad_value($value, 'Image::Size error: ', $err);
-    }
-
-    # Invalid or UNDEF_CONFIG
-    $value->{value} = {
-	uri => $_MISSING,
-	width => 1,
-	height => 1,
-    };
-    $value->{html} = qq! src="$_MISSING" width=1 height=1!;
-
-    # We cache misses to avoid lots of noise
-    $_CACHE{$f} = {%{$value}};
     return;
 }
 
 #=PRIVATE METHODS
+
+# _find(proto, string name, Bivio::Collection::Attributes req_or_facade) : hash_ref
+#
+# Returns the value hash_ref
+#
+sub _find {
+    my($proto, $name, $req_or_facade) = @_;
+    my($facade) = Bivio::UI::Facade->get_from_request_or_self($req_or_facade);
+
+    # NOTE: We cache without the suffix.
+    my($file_name) = $facade->get_local_file_name(
+	    Bivio::UI::LocalFileType->PLAIN, $_URI.$name);
+
+    # Is it in the cache?
+    my($cache) = $facade->get('want_local_file_cache');
+    return $_CACHE{$file_name} if $cache && $_CACHE{$file_name};
+
+    my($w, $h, $err);
+    foreach my $suffix (@$_FILE_SUFFIX_SEARCH_LIST) {
+	# Try to read the file
+	my($f) = $file_name.$suffix;
+	next unless -r $f;
+
+	($w, $h, $err) = Image::Size::imgsize($f);
+	next unless defined($w);
+
+	# Valid image.  Save values.
+	my($u) = $_URI.$name.$suffix;
+	my($value) = {
+	    value => {
+		uri => $u,
+		width => $w,
+		height => $h,
+	    },
+	    html => qq! src="$u" width=$w height=$h!,
+	};
+	$_CACHE{$file_name} = $value if $cache;
+	return $value;
+    }
+
+    # Some type of error
+    Bivio::IO::Alert->warn(
+	    $facade, '.Icon.', $name,
+	    ($err ? (': Image::Size error: ', $err) : ': not found'));
+
+    my($value) = {
+	value => {
+	    uri => $_MISSING,
+	    width => 1,
+	    height => 1,
+	},
+	html => qq! src="$_MISSING" width=1 height=1!,
+    };
+
+    # We cache misses to avoid lots of noise
+    $_CACHE{$file_name} = $value if $cache;
+    return $value;
+}
 
 =head1 COPYRIGHT
 
