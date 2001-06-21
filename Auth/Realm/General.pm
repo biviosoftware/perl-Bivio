593e66f9092136b2ed815468f84263e28accddcf
Paul Moeller <moeller@bivio.biz>
2001-06-21 20:24:08 +0000

    added 'use_permissions' configuration attribute, avoids check permissions database for apps which don't require it

--- Auth/Realm/General.pm
+++ Auth/Realm/General.pm
@@ -32,9 +32,14 @@ the one without a specific owner.
 =cut
 
 #=IMPORTS
+use Bivio::IO::Config;
 
 #=VARIABLES
 my($_SELF);
+my($_USE_PERMISSIONS) = 1;
+Bivio::IO::Config->register({
+    use_permissions => 1,
+});
 
 =head1 FACTORIES
 
@@ -55,6 +60,20 @@ sub new {
 
 =cut
 
+=for html <a name="can_user_execute_task"></a>
+
+=head2 can_user_execute_task(Bivio::Agent::Task task, Bivio::Agent::Request req) : boolean
+
+Returns true if I<auth_user> of I<req> can execute I<task>.
+
+=cut
+
+sub can_user_execute_task {
+    my($self, $task, $req) = @_;
+    return 1 unless $_USE_PERMISSIONS;
+    return $self->SUPER::can_user_execute_task($task, $req);
+}
+
 =for html <a name="get_instance"></a>
 
 =head2 static get_instance() : Bivio::Auth::Realm::General
@@ -67,6 +86,27 @@ sub get_instance {
     my($proto) = @_;
     $_SELF = $proto->new unless $_SELF;
     return $_SELF;
+}
+
+=for html <a name="handle_config"></a>
+
+=head2 static handle_config(hash cfg)
+
+=over 4
+
+=item use_permissions : boolean [1]
+
+Are GENERAL permissions checked prior to executing the task? This allows
+avoiding a database call for apps which don't require one.
+
+=back
+
+=cut
+
+sub handle_config {
+    my(undef, $cfg) = @_;
+    $_USE_PERMISSIONS = $cfg->{use_permissions};
+    return;
 }
 
 #=PRIVATE METHODS
