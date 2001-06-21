b5ff9fd44d04bdfd8981ae9ede5451d36831f7d7
Rob Nagler <github@q33.us>
2001-06-21 08:31:23 +0000

    prefix view code with 'use strict; '

--- UI/ViewLanguage.pm
+++ UI/ViewLanguage.pm
@@ -366,8 +366,9 @@ sub _die {
 sub _eval_code {
     my($code) = @_;
 #    _assert_in_eval('eval');
-    _trace($code) if $_TRACE;
-    return Bivio::Die->eval_or_die($code);
+    my($copy) = 'use strict; '.$$code;
+    _trace($copy) if $_TRACE;
+    return Bivio::Die->eval_or_die(\$copy);
 }
 
 # _eval_view(Bivio::UI::View view) : Bivio::Die
