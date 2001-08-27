7b0b689423f2454a20f16c00a7ff7dfe04295149
Rob Nagler <github@q33.us>
2001-08-27 04:30:44 +0000

    Release scope

--- PetShop/Widget/Search.pm
+++ PetShop/Widget/Search.pm
@@ -9,6 +9,10 @@ $_ = $Bivio::PetShop::Widget::Search::VERSION;
 
 Bivio::PetShop::Widget::Search - search box and button
 
+=head1 RELEASE SCOPE
+
+bOP
+
 =head1 SYNOPSIS
 
     use Bivio::PetShop::Widget::Search;
