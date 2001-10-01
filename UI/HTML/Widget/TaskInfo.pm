6fe318c12f110b9842f9878bb2c08b912656eff3
Paul Moeller <moeller@bivio.biz>
2001-10-01 18:16:08 +0000

    part of bOP release

--- UI/HTML/Widget/TaskInfo.pm
+++ UI/HTML/Widget/TaskInfo.pm
@@ -9,6 +9,10 @@ $_ = $Bivio::UI::HTML::Widget::TaskInfo::VERSION;
 
 Bivio::UI::HTML::Widget::TaskInfo - displays the current task information
 
+=head1 RELEASE SCOPE
+
+bOP
+
 =head1 SYNOPSIS
 
     use Bivio::UI::HTML::Widget::TaskInfo;
