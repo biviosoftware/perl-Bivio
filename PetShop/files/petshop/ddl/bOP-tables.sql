1dfc1bc29565549c0e9e7aeb368738d22a8e187f
Paul Moeller <moeller@bivio.biz>
2001-10-08 15:16:27 +0000

    NUMBER -> NUMERIC, added primary key and NOT NULL constraints to support Postgres

--- PetShop/files/petshop/ddl/bOP-tables.sql
+++ PetShop/files/petshop/ddl/bOP-tables.sql
@@ -12,65 +12,72 @@
 --
 ----------------------------------------------------------------
 CREATE TABLE club_t (
-  club_id NUMBER(18),
-  start_date DATE
+  club_id NUMERIC(18),
+  start_date DATE,
+  CONSTRAINT club_t1 PRIMARY KEY(club_id)
 )
 /
 
 CREATE TABLE db_upgrade_t (
-  version VARCHAR2(30),
-  run_date_time DATE
+  version VARCHAR(30),
+  run_date_time DATE NOT NULL,
+  CONSTRAINT db_upgrade_t1 PRIMARY KEY(version)
 )
 /
 
 CREATE TABLE email_t (
-  realm_id NUMBER(18),
-  location NUMBER(2),
-  email VARCHAR2(100),
-  want_bulletin NUMBER(1)
+  realm_id NUMERIC(18) NOT NULL,
+  location NUMERIC(2) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  want_bulletin NUMERIC(1) NOT NULL,
+  CONSTRAINT email_t1 PRIMARY KEY(realm_id, location)
 )
 /
 
 CREATE TABLE lock_t (
-  realm_id NUMBER(18) primary key
+  realm_id NUMERIC(18) primary key
 )
 /
 
 CREATE TABLE realm_owner_t (
-  realm_id NUMBER(18),
-  name VARCHAR2(30),
-  password VARCHAR2(30),
-  realm_type NUMBER(2),
-  display_name VARCHAR2(100),
-  creation_date_time DATE
+  realm_id NUMERIC(18),
+  name VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  realm_type NUMERIC(2) NOT NULL,
+  display_name VARCHAR(100) NOT NULL,
+  creation_date_time DATE NOT NULL,
+  CONSTRAINT realm_owner_t1 PRIMARY KEY(realm_id)
 )
 /
 
 CREATE TABLE realm_role_t (
-  realm_id NUMBER(18),
-  role NUMBER(2),
-  permission_set CHAR(30)
+  realm_id NUMERIC(18) NOT NULL,
+  role NUMERIC(2) NOT NULL,
+  permission_set CHAR(30) NOT NULL,
+  CONSTRAINT realm_role_t1 PRIMARY KEY(realm_id, role)
 )
 /
 
 CREATE TABLE realm_user_t (
-  realm_id NUMBER(18),
-  user_id NUMBER(18),
-  role NUMBER(2),
-  honorific NUMBER(2),
-  creation_date_time DATE
+  realm_id NUMERIC(18) NOT NULL,
+  user_id NUMERIC(18) NOT NULL,
+  role NUMERIC(2) NOT NULL,
+  honorific NUMERIC(2) NOT NULL,
+  creation_date_time DATE NOT NULL,
+  CONSTRAINT realm_user_t1 PRIMARY KEY(realm_id, user_id)
 )
 /
 
 CREATE TABLE user_t (
-  user_id NUMBER(18),
-  first_name VARCHAR2(30),
-  first_name_sort VARCHAR2(30),
-  middle_name VARCHAR2(30),
-  middle_name_sort VARCHAR2(30),
-  last_name VARCHAR2(30),
-  last_name_sort VARCHAR2(30),
-  gender NUMBER(1),
-  birth_date DATE
+  user_id NUMERIC(18),
+  first_name VARCHAR(30),
+  first_name_sort VARCHAR(30),
+  middle_name VARCHAR(30),
+  middle_name_sort VARCHAR(30),
+  last_name VARCHAR(30),
+  last_name_sort VARCHAR(30),
+  gender NUMERIC(1) NOT NULL,
+  birth_date DATE,
+  CONSTRAINT user_t1 PRIMARY KEY(user_id)
 )
 /
