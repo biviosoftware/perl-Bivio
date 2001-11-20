f28fdafccaf1d9928dd1364df7d4a270a7e3a055
Rob Nagler <github@q33.us>
2001-11-20 04:12:16 +0000

    Version

---
+++ bOP.pm
@@ -0,0 +1,62 @@
+# Copyright (c) 2001 bivio Software Artisans Inc.  All Rights reserved.
+# $Id$
+package Bivio::bOP;
+use strict;
+$Bivio::bOP::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+$_ = $Bivio::bOP::VERSION;
+
+=head1 NAME
+
+Bivio::bOP - bivio OLTP Platform (bOP) overview and version
+
+=head1 RELEASE SCOPE
+
+bOP
+
+=head1 SYNOPSIS
+
+    use Bivio::bOP;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio>
+
+=cut
+
+use Bivio;
+@Bivio::bOP::ISA = ('Bivio');
+
+=head1 DESCRIPTION
+
+C<Bivio::bOP>
+
+=cut
+
+#=IMPORTS
+
+#=VARIABLES
+my($_PACKAGE) = __PACKAGE__;
+
+=head1 METHODS
+  Renamed bivio.net -> bivio.biz
+  Added Bivio::bOP as source of version number
+  Bug fixes and enhancements which we'll try to keep better track of.
+
+
+=cut
+
+Copyright (c) 2001 bivio Software Artisans Inc.  All Rights reserved.
+
+=head1 COPYRIGHT
+
+$Id$
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
