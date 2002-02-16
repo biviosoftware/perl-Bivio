4258914b1bd32eae9f1e396f6eb64c2985f56ae7
Paul Moeller <moeller@bivio.biz>
2002-02-16 01:02:04 +0000

    fixed warn::

--- Util/HTTPLog.pm
+++ Util/HTTPLog.pm
@@ -243,7 +243,7 @@ sub _initialize {
 	    "can't login as offline user",
 	    'Bivio::Data::EW::ClubImporter::_parse_tax_id.*changed to',
 	    "MemberAllocationList.*report_date isn't on year-end",
-	    'warn::.*incorrect imported allocations',
+	    'warn:.*incorrect imported allocations',
 	    "::_create_stock_transfer_entry.*Couldn't find related stock",
 	    'HTTP::Form::parse.*unknown form Content-Type: <undef>',
 	    '::warn:\d+ large audit, \d+ entries',
