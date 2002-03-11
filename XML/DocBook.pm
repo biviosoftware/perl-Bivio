1a6d5eaea62af021009a3a6878203b65c6ad3792
Rob Nagler <github@q33.us>
2002-03-11 15:20:57 +0000

    Added tag lookup in context of parent_tag Refactored names of methods to be clearer Added more tags (sect1, literal, blockquote, etc.)

--- XML/DocBook.pm
+++ XML/DocBook.pm
@@ -63,17 +63,24 @@ use HTML::Entities ();
 use XML::Parser ();
 
 #=VARIABLES
-my($_TO_HTML) = _to_html_compile({
+my($_XML_TO_HTML_PROGRAM) = _compile_program({
     attribution => {
 	prefix => '<div align=right>-- ',
 	suffix => '</div>',
     },
+    blockquote => ['blockquote'],
+    'chapter/title' => ['h1'],
     chapter => ['html', 'body'],
+    command => ['tt'],
     emphasis => ['b'],
     epigraph => [],
+    function => ['tt'],
+    literal => ['tt'],
     para => ['p'],
+    programlisting => ['blockquote', 'pre'],
+    sect1 => [],
+    'sect1/title' => ['h2'],
     simplesect => [],
-    title => ['h1'],
 });
 
 =head1 METHODS
@@ -90,60 +97,84 @@ Converts I<xml_file> from XML to HTML.
 
 sub to_html {
     my($self, $xml_file) = @_;
-    return _to_html(XML::Parser->new(Style => 'Tree')->parsefile($xml_file));
+    return _to_html(
+	'', XML::Parser->new(Style => 'Tree')->parsefile($xml_file));
 }
 
 #=PRIVATE METHODS
 
-# _to_html(array_ref tree) : string_ref
+# _compile_program(hash_ref config) : hash_ref
 #
-# Convert XML tree into HTML.
+# Creates the $_XML_TO_HTML_PROGRAM hash from $config, which is a mapping of
+# XML tags to HTML commands.  If the HTML command is an array_ref, calls
+# _compile_tags_to_html to create the prefix and suffix.
 #
-sub _to_html {
-    my($tree) = @_;
-    my($res) = '';
-    $res .= _to_html_node(splice(@$tree, 0, 2)) while @$tree;
-    return \$res;
-}
-
-# _to_html_compile(hash_ref config) : hash_ref
-#
-# Creates the $_TO_HTML hash from $config, which is a mapping of XML tags to
-# HTML commands.  If the HTML command is an array_ref, calls _to_html_tags to
-# create the prefix and suffix.
-#
-sub _to_html_compile {
+sub _compile_program {
     my($config) = @_;
     while (my($xml, $html) = each(%$config)) {
 	$config->{$xml} = {
-	    prefix => _to_html_tags($html, ''),
-	    suffix => _to_html_tags([reverse(@$html)], '/'),
+	    prefix => _compile_tags_to_html($html, ''),
+	    suffix => _compile_tags_to_html([reverse(@$html)], '/'),
 	} if ref($html) eq 'ARRAY';
     }
     return $config;
 }
 
-# _to_html_node(string tag, array_ref tree) : string
-#
-# Lookup $tag in $_TO_HTML and evaluate.
-#
-sub _to_html_node {
-    my($tag, $tree) = @_;
-    return HTML::Entities::encode($tree) unless $tag;
-    die($tag, ': unhandled tag') unless my $op = $_TO_HTML->{$tag};
-    # We ignore the attributes for now,
-    shift(@$tree);
-    return $op->{prefix} . ${_to_html($tree)} . $op->{suffix};
-}
-
-# _to_html_tags(array_ref names, string prefix) : string
+# _compile_tags_to_html(array_ref names, string prefix) : string
 #
 # Converts @$names to HTML tags with prefix ('/' or ''), and concatenates
 # the tags into a string.
 #
-sub _to_html_tags {
+sub _compile_tags_to_html {
     my($names, $prefix) = @_;
     return join('', map {"<$prefix$_>"} @$names);
+}
+
+# _eval_child(string tag, array_ref children, string parent_tag) : string
+#
+# Lookup $tag in context of $parent_tag to find operator, evaluate $children,
+# and then evaluate the found operator.  Returns the result of _eval_op.
+#
+sub _eval_child {
+    my($tag, $children, $parent_tag) = @_;
+    return HTML::Entities::encode($children) unless $tag;
+    # We ignore the attributes for now.
+    shift(@$children);
+    return _eval_op(
+	_lookup_op($tag, $parent_tag),
+	_to_html($tag, $children));
+}
+
+# _eval_op(hash_ref op, string_ref html) : string
+#
+# Surround $html with prefix and suffix from $op.  Return concatenation.
+#
+sub _eval_op {
+    my($op, $html) = @_;
+    return $op->{prefix} . $$html . $op->{suffix};
+}
+
+# _lookup_op(string tag, string parent_tag) : hash_ref
+#
+# Lookup $parent_tag/$tag or $tag in $_XML_TO_HTML_PROGRAM and return.
+# Dies if not found.
+#
+sub _lookup_op {
+    my($tag, $parent_tag) = @_;
+    return $_XML_TO_HTML_PROGRAM->{"$parent_tag/$tag"}
+	|| $_XML_TO_HTML_PROGRAM->{$tag}
+	|| die("$parent_tag/$tag: unhandled tag");
+}
+
+# _to_html(string tag, array_ref children) : string_ref
+#
+# Concatenate evaluation of $children and return the resultant HTML.
+#
+sub _to_html {
+    my($tag, $children) = @_;
+    my($res) = '';
+    $res .= _eval_child(splice(@$children, 0, 2), $tag) while @$children;
+    return \$res;
 }
 
 =head1 COPYRIGHT
