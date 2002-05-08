4cbdc2762f472b1ac7f3c8d8b43eb33ce4f9a4cd
Rob Nagler <github@q33.us>
2002-05-08 22:22:39 +0000

    added recursive searching

--- Test/Util.pm
+++ Test/Util.pm
@@ -46,9 +46,11 @@ which is a subclass of L<Bivio::Test::Language|Bivio::Test::Language>.
 =head2 USAGE : string
 
 Returns:
-  usage: b-test [options] command [args...]
-  commands:
-      unit test_files... -- runs the tests under Test::Harness
+ usage: b-test [options] command [args...]
+ commands:
+    accept tests... - runs the tests (.btest) under Bivio::Test::Language
+    cleanup tests... - runs the cleanup function of tests (.btest) 
+    unit tests/dirs... -- runs the tests (*.t) under Test::Harness
 
 =cut
 
@@ -58,13 +60,16 @@ usage: b-test [options] command [args...]
 commands:
     accept tests... - runs the tests (.btest) under Bivio::Test::Language
     cleanup tests... - runs the cleanup function of tests (.btest) 
-    unit tests... -- runs the tests (.t) under Test::Harness
+    unit tests/dirs... -- runs the tests (*.t) under Test::Harness
 EOF
 }
 
 #=IMPORTS
 use Test::Harness ();
 use Bivio::Test::Language;
+use File::Find ();
+use File::Spec ();
+use Bivio::IO::File;
 
 #=VARIABLES
 
@@ -100,15 +105,34 @@ sub cleanup {
 
 =for html <a name="unit"></a>
 
-=head2 unit(string test_file, ...)
+=head2 unit(string test, ...)
 
-Executes I<test_file>(s) by calling C<Test::Harness::runtests>.
+Executes I<test>(s) by calling C<Test::Harness::runtests>.  I<test> may be
+a directory or file name.  If it is a directory, all tests (C<*.t>) files
+will be executed.  All tests must end in C<*.t>.
 
 =cut
 
 sub unit {
-    my($self, @test_file) = @_;
-    Test::Harness::runtests(@test_file);
+    my($self, @test) = @_;
+    $self->usage_error('must supply test files or directories') unless @test;
+    my($tests) = {};
+    File::Find::find({
+	no_chdir => 1,
+	wanted => sub {
+	    return unless $_ =~ /\.t$/ && -r $_;
+	    my(undef, $d, $f) = File::Spec->splitpath($_);
+	    push(@{$tests->{$d} ||= []}, $f);
+	    return;
+	}},
+	@test);
+    foreach my $d (sort(keys(%$tests))) {
+	$self->print("*** Entering: $d\n");
+	# Test::Harness is not re-entrant
+	system("cd '$d' && perl -e 'use Test::Harness; runtests(qw{"
+	    . join(' ', sort(@{$tests->{$d}})) . "})'");
+	$self->print("*** Leaving: $d\n\n");
+    }
     return;
 }
 
