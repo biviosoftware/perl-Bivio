1912797df121c0beeb28cbc782ffe1a179ee0081
Paul Moeller <moeller@bivio.biz>
2002-05-30 22:43:15 +0000

    added code back until we disallow method name conflicts

--- ShellUtil.pm
+++ ShellUtil.pm
@@ -984,7 +984,11 @@ sub _method_ok {
     my($self, $method) = @_;
     return 0 unless $method =~ /^([a-z]\w*)$/i;
     return 0 if $method =~ /^handle_/;
-    return 0 unless $self->can($method);
+#    return 0 unless $self->can($method);
+    my($can) = $self->can($method);
+    return 0 unless $can;
+    return 1 if $can eq \&{ref($self).'::'.$method};
+
     return 0 if ref($self) eq __PACKAGE__;
     return 1 if $method eq 'usage';
     return 0 if __PACKAGE__->can($method);
