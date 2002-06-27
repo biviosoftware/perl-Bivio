eb9b05da2bb082a50ee5790bdf0ac121b0bd0cb0
Rob Nagler <github@q33.us>
2002-06-27 17:43:34 +0000

    * Bivio-bOP.spec makes html publicly readable * Bivio::Agent::Request set_user/set_realm return their args * Bivio::Test::Language::HTTP supports HTTP acceptance testing * Bivio::BConf include Bivio::Test::Language::HTTP * Bivio::PetShop::Test::PetShop uses Bivio::Test::Language::HTTP * Bivio::Test::HTMLParser::Forms/Links now work * Bivio::Test::Language added logging * Bivio::Test cases accept a code_ref for expected which does compare

--- bOP.pm
+++ bOP.pm
@@ -30,7 +30,7 @@ Model-View-Controller (MVC) architecture.  At the lowest level, bOP provides a
 cohesive infrastructure for any Perl application.
 
 We'll be writing more here later.  Please visit
-http://www.bivio.biz for more info. 
+http://www.bivio.biz for more info.
 
 =cut
 
@@ -41,6 +41,9 @@ http://www.bivio.biz for more info.
 =head1 CHANGES
 
   $Log$
+  * Bivio::Test::HTMLParser::Forms/Links now work
+  * Bivio::Test::Language added logging
+  * Bivio::Test cases accept a code_ref for expected which does compare
 
   Revision 1.17  2002/06/22 22:32:54  nagler
   * PetShop facade not "is_production"
