14345143b9eadfe87119ea3d92933b5dcf2f7551
Paul Moeller <moeller@bivio.biz>
2002-08-01 18:36:54 +0000

    code cleanup

--- PetShop/Model/UserAccountForm.pm
+++ PetShop/Model/UserAccountForm.pm
@@ -35,6 +35,14 @@ C<Bivio::PetShop::Model::UserAccountForm> creates a new user account.
 =cut
 
 #=IMPORTS
+use Bivio::Auth::RealmType;
+use Bivio::Die;
+use Bivio::PetShop::Type::EntityLocation;
+use Bivio::PetShop::Type::UserStatus;
+use Bivio::PetShop::Type::UserType;
+use Bivio::Type::Honorific;
+use Bivio::Type::Location;
+use Bivio::Type::Password;
 
 #=VARIABLES
 
@@ -55,28 +63,26 @@ sub execute_empty {
     return unless _is_editing($self);
 
     my($req) = $self->get_request;
-    my($realm) = $req->get('auth_user');
-    $self->load_from_model_properties($realm);
+    $self->load_from_model_properties($req->get('auth_user'));
 
     # create a default account if necessary
-    my($account) = Bivio::Biz::Model->new($req, 'UserAccount');
+    my($account) = $self->new($req, 'UserAccount');
     unless ($account->unsafe_load) {
-	$account = _create_default_account($realm);
+	$account = _create_default_account($self);
     }
 
     foreach my $model (qw(EntityAddress EntityPhone)) {
 	$self->load_from_model_properties(
-		Bivio::Biz::Model->new($req, $model)->load({
+		$self->new($req, $model)->load({
 		    entity_id => $account->get('entity_id'),
 		    location => Bivio::PetShop::Type::EntityLocation
 		    ->PRIMARY,
 		}));
     }
 
+    $self->load_from_model_properties($self->new($req, 'User')->load);
     $self->load_from_model_properties(
-	    Bivio::Biz::Model->new($req, 'User')->load);
-    $self->load_from_model_properties(
-	    Bivio::Biz::Model->new($req, 'Email')->load({
+	    $self->new($req, 'Email')->load({
 		location => Bivio::Type::Location->HOME,
 	    }));
     return;
@@ -94,31 +100,25 @@ sub execute_ok {
     my($self) = @_;
     my($req) = $self->get_request;
 
-    my($is_editing) = _is_editing($self);
-    _create_user($self) unless $is_editing;
+    _create_user($self)
+	unless _is_editing($self);
 
-    my($realm) = $req->get('auth_user');
-    my($account) = Bivio::Biz::Model->new($req, 'UserAccount')->load;
+    my($account) = $self->new($req, 'UserAccount')->load;
 
     foreach my $model (qw(User Email)) {
-	my($values) = $self->get_model_properties($model);
-
-	$values->{location} = Bivio::Type::Location->HOME
-		if $model eq 'Email';
-
-	Bivio::Biz::Model->new($req, $model)->load->update($values);
+	$self->new($req, $model)->load->update(
+	    $self->get_model_properties($model));
     }
 
     foreach my $model (qw(EntityAddress EntityPhone)) {
-	Bivio::Biz::Model->new($req, $model)->load({
+	$self->new($req, $model)->load({
 	    entity_id => $account->get('entity_id'),
 	    location => Bivio::PetShop::Type::EntityLocation->PRIMARY,
 	})->update($self->get_model_properties($model));
     }
 
-    die("invalid password")
-	    unless $realm->has_valid_password;
-
+    Bivio::Die->die("invalid password")
+	    unless $req->get('auth_user')->has_valid_password;
     return;
 }
 
@@ -190,32 +190,27 @@ sub validate {
 
 #=PRIVATE METHODS
 
-# _create_default_account(Bivio::Biz::Model::RealmOwner realm)
+# _create_default_account(self)
 #
 # Creates the default account/address/phone entries.
 #
 sub _create_default_account {
-    my($realm) = @_;
-    my($req) = $realm->get_request;
+    my($self) = @_;
+    my($req) = $self->get_request;
 
-    my($entity) = Bivio::Biz::Model->new($req, 'Entity')->create({});
-
-    my($account) = Bivio::Biz::Model->new($req, 'UserAccount')->create({
-	user_id => $realm->get('realm_id'),
-	entity_id => $entity->get('entity_id'),
+    my($account) = $self->new($req, 'UserAccount')->create({
+	user_id => $req->get('auth_user_id'),
+	entity_id => $self->new($req, 'Entity')->create->get('entity_id'),
 	status => Bivio::PetShop::Type::UserStatus->CUSTOMER,
 	user_type => Bivio::PetShop::Type::UserType->HOME_CONSUMER,
     });
 
-    Bivio::Biz::Model->new($req, 'EntityAddress')->create({
-	entity_id => $entity->get('entity_id'),
-	location => Bivio::PetShop::Type::EntityLocation->PRIMARY,
-    });
-
-    Bivio::Biz::Model->new($req, 'EntityPhone')->create({
-	entity_id => $entity->get('entity_id'),
-	location => Bivio::PetShop::Type::EntityLocation->PRIMARY,
-    });
+    foreach my $model (qw(EntityAddress EntityPhone)) {
+	$self->new($req, $model)->create({
+	    entity_id => $account->get('entity_id'),
+	    location => Bivio::PetShop::Type::EntityLocation->PRIMARY,
+	});
+    }
     return $account;
 }
 
@@ -227,36 +222,37 @@ sub _create_user {
     my($self) = @_;
     my($req) = $self->get_request;
 
-    my($user) = Bivio::Biz::Model->new($req, 'User')->create(
+    my($user) = $self->new($req, 'User')->create(
 	    $self->get_model_properties('User'));
 
-    my($values) = $self->get_model_properties('RealmOwner');
-    my($realm) = Bivio::Biz::Model->new($req, 'RealmOwner')->create({
-	%$values,
+    my($realm) = $self->new($req, 'RealmOwner')->create({
+	%{$self->get_model_properties('RealmOwner')},
 	realm_id => $user->get('user_id'),
 	realm_type => Bivio::Auth::RealmType->USER,
-	password => Bivio::Type::Password->encrypt($values->{password}),
+	password => Bivio::Type::Password->encrypt(
+	    $self->get('RealmOwner.password')),
     });
 
-    Bivio::Biz::Model->new($req, 'RealmUser')->create({
+    $self->new($req, 'RealmUser')->create({
 	realm_id => $user->get('user_id'),
 	user_id => $user->get('user_id'),
 	honorific => Bivio::Type::Honorific->SELF,
     });
 
-    Bivio::Biz::Model->new($req, 'Email')->create({
+    $self->new($req, 'Email')->create({
 	realm_id => $user->get('user_id'),
 	location => Bivio::Type::Location->HOME,
 	%{$self->get_model_properties('Email')},
     });
 
     # log the user in
-    Bivio::Biz::Model->get_instance('UserLoginForm')->execute($req, {
+    $self->get_instance('UserLoginForm')->execute($req, {
 	realm_owner => $realm,
     });
 
-    $req->set_realm(Bivio::Auth::Realm->new($realm));
-    _create_default_account($realm);
+    $req->set_user($realm);
+    $req->set_realm($realm);
+    _create_default_account($self);
     return;
 }
 
