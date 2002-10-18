b942f58c7a8c13610c2c44c45d20b5d3a3694052
Rob Nagler <github@q33.us>
2002-10-18 05:17:56 +0000

    * Bivio::UI::HTML::Format::Printf added

--- bOP.pm
+++ bOP.pm
@@ -30,7 +30,7 @@ Model-View-Controller (MVC) architecture.  At the lowest level, bOP provides a
 cohesive infrastructure for any Perl application.
 
 We'll be writing more here later.  Please visit
-http://www.bivio.biz for more info.
+http://www.bivio.biz for more info. 
 
 =cut
 
@@ -41,6 +41,9 @@ http://www.bivio.biz for more info.
 =head1 CHANGES
 
   $Log$
+
+  Revision 1.25  2002/10/18 05:17:56  nagler
+  * Bivio::UI::HTML::Format::Printf added
 
   Revision 1.24  2002/10/18 03:27:00  nagler
   * Bivio::Biz::Action::MailReceiveStatus missed last release
