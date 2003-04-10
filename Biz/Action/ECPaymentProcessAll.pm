c55509456eceb3f49920d3b0e8efe98882201ea2
Paul Moeller <moeller@bivio.biz>
2003-04-10 23:07:15 +0000

    added config to determine which module does the credit card processing

--- Biz/Action/ECPaymentProcessAll.pm
+++ Biz/Action/ECPaymentProcessAll.pm
@@ -39,17 +39,43 @@ one payment at a time.
 #=IMPORTS
 use Bivio::Agent::Task;
 use Bivio::Agent::TaskId;
-use Bivio::Biz::Action::ECCreditCardProcessor;
+use Bivio::IO::ClassLoader;
+use Bivio::IO::Config;
 use Bivio::IO::Trace;
 use Bivio::Type::ECPaymentStatus;
 
 #=VARIABLES
 use vars ('$_TRACE');
 Bivio::IO::Trace->register;
+my($_PROCESSOR);
+Bivio::IO::Config->register({
+    processor => 'Bivio::Biz::Action::ECCreditCardProcessor',
+});
 
 =head1 METHODS
 
 =cut
+
+=for html <a name="handle_config"></a>
+
+=head2 static handle_config(hash cfg)
+
+=over 4
+
+=item processor : string [Bivio::Biz::Action::ECCreditCardProcessor]
+
+The module which handles the credit card processing.
+
+=back
+
+=cut
+
+sub handle_config {
+    my(undef, $cfg) = @_;
+    $_PROCESSOR = $cfg->{processor};
+    Bivio::IO::ClassLoader->simple_require($_PROCESSOR);
+    return;
+}
 
 =for html <a name="internal_execute"></a>
 
@@ -65,14 +91,14 @@ sub internal_execute {
     my($self, $req) = @_;
     # check batch before.  Sometimes there is an error downloading
     # the status, and we have accidentally resubmitted a payments.
-    Bivio::Biz::Action::ECCreditCardProcessor->check_transaction_batch($req);
+    $_PROCESSOR->check_transaction_batch($req);
     my($ecp) = Bivio::Biz::Model->new($req, 'ECPayment');
     my($it) = $ecp->unauth_iterate_start('creation_date_time asc',
 	{status => Bivio::Type::ECPaymentStatus->needs_processing_list});
     while ($ecp->iterate_next_and_load($it)) {
         $req->set_user($ecp->get('user_id'));
         $req->set_realm($ecp->get('realm_id'));
-	Bivio::Biz::Action::ECCreditCardProcessor->execute_process($req);
+        $_PROCESSOR->execute_process($req);
     }
     $ecp->iterate_end($it);
     return 0;
