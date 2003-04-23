ea2b5a082bba7d4597eac69cea484ee06340d834
Paul Moeller <moeller@bivio.biz>
2003-04-23 22:42:20 +0000

    added warning if Type->from_literal() called in scalar context allow facades URIs with '.'

--- bOP.pm
+++ bOP.pm
@@ -30,7 +30,7 @@ Model-View-Controller (MVC) architecture.  At the lowest level, bOP provides a
 cohesive infrastructure for any Perl application.
 
 We'll be writing more here later.  Please visit
-http://www.bivio.biz for more info. 
+http://www.bivio.biz for more info.
 
 =cut
 
@@ -41,6 +41,9 @@ http://www.bivio.biz for more info.
 =head1 CHANGES
 
   $Log$
+  Revision 1.60  2003/04/23 22:42:20  moeller
+  added warning if Type->from_literal() called in scalar context
+  allow facades URIs with '.'
 
   Revision 1.59  2003/04/15 23:20:39  moeller
   readonly text fields are 'disabled' again
