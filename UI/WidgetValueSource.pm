d9f8a11fa61224b0999ef302ba753e62234437e8
Rob Nagler <github@q33.us>
2004-02-23 13:43:08 +0000

    added recursive get of widget values

--- UI/WidgetValueSource.pm
+++ UI/WidgetValueSource.pm
@@ -420,9 +420,17 @@ sub _die {
 #
 sub _eval_args {
     my($self) = shift;
-    return map {
-	ref($_) eq 'ARRAY' ? $self->get_widget_value(@$_) : $_;
-    } @_;
+    return map({
+	my($x) = $_;
+	my($i) = 10;
+	while (ref($x) eq 'ARRAY') {
+	    $x = $self->get_widget_value(@$x);
+	    Bivio::Die->die(
+		'infinite loop trying to unwind widget value: ', $_,
+	    ) if --$i < 0;
+	}
+	$x;
+    } @_);
 }
 
 =head1 COPYRIGHT
