f7c4583d77083d7ce7dc4718144f51f73302a500
Rob Nagler <github@q33.us>
2004-07-29 21:07:00 +0000

    export structure of number through from_parts and to_parts

--- Type/PrimaryId.pm
+++ Type/PrimaryId.pm
@@ -38,22 +38,15 @@ Compare C<PrimaryId> values using C<eq> and C<ne>, B<not> the numeric
 equivalents.  PrimaryIds are larger that fit into a 32-bit integer.
 
 All C<PrimaryId> values are unique within a given "universe".  PrimaryIds are
-"structured", but you should not depend on this structure.  The purpose of the
+"structured", but you should avoid depending on this structure.  The purpose of the
 structure is to allow for easy horizontal and vertical partitioning.  The lower
 five digits identify the table and a site.  This leaves 13 digits for
 the rows.  By using the lower digits, we avoid
 large numbers until we have large numbers of users and we can expand the
 space without having to change the numbering scheme, or all tables.
-The decision on the number
-of digits for the site and the number of digits for the type has yet to be
-made.  Since we have only one site and many types, the lowest digits identify
-the type.  See F<create_sequences.sql> for a more complete discussion.
 
-Late breaking news.... The fifth digit is now a tag to allow us to
-load static info, e.g. FileVolume index.
-
-More news: ec_payment_s is using digits 4 & 5 for a site id.  I think
-it was a mistake to use all three digits for the volume.
+L<to_parts|"to_parts"> and L<from_parts|"from_parts"> allow you to take apart
+the PrimaryId.
 
 =cut
 
@@ -61,7 +54,6 @@ it was a mistake to use all three digits for the volume.
 #=IMPORTS
 
 #=VARIABLES
-my($_FILE_VOLUME_TAG) = '1';
 
 =head1 METHODS
 
@@ -125,6 +117,20 @@ sub from_literal {
     return (undef, Bivio::TypeError::PRIMARY_ID());
 }
 
+=for html <a name="from_parts"></a>
+
+=head2 static from_parts(hash_ref parts) : string
+
+Returns parts (see L<to_parts|"to_parts">) as string.
+
+=cut
+
+sub from_parts {
+    my(undef, $parts) = @_;
+    return sprintf('%s%1d%02d%02d',
+	@{$parts}{qw(number version site type)});
+}
+
 =for html <a name="get_decimals"></a>
 
 =head2 static get_decimals : int
@@ -186,22 +192,6 @@ sub get_width {
     return 18;
 }
 
-=for html <a name="set_file_volume_in_realm_id"></a>
-
-=head2 set_file_volume_in_realm_id(string realm_id, int volume) : string
-
-This guarantees a unique, static file volume id.
-
-=cut
-
-sub set_file_volume_in_realm_id {
-    my($self, $realm_id, $volume) = @_;
-    die($realm_id, ': invalid realm_id')
-	    if $realm_id < get_min();
-    substr($realm_id, -5, 3) = sprintf('%d%02d', $_FILE_VOLUME_TAG, $volume);
-    return $realm_id;
-}
-
 =for html <a name="to_html"></a>
 
 =head2 to_html(any value) : string
@@ -218,7 +208,7 @@ sub to_html {
 
 =for html <a name="to_literal"></a>
 
-=head2 to_literal(any value) : string
+=head2 static to_literal(any value) : string
 
 Converts value L<to_literal|"to_literal">.  If the value is undef, returns the
 empty string.  Otherwise, returns as is--always valid literal.
@@ -226,8 +216,29 @@ empty string.  Otherwise, returns as is--always valid literal.
 =cut
 
 sub to_literal {
-    my($self, $value) = @_;
+    my(undef, $value) = @_;
     return defined($value) ? $value : '';
+}
+
+=for html <a name="to_parts"></a>
+
+=head2 static to_parts(string value) : hash_ref
+
+Returns the primary_id decomposed into parts: number, site, version, type
+
+=cut
+
+sub to_parts {
+    my(undef, $value) = @_;
+    Bivio::Die->die($value, 'bad value')
+        unless $value =~ /^(\d+)(\d)(\d{2})(\d{2})$/;
+    return {
+	# Convert all but number, because number may be larger than range
+	number => $1,
+	version => $3 + 0,
+	site => $2 + 0,
+	type => $4 + 0,
+    };
 }
 
 =for html <a name="to_query"></a>
---
+++ Type/t/PrimaryId.t
@@ -0,0 +1,37 @@
+#!perl -w
+# $Id$
+use strict;
+use Bivio::Test;
+Bivio::Test->new('Bivio::Type::PrimaryId')->unit([
+    'Bivio::Type::PrimaryId' => [
+	to_parts => [
+	    '100001' => [{
+		version => 0,
+		type => 1,
+		site => 0,
+		number => 1,
+	     }],
+	    '111111111111100001' => [{
+		version => 0,
+		type => 1,
+		site => 0,
+		number => '1111111111111',
+	     }],
+	    '1' => Bivio::DieCode->DIE,
+	],
+	from_parts => [
+	    [{
+		version => 0,
+		type => 1,
+		site => 0,
+		number => 1,
+	    }] => '100001',
+	    [{
+		version => 0,
+		type => 1,
+		site => 1,
+		number => '1111111111111',
+	    }] => '111111111111100101',
+	],
+    ],
+]);
