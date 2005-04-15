f288f423f311e6195a9b3c29751c75e6278fd53a
Paul Moeller <moeller@bivio.biz>
2005-04-15 19:23:58 +0000

    * Bivio::Agent::Request deprecated task_ok(),   use can_user_execute_task() instead * Bivio::Biz::Model::ForbiddenForm now uses $req->can_user_execute_task() * Bivio::Test::HTMLParser::Tables improved error message when table   not found by name * Bivio::Type::Enum deprecated calls to to_sql_param() with a scalar value * Bivio::Type::Number deprecated math calls with undef arguments * Bivio::UI::HTML::Widget::ListActions now uses $req->can_user_execute_task() * Date.t added more from_literal() and to_xml() test cases

--- bOP.pm
+++ bOP.pm
@@ -30,7 +30,7 @@ Model-View-Controller (MVC) architecture.  At the lowest level, bOP provides a
 cohesive infrastructure for any Perl application.
 
 We'll be writing more here later.  Please visit
-http://www.bivio.biz for more info. 
+http://www.bivio.biz for more info.
 
 =cut
 
@@ -41,6 +41,9 @@ http://www.bivio.biz for more info.
 =head1 CHANGES
 
   $Log$
+  * Bivio::Type::Number deprecated math calls with undef arguments
+  * Bivio::UI::HTML::Widget::ListActions now uses $req->can_user_execute_task()
+  * Date.t added more from_literal() and to_xml() test cases
 
   Revision 2.61  2005/04/04 19:24:22  moeller
   * Bivio::UI::HTML::Widget::Enum added internal_new_args()
