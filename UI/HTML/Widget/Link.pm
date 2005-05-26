c696c4833f46d65e21a7c75e0173259d46e66629
Rob Nagler <github@q33.us>
2005-05-26 19:08:59 +0000

    removed all caching. Call format_uri if href is a hash_ref

--- UI/HTML/Widget/Link.pm
+++ UI/HTML/Widget/Link.pm
@@ -55,22 +55,19 @@ I<event_handler> will be rendered before this field.
 
 =item href : any (required)
 
-Value to use for C<HREF> attribute of C<A> tag.  If I<href> is a valid
-enum name or is an actual TaskId instance, I<href> will be treated as a task.
+Value to use for C<HREF> attribute of C<A> tag.  If I<href> renders to a valid
+enum name or is an L<Bivio::Agent::TaskId|Bivio::Agent::TaskId>,
+I<href> will be passed passed
+using L<Bivio::Agent::Request::format_stateless_uri|Bivio::Agent::Request/"format_stateless_uri">
+If I<href> renders as a hash_ref, it will passed to
+L<Bivio::Agent::Request::format_uri|Bivio::Agent::Request/"format_uri">.
 Otherwise, I<href> will be treated as a literal uri.
 
-If I<href> is an array_ref, it will be dereferenced and passed to
-C<$source-E<gt>get_widget_value> to get the uri to use.
-
-Literal text to use for C<HREF> attribute of C<A> tag.
-If href is in all capital letters, then it is treated as a task id,
-and a widget value for format_stateless_uri() will be used.
-
-=item link_target : string [] (inherited)
+=item link_target : any [] (inherited)
 
 The value to be passed to the C<TARGET> attribute of C<A> tag.
 
-=item name : string []
+=item name : any []
 
 Anchor name.
 
@@ -112,12 +109,6 @@ If I<attributes> supplied, creates with attribute (name, value) pairs.
 
 =cut
 
-sub new {
-    my($self) = Bivio::UI::Widget::ControlBase::new(@_);
-    $self->[$_IDI] = {};
-    return $self;
-}
-
 =head1 METHODS
 
 =cut
@@ -132,24 +123,20 @@ Render the link.
 
 sub control_on_render {
     my($self, $source, $buffer) = @_;
-    my($fields) = $self->[$_IDI];
-
-    $$buffer .= $fields->{prefix};
+    $$buffer .= '<a' . $_VS->vs_link_target_as_html($self, $source);
+    $self->unsafe_render_attr('attributes', $source, $buffer);
     my($n) = '';
     $$buffer .= ' name="' . Bivio::HTML->escape($n) . '"'
 	if $self->unsafe_render_attr('name', $source, \$n);
-    if ($fields->{href}) {
-	my($href) = $source->get_widget_value(@{$fields->{href}});
-	$$buffer .= ' href="'
-	    . _fixup_href($source->get_request,	$href)
-	    . '"'
-	    if defined($href);
-    }
-    my($handler) = $self->unsafe_get('event_handler');
+    my($href) = _render_href($self, $source);
+    $$buffer .= qq{ href="$href"}
+        if defined($href);
+    my($handler) = $self->unsafe_resolve_widget_value(
+	$self->unsafe_get('event_handler'), $source);
     $$buffer .= $handler->get_html_field_attributes(undef, $source)
 	if $handler;
     $$buffer .= '>';
-    $self->render_value('value', $fields->{value}, $source, $buffer);
+    $self->render_attr('value', $source, $buffer);
     $$buffer .= '</a>';
     $handler->render($source, $buffer)
 	if $handler;
@@ -167,31 +154,15 @@ It is fully initialized after first render.
 
 sub initialize {
     my($self) = @_;
-    my($fields) = $self->[$_IDI];
-    return if $fields->{value};
-
-    # href and value both must be defined
-    my($p, $s) = ('<a'.$_VS->vs_link_target_as_html($self), '');
-    my($a) = $self->unsafe_get('attributes');
-    $p .= $a if $a;
-
-    $self->unsafe_initialize_attr('event_handler');
-    $self->unsafe_initialize_attr('name');
-    $fields->{value} = $self->initialize_attr('value');
-    $fields->{value} = $_VS->vs_new(
-	    'String', $fields->{value},
-	)->put_and_initialize(parent => $self)
-	unless ref($fields->{value})
-	    && UNIVERSAL::isa($fields->{value}, 'Bivio::UI::Widget');
-    $fields->{href} = _initialize_href($self);
-    unless (ref($fields->{href})) {
-	# Format literally if a constant, but not if 0
-	$p .= ' href="'.$fields->{href}.'"'
-	    if $fields->{href};
-	delete($fields->{href});
-    }
-    $fields->{prefix} = $p;
-    return $self->SUPER::initialize();
+    $self->mapcar(
+	'unsafe_initialize_attr',
+	[qw(attributes event_handler name link_target)],
+    );
+    my($v) = $self->get('value');
+    $self->put(value => $_VS->vs_new('String', $v))
+	unless UNIVERSAL::isa($v, 'Bivio::UI::Widget');
+    $self->mapcar('initialize_attr', [qw(value href)]);
+    return shift->SUPER::initialize(@_);
 }
 
 =for html <a name="internal_as_string"></a>
@@ -210,7 +181,7 @@ sub internal_as_string {
 
 =for html <a name="internal_new_args"></a>
 
-=head2 static internal_new_args() : 
+=head2 static internal_new_args() :  hash_ref
 
 Implements positional argument parsing for L<new|"new">.
 
@@ -218,8 +189,11 @@ Implements positional argument parsing for L<new|"new">.
 
 sub internal_new_args {
     my(undef, $value, $href, $attributes) = @_;
-    return '"value" must be defined' unless defined($value);
-    return '"href" must be defined' unless defined($href);
+#TODO: generalize, and possibly couple with as_string
+    return '"value" must be defined'
+	unless defined($value);
+    return '"href" must be defined'
+	unless defined($href);
     return {
 	value => $value,
 	href => $href,
@@ -229,36 +203,32 @@ sub internal_new_args {
 
 #=PRIVATE METHODS
 
-# _fixup_href(Bivio::Agent::Request req, any href) : string
+# _render_href(self, any source) : string
 #
-# Returns a string.  May format if not already a TaskId.
+# Returns a string.  May format it using format_uri or format_stateless_uri
 #
-sub _fixup_href {
-    my($req, $href) = @_;
-    return $href unless ref($href);
-    Bivio::Die->die('unknown type for href: ', $href)
-	unless ref($href) eq 'Bivio::Agent::TaskId';
-    return $req->format_stateless_uri($href);
-}
-
-# _initialize_href(self) : any
-#
-# Returns the href as initialized.
-#
-sub _initialize_href {
-    my($self) = @_;
-    my($href) = $self->initialize_attr('href');
-    if (ref($href)) {
-	return $href if ref($href) eq 'ARRAY';
-	$self->die('href', undef, 'unknown type for href: ', $href)
-		unless ref($href) eq 'Bivio::Agent::TaskId';
-	return [['->get_request'], '->format_stateless_uri',
-	    Bivio::Agent::TaskId->$href()];
+sub _render_href {
+    my($self, $source) = @_;
+    my($href) = $self->unsafe_resolve_widget_value($self->get('href'), $source);
+    if (UNIVERSAL::isa($href, 'Bivio::UI::Widget')) {
+	my($v) = $href;
+	$href = undef;
+	$self->unsafe_render_value('href', $v, $source, \$href);
     }
-    return [['->get_request'], '->format_stateless_uri',
-	Bivio::Agent::TaskId->$href()]
-	    if Bivio::Agent::TaskId->is_valid_name($href);
-    return $href;
+    return undef
+	unless defined($href) && length($href);
+    return $href
+	unless ref($href) || Bivio::Agent::TaskId->is_valid_name($href);
+    my($req) = $source->get_request;
+    return $req->format_stateless_uri($href)
+	if !ref($href) || UNIVERSAL::isa($href, 'Bivio::Agent::TaskId');
+    return $req->format_uri($href)
+	if ref($href) eq 'HASH';
+    $self->die(
+	'href', $source,
+	$href, ': unknown type for href (must be scalar, hash, or TaskId)'
+    );
+    # DOES NOT RETURN
 }
 
 =head1 COPYRIGHT
