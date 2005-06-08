1e79d40811c7ab6d13c4cd0c87775abaad3f68a3
Rob Nagler <github@q33.us>
2005-06-08 14:20:18 +0000

    from_literal does date windowing

--- Type/Year.pm
+++ Type/Year.pm
@@ -35,7 +35,25 @@ C<Bivio::Type::Year> date year field type
 
 =cut
 
+=head1 CONSTANTS
+
+=cut
+
+=for html <a name="WINDOW_SIZE"></a>
+
+=head2 WINDOW_SIZE : int
+
+Number of years in the future that counts in this century, unless it crosses
+year boundaries.
+
+=cut
+
+sub WINDOW_SIZE {
+    return 20;
+}
+
 #=IMPORTS
+use Bivio::Type::DateTime;
 
 #=VARIABLES
 
@@ -43,9 +61,35 @@ C<Bivio::Type::Year> date year field type
 
 =cut
 
+=for html <a name="from_literal"></a>
+
+=head2 static from_literal(string value) : array
+
+Converts value.  Handles date windowing (+20 years).
+
+=cut
+
+sub from_literal {
+    my($proto) = shift;
+    my($res, $err) = $proto->SUPER::from_literal(@_);
+    return ($res, $err)
+        unless $err && $err == Bivio::TypeError->NUMBER_RANGE;
+    ($res, $err) = Bivio::Type::Integer->from_literal(@_);
+    return ($res, $err)
+	if $err;
+    return (undef, Bivio::TypeError->NUMBER_RANGE)
+	unless $res >= 0 && $res < 100;
+    my($century) = int(Bivio::Type::DateTime->now_as_year / 100) * 100;
+    return ($res + $century -
+		($res <= Bivio::Type::DateTime->now_as_year % 100
+		     + $proto->WINDOW_SIZE
+		? 0 : 100),
+	    undef);
+}
+
 =for html <a name="get_max"></a>
 
-=head2 static get_max : any
+=head2 static get_max : int
 
 Returns 9999.
 
@@ -57,14 +101,14 @@ sub get_max {
 
 =for html <a name="get_min"></a>
 
-=head2 static get_min : any
+=head2 static get_min : int
 
-Returns 0.
+Returns 100.
 
 =cut
 
 sub get_min {
-    return 0;
+    return 100;
 }
 
 =for html <a name="get_width"></a>
