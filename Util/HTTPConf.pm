93a7f2759b02c5c38e6dca792094e8c02b669c67
Rob Nagler <github@q33.us>
2005-08-14 23:43:53 +0000

    first try on production

--- Util/HTTPConf.pm
+++ Util/HTTPConf.pm
@@ -61,50 +61,105 @@ use Bivio::IO::File;
 
 #=VARIABLES
 my($_DATA);
-Bivio::IO::Config->register(my $_CFG = {
-    ports => {},
-    server_status_uri => '/s',
-    webmaster_email => Bivio::IO::Config->REQUIRED,
-});
+my($_VARS) = {
+    is_production => 0,
+    legacy_rewrite_rules => '',
+    cookie_tag => undef,
+    listen => undef,
+    mail_host => undef,
+    root_prefix => undef,
+    server_admin => undef,
+    server_status_allow => '127.0.0.1',
+    server_status_location => '/s',
+    servers => 4,
+    httpd_init_rc => '/etc/rc.d/init.d/httpd',
+    httpd_httpd_conf => '/etc/httpd/conf/httpd.conf',
+    # Users can supply certain params here
+    httpd => my $_HTTPD_VARS = {
+	app => 'httpd',
+	listen => '80',
+    },
+    # Trick to help _replace_vars
+    '$' => '$',
+};
 
 =head1 METHODS
 
 =cut
 
-=for html <a name="gen_app"></a>
+=for html <a name="generate"></a>
 
-=head2 gen_app(string app)
+=head2 create_files(string vars)
+
+Creates the files
 
 =cut
 
-sub gen_app {
-    my($self, $app, $cfg) = _app_cfg(@_);
-    Bivio::IO::File->mkdir_parent_only($cfg->{httpd_conf});
-    return Bivio::IO::File->write(
-	$cfg->{httpd_conf},
-	_httpd_conf($cfg),
-    );
+sub generate {
+    my($vars) = shift->validate_vars(@_);
+    umask(027);
+    foreach my $v (map(_app_vars($vars->{$_}), @{$vars->{apps}})) {
+	_write(_httpd_conf($v));
+	_mkdir($v->{log_directory});
+	_write(_app_bconf($v));
+	_write(_app_init_rc($v));
+    }
+    _httpd_vars($vars);
+    _write($vars->{httpd_init_rc}, _httpd_init_rc());
+    _write(_httpd_conf($vars->{httpd}));
+    return;
 }
 
-=for html <a name="handle_config"></a>
+=for html <a name="validate_vars"></a>
 
-=head2 static handle_config(hash cfg)
+=head2 static validate_vars(string vars) : hash_ref
+
+The configuration for an application server is provided via I<vars>, and
+returned by this routine as a hash_ref.  I<vars> is a perl hash_ref whose keys
+are the applications to configure, and values are hash_refs containing the
+config, e.g.
+
+    {
+        petshop => {
+            listen => 8080,
+            server_admin => 'webmaster@bivio.biz',
+        },
+    }
+
+I<vars> may also contain default entries for the attributes defined below, e.g.
+
+    {
+        server_admin => 'webmaster@bivio.biz',
+        petshop => {
+            listen => 8080,
+        },
+        my_app => {
+            listen => 8081,
+        },
+    }
+
+The attributes and their global defaults are defined as follows:
 
 =over 4
 
-=item ports : hash_ref [{}]
+=item listen : int (required)
 
-Hash of app to port, e.g.
+Port the app server listens on.
 
-    ports => {
-        petshop => 8080,
-    },
+=item servers : int [4]
 
-=item server_status_uri : string [/s]
+Number of application servers to start.  The number of front-end httpd
+servers will be 2 x the sum of I<servers> for all apps being configured.
+
+=item server_status_allow : string [127.0.0.1]
+
+Addresses from which we allow access to I<server_status_location>
+
+=item server_status_location : string [/s]
 
 Location of Apache server-status
 
-=item webmaster_email : string (required)
+=item server_admin : string (required)
 
 Email address of webmaster to be set to ServerAdmin.
 
@@ -112,72 +167,129 @@ Email address of webmaster to be set to ServerAdmin.
 
 =cut
 
-sub handle_config {
-    my(undef, $cfg) = @_;
-    Bivio::Die->die($cfg->{ports}, ': ports must be a hash_ref')
-	unless ref($cfg->{ports}) eq 'HASH';
-    __PACKAGE__->convert_literal('Email', $cfg->{webmaster_email});
-    $_CFG = $cfg;
-    return;
+sub validate_vars {
+    my($self, $vars) = @_;
+    $vars = {
+	%$_VARS,
+	%{Bivio::Die->eval_or_die($vars)},
+    };
+    foreach my $app (
+	@{$vars->{apps} = [grep(!exists($_VARS->{$_}), sort(keys(%$vars)))]},
+    ) {
+	foreach my $k (keys(%$_VARS)) {
+	    my($v) = $vars->{$app};
+	    $self->usage_error($k, ': variable missing value for ', $app, "\n")
+		unless defined($v->{$k}) || defined($v->{$k} = $vars->{$k});
+	}
+	$vars->{$app}->{app} = $app;
+    }
+    return $vars
 }
 
 #=PRIVATE SUBROUTINES
 
-# _app_cfg(self, string app) : hash_ref
+# _app_bconf(hash_ref vars) : array
 #
-# Return configuration used to write _httpd_conf
 #
-sub _app_cfg {
-    my($self, $app) = @_;
-    return ($self, $app, {
-	bconf => "/etc/$app.bconf",
+#
+sub _app_bconf {
+    my($vars) = @_;
+    return _replace_vars($vars, bconf => <<'EOF');
+use $root_prefix::BConf;
+$root_prefix::BConf->merge_dir({
+    'Bivio::Agent::Request' => {
+        is_production => $is_production,
+    },
+    'Bivio::Delegate::Cookie' => {
+        tag => '$cookie_tag',
+    },
+    'Bivio::UI::Facade' => {
+        local_file_root => '/var/www/facades',
+        want_local_file_cache => 1,
+        http_suffix => '$http_suffix',
+        mail_host => '$mail_host',
+    },
+    'Bivio::Util::HTTPLog' => {
+        error_file => '$log_directory/error_log',
+    },
+});
+EOF
+}
+
+# _app_vars(hash_ref vars) : hash_ref
+#
+# Augments vars for a single app ($vars->{$app}) to include _app_vars.  Returns
+# a $vars with updated config.
+#
+sub _app_vars {
+    my($vars) = @_;
+    my($app) = $vars->{app};
+    my($bconf) = "/etc/$app.bconf";
+    %$vars = (
+	%$vars,
+	bconf => $bconf,
 	document_root => "/var/www/facades/$app/plain",
-	generator => ('$Header$' =~ /Header:\s*(.+?)\s*\$/i)[0] || __PACKAGE__,
-	httpd_conf => "etc/httpd/conf/$app.conf",
-	listen => $_CFG->{ports}->{$app}
-	    || $self->usage_error($app, ': no port configured'),
+	httpd_conf => "/etc/httpd/conf/$app.conf",
+	init_rc => "/etc/rc.d/init.d/$app",
 	lock_file => "/var/lock/subsys/$app",
 	log_directory => "/var/log/$app",
-	location_server_status => $_CFG->{server_status_uri},
-	mime_types => '/etc/mime.types',
 	pid_file => "/var/run/$app.pid",
+	permanent_redirects => ($vars->{http_suffix} || '') =~ /^www\.(.*)$/
+	    ? <<"EOF" : '',
+<VirtualHost *>
+    ServerName $1
+    RedirectPermanent / http://www.$1/
+</VirtualHost>
+EOF
 	process_name => "$app-httpd",
-	server_admin => $_CFG->{webmaster_email},
-    });
+	content => <<"EOF",
+PerlWarn on
+PerlFreshRestart off
+PerlSetEnv BCONF $bconf
+PerlTransHandler Apache::OK
+# Override the translation handler to avoid local file permission checks
+PerlModule Bivio::Agent::HTTP::Dispatcher
+
+<Location />
+    SetHandler perl-script
+    PerlHandler Bivio::Agent::HTTP::Dispatcher
+</Location>
+EOF
+    );
+    return $vars;
 }
 
-# _httpd_conf(hash_ref cfg) : string_ref
+# _app_init_rc(hash_ref vars) : array
 #
-# Copy __DATA__, and fill in $cfg.
+# Copies template for vars.
+#
+sub _app_init_rc {
+    my($vars) = @_;
+    return _replace_vars($vars, init_rc => <<'EOF');
+#!/bin/bash
+#
+# Startup script for the $app App Server
+#
+# chkconfig: 345 84 16
+# description: $app Application Server
+# processname: $app
+# pidfile: $pid_file
+# config: $httpd_conf
+
+b_httpd_app=$app
+
+# Source function library.
+. $httpd_init_rc
+EOF
+}
+
+# _httpd_conf(hash_ref vars) : array
+#
+# Replace vars in template, and return (name, data)
 #
 sub _httpd_conf {
-    my($cfg) = @_;
-    unless ($_DATA) {
-	local($/);
-	$_DATA = <DATA>;
-	close(DATA);
-    }
-    my($d) = $_DATA;
-    $d =~ s/\$(\w+)/$cfg->{$1} || die("$1: bad __DATA__")/seg;
-    return \$d;
-}
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
-
-1;
-__DATA__
-################################################################
-# Automatically Generated File; LOCAL CHANGES WILL BE LOST!
-# By: $generator
-################################################################
+    my($vars) = @_;
+    return _replace_vars($vars, 'httpd_conf', <<'EOF');
 ResourceConfig /dev/null
 AccessConfig /dev/null
 
@@ -193,9 +305,9 @@ KeepAlive On
 MaxKeepAliveRequests 100
 KeepAliveTimeout 15
 MinSpareServers 1
-MaxSpareServers 4
-StartServers 4
-MaxClients 4
+MaxSpareServers $servers
+StartServers $servers
+MaxClients $servers
 MaxRequestsPerChild 100000
 LimitRequestBody 4194304
 
@@ -203,7 +315,7 @@ ServerRoot /etc/httpd
 PidFile $pid_file
 LockFile $lock_file
 ScoreBoardFile $log_directory/apache_runtime_status
-TypesConfig $mime_types
+TypesConfig /etc/mime.types
 DefaultType text/plain
 LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
 CustomLog $log_directory/access_log combined
@@ -211,34 +323,242 @@ ErrorLog $log_directory/error_log
 LogLevel info
 ExtendedStatus On
 
-PerlWarn on
-PerlFreshRestart off
-PerlSetEnv BCONF $bconf
-PerlTransHandler Apache::OK
-# Override the translation handler to avoid local file permission checks
-PerlModule Bivio::Agent::HTTP::Dispatcher
-
 DocumentRoot $document_root
+
 <Directory />
     AllowOverride None
     Options FollowSymLinks
-    deny from all
-    allow from 127.0.0.1
 </Directory>
 
-<Location />
-    SetHandler perl-script
-    PerlHandler Bivio::Agent::HTTP::Dispatcher
-</Location>
-
-<Location $location_server_status>
+<Location $server_status_location>
     SetHandler server-status
     deny from all
-    allow from 127.0.0.1
+    allow from $server_status_allow
 </Location>
+
+$content
 
 BrowserMatch "Mozilla/2" nokeepalive
 BrowserMatch "MSIE 4\.0b2;" nokeepalive downgrade-1.0 force-response-1.0
 BrowserMatch "RealPlayer 4\.0" force-response-1.0
 BrowserMatch "Java/1\.0" force-response-1.0
 BrowserMatch "JDK/1\.0" force-response-1.0
+EOF
+}
+
+# _mkdir(string name) : string
+#
+# Makes directory
+#
+sub _mkdir {
+    my($name) = @_;
+    $name =~ s,^/,,;
+    return Bivio::IO::File->mkdir_p($name);
+}
+
+# _httpd_httpd_conf(hash_ref vars) : array
+#
+# Replace vars
+#
+sub _httpd_httpd_conf {
+    return;
+}
+
+# _httpd_init_rc() : string
+#
+# Return <DATA>
+#
+sub _httpd_init_rc {
+    unless ($_DATA) {
+	local($/);
+	$_DATA = <DATA>;
+	close(DATA);
+    }
+    my($d) = $_DATA;
+    return \$d;
+}
+
+# _httpd_vars(hash_ref vars)
+#
+# Create the $vars->{httpd} values
+#
+sub _httpd_vars {
+    my($vars) = @_;
+    my($v) = $vars->{httpd};
+    %$v = (%$_VARS, %$_HTTPD_VARS, %$v);
+    _app_vars($v);
+    my($t);
+    $v->{content} = join(
+	"\n",
+	map(
+	    $_ =~ /\n/ ? ($t = $_, '')[1]
+		: ${(_replace_vars($vars->{$_}, "_httpd_vars($_)", $t))[1]},
+	    <<'EOF', 'httpd',
+
+NameVirtualHost *
+<VirtualHost *>
+    ServerName $host_name
+    DocumentRoot /var/www/html
+</VirtualHost>
+EOF
+	    <<'EOF', @{$vars->{apps}},
+<VirtualHost *>
+    ServerName $http_suffix
+    DocumentRoot /var/www/facades/$app/plain
+    ProxyVia on
+    ProxyIOBufferSize 4194304
+    RewriteEngine On
+    RewriteOptions inherit
+    $legacy_rewrite_rules
+    RewriteRule ^/./ - [L]
+    RewriteRule .*favicon.ico$$ /i/favicon.ico [L]
+    RewriteRule ^(.*) http://%{HTTP_HOST}:$listen$$1 [proxy]
+</VirtualHost>
+$permanent_redirects
+EOF
+	),
+    );
+    my($n) = 0;
+    foreach my $i (map($vars->{$_}->{servers}, @{$vars->{apps}})) {
+	$n += $i;
+    }
+    $v->{server_admin} ||= 'webmaster@' . $v->{host_name};
+    $v->{servers} = $n * 2;
+    return;
+}
+
+# _replace_vars(hash_ref vars, string name, string template) : array
+#
+# Replaces vars in template to generate $vars->{$name} file.
+#
+sub _replace_vars {
+    my($vars, $name, $template) = @_;
+    # One of the $_VARS is '$'
+    $template =~ s{\$(\w+|\$)}{
+	defined($vars->{$1}) ? $vars->{$1}
+	    : Bivio::Die->die("$1: in template ($name), but not in ", $vars)
+    }xseg;
+    return ($vars->{$name}, \$template);
+}
+
+# _write(string name, any data) : string
+#
+# Writes name, stripping off leading /.
+#
+sub _write {
+    my($name, $data) = @_;
+    $name =~ s{^/}{};
+    Bivio::IO::File->mkdir_parent_only($name);
+    my($generator) = ('$Header$' =~ m{Header:\s*(.+?)\s*\$}i)[0]
+	|| __PACKAGE__;
+    $$data =~ s{^(#!.+?\n|)}{$1 . <<"EOF"}es;
+################################################################
+# Automatically Generated File; LOCAL CHANGES WILL BE LOST!
+# By: $generator
+################################################################
+EOF
+    return Bivio::IO::File->write($name, $$data);
+}
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
+__DATA__
+#!/bin/bash
+#
+# Startup script for the Apache Web Server
+#
+# chkconfig: 345 85 15
+# description: Apache is a World Wide Web server.  It is used to serve \
+#	       HTML files and CGI.
+# processname: httpd
+# pidfile: /var/run/httpd.pid
+# config: /etc/httpd/conf/access.conf
+# config: /etc/httpd/conf/httpd.conf
+# config: /etc/httpd/conf/srm.conf
+
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+# This will prevent initlog from swallowing up a pass-phrase prompt.
+INITLOG_ARGS=""
+
+# Source additional OPTIONS if we have them.
+if [ -f /etc/sysconfig/apache ] ; then
+	. /etc/sysconfig/apache
+fi
+
+httpd=${b_httpd_app:-/usr/sbin/httpd}
+prog=$(basename $httpd)
+RETVAL=0
+
+# Change the major functions into functions.
+moduleargs() {
+	moduledir=/usr/lib/apache
+	moduleargs=`
+	/usr/bin/find ${moduledir} -type f -perm -0100 -name "*.so" | env -i tr '[:lower:]' '[:upper:]' | awk '{\
+		gsub(/.*\//,"");\
+		gsub(/^MOD_/,"");\
+		gsub(/^LIB/,"");\
+		gsub(/\.SO$/,"");\
+		print "-DHAVE_" $0}'`
+	echo ${moduleargs}
+}
+start() {
+	echo -n $"Starting $prog: "
+	# The goal of this is to change the process name
+	daemon --check=$prog perl -e "'exec {q{/usr/sbin/httpd}} (qw{$prog $(moduleargs) $OPTIONS -f /etc/httpd/conf/$prog.conf}) or die(qq{exec: \$!})'"
+	RETVAL=$?
+	echo
+	[ $RETVAL = 0 ] && touch /var/lock/subsys/$prog
+	return $RETVAL
+}
+stop() {
+	echo -n $"Stopping $prog: "
+	killproc $prog
+	RETVAL=$?
+	echo
+	[ $RETVAL = 0 ] && rm -f /var/lock/subsys/$prog /var/run/$prog.pid
+}
+
+# See how we were called.
+case "$1" in
+  start)
+	start
+	;;
+  stop)
+	stop
+	;;
+  status)
+	status $prog
+	;;
+  restart)
+	stop
+	start
+	;;
+  reload)
+	echo -n $"Reloading $prog: "
+	killproc $process -HUP
+	RETVAL=$?
+	echo
+	;;
+  condrestart)
+	if [ -f /var/run/$prog.pid ] ; then
+		stop
+		start
+	fi
+	;;
+  *)
+	echo $"Usage: $prog {start|stop|restart|reload|condrestart|status}"
+	exit 1
+esac
+
+exit $RETVAL
--- Util/t/HTTPConf.t
+++ Util/t/HTTPConf.t
@@ -4,8 +4,54 @@ use strict;
 use Bivio::Test;
 Bivio::Test->new('Bivio::Util::HTTPConf')->unit([
     'Bivio::Util::HTTPConf' => [
-	gen_app => [
-	    petshop => 'etc/httpd/petshop.conf',
+	generate => [
+	    <<'EOF',
+{
+    httpd => {
+        host_name => 'proxy.com',
+    },
+    a1 => {
+        cookie_tag => 'A1T',
+        http_suffix => 'www.a1.com',
+        listen => 2121,
+        mail_host => 'a2.com',
+        root_prefix => 'A1',
+        server_admin => 'a1@a1.com',
+    },
+    a2 => {
+        is_production => 1,
+        cookie_tag => 'A2T',
+        root_prefix => 'A2',
+        listen => 1010,
+        legacy_rewrite_rules => 
+             'RewriteRule ^(/[^/]+\.html)$ /hm$1 [R=permanent,L]',
+        mail_host => 'a2.com',
+        http_suffix => 'a2.com',
+        servers => 333,
+    },
+    server_admin => 'default@default.com',
+};
+EOF
+            sub {
+		my($vars) = Bivio::Die->eval_or_die(shift->get('params')->[0]);
+		foreach my $app (grep(/^a\d+$/, sort(keys(%$vars)))) {
+		    foreach my $x (
+			["etc/httpd/conf/$app.conf", qw(listen server_admin servers)],
+			["etc/$app.bconf", qw(cookie_tag http_suffix mail_host)],
+		    ) {
+			my($file) = shift(@$x);
+			my($c) = Bivio::IO::File->read($file);
+			foreach my $k (@$x) {
+			    my($n) = ($k =~ /([a-z]+)$/)[0];
+			    # 4 is default servers, only default being tested
+			    my($v) = $vars->{$app}->{$k} || $vars->{$k} || 4;
+			    die("$n $v: not found in $file")
+				unless $$c =~ /${n}[^\n]+$v/is;
+			}
+		    }
+		}
+		return 1;
+	    },
 	],
     ],
 ]);
