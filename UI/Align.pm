7609f35ca9fabc782343f3e86ce4257ce00c8149
Rob Nagler <github@q33.us>
2005-08-29 18:20:10 +0000

    updated copyright

--- UI/Align.pm
+++ UI/Align.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 1999,2000 bivio Inc.  All rights reserved.
+# Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::UI::Align;
 use strict;
@@ -164,7 +164,7 @@ sub as_html {
 
 =head1 COPYRIGHT
 
-Copyright (c) 1999,2000 bivio Inc.  All rights reserved.
+Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
 
 =head1 VERSION
 
