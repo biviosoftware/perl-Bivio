d6bab9f0e003552f86d53f6f06482ae0bf79c1a8
Rob Nagler <github@q33.us>
2005-10-01 17:28:10 +0000

    *** empty log message ***

---
+++ Biz/Action/TestBackdoor.pm
@@ -0,0 +1,22 @@
+# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::TestBackdoor;
+use strict;
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+use base ('Bivio::Biz::Action');
+@Bivio::Biz::Action::TestBackdoor::ISA = ('Bivio::Biz::Action');
+
+sub execute {
+    my($proto, $req) = @_;
+    # Extract form_model from $req.query and execute with remaining query
+    # arguments.
+    my($m) = Bivio::Biz::Model->get_instance(
+	delete((my $q = $req->get('query'))->{form_model}));
+    $m->execute($req, {
+	map(($_ => $m->get_field_type($_)->from_literal_or_die($q->{$_})),
+	    keys(%$q)),
+    });
+    return;
+}
+
+1;
