b7bc1c3fa2440d5ba602ff9bc7065c1dd15f9e9e
Rob Nagler <github@q33.us>
2005-10-24 16:28:27 +0000

    *** empty log message ***

---
+++ Biz/Action/UserPasswordQuery.pm
@@ -0,0 +1,47 @@
+# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::UserPasswordQuery;
+use strict;
+use base ('Bivio::Biz::Action');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_KEY) = 'x';
+
+sub execute {
+    my($proto, $req) = @_;
+    my($pw) = delete($req->get('query')->{$_KEY});
+    my($u) = $req->get_nested(qw(auth_realm owner));
+    if (Bivio::Die->catch(sub {
+	Bivio::Die->throw_quietly('invalid password in query')
+	    unless $u->get_field_type('password')->is_equal(
+	    $u->get('password'), $pw);
+	Bivio::Biz::Model->get_instance('UserLoginForm')->execute($req, {
+	    realm_owner => $u,
+	});
+    })) {
+	$proto->get_instance('Acknowledgement')->save_label(
+	    password_nak => $req);
+	Bivio::Die->throw(NOT_FOUND => {
+	    entity => $pw,
+	    realm => $u,
+	});
+	# DOES NOT RETURN
+    }
+    $proto->new({password => $pw})->put_on_request($req, 1);
+    $proto->get_instance('Acknowledgement')->save_label($req);
+    return 'server_redirect.password_task';
+}
+
+sub format_uri {
+    my(undef, $req) = @_;
+    my($pw) = sprintf('%08d', int(rand(100_000_000)) + 1);
+    $req->get_nested(qw(auth_realm owner))->update_password($pw);
+    return $req->format_http({
+	task_id => $req->get_nested(qw(task reset_task)),
+	# sprintf ensures is at least six chars
+	query => {$_KEY => $pw},
+	no_context => 1,
+    });
+}
+
+1;
---
+++ Biz/Model/UserPasswordQueryForm.pm
@@ -0,0 +1,151 @@
+# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::UserPasswordQueryForm;
+use strict;
+$Bivio::Biz::Model::UserPasswordQueryForm::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+$_ = $Bivio::Biz::Model::UserPasswordQueryForm::VERSION;
+
+=head1 NAME
+
+Bivio::Biz::Model::UserPasswordQueryForm - request password reset
+
+=head1 RELEASE SCOPE
+
+bOP
+
+=head1 SYNOPSIS
+
+    use Bivio::Biz::Model::UserPasswordQueryForm;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio::Biz::FormModel>
+
+=cut
+
+use Bivio::Biz::FormModel;
+@Bivio::Biz::Model::UserPasswordQueryForm::ISA = ('Bivio::Biz::FormModel');
+
+=head1 DESCRIPTION
+
+C<Bivio::Biz::Model::UserPasswordQueryForm>
+
+=cut
+
+#=IMPORTS
+
+#=VARIABLES
+
+=head1 METHODS
+
+=cut
+
+=for html <a name="execute_empty"></a>
+
+=head2 execute_empty()
+
+Default the email address to the user in the cookie if present.
+
+=cut
+
+sub execute_empty {
+    my($self) = @_;
+    my($req) = $self->get_request;
+    $self->SUPER::execute_empty;
+    my($cookie) = $req->unsafe_get('cookie');
+    return unless $cookie;
+    my($user_id) = $cookie->unsafe_get(
+        $self->get_instance('UserLoginForm')->USER_FIELD);
+    return unless $user_id;
+    my($email) = $self->new($req, 'Email');
+    return unless $email->unauth_load({
+        realm_id => $user_id,
+    });
+    $email = $email->unsafe_get('email');
+    return unless Bivio::Type->get_instance('Email')->is_valid($email);
+    $self->internal_put_field('Email.email' => $email);
+    return;
+}
+
+=for html <a name="execute_ok"></a>
+
+=head2 execute_ok() : string
+
+Sets the user's password to a random value. Saves the reset URI in the
+'uri' field. Performs a server redirect to the next task when done.
+
+=cut
+
+sub execute_ok {
+    my($self) = @_;
+    my($req) = $self->get_request;
+    $self->internal_put_field(
+	uri => Bivio::Biz::Action->get_instance('UserPasswordQuery')
+	    ->format_uri($req),
+    );
+    return 'server_redirect.next';
+}
+
+=for html <a name="internal_initialize"></a>
+
+=head2 internal_initialize() : hash_ref
+
+Returns config
+
+=cut
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+	version => 1,
+	visible => [
+	    'Email.email',
+	],
+	other => [
+	    {
+		name => 'uri',
+		type => 'Line',
+		constraint => 'NONE',
+	    },
+	],
+    });
+}
+
+=for html <a name="validate"></a>
+
+=head2 validate()
+
+Ensure email exists.
+
+=cut
+
+sub validate {
+    my($self) = @_;
+    return if $self->in_error;
+    my($e) = $self->new_other('Email');
+    if ($e->unauth_load({email => $self->get('Email.email')})) {
+	$self->get_request->set_realm($e->get('realm_id'));
+	$self->internal_put_error(qw(Email.email PASSWORD_QUERY_SUPER_USER))
+	    if $self->get_request->is_super_user($e->get('realm_id'));
+    }
+    else {
+	$self->internal_put_error(qw(Email.email NOT_FOUND));
+    }
+    return;
+}
+
+#=PRIVATE SUBROUTINES
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003 bivio Software Artisans, Inc.  All Rights Reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
