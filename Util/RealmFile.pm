ffcbe68a90b4e7c32c40591c35fae668bdc7a119
Rob Nagler <github@q33.us>
2005-10-24 20:44:42 +0000

    pass volume and is_public

--- Util/RealmFile.pm
+++ Util/RealmFile.pm
@@ -2,82 +2,56 @@
 # $Id$
 package Bivio::Util::RealmFile;
 use strict;
-use base ('Bivio::ShellUtil');
+use base 'Bivio::ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-=head1 DESCRIPTION
+sub OPTIONS {
+    return {
+	 %{shift->SUPER::OPTIONS(@_)},
+	 volume => [FileVolume => 'PLAIN'],
+	 is_public => [Boolean => 0],
+    };
+}
 
-C<Bivio::Util::RealmFile> is an interface to administer realm files.
-
-=cut
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="USAGE"></a>
-
-=head2 USAGE : string
-
-Returns usage string.
-
-=cut
+sub OPTIONS_USAGE {
+    return shift->SUPER::OPTIONS_USAGE(@_) . <<'EOF'
+    -volume [FileVolume] - file volume to operate on (default: PLAIN)
+    -is_public - operate on public files (default: 0)
+EOF
+}
 
 sub USAGE {
     return <<'EOF';
 usage: b-realm-file [options] command [args...]
 commands:
-    import_tree root -- imports a directory/files
+    import_tree root -- imports files in current directory into root of current realm
 EOF
 }
 
-#=IMPORTS
 use Bivio::Biz::Model::RealmFile;
-use File::Find;
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="import_tree"></a>
-
-=head2 import_tree(string root)
-
-Imports a directory and its files.
-
-=cut
+use File::Find ();
 
 sub import_tree {
     my($self, $root) = @_;
+    $root = $self->convert_literal('FilePath', $root || '');
     my($req) = $self->initialize_ui;
-    my($foo) = [];
-    find(
+    File::Find::find(
 	sub{
-	    print("$File::Find::name\n");
-	    push(@$foo, $File::Find::name =~ m{^\./(.+)})
-		unless -d $_;
+	    return if -d $_;
+	    my($f) = $File::Find::name =~ m{^\./(.+)};
+	    Bivio::Biz::Model->new($req, 'RealmFile')->create_with_content(
+		{
+		    path => $self->convert_literal('FilePath', "$root/$f"),
+		    creation_date_time => Bivio::Type::DateTime->from_unix(
+			(stat($f))[9],
+		    ),
+		    map(($_ => $self->get($_)), qw(volume is_public)),
+		},
+		Bivio::IO::File->read($f),
+	    );
 	}, '.',
     );
-    my($f) = Bivio::Biz::Model->new($req, 'RealmFile');
-    foreach my $p (sort(@$foo)) {
-	my($x, $e) = Bivio::Type::FilePath->from_literal("$root/$p");
-	if ($e) {
-	    $self->print($p, ": bad file path: ", $e->get_name);
-	    next;
-	}
-	$f->create_with_content({
-	    path => $self->convert_literal('FilePath', "$root/$p"),
-	    creation_date_time => Bivio::Type::DateTime->from_unix(
-		(stat($p))[9],
-	    ),
-	    volume => Bivio::Type::FileVolume->PLAIN,
-	},
-	Bivio::IO::File->read($p),
-	);
-    }
     return;
 }
 
