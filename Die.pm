d9b531243c006592777f81f84863b73070dcc78c
Rob Nagler <github@q33.us>
2005-10-24 16:13:20 +0000

    copyright

--- Die.pm
+++ Die.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 1999,2000,2001 bivio Inc.  All rights reserved.
+# Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Die;
 use strict;
@@ -707,7 +707,7 @@ sub _print_stack {
 
 =head1 COPYRIGHT
 
-Copyright (c) 1999,2000,2001 bivio Inc.  All rights reserved.
+Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
 
 =head1 VERSION
 
