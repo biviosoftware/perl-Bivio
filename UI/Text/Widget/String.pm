2fd8327acaf19822efe224a584e3ba0b22520a31
David Farber <david@bivio.biz>
2005-10-26 22:19:17 +0000

    String can now take a scalar_ref as an argument

--- UI/Text/Widget/String.pm
+++ UI/Text/Widget/String.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2005 bivio Software Artisans, Inc.  All Rights Reserved.
+# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::Text::Widget::String;
 use strict;
@@ -70,11 +70,27 @@ sub internal_new_args {
     };
 }
 
+=for html <a name="unsafe_resolve_widget_value"></a>
+
+=head2 unsafe_resolve_widget_value(any value, any source) : any
+
+Allow scalar refs to render as a string.
+
+=cut
+
+sub unsafe_resolve_widget_value {
+    my($self, $value, $source) = @_;
+    $value = $self->SUPER::unsafe_resolve_widget_value($value, $source);
+    $value = $$value
+	if ref($value) eq 'SCALAR';
+    return $value;
+}
+
 #=PRIVATE SUBROUTINES
 
 =head1 COPYRIGHT
 
-Copyright (c) 2005 bivio Software Artisans, Inc.  All Rights Reserved.
+Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
 
 =head1 VERSION
 
