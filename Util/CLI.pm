81d27a8364e601bae311ec99d319718132c90a92
Rob Nagler <github@q33.us>
2005-10-27 19:52:28 +0000

    *** empty log message ***

---
+++ Util/CLI.pm
@@ -0,0 +1,39 @@
+# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Util::CLI;
+use strict;
+use base 'Bivio::ShellUtil';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($AUTOLOAD, $_UTIL);
+
+sub USAGE {
+    return <<'EOF';
+usage: b-cli [options] command [args...]
+commands:
+    batch -- execute a list of command line operations
+EOF
+}
+
+sub AUTOLOAD {
+    my($func) = $AUTOLOAD;
+    $func =~ s/.*:://;
+    return if $func eq 'DESTROY';
+    if ($func =~ /^[A-Z]/) {
+	$_UTIL = Bivio::IO::ClassLoader->map_require('ShellUtil', $func);
+	return @_ ? $_UTIL->main(@_) : ();
+    }
+    die('you need to call a ShellUtil class first, e.g. RealmAdmin();')
+	unless $_UTIL;
+    return $_UTIL->main($func, @_);
+}
+
+sub batch {
+    my($self) = @_;
+    local($_UTIL);
+    return Bivio::Die->eval_or_die(
+	'use strict;' . ${$self->read_input()},
+    );
+}
+
+1;
