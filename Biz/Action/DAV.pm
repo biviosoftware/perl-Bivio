d133a7f84a3afef5b3af9dec79a407a88ea1f4a3
Rob Nagler <github@q33.us>
2005-11-10 14:02:29 +0000

    redesigned to split of data component from DAV implementation

--- Biz/Action/DAV.pm
+++ Biz/Action/DAV.pm
@@ -3,7 +3,6 @@
 package Bivio::Biz::Action::DAV;
 use strict;
 use base 'Bivio::Biz::Action';
-use Bivio::Type::FileName;
 use Bivio::IO::Trace;
 use Bivio::Biz::Model;
 
@@ -11,7 +10,7 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_DT) = Bivio::Type->get_instance('DateTime');
 our($_TRACE);
 my($_DIE) = {
-    ALREADY_EXISTS => 'HTTP_CONFLICT',
+    ALREADY_EXISTS => 'HTTP_PRECONDITION_FAILED',
     DIE => 'FORBIDDEN',
     IO_ERROR => 'FORBIDDEN',
     FORBIDDEN => 'FORBIDDEN',
@@ -23,45 +22,49 @@ my($_DIE) = {
     DB_CONSTRAINT => 'HTTP_CONFLICT',
     UPDATE_COLLISION => 'HTTP_CONFLICT',
 };
-my($_FP) = Bivio::Biz::Model->get_instance('RealmFile')->get_field_type('path');
-
+# This list should be complete, even though we don't implement them all
+my($_WRITABLE) = qr/^(copy|delete|edit|lock|mkcol|move|put|proppatch|save|unlock)$/;
 sub execute {
-    my($proto, $req) = @_;
+    my(undef, $req) = @_;
     my($s) = {
-	proto => $proto,
-	file => Bivio::Biz::Model->new($req, 'RealmFile'),
 	req => $req,
 	reply => $req->get('reply'),
 	r => $req->get('r'),
-	method => $req->get('r')->method,
-	role => _role($req),
+	method => lc($req->get('r')->method),
+	uri => $req->get('uri'),
+	path_info => $req->get('path_info'),
+	root_model => $req->get_by_regexp(qr{Model\.\w+DAVList}),
     };
-    my($p, $e) = $_FP->from_literal($req->get('path_info') || '/');
-    return _output(
-	$s, BAD_REQUEST => 'Invalid resource name: ' . $req->get('path_info'),
-    ) if $e;
-    $s->{path} = $p;
-    $s->{file}->unsafe_load(_file_args($s));
-    my($op) = \&{'_dav_' . lc($s->{method})};
-    return 1
-	if _content($s);
-    _trace(
-	$s->{method},
-	' ',
-	$req->get('uri'),
-	' ',
-	{$s->{r}->headers_in}, "\n", \($s->{content}),
-    ) if $_TRACE;
-    return _other_op($s)
-        unless defined(&$op);
-    return 1
-	if _precondition($s);
     my($die) = Bivio::Die->catch(sub {
-	$op->($s);
+        return unless $s->{list} = _load(
+	    $s, $req->get('auth_realm'), $req->get('path_info'));
+	my($op) = \&{'_dav_' . $s->{method}};
+	return if _content($s);
+	_trace($s->{method}, ' ', $s->{uri}, ' ',
+	       {$s->{r}->headers_in}, "\n", \($s->{content})
+	) if $_TRACE;
+	return _other_op($s)
+	    unless defined(&$op);
+        return if _precondition($s);
+	return $op->($s);
     });
-    return $die ? _output(
-	$s, ($_DIE->{$die->get('code')->get_name} || 'SERVER_ERROR'))
-	: 1;
+    _output($s, ($_DIE->{$die->get('code')->get_name} || 'SERVER_ERROR'))
+	if $die;
+    return 1;
+}
+
+sub _call {
+    my($s) = shift;
+    my($method, $list) = (shift, $s->{list});
+    if (ref($method)) {
+	$list = $method;
+	$method = shift;
+    }
+    $method = "dav_$method";
+    _trace($list, "->$method", \@_) if $_TRACE;
+    Bivio::Die->throw(FORBIDDEN => "$s->{method} not permitted on: $s->{uri}")
+        unless $list->can($method);
+    return $list->$method(@_);
 }
 
 sub _content {
@@ -84,19 +87,22 @@ sub _copy_move {
     my($s) = @_;
     my($d) = Bivio::HTML->unescape_uri($s->{r}->header_in('destination') || '');
     return _output($s, BAD_REQUEST => "cannot move across servers: $d")
-	unless $d =~ s/^\Q@{[_format_http($s)]}//;
-    $s->{dest} = ($_FP->from_literal($d))[0];
-    return _output($s, BAD_REQUEST => 'Invalid destination name')
-	unless $s->{dest};
-    return
-	unless ($s->{dest_file} = $s->{file}->new)->unsafe_load({
-	    %{_file_args($s)},
-	    path => $s->{dest},
-	});
+	unless $d =~ s/^\Q@{[_fix_http($s, $s->{req}->format_http_prefix)]}//;
+    return _output($s, HTTP_NOT_IMPLEMENTED => 'Depth 0 unsupported for COPY')
+	if $s->{method} eq 'copy' && !_depth($s);
+    my($t, $r, $path_info) = Bivio::UI::Task->parse_uri($d, $s->{req});
+    return _output(
+	$s, FORBIDDEN => "cannot $s->{method} across file system tasks"
+    ) unless $t == $s->{req}->get('task_id');
+    return unless $s->{dest_list} = _load($s, $r, $path_info, 1);
+    return _output(
+	$s, FORBIDDEN => "cannot $s->{method} across file system classes"
+    ) unless $s->{dest_list}->isa(ref($s->{list}));
     return _output($s, HTTP_PRECONDITION_FAILED => 'Destination exists')
-	if ($s->{r}->header_in('overwrite') || 'T') =~ /f/i;
-    $s->{dest_file}->delete_deep;
-    $s->{dest_existed} = 1;
+	if ($s->{dest_existed} = _exists($s->{dest_list}))
+	&& ($s->{r}->header_in('overwrite') || 'T') =~ /f/i;
+    _call($s, $s->{dest_list}, 'delete')
+	if $s->{dest_existed};
     return;
 }
 
@@ -104,29 +110,28 @@ sub _dav_copy {
     my($s) = @_;
     return 1
 	if _copy_move($s);
-    return _output($s, HTTP_NOT_IMPLEMENTED => 'Depth 0 unsupported for COPY')
-	unless _depth($s);
-    $s->{file}->copy_deep($s->{dest});
+    _call($s, copy => $s->{dest_list});
     return _output($s, $s->{dest_existed} ? 'HTTP_NO_CONTENT' : 'HTTP_CREATED');
 }
 
 sub _dav_delete {
     my($s) = @_;
-    $s->{file}->delete_deep;
+    _call($s, 'delete')
+	if $s->{exists};
     return _output($s, 'HTTP_OK');
 }
 
 sub _dav_edit {
-    my($s) = @_;
-    return _output(
-	$s, HTTP_OK => $s->{file}->get_content_type, $s->{file}->get_handle);
+    return _dav_get(@_);
 }
 
 sub _dav_get {
     my($s) = @_;
-    $s->{reply}->set_last_modified($s->{file}->get('modified_date_time'));
+    my($p) = _call($s, 'propfind');
+    $s->{reply}->set_last_modified($p->{getlastmodified})
+	if $p->{getlastmodified};
     return _output(
-	$s, HTTP_OK => $s->{file}->get_content_type, $s->{file}->get_handle);
+	$s, HTTP_OK => $p->{getcontenttype}, _call($s, 'get'));
 }
 
 sub _dav_head {
@@ -140,10 +145,10 @@ sub _dav_lock {
 sub _dav_mkcol {
     my($s) = @_;
     return _output($s, HTTP_CONFLICT => 'already exists')
-	if $s->{file}->is_loaded;
+	if _exists($s->{list});
     return _output($s, 'HTTP_UNSUPPORTED_MEDIA_TYPE')
 	if length($s->{content});
-    $s->{file}->create_folder(_file_args($s));
+    _call($s, 'mkcol');
     return _output($s, 'HTTP_CREATED');
 }
 
@@ -151,10 +156,7 @@ sub _dav_move {
     my($s) = @_;
     return 1
 	if _copy_move($s);
-    $s->{file}->update({
-	%{_file_args($s)},
-	path => $s->{dest},
-    });
+    _call($s, move => $s->{dest_list});
     return _output($s, $s->{dest_existed} ? 'HTTP_NO_CONTENT' : 'HTTP_CREATED');
 }
 
@@ -163,10 +165,14 @@ sub _dav_options {
     $s->{reply}->set_header(
 	Allow => join(
 	    ', ',
-	    qw(COPY DELETE GET HEAD LOCK MOVE OPTIONS POST PROPFIND UNLOCK),
-	    $s->{file}->unsafe_get('is_folder')
-		? qw(BROWSE INDEX MKCOL)
-		: qw(EDIT PUT SAVE),
+	    map(uc($_),
+		grep(
+		    !$s->{is_read_only} || $_ !~ $_WRITABLE,
+		    qw(copy delete get head lock move options propfind unlock),
+		    _call($s, 'propfind')->{getcontenttype}
+			? qw(edit put) : qw(mkcol),
+		),
+	    ),
 	),
     );
     return _output($s, 'HTTP_OK', '', \(''));
@@ -193,21 +199,19 @@ sub _dav_propfind {
 			 [propstat => [
 			     [prop => [
 				 [displayname => Bivio::Type::String->to_xml(
-				     $x->{path} =~ m{([^/]+)$},
+				     $x->{displayname},
 				 )],
-				 # MS
-				 [isroot => $x->{isroot} || '0'],
-				 [getlastmodified => $_DT->rfc822(
-				     $_DT->to_unix(
-					 $x->{modified_date_time}))],
-				 $x->{is_folder} ? (
-				     [getcontenttype => 'text/html'],
+				 $x->{getlastmodified}
+				     ? [getlastmodified => $_DT->rfc822(
+					 $x->{getlastmodified})]
+				     : (),
+				 $x->{getcontenttype} ? (
 				     [resourcetype => [
 					 [collection => ''],
 				     ]],
 				 ) : (
-				     [getcontenttype => $x->{type}],
-				     [getcontentlength => $x->{length}],
+				     [getcontenttype => $x->{getcontenttype}],
+				     [getcontentlength => $x->{getcontentlength}],
 				     [resourcetype => ''],
 				 ),
 			     ]],
@@ -216,25 +220,8 @@ sub _dav_propfind {
 		     ]],
 		 );
 	     }
-		 ($noroot ? () : {
-		     %{$s->{file}->get_shallow_copy},
-		     isroot => 1,
-		 }),
-		 $depth && $s->{file}->get('is_folder')
-		     ? @{$s->{file}->map_folder(
-			 sub {
-			     my($f) = @_;
-			     my($res) = $f->get_shallow_copy;
-			     unless ($res->{is_folder}) {
-				 $res->{length} = $f->get_content_length;
-				 $res->{type} = $f->get_content_type;
-			     }
-			     return $res;
-			  }, {
-			      _public_only($s) ? (is_public => 1) : (),
-			  },
-		     )}
-		     : (),
+	         ($noroot ? () : _call($s, 'propfind')),
+		 @{_call($s, 'propfind_children')},
 	     ),
 	     "</D:multistatus>\n",
 	 ),
@@ -243,10 +230,8 @@ sub _dav_propfind {
 
 sub _dav_put {
     my($s) = @_;
-    my($op) = ($s->{file}->is_loaded ? 'update' : 'create') . '_with_content';
-    $s->{file}->$op(_file_args($s), \$s->{content});
-    $op =~ s/_with_content/d/;
-    return _output($s, HTTP_OK => "$op $s->{path}");
+    _call($s, put => \$s->{content});
+    return _output($s, HTTP_OK => "PUT $s->{uri}");
 }
 
 sub _dav_unlock {
@@ -259,29 +244,61 @@ sub _depth {
     return defined($x) && length($x) ? $x : 'infinity';
 }
 
-sub _file_args {
-    my($s) = @_;
-    return {
-	volume => $s->{req}->get('Type.FileVolume'),
-	path => $s->{path},
-	_public_only($s) ? (is_public => 1) : (),
-    };
+sub _exists {
+    return shift->get_result_set_size > 0 ? 1 : 0;
+}
+
+sub _fix_http {
+    my($s, $v) = @_;
+    # Must match what the user asked for exactly
+    $v =~ s{^(https?://)[^/:]+}{$1@{[$s->{r}->hostname]}}
+	|| Bivio::Die->throw_die(DIE => $v);
+    return $v;
 }
 
 sub _format_http {
     my($s, $x) = @_;
-    $x = {
-	path => '/',
-	is_folder => 0,
-    } unless $x;
     my($res) = $s->{req}->format_http({
 	task_id => $s->{req}->get('task_id'),
 	query => undef,
-	path_info => $x->{path},
-    }) . ($x->{is_folder} && $x->{path} !~ m{/$} ? '/' : '');
-    # Must match what the user asked for exactly
-    $res =~ s{^(https?://)[^/:]+}{$1@{[$s->{r}->hostname]}} || die;
-    return $res;
+	path_info => "$s->{path_info}/$x->{uri}",
+    });
+    $res .= '/'
+	unless $res =~ m{/$} || $x->{getcontenttype};
+    return _fix_http($s, $res);
+}
+
+sub _load {
+    my($s, $realm, $path, $is_dest) = @_;
+    my($q) = {
+	path => $path,
+	realm => $realm,
+	model => $s->{root_model}->new,
+    };
+    my($m);
+    while (1) {
+	_trace($q) if $_TRACE;
+	unless ($q->{realm}->does_user_have_permissions(
+	    ${Bivio::Auth::PermissionSet->from_array(['DATA_READ'])},
+	    $s->{req},
+	)) {
+	    _output($s, FORBIDDEN => 'User does not have access');
+	    return;
+	}
+	$q->{is_read_only} = $q->{realm}->does_user_have_permissions(
+	    ${Bivio::Auth::PermissionSet->from_array(['DATA_WRITE'])},
+	    $s->{req},
+	) ? 0 : 1;
+	$m = $q->{model}->dav_load($q);
+	last unless ref($m) eq 'HASH';
+	$q = $m;
+    }
+    if ($q->{is_read_only} && $is_dest) {
+	_output($s, FORBIDDEN => 'Destination is read-only');
+	return;
+    }
+    $s->{is_read_only} = $q->{is_read_only};
+    return $m;
 }
 
 sub _other_op {
@@ -309,9 +326,12 @@ sub _output {
 
 sub _precondition {
     my($s) = @_;
+    $s->{is_read_only} ||= _call($s, 'is_read_only');
     return _output($s, FORBIDDEN => 'Write operations not permitted')
-	if _read_only($s) && $s->{method} =~ /^(COPY|DELETE|EDIT|LOCK|MKCOL|MOVE|PROPPATCH|PUT|UNLOCK)$/i;
-    my($exists) = $s->{file}->is_loaded;
+	if $s->{is_read_only} && $s->{method} =~ $_WRITABLE;
+    $s->{exists} = _exists($s->{list});
+    $s->{list}->set_cursor(0)
+	if $s->{exists};
     foreach my $x (
 	['if-non-match' => sub {
 	     return shift(@_)
@@ -337,23 +357,22 @@ sub _precondition {
 	next unless my $v = $s->{r}->header_in($h);
 	if ($h =~ /match/) {
 	    return 1
-		if $op->($exists)
+		if $op->($s->{exists})
 	}
 	else {
 	    my($t) = $_DT->from_literal($v);
 	    return _output($s, BAD_REQUEST => "Invalid time: $h: $v")
 		unless $t;
 	    return 1
-		if $exists && $op->(
-		    $_DT->compare($s->{file}->get('modified_date_time'), $t));
+		if $s->{exists} && $op->(
+		    $_DT->compare(
+			$s->{list}->get_by_regexp('modified_date_time'), $t));
 	}
     }
-    return _output($s, NOT_FOUND => "Resource does not exist: $s->{path}")
-	unless $exists || $s->{method} !~ /^(BROWSE|COPY|DELETE|EDIT|GET|HEAD|LOCK|MOVE|OPTIONS|PROPFIND|PROPPATCH|UNLOCK)$/i;
-    return _output($s, FORBIDDEN => 'Cannot operate on root directory')
-	if $s->{path} eq '/' && $s->{method} =~ /^(COPY|DELETE|EDIT|MOVE|PUT|SAVE)$/;
-    return _output($s, FORBIDDEN => "Resource is a directory: $s->{path}")
-	if $exists && $s->{file}->get('is_folder') && $s->{method} =~ /^(EDIT|SAVE|GET|HEAD|PUT)$/;
+    return _output($s, NOT_FOUND => "Resource does not exist: $s->{uri}")
+	unless $s->{exists} || $s->{method} !~ /^(copy|delete|edit|get|head|lock|move|options|propfind|unlock)$/;
+    return _output($s, FORBIDDEN => "Resource is a directory: $s->{uri}")
+	if $s->{exists} && $s->{list}->get_by_regexp('is_folder') && $s->{method} =~ /^(edit|get|head|put)$/;
     return;
 }
 
@@ -367,23 +386,6 @@ sub _propfind_render {
 	       "</D:$t>\n"
 	   ) : "<D:$t/>\n";
     } @_);
-}
-
-sub _public_only {
-    return shift->{role} eq 'ANONYMOUS';
-}
-
-sub _read_only {
-    return shift->{role} ne 'ADMINISTRATOR';
-}
-
-sub _role {
-    my($req) = @_;
-    foreach my $r (qw(ADMINISTRATOR MEMBER)) {
-	return $r
-	    if grep($_->equals_by_name($r), @{$req->get('auth_roles')});
-    }
-    return 'ANONYMOUS';
 }
 
 1;
