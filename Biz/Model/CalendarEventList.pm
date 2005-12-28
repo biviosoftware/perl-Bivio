8fdf08c5e0df5bffe422a7d6898765ca925ad661
Rob Nagler <github@q33.us>
2005-12-28 04:41:35 +0000

    moved update_from_ics from CalendarEvent added uid to list

--- Biz/Model/CalendarEventList.pm
+++ Biz/Model/CalendarEventList.pm
@@ -5,6 +5,7 @@ use strict;
 use base 'Bivio::Biz::ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_CE) = Bivio::Biz::Model->get_instance('CalendarEvent');
 
 sub internal_initialize {
     my($self) = @_;
@@ -15,17 +16,49 @@ sub internal_initialize {
         primary_key => [
 	    [qw{CalendarEvent.calendar_event_id RealmOwner.realm_id}],
 	 ],
-	order_by => [
-	    'CalendarEvent.dtstart',
-	    'CalendarEvent.dtend',
-	],
-	other => [
-	    'RealmOwner.display_name',
-	    'RealmOwner.name',
-	    'CalendarEvent.location',
-	    'CalendarEvent.description',
-	],
+	order_by => [qw(
+	    CalendarEvent.dtstart
+	    CalendarEvent.dtend
+	)],
+	other => [qw(
+	    RealmOwner.display_name
+	    RealmOwner.name
+	    RealmOwner.creation_date_time
+	    CalendarEvent.modified_date_time
+	    CalendarEvent.location
+	    CalendarEvent.description
+	    CalendarEvent.url
+	), {
+	    name => 'uid',
+	    type => 'RealmOwner.name',
+	}],
     });
+}
+
+sub internal_post_load_row {
+    my($self, $row) = @_;
+    $row->{uid} = $_CE->id_to_uid($row->{'CalendarEvent.calendar_event_id'});
+    return 1;
+}
+
+sub update_from_ics {
+    my($self, $ics) = @_;
+    my($old) = {map(($_->{uid} => $_), @{$self->map_iterate})};
+    my($ce) = $self->new_other('CalendarEvent');
+    foreach my $v (@{Bivio::MIME::Calendar->from_ics($ics)}) {
+        if (my $x = delete($old->{$v->{uid}})) {
+	    $ce->load({calendar_event_id => $x->{calendar_event_id}})
+		->update_from_vevent($v);
+	}
+	else {
+	    $ce->create_from_vevent($v);
+	}
+    }
+    foreach my $x (values(%$old)) {
+	$ce->load({calendar_event_id => $x->{calendar_event_id}})
+	    ->cascade_delete;
+    }
+    return;
 }
 
 1;
