8b26e98e800e5dbc5e5801fe88679d549b41a5f2
Alex Viggio <aviggio@bivio.biz>
2006-01-16 05:44:39 +0000

    *** empty log message ***

---
+++ Biz/Model/Website.pm
@@ -0,0 +1,22 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::Website;
+use strict;
+use base 'Bivio::Biz::Model::LocationBase';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    return {
+	version => 1,
+	table_name => 'website_t',
+	columns => {
+            realm_id => ['RealmOwner.realm_id', 'PRIMARY_KEY'],
+            location => ['Location', 'PRIMARY_KEY'],
+            url => ['HTTPURI', 'NONE'],
+        },
+	auth_id => 'realm_id',
+    };
+}
+
+1;
