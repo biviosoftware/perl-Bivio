2fadee5b0da225e61ca4e80528930c28595ac89c
Rob Nagler <github@q33.us>
2006-01-16 05:33:03 +0000

    *** empty log message ***

---
+++ Biz/Model/RealmMail.pm
@@ -0,0 +1,129 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::RealmMail;
+use strict;
+use base 'Bivio::Biz::PropertyModel';
+use Bivio::Mail::Incoming;
+use Bivio::IO::Trace;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_MAX_LINE) = Bivio::Type->get_instance('Line')->get_width;
+my($_MAX_EMAIL) = Bivio::Type->get_instance('Email')->get_width;
+my($_FOLDER) = __PACKAGE__->get_instance('Forum')->MAIL_FOLDER;
+my($_ILLEGAL_CHAR_REGEXP) = __PACKAGE__->get_instance('RealmFile')
+    ->get_field_type('path')->ILLEGAL_CHAR_REGEXP;
+our($_TRACE);
+my($_DT) = Bivio::Type->get_instance('DateTime');
+
+sub create_from_file {
+    my($self, $file) = @_;
+    return _create($self, Bivio::Mail::Incoming->new($file->get_content), $file);
+}
+
+sub create_from_rfc822 {
+    my($self, $rfc822) = @_;
+    return _create($self, _create_file($self, $rfc822));
+}
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+        table_name => 'realm_mail_t',
+	as_string_fields => [qw(message_id)],
+        columns => {
+            realm_file_id => ['RealmFile.realm_file_id', 'PRIMARY_KEY'],
+	    # Index on realm_id, message_id.  Does it need to be unique?
+            realm_id => ['RealmOwner.realm_id', 'NOT_NULL'],
+	    message_id => ['Line', 'NOT_NULL'],
+	    thread_root_id => ['RealmFile.realm_file_id', 'NOT_NULL'],
+	    thread_parent_id => ['RealmFile.realm_file_id', 'NONE'],
+            from_email => ['Email', 'NOT_NULL'],
+            subject => ['Line', 'NOT_NULL'],
+            subject_lc => ['Line', 'NOT_NULL'],
+        },
+        auth_id => 'realm_id',
+    });
+}
+
+sub _chomp_subject {
+    my($s, $sortable) = @_;
+    $s = ''
+	unless defined($s);
+    $s =~ s/\s+/ /;
+    0 while $s =~ s/^(\s+|\[\S*\]|[a-z]{1,3}(:|\[\d+\])|\.)//i;
+    $s =~ s{\s$|/|$_ILLEGAL_CHAR_REGEXP}{}og
+	if $sortable;
+    return length($s) ? substr($s, 0, $_MAX_LINE) : '(No Subject)';
+}
+
+sub _create {
+    my($self, $in, $file) = @_;
+    $self->create(
+	_thread_values({
+	    map(($_ => $file->get($_)), qw(realm_id realm_file_id)),
+	    message_id => substr($in->get_message_id, 0, $_MAX_LINE),
+	    from_email => substr(lc(($in->get_from)[0]), 0, $_MAX_EMAIL),
+	    subject => _chomp_subject($in->get_subject),
+	    subject_lc => lc(_chomp_subject($in->get_subject, 1)),
+	}, $in),
+    );
+    return $in;
+}
+
+sub _create_file {
+    my($self, $rfc822) = @_;
+    my($in) = Bivio::Mail::Incoming->new($rfc822);
+    my($date) = Bivio::Type::DateTime->from_unix($in->get_date_time || time);
+    return (
+	$in,
+	$self->new_other('RealmFile')->create_with_content({
+	    override_is_read_only => 1,
+	    path => $_FOLDER
+		. '/'
+		. join('-', $_DT->get_parts($date, qw(year month)))
+		. '/'
+		. _chomp_subject($in->get_subject, 1)
+		. ' '
+		. $_DT->to_file_name($date)
+		. sprintf('%03d', int(rand(1_000)))
+		. '.eml',
+	    user_id => $self->get_request->get('auth_user_id')
+		|| $self->new_other('RealmUser')
+		    ->get_any_online_admin->get('realm_id'),
+	    modified_date_time => $date,
+	}, $rfc822),
+    );
+}
+
+sub _thread_values {
+    my($values, $in) = @_;
+    my($row);
+    foreach my $ref ($in->get_references) {
+        last if $row = Bivio::SQL::Connection->execute_one_row(
+            'SELECT realm_file_id, thread_root_id, message_id
+            FROM realm_mail_t
+            WHERE realm_id = ?
+            AND realm_file_id < ?
+            AND message_id = ?',
+	    [@{$values}{qw(realm_id realm_file_id)}, $ref],
+	);
+    }
+    $row = Bivio::SQL::Connection->execute_one_row(
+	'SELECT realm_file_id, thread_root_id, subject_lc
+        FROM realm_mail_t
+        WHERE realm_id = ?
+	AND thread_parent_id IS NULL
+	AND realm_file_id < ?
+        AND subject_lc = ?
+	ORDER BY realm_file_id ASC',
+	[@{$values}{qw(realm_id realm_file_id subject_lc)}],
+    ) unless $row;
+    $row ||= [];
+    _trace($values->{realm_file_id}, ' ', $row) if $_TRACE;
+    $values->{thread_parent_id} = $row->[0];
+    $values->{thread_root_id} = $row->[1] || $values->{realm_file_id};
+    return $values;
+}
+
+1;
---
+++ Test/PropertyModel.pm
@@ -0,0 +1,131 @@
+# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Test::PropertyModel;
+use strict;
+$Bivio::Test::PropertyModel::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+$_ = $Bivio::Test::PropertyModel::VERSION;
+
+=head1 NAME
+
+Bivio::Test::PropertyModel - x
+
+=head1 RELEASE SCOPE
+
+Bivio
+
+=head1 SYNOPSIS
+
+    use Bivio::Test::PropertyModel;
+
+=cut
+
+=head1 EXTENDS
+
+L<Bivio::Test>
+
+=cut
+
+use Bivio::Test::ModelBase;
+use Bivio::Test::Request;
+@Bivio::Test::PropertyModel::ISA = ('Bivio::Test::ModelBase');
+
+=head1 DESCRIPTION
+
+C<Bivio::Test::PropertyModel>
+
+=cut
+
+#=IMPORTS
+
+#=VARIABLES
+Bivio::Test::Request->initialize_fully;
+
+=head1 FACTORIES
+
+=cut
+
+=for html <a name="new"></a>
+
+=head2 static new() : Bivio::Test::PropertyModel
+
+=cut
+
+sub new {
+    my($proto, $attrs) = @_;
+    my($model) = ref($attrs) ? delete($attrs->{model}) : $attrs;
+    $attrs = {}
+	unless ref($attrs);
+    my($m) = Bivio::Biz::Model->get_instance($model);
+    return $proto->SUPER::new({
+	class_name => $m->package_name,
+	check_return => sub {
+	    my($case, $actual, $expect) = @_;
+	    return $expect
+		unless $case->get('method') =~ /^(create|update)/;
+	    my($e) = $expect->[0];
+	    return $expect
+		unless ref($e) eq 'HASH' && @$expect == 1;
+	    my($o) = $case->get('object');
+	    $e = _walk_tree_expect($case, $e);
+	    $case->actual_return([_walk_tree_actual($case, $e, [])]);
+	    return [$e];
+	},
+	#TODO compute_return
+	%$attrs,
+    });
+}
+
+=head1 METHODS
+
+=cut
+
+=for html <a name="unit"></a>
+
+=head2 unit(array_ref case_group)
+
+Example RealmMail.bunit.
+
+=cut
+
+sub unit {
+    return shift->new(shift)->unit(shift)
+	if @_ == 3;
+    my($self, $method_groups) = @_;
+    return $self->SUPER::unit([
+	[Bivio::Test::Request->get_instance] => $method_groups,
+    ]);
+}
+
+#=PRIVATE SUBROUTINES
+
+sub _walk_tree_actual {
+    my($case, $e, $names) = @_;
+    my($o) = $case->get('object');
+    return ref($e) eq 'HASH'
+	? {map(($_ => _walk_tree_actual($case, $e->{$_}, [@$names, $_])),
+	       keys(%$e))}
+	: @$names == 1 && $o->has_fields($names->[0])
+	? $o->get($names->[0])
+	: Bivio::Test::Request->get_instance->unsafe_get_nested(@$names);
+}
+
+sub _walk_tree_expect {
+    my($case, $e) = @_;
+    return ref($e) eq 'HASH'
+	? {map(($_ => _walk_tree_expect($case, $e->{$_})), keys(%$e))}
+	: ref($e) eq 'CODE'
+	    ? $e->($case)
+	    : $e;
+}
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+
+=head1 VERSION
+
+$Id$
+
+=cut
+
+1;
