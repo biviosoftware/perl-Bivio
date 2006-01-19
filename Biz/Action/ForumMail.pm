e852e6da03e8ff09e185fe3d3911ffa1d357253b
Rob Nagler <github@q33.us>
2006-01-19 18:43:47 +0000

    used RealmMail

--- Biz/Action/ForumMail.pm
+++ Biz/Action/ForumMail.pm
@@ -27,37 +27,4 @@ sub execute {
     return;
 }
 
-sub _forward_msg {
-######################## NEED TO COPY
-    my($req, $members_only) = @_;
-    my($realm_owner) = $req->get('auth_realm')->get('owner');
-    # Do NOT forward message if we detected a potential mail loop
-
-    return if $req->unsafe_get('mail_in_loop');
-
-    my($emails) = Bivio::Biz::Model->new($req, 'ClubUserList')->load_all
-	    ->get_outgoing_emails;
-    $req->throw_die('NOT_FOUND', 'all emails marked as invalid')
-            unless $emails;
-
-    my($msg) = $req->get('mail');
-    $msg->add_recipients($emails);
-
-    my($set_reply_to) = Societas::Biz::Model::Preferences
-            ->get_club_pref($req, 'MAIL_SET_REPLY_TO');
-    $msg->set_headers_for_list_send(
-	'recipient',
-	$realm_owner->get('display_name'),
-	$set_reply_to,
-	1,
-	$req,
-    );
-    # Include the club's URI in the header for convenience
-    $msg->get_head->replace('Organization',
-            $req->format_http(Bivio::Agent::TaskId::CLUB_HOME(),
-                    undef, $req->get('auth_realm')));
-    $msg->enqueue_send;
-    return;
-}
-
 1;
