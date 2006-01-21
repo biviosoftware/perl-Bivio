6756d1af09d98140e5857b37e561d540902383b9
Rob Nagler <github@q33.us>
2006-01-21 18:36:45 +0000

    *** empty log message ***

---
+++ Type/Object.pm
@@ -0,0 +1,11 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Type::Object;
+use strict;
+use base 'Bivio::Type';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+# Any object
+
+1;
