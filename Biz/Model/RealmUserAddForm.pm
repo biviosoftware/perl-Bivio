3257703336d75470d2b60265ec9341cbc5a94c1e
Rob Nagler <github@q33.us>
2006-02-14 17:08:50 +0000

    typo

--- Biz/Model/RealmUserAddForm.pm
+++ Biz/Model/RealmUserAddForm.pm
@@ -59,7 +59,7 @@ sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
-	visble => $self->internal_initialize_local_fields(
+	visible => $self->internal_initialize_local_fields(
 	    [qw(not_mail_recipient administrator file_writer)],
 	    qw(Boolean NONE),
 	),
