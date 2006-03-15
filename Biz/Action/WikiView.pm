d5d307313aae84643e345ea28cd592a782813ba7
Rob Nagler <github@q33.us>
2006-03-15 03:54:38 +0000

    *** empty log message ***

---
+++ Biz/Action/WikiView.pm
@@ -0,0 +1,36 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::WikiView;
+use strict;
+use base 'Bivio::Biz::Action';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_WN) = Bivio::Type->get_instance('WikiName');
+my($_WT) = Bivio::Type->get_instance('WikiText');
+
+sub execute {
+    my($proto, $req) = @_;
+    unless ($req->unsafe_get('path_info')) {
+	# To avoid name space issues, there always needs to be a path_info
+	$req->put(path_info => 'StartPage');
+	return $req->get('task_id');
+    }
+    my($p) = $_WN->absolute_path($req->get('path_info'));
+    my($rf) = Bivio::Biz::Model->new($req, 'RealmFile');
+    $proto->new->put_on_request($req, 1)->put(
+	name => $_WN->get_tail($p),
+	exists => 0,
+    )->put(
+	html => $_WT->render_html(
+	    $rf->load({path => $p})->get_content,
+	),
+	modified_date_time => $rf->get('modified_date_time'),
+	author => $rf->new_other('Email')->unauth_load_or_die({
+	    realm_id => $rf->get('user_id'),
+	})->get('email'),
+	exists => 1,
+    );
+    return 0;
+}
+
+1;
