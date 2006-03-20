63ca37204f876a491d12cbc2c282ebfefd3be352
Rob Nagler <github@q33.us>
2006-03-20 19:36:35 +0000

    modularize view autoload

---
+++ UI/ViewLanguageAUTOLOAD.pm
@@ -0,0 +1,26 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::ViewLanguageAUTOLOAD;
+use strict;
+use base 'Bivio::UNIVERSAL';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($AUTOLOAD);
+
+sub AUTOLOAD {
+    return Bivio::UI::ViewLanguage->call_method(
+	$AUTOLOAD, 'Bivio::UI::ViewLanguage', @_,
+    );
+}
+
+sub import {
+    my($pkg) = caller();
+    no strict qw(refs);
+    *{$pkg.'::AUTOLOAD'} = \$AUTOLOAD;
+    # Suppress 'Subroutine %s redefined' warning
+    local($^W) = 0;
+    *{$pkg.'::AUTOLOAD'} = \&AUTOLOAD;
+    return;
+}
+
+1;
