33923fd395c245ab1e142e1c0f612c9297eaa191
Rob Nagler <github@q33.us>
2006-03-21 20:26:09 +0000

    fixup to use initialize

--- UI/XHTML/Widget/RoundedBox.pm
+++ UI/XHTML/Widget/RoundedBox.pm
@@ -2,22 +2,31 @@
 # $Id$
 package Bivio::UI::XHTML::Widget::RoundedBox;
 use strict;
-use base 'Bivio::UI::Widget::Join';
+use base 'Bivio::UI::HTML::Widget::Tag';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-sub new {
-    my($self) = shift->SUPER::new(@_);
-    return $self->put(
-        values => [
+sub initialize {
+    my($self) = @_;
+    $self->put_unless_exists(
+	class => 'rounded_box',
+    )->put(
+        value => Join([
 	    Tag(div => '', 'top_left'),
 	    Tag(div => '', 'top_right'),
-	    @{$self->get('values')},
+	    Tag(div => $self->get('value'), 'body'),
 	    Tag(div => '', 'bottom_left'),
 	    Tag(div => '', 'bottom_right'),
-	],
+	]),
     );
+    return shift->SUPER::initialize(@_);
+}
+
+
+sub internal_new_args {
+    return shift->SUPER::internal_new_args(div => @_);
 }
 
 1;
+
