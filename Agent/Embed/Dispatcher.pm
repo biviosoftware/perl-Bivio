f6b8d9db4c19d60f41a7c102fa0cba1e4a6c5224
Rob Nagler <github@q33.us>
2006-03-24 07:39:02 +0000

    *** empty log message ***

---
+++ Agent/Embed/Dispatcher.pm
@@ -0,0 +1,40 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Agent::Embed::Dispatcher;
+use strict;
+use base 'Bivio::Agent::Dispatcher';
+use Bivio::Agent::Embed::Request;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($_SELF) = __PACKAGE__->initialize;
+
+sub call_task {
+    shift;
+    my($req) = shift;
+    $req->internal_clear_current;
+    my($die) = $_SELF->process_request($req, @_);
+    $req->internal_set_current;
+    $die->throw
+	if $die;
+    return (
+	$req->get('Bivio::Agent::Embed::Reply'),
+	$req->delete('Bivio::Agent::Embed::Reply'),
+    )[0];
+}
+
+sub create_request {
+    shift;
+    return Bivio::Agent::Embed::Request->new(@_);
+}
+
+sub initialize {
+    return if $_SELF;
+    my($prev) = Bivio::Agent::Request->get_current;
+    $_SELF = shift->new;
+    $_SELF->SUPER::initialize;
+    Bivio::Agent::Embed::Request->clear_current
+        unless $prev;
+    return $_SELF
+}
+
+1;
---
+++ Agent/Embed/Request.pm
@@ -0,0 +1,58 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Agent::Embed::Request;
+use strict;
+use base 'Bivio::Agent::Request';
+use Bivio::Agent::Embed::Reply;
+use Bivio::Agent::HTTP::Query;
+use Bivio::HTML;
+use Bivio::UI::Task;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub get_form {
+    my($self) = @_;
+    return $self->get('form');
+}
+
+sub new {
+    my(undef, $req, $full_uri) = @_;
+    my($query) = $1
+	if $full_uri =~ s/\?(.*)//;
+    my($self) = shift->internal_new({});
+    $self->put_durable(
+	@{$req->map_each(sub {
+	    my(undef, $k, $v) = @_;
+            return $k =~ m{
+                ^(?:client_addr|r|is_secure|timezone|auth_user|super_user_id)$
+		|\bUserAgent$
+	    }ix ? ($k => $v) : ();
+        })},
+	parent_request => $req,
+	reply => Bivio::Agent::Embed::Reply->new->put(parent_request => $req),
+	embed_level => ($req->unsafe_get('embed_level') || 0) + 1,
+    );
+    $self->throw_die(DIE => {
+	message => 'embedding too deep; possible nested loop',
+	embed_level => $self->get('embed_level'),
+	parent_request => $self->get('parent_request'),
+    }) if $self->get('embed_level') > 2;
+    if (my $f = $req->unsafe_get('Bivio::UI::Facade')) {
+	$f->setup_request($self);
+    }
+    my($task_id, $auth_realm, $path_info, $uri)
+	= Bivio::UI::Task->parse_uri($full_uri, $self);
+    $self->internal_set_current();
+    $query = Bivio::Agent::HTTP::Query->parse($query);
+    delete($query->{auth_id})
+	if $query;
+    return $self->put_durable(
+	uri => $uri && Bivio::HTML->escape_uri($uri),
+	query => $query,
+	path_info => $path_info,
+	task_id => $task_id,
+	form => undef,
+    )->internal_initialize($auth_realm, $self->get('auth_user'));
+}
+
+1;
