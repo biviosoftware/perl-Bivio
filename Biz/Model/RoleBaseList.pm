978d7b615fd5d86d1c64af282ba5b06da95f433e
Rob Nagler <github@q33.us>
2006-04-03 03:49:16 +0000

    *** empty log message ***

---
+++ Biz/Model/RoleBaseList.pm
@@ -0,0 +1,36 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::RoleBaseList;
+use strict;
+use base 'Bivio::Biz::ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+	other => [{
+	    name => 'roles',
+	    type => 'Array',
+	    constraint => 'NONE',
+	}],
+    });
+}
+
+sub internal_load_rows {
+    my($self) = @_;
+    my($pk) = $self->get_info('primary_key_names');
+    $self->die('must be single-valued primary_key')
+	unless @$pk == 1;
+    $pk = $pk->[0];
+    my($roles) = {};
+    return [grep(
+	push(
+	    @{$_->{roles} = ($roles->{$_->{$pk}} ||= [])},
+	    $_->{'RealmUser.role'}
+	) == 1,
+	@{shift->SUPER::internal_load_rows(@_)},
+    )];
+}
+
+1;
