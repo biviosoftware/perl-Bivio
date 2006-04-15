b2d26ae03e948d811c62c5834bdf1c7eb800cb6e
Rob Nagler <github@q33.us>
2006-04-15 02:37:14 +0000

    TaskMenu accepts hash_ref for an element, which contains named parameters including all the format_uri named params.  Interface changed to take task_id and label as first two elements.

--- UI/XHTML/Widget/TaskMenu.pm
+++ UI/XHTML/Widget/TaskMenu.pm
@@ -4,27 +4,55 @@ package Bivio::UI::XHTML::Widget::TaskMenu;
 use strict;
 use base 'Bivio::UI::HTML::Widget::Tag';
 use Bivio::UI::ViewLanguageAUTOLOAD;
+use Bivio::Agent::Request;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_URI) = Bivio::Agent::Request->FORMAT_URI_PARAMETERS;
+my($_PARAMS) = [
+    'task_id',
+    'label',
+    @$_URI[1..$#$_URI],
+    'control',
+    'uri',
+];
+
+sub internal_as_string {
+    my($self) = @_;
+    return map(
+	(ref($_) =~ /Link/ ? $_->get('_task_menu_cfg')->{task_id}->get_name
+	    : ref($_) eq 'ARRAY' ? $_->[0]
+	    : ref($_) eq 'HASH' ? $_->{task_id}
+	    : $_),
+	@{$self->unsafe_get('task_map') || []},
+    );
+}
 
 sub initialize {
     my($self) = @_;
     $self->put_unless_exists(
-	task_name => [['->get_request'], 'task_id', '->get_name'],
+	selected_task_name => [['->get_request'], 'task_id', '->get_name'],
 	class => 'task_menu',
     );
-    $self->initialize_attr('task_name');
+    $self->initialize_attr('selected_task_name');
     $self->put(
 	tag => 'div',
 	task_map => [map({
-	    my($task, $href, $label, $control) = ref($_) ? @$_ : $_;
-	    $task = Bivio::Agent::TaskId->$task();
-	    $self->initialize_value($task->get_name, Link(
-		vs_text('task_menu', 'title', $label || $task->get_name),
-		$href || $task,
+	    my(undef, $cfg) = $self->name_parameters(
+		$_PARAMS, ref($_) eq 'ARRAY' ? $_ : [$_]);
+	    $cfg->{task_id} = Bivio::Agent::TaskId->from_any($cfg->{task_id});
+	    $cfg->{label} ||= $cfg->{task_id}->get_name;
+	    $cfg->{uri} ||= URI({
+#TODO: Should this default get moved to URI?
+		query => undef,
+		path_info => undef,
+		_cfg($cfg, @$_URI),
+	    });
+	    $self->initialize_value($cfg->{label}, Link(
+		vs_text('task_menu', 'title', $cfg->{label}),
+		$cfg->{uri},
 		{
-		    _task_menu_task_id => $task,
-		    $control ? (control => $control) : (),
+		    _task_menu_cfg => $cfg,
+		    _cfg($cfg, 'control'),
 		},
 	    ));
 	} @{$self->get('task_map')})],
@@ -39,17 +67,21 @@ sub internal_new_args {
 sub render_tag_value {
     my($self, $source, $buffer) = @_;
     my($req) = $self->get_request;
-    my($selected) = $self->render_simple_attr('task_name', $source);
+    my($selected) = $self->render_simple_attr('selected_task_name', $source);
     my($need_sep) = 0;
     foreach my $w (@{$self->get('task_map')}) {
-	my($t) = $w->get('_task_menu_task_id');
-	next unless $req->can_user_execute_task($t);
+	my($cfg) = $w->get('_task_menu_cfg');
+	my($r) = $self->render_simple_value($cfg->{realm}, $source);
+	next unless $req->can_user_execute_task(
+	    $cfg->{task_id},
+	    $r || undef,
+	);
 	my($b) = '';
 #TODO: Shouldn't change global state.  Rather put a closure that renders
 #       with a value off the request (or lexical value ?)
 	$w->put(class => join(' ',
 	    $need_sep ? 'want_sep' : (),
-	    $t->equals_by_name($selected) ? 'selected' : (),
+	    $cfg->{task_id}->equals_by_name($selected) ? 'selected' : (),
 	))->render(
 	    $source, \$b,
         );
@@ -58,6 +90,11 @@ sub render_tag_value {
 	$$buffer .= $b;
     }
     return;
+}
+
+sub _cfg {
+    my($cfg) = shift;
+    return map(exists($cfg->{$_}) ? ($_ => $cfg->{$_}) : (), @_),
 }
 
 1;
