11390bf52c46e8073b5e7c5c59a9f00d72c584ee
Rob Nagler <github@q33.us>
2006-04-18 21:33:48 +0000

    *** empty log message ***

---
+++ UI/HTML/Widget/EmptyTag.pm
@@ -0,0 +1,26 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::HTML::Widget::EmptyTag;
+use strict;
+use base 'Bivio::UI::HTML::Widget::Tag';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub initialize {
+    my($self) = @_;
+    $self->put_unless_exists(
+	tag_if_empty => 1,
+	value => '',
+    );
+    return shift->SUPER::initialize(@_);
+}
+
+sub internal_as_string {
+    return shift->unsafe_get('tag', 'class');
+}
+
+sub internal_new_args {
+    return shift->internal_compute_new_args(['tag'], \@_);
+}
+
+1;
