955e6fe42da86afe62680f06642ce2a5d90073f0
Rob Nagler <github@q33.us>
2006-05-13 22:05:56 +0000

    *** empty log message ***

---
+++ Biz/Action/EmptyReply.pm
@@ -0,0 +1,32 @@
+# Copyright (c) 2002-2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::EmptyReply;
+use strict;
+use base 'Bivio::Biz::Action';
+use Bivio::Ext::ApacheConstants;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub execute {
+    my($proto, $req, $status) = @_;
+    $status ||= 'HTTP_OK';
+    my($buffer) = '';
+    $req->get('reply')
+	->set_http_status(Bivio::Ext::ApacheConstants->$status())
+	->set_output(\$buffer);
+    return 0;
+}
+
+sub execute_forbidden {
+    return shift->execute(shift, 'FORBIDDEN');
+}
+
+sub execute_no_resources {
+    return shift->execute(shift, 'HTTP_SERVICE_UNAVAILABLE');
+}
+
+sub execute_not_found {
+    return shift->execute(shift, 'NOT_FOUND');
+}
+
+1;
