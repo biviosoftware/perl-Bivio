ed14043221b4ce406401cf73eca6fc1001188058
Paul Moeller <moeller@bivio.biz>
2006-06-24 16:35:13 +0000

    fixup the bivio test recipient header on the message to be forwarded

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -9,8 +9,11 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub execute {
     my(undef, $req) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
+    my($msg) = $mr->get('message')->{content};
+    # fix up test recipient for remote acceptance tests
+    $$msg =~ s/^(@{[Bivio::Mail::Common->TEST_RECIPIENT_HDR]}:\s).*$/$1@{[$mr->get('recipient')]}/m;
     # Let sendmail handle mail loop detection (for now)
-    Bivio::Mail::Incoming->new($mr->get('message')->{content})
+    Bivio::Mail::Incoming->new($msg)
         ->set_recipients($mr->get('recipient'), $req)->enqueue_send($req);
     return;
 }
