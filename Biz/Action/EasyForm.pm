f8aec65c2cc83263b751fa1e25a9c17663716427
Alex Viggio <aviggio@bivio.biz>
2006-07-12 22:13:38 +0000

    Added attributes to request to support optional email alert

--- Biz/Action/EasyForm.pm
+++ Biz/Action/EasyForm.pm
@@ -11,7 +11,12 @@ my($_FP) = __PACKAGE__->use('Type.FilePath');
 
 sub execute {
     my($proto, $req) = @_;
-    my($form, $dir, $rf, $fields);
+    my($m) = $req->get('r')->method;
+    $req->throw_die(CORRUPT_FORM => {
+	message => 'must be HTTP POST',
+        entity => $m,
+    }) unless lc($m) eq 'post';
+    my($form, $dir, $rf, $fields, $has_error, $error_msg);
     if (my $die = Bivio::Die->catch(sub {
 	# Make sure we really do have a uri for this task, and only
 	# get the last part.  This is extra safety so we couldn't possibly
@@ -45,24 +50,34 @@ sub execute {
 	    }, \(''));
 	my $keys = [sort(keys(%$form))]
 	    if $form;
-	$rf->append_content(
-	    \($dir
-	    . $req->get('path_info')
+	$error_msg = $dir . $req->get('path_info')
 	    . '.csv: failed with error: '
 	    . ($die->get('attrs')->{message}
 		   || (($die->get('code')->eq_model_not_found
 		       ? 'CSV file not found'
 		       : $die->get('code')->as_string)
 		       . "\n"))
-	    . ($form ? ${Bivio::Util::CSV->to_csv_text([
+	    . (($form ? ${Bivio::Util::CSV->to_csv_text([
 		$keys,
 		[map($form->{$_}, @$keys)],
-	    ])} : '<no form data>')
-	  ),
-	);
+	    ])} : '<no form data>') . "\n");
+	$rf->append_content(\($error_msg));
+	$has_error = 1;
     }
-    $req->client_redirect($req->get('query')->{goto});
-    return;
+    my($rn) = $req->get_nested(qw(auth_realm owner name));
+    my($fn) = $has_error ? '/Error.log' : $req->get('path_info') . '.csv';
+    $req->put(easy_form => Bivio::Collection::Attributes
+	->new({
+	    mail_to => $rn,
+	    file_path => '/' . $rn . '/file/' . (($dir =~ /(.*)\/$/)[0]) . $fn,
+	    error => $has_error,
+	    error_msg => $error_msg,
+	    labels => $fields,
+	    values => defined($form) ? {%$form} : {},
+	}));
+    return {
+	uri => $req->get('query')->{goto},
+    };
 }
 
 sub _csv_fields {
@@ -78,9 +93,9 @@ sub _csv_fields {
 
 sub _form_fields {
     my($req, $dir) = @_;
-#TODO: get_form needs a static value from form_model for a file upload
-#     $req->put(form_model => 'Bivio::Biz::FormModel');
     my($form) = $req->get_form();
+    Bivio::Die->die('undefined form submitted')
+	unless defined($form);
     $form = {
 	map((lc($_) => _to_string(lc($_), $form->{$_}, $dir, $req)),
 	    keys(%$form)),
@@ -104,6 +119,8 @@ sub _to_string {
 	->load({path => $dir . $req->get('path_info')});
     Bivio::Die->die($rf->get('path'), ': must be a folder to receive files')
 	unless $rf->get('is_folder');
+    return ''
+	unless defined($_FP->get_clean_tail($value->{filename}));
     return $rf->create_with_content({
 	path => $_FP->join(
 	    $rf->get('path'),
@@ -117,7 +134,6 @@ sub _to_string {
 	    || Bivio::Biz::Model::RealmUser->new
 		    ->get_any_online_admin->get('realm_id'),
     }, $value->{content})->get('path');
-    return;
 }
 
 1;
