f63c95e87aa01b337dfc86e4f74fb8bfdc2a1c34
Rob Nagler <github@q33.us>
2006-07-13 18:11:06 +0000

    * Bivio::Type::BlogFileName subclasses Bivio::Type::DocletFileName.   API standardized (to_absolute, is_absolute, PATH_REGEX, REGEX).

--- Type/BlogFileName.pm
+++ Type/BlogFileName.pm
@@ -2,35 +2,40 @@
 # $Id$
 package Bivio::Type::BlogFileName;
 use strict;
-use base 'Bivio::Type::Line';
+use base 'Bivio::Type::DocletFileName';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_FP) = Bivio::Type->get_instance('FilePath');
-my($_IS_PATH) = qr{@{[$_FP->join(
-    '^(?:' . join('|', map(__PACKAGE__->to_path(undef, $_), 0, 1)) . ')',
-    '\d{6}',
-    '\d{8}$',
-)]}}oisx;
+my($_DT) = Bivio::Type->get_instance('DateTime');
 
 sub PRIVATE_FOLDER {
-    return '/Blog';
+    return shift->BLOG_FOLDER;
 }
 
-sub PUBLIC_FOLDER {
-    return $_FP->join($_FP->PUBLIC_FOLDER, shift->PRIVATE_FOLDER);
+sub PATH_REGEX {
+    return qr{@{[shift->join('\d{6}', '\d{8}')]}}o;
+}
+
+sub REGEX {
+    return qr{\d{14}};
 }
 
 sub SQL_LIKE_BASE {
-    return _base('_' x shift->get_width);
+    my($proto) = @_;
+    return _base($proto, '_' x $proto->get_width);
+}
+
+sub from_date_time {
+    my(undef, $date_time) = @_;
+    return $_DT->to_file_name($date_time);
 }
 
 sub from_literal {
-    my(undef, $value) = @_;
+    my($proto, $value) = @_;
     return (undef, undef)
 	unless defined($value) && length($value);
     # This is overly friendly, but we are only parsing URLs and such
-    $value =~ s,\D,,g;
-    return $value =~ /^\d{14}$/ ? $value
+    $value =~ s{\D}{}g;
+    return $value =~ /^@{[$proto->REGEX]}$/ ? $value
 	: (undef, Bivio::TypeError->BLOG_FILE_NAME);
 }
 
@@ -48,34 +53,20 @@ sub get_width {
     return 14;
 }
 
-sub is_path {
-    my(undef, $path) = @_;
-    return $path =~ $_IS_PATH ? 1 : 0;
-}
-
-sub to_path {
-    my($proto, $value, $is_public) = @_;
-    return $_FP->join(
-	$is_public ? $proto->PUBLIC_FOLDER : $proto->PRIVATE_FOLDER,
-	defined($value) ? _base($value) : $value,
-    );
-}
-
-sub to_sql_like_path {
-    my($proto, $is_public) = @_;
-    return lc($proto->to_path('_' x $proto->get_width, $is_public));
+sub to_absolute {
+    my($proto, $value) = (shift, shift);
+    return $proto->SUPER::to_absolute($value && _base($proto, $value), @_);
 }
 
 sub to_sql_param {
-    my(undef, $value) = @_;
-    return _base($value);
+    return _base(@_);
 }
 
 sub _base {
-    my($value) = @_;
+    my($proto, $value) = @_;
     Bivio::Die->die($value, ': invalid value')
         unless $value =~ /^([\d_]{6})([\d_]{8})$/;
-    return $_FP->join($1, $2);
+    return $proto->join($1, $2);
 }
 
 1;
