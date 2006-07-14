8c923c5d0510aff863f4e3fdbe2bf6215e71e193
Rob Nagler <github@q33.us>
2006-07-14 23:02:44 +0000

    transaction model changed again destroy_db added query api changed to select list of public/private realm_ids

--- Search/Xapian.pm
+++ Search/Xapian.pm
@@ -29,27 +29,54 @@ Bivio::IO::Config->register(my $_CFG = {
 
 sub delete_realm_file {
     my($proto, $realm_file_or_id, $req) = @_;
-    # IMPORTANT: Make sure you have a new instance for writes
-    _delete(
-	$proto,
-	$req || $realm_file_or_id->get_request,
-	'Q' . (
+    return _queue(
+	$proto, [
+	    delete_realm_file =>
 	    ref($realm_file_or_id) ? $realm_file_or_id->get('realm_file_id')
-	    : $realm_file_or_id
-	),
-    );
+		: $realm_file_or_id,
+	], $req
+    ) unless ref($proto);
+    _delete($proto, 'Q' . $realm_file_or_id, $req);
     return;
 }
 
-sub handle_commit {
-    my($self) = @_;
+sub destroy_db {
+    my(undef, $req) = @_;
+    Bivio::Die->die('general lock must be acquired')
+	unless Bivio::Biz::Model->new($req, 'Lock')->is_general_acquired;
+    Bivio::IO::Alert->info($_CFG->{db_path}, ': deleting');
+    Bivio::IO::File->rm_rf($_CFG->{db_path});
+    return;
+}
+
+sub execute {
+    my($proto, $req) = @_;
+    my($self) = $req->get(ref($proto) || $proto);
     my($db) = Search::Xapian::WritableDatabase->new(
 	$_CFG->{db_path}, Search::Xapian->DB_CREATE_OR_OPEN);
+    $self->put(db => $db);
     foreach my $op (@{$self->get('ops')}) {
+	_trace($op) if $_TRACE;
 	my($method) = shift(@$op);
-	$db->$method(@$op);
+	$self->$method(@$op);
     }
+    $self->delete('db');
     $db->flush;
+    return 0;
+}
+
+sub handle_commit {
+    my($self, $req) = @_;
+    if (Bivio::Biz::Model->new($req, 'Lock')->is_general_acquired) {
+	$req->put(ref($self) => $self);
+	$self->execute($req);
+	return;
+    }
+    $self->use('Bivio::Agent::Job::Dispatcher')->enqueue(
+	$req,
+	'JOB_XAPIAN_COMMIT',
+	{ref($self) => $self},
+    );
     return;
 }
 
@@ -64,23 +91,30 @@ sub handle_rollback {
 }
 
 sub update_realm_file {
-    my($proto, $realm_file) = @_;
-    # IMPORTANT: Make sure you have a new instance for writes
+    my($proto, $realm_file, $req) = @_;
+    return _queue(
+	$proto,
+	[update_realm_file => $realm_file->get('realm_file_id')],
+	$realm_file->get_request,
+    ) unless ref($proto);
+    my($rf) = Bivio::Biz::Model->new($req, 'RealmFile');
+    return unless $rf->unauth_load({realm_file_id => $realm_file})
+	&& !$rf->get('is_folder');
     _replace(
 	$proto,
-	$realm_file->get_request,
-	$realm_file->simple_package_name,
-	$realm_file->get(qw(realm_id realm_file_id)),
-	$proto->use('Bivio::Search::RealmFile')->parse_for_xapian($realm_file),
+	$req,
+	$rf->simple_package_name,
+	$rf->get(qw(realm_id realm_file_id)),
+	$proto->use('Bivio::Search::RealmFile')->parse_for_xapian($rf),
     );
     return;
 }
 
 sub query {
-    my($proto, $phrase, $offset, $length, $realm_ids, $only_is_public) = @_;
-# Need to narrow by realm_ids, is_public, etc.
+    my($proto, $phrase, $offset, $length, $private_realm_ids, $public_realm_ids_or_all) = @_;
     $offset ||= 0;
     $length ||= $_LENGTH;
+    $private_realm_ids ||= [];
     my($db) = Search::Xapian::Database->new($_CFG->{db_path});
     my($qp) = Search::Xapian::QueryParser->new();
     $qp->set_stemmer($_STEMMER);
@@ -89,11 +123,20 @@ sub query {
     my($q) = Search::Xapian::Query->new(
  	Search::Xapian->OP_AND,
  	$qp->parse_query($phrase, $_FLAGS),
-	$realm_ids && @$realm_ids ? Search::Xapian::Query->new(
+	Search::Xapian::Query->new(
 	    Search::Xapian->OP_OR,
-	    map(Search::Xapian::Query->new("XREALMID:$_"), @$realm_ids),
-	) : (),
-	$only_is_public ? Search::Xapian::Query->new("XISPUBLIC:1") : (),
+	    map(Search::Xapian::Query->new("XREALMID:$_"), @$private_realm_ids),
+	    ref($public_realm_ids_or_all) ? map(
+		Search::Xapian::Query->new(
+		    Search::Xapian->OP_AND,
+		    Search::Xapian::Query->new("XREALMID:$_"),
+		    Search::Xapian::Query->new('XISPUBLIC:1'),
+		),
+		@$public_realm_ids_or_all
+	    ) : $public_realm_ids_or_all
+	    ? Search::Xapian::Query->new('XISPUBLIC:1')
+	    : (),
+	),
     );
     my($res) = [map({
 	my($x) = $_;
@@ -128,25 +171,25 @@ sub query_list_model_initialize {
     });
 }
 
-sub _op {
+sub _delete {
+    my($self, $primary_id, $req) = @_;
+    return unless $primary_id;
+    $self->get('db')->delete_document_by_term($primary_id);
+    return;
+}
+
+sub _queue {
     my($proto, $op, $req) = @_;
     my($self) = $req->unsafe_get_txn_resource($proto);
     $req->push_txn_resource($self = $proto->new({ops => []}))
 	unless $self;
+    _trace($op) if $_TRACE;
     push(@{$self->get('ops')}, $op);
     return;
 }
 
-sub _delete {
-    my($proto, $req, $primary_id) = @_;
-    return unless $primary_id;
-    _op($proto, [delete_document_by_term => $primary_id], $req);
-    _trace($primary_id) if $_TRACE;
-    return;
-}
-
 sub _replace {
-    my($proto, $req, $class, $realm_id, $primary_id, $terms, $postings) = @_;
+    my($self, $req, $class, $realm_id, $primary_id, $terms, $postings) = @_;
     return unless $terms;
     my($doc) = Search::Xapian::Document->new;
     $doc->set_data('');
@@ -155,7 +198,8 @@ sub _replace {
     $doc->add_value(2, $primary_id);
     $primary_id = "Q$primary_id";
     foreach my $t ($primary_id, @$terms) {
-	$doc->add_term($t);
+#TODO: What is the actual max term length
+	$doc->add_term(substr($t, 0, 240));
     }
     my($i) = 1;
     foreach my $p (@$postings) {
@@ -165,8 +209,7 @@ sub _replace {
 	    unless $s eq $p;
 	$doc->add_posting($s, $i++);
     }
-    _op($proto, [replace_document_by_term => $primary_id, $doc], $req);
-    _trace($primary_id) if $_TRACE;
+    $self->get('db')->replace_document_by_term($primary_id, $doc);
     return;
 }
 
