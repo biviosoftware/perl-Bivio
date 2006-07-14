e5e32545565c8cf3bf670fa34a6221f6de566108
Rob Nagler <github@q33.us>
2006-07-14 23:04:47 +0000

    added from_content

--- Type/BlogTitle.pm
+++ Type/BlogTitle.pm
@@ -5,5 +5,12 @@ use strict;
 use base 'Bivio::Type::Line';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_BC) = Bivio::Type->get_instance('BlogContent');
+
+sub from_content {
+    my($self, $content) = @_;
+    my($res) = $_BC->split($content);
+    return ref($res) ? '(No Title)' : $res;
+}
 
 1;
