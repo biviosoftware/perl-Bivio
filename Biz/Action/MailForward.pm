f7ceefe8e44bc15cda5b50ec520dbdeb6b642fe4
Rob Nagler <github@q33.us>
2006-07-29 02:14:09 +0000

    undo X-Bivio-Test-Recipient hack

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -9,11 +9,8 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub execute {
     my(undef, $req) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
-    my($msg) = $mr->get('message')->{content};
-    # fix up test recipient for remote acceptance tests
-    $$msg =~ s/^(@{[Bivio::Mail::Common->TEST_RECIPIENT_HDR]}:\s).*$/$1@{[$mr->get('recipient')]}/m;
     # Let sendmail handle mail loop detection (for now)
-    Bivio::Mail::Incoming->new($msg)
+    Bivio::Mail::Incoming->new($mr->get('message')->{content})
         ->set_recipients($mr->get('recipient'), $req)->enqueue_send($req);
     return;
 }
