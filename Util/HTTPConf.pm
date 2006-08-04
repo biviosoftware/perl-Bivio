c2a8bc7c44c7c6d013293b003ed42dc8516733fb
Alex Viggio <aviggio@bivio.biz>
2006-08-04 21:24:18 +0000

    Override UseCanonicalName and replace '%v' with '%V' in LogFormat

--- Util/HTTPConf.pm
+++ Util/HTTPConf.pm
@@ -327,7 +327,8 @@ LockFile $lock_file
 ScoreBoardFile $log_directory/apache_runtime_status
 TypesConfig /etc/mime.types
 DefaultType text/plain
-LogFormat "%v %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+UseCanonicalName Off
+LogFormat "%V %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
 CustomLog $log_directory/access_log combined
 ErrorLog $log_directory/error_log
 LogLevel info
