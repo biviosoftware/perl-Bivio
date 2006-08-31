1bd38fc49a3463270b62fbf9199afcfe41463d56
Alex Viggio <aviggio@bivio.biz>
2006-08-31 23:14:56 +0000

    *** empty log message ***

---
+++ Type/ForumEmailMode.pm
@@ -0,0 +1,17 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Type::ForumEmailMode;
+use strict;
+use base 'Bivio::Type::Enum';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+__PACKAGE__->compile([
+    UNKNOWN => [0, "Select Option"],
+    DEFAULT => [1, "Forum Members"],
+    ADMIN_ONLY_FORUM_EMAIL => [2, "Forum Administrators"],
+    SYSTEM_USER_FORUM_EMAIL => [3, "Any User"],
+    PUBLIC_FORUM_EMAIL => [4, "Anyone"],
+]);
+
+1;
