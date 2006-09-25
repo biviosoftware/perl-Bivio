e9ab7390758b95c781796ed58d0c41dc545dde77
Paul Moeller <moeller@bivio.biz>
2006-09-25 17:06:30 +0000

    remove 'site' arguments and app specific methods

--- UI/HTML/Format/Link.pm
+++ UI/HTML/Format/Link.pm
@@ -47,10 +47,7 @@ links.
 
 =head2 static get_widget_value(any href) : string
 
-=head2 static get_widget_value(any href, string site, any site_args, ...) : string
-
-Returns an href, possibly as an /goto link.  If I<site> is specified,
-must match one of the site implementations below.
+Returns an href, possibly as an /goto link.
 
 href may be a L<Bivio::Agent::TaskId|Bivio::Agent::TaskId> or it
 may be a name of TaskId.
@@ -58,22 +55,17 @@ may be a name of TaskId.
 =cut
 
 sub get_widget_value {
-    my(undef, $href, $site) = (shift, shift, shift);
-    my($tag);
-    ($href, $tag) = &{\&{'_site_'.$site}}($href, @_)
-	    if $site;
+    my($source, $href) = @_;
     return $href if _format_task(\$href);
 
     # Not an offsite uri?  Just return $href.
-    return $href unless $href =~ /^\w+:/ || $tag;
+    return $href unless $href =~ /^\w+:/;
 
     return Bivio::UI::Task->format_realmless_uri(
-	    Bivio::Agent::TaskId::CLIENT_REDIRECT(),
-	   $tag,
-	   Bivio::Agent::Request->get_current)
-	    .'?'
-	    .Bivio::Biz::Action::ClientRedirect->QUERY_TAG()
-	    .'='.Bivio::HTML->escape_query($href);
+	Bivio::Agent::TaskId->CLIENT_REDIRECT, undef,
+	$source->get_request)
+	. '?' . Bivio::Biz::Action::ClientRedirect->QUERY_TAG
+	. '=' . Bivio::HTML->escape_query($href);
 }
 
 #=PRIVATE METHODS
@@ -97,36 +89,6 @@ sub _format_task {
     # This will result in an "onsite" uri, but may have https:/ in front
     $$href = Bivio::Agent::Request->get_current->format_stateless_uri($$href);
     return 1;
-}
-
-# _site_advertizement(any uri, string tag) : string
-#
-# Returns its arguments.  You may pass a TASK as the uri.  If there
-# is a tag, it will be return and used.
-#
-sub _site_advertizement {
-    return @_;
-}
-
-# _site_yahoo_quotes(array_ref ticker) : string
-#
-# Returns href for Yahoo Quotes site.
-#
-sub _site_yahoo_quotes {
-    my($tickers) = @_;
-    return 'http://quote.yahoo.com/q?d=v1&s='.
-	    Bivio::HTML->escape_query(join(' ', @$tickers));
-}
-
-# _site_zacks_quotes(string ticker) : string
-#
-# Returns href for Zacks Quotes site.
-#
-sub _site_zacks_quotes {
-    my($ticker) = @_;
-    return 'http://lada.zacks.com/jmfr2/jmfr2_q.php3?t='
-	    .Bivio::HTML->escape_query($ticker)
-	    .'&top=2&sub=1&partner=BIVIO';
 }
 
 =head1 COPYRIGHT
