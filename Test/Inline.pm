bc50178c08aeb64aff4e4684f0852e6a31d0b20e
Rob Nagler <github@q33.us>
2006-10-24 14:36:38 +0000

    *** empty log message ***

---
+++ Test/Inline.pm
@@ -0,0 +1,27 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Test::Inline;
+use strict;
+use base 'Bivio::Test::Unit';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub run_unit {
+    return shift->SUPER::run_unit(@_)
+	if @_ == 3;
+    my($self, $sub_or_cases) = @_;
+    my($req) = $self->use('Bivio::Test::Request')->initialize_fully;
+    return $self->SUPER::run_unit([
+	map({
+	    my($op) = $_;
+	    Bivio::Die->die($op, ': imperative cases are subs only')
+	        unless ref($op) eq 'CODE';
+	    ($self->builtin_class() => sub {
+		$op->();
+		return 1;
+	    });
+	} ref($sub_or_cases) eq 'CODE' ? $sub_or_cases : @$sub_or_cases),
+    ]);
+}
+
+1;
