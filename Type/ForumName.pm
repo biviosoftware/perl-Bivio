541236c8f1973a7f6fde512fcf6dbdb129d9d42c
Rob Nagler <github@q33.us>
2006-11-05 00:58:43 +0000

    * Bivio::Type::ForumName cleans up the name and supports   FIRST_CHAR_REGEXP better sharing with REGEXP

--- Type/ForumName.pm
+++ Type/ForumName.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2006 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::ForumName;
 use strict;
@@ -6,35 +6,41 @@ use base 'Bivio::Type::Name';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_SEP) = Bivio::Type->get_instance('RealmName')->SPECIAL_SEPARATOR;
-# This is same as SimpleRealmName, but this is loosely coupled
-my($_REGEXP_TOP) = qr/^([a-z][a-z0-9_]{2,})($_SEP|$)(.*)/io;
-my($_REGEXP) = qr/^([a-z][a-z0-9_]{2,})[\w$_SEP]*$/io;
+
+sub FIRST_CHAR_REGEXP {
+    return qr{[a-z]};
+}
+
+sub REGEXP {
+    my($c) = shift->FIRST_CHAR_REGEXP;
+    return qr/^($c\w{2,})(?:$_SEP(\w+($_SEP\w+)*)|)$/is;
+}
+
+sub extract_rest {
+    my($proto, $value) = @_;
+    return ($value =~ $proto->REGEXP)[1];
+}
 
 sub extract_top {
     my($proto, $value) = @_;
-    return ($value =~ $_REGEXP_TOP)[0];
-}
-
-sub extract_bottom {
-    my($proto, $value) = @_;
-    return ($value =~ $_REGEXP_TOP)[2];
+    return ($value =~ $proto->REGEXP)[0];
 }
 
 sub from_literal {
     my($proto, $value) = @_;
-    $value =~ s/^\s+|\s+$//g
+    $value =~ s/^\s+|\s+$//sg
 	if defined($value);
     my($v, $e) = $proto->SUPER::from_literal($value);
     return ($v, $e)
 	unless defined($v);
     return (undef, Bivio::TypeError->FORUM_NAME)
-	unless $v =~ $_REGEXP;
+	unless $v =~ $proto->REGEXP;
     return lc($v);
 }
 
 sub is_top {
     my($proto, $value) = @_;
-    return ($value =~ $_REGEXP_TOP)[0] eq $value ? 1 : 0;
+    return $proto->extract_top($value) eq $value ? 1 : 0;
 }
 
 1;
