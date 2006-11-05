244d7386f23dfa215449f6e1f9c66f3779bf2b0b
Rob Nagler <github@q33.us>
2006-11-05 01:01:58 +0000

    START_PAGE

--- Type/WikiName.pm
+++ Type/WikiName.pm
@@ -19,6 +19,10 @@ sub REGEX {
     return qr{((?-i:[A-Z][A-Z0-9]*[a-z][a-z0-9]*[A-Z][A-za-z0-9]*))}o;
 }
 
+sub START_PAGE {
+    return 'StartPage';
+}
+
 sub from_literal {
     my($proto) = shift;
     my($v, $e) = $proto->SUPER::from_literal(@_);
