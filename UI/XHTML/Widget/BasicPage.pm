3f8308e28f3c40609f8389a1707ff84f3ef36112
Rob Nagler <github@q33.us>
2006-11-05 01:06:25 +0000

    basic_* prefix

--- UI/XHTML/Widget/BasicPage.pm
+++ UI/XHTML/Widget/BasicPage.pm
@@ -11,7 +11,7 @@ sub new {
     my($proto, $attrs) = @_;
     view_declare(qw(page_title));
     view_put(map(
-	("simple_page_$_->[0]" => delete($attrs->{$_->[0]}) || $_->[1] || Join([''])),
+	("basic_page_$_->[0]" => delete($attrs->{$_->[0]}) || $_->[1] || Join([''])),
 	[title => vs_text(
 	    [sub {"page.title.$_[1]"}, ['task_id', '->get_name']])],
 	['meta_info'],
@@ -21,12 +21,12 @@ sub new {
     ));
     return $proto->SUPER::new($attrs)->put_unless_exists(
 	head => Join([
-	    Title([vs_site_name(), view_widget_value('simple_page_title')]),
-	    view_widget_value('simple_page_meta_info'),
-	    view_widget_value('simple_page_script'),
+	    Title([vs_site_name(), view_widget_value('basic_page_title')]),
+	    view_widget_value('basic_page_meta_info'),
+	    view_widget_value('basic_page_script'),
 	]),
-	body => view_widget_value('simple_page_content'),
-	style => view_widget_value('simple_page_style'),
+	body => view_widget_value('basic_page_content'),
+	style => view_widget_value('basic_page_style'),
 	xhtml => 1,
     );
 }
