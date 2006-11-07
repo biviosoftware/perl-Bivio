c03c7e51e137a76e5fd578f20e5e836cf68bdc83
Rob Nagler <github@q33.us>
2006-11-07 16:12:36 +0000

    Refactored to use File::Find to avoid forking every request and to simplify the code.   There were a few issues with files disappearing during handler calls which caused uninitialized variables that this refactoring also fixes.

--- Test/Reload.pm
+++ Test/Reload.pm
@@ -2,114 +2,57 @@
 # $Id$
 package Bivio::Test::Reload;
 use strict;
-$Bivio::Test::Reload::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Test::Reload::VERSION;
-
-=head1 NAME
-
-Bivio::Test::Reload - x
-
-=head1 RELEASE SCOPE
-
-Artisans
-
-=head1 SYNOPSIS
-
-    use Bivio::Test::Reload;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UNIVERSAL>
-
-=cut
-
-use Bivio::UNIVERSAL;
-@Bivio::Test::Reload::ISA = ('Bivio::UNIVERSAL');
-
-=head1 DESCRIPTION
-
-C<Bivio::Test::Reload>
-
-=cut
-
-#=IMPORTS
-use Bivio::IO::Alert;
+use base 'Bivio::UNIVERSAL';
 use Bivio::IO::ClassLoader;
-use Bivio::IO::Trace;
+use File::Find ();
 
-#=VARIABLES
-my($_CONF) = map({chomp($_); $_} `ls httpd*.conf`);
-my($_WATCH) = [
-    map({$_ =~ s{/BConf.pm$}{}; $_}
-        map({$INC{$_}}
-	     grep({$_ =~ /BConf.pm$/} keys(%INC))))
-];
-
-_trace('httpd.conf: ', $_CONF);
-_trace('watched directories: ', $_WATCH);
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="handler"></a>
-
-=head2 handler(??) : boolean
-
-Always returns true.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_LAST_TIME) = time;
+my($_WATCH) = [grep(s{/BConf.pm$}{}, @{[values(%INC)]})];
+# File::Find doesn't put '.' in the Path
+#TODO: Use "relative_path", which I think File::Find uses
+my($_INC) = [map($_ eq '.' ? '' : "$_/", @INC)];
+Bivio::IO::Alert->info('Watching: ', $_WATCH);
 
 sub handler {
-
-    my($unload) = [_modified()];
-    return 1
-        unless @$unload;
-
-    foreach my $module (@$unload) {
-	Bivio::IO::Alert->warn('Unloading: ', $module);
-	Bivio::IO::ClassLoader->delete_require($module);
+    if (my $modified = _modified()) {
+	_do(delete_require => $modified);
+	_do(simple_require => $modified);
+	$_LAST_TIME = time;
     }
-
-    foreach my $module (@$unload) {
-	Bivio::IO::Alert->warn('Loading: ', $module);
-	Bivio::IO::ClassLoader->simple_require($module);
-    }
-
-    system('touch', "$_CONF");
     return 1;
 }
 
-#=PRIVATE SUBROUTINES
+sub _do {
+    my($method, $modules) = @_;
+    foreach my $m (@$modules) {
+	Bivio::IO::Alert->info($method, ': ', $m);
+	Bivio::IO::ClassLoader->$method($m);
+    }
+    return;
+}
 
-# _modified() : ...
 sub _modified {
-    return _to_module(map({
-        _trace("Searching $_ for changed files...");
-	`find $_ -name 'Test' -prune -o -name 'files' -prune -o -name '.*' -prune -o \\( -name '*pm' -a -newer $_CONF \\) -print`;
-    } @$_WATCH));
+    my($res) = [];
+    File::Find::find({
+	no_chdir => 1,
+	wanted => sub {
+	    push(@$res, _modified_path($_))
+		unless $File::Find::prune
+	        = $_ =~ m{(?:^|/)(?:Test|files|CVS|t|\..*)$}s;
+	    return;
+	},
+    }, @$_WATCH);
+    return @$res ? $res : undef;
 }
 
-# _to_module() : 
-sub _to_module {
-    my(@paths) = @_;
-    return map({
-        my($path) = $_;
-        grep({$_ =~ s{/}{::}g if $_; $_}
-            map({$path =~ m{^$_/(.*)\.pm$}; $1} @INC));
-    } @paths);
+sub _modified_path {
+    my($path) = @_;
+    return unless $path =~ /\.pm$/ and my $t = (stat($path))[9];
+    return unless $t > $_LAST_TIME
+	and $path = (map($path =~ m{^$_(.*)\.pm$}, @$_INC))[0];
+    $path =~ s{/}{::}g;
+    return $path;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
