142f14511b363514d5a8203b1ee143f043b8333b
Rob Nagler <github@q33.us>
2006-11-08 22:56:09 +0000

    tag_if_empty generates <br /> tag_if_empty is set for empty tag names (BR, etc.) automatically

--- UI/HTML/Widget/Tag.pm
+++ UI/HTML/Widget/Tag.pm
@@ -3,8 +3,24 @@
 package Bivio::UI::HTML::Widget::Tag;
 use strict;
 use base 'Bivio::UI::HTML::Widget::ControlBase';
+use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_EMPTY) = [qw(
+    area
+    base
+    basefont
+    br
+    col
+    frame
+    hr
+    img
+    input
+    isindex
+    link
+    meta
+    param
+)];
 
 sub control_on_render {
     my($self, $source, $buffer) = @_;
@@ -15,18 +31,27 @@ sub control_on_render {
     my($t) = lc(${$self->render_attr('tag')});
     $self->die('tag', $source, $t, ': is not a valid HTML tag')
 	unless $t =~ /^[a-z]+\d*$/;
-    my($end) = "</$t>";
+    my($end) = length($b) ? ">$b</$t>" : ' />';
     $self->SUPER::control_on_render($source, \$t);
-    $$buffer .= "<$t>$b$end";
+    $$buffer .= "<$t$end";
     return;
 }
 
 sub initialize {
     my($self) = @_;
-    $self->map_invoke(
-	'unsafe_initialize_attr',
-	[qw(tag value)],
-    );
+    my($init) = [qw(tag value)];
+    unless ($self->unsafe_get('html_attrs')) {
+	my($a) = $self->map_each(sub {
+            my(undef, $k) = @_;
+	    return $k =~ /^[A-Z]+[0-9]?$/ ? $k : ();
+	});
+	$self->put(html_attrs => vs_html_attrs_merge($a))
+	    if @$a;
+    }
+    $self->put_unless_exists(tag_if_empty => 1)
+	if _empty($self->get('tag'));
+    $self->map_invoke(initialize_attr => [qw(tag value)]);
+    $self->unsafe_initialize_attr('tag_if_empty');
     return shift->SUPER::initialize(@_);
 }
 
@@ -35,7 +60,23 @@ sub internal_as_string {
 }
 
 sub internal_new_args {
-    return shift->internal_compute_new_args([qw(tag value)], \@_);
+    my($proto, $tag) = splice(@_, 0, 2);
+    return $proto->internal_compute_new_args(
+	[qw(tag value)],
+	[
+	    $tag,
+	    (!defined($_[0]) || $_[0] ne '') && _empty($tag) ? '' : (),
+	    @_,
+	],
+    );
+}
+
+sub _empty {
+    my($tag) = @_;
+    return 0
+	if ref($tag);
+    $tag = lc($tag);
+    return grep($tag eq $_, @$_EMPTY) ? 1 : 0;
 }
 
 1;
