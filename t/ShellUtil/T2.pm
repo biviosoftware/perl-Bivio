2bd3e18e29922bd3d2a7f46e2167430afbd1f0b5
Rob Nagler <github@q33.us>
2006-11-11 18:12:18 +0000

    *** empty log message ***

---
+++ t/ShellUtil/T2.pm
@@ -0,0 +1,9 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::t::ShellUtil::T2;
+use strict;
+use base 'Bivio::ShellUtil';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
