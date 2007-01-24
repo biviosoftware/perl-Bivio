e83f35ecd9197dcb660b575b993134eb3cbff648
Rob Nagler <github@q33.us>
2007-01-24 16:18:05 +0000

    *** empty log message ***

---
+++ Base.pm
@@ -0,0 +1,20 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Base;
+use strict;
+use base 'Bivio::UNIVERSAL';
+use Bivio::IO::ClassLoader;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub import {
+    my($first, $class) = @_;
+    Bivio::Die->eval_or_die(
+	'package ' . (caller(0))[0] . ";use base '"
+	. __PACKAGE__->use($class || Bivio::Die->die(
+	    'must supply a class name as an argument to use Bivio::Base'
+        )) . "';1");
+    return shift->SUPER::import($first);
+}
+
+1;
---
+++ t/Base/T1.pm
@@ -0,0 +1,9 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::t::Base::T1;
+use strict;
+use Bivio::Base 'Type.String';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
