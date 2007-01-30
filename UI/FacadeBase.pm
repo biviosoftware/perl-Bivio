f00e676cf76d739e9a53439fe1fa0e117a279d45
Rob Nagler <github@q33.us>
2007-01-30 11:00:55 +0000

    break into sections, and include more text for views

--- UI/FacadeBase.pm
+++ UI/FacadeBase.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2007 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::FacadeBase;
 use strict;
@@ -6,14 +6,335 @@ use base 'Bivio::UI::Facade';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-sub new {
-    my(undef, $config) = @_;
-    _merge(_cfg_tuple(), $config);
-    return shift->SUPER::new(@_);
+sub HELP_WIKI_REALM_NAME {
+    die('HELP_WIKI_REALM_NAME: must be defined if using wiki');
 }
 
-sub _cfg_tuple {
-    return !Bivio::Agent::TaskId->unsafe_from_name('FORUM_TUPLE_SLOT_TYPE_LIST') ? {} : {
+sub MAIL_RECEIVE_PREFIX {
+    return '_mail_receive_';
+}
+
+sub new {
+    my($proto, $config, @rest) = @_;
+    return $proto->SUPER::new(
+	_merge(
+	    _cfg_base(),
+	    map({
+		my($x) = \&{"_cfg_$_"};
+		Bivio::Agent::TaskId->unsafe_from_name($_) ? $x->($proto) : ();
+	    } qw(
+		forum_tuple_list
+		forum_wiki_view
+		login
+		mail_receive_dispatch
+                dav
+	    )),
+	    $config,
+	),
+    );
+}
+
+sub _cfg_base {
+    return {
+	clone => undef,
+	is_production => 1,
+	Color => [
+	    [[qw(
+		table_separator
+		table_odd_row_bg
+		table_even_row_bg
+		page_alink
+		page_link
+		page_link_hover
+		page_bg
+		page_text
+		page_vlink
+		summary_line
+		error
+		warning
+	    )] => -1],
+	],
+	Font => [
+	    map([$_->[0] => [qq{class=$_->[1]}]],
+		[error => 'field_err'],
+		[warning => 'warn'],
+		[checkbox => 'check'],
+		[form_submit => 'button'],
+		[list_action => 'list_action'],
+	    ),
+	    [[qw{
+		default
+		form_field_checkbox
+		form_field_description
+		form_submit
+		input_field
+		mailto
+		number_cell
+		page_heading
+		page_text
+		radio
+		search_field
+		table_cell
+		table_heading
+		list_action
+	    }] => []],
+	],
+	FormError => [
+	    [NULL => 'You must supply a value for vs_fe("label");.'],
+	],
+	HTML => [
+	    [want_secure => 0],
+	    [table_default_align => 'left'],
+	],
+	Task => [
+	    [CLUB_HOME => '?'],
+	    [DEFAULT_ERROR_REDIRECT_FORBIDDEN => undef],
+	    [FAVICON_ICO => 'favicon.ico'],
+	    [FORBIDDEN => undef],
+	    [LOCAL_FILE_PLAIN => ['i/*', 'f/*']],
+	    [MY_SITE => 'my-site'],
+	    [MY_CLUB_SITE => undef],
+	    [SHELL_UTIL => undef],
+	    [SITE_ROOT => '*'],
+	    [USER_HOME => '?'],
+	    [ROBOTS_TXT => 'robots.txt'],
+	    [TEST_BACKDOOR => '_test_backdoor'],
+	],
+	Text => [
+	    [support_email => 'support'],
+#TODO:	    [support_phone => '(800) 555-1212'],
+	    [home_page_uri => '/hm/index'],
+	    [view_execute_uri_prefix => 'SiteRoot->'],
+	    [favicon_uri => '/i/favicon.ico'],
+	    [form_error_title => 'Please correct the errors below:'],
+	    [none => ''],
+	    [Image_alt => [
+		dot => '',
+		sort_up => 'This column sorted in descending order',
+		sort_down => 'This column sorted in ascending order',
+	    ]],
+	    [ok_button => '   OK   '],
+	    [cancel_button => ' Cancel '],
+	    [['Email.email', 'login'] => 'Email'],
+	    [password => 'Password'],
+	    [confirm_password => 'Re-enter Password'],
+	    [display_name => 'Your Full Name'],
+	    [first_name => 'First Name'],
+	    [middle_name => 'Middle Name'],
+	    [street1 => 'Street Line 1'],
+	    [street2 => 'Street Line 2'],
+	    [city => 'City'],
+	    [state => 'State'],
+	    [zip => 'Zip'],
+	    [country => 'Country'],
+	    [empty_list_prose => 'This list is empty.'],
+	    [xlink => [
+		SITE_ROOT => 'Home',
+	    ]],
+	    [[qw(paged_detail paged_list)] => [
+		prev => 'back',
+		next => 'next',
+		list => 'back to list',
+	    ]],
+	    [prose => [
+		mail_from_name => 'String(vs_site_name()); Support',
+		xhtml_su_logout => <<'EOF',
+Acting as User: <br />
+String(['auth_user', 'display_name']);<br />
+Click here to exit.
+EOF
+		user_create_done => <<'EOF',
+We have sent a confirmation email to
+String(['Model.UserRegisterForm', 'Email.email']);.
+Please follow the instructions in this email message to complete
+your registration with vs_site_name();.
+EOF
+		password_query_mail_subject => 'vs_site_name(); Password Assistance',
+		password_query_mail => <<'EOF',
+Please follow the link to reset your password:
+
+Join([['Model.UserPasswordQueryForm', 'uri']]);
+
+For your security, this link may be used one time only to set your
+password.
+
+You may contact customer support by replying to this message.
+
+Thank you,
+vs_site_name(); Support
+EOF
+		user_create_mail_subject => 'vs_site_name(); Registration Verification',
+		user_create_mail => <<'EOF',
+Thank you for registering with vs_site_name();.
+In order to complete your registration, please click on the following link:
+
+String(['Model.UserRegisterForm', 'uri']);
+
+For your security, this link may be used one time only to set your
+password.
+
+You may contact customer support by replying to this message.
+
+Thank you,
+vs_site_name(); Support
+EOF
+	    ]],
+	],
+    };
+}
+
+sub _cfg_dav {
+    return {
+	Task => [
+	    [DAV => 'dv/*'],
+	],
+	Text => [
+	    [ForumList => [
+		'RealmOwner.name' => 'Forum',
+		'RealmOwner.display_name' => 'Title',
+		'Forum.want_reply_to' => 'Reply-To List?',
+		'admin_only_forum_email' => 'Admin Only Email?',
+		'system_user_forum_email' => 'System User Email?',
+		'public_forum_email' => 'Public Email?',
+		'Forum.forum_id' => 'Database Key',
+	    ]],
+	    [ForumUserList => [
+		'Email.email' => 'Email',
+		mail_recipient => 'Subscribed?',
+		file_writer => 'Write Files?',
+		administrator => 'Administrator?',
+		'RealmUser.user_id' => 'Database Key',
+	    ]],
+	    [EmailAliasList => [
+		'EmailAlias.incoming' => 'From Email',
+		'EmailAlias.outgoing' => 'To Email or Forum',
+		'primary_key' => 'Database Key',
+	    ]],
+	],
+    };
+}
+
+sub _cfg_login {
+    return {
+	Constant => [
+	    [my_site_login => {
+		task_id => 'MY_SITE',
+	    }],
+	    [xlink_login_no_context => {
+		task_id => 'LOGIN',
+		no_context => 1,
+	    }],
+	    [xlink_user_create_no_context => {
+		task_id => 'USER_CREATE',
+		no_context => 1,
+	    }],
+	],
+	FormError => [
+	    ['UserLoginForm.RealmOwner.password.PASSWORD_MISMATCH' => 
+		 q{The password you entered does not match the value stored in our database. Please remember that passwords are case-sensitive, i.e. "HELLO" is not the same as "hello".},],
+	],
+	Task => [
+	    [LOGIN => 'pub/login'],
+	    [LOGOUT => 'pub/logout'],
+	    [USER_CREATE => 'pub/register'],
+	    [USER_CREATE_DONE => undef],
+	    [GENERAL_USER_PASSWORD_QUERY => 'pub/forgot-password'],
+	    [GENERAL_USER_PASSWORD_QUERY_MAIL => undef],
+	    [GENERAL_USER_PASSWORD_QUERY_ACK => undef],
+	    [USER_PASSWORD_RESET => '?/new-password'],
+	    [USER_PASSWORD => '?/password'],
+	    [ADM_SUBSTITUTE_USER => 'adm/su'],
+	    [DEFAULT_ERROR_REDIRECT_MISSING_COOKIES => 'pub/missing-cookies'],
+	],
+	Text => [
+	    [UserLoginForm => [
+		ok_button => 'Login',
+		prose => [
+		    prologue => q{P(XLink('user_create_no_context'));},
+		    epilogue => q{P(XLink('GENERAL_USER_PASSWORD_QUERY'));},
+		],
+	    ]],
+	    [password => 'Password'],
+	    [[qw(UserCreateForm UserRegisterForm)]  => [
+		ok_button => 'Register',
+		'confirm_password.field_description' => q{Enter your password again.},
+		confirm_password => 'Re-enter Password',
+		prose => [
+		    prologue => q{P(XLink('GENERAL_USER_PASSWORD_QUERY'));},
+		    epilogue => q{P(XLink('login_no_context'));},
+		],
+	    ]],
+	    [UserPasswordForm => [
+		old_password => 'Current Password',
+		new_password => 'New Password',
+		confirm_new_password => 'Re-enter New Password',
+		ok_button => 'Update',
+	    ]],
+	    [UserPasswordQueryForm => [
+		ok_button => 'Reset Password',
+	    ]],
+	    [acknowledgement => [
+		GENERAL_USER_PASSWORD_QUERY => q{An email has been sent to String([qw(Model.UserPasswordQueryForm Email.email)]); with a link to reset your password.},
+		USER_PASSWORD_RESET => q{Your password has been reset.  Please choose a new one.},
+		USER_PASSWORD => q{Your password has been changed.},
+		password_nak => q{We're sorry, but the "vs_text('xlink.GENERAL_USER_PASSWORD_QUERY');" link you clicked is no longer valid.  You will need to reset your password again.},
+		USER_FORUM_TREE => q{Your subscriptions have been updated.},
+		user_create_password_reset => q{You are already registered.  Your password has been reset.  An email has been sent to String([qw(Model.UserPasswordQueryForm Email.email)]); with a link to choose a new password.},
+		GENERAL_CONTACT => 'Your inquiry has been sent.  Thank you!',
+	    ]],
+	    [title => [
+		 GENERAL_USER_PASSWORD_QUERY => 'Password Assistance',
+	     ]],
+	    [[qw(title xlink)] => [
+		GENERAL_CONTACT => 'Contact Us',
+		USER_PASSWORD  => 'Password',
+		[qw(LOGIN my_site_login)] => 'Login',
+		LOGOUT => 'Logout',
+		USER_CREATE => 'Register',
+		GENERAL_USER_PASSWORD_QUERY_ACK => 'Password Assistance Sent',
+		ADM_SUBSTITUTE_USER => 'Act as User',
+		SITE_ROOT => 'Home',
+	    ]],
+	    [xlink => [
+		GENERAL_USER_PASSWORD_QUERY => 'Forgot password?',
+		login_no_context => 'Already registered?  Click here to login.',
+		user_create_no_context => 'Not registered? Click here to register.',
+	    ]],
+	    ['page3.title' => [
+		LOGIN => 'Please Login',
+		USER_CREATE => 'Please Register',
+		GENERAL_CONTACT => 'Please Contact Us',
+		USER_PASSWORD  => 'Your Password',
+		USER_CREATE_DONE => 'Registration Email Sent',
+		SITE_ROOT => '',
+	    ]],
+	],
+    };
+}
+
+sub _cfg_mail_receive_dispatch {
+    my($proto) = @_;
+    return {
+	Task => [
+	    [FORUM_EASY_FORM => '?/Forms/*'],
+	    [FORUM_FILE => '?/file/*'],
+	    [FORUM_MAIL_RECEIVE => '?/' . $proto->MAIL_RECEIVE_PREFIX],
+	    [FORUM_MAIL_REFLECTOR => undef],
+	    [FORUM_PUBLIC_FILE => '?/public/*'],
+	    [MAIL_RECEIVE_DISPATCH => '_mail_receive/*'],
+	    [MAIL_RECEIVE_FORWARD => undef],
+	    [MAIL_RECEIVE_IGNORE => undef],
+	    [MAIL_RECEIVE_NOT_FOUND => undef],
+	    [MAIL_RECEIVE_NO_RESOURCES => undef],
+	    [USER_MAIL_BOUNCE => '?/' . $proto->MAIL_RECEIVE_PREFIX . Bivio::Biz::Model->get_instance('RealmMailBounce')->TASK_URI],
+	],
+	Text => [
+	    ['MailReceiveDispatchForm.uri_prefix' => $proto->MAIL_RECEIVE_PREFIX],
+	],
+    };
+}
+sub _cfg_forum_tuple_list {
+    return {
 	Task => [
 	    [FORUM_TUPLE_DEF_EDIT => '?/edit-db-schemas'],
 	    [FORUM_TUPLE_DEF_LIST => '?/db-schemas'],
@@ -36,6 +357,7 @@ sub _cfg_tuple {
 		FORUM_TUPLE_LIST =>
 		    q{String([qw(Model.TupleUseList TupleUse.label)]); Records},
 		FORUM_TUPLE_HISTORY =>
+
 		    q{String([qw(Model.TupleUseList TupleUse.label)]); Record #String([qw(Model.TupleList Tuple.tuple_num)]); History},
 		FORUM_TUPLE_MAIL_THREAD =>
 		    q{String([qw(Model.TupleUseList TupleUse.label)]); Record #String([qw(Model.Tuple tuple_num)]);},
@@ -158,12 +480,70 @@ sub _cfg_tuple {
     };
 }
 
+sub _cfg_forum_wiki_view {
+    my($proto) = @_;
+    return {
+	Task => [
+	    [FORUM_WIKI_EDIT => '?/wiki-edit/*'],
+	    [FORUM_WIKI_VIEW => ['?/wiki/*']],
+	    [FORUM_WIKI_NOT_FOUND => undef],
+	    [HELP => 'help/*'],
+	    [HELP_NOT_FOUND => undef],
+	],
+	Constant => [
+	    [help_wiki_realm_id => sub {
+		 my($req) = shift->get_request;
+		 return Bivio::Die->eval(
+		     sub {
+			 return Bivio::Biz::Model->new($req, 'RealmOwner')
+			     ->unauth_load_or_die({
+				 name => $proto->HELP_WIKI_REALM_NAME
+			     })->get('realm_id');
+		     },
+		 ) || 1;
+	    }],
+	],
+	Text => [
+	    [HelpWiki => [
+		header => 'Help',
+		footer => '',
+	    ]],
+	    [acknowledgement => [
+		FORUM_WIKI_EDIT => 'Update accepted.  Please proofread for formatting errors.',
+		FORUM_WIKI_NOT_FOUND => 'Page not found.  Please create it.',
+	    ]],
+	    [title => [
+		FORUM_WIKI_NOT_FOUND => 'Wiki Page Not Found',
+		HELP_NOT_FOUND => 'Help Page Not Found',
+		HELP => 'Help',
+		FORUM_WIKI_EDIT => 'Edit Wiki Page',
+		FORUM_WIKI_VIEW => 'Wiki',
+	    ]],
+	    ['task_menu.title' => [
+		FORUM_WIKI_EDIT => 'Add New Page',
+		FORUM_WIKI_EDIT_PAGE => 'edit this page',
+	    ]],
+	    [acknowledgement => [
+		FORUM_WIKI_EDIT => 'Update accepted.  Please proofread for formatting errors.',
+		FORUM_WIKI_NOT_FOUND => 'Wiki page not found.  Please create it.',
+	    ]],
+	],
+    };
+}
+
 sub _merge {
-    my($my, $child) = @_;
-    foreach my $k (keys(%$my)) {
-	push(@{$child->{$k} ||= []}, @{$my->{$k}});
+    my($child) = pop(@_);
+    foreach my $cfg (@_) {
+	foreach my $k (keys(%$cfg)) {
+	    if (ref($child->{$k}) eq 'ARRAY') {
+		unshift(@{$child->{$k}}, @{$cfg->{$k}});
+	    }
+	    else {
+		$child->{$k} = $cfg->{$k};
+	    }
+	}
     }
-    return;
+    return $child;
 }
 
 1;
