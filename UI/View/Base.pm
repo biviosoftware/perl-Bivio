40d7d9c5c827b2f3fb2bb2410b42e4ee3b45a314
Rob Nagler <github@q33.us>
2007-02-28 20:08:09 +0000

    internal_xhtml_adorned, internal_text_as_prose,

--- UI/View/Base.pm
+++ UI/View/Base.pm
@@ -36,7 +36,7 @@ sub mail {
     view_put(
 	mail_from => Mailbox(
 	    vs_text('support_email'),
-	    vs_text_as_prose('mail_from_name'),
+	    $self->internal_text_as_prose('support_name'),
 	),
 	mail_recipients => '',
     );
@@ -56,32 +56,27 @@ sub internal_body {
 
 sub internal_body_from_name_as_prose {
     my($self, $name) = @_;
-    return $self->internal_body(vs_text_as_prose(
-	$name || $self->get('view_name')));
+    return $self->internal_body(
+	$self->internal_text_as_prose($name || $self->get('view_name')));
 }
 
 sub internal_put_base_attr {
-    my($self, $attr, $value) = @_;
-    view_put($self->internal_base_type . "_$attr" => $value);
+    my($self) = shift;
+    $self->map_by_two(sub {
+        view_put($self->internal_base_type . '_' . shift(@_) => shift(@_));
+	return;
+    }, \@_);
     return;
 }
 
-sub pre_compile {
-    my($self) = @_;
-    my($n) = $self->get('view_name');
-    return
-	if __PACKAGE__->can($n);
-    view_parent($n =~ /_(mail|csv)$/ ? $1 : 'xhtml');
-    return;
+sub internal_text_as_prose {
+    my($self, $name) = @_;
+    return vs_text_as_prose($self->simple_package_name . '.' . $name);
 }
 
-sub xhtml {
+sub internal_xhtml_adorned {
     my($self) = @_;
-    view_class_map('XHTMLWidget');
-    view_shortcuts($self->VIEW_SHORTCUTS);
-#    view_declare('page3_title');
     view_put(
-	xhtml_body => '',
 	xhtml_realm => '',
 	xhtml_tools => '',
 	xhtml_topic => '',
@@ -91,60 +86,79 @@ sub xhtml {
 	    'USER_PASSWORD',
 	]),
     );
+    return Page3({
+	head1 => $self->internal_text_as_prose('page3.head1'),
+	head2 => Join([
+	    P_realm(view_widget_value('xhtml_realm')),
+	    P_menu(view_widget_value('xhtml_menu')),
+	    P_title($self->internal_text_as_prose('xhtml_title')),
+	]),
+	head3 => Director(['user_state', '->get_name'], {
+	    JUST_VISITOR => XLink('USER_CREATE'),
+	    LOGGED_OUT => XLink('my_site_login'),
+	    LOGGED_IN => XLink('LOGOUT'),
+	}),
+	content => Join([
+	    DIV_top(Join([
+		DIV_tools(Join([
+		    view_widget_value('xhtml_tools'),
+		    view_widget_value('xhtml_pager'),
+		], {
+		    join_separator => EmptyTag(DIV => 'sep'),
+		})),
+		map(
+		    DIV(view_widget_value("xhtml_$_"), $_),
+		    qw(topic byline),
+		),
+	    ])),
+	    DIV_body(view_widget_value('xhtml_body')),
+	    DIV_bottom(
+		DIV_tools(Join([
+		    view_widget_value('xhtml_tools'),
+		    view_widget_value('xhtml_pager'),
+		], {
+		    join_separator => EmptyTag(DIV => 'sep'),
+		})),
+	    ),
+	]),
+	foot2 => TaskMenu([
+	    'SITE_ROOT',
+	]),
+    });
+}
+
+sub pre_compile {
+    my($self) = @_;
+    my($n) = $self->get('view_name');
+    return
+	if __PACKAGE__->can($n);
+    view_parent('Base->' . ($n =~ /_(mail|csv|rss)$/ ? $1 : 'xhtml'));
+#    view_parent(($n =~ /_(mail|csv|rss)$/ ? $1 : 'xhtml'));
+    return;
+}
+
+sub rss {
+    my($self) = @_;
+    view_class_map('XHTMLWidget');
+    view_shortcuts($self->VIEW_SHORTCUTS);
+    return;
+}
+
+sub xhtml {
+    my($self) = @_;
+    view_class_map('XHTMLWidget');
+    view_shortcuts($self->VIEW_SHORTCUTS);
+    view_put(
+	xhtml_body => '',
+    );
     view_main(Director([
 	sub {shift->get_request->isa('Bivio::Agent::Embed::Request') ? 1 : 0},
     ], {
 	1 => SimplePage(view_widget_value('xhtml_body')),
-	0 => Page3({
-	    head1 => If(['->is_substitute_user'],
-		Link(
-		    RoundedBox(vs_text_as_prose('xhtml_su_logout')),
-		    'LOGOUT',
-		    'su',
-		),
-		Link('', '/', 'logo'),
-	    ),
-	    head2 => Join([
-		P_realm(view_widget_value('xhtml_realm')),
-		P_menu(view_widget_value('xhtml_menu')),
-		P_title(Prose(
-		    vs_text([sub {"page3.title.$_[1]"}, ['task_id', '->get_name']]),
-		)),
-	    ]),
-	    head3 => Director(['user_state', '->get_name'], {
-		JUST_VISITOR => XLink('USER_CREATE'),
-		LOGGED_OUT => XLink('my_site_login'),
-		LOGGED_IN => XLink('LOGOUT'),
-	    }),
-	    content => Join([
-		DIV_top(Join([
-		    DIV_tools(Join([
-			view_widget_value('xhtml_tools'),
-			view_widget_value('xhtml_pager'),
-		    ], {
-			join_separator => EmptyTag(DIV => 'sep'),
-		    })),
-		    map(
-			DIV(view_widget_value("xhtml_$_"), $_),
-			qw(topic byline),
-		    ),
-		])),
-		DIV_body(view_widget_value('xhtml_body')),
-		DIV_bottom(
-		    DIV_tools(Join([
-			view_widget_value('xhtml_tools'),
-			view_widget_value('xhtml_pager'),
-		    ], {
-			join_separator => EmptyTag(DIV => 'sep'),
-		    })),
-		),
-	    ]),
-	    foot2 => TaskMenu([
-		'SITE_ROOT',
-	    ]),
-	}),
+	0 => $self->internal_xhtml_adorned,
     }));
     return;
 }
+
 
 1;
