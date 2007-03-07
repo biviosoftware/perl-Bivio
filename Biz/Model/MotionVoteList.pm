be1a438b9bddc00b4b75b204b575f0b2ebfdd232
Alex Viggio <aviggio@bivio.biz>
2007-03-07 08:47:20 +0000

    Define parent_id to restrict results to one motion

--- Biz/Model/MotionVoteList.pm
+++ Biz/Model/MotionVoteList.pm
@@ -5,18 +5,20 @@ use strict;
 use base 'Bivio::Biz::ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-#my($_IDI) = __PACKAGE__->instance_data_index;
 
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
 	can_iterate => 0,
-	primary_key => [[qw(MotionVote.user_id Email.realm_id)]],
+	parent_id => 'MotionVote.motion_id',
 	order_by => [
  	    'MotionVote.creation_date_time',
 	    'MotionVote.vote',
 	    'Email.email',
+	],
+	other => [
+	    [qw(MotionVote.user_id Email.realm_id)],
 	],
 	auth_id => ['MotionVote.realm_id'],
     });
