c568c1d1b13b1fed83286ffd293263e538374798
Paul Moeller <moeller@bivio.biz>
2007-03-13 22:17:40 +0000

    * Bivio::IO::ClassLoader simple_require had too much dynamic binding   going on * Bivio::Biz::Model::BlogCreateForm can't die to see if exists,   because transaction fails. This is problematic, but there's not   much that can be done. * Bivio::Biz::Random use /dev/urandom when available, because   /dev/random blocks * Bivio::Test::Unit added builtin_chomp_and_return * Bivio::Type::BlogFileName share from_literal Added   from_literal_stripper * Bivio::Type::EnumDelegator call static method on delegate if $proto   is a string; otherwise use instance strip POD * Bivio::UI::XHTML::Widget::WikiStyle wiki can be private/public * Bivio::UNIVERSAL grep_methods added inheritance_ancestors replaces   inheritance_ancestor_list

--- bOP.pm
+++ bOP.pm
@@ -30,7 +30,7 @@ Model-View-Controller (MVC) architecture.  At the lowest level, bOP provides a
 cohesive infrastructure for any Perl application.
 
 We'll be writing more here later.  Please visit
-http://www.bivio.biz for more info.
+http://www.bivio.biz for more info. 
 
 =cut
 
@@ -41,6 +41,9 @@ http://www.bivio.biz for more info.
 =head1 CHANGES
 
   $Log$
+  Revision 4.83  2007/03/13 17:54:52  moeller
+  more info later
+
   Revision 4.82  2007/03/10 00:45:41  nagler
   * Bug fixes from previous release
   * Bivio::UI::HTML::Widget::Table.column_height added
