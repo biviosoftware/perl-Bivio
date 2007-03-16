089f0faa1398fe31c14be8a4d1089f7b394f4104
Rob Nagler <github@q33.us>
2007-03-16 17:41:43 +0000

    *** empty log message ***

---
+++ t/UNIVERSAL/DataSectionMissing.pm
@@ -0,0 +1,9 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::t::UNIVERSAL::DataSectionMissing;
+use strict;
+use Bivio::Base 'Bivio::UNIVERSAL';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
---
+++ t/UNIVERSAL/DataSectionOK.pm
@@ -0,0 +1,11 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::t::UNIVERSAL::DataSectionOK;
+use strict;
+use Bivio::Base 'Bivio::UNIVERSAL';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
+__DATA__
+data ok
