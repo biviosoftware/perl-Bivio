c9db1cb440852555d9cba604cbeb43165cd2b900
Rob Nagler <github@q33.us>
2007-03-21 12:29:16 +0000

    *** empty log message ***

---
+++ UI/View/ThreePartPage.pm
@@ -0,0 +1,96 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::View::ThreePartPage;
+use strict;
+use Bivio::Base 'Bivio::UI::View::Base';
+use Bivio::UI::ViewLanguageAUTOLOAD;
+#
+# You must create your own View.Base if you want to use this class.  See
+# PetShop's View.Base.
+#
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_xhtml_adorned {
+    my($self) = @_;
+    view_put(
+	xhtml_title => If(
+	    [qw(auth_realm ->has_owner)],
+	    DIV_realm(String([qw(auth_realm owner display_name)])),
+	    vs_text_as_prose('xhtml_title'),
+	),
+	vs_pager => '',
+	xhtml_head_tags => '',
+	xhtml_rss_task => '',
+	xhtml_tools => '',
+	xhtml_topic => '',
+	xhtml_byline => '',
+	xhtml_main_left => '',
+	xhtml_main_right => '',
+	xhtml_selector => '',
+	xhtml_footer_left => XLink('back_to_top'),
+	xhtml_footer_middle => '',
+	xhtml_footer_right => vs_text_as_prose('xhtml_copyright'),
+	xhtml_header_middle => '',
+    );
+    view_put(
+	xhtml_header_left => vs_text_as_prose('xhtml_logo'),
+	xhtml_header_right => vs_text_as_prose('xhtml_user_state'),
+	xhtml_style => StyleSheet(URI({task_id => 'SITE_CSS'})),
+	xhtml_main_middle => Join([
+	    Acknowledgement(),
+	    DIV_main_title(view_widget_value('xhtml_title')),
+	    DIV_top(Join([
+		DIV_selector(
+		    view_widget_value('xhtml_selector')),
+		DIV_tools(Join([
+		    view_widget_value('xhtml_tools'),
+		    view_widget_value('vs_pager'),
+		], {
+		    join_separator => EmptyTag(DIV => 'sep'),
+		})),
+		DIV_topic(view_widget_value('xhtml_topic')),
+		DIV_byline(view_widget_value('xhtml_byline')),
+	    ])),
+	    DIV_main_body(view_widget_value('xhtml_body')),
+	    DIV_bottom(
+		DIV_tools(Join([
+		    view_widget_value('xhtml_tools'),
+		    view_widget_value('vs_pager'),
+		], {
+		    join_separator => EmptyTag(DIV => 'sep'),
+		})),
+	    ),
+	]),
+    );
+    return Page({
+	style => view_widget_value('xhtml_style'),
+	head => Join([
+	    vs_text_as_prose('xhtml_head_title'),
+	    view_widget_value('xhtml_head_tags'),
+	    EmptyTag(link => {
+		control => view_widget_value('xhtml_rss_task'),
+		html_attrs => [qw(rel type title href)],
+		rel => 'alternate',
+		type => 'application/rss+xml',
+		title => Prose(
+		    vs_text(
+			'rsslink', 'title', view_widget_value('xhtml_rss_task')),
+		),
+		href => URI({
+		    task_id => view_widget_value('xhtml_rss_task'),
+		    query => undef,
+		}),
+	    }),
+	]),
+	body => Join([
+	    '<a name="top"></a>',
+	    vs_grid3('header'),
+	    vs_grid3('main'),
+	    vs_grid3('footer'),
+	]),
+	xhtml => 1,
+    });
+}
+
+1;
---
+++ UI/Widget/SimplePage.pm
@@ -0,0 +1,21 @@
+# Copyright (c) 2005-2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::HTML::Widget::SimplePage;
+use strict;
+use Bivio::Base 'Bivio::UI::Widget::Simple';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub execute {
+    my($self, $req) = @_;
+    return $self->execute_with_content_type(
+	$req, $self->render_attr('content_type'));
+}
+
+sub initialize {
+    my($self) = @_;
+    $self->initialize_attr('content_type', 'text/html');
+    return shift->SUPER::initialize(@_);
+}
+
+1;
