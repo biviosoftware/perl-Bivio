d693fd2cad4fe737472c521d777ef31234a09893
Rob Nagler <github@q33.us>
2007-03-22 16:41:53 +0000

    minor rearrangements

--- UI/View/ThreePartPage.pm
+++ UI/View/ThreePartPage.pm
@@ -14,29 +14,34 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub internal_xhtml_adorned {
     my($self) = @_;
     view_put(
-	xhtml_title => If(
-	    [qw(auth_realm ->has_owner)],
-	    DIV_realm(String([qw(auth_realm owner display_name)])),
+	xhtml_title => Join([
+	    DIV_realm(String([qw(auth_realm owner display_name)]), {
+		control => vs_realm_type('forum'),
+	    }),
 	    vs_text_as_prose('xhtml_title'),
-	),
+	]),
 	vs_pager => '',
 	xhtml_head_tags => '',
 	xhtml_rss_task => '',
 	xhtml_tools => '',
 	xhtml_topic => '',
 	xhtml_byline => '',
-	xhtml_main_left => '',
-	xhtml_main_right => '',
 	xhtml_selector => '',
+	xhtml_header_left => vs_text_as_prose('xhtml_logo'),
+	xhtml_header_middle => '',
+	xhtml_header_right => vs_text_as_prose('xhtml_user_state'),
+	xhtml_main_left => '',
+	xhtml_main_right => HelpWiki(),
 	xhtml_footer_left => XLink('back_to_top'),
 	xhtml_footer_middle => '',
 	xhtml_footer_right => vs_text_as_prose('xhtml_copyright'),
-	xhtml_header_middle => '',
     );
     view_put(
-	xhtml_header_left => vs_text_as_prose('xhtml_logo'),
-	xhtml_header_right => vs_text_as_prose('xhtml_user_state'),
-	xhtml_style => StyleSheet(URI({task_id => 'SITE_CSS'})),
+	xhtml_style => Join([
+	    StyleSheet('SITE_CSS'),
+	    StyleSheet('FORUM_CSS'),
+	    WikiStyle(),
+	]),
 	xhtml_main_middle => Join([
 	    Acknowledgement(),
 	    DIV_main_title(view_widget_value('xhtml_title')),
@@ -84,7 +89,8 @@ sub internal_xhtml_adorned {
 	    }),
 	]),
 	body => Join([
-	    '<a name="top"></a>',
+	    EmptyTag(a => {html_attrs => ['name'], name => 'top'}),
+            vs_first_focus(),
 	    vs_grid3('header'),
 	    vs_grid3('main'),
 	    vs_grid3('footer'),
