2bde76a09199326857afa8d398329bfcd927422d
Alex Viggio <aviggio@bivio.biz>
2007-03-30 06:40:31 +0000

    *** empty log message ***

---
+++ UI/CSV/Widget/SimplePage.pm
@@ -0,0 +1,14 @@
+# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::CSV::Widget::SimplePage;
+use strict;
+use base 'Bivio::UI::Widget::Simple';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub execute {
+    my($self, $req) = @_;
+    return $self->execute_with_content_type($req, 'text/csv');
+}
+
+1;
