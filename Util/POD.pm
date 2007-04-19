9f4d7444f6f35912d311ca22868ddfa449ecfc4f
Rob Nagler <github@q33.us>
2007-04-19 02:54:57 +0000

    to_comments added

--- Util/POD.pm
+++ Util/POD.pm
@@ -1,54 +1,18 @@
-# Copyright (c) 2001 bivio Software, Inc.  All Rights reserved.
+# Copyright (c) 2001-2007 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Util::POD;
 use strict;
-$Bivio::Util::POD::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Util::POD::VERSION;
+use Bivio::Base 'Bivio::ShellUtil';
+use Bivio::HTML;
+use Bivio::IO::File;
+use Bivio::IO::Trace;
+use File::Basename ();
+use File::Find ();
+use Pod::Html ();
 
-=head1 NAME
-
-Bivio::Util::POD - manipulate pod (perl's plain old documentation)
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Util::POD;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::ShellUtil>
-
-=cut
-
-use Bivio::ShellUtil;
-@Bivio::Util::POD::ISA = ('Bivio::ShellUtil');
-
-=head1 DESCRIPTION
-
-C<Bivio::Util::POD> manipulates pod (perl's plain old documentation).
-
-L<to_html|"to_html"> takes the drudgery of running I<Pod::Html>.
-It makes some assumptions, creates a relative tree, and creates
-an index.
-
-=cut
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="OPTIONS"></a>
-
-=head2 OPTIONS : hash_ref
-
-Adds the I<-checkout> and I<-package_dir> options.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($_TRACE);
+my($_HTML_ROOT_TAG) = '/SOME/UNIQUE/PATH/TO/FIND';
 
 sub OPTIONS {
     my($proto) = @_;
@@ -57,16 +21,6 @@ sub OPTIONS {
     $res->{checkout} = ['Boolean', 0];
     return $res;
 }
-
-=for html <a name="OPTIONS_USAGE"></a>
-
-=head2 OPTIONS_USAGE : string
-
-special options:
-    -checkout - runs cvs checkout on package_dir first
-    -package_dir dir - limits the operation to this package's directory
-
-=cut
 
 sub OPTIONS_USAGE {
     my($proto) = @_;
@@ -77,15 +31,6 @@ special options:
 EOF
 }
 
-=for html <a name="USAGE"></a>
-
-=head2 USAGE : string
-
-commands:
-    to_html input_dir output_dir -- converts input_dir pods to output_dir html
-
-=cut
-
 sub USAGE {
     return <<'EOF';
 usage: b-pod [options] command [args...]
@@ -94,34 +39,145 @@ commands:
 EOF
 }
 
-#=IMPORTS
-use Bivio::HTML;
-use Bivio::IO::File;
-use Bivio::IO::Trace;
-use File::Basename ();
-use File::Find ();
-use Pod::Html ();
-
-#=VARIABLES
-use vars ('$_TRACE');
-Bivio::IO::Trace->register;
-my($_HTML_ROOT_TAG) = '/SOME/UNIQUE/PATH/TO/FIND';
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="to_html"></a>
-
-=head2 to_html(string input_dir, string output_dir) : files
-
-Parses I<input_dir> for perl modules (file names that end with .pm)
-and programs (files which contain #!.*perl).  Once parsed, converts
-all files to html and writes result to I<output_dir>.
-
-Makes all links relative. Generates an index.
-
-=cut
+sub to_comments {
+    my($self, $file_pm) = @_;
+    Bivio::IO::File->do_read_write($file_pm, sub {
+        my($which_hash) = 'init';
+	my($toss_for_emacs) = 0;
+	my($in_description) = 0;
+        my($parts) = {
+	    constant => [],
+	    method => [],
+	    private => [],
+	    nl => [''],
+	    init => [q{our($VERSION) = sprintf('%d.%02d', q$}
+	        . q{Revision: 0.0$ =~ /\d+/g);}],
+	    end => ['1;', ''],
+	};
+	my($header_done) = 0;
+	my($copy_to_end);
+	my($lineno) = 0;
+	my($err) = sub {
+	    Bivio::Die->die("$file_pm:$lineno ", @_, "\n", $parts);
+	};
+	my($push) = sub {
+	    my($part, $v) = @_;
+	    $parts->{$part} ||= [];
+	    push(@{$parts->{$part}}, $v)
+		if defined($v);
+	    return $parts->{$part};
+	};
+	my($clear) = sub {
+	    my($part) = @_;
+	    return delete($parts->{$part});
+	};
+	my($clear_comment) = sub {
+	    my($c) = $clear->('comment') || [];
+	    pop(@$c)
+		while @$c && $c->[$#$c] =~ /^#\s*$/;
+	    shift(@$c)
+		while @$c && $c->[0] =~ /^#\s*(?:$|_\w+\()/;
+	    return $c;
+	};
+	my($line);
+	foreach $line (split(/\n/, ${shift(@_)})) {
+	    $lineno++;
+	    if ($line =~ /^=/ && !$parts->{pod}) {
+		$which_hash = 'comment'
+		    if $line =~ /^=head1 (?:METHOD|FACTORIES)/;
+		$push->('pod', undef);
+		$in_description = 1
+		    if $line =~ /^=head1\s+DESCRIPTION/i;
+		$header_done = 1;
+		$clear->('comment');
+	    }
+	    if (my $pod = $parts->{pod}) {
+		$line =~ s/^=item //;
+		$push->(pod => $line)
+		    unless $line =~ /^=/;
+		if ($line =~ /^=cut/) {
+		    $parts->{comment} = [
+			grep(s{^}{#@{[$_ =~ /\S/ ? ' ' : '']}}s,
+			    @{$clear->('pod') || []}),
+		    ];
+		    if ($in_description) {
+			$parts->{description} = $clear_comment->();
+			$in_description = 0;
+		    }
+		}
+		next;
+	    }
+	    unless ($header_done) {
+		$push->(header => $line)
+		    if $line =~ /^(?:# Copyright|package |use strict|# \$Id)/;
+		next;
+	    }
+	    next
+		if $line =~ /^#=/;
+	    if ($line =~ /^#/ && !$parts->{sub}) {
+		$push->($which_hash => $line);
+		next;
+	    }
+	    if ($line =~ s/^use vars //) {
+		$push->(init => [map("our($_);", Bivio::Die->eval_or_die($line))]);
+		next;
+	    }
+	    next
+		if $line =~ /^(?:Bivio::IO::Trace|1;)/;
+	    if ($line =~ /^\$_\s*=\s*\<\<\'\}/) {
+		$toss_for_emacs = 1;
+		next;
+	    }
+	    if ($line =~ /^use /) {
+		$push->(import => $line)
+		     if $parts->{import};
+		next;
+	    }
+	    if ($line =~ /^\@.*::ISA = (?:qw.|\(')([\w:]+)/) {
+		$push->(import => qq{use Bivio::Base '$1';});
+		next;
+	    }
+	    $push->('sub')
+		if $line =~ /^sub \w+ \{/;
+	    if (my $sub = $parts->{sub}) {
+		if (@$sub == 1) {
+		    if ($line =~ /^\s+my/) {
+			$push->(sub => $line);
+			$line = undef;
+		    }
+		    push(@$sub, grep(s/^/    /, @{$clear_comment->()}));
+		    next unless defined($line);
+		}
+		$push->(sub => $line);
+		if ($line =~ /^\}\s*$/) {
+		    $push->(sub => '');
+		    $clear->('sub');
+		    $push->(
+			$sub->[0] =~ /sub [A-Z]/ ? 'constant'
+			    : $sub->[0] =~ /sub _/ ? 'private' : 'method',
+			$sub,
+		    ) unless $toss_for_emacs;
+		    $toss_for_emacs = 0;
+		}
+		next;
+	    }
+	    if ($line =~ /^\S/ && $parts->{init}) {
+		$push->(init => $line);
+		next;
+	    }
+	}
+	# sort subs and imports
+	# print sections
+	$parts->{import} = [sort(@{$parts->{import}})];
+	return join(
+	    "\n",
+	    map(ref($_) ? @$_ : $_,
+		map(@{$parts->{$_} || []},
+		qw(header base import nl description nl init nl constant method private end))),
+        );
+    });
+    return;
+}
 
 sub to_html {
     my($self, $input_dir, $output_dir) = @_;
@@ -142,12 +198,6 @@ sub to_html {
     return "Translated ".int(@{$state->{modules}})." files\n";
 }
 
-#=PRIVATE METHODS
-
-# _checkout(self, string input_dir)
-#
-# Runs cvs checkout on files
-#
 sub _checkout {
     my($self, $input_dir) = @_;
     my($pkg) = $self->unsafe_get('package_dir');
@@ -158,10 +208,6 @@ sub _checkout {
     return;
 }
 
-# _find_files(hash_ref state)
-#
-# Sets hash_ref $files in $state.
-#
 sub _find_files {
     my($state) = @_;
     my($pkg) = $state->{package_dir};
@@ -205,10 +251,6 @@ sub _find_files {
     return %{$state->{pod}} ? 1 : 0;
 }
 
-# _fixup_html(string module, string html)
-#
-# Rewrites html with $_HTML_ROOT_TAG replaced with relative paths.
-#
 sub _fixup_html {
     my($module, $html) = @_;
     my($rel_root) = $module;
@@ -221,10 +263,6 @@ sub _fixup_html {
     return
 }
 
-# _html_index(hash_ref state)
-#
-# Creates an index of all the files.
-#
 sub _html_index {
     my($state) = @_;
     my($pkg) = $state->{package_dir};
@@ -252,11 +290,6 @@ sub _html_index {
     return;
 }
 
-# _pod_info(string file) : string
-#
-# Parses the info from file.  If the file doesn't have pod, returns
-# undef.  Otherwise, returns
-#
 sub _pod_info {
     my($file) = @_;
     my($desc) = '';
@@ -288,10 +321,6 @@ sub _pod_info {
     return $res;
 }
 
-# _to_html(hash_ref state)
-#
-# Generates HTML for $state->{modules}.
-#
 sub _to_html {
     my($state) = @_;
     foreach my $module (@{$state->{modules}}) {
@@ -311,15 +340,5 @@ sub _to_html {
     unlink('pod2htmi.x~~');
     return;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All Rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
