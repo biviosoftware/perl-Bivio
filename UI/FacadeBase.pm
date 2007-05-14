0bbf3c357960207515ea5feb735f7e1596fcd1da
Rob Nagler <github@q33.us>
2007-05-14 20:03:56 +0000

    FormError.image_file

--- UI/FacadeBase.pm
+++ UI/FacadeBase.pm
@@ -126,6 +126,8 @@ sub _cfg_base {
 	    [NULL => 'You must supply a value for vs_fe("label");.'],
 	    [EXISTS => 'vs_fe("label"); already exists in our database.'],
 	    [NOT_FOUND => 'vs_fe("label"); was not found in our database.'],
+	    ['image_file.TOO_MANY' => 'vs_fe("label"); contains multiple images, please upload a file which contains only one image.'],
+	    ['image_file.SYNTAX_ERROR' => 'vs_fe("label"); unknown or invalid image format.  Please verify file, and change to an acceptable format (e.g. png, gif, jpg), and retry upload.'],
 	],
 	HTML => [
 	    [want_secure => 0],
