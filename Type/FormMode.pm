fc471c06332b63cca240dbc0ca3e70cf51fc31b8
Rob Nagler <github@q33.us>
2007-05-17 17:11:30 +0000

    *** empty log message ***

--- Type/FormMode.pm
+++ Type/FormMode.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2007 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::FormMode;
 use strict;
@@ -7,5 +7,16 @@ use base 'Bivio::Type::Enum';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 __PACKAGE__->compile_with_numbers([qw(EDIT CREATE)]);
+
+sub setup_by_list_this {
+    my($proto, $list, $detail_name) = @_;
+    my($q) = $list->parse_query_from_request;
+    my($self) = $proto->from_name(
+	$q->get('this') && $list->unsafe_load_this($q)
+	    ? ('EDIT', $list->get_model($detail_name))[0] : 'CREATE',
+    );
+    $self->execute($list->get_request);
+    return $self;
+}
 
 1;
