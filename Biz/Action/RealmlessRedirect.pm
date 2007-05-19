f505c1bcf6e1ccc06be6e67267f1c6b909a2a8a9
Rob Nagler <github@q33.us>
2007-05-19 15:46:04 +0000

    Factored out internal_choose_realm which picks most recently joined and longest realm name.

--- Biz/Action/RealmlessRedirect.pm
+++ Biz/Action/RealmlessRedirect.pm
@@ -5,6 +5,7 @@ use strict;
 use Bivio::Base 'Bivio::Biz::Action';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DT) = Bivio::Type->get_instance('DateTime');
 
 sub execute {
     my($proto, $req) = @_;
@@ -13,18 +14,41 @@ sub execute {
     return $us->equals_by_name('JUST_VISITOR')
 	? 'visitor_task'
 	: $req->get('auth_user')
-	? _set_realm($req, Bivio::Agent::Task->get_by_id($t->get('home_task')))
+	? _set_realm($proto, $req, $t->get('home_task'))
 	|| 'unauth_task'
 	: Bivio::Agent::TaskId->LOGIN;
 }
 
+sub internal_choose_realm {
+    my($proto, $user_realms, $task) = @_;
+    my($res);
+    my($rt) = $task->get('realm_type');
+    $user_realms->do_rows(sub {
+        my($row) = shift->get_shallow_copy;
+	$res = $row
+	    if $rt->equals($row->{'RealmOwner.realm_type'})
+	    && (!$res
+	    || $_DT->compare(
+		$res->{'RealmUser.creation_date_time'},
+		$row->{'RealmUser.creation_date_time'},
+	    ) < 0
+	    || ($res->{'RealmOwner.name'} cmp $row->{'RealmOwner.name'}) < 0);
+        return 1;
+    });
+    return $res && $res->{'RealmUser.realm_id'};
+}
+
 sub _set_realm {
-    my($req, $t) = @_;
-    return unless my $l = Bivio::Biz::Model->new($req, 'UserRealmList')
-	->unauth_load_all({auth_id => $req->get('auth_user_id')})
-	->find_row_by_type($t->get('realm_type'));
-    $req->set_realm($l->get('RealmUser.realm_id'));
-    return $t->get('id');
+    my($proto, $req, $task) = @_;
+    $task = Bivio::Agent::Task->get_by_id($task);
+    $req->set_realm(
+	$proto->internal_choose_realm(
+	    Bivio::Biz::Model->new($req, 'UserRealmList')
+	        ->unauth_load_all({auth_id => $req->get('auth_user_id')}),
+	    $task,
+	) || return
+    );
+    return $task->get('id');
 }
 
 1;
