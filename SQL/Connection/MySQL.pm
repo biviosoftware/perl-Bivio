613f3876539c186c4dc5c43a9fde3bfec6e2b1a6
Paul Moeller <moeller@bivio.biz>
2007-05-21 15:55:08 +0000

    added internal_get_retry_sleep() to reconnect is server goes away

--- SQL/Connection/MySQL.pm
+++ SQL/Connection/MySQL.pm
@@ -2,68 +2,19 @@
 # $Id$
 package Bivio::SQL::Connection::MySQL;
 use strict;
-$Bivio::SQL::Connection::MySQL::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::SQL::Connection::MySQL::VERSION;
+use Bivio::Base 'Bivio::SQL::Connection';
 
-=head1 NAME
-
-Bivio::SQL::Connection::MySQL - connection to a PostgreSQL database
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::SQL::Connection::MySQL;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::SQL::Connection>
-
-=cut
-
-use Bivio::SQL::Connection;
-@Bivio::SQL::Connection::MySQL::ISA = ('Bivio::SQL::Connection');
-
-=head1 DESCRIPTION
-
-C<Bivio::SQL::Connection::MySQL>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_dbi_prefix"></a>
-
-=head2 static get_dbi_prefix(hash_ref cfg) : string
-
-Returns the PostgreSQL DBI connection prefix.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_dbi_prefix {
+    # Returns the PostgreSQL DBI connection prefix.
     return 'DBI:mysql:database=';
 }
 
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
+sub internal_get_retry_sleep {
+    my($self, $error, $message) = @_;
+    return 5 if $message =~ /MySQL server has gone away/;
+    return undef;
+}
 
 1;
