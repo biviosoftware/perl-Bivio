8db195f04b92d6ebd8e9a4677f7d1d849dc91ac6
Rob Nagler <github@q33.us>
2007-05-24 03:27:08 +0000

    added image_file.EXISTS

--- UI/FacadeBase.pm
+++ UI/FacadeBase.pm
@@ -127,6 +127,7 @@ sub _cfg_base {
 	    [EXISTS => 'vs_fe("label"); already exists in our database.'],
 	    [NOT_FOUND => 'vs_fe("label"); was not found in our database.'],
 	    ['image_file.TOO_MANY' => 'vs_fe("label"); contains multiple images, please upload a file which contains only one image.'],
+	    ['image_file.EXISTS' => 'vs_fe("label"); image already exists.  Please choose another name.'],
 	    ['image_file.SYNTAX_ERROR' => 'vs_fe("label"); unknown or invalid image format.  Please verify file, and change to an acceptable format (e.g. png, gif, jpg), and retry upload.'],
 	],
 	HTML => [
