23fea90fd0d36b3bca012132f4d5968287e9839e
Rob Nagler <github@q33.us>
2007-05-24 02:20:58 +0000

    moved file_field from FormModel

--- Type/FileField.pm
+++ Type/FileField.pm
@@ -1,74 +1,40 @@
-# Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2007 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::FileField;
 use strict;
-$Bivio::Type::FileField::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::FileField::VERSION;
-
-=head1 NAME
-
-Bivio::Type::FileField - a form field which contains a file
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::FileField;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type>
-
-=cut
-
-use Bivio::Type;
-@Bivio::Type::FileField::ISA = ('Bivio::Type');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::FileField> is a hash_ref.  The attributes are:
-
-=over 4
-
-=item content
-
-A scalar_ref pointing to the content.
-
-=item content_type
-
-The I<content-type> string in the input.
-
-=item filename
-
-Name supplied in the I<content-disposition> attribute.
-
-=back
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::Type';
 use Bivio::Type::Text;
 
-#=VARIABLES
+# C<Bivio::Type::FileField> is a hash_ref.  The attributes are:
+#
+#
+# content
+#
+# A scalar_ref pointing to the content.
+#
+# content_type
+#
+# The I<content-type> string in the input.
+#
+# filename
+#
+# Name supplied in the I<content-disposition> attribute.
 
-=head1 METHODS
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-=cut
-
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(any value) : hash_ref
-
-Checks the incoming file for valid values.
-
-=cut
+sub from_disk {
+    my($proto, $file_name) = @_;
+    return {
+	filename => $proto->use('Type.FilePath')->get_tail($file_name),
+	content_type => $proto->use('Bivio::MIME::Type')
+	    ->unsafe_from_extension($file_name),
+        content => $proto->use('Bivio::IO::File')->read($file_name),
+    };
+}
 
 sub from_literal {
     my($proto, $value) = @_;
+    # Checks the incoming file for valid values.
     $proto->internal_from_literal_warning
         unless wantarray;
     return (undef, undef) unless defined($value);
@@ -95,78 +61,31 @@ sub from_literal {
     return (undef, Bivio::TypeError::EMPTY());
 }
 
-=for html <a name="from_sql_column"></a>
-
-=head2 static from_sql_column(scalar param) : undef
-
-B<NOT SUPPORTED>
-
-=cut
-
 sub from_sql_column {
+    # B<NOT SUPPORTED>
     die("can't convert a FileField from sql");
 }
 
-=for html <a name="get_width"></a>
-
-=head2 get_width() : int
-
-Returns same as Text.
-
-=cut
-
 sub get_width {
+    # Returns same as Text.
     return Bivio::Type::Text->get_width;
 }
 
-=for html <a name="to_literal"></a>
-
-=head2 static to_literal(hash_ref value) : string
-
-Returns the filename, not the content.
-
-=cut
-
 sub to_literal {
     my(undef, $value) = @_;
+    # Returns the filename, not the content.
     return ref($value) eq 'HASH' && defined($value->{filename})
 	    ? $value->{filename} : '';
 }
 
-=for html <a name="to_query"></a>
-
-=head2 static to_query(any value) : string
-
-B<NOT SUPPORTED>
-
-=cut
-
 sub to_query {
+    # B<NOT SUPPORTED>
     die("can't convert a FileField to a query");
 }
 
-=for html <a name="to_uri"></a>
-
-=head2 static to_uri(any value) : string
-
-B<NOT SUPPORTED>
-
-=cut
-
 sub to_uri {
+    # B<NOT SUPPORTED>
     die("can't convert a FileField to a uri");
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
