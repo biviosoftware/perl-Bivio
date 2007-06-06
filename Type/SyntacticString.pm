9332de01d965807adc6d82e454eff119386b5058
Rob Nagler <github@q33.us>
2007-06-06 15:40:13 +0000

    added internal_post_from_literal and internal_pre_from_literal

--- Type/SyntacticString.pm
+++ Type/SyntacticString.pm
@@ -2,24 +2,34 @@
 # $Id$
 package Bivio::Type::SyntacticString;
 use strict;
-use base 'Bivio::Type::String';
+use Bivio::Base 'Type.String';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub from_literal {
-    my($proto, $value) = splice(@_, 0, 2);
-    if (defined($value)) {
-	$value =~ s/^\s+|\s+$//g;
-	$value =~ s/\s+/ /g;
-    }
-    my($v, $e) = $proto->SUPER::from_literal($value, @_);
+    my($proto, $value) = @_;
+    $value = $proto->internal_pre_from_literal($value);
+    my($v, $e) = $proto->SUPER::from_literal($value);
     return !defined($v) ? ($v, $e)
-	: $v =~ /^@{[$proto->REGEX]}$/ ? $v
+	: $v =~ /^@{[$proto->REGEX]}$/ ? $proto->internal_post_from_literal($v)
 	: (undef, Bivio::TypeError->SYNTAX_ERROR);
 }
 
 sub get_width {
     return 100;
+}
+
+sub internal_post_from_literal {
+    return $_[1];
+}
+
+sub internal_pre_from_literal {
+    my($proto, $value) = @_;
+    if (defined($value)) {
+	$value =~ s/^\s+|\s+$//g;
+	$value =~ s/\s+/ /g;
+    }
+    return $value;
 }
 
 1;
