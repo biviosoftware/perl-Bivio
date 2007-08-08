1d8d844a644fb872af9eb4103b725d572f7466f4
Rob Nagler <github@q33.us>
2007-08-08 15:26:38 +0000

    rmpod

--- UI/HTML/Widget/String.pm
+++ UI/HTML/Widget/String.pm
@@ -1,160 +1,88 @@
-# Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2007 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::UI::HTML::Widget::String;
 use strict;
-$Bivio::UI::HTML::Widget::String::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::String::VERSION;
-
-=head1 NAME
-
-Bivio::UI::HTML::Widget::String - renders a string with font decoration
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::String;
-    Bivio::UI::HTML::Widget::String->new($attrs);
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::HTML::Widget::String::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::String> draws a string with decoration.  Does no
-alignment (see L<Bivio::UI::HTML::Widget::Grid|Bivio::UI::HTML::Widget::Grid>
-for layout issues).
-
-The string is html-escaped and newlines are converted to C<E<lt>brE<gt>>.
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item escape_html : boolean [see description]
-
-Should we escape HTML specials within the value?
-
-True by default for non-widgets.  False by default if the value or the widget
-value is a widget.
-
-=item format : Bivio::UI::HTML::Format []
-
-=item format : string []
-
-The name of the formatter to use on I<value> before escaping the html.
-Only valid if I<value> is not a widget.
-
-=item hard_newlines : boolean [1]
-
-Newlines force hard breaks in the text.  Only valid if
-I<escape_html> is true.
-
-=item hard_spaces : boolean [0]
-
-Replace all spaces (not tabs or newlines) in the text with &nbsp;.
-Only valid if I<escape_html> is true.
-
-=item pad_left : int [0]
-
-Number of non-breaking spaces to pad on the left.
-
-=item pad_right : int [0]
-
-Number of non-breaking spaces to pad on the right.
-
-=item string_font : string [] (inherited, dynamic)
-
-The value to be passed to L<Bivio::UI::Font|Bivio::UI::Font>.
-
-=item undef_value : string ['']
-
-What to display if I<value> is C<undef>.
-Not used if I<value> is a constant.
-
-=item value : array_ref (required)
-
-Dereferenced and passed to C<$source-E<gt>get_widget_value>
-to get string to use (see below).  The return value of
-get_widget_value may be widget in which case render is called.
-
-=item value : string (required)
-
-Text to render.
-
-=item value : Bivio::UI::Widget (required)
-
-The widget to render.  Typically, can only be
-L<Bivio::UI::Widget::Join|Bivio::UI::Widget::Join>,
-but may be any widget.  Purpose is to be able to set the
-font on a collection of strings join.
-
-The values returned by the widget are not "escaped".  The widget
-must generate proper html.
-
-=back
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::UI::Widget';
 use Bivio::Die;
 use Bivio::HTML;
 use Bivio::UI::Font;
 use Bivio::UI::HTML::Format;
 
-#=VARIABLES
+# C<Bivio::UI::HTML::Widget::String> draws a string with decoration.  Does no
+# alignment (see L<Bivio::UI::HTML::Widget::Grid|Bivio::UI::HTML::Widget::Grid>
+# for layout issues).
+#
+# The string is html-escaped and newlines are converted to C<E<lt>brE<gt>>.
+#
+#
+#
+# escape_html : boolean [see description]
+#
+# Should we escape HTML specials within the value?
+#
+# True by default for non-widgets.  False by default if the value or the widget
+# value is a widget.
+#
+# format : Bivio::UI::HTML::Format []
+#
+# format : string []
+#
+# The name of the formatter to use on I<value> before escaping the html.
+# Only valid if I<value> is not a widget.
+#
+# hard_newlines : boolean [1]
+#
+# Newlines force hard breaks in the text.  Only valid if
+# I<escape_html> is true.
+#
+# hard_spaces : boolean [0]
+#
+# Replace all spaces (not tabs or newlines) in the text with &nbsp;.
+# Only valid if I<escape_html> is true.
+#
+# pad_left : int [0]
+#
+# Number of non-breaking spaces to pad on the left.
+#
+# pad_right : int [0]
+#
+# Number of non-breaking spaces to pad on the right.
+#
+# string_font : string [] (inherited, dynamic)
+#
+# The value to be passed to L<Bivio::UI::Font|Bivio::UI::Font>.
+#
+# undef_value : string ['']
+#
+# What to display if I<value> is C<undef>.
+# Not used if I<value> is a constant.
+#
+# value : array_ref (required)
+#
+# Dereferenced and passed to C<$source-E<gt>get_widget_value>
+# to get string to use (see below).  The return value of
+# get_widget_value may be widget in which case render is called.
+#
+# value : string (required)
+#
+# Text to render.
+#
+# value : Bivio::UI::Widget (required)
+#
+# The widget to render.  Typically, can only be
+# L<Bivio::UI::Widget::Join|Bivio::UI::Widget::Join>,
+# but may be any widget.  Purpose is to be able to set the
+# font on a collection of strings join.
+#
+# The values returned by the widget are not "escaped".  The widget
+# must generate proper html.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
-
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(any value, string font, hash_ref attributes) : Bivio::UI::HTML::Widget::String
-
-=head2 static new(any value, hash_ref attributes) : Bivio::UI::HTML::Widget::String
-
-Create a C<String> widget with I<value> and I<font> (if supplied and defined).
-Pass C<0> (zero) as I<font> to set "no font".  Will not set font, if C<undef>.
-Optionally, pass other I<attributes>.
-
-=head2 static new(hash_ref attributes) : Bivio::UI::HTML::Widget::String
-
-If I<attributes> supplied, creates with attribute (name, value) pairs.
-
-=cut
-
-sub new {
-    my($self) = shift->SUPER::new(@_);
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes static information and child widgets.
-
-=cut
 
 sub initialize {
     my($self) = @_;
+    # Initializes static information and child widgets.
     my($fields) = $self->[$_IDI];
     return if exists($fields->{value});
     $fields->{font} = $self->ancestral_get('string_font', undef);
@@ -192,16 +120,9 @@ sub initialize {
     return;
 }
 
-=for html <a name="internal_new_args"></a>
-
-=head2 static internal_new_args(any arg, ...) : any
-
-Implements positional argument parsing for L<new|"new">.
-
-=cut
-
 sub internal_new_args {
     my(undef, $value, $font, $attributes) = @_;
+    # Implements positional argument parsing for L<new|"new">.
     return '"value" attribute must be defined' unless defined($value);
     if (ref($font) eq 'HASH' && !defined($attributes)) {
 	$attributes = $font;
@@ -214,16 +135,21 @@ sub internal_new_args {
     };
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Render the object.  Outputs nothing if result is empty.
-
-=cut
+sub new {
+    my($self) = shift->SUPER::new(@_);
+    # Create a C<String> widget with I<value> and I<font> (if supplied and defined).
+    # Pass C<0> (zero) as I<font> to set "no font".  Will not set font, if C<undef>.
+    # Optionally, pass other I<attributes>.
+    #
+    #
+    # If I<attributes> supplied, creates with attribute (name, value) pairs.
+    $self->[$_IDI] = {};
+    return $self;
+}
 
 sub render {
     my($self, $source, $buffer) = @_;
+    # Render the object.  Outputs nothing if result is empty.
     my($fields) = $self->[$_IDI];
     Bivio::Die->die("String widget not initialized: ", $self->get('value'))
 	    unless exists($fields->{value});
@@ -271,14 +197,9 @@ sub render {
     return;
 }
 
-#=PRIVATE METHODS
-
-# _escape(hash_ref fields, string value) : string
-#
-# Escapes the value.
-#
 sub _escape {
     my($fields, $value) = @_;
+    # Escapes the value.
     $value = Bivio::HTML->escape($value);
     $value =~ s/ /&nbsp;/sg if $fields->{hard_spaces};
     $value =~ s{\n}{<br />}mg if $fields->{hard_newlines};
@@ -286,13 +207,10 @@ sub _escape {
     return $value;
 }
 
-# _format(hash_ref fields, string value) : string
-#
-# Formats and escapes the string and replaces newlines with <br>.
-# An all space string equates to a &nbsp;
-#
 sub _format {
     my($fields, $value) = @_;
+    # Formats and escapes the string and replaces newlines with <br>.
+    # An all space string equates to a &nbsp;
     if ($fields->{format}) {
 	$value = $fields->{format}->get_widget_value($value);
 	return $value if $fields->{format}->result_is_html;
@@ -305,15 +223,5 @@ sub _format {
     # Note the treatment of escape when -1 or +1.
     return $fields->{escape} ? _escape($fields, $value) : $value;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
