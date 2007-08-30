344598af5d3fe64434a073247ed09f8c92a1868b
Rob Nagler <github@q33.us>
2007-08-30 20:46:48 +0000

    *** empty log message ***

---
+++ t/Base/After/T2.pm
@@ -0,0 +1,9 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::t::Base::After::T2;
+use strict;
+use Bivio::Base 'Bivio::UNIVERSAL';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
