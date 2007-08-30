0ebc15961849539faccae885d1e9c25e87a79779
Rob Nagler <github@q33.us>
2007-08-30 20:45:01 +0000

    after_in_map testing

--- IO/t/ClassLoader/After/Valid.pm
+++ IO/t/ClassLoader/After/Valid.pm
@@ -1,8 +1,8 @@
 # Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
 # $Id$
-package Bivio::IO::t::ClassLoader::Valid;
+package Bivio::IO::t::ClassLoader::After::Valid;
 use strict;
-use base 'Bivio::UNIVERSAL';
+use base 'Bivio::IO::t::ClassLoader::Valid';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
