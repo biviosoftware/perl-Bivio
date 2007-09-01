0a00028e75e6ff96ccbed44508c4fccf42a05dfa
Rob Nagler <github@q33.us>
2007-09-01 23:30:06 +0000

    added hex_key

--- Util/OTP.pm
+++ Util/OTP.pm
@@ -11,21 +11,24 @@ sub USAGE {
     return <<'EOF';
 usage: b-otp [options] command [args...]
 commands:
-    to_six_word sequence_number seed [passphrase] -- generates a one time password
+    hex_key sequence_number seed [passphrase] -- returns one time password in hex
+    six_word_key sequence_number seed [passphrase] -- returns in six word format
 EOF
 }
 
-sub to_six_word {
-    my($self, $count, $seed, $passphrase) = @_;
-    $count = $self->convert_literal(OTPSequence => $count);
-    $seed = $self->convert_literal(OTPSeed => $seed);
-    $passphrase = $self->convert_literal(
-	OTPPassphrase =>
-	    $passphrase
-	    || $self->use('Bivio::IO::TTY')->read_password('Passphrase: '));
-    return Bivio::OTP::RFC2289->to_six_word_format(
-        Bivio::OTP::RFC2289->compute($passphrase, $seed, $count)
-    );
+sub hex_key {
+    my(undef, @args) = shift->arg_list(\@_, [
+	'OTPSequence',
+	'OTPSeed',
+	[passphrase => OTPPassphrase => sub {
+	     shift->use('Bivio::IO::TTY')->read_password('Passphrase: ');
+	}],
+    ]);
+    return Bivio::OTP::RFC2289->compute(@args);
+}
+
+sub six_word_key {
+    return Bivio::OTP::RFC2289->to_six_word_format(shift->hex_key(@_));
 }
 
 1;
