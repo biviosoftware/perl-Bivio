6d01241fd9796a18a2f23656f269d6f0fb87742e
Paul Moeller <moeller@bivio.biz>
2007-09-01 21:31:35 +0000

    added require_otp, added is_leaf(), refactored create()

--- Biz/Model/Forum.pm
+++ Biz/Model/Forum.pm
@@ -8,34 +8,34 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub create {
     my($self, $values) = @_;
-    $values->{want_reply_to} = 1
-	unless defined($values->{want_reply_to});
-    $values->{is_public_email} = 0
-	unless defined($values->{is_public_email});
-    $values->{parent_realm_id} = Bivio::Auth::Realm->get_general->get('id'),
-	unless defined($values->{parent_realm_id});
-    return shift->SUPER::create(@_);
+    return $self->SUPER::create({
+	want_reply_to => 1,
+	is_public_email => 0,
+	parent_realm_id => Bivio::Auth::Realm->get_general->get('id'),
+	require_otp => 0,
+	%$values,
+    });
 }
 
 sub create_realm {
     my($self, $forum, $realm_owner, $admin_user_id) = @_;
-    $forum->{want_reply_to} ||= 0;
-    $forum->{is_public_email} ||= 0;
-    my($req) = $self->get_request;
     my($ro) = $self->new_other('RealmOwner')->create({
 	%$realm_owner,
 	realm_type => Bivio::Auth::RealmType->FORUM,
 	realm_id => $self->create({
+	    want_reply_to => 0,
+	    is_public_email => 0,
 	    %$forum,
-	    parent_realm_id => $req->get('auth_id'),
+	    parent_realm_id => $self->req('auth_id'),
 	})->get('forum_id'),
     });
-    $self->get_request->with_realm($ro, sub {
+    $self->req->with_realm($self->get('forum_id'), sub {
         $self->new_other('RealmFile')->init_realm;
 	return;
     });
     $self->new_other('ForumUserAddForm')->copy_admins(
-	$ro->get('realm_id'), $admin_user_id);
+	$self->get('forum_id'), $admin_user_id);
+#TODO: remove this hack
     # Reset state after ForumUserAddForm messed it up
     $self->put_on_request;
     $ro->put_on_request;
@@ -53,28 +53,40 @@ sub internal_initialize {
 	    parent_realm_id => ['PrimaryId', 'NOT_NULL'],
 	    want_reply_to => ['Boolean', 'NOT_NULL'],
 	    is_public_email => ['Boolean', 'NOT_NULL'],
+	    require_otp => ['Boolean', 'NOT_NULL'],
         },
+	other => [
+            [qw(forum_id RealmOwner.realm_id)],
+	],
 	auth_id => 'forum_id',
     });
 }
 
+sub is_leaf {
+    my($self) = @_;
+    return @{$self->new_other('Forum')->map_iterate(
+	sub {
+	    return 1;
+	}, 'unauth_iterate_start', 'forum_id', {
+	    parent_realm_id => $self->get('forum_id'),
+	})} ? 0 : 1;
+}
+
 sub is_root {
     my($self) = @_;
-    return $self->get('parent_realm_id') == Bivio::Auth::RealmType->GENERAL->as_int ? 1 : 0;
+    return $self->get('parent_realm_id')
+	== Bivio::Auth::RealmType->GENERAL->as_int ? 1 : 0;
 }
 
 sub unauth_cascade_delete {
     my($self) = @_;
-    my($req) = $self->get_request;
-    my($ro) = $self->new_other('RealmOwner')
-	->unauth_load_or_die({realm_id => $self->get('forum_id')});
-    $req->with_realm($self->get('forum_id'), sub {
+    $self->req->with_realm($self->get('forum_id'), sub {
 	_delete_children($self);
-        $ro->cascade_delete;
+	$self->get_model('RealmOwner')->cascade_delete;
 	return;
     });
-    $req->set_realm(undef)
-	if $req->get('auth_id') eq $self->get('forum_id');
+    $self->req->set_realm(undef)
+	if $self->req('auth_id') eq $self->get('forum_id');
     return;
 }
 
