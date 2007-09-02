df457cfb89df23cbb75672c339428caf7f080588
Paul Moeller <moeller@bivio.biz>
2007-09-02 01:24:49 +0000

    demo data now uses CSV format

--- PetShop/Util.pm
+++ PetShop/Util.pm
@@ -190,6 +190,7 @@ EOF
 use Bivio::Auth::Role;
 use Bivio::Biz::Util::RealmRole;
 use Bivio::Type::DateTime;
+use Bivio::Util::CSV;
 
 #=VARIABLES
 my($_DT) = 'Bivio::Type::DateTime';
@@ -356,47 +357,37 @@ sub _init_demo_categories {
 #
 sub _init_demo_items {
     my($self) = @_;
-    my($item) = Bivio::Biz::Model->new($self->get_request, 'Item');
-    my($inventory) = Bivio::Biz::Model->new($self->get_request, 'Inventory');
-    my($status) = Bivio::Type->get_instance('ItemStatus')->OK;
-    foreach my $row (
-	    'EST-1:FI-SW-01:16.5:10:Large',
-	    'EST-2:FI-SW-01:16.5:10:Small',
-	    'EST-3:FI-SW-02:18.5:12:Toothless',
-	    'EST-4:FI-FW-01:18.5:12:Spotted',
-	    'EST-5:FI-FW-01:18.5:12:Spotless',
-	    'EST-6:K9-BD-01:18.5:12:Male Adult',
-	    'EST-7:K9-BD-01:18.5:12:Female Puppy',
-	    'EST-8:K9-PO-02:18.5:12:Male Puppy',
-	    'EST-9:K9-DL-01:18.5:12:Spotless Male Puppy',
-	    'EST-10:K9-DL-01:18.5:12:Spotted Adult Female',
-	    'EST-11:RP-SN-01:18.5:12:Venomless',
-	    'EST-12:RP-SN-01:18.5:12:Rattleless',
-	    'EST-13:RP-LI-02:18.5:12:Green Adult',
-	    'EST-14:FL-DSH-01:58.5:12:Tailless',
-	    'EST-15:FL-DSH-01:23.5:12:With tail',
-	    'EST-16:FL-DLH-02:93.5:12:Adult Female',
-	    'EST-17:FL-DLH-02:93.5:12:Adult Male',
-	    'EST-18:AV-CB-01:193.5:92:Adult Male',
-	    'EST-19:AV-SB-02:15.5:2:Adult Male',
-	    'EST-20:FI-FW-02:5.5:2:Adult Male',
-	    'EST-21:FI-FW-02:5.29:1:Adult Female',
-	    'EST-22:K9-RT-01:135.5:100:Adult Male',
-	    'EST-23:K9-RT-01:145.49:100:Adult Female',
-	    'EST-24:K9-RT-02:255.5:92:Adult Male',
-	    'EST-25:K9-RT-02:325.29:90:Adult Female',
-	   ) {
-	my(@col) = split(/:/, $row);
-	$item->create({
-	    item_id => $col[0],
-	    product_id => $col[1],
-	    list_price => $col[2],
-	    unit_cost => $col[3],
-	    status => $status,
-	    attr1 => $col[4],
-	});
-	$inventory->create({
-	    item_id => $col[0],
+
+    foreach my $record (@{Bivio::Util::CSV->parse_records(<<'EOF')}) {
+item_id,product_id,list_price,unit_cost,attr1
+EST-1,FI-SW-01,16.5,10,Large
+EST-2,FI-SW-01,16.5,10,Small
+EST-3,FI-SW-02,18.5,12,Toothless
+EST-4,FI-FW-01,18.5,12,Spotted
+EST-5,FI-FW-01,18.5,12,Spotless
+EST-6,K9-BD-01,18.5,12,Male Adult
+EST-7,K9-BD-01,18.5,12,Female Puppy
+EST-8,K9-PO-02,18.5,12,Male Puppy
+EST-9,K9-DL-01,18.5,12,Spotless Male Puppy
+EST-10,K9-DL-01,18.5,12,Spotted Adult Female
+EST-11,RP-SN-01,18.5,12,Venomless
+EST-12,RP-SN-01,18.5,12,Rattleless
+EST-13,RP-LI-02,18.5,12,Green Adult
+EST-14,FL-DSH-01,58.5,12,Tailless
+EST-15,FL-DSH-01,23.5,12,With tail
+EST-16,FL-DLH-02,93.5,12,Adult Female
+EST-17,FL-DLH-02,93.5,12,Adult Male
+EST-18,AV-CB-01,193.5,92,Adult Male
+EST-19,AV-SB-02,15.5,2,Adult Male
+EST-20,FI-FW-02,5.5,2,Adult Male
+EST-21,FI-FW-02,5.29,1,Adult Female
+EST-22,K9-RT-01,135.5,100,Adult Male
+EST-23,K9-RT-01,145.49,100,Adult Female
+EST-24,K9-RT-02,255.5,92,Adult Male
+EST-25,K9-RT-02,325.29,90,Adult Female
+EOF
+        $self->model('Inventory')->create({
+	    item_id => $self->model('Item')->create($record)->get('item_id'),
 	    quantity => 10_000,
 	});
     }
@@ -409,33 +400,26 @@ sub _init_demo_items {
 #
 sub _init_demo_products {
     my($self) = @_;
-    my($cat) = Bivio::Type->get_instance('Category');
-    my($model) = Bivio::Biz::Model->new($self->get_request, 'Product');
-    foreach my $row (
-	    'FI-SW-01:FISH:Angelfish:angelfish:Salt Water fish from Australia',
-	    'FI-SW-02:FISH:Tiger Shark:tigershark:Salt Water fish from Australia',
-	    'FI-FW-01:FISH:Koi:koi:Fresh Water fish from Japan',
-	    'FI-FW-02:FISH:Goldfish:reeffish:Fresh Water fish from China',
-	    'K9-BD-01:DOGS:Corgi:corgi:Friendly dog from Wales',
-	    'K9-PO-02:DOGS:Poodle:poodle:Cute dog from France',
-	    'K9-DL-01:DOGS:Dalmation:dalmation:Great dog for a Fire Station',
-	    'K9-RT-01:DOGS:German Shepherd:shepherd:Great family dog',
-	    'K9-RT-02:DOGS:Labrador Retriever:lab:Great hunting dog',
-	    'RP-SN-01:REPTILES:Rattlesnake:rattlesnake:Doubles as a watch dog',
-	    'RP-LI-02:REPTILES:Iguana:iguana:Friendly green friend',
-	    'FL-DSH-01:CATS:Manx:manx:Great for reducing mouse populations',
-	    'FL-DLH-02:CATS:Persian:persian:Friendly house cat, doubles as a princess',
-	    'AV-CB-01:BIRDS:Amazon Parrot:parrot:Great companion for up to 75 years',
-	    'AV-SB-02:BIRDS:Finch:finch:Great stress reliever') {
 
-	my(@col) = split(/:/, $row);
-	$model->create({
-	    product_id => $col[0],
-	    category_id => $cat->from_name($col[1])->get_name,
-	    name => $col[2],
-	    image_name => $col[3],
-	    description => $col[4],
-	});
+    foreach my $record (@{Bivio::Util::CSV->parse_records(<<'EOF')}) {
+product_id,category_id,name,image_name,description
+FI-SW-01,FISH,Angelfish,angelfish,Salt Water fish from Australia
+FI-SW-02,FISH,Tiger Shark,tigershark,Salt Water fish from Australia
+FI-FW-01,FISH,Koi,koi,Fresh Water fish from Japan
+FI-FW-02,FISH,Goldfish,reeffish,Fresh Water fish from China
+K9-BD-01,DOGS,Corgi,corgi,Friendly dog from Wales
+K9-PO-02,DOGS,Poodle,poodle,Cute dog from France
+K9-DL-01,DOGS,Dalmation,dalmation,Great dog for a Fire Station
+K9-RT-01,DOGS,German Shepherd,shepherd,Great family dog
+K9-RT-02,DOGS,Labrador Retriever,lab,Great hunting dog
+RP-SN-01,REPTILES,Rattlesnake,rattlesnake,Doubles as a watch dog
+RP-LI-02,REPTILES,Iguana,iguana,Friendly green friend
+FL-DSH-01,CATS,Manx,manx,Great for reducing mouse populations
+FL-DLH-02,CATS,Persian,persian,Friendly house cat, doubles as a princess
+AV-CB-01,BIRDS,Amazon Parrot,parrot,Great companion for up to 75 years
+AV-SB-02,BIRDS,Finch,finch,Great stress reliever
+EOF
+        $self->model('Product')->create($record);
     }
     return;
 }
