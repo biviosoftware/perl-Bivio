37be89a93d357aa8f25c7be99f82f705e89c351d
Rob Nagler <github@q33.us>
2007-09-03 06:29:03 +0000

    fmt

--- Biz/Model/UserLoginForm.pm
+++ Biz/Model/UserLoginForm.pm
@@ -194,8 +194,7 @@ sub validate {
     }
     my($owner) = $self->validate_login;
     return
-	if !$owner
-	    || ($self->in_error && !$owner->require_otp);
+	if !$owner || ($self->in_error && !$owner->require_otp);
     unless ($owner->get_field_type('password')->is_equal(
 	$owner->get('password'),
 	$self->get('RealmOwner.password'),
