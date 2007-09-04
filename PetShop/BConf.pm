32a31dd5e60dae686ba20db2848b5f16463f814d
Rob Nagler <github@q33.us>
2007-09-04 04:31:53 +0000

    fpc

--- PetShop/BConf.pm
+++ PetShop/BConf.pm
@@ -17,8 +17,7 @@ sub dev_overrides {
 
 sub merge_overrides {
     my($proto) = @_;
-    return {
-	$proto->default_merge_overrides('Bivio/PetShop' => 'pet' => 'bivio Software, Inc.'),
+    return Bivio::IO::Config->merge_list({
 	'Bivio::Biz::Model::MailReceiveDispatchForm' => {
 	    ignore_dashes_in_recipient => 1,
 	},
@@ -33,18 +32,18 @@ sub merge_overrides {
 		'Bivio::Agent::TaskId' => 'Bivio::PetShop::Delegate::TaskId',
 		'Bivio::Agent::HTTP::Cookie' => 'Bivio::Delegate::Cookie',
 		'Bivio::Auth::RealmType'
-                    => 'Bivio::PetShop::Delegate::RealmType',
+		    => 'Bivio::PetShop::Delegate::RealmType',
 		'Bivio::Auth::Support' => 'Bivio::Delegate::SimpleAuthSupport',
 		'Bivio::Auth::Permission'
 		    => 'Bivio::PetShop::Delegate::Permission',
 		'Bivio::Auth::Role' => 'Bivio::PetShop::Delegate::Role',
-                'Bivio::Type::ECService'
-                    => 'Bivio::PetShop::Delegate::ECService',
-                'Bivio::Type::Location'
-                    => 'Bivio::PetShop::Delegate::Location',
+		'Bivio::Type::ECService'
+		    => 'Bivio::PetShop::Delegate::ECService',
+		'Bivio::Type::Location'
+		    => 'Bivio::PetShop::Delegate::Location',
 		'Bivio::TypeError' => 'Bivio::PetShop::Delegate::TypeError',
 		'Bivio::UI::HTML::FormErrors'
-	    	    => 'Bivio::PetShop::Delegate::FormErrors',
+		    => 'Bivio::PetShop::Delegate::FormErrors',
 	    },
 	    maps => {
 		Action => ['Bivio::PetShop::Action'],
@@ -71,7 +70,9 @@ sub merge_overrides {
 	    http_suffix => 'bivio.biz',
 	    mail_host => 'bivio.biz',
 	},
-    };
+    }, {
+	$proto->default_merge_overrides('Bivio/PetShop' => 'pet' => 'bivio Software, Inc.'),
+    });
 }
 
 1;
