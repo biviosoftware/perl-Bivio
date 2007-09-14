a05d0801406333be331ceb7a6a7ac2f8b45bfd74
Rob Nagler <github@q33.us>
2007-09-14 03:22:27 +0000

    use WikiText format_uri()

--- UI/WikiText/Embed.pm
+++ UI/WikiText/Embed.pm
@@ -12,16 +12,9 @@ sub render_html {
     my($proto, $args) = @_;
     Bivio::Die->die($args->{attrs}, ': does not accept attributes')
         if %{$args->{attrs}};
-    my($uri) = $args->{value};
+    my($uri) = $args->{proto}->format_uri($args->{value}, $args);
     return Bivio::Die->die('invalid URI, must begin with a /')
 	unless $uri =~ s{^/+}{/};
-    $uri = '/'
-	. Bivio::Biz::Model->new($args->{req}, 'RealmOwner')
-	    ->unauth_load_or_die({realm_id => $args->{realm_id}})
-	    ->get('name')
-	. $uri
-        if $uri =~ s{^/my(?=/)}{}s
-	&& !Bivio::Auth::Realm->is_default_id($args->{realm_id});
     return ${$proto->use('Bivio::Agent::Embed::Dispatcher')
 	->call_task($args->{req}, $args->{req}->format_uri({
 	    uri => $uri,
