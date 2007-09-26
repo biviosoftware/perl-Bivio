0cc1ae60f357ad544c900870493ebc8c5b06b0b5
Alex Viggio <aviggio@bivio.biz>
2007-09-26 20:31:16 +0000

    Reuse xhtml_copyright

--- UI/XHTML/Widget/Page3.pm
+++ UI/XHTML/Widget/Page3.pm
@@ -22,11 +22,7 @@ sub new {
 	['content'],
 	[foot1 => Link(String('back to top'), '#top')],
 	['foot2'],
-	[foot3 => Prose(<<"EOF")],
-Copyright &copy; @{[__PACKAGE__->use('Type.DateTime')->now_as_year]} vs_text('site_copyright');<br />
-All rights reserved.<br />
-Link('Developed by bivio', 'http://www.bivio.biz');
-EOF
+	[foot3 => vs_text_as_prose('xhtml_copyright')],
 	[style => StyleSheet('/f/base.css')],
     ));
     return $proto->SUPER::new($attrs)->put_unless_exists(
