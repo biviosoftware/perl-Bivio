1206aad02ffd2beb8688950bb8c6fb9deffb4e3c
Paul Moeller <moeller@bivio.biz>
2007-09-28 17:47:50 +0000

    render undef as '' rm pod

--- UI/HTML/Format/Printf.pm
+++ UI/HTML/Format/Printf.pm
@@ -2,69 +2,16 @@
 # $Id$
 package Bivio::UI::HTML::Format::Printf;
 use strict;
-$Bivio::UI::HTML::Format::Printf::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+use Bivio::Base 'Bivio::UI::HTML::Format';
 
-=head1 NAME
+# C<Bivio::UI::HTML::Format::Printf> formats a value using C<sprintf>.
 
-Bivio::UI::HTML::Format::Printf - allows printf formatting of values
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Format::Printf;
-    Bivio::UI::HTML::Format::Printf->get_widget_value($value, $format);
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::HTML::Format>
-
-=cut
-
-use Bivio::UI::HTML::Format;
-@Bivio::UI::HTML::Format::Printf::ISA = ('Bivio::UI::HTML::Format');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Format::Printf> formats a value using C<sprintf>.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_widget_value"></a>
-
-=head2 static get_widget_value(any value, string format) : string
-
-Formats a value using C<sprintf>.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_widget_value {
     my(undef, $value, $format) = @_;
+    return '' unless defined($value);
     return sprintf($format, $value);
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
