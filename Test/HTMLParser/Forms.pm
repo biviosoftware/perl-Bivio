f8c93bc66fe13e7685b548f12b4e96eaef4bae37
Paul Moeller <moeller@bivio.biz>
2007-10-14 18:36:36 +0000

    rm pod

--- Test/HTMLParser/Forms.pm
+++ Test/HTMLParser/Forms.pm
@@ -2,43 +2,13 @@
 # $Id$
 package Bivio::Test::HTMLParser::Forms;
 use strict;
-$Bivio::Test::HTMLParser::Forms::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Test::HTMLParser::Forms::VERSION;
-
-=head1 NAME
-
-Bivio::Test::HTMLParser::Forms - models forms on the page
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Test::HTMLParser::Forms;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Test::HTMLParser>
-
-=cut
-
-use Bivio::Test::HTMLParser;
-@Bivio::Test::HTMLParser::Forms::ISA = ('Bivio::Test::HTMLParser');
-
-=head1 DESCRIPTION
-
-C<Bivio::Test::HTMLParser::Forms> models the forms on a page.
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::Test::HTMLParser';
 use Bivio::IO::Config;
 use Bivio::IO::Trace;
 
-#=VARIABLES
+# C<Bivio::Test::HTMLParser::Forms> models the forms on a page.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 __PACKAGE__->register(['Cleaner']);
 Bivio::IO::Config->register(my $_CFG = {
@@ -49,60 +19,25 @@ Bivio::IO::Config->register(my $_CFG = {
     disable_checkbox_heading => {},
 });
 
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(Bivio::Test::HTMLParser parser) : Bivio::Test::HTMLParser::Forms
-
-Parses cleaned html for forms.
-
-=cut
-
-sub new {
-    my($proto, $parser) = @_;
-    my($self) = $proto->SUPER::new;
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_by_field_names"></a>
-
-=head2 get_by_field_names(any name, ...) : hash_ref
-
-Returns the form data by finding by I<name>(s) in visible and submit fields of
-forms.  See interpretation of I<name> in L<unsafe_get_field|"unsafe_get_field">.
-
-=cut
-
 sub get_by_field_names {
     my($self, @name) = @_;
+    # Returns the form data by finding by I<name>(s) in visible and submit
+    # fields of forms.  See interpretation of I<name> in
+    # L<unsafe_get_field|"unsafe_get_field">.
     my($form) = shift->unsafe_get_by_field_names(@_);
     return $form
 	if $form;
 
-    Bivio::Die->die(\@name, ': no form matches named fields; all visible form fields: ',
+    Bivio::Die->die(\@name,
+	': no form matches named fields; all visible form fields: ',
         map({[sort(keys(%{$_->{visible}}), keys(%{$_->{submit}}))]}
             values(%{$self->get_shallow_copy})));
 }
 
-=for html <a name="get_field"></a>
-
-=head2 static get_field(hash_ref form, any name) : hash_ref
-
-Calls L<unsafe_get_field|"unsafe_get_field"> and dies unless matches
-fields exactly.
-
-=cut
-
 sub get_field {
     my($proto, $form, $name) = @_;
+    # Calls L<unsafe_get_field|"unsafe_get_field"> and dies unless matches
+    # fields exactly.
     my($res) = shift->unsafe_get_field(@_);
     Bivio::Die->die($name, ': ',
 	(@$res ? ('matches too many fields: ', @$res) : 'field not found'),
@@ -111,17 +46,10 @@ sub get_field {
     return $res->[0];
 }
 
-=for html <a name="get_ok_button"></a>
-
-=head2 get_ok_button(any form) : string
-
-Returns name of ok_button.  There cannot be more than one button, excluding
-cancel.  If there are no submit buttons, returns undef.
-
-=cut
-
 sub get_ok_button {
     my($self, $form) = @_;
+    # Returns name of ok_button.  There cannot be more than one button,
+    # excluding cancel.  If there are no submit buttons, returns undef.
     $form = $self->get_by_field_names($form)
 	unless ref($form) eq 'HASH';
     my(@ok) = grep(!/cancel/i, keys(%{$form->{submit}}));
@@ -130,46 +58,28 @@ sub get_ok_button {
     return $ok[0];
 }
 
-=for html <a name="handle_config"></a>
-
-=head2 static handle_config(hash cfg)
-
-=over 4
-
-=item error_class : string [form_field_error]
-
-unique class for error text on page. If found, assumes form failed.
-
-=item error_color : string [#990000]
-
-unique color for error text on page. If found, assumes form failed.
-
-=item disable_checkbox_heading : hash_ref {}
-
-Controls whether a checkbox in a table uses the table heading as a
-label, or if the label is parsed from nearby text.
-The key is the name of the table heading.
-
-=back
-
-=cut
-
 sub handle_config {
     my(undef, $cfg) = @_;
+    # error_class : string [form_field_error]
+    #
+    # unique class for error text on page. If found, assumes form failed.
+    #
+    # error_color : string [#990000]
+    #
+    # unique color for error text on page. If found, assumes form failed.
+    #
+    # disable_checkbox_heading : hash_ref {}
+    #
+    # Controls whether a checkbox in a table uses the table heading as a
+    # label, or if the label is parsed from nearby text.
+    # The key is the name of the table heading.
     $_CFG = $cfg;
     return;
 }
 
-=for html <a name="html_parser_end"></a>
-
-=head2 html_parser_end(string tag, string origtext)
-
-Dispatch to the _end_XXX routines.
-
-=cut
-
 sub html_parser_end {
     my($self, $tag) = @_;
+    # Dispatch to the _end_XXX routines.
     my($fields) = $self->[$_IDI];
     return _end_th($fields)
 	if $tag eq 'th';
@@ -186,16 +96,9 @@ sub html_parser_end {
     return;
 }
 
-=for html <a name="html_parser_start"></a>
-
-=head2 html_parser_start(string tag, hash_ref attr, array_ref attrseq, string origtext)
-
-Calls _fixup_attr then dispatches to the _start_XXX routines.
-
-=cut
-
 sub html_parser_start {
     my($self, $tag, $attr) = @_;
+    # Calls _fixup_attr then dispatches to the _start_XXX routines.
     my($fields) = $self->[$_IDI];
     _fixup_attr($tag, $attr);
     if (($attr->{class} || '') eq 'label') {
@@ -208,27 +111,21 @@ sub html_parser_start {
     return _start_option($fields, $attr)
 	if $tag eq 'option';
     return _start_input($self, $attr)
-	if $tag eq 'input' || ($attr->{type} && $tag !~ /^(?:link|style|script)$/);
+	if $tag eq 'input'
+	    || ($attr->{type} && $tag !~ /^(?:link|style|script)$/);
     return _start_maybe_err($fields, $attr)
 	if $tag =~ /^(font|span|div)$/;
     return;
 }
 
-=for html <a name="html_parser_text"></a>
-
-=head2 html_parser_text(string text)
-
-Text is applied as labels to form fields. It is cleaned first.
-In the case of textareas, the cleaned text is saved as the "value"
-(see _end_textarea).
-
-We assume that we can parse an entire sequence of text for
-column headers, etc.
-
-=cut
-
 sub html_parser_text {
     my($self, $text) = @_;
+    # Text is applied as labels to form fields. It is cleaned first.
+    # In the case of textareas, the cleaned text is saved as the "value"
+    # (see _end_textarea).
+    #
+    # We assume that we can parse an entire sequence of text for
+    # column headers, etc.
     my($fields) = $self->[$_IDI];
     if ($fields->{textarea}) {
 	$fields->{textarea}->{value}
@@ -248,16 +145,17 @@ sub html_parser_text {
     return;
 }
 
-=for html <a name="unsafe_get_by_field_names"></a>
-
-=head2 unsafe_get_by_field_names()
-
-Look for form by field names
-
-=cut
+sub new {
+    my($proto, $parser) = @_;
+    # Parses cleaned html for forms.
+    my($self) = $proto->SUPER::new;
+    $self->[$_IDI] = {};
+    return $self;
+}
 
 sub unsafe_get_by_field_names {
     my($self, @name) = @_;
+    # Look for form by field names
     my($found);
     my($forms) = $self->get_shallow_copy;
     FORM: while (my($form, $values) = each(%$forms)) {
@@ -272,18 +170,12 @@ sub unsafe_get_by_field_names {
     return $found;
 }
 
-=for html <a name="unsafe_get_field"></a>
-
-=head2 static unsafe_get_field(hash_ref form, any name) : array_ref
-
-Returns field from I<form>.  I<name> may be a string or a regular expression.
-If it is a string and it matches '(?.*)', I<name> will be treated as a regular
-expression.  Returns list of matches or empty array_ref.
-
-=cut
-
 sub unsafe_get_field {
     my(undef, $form, $name) = @_;
+    # Returns field from I<form>.  I<name> may be a string or a regular
+    # expression. If it is a string and it matches '(?.*)', I<name> will be
+    # treated as a regular expression.
+    # Returns list of matches or empty array_ref.
     $name = $name =~ /^\(\?.*\)$/s ? qr/$name/ : qr/^\Q$name\E$/s
 	unless ref($name);
     my($res) = [];
@@ -295,22 +187,14 @@ sub unsafe_get_field {
     return $res;
 }
 
-#=PRIVATE METHODS
-
-# _empty(string value) : boolean
-#
-# Returns true if !defined or zero length
-#
 sub _empty {
+    # Returns true if !defined or zero length
     return !grep(defined($_) && length($_), @_);
 }
 
-# _end_form(self)
-#
-# Ends the form and puts in $fields->{current}.
-#
 sub _end_form {
     my($self) = @_;
+    # Ends the form and puts in $fields->{current}.
     my($fields) = $self->[$_IDI];
     Bivio::Die->die('unlabeled form field: ', $fields->{input},
 	' form: ', $fields->{current})
@@ -342,12 +226,9 @@ sub _end_form {
     return;
 }
 
-# _end_maybe_err(hash_ref fields)
-#
-# Ends the current tag which may contain err.
-#
 sub _end_maybe_err {
     my($fields) = @_;
+    # Ends the current tag which may contain err.
     my($f) = pop(@{$fields->{maybe_err}});
     return
 	unless (
@@ -361,33 +242,23 @@ sub _end_maybe_err {
     return;
 }
 
-# _end_select(hash_ref fields)
-#
-# Ends the select.
-#
 sub _end_select {
     my($fields) = @_;
     $fields->{select} = undef;
     return;
 }
 
-# _end_table(hash_ref fields)
-#
-# The only tables we track are "data" tables.
-#
 sub _end_table {
     my($fields) = @_;
+    # The only tables we track are "data" tables.
     $fields->{in_data_table}-- if $fields->{in_data_table};
     return;
 }
 
-# _end_textarea(hash_ref fields)
-#
-# Saves the value for the textarea.  May not have "text", because
-# textarea might be blank.
-#
 sub _end_textarea {
     my($fields) = @_;
+    # Saves the value for the textarea.  May not have "text", because
+    # textarea might be blank.
     _trace($fields->{textarea}) if $_TRACE;
     $fields->{textarea}->{value} = ''
         unless defined($fields->{textarea}->{value});
@@ -395,12 +266,9 @@ sub _end_textarea {
     return;
 }
 
-# _end_th(hash_ref fields)
-#
-# Ends the "th".
-#
 sub _end_th {
     my($fields) = @_;
+    # Ends the "th".
     # There's a weird case where {text} will be the empty string,
     # but that's ok in this case.
     $fields->{text} = ' '
@@ -411,13 +279,10 @@ sub _end_th {
     return;
 }
 
-# _fixup_attr(string tag, hash_ref attr)
-#
-# Lowercases all attr values which we care about.  Sets type
-# for select and textarea.
-#
 sub _fixup_attr {
     my($tag, $attr) = @_;
+    # Lowercases all attr values which we care about.  Sets type
+    # for select and textarea.
     while (my($k, $v) = each(%$attr)) {
 	$attr->{$k} = lc($v) if $k =~ /^(?:method|name|type)$/;
     }
@@ -431,22 +296,16 @@ sub _fixup_attr {
     return;
 }
 
-# _have_prefix_label(hash_ref fields) : boolean
-#
-# Returns true if $fields->{text} is a prefix label (ends with colon)
-#
 sub _have_prefix_label {
     my($fields) = @_;
+    # Returns true if $fields->{text} is a prefix label (ends with colon)
     return $fields->{text} && $fields->{text} =~ /:$/;
 }
 
-# _label_field(hash_ref fields, string class, hash_ref attr)
-#
-# Labels all fields, checking for duplicates.  Allows _radio
-# for labels, however.
-#
 sub _label_field {
     my($fields, $class, $attr) = @_;
+    # Labels all fields, checking for duplicates.  Allows _radio
+    # for labels, however.
     _trace($attr) if $_TRACE;
     push(@{$fields->{current}->{$class}->{$attr->{label}} ||= []}, $attr);
     _trace($fields->{current}, ' ', $attr);
@@ -457,25 +316,19 @@ sub _label_field {
     return;
 }
 
-# _label_hidden(hash_ref fields, hash_ref attr)
-#
-# Labels the hidden fields.
-#
 sub _label_hidden {
     my($fields, $attr) = @_;
+    # Labels the hidden fields.
     $attr->{label} = $attr->{name};
     _label_field($fields, 'hidden', $attr);
     return;
 }
 
-# _label_option(hash_ref fields)
-#
-# Labels the option and adds to select or radio.  We label the select (not the
-# radio) if it hasn't already been labeled and the option is selected.  This
-# handles the Select Site, Select Investment, etc. cases.
-#
 sub _label_option {
     my($fields) = @_;
+    # Labels the option and adds to select or radio.  We label the select
+    # (not the radio) if it hasn't already been labeled and the option is
+    # selected.  This handles the Select Site, Select Investment, etc. cases.
     my($which) = $fields->{radio} ? 'radio' : 'option';
     my($group) = $fields->{radio}
 	    ? $fields->{radios}->{$fields->{radio}->{name}}
@@ -500,12 +353,9 @@ sub _label_option {
     return;
 }
 
-# _label_radio(hash_ref fields)
-#
-# Labels a radio button and puts it in the appropriate radio hash.
-#
 sub _label_radio {
     my($fields) = @_;
+    # Labels a radio button and puts it in the appropriate radio hash.
     my($r) = $fields->{radio};
     $r->{label} = _text($fields);
 
@@ -513,12 +363,9 @@ sub _label_radio {
     return;
 }
 
-# _label_submit(self, hash_ref attr)
-#
-# Labels the submit fields.
-#
 sub _label_submit {
     my($self, $attr) = @_;
+    # Labels the submit fields.
     my($fields) = $self->[$_IDI];
     $attr->{label} = $self->get('cleaner')->text(
 	$attr->{src} ? _submit_label_clean($attr->{src})
@@ -533,12 +380,9 @@ sub _label_submit {
     return;
 }
 
-# _label_visible(hash_ref fields)
-#
-# Labels the current input field.
-#
 sub _label_visible {
     my($fields) = @_;
+    # Labels the current input field.
     my($label) = _text($fields);
 
     # We don't label selects with blanks.  Rather with the selected value.
@@ -555,13 +399,10 @@ sub _label_visible {
     return;
 }
 
-# _leftover_input(hash_ref fields, hash_ref next)
-#
-# Left over input field at start of new input.  Anonymous field.  Save
-# context for next field.
-#
 sub _leftover_input {
-    my($fields, $next) = @_;
+    my($fields) = @_;
+    # Left over input field at start of new input.  Anonymous field.  Save
+    # context for next field.
     my(@save) = @{$fields}{qw{text prev_cell_text}};
     $fields->{text} = '_anon';
     _label_visible($fields);
@@ -569,12 +410,9 @@ sub _leftover_input {
     return;
 }
 
-# _start_form(hash_ref fields, hash_ref attr)
-#
-# Starts a form.
-#
 sub _start_form {
     my($fields, $attr) = @_;
+    # Starts a form.
     $fields->{current} = {
 	%$attr,
 	visible => {},
@@ -585,19 +423,16 @@ sub _start_form {
     return;
 }
 
-# _start_input(self, hash_ref attr)
-#
-# Starts a new field.   Certain fields have labels before.  Others
-# have labels after.  Some have labels as the column header.
-#
 sub _start_input {
     my($self, $attr) = @_;
+    # Starts a new field.   Certain fields have labels before.  Others
+    # have labels after.  Some have labels as the column header.
     $attr->{type} ||= 'text';
     my($fields) = $self->[$_IDI];
     _trace($fields->{text}, ' ', $fields->{input}, ' ',
 	$fields->{prev_cell_text}, ' ', $attr)
 	if $_TRACE;
-    _leftover_input($fields, $attr) if $fields->{input};
+    _leftover_input($fields) if $fields->{input};
 
     return _label_hidden($fields, $attr) if $attr->{type} eq 'hidden';
 
@@ -641,12 +476,9 @@ sub _start_input {
     return;
 }
 
-# _start_maybe_err(hash_ref fields, hash_ref attr)
-#
-# Saves current tag info.
-#
 sub _start_maybe_err {
     my($fields, $attr) = @_;
+    # Saves current tag info.
     if (($fields->{text} || '') =~ m/:$/) {
 	$fields->{prev_cell_text} = $fields->{text};
 	$fields->{text} = undef;
@@ -658,12 +490,9 @@ sub _start_maybe_err {
     return;
 }
 
-# _start_option(hash_ref fields, hash_ref attr)
-#
-# Handles an OPTION tag.
-#
 sub _start_option {
     my($fields, $attr) = @_;
+    # Handles an OPTION tag.
     Bivio::Die->die('not in a select: ', $fields)
 	unless $fields->{select};
     $fields->{select}->{options} ||= {};
@@ -671,13 +500,10 @@ sub _start_option {
     return;
 }
 
-# _start_radio(hash_ref fields)
-#
-# Deals with a new radio.   We may have to start a new record in
-# $fields->{radios}.  All radios are labelled non-uniquely "_radio".
-#
 sub _start_radio {
     my($fields) = @_;
+    # Deals with a new radio.   We may have to start a new record in
+    # $fields->{radios}.  All radios are labelled non-uniquely "_radio".
     my($r) = $fields->{radio} = $fields->{input};
     $fields->{input} = undef;
 
@@ -694,13 +520,9 @@ sub _start_radio {
     return;
 }
 
-
-# _start_tx(hash_ref fields, hash_ref attr, string tag)
-#
-# Starts a TD, TH, TR, or TABLE.
-#
 sub _start_tx {
     my($fields, $attr, $tag) = @_;
+    # Starts a TD, TH, TR, or TABLE.
     if ($tag =~ /th|td/) {
 	$fields->{prev_cell_text} = $fields->{text}
 	    if defined($fields->{text}) && length($fields->{text});
@@ -723,22 +545,16 @@ sub _start_tx {
     return;
 }
 
-# _submit_label_clean(string src) : string
-#
-# Grabs icon name.
-#
 sub _submit_label_clean {
     my($src) = @_;
+    # Grabs icon name.
     $src =~ /(?:.*\/)?([^\/]+)\.\w+$/;
     return $1;
 }
 
-# _text(hash_ref fields, boolean no_die) : string
-#
-# Returns the text field or dies if zero length.  Won't die if !$no_die.
-#
 sub _text {
     my($fields, $no_die) = @_;
+    # Returns the text field or dies if zero length.  Won't die if !$no_die.
     my($res) = !_empty($fields->{text})
 	? $fields->{text}
 	: !_empty($fields->{prev_cell_text})
@@ -749,12 +565,9 @@ sub _text {
     return $res;
 }
 
-# _unwind_duplicates(hash_ref fields)
-#
-# Renames duplicates and unwinds singletons.
-#
 sub _unwind_duplicates {
     my($fields) = @_;
+    # Renames duplicates and unwinds singletons.
     foreach my $class (qw(visible hidden submit)) {
 	my($c) = $fields->{current}->{$class};
 	foreach my $k (keys(%$c)) {
@@ -783,16 +596,5 @@ sub _unwind_duplicates {
     }
     return;
 }
-
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002-2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
