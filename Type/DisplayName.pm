941306fb5c9640678587584a0612c92f27c13d38
Rob Nagler <github@q33.us>
2007-10-17 17:06:21 +0000

    to_camel_case

--- Type/DisplayName.pm
+++ Type/DisplayName.pm
@@ -2,12 +2,18 @@
 # $Id$
 package Bivio::Type::DisplayName;
 use strict;
-use base 'Bivio::Type::Line';
+use Bivio::Base 'Type.Line';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_width {
     return 500;
+}
+
+sub to_camel_case {
+    my(undef, $value) = @_;
+    return !$value ? $value
+	: join(' ', map(ucfirst($_), split(/[\W_]+/, $value)));
 }
 
 1;
