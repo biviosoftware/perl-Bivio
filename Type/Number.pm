c60e03df7ffe4070fe0d720152c0445b19061f94
Rob Nagler <github@q33.us>
2007-10-20 21:05:57 +0000

    rmpod

--- Type/Number.pm
+++ Type/Number.pm
@@ -1,183 +1,88 @@
-# Copyright (c) 1999,2000 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2007 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::Number;
 use strict;
-$Bivio::Type::Number::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::Number::VERSION;
-
-=head1 NAME
-
-Bivio::Type::Number - base class for all number types
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::Number;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type>
-
-=cut
-
-use Bivio::Type;
-@Bivio::Type::Number::ISA = qw(Bivio::Type);
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::Number> is the abstract base class for all number types.
-It provides arbitrary precision arithmetic for like-based numbers.
-
-=cut
-
-#=IMPORTS
-# also uses Bivio::TypeError dynamically
+use base 'Bivio::Type';
 use Bivio::IO::ClassLoader;
 use GMP::Mpf ();
 
-#=VARIABLES
+# C<Bivio::Type::Number> is the abstract base class for all number types.
+# It provides arbitrary precision arithmetic for like-based numbers.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+# also uses Bivio::TypeError dynamically
 my($_FUDGE) = _mpf('1e-20');
 my($_HALF) = _mpf('0.5');
 my($_POWER) = {};
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="abs"></a>
-
-=head2 static abs(string v) : string
-
-Converts a negative into a positive number.
-
-=cut
-
 sub abs {
     my($proto, $v) = @_;
+    # Converts a negative into a positive number.
     return _format($proto, abs(_mpf($v)));
 }
 
-=head2 static add(string v, string v2, int decimals) : string
-
-Adds two numbers and returns the result using the specified decimal precision.
-If decimals is undef, then the default precision is used.
-
-=cut
-
 sub add {
     my($proto, $v, $v2, $decimals) = @_;
+    # Adds two numbers and returns the result using the specified decimal precision.
+    # If decimals is undef, then the default precision is used.
     return _format($proto,
         GMP::Mpf::overload_addeq(_mpf($v), _mpf($v2), 0), $decimals);
 }
 
-=for html <a name="can_be_negative"></a>
-
-=head2 static can_be_negative : boolean
-
-Returns true if L<get_min|"get_min"> is less than 0.
-
-=cut
-
 sub can_be_negative {
     my($proto) = @_;
+    # Returns true if L<get_min|"get_min"> is less than 0.
     return $proto->compare($proto->get_min, 0) < 0 ? 1 : 0;
 }
 
-=for html <a name="can_be_positive"></a>
-
-=head2 static can_be_positive : boolean
-
-Returns true if L<get_max|"get_max"> is greater than 0.
-
-=cut
-
 sub can_be_positive {
     my($proto) = @_;
+    # Returns true if L<get_max|"get_max"> is greater than 0.
     return $proto->compare($proto->get_max, 0) > 0 ? 1 : 0;
 }
 
-=for html <a name="can_be_zero"></a>
-
-=head2 static can_be_zero : boolean
-
-Returns true if range crosses through zero.
-
-=cut
-
 sub can_be_zero {
     my($proto) = @_;
+    # Returns true if range crosses through zero.
     return $proto->compare($proto->get_max, 0) >= 0
         && $proto->compare($proto->get_min, 0) <= 0 ? 1 : 0;
 }
 
-=for html <a name="compare_defined"></a>
-
-=head2 static compare_defined(string left, string right, int decimals) : int
-
-See L<Bivio::Type::compare_defined|Bivio::Type/"compare_defined">.
-
-=cut
-
 sub compare_defined {
     my($proto, $left, $right, $decimals) = @_;
+    # See L<Bivio::Type::compare_defined|Bivio::Type/"compare_defined">.
     return _mpf($left) <=> _mpf($right);
 }
 
-=for html <a name="div"></a>
-
-=head2 static div(string numerator, string denominator, int decimals) : string
-
-Divides numerator by denominator and returns the result using the specified
-decimal precision.
-
-Dies if dividing by 0.
-
-=cut
-
 sub div {
     my($proto, $v, $v2, $decimals) = @_;
+    # Divides numerator by denominator and returns the result using the specified
+    # decimal precision.
+    #
+    # Dies if dividing by 0.
     Bivio::Die->die('divide by zero: ', $v, '/', $v2)
         if ! defined($v2) || $v2 =~ /^[0.]+$/;
     return _format($proto,
         GMP::Mpf::overload_diveq(_mpf($v), _mpf($v2), 0), $decimals);
 }
 
-=for html <a name="fraction_as_string"></a>
-
-=head2 static fraction_as_string(string number, int decimals) : string
-
-Returns the fractional part of I<number> up to decimals without
-the leading decimal with rounding.  If I<decimals> is zero, always
-returns the empty string.
-
-=cut
-
 sub fraction_as_string {
     my($proto, $number, $decimals) = @_;
+    # Returns the fractional part of I<number> up to decimals without
+    # the leading decimal with rounding.  If I<decimals> is zero, always
+    # returns the empty string.
     return '' if $decimals == 0;
     my($res) = $proto->round($number, $decimals);
     $res =~ s/.*\.//;
     return $res;
 }
 
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value) : string
-
-Makes sure is a number.  Does not except scientific notation.
-Allows fractional values like "-7 11/15" or "32/3". Fractional
-values are converted to decimal using the precision returned by
-get_decimals().
-
-=cut
-
 sub from_literal {
     my($proto, $value) = @_;
+    # Makes sure is a number.  Does not except scientific notation.
+    # Allows fractional values like "-7 11/15" or "32/3". Fractional
+    # values are converted to decimal using the precision returned by
+    # get_decimals().
     $proto->internal_from_literal_warning
         unless wantarray;
     return undef unless defined($value) && $value =~ /\S/;
@@ -229,128 +134,65 @@ sub from_literal {
     return (undef, Bivio::TypeError->NUMBER_RANGE);
 }
 
-=for html <a name="get_decimals"></a>
-
-=head2 get_decimals() : int
-
-Abstract method to be defined by subclasses.
-
-=cut
-
 sub get_decimals {
+    # Abstract method to be defined by subclasses.
     die("abstract method");
 }
 
-=for html <a name="max"></a>
-
-=head2 max(string left, string right) : string
-
-Returns maximum of I<left> and I<right>.
-
-=cut
-
 sub max {
     my($self, $left, $right) = @_;
+    # Returns maximum of I<left> and I<right>.
     return $self->compare($left, $right) <= 0 ? $right : $left;
 }
 
-=for html <a name="min"></a>
-
-=head2 min(string left, string right) : string
-
-Returns minimum of I<left> and I<right>.
-
-=cut
-
 sub min {
     my($self, $left, $right) = @_;
+    # Returns minimum of I<left> and I<right>.
     return $self->compare($left, $right) <= 0 ? $left : $right;
 }
 
-=for html <a name="mul"></a>
-
-=head2 mul(string v, string v2, int decimals) : string
-
-Multiplies two numbers and returns the result using the specified decimal
-precision.
-If decimals is undef, then the default precision is used.
-
-=cut
-
 sub mul {
     my($proto, $v, $v2, $decimals) = @_;
+    # Multiplies two numbers and returns the result using the specified decimal
+    # precision.
+    # If decimals is undef, then the default precision is used.
     return _format($proto,
         GMP::Mpf::overload_muleq(_mpf($v), _mpf($v2), 0), $decimals);
 }
 
-=for html <a name="neg"></a>
-
-=head2 neg(string number) : string
-
-Returns a number with the opposite sign from the specified one.
-
-=cut
-
 sub neg {
     my($proto, $number) = @_;
+    # Returns a number with the opposite sign from the specified one.
     return _format($proto, - _mpf($number));
 }
 
-=for html <a name="round"></a>
-
-=head2 round(string number, int decimals) : string
-
-Rounds the number to the specified number of decimal places.
-
-=cut
-
 sub round {
     my($proto, $number, $decimals) = @_;
+    # Rounds the number to the specified number of decimal places.
     Bivio::Die->die('invalid decimals: ', $decimals) if $decimals < 0;
     return _format($proto, _mpf($number), $decimals);
 }
 
-=for html <a name="sign"></a>
-
-=head2 static sign(string number) : int
-
-Returns -1, 0, +1 depending on the sign of number.
-
-=cut
-
 sub sign {
     my($proto, $number) = @_;
+    # Returns -1, 0, +1 depending on the sign of number.
     my($sign) = $number =~ /^([-+])/;
     return $sign eq '-' ? -1 : 1 if defined($sign);
     return $proto->compare($number, 0) == 0 ? 0 : 1;
 }
 
-=for html <a name="sub"></a>
-
-=head2 static sub(string v, string v2, int decimals) : string
-
-Subtracts v2 from v and returns the result using the specified decimal
-precision.
-If decimals is undef, then the default precision is used.
-
-=cut
-
 sub sub {
     my($proto, $v, $v2, $decimals) = @_;
+    # Subtracts v2 from v and returns the result using the specified decimal
+    # precision.
+    # If decimals is undef, then the default precision is used.
     return _format($proto,
         GMP::Mpf::overload_subeq(_mpf($v), _mpf($v2), 0), $decimals);
 }
 
-=for html <a name="to_literal"></a>
-
-=head2 to_literal(any value) : string
-
-Converts from internal form to a literal string value.
-
-=cut
-
 sub to_literal {
     my($proto, $value) = @_;
+    # Converts from internal form to a literal string value.
     return $proto->SUPER::to_literal($value)
 	unless defined($value);
 
@@ -368,32 +210,18 @@ sub to_literal {
     return $value;
 }
 
-=for html <a name="trunc"></a>
-
-=head2 trunc(string number, int decimals) : string
-
-Truncates the number to the specified number of decimal places.
-
-=cut
-
 sub trunc {
     my($proto, $number, $decimals) = @_;
+    # Truncates the number to the specified number of decimal places.
     Bivio::Die->die('invalid decimals: ', $decimals) if $decimals < 0;
     my($pow) = 10 ** $decimals;
     return return _format($proto, GMP::Mpf::trunc($number * $pow) / $pow,
         $decimals);
 }
 
-#=PRIVATE METHODS
-
-# _format(proto, ref v) : string
-#
-# _format(proto, ref v, int decimals) : string
-#
-# Formats the amount, rounded to the specified number of decimals.
-#
 sub _format {
     my($proto, $v, $decimals) = @_;
+    # Formats the amount, rounded to the specified number of decimals.
     $decimals = $proto->get_decimals
         unless defined($decimals);
     # add in a fudge factor for for values such as 0.07 which is represented
@@ -406,12 +234,9 @@ sub _format {
        GMP::Mpf::floor($v * $pow + $_HALF) / $pow);
 }
 
-# _mpf(string value) : ref
-#
-# Returns a GMP::Mpf value for the specified string value.
-#
 sub _mpf {
     my($value) = @_;
+    # Returns a GMP::Mpf value for the specified string value.
 
     unless (defined($value)) {
         Bivio::IO::Alert->warn_deprecated(
@@ -427,16 +252,5 @@ sub _mpf {
     # represented by Mpf (using 500 bits of precision)
     return GMP::Mpf::mpf($value . '', 500);
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999,2000 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
-
 
 1;
