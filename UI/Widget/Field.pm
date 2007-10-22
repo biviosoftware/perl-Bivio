fce3510dcd597203e9ea90434e80276057fb2ceb
Rob Nagler <github@q33.us>
2007-10-22 17:19:12 +0000

    *** empty log message ***

---
+++ UI/Widget/Field.pm
@@ -0,0 +1,37 @@
+# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::Widget::Field;
+use strict;
+use Bivio::Base 'Widget.ControlBase';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub control_on_render {
+    my($self, $source, $buffer) = @_;
+    my($field) = $self->render_simple_attr('field_name', $source);
+    my($to) = $self->render_simple_attr('to_method', $source);
+    $$buffer .= $source->get_field_type($field)->$to($source->get($field));
+    return;
+}
+
+sub initialize {
+    my($self) = @_;
+    $self->initialize_attr('field_name');
+    $self->initialize_attr(to_method => 'to_string');
+    return shift->SUPER::initialize(@_);
+}
+
+
+sub internal_as_string {
+    return shift->unsafe_get('field_name');
+}
+
+sub internal_new_args {
+    my(undef, $field_name, $attrs) = @_;
+    return {
+	field_name => $field_name,
+	($attrs ? %$attrs : ()),
+    };
+}
+
+1;
