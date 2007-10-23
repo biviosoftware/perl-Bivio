9fcfb9991c2a4f7d5cdd4357484b9435ced472f4
Rob Nagler <github@q33.us>
2007-10-23 20:15:29 +0000

    identify 3 standard locations

--- Delegate/SimpleLocation.pm
+++ Delegate/SimpleLocation.pm
@@ -1,72 +1,18 @@
-# Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2007 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Delegate::SimpleLocation;
 use strict;
-$Bivio::Delegate::SimpleLocation::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Delegate::SimpleLocation::VERSION;
+use Bivio::Base 'Bivio::Delegate';
 
-=head1 NAME
-
-Bivio::Delegate::SimpleLocation - implements Type.Location
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Delegate::SimpleLocation;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Delegate>
-
-=cut
-
-use Bivio::Delegate;
-@Bivio::Delegate::SimpleLocation::ISA = ('Bivio::Delegate');
-
-=head1 DESCRIPTION
-
-C<Bivio::Delegate::SimpleLocation> is the minimum you need for
-a Location type.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_delegate_info"></a>
-
-=head2 get_delegate_info() : array_ref
-
-Returns HOME.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_delegate_info {
+    # Returns HOME.
     return [
 	HOME => [1],
+	WORK => [2],
+	MOBILE => [3],
     ];
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
