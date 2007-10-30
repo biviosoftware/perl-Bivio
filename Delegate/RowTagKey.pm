3e4a50ec5beced802999d670013b9e85a44db83a
Rob Nagler <github@q33.us>
2007-10-30 20:37:38 +0000

    remove []

--- Delegate/RowTagKey.pm
+++ Delegate/RowTagKey.pm
@@ -8,10 +8,10 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_delegate_info {
     return [
-        UNKNOWN => [0],
-	ERROR_DETAIL => [1],
-	RELATED_ID => [2],
-	LAST_RESERVED_VALUE => [99],
+        UNKNOWN => 0,
+	ERROR_DETAIL => 1,
+	RELATED_ID => 2,
+	LAST_RESERVED_VALUE => 99,
     ];
 }
 
