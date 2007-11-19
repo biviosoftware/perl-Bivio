ce80eb0760d6444baf746c15b1a524545740278f
Rob Nagler <github@q33.us>
2007-11-19 18:21:13 +0000

    let SyntacticString do the work

--- Type/USZipCode9.pm
+++ Type/USZipCode9.pm
@@ -1,88 +1,27 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2007 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::USZipCode9;
 use strict;
-$Bivio::Type::USZipCode9::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::USZipCode9::VERSION;
+use Bivio::Base 'Type.USZipCode';
 
-=head1 NAME
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-Bivio::Type::USZipCode9 - must be 9 digit zip
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::USZipCode9;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::USZipCode>
-
-=cut
-
-use Bivio::Type::USZipCode;
-@Bivio::Type::USZipCode9::ISA = ('Bivio::Type::USZipCode');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::USZipCode9>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value) : array
-
-Ensures 9 digits.
-
-=cut
-
-sub from_literal {
-    my($v, $e) = shift->SUPER::from_literal(@_);
-    return ($v, $e)
-	unless $v;
-    return (undef, Bivio::TypeError->US_ZIP_CODE_9)
-	unless length($v) == 9;
-    return $v;
+sub TOO_SHORT_ERROR {
+    return Bivio::TypeError->US_ZIP_CODE_9;
 }
 
-=for html <a name="to_html"></a>
+sub get_min_width {
+    return 9;
+}
 
-=head2 static to_html(string value) : string
-
-Insert the '-'.
-
-=cut
+sub get_width {
+    return 9;
+}
 
 sub to_html {
     my(undef, $value) = @_;
     substr($value, 5, 0) = '-';
     return $value;
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
