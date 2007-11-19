ce17c6905dfc86e95cc386a37fdd193edf00d23b
Rob Nagler <github@q33.us>
2007-11-19 18:00:34 +0000

    defaults TYPE_ERROR

--- Type/SyntacticString.pm
+++ Type/SyntacticString.pm
@@ -6,13 +6,18 @@ use Bivio::Base 'Type.String';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub TYPE_ERROR {
+    return Bivio::TypeError->SYNTAX_ERROR;
+}
+
 sub from_literal {
     my($proto, $value) = @_;
-    $value = $proto->internal_pre_from_literal($value);
+    $value = $proto->internal_pre_from_literal($value)
+	if defined($value);
     my($v, $e) = $proto->SUPER::from_literal($value);
     return !defined($v) ? ($v, $e)
 	: $v =~ /^@{[$proto->REGEX]}$/ ? $proto->internal_post_from_literal($v)
-	: (undef, Bivio::TypeError->SYNTAX_ERROR);
+	: (undef, $proto->TYPE_ERROR);
 }
 
 sub get_width {
@@ -24,11 +29,9 @@ sub internal_post_from_literal {
 }
 
 sub internal_pre_from_literal {
-    my($proto, $value) = @_;
-    if (defined($value)) {
-	$value =~ s/^\s+|\s+$//g;
-	$value =~ s/\s+/ /g;
-    }
+    my(undef, $value) = @_;
+    $value =~ s/^\s+|\s+$//g;
+    $value =~ s/\s+/ /g;
     return $value;
 }
 
