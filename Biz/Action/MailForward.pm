ab109f6409ebb27c7855ff33cb0377bf96c5261e
Alex Viggio <aviggio@bivio.biz>
2007-12-12 22:24:21 +0000

    Refactored to use Outgoing

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -9,9 +9,9 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub execute {
     my(undef, $req) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
-    # Let sendmail handle mail loop detection (for now)
-    Bivio::Mail::Incoming->new($mr->get('message')->{content})
-        ->set_recipients($mr->get('recipient'), $req)->enqueue_send($req);
+    my($out) = Bivio::Mail::Outgoing->new($mr->get('message')->{content})
+	->set_recipients($mr->get('recipient'), $req)
+	    ->enqueue_send($req);
     return;
 }
 
