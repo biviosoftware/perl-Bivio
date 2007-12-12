242a4abe509951a5b94ecb05bdc30b98c9bad17d
Alex Viggio <aviggio@bivio.biz>
2007-12-12 21:35:27 +0000

    rmpod

--- Mail/Common.pm
+++ Mail/Common.pm
@@ -1,60 +1,17 @@
-# Copyright (c) 1999-2006 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2007 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Mail::Common;
 use strict;
-$Bivio::Mail::Common::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Mail::Common::VERSION;
-
-=head1 NAME
-
-Bivio::Mail::Common - utilities for mail modules
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Mail::Common;
-
-=cut
-
-use base 'Bivio::Collection::Attributes';
-
-=head1 DESCRIPTION
-
-C<Bivio::Mail::Common>
-
-=cut
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="TEST_RECIPIENT_HDR"></a>
-
-=head2 static TEST_RECIPIENT_HDR()
-
-Returns header where recipient is inserted into msg.  Only if
-$req.is_test.
-
-=cut
-
-sub TEST_RECIPIENT_HDR {
-    return 'X-Bivio-Test-Recipient';
-}
-
-#=IMPORTS
 use Bivio::Agent::Request;
+use Bivio::Base 'Bivio::Collection::Attributes';
 use Bivio::Die;
 use Bivio::IO::Config;
 use Bivio::IO::Trace;
 use IO::File ();
 use User::pwent ();
 
-#=VARIABLES
-use vars qw($_TRACE);
-Bivio::IO::Trace->register;
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($_TRACE);
 Bivio::IO::Config->register(my $_CFG = {
     errors_to => 'postmaster',
     # Deliver in background so errors are sent via e-mail
@@ -65,37 +22,25 @@ Bivio::IO::Config->register(my $_CFG = {
 #TODO: get rid of global state - put it on the request instead
 my($_IDI) = __PACKAGE__->instance_data_index;
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="enqueue_send"></a>
-
-=head2 enqueue_send(Bivio::Agent::Request req) : self
-
-Queues I<self> for sending on commit.
-
-=cut
+sub TEST_RECIPIENT_HDR {
+    # Returns header where recipient is inserted into msg.  Only if
+    # $req.is_test.
+    return 'X-Bivio-Test-Recipient';
+}
 
 sub enqueue_send {
     my($self, $req) = shift->internal_req(@_);
+    # Queues I<self> for sending on commit.
     $req->push_txn_resource($self);
     return $self;
 }
 
-=for html <a name="format_as_bounce"></a>
-
-=head2 static format_as_bounce(string err, string recipients, string_ref msg, string errors_to, Bivio::Agent::Request req) : string_ref
-
-Creates an error message to be sent to 'errors_to'.  I<recipients> will
-be retrieved with I<unsafe_get_recipients> if not supplied.
-I<msg> will be retrieved with I<as_string> if not supplied.
-I<errors_to> will be retrieved from I<errors_to> config if not supplied.
-
-=cut
-
 sub format_as_bounce {
     my($proto, $err, $recipients, $msg, $errors_to, $req) = @_;
+    # Creates an error message to be sent to 'errors_to'.  I<recipients> will
+    # be retrieved with I<unsafe_get_recipients> if not supplied.
+    # I<msg> will be retrieved with I<as_string> if not supplied.
+    # I<errors_to> will be retrieved from I<errors_to> config if not supplied.
     $msg ||= \($proto->as_string);
     $recipients ||= $proto->unsafe_get_recipients || '<>';
     my($u) = User::pwent::getpwuid($>);
@@ -117,83 +62,43 @@ $$msg
 EOF
 }
 
-=for html <a name="get_last_queued_message"></a>
-
-=head2 get_last_queued_message(Bivio::Agent::Request req) : Bivio::Mail::Common
-
-Return the last message queued.
-
-=cut
-
 sub get_last_queued_message {
     my($self, $req) = @_;
+    # Return the last message queued.
     return pop(@{[
         grep(UNIVERSAL::isa($_, $self), @{$req->get('txn_resources')})]});
 }
 
-
-=for html <a name="handle_commit"></a>
-
-=head2 handle_commit(Bivio::Agent::Request req)
-
-Send self.
-
-=cut
-
 sub handle_commit {
+    # Send self.
     shift->send(shift);
     return;
 }
 
-=for html <a name="handle_config"></a>
-
-=head2 static handle_config(hash cfg)
-
-=over 4
-
-=item errors_to : string [postmaster]
-
-To whom should errors be sent.
-
-=item sendmail : string [/usr/lib/sendmail -O DeliveryMode=b -i]
-
-How to send mail.  Must accept a list of recipients on the
-command line.  Arguments must be easily separated, i.e. no quoting.
-
-=back
-
-=cut
-
 sub handle_config {
     my(undef, $cfg) = @_;
+    # errors_to : string [postmaster]
+    #
+    # To whom should errors be sent.
+    #
+    # sendmail : string [/usr/lib/sendmail -O DeliveryMode=b -i]
+    #
+    # How to send mail.  Must accept a list of recipients on the
+    # command line.  Arguments must be easily separated, i.e. no quoting.
     Bivio::Die->die($cfg->{errors_to}, ': invalid errors_to')
         if $cfg->{errors_to} =~ /['\\]/;
     $_CFG = $cfg;
     return;
 }
 
-=for html <a name="handle_rollback"></a>
-
-=head2 handle_rollback()
-
-Do nothing.
-
-=cut
-
 sub handle_rollback {
+    # Do nothing.
     return;
 }
 
-=for html <a name="internal_req"></a>
-
-=head2 internal_req(Bivio::Agent::Request req) : Bivio::Agent::Request
-
-Returns request.  Warns deprecated if I<req> not supplied
-
-=cut
-
 sub internal_req {
     my($self, $req) = @_;
+    # Returns request.  Warns deprecated if I<req> not supplied
     return (
 	$self,
 	$req ? $req : (
@@ -202,25 +107,14 @@ sub internal_req {
     ));
 }
 
-=for html <a name="send"></a>
-
-=head2 static send(string or array_ref recipients, string msg) : self
-
-=head2 static send(string or array_ref recipients, string_ref msg, int offset, Bivio::Agent::Request req) : self
-
-=head2 static send(string or array_ref recipients, string_ref msg, int offset, string from, Bivio::Agent::Request req) : self
-
-Sends a message via configured C<sendmail> program.  Errors are
-mailed back to configured C<errors_to>--except if no I<recipients>
-or no I<msg> iwc an exception is raised.
-
-Bounces are sent back to $from. $from is the envelope FROM, ie.
-the -f argument given to sendmail.
-
-=cut
-
 sub send {
     my($self, $recipients, $msg, $offset, $from, $req) = @_;
+    # Sends a message via configured C<sendmail> program.  Errors are
+    # mailed back to configured C<errors_to>--except if no I<recipients>
+    # or no I<msg> iwc an exception is raised.
+    #
+    # Bounces are sent back to $from. $from is the envelope FROM, ie.
+    # the -f argument given to sendmail.
     $recipients ||= $self->unsafe_get_recipients
 	|| Bivio::Die->die('no recipients');
     $recipients = join(',', @$recipients)
@@ -253,21 +147,12 @@ sub send {
     return $self;
 }
 
-=for html <a name="set_recipients"></a>
-
-=head2 set_recipients(string email_list) : self
-
-=head2 set_recipients(array_ref email_list) : self
-
-Sets the recipient of this mail message.  It does not modify the
-headers, i.e. To:, etc.  I<email_list> may be a single scalar
-containing multiple addresses (separated by commas)
-or an array whose elements may contain scalar lists.
-
-=cut
-
 sub set_recipients {
     my($self, $email_list) = @_;
+    # Sets the recipient of this mail message.  It does not modify the
+    # headers, i.e. To:, etc.  I<email_list> may be a single scalar
+    # containing multiple addresses (separated by commas)
+    # or an array whose elements may contain scalar lists.
     return $self->put(recipients => join(
 	',',
 	map(@{Bivio::Mail::Address->parse_list($_)},
@@ -276,41 +161,21 @@ sub set_recipients {
     ));
 }
 
-=for html <a name="unsafe_get_recipients"></a>
-
-=head2 unsafe_get_recipients() : string
-
-Returns recipients.
-
-=cut
-
 sub unsafe_get_recipients {
+    # Returns recipients.
     return shift->unsafe_get('recipients');
 }
 
-
-=for html <a name="user_email"></a>
-
-=head2 user_email(Bivio::Agent::Request req) : array
-
-Returns ($email, $name)
-
-=cut
-
 sub user_email {
     my(undef, $req) = @_;
+    # Returns ($email, $name)
     my($name) = getpwuid($>) || 'intruder';
     return ($req->format_email($name), $name);
 }
 
-#=PRIVATE METHODS
-
-# _send(proto, string recipients, string_ref msg, int offset, string from) : string
-#
-# Attempts to send the message. Returns an error string on failure.
-#
 sub _send {
     my($proto, $recipients, $msg, $offset, $from, $req) = @_;
+    # Attempts to send the message. Returns an error string on failure.
     _trace('sending to ', $recipients) if $_TRACE;
     if ($req->is_test) {
 	return grep(
@@ -336,15 +201,5 @@ sub _send {
     Bivio::IO::Alert->warn($die ? $die->as_string : "$command: status = $?");
     return 'I/O error';
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2006 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
