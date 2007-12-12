e8122090deb59a63721e9dba8339c8bdf4e26569
Rob Nagler <github@q33.us>
2007-12-12 04:06:46 +0000

    most calls now support regexes in place of string values (column_name, table_name, etc.)

--- Test/HTMLParser/Tables.pm
+++ Test/HTMLParser/Tables.pm
@@ -45,11 +45,15 @@ sub find_row {
     #
     # If I<table_name> not supplied, calls L<get_by_headings|"get_by_headings"> with
     # I<column_name> for table.
-    my($table_name) = @_ > 2 ? shift
-	: $self->get_by_headings($_[0])->{headings}->[0]->get('text');
+    my($table_name) = shift
+	if @_ > 2;
     my($column_name, $column_value) = @_;
+    $table_name = $self->get_by_headings(
+	defined($table_name) ? $table_name : $column_name,
+    )->{headings}->[0]->get('text')
+	if !defined($table_name) || ref($table_name);
     my($found_row);
-    _assert_column($self, $table_name, $column_name);
+    $column_name = _assert_column($self, $table_name, $column_name);
     my($misses) = [];
     $self->do_rows($table_name,
 	sub {
@@ -59,14 +63,14 @@ sub find_row {
 	    my($t) = $row->{$column_name}->get('text');
 	    push(@$misses, $t);
 	    $found_row = $row
-		if ref($column_value) eq 'Regexp' ? $t =~ $column_value
-		    : $t eq $column_value;
+		if _eq($column_value, $t);
 	    return $found_row ? 0 : 1;
 	});
-    return $found_row || Bivio::Die->die(
+    Bivio::Die->die(
 	$column_value, ': not found in column "', $column_name,
 	'" values: ', $misses,
-    );
+    ) unless $found_row;
+    return wantarray ? ($found_row, $column_name) : $found_row;
 }
 
 sub get_by_headings {
@@ -77,7 +81,7 @@ sub get_by_headings {
  TABLE: while (my($table, $values) = each(%$tables)) {
 	foreach my $n (@name) {
 	    next TABLE
-		unless grep($n eq $_->get('text'), @{$values->{headings}});
+		unless grep(_eq($n, $_->get('text')), @{$values->{headings}});
 	}
 	Bivio::Die->die(\@name, ': too many tables matched headings')
 	    if $found;
@@ -130,15 +134,18 @@ sub new {
 sub _assert_column {
     my($self, $table_name, $column_name) = @_;
     my($table) = _assert_table($self, $table_name);
-    Bivio::Die->die('column name not found: ', $column_name)
-        unless grep($column_name eq $_->get('text'), @{$table->{headings}});
-    return;
+    my(@match) = grep(_eq($column_name, $_->get('text')), @{$table->{headings}});
+    Bivio::Die->die($column_name, ': column name not found')
+        unless @match;
+    Bivio::Die->die(\@match, ': too many columns found for ', $column_name)
+#TODO: There's an odd test case which requires this $column_name condition
+        if $column_name && @match > 1;
+    return $match[0]->get('text');
 }
 
 sub _assert_table {
     my($self, $table_name) = @_;
-    Bivio::Die->die('table name: "', $table_name,
-        '" not found in ', $self->get_keys)
+    Bivio::Die->die($table_name, ': table not found; tables=', $self->get_keys)
         unless $self->unsafe_get($table_name);
     return $self->get($table_name);
 }
@@ -172,8 +179,8 @@ sub _end_table {
     return unless $fields->{in_data_table} && !--$fields->{in_data_table};
     # Delete totally empty rows (probably separators)
     _delete_empty_rows($fields->{table}->{rows});
-
-    my($elements) = $self->get('elements');
+ 
+   my($elements) = $self->get('elements');
     my($name) = $fields->{table}->{label} ||= '_anon#'
         . keys(%{$self->get('elements')});
 
@@ -209,6 +216,11 @@ sub _end_th {
     my($t) = _save_cell($self, $fields, $fields->{table}->{headings});
     $fields->{table}->{label} ||= $t;
     return;
+}
+
+sub _eq {
+    my($expect, $actual) = @_;
+    return ref($expect) ? $actual =~ $expect : $actual eq $expect;
 }
 
 sub _found_table {
