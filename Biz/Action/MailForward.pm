609ecc052d6f718d60d601cd105f9251085dae21
Rob Nagler <github@q33.us>
2007-12-13 04:52:18 +0000

    Was not working, because Mail.Outgoing->new does not take content as a param

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -9,9 +9,10 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub execute {
     my(undef, $req) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
-    my($out) = Bivio::Mail::Outgoing->new($mr->get('message')->{content})
-	->set_recipients($mr->get('recipient'), $req)
-	    ->enqueue_send($req);
+    # Let sendmail handle mail loop detection (for now)
+    Bivio::Mail::Outgoing->new(
+	Bivio::Mail::Incoming->new($mr->get('message')->{content}),
+    )->set_recipients($mr->get('recipient'), $req)->enqueue_send($req);
     return;
 }
 
