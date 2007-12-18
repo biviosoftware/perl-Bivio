8aa501c26a0b98eb334eecafb049e1f2bfd54966
Rob Nagler <github@q33.us>
2007-12-18 22:49:24 +0000

    create Test.Request

--- Test/ShellUtil.pm
+++ Test/ShellUtil.pm
@@ -6,6 +6,13 @@ use Bivio::Base 'TestUnit.Unit';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub new_unit {
+    return shift->call_super_before(\@_, sub {
+        shift->use('TestUnit.Request')->get_instance;
+	return;
+    });
+}
+
 sub unit {
     my($self, $cases) = @_;
     return $self->SUPER::unit([
