04e14e653eebb8b13ecf9d91a8e615f7dcfffa3c
Rob Nagler <github@q33.us>
2007-12-18 22:52:17 +0000

    added from_string_ref() rmpod

--- Type/FileField.pm
+++ Type/FileField.pm
@@ -3,24 +3,17 @@
 package Bivio::Type::FileField;
 use strict;
 use Bivio::Base 'Bivio::Type';
-use Bivio::Type::Text;
-
-# C<Bivio::Type::FileField> is a hash_ref.  The attributes are:
-#
-#
-# content
-#
-# A scalar_ref pointing to the content.
-#
-# content_type
-#
-# The I<content-type> string in the input.
-#
-# filename
-#
-# Name supplied in the I<content-disposition> attribute.
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_T) = __PACKAGE__->use('Type.Text');
+
+sub from_string_ref {
+    return shift->from_literal({
+	content => shift,
+	filename => shift || '',
+	content_type => shift || 'application/octet-stream',
+    });
+}
 
 sub from_disk {
     my($v, $e) = shift->unsafe_from_disk(@_);
@@ -35,57 +28,43 @@ sub from_disk {
 
 sub from_literal {
     my($proto, $value) = @_;
-    # Checks the incoming file for valid values.
     $proto->internal_from_literal_warning
         unless wantarray;
-    return (undef, undef) unless defined($value);
-
+    return (undef, undef)
+	unless defined($value);
     unless (ref($value) eq 'HASH') {
-	# url-encoded form?
-	return (undef, Bivio::TypeError::FILE_FIELD()) if length($value);
-
-	# NULL field
+	return (undef, Bivio::TypeError->FILE_FIELD)
+	    if length($value);
 	return (undef, undef);
     }
-
-    return $value if length(${$value->{content}});
-
-    # NULL if no filename
+    return $value
+	if length(${$value->{content}});
     return (undef, undef)
-	    unless $value->{filename};
-
-    # NOT_FOUND if no content_type
-    return (undef, Bivio::TypeError::NOT_FOUND())
-	    unless $value->{content_type};
-
-    # Else zero length file
-    return (undef, Bivio::TypeError::EMPTY());
+	unless $value->{filename};
+    return (undef, Bivio::TypeError->NOT_FOUND)
+	unless $value->{content_type};
+    return (undef, Bivio::TypeError->EMPTY);
 }
 
 sub from_sql_column {
-    # B<NOT SUPPORTED>
     die("can't convert a FileField from sql");
 }
 
 sub get_width {
-    # Returns same as Text.
-    return Bivio::Type::Text->get_width;
+    return $_T->get_width;
 }
 
 sub to_literal {
     my(undef, $value) = @_;
-    # Returns the filename, not the content.
     return ref($value) eq 'HASH' && defined($value->{filename})
-	    ? $value->{filename} : '';
+	? $value->{filename} : '';
 }
 
 sub to_query {
-    # B<NOT SUPPORTED>
     die("can't convert a FileField to a query");
 }
 
 sub to_uri {
-    # B<NOT SUPPORTED>
     die("can't convert a FileField to a uri");
 }
 
