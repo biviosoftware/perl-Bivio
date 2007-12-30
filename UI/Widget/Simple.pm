32140ec66a3c0bf74bb49772c46a088a5afab5fa
Rob Nagler <github@q33.us>
2007-12-30 20:00:47 +0000

    rmpod

--- UI/Widget/Simple.pm
+++ UI/Widget/Simple.pm
@@ -2,77 +2,13 @@
 # $Id$
 package Bivio::UI::Widget::Simple;
 use strict;
-$Bivio::UI::Widget::Simple::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::Widget::Simple::VERSION;
+use Bivio::Base 'UI.Widget';
 
-=head1 NAME
-
-Bivio::UI::Widget::Simple - executes sub-widget
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::Widget::Simple;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::Widget::Simple::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::Widget::Simple> is a usuable base class for simple widgets.
-Can serve as a placeholder.
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item value : any (required)
-
-Is a or returns a widget to render.
-
-=back
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes I<value>.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub initialize {
     return shift->initialize_attr('value');
 }
-
-=for html <a name="internal_new_args"></a>
-
-=head2 static internal_new_args(any value, hash_ref attributes) : hash_ref
-
-Implements positional argument parsing for L<new|"new">.  I<attributes> is
-optional.
-
-=cut
 
 sub internal_new_args {
     my(undef, $value, $attributes) = @_;
@@ -84,28 +20,8 @@ sub internal_new_args {
     };
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Renders I<value>.
-
-=cut
-
 sub render {
     return shift->render_attr('value', @_);
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
