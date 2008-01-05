1244e1ff9afee9177d9e5ac5a6a546f37f1448d4
Rob Nagler <github@q33.us>
2008-01-05 17:45:19 +0000

    error indicator is a string from facade now

--- UI/XHTML/Widget/FormFieldLabel.pm
+++ UI/XHTML/Widget/FormFieldLabel.pm
@@ -2,10 +2,11 @@
 # $Id$
 package Bivio::UI::XHTML::Widget::FormFieldLabel;
 use strict;
-use base 'Bivio::UI::Widget::Prose';
-use Bivio::UI::HTML::Widget::ControlBase;
+use Bivio::Base 'XHTMLWidget.Prose';
+use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_CB) = __PACKAGE__->use('XHTMLWidget.ControlBase');
 
 sub internal_as_string {
     my($self) = @_;
@@ -14,23 +15,25 @@ sub internal_as_string {
 
 sub internal_new_args {
     shift;
-    return Bivio::UI::HTML::Widget::ControlBase->internal_compute_new_args(
-	[qw(field label)],
-	\@_,
-    );
+    return $_CB->internal_compute_new_args([qw(field label)], \@_);
 }
 
 sub initialize {
     my($self) = @_;
     return if $self->unsafe_get('value');
+    my($class) = $self->ancestral_get('form_class');
+    my($field) = $self->ancestral_get('field');
     $self->put(
-	value => $self->get('label'),
-	cell_class => [sub {$_[1] ? 'label_err' : 'label_ok'}, [
-	    ['->get_request'],
-	    $self->ancestral_get('form_class'),
-	    '->get_field_error',
-	    $self->get('field'),
-	]],
+	value => Join([
+	    If([['->req', $class], '->get_field_error', $field],
+	       vs_text(
+		   $class->simple_package_name, 'prose', 'error_indicator')),
+	    $self->get('label'),
+	]),
+	cell_class => [
+	    sub {$_[1] ? 'label_err' : 'label_ok'},
+	    [['->req', $class], '->get_field_error', $field],
+	],
     );
     return shift->SUPER::initialize(@_);
 }
