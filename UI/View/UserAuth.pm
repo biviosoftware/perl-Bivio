f446327400b48b5b5c8dd4b74ca4d6a5965e76c6
Rob Nagler <github@q33.us>
2008-01-06 23:38:00 +0000

    added general_contact_mail

--- UI/View/UserAuth.pm
+++ UI/View/UserAuth.pm
@@ -13,6 +13,21 @@ sub adm_substitute_user {
     }]));
 }
 
+sub general_contact_mail {
+    return shift->internal_put_base_attr(
+	from => ['Model.ContactForm', 'from'],
+	to => Join([
+	    Mailbox(
+		vs_text('support_email'),
+		vs_text_as_prose('support_name'),
+	    ),
+	    ['Model.ContactForm', 'from'],
+	], ','),
+	subject => 'Web Contact',
+	body => ['Model.ContactForm', 'text'],
+    );
+}
+
 sub general_contact {
     return shift->internal_body(vs_simple_form(ContactForm => [qw(
         ContactForm.from
