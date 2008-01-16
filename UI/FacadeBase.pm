a51899a8f7ae5cf3358ecff20fa48d6452101c83
Rob Nagler <github@q33.us>
2008-01-16 23:09:41 +0000

    fpc: not sure why can't say [qw(login email)]

--- UI/FacadeBase.pm
+++ UI/FacadeBase.pm
@@ -189,7 +189,7 @@ sub _cfg_base {
 	    ]],
 	    [ok_button => '   OK   '],
 	    [cancel_button => ' Cancel '],
-	    [[qw(email login)] => 'Email'],
+	    [[qw(Email.email login email)] => 'Email'],
 	    [password => 'Password'],
 	    [confirm_password => 'Re-enter Password'],
 	    [display_name => 'Your Full Name'],
