ed276c58b5a968f35fc2b7ea6208705f4b3091bf
Rob Nagler <github@q33.us>
2008-01-16 14:01:57 +0000

    rmpod

--- Biz/Model/NumberedList.pm
+++ Biz/Model/NumberedList.pm
@@ -2,70 +2,13 @@
 # $Id$
 package Bivio::Biz::Model::NumberedList;
 use strict;
-$Bivio::Biz::Model::NumberedList::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+use Bivio::Base 'Biz.ListModel';
 
-=head1 NAME
-
-Bivio::Biz::Model::NumberedList - for list forms which want to display blank fields
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Biz::Model::NumberedList;
-    Bivio::Biz::Model::NumberedList->new();
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Biz::ListModel>
-
-=cut
-
-use Bivio::Biz::ListModel;
-@Bivio::Biz::Model::NumberedList::ISA = ('Bivio::Biz::ListModel');
-
-=head1 DESCRIPTION
-
-C<Bivio::Biz::Model::NumberedList>
-
-=cut
-
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="PAGE_SIZE"></a>
-
-=head2 PAGE_SIZE : int
-
-Returns a size that is reasonable for a list form.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub PAGE_SIZE {
     return 10;
 }
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="internal_initialize"></a>
-
-=head2 internal_initialize() : hash_ref
-
-B<FOR INTERNAL USE ONLY>
-
-=cut
 
 sub internal_initialize {
     return {
@@ -80,29 +23,9 @@ sub internal_initialize {
     };
 }
 
-=for html <a name="internal_load_rows"></a>
-
-=head2 internal_load_rows(Bivio::SQL::ListQuery query, string where, array_ref params, Bivio::SQL::ListSupport sql_support) : array_ref
-
-Loads a list of I<count> numbers.
-
-=cut
-
 sub internal_load_rows {
     my($self, $query, $where, $params, $sql_support) = @_;
     return [map(+{index => $_}, 0..($query->get('count')-1))];
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2000-2006 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
