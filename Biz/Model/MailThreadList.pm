b50be66f09373355d6d8629b9ecec5bf49c6579c
Rob Nagler <github@q33.us>
2008-01-29 18:47:10 +0000

    added get_subject, get_mail_part_list & NOT_FOUND_IF_EMPTY removed unused fields can_iterate => 1

--- Biz/Model/MailThreadList.pm
+++ Biz/Model/MailThreadList.pm
@@ -1,39 +1,46 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::MailThreadList;
 use strict;
-use base 'Bivio::Biz::ListModel';
+use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub NOT_FOUND_IF_EMPTY {
+    return 1;
+}
+
+sub get_mail_part_list {
+    my($self) = @_;
+    return $self->new_other('MailPartList')->load_all({
+	parent_id => $self->get('RealmMail.realm_file_id'),
+    });
+}
+
+sub get_subject {
+    my($self) = @_;
+    $self->set_cursor_or_die(0)
+	unless $self->has_cursor;
+    return $self->get('RealmMail.subject');
+}
 
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
+	can_iterate => 1,
         primary_key => [[qw(RealmMail.realm_file_id RealmFile.realm_file_id)]],
 	order_by => [qw(
             RealmFile.modified_date_time
 	    RealmMail.from_email
+            RealmMail.subject_lc
 	)],
 	other => [
-	    {
-		name => 'part_list',
-		type => $self->get_instance('MailPartList')->package_name,
-		in_list => 1,
-		constraint => 'NONE',
-	    },
+            'RealmMail.subject',
 	],
 	auth_id => [qw(RealmMail.realm_id RealmFile.realm_id)],
 	parent_id => 'RealmMail.thread_root_id',
     });
-}
-
-sub internal_post_load_row {
-    my($self, $row) = @_;
-    $row->{part_list} = $self->new_other('MailPartList')->load_all({
-	parent_id => $row->{'RealmMail.realm_file_id'},
-    });
-    return 1;
 }
 
 1;
