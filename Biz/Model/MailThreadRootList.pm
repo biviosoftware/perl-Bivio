ed2a8265792bedae42c4eeb289aa03fd3d129b4f
Rob Nagler <github@q33.us>
2008-01-29 18:46:01 +0000

    *** empty log message ***

---
+++ Biz/Model/MailThreadRootList.pm
@@ -0,0 +1,29 @@
+# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::MailThreadRootList;
+use strict;
+use Bivio::Base 'Model.MailThreadList';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub drilldown_uri {
+    my($self) = @_;
+    my($req) = $self->req;
+    return $req->format_uri({
+	task_id => $req->get_nested(qw(task thread_task)),
+	query => $self->format_query('THIS_CHILD_LIST'),
+    });
+}
+
+sub internal_initialize {
+    my($self) = @_;
+    my($info) = $self->SUPER::internal_initialize;
+    delete($info->{parent_id});
+    return $self->merge_initialize_info($info, {
+	other => [
+	    ['RealmMail.thread_parent_id', [undef]],
+	],
+    });
+}
+
+1;
---
+++ UI/XHTML/Widget/MailHeader.pm
@@ -0,0 +1,41 @@
+# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::XHTML::Widget::MailHeader;
+use strict;
+use Bivio::Base 'XHTMLWidget.Join';
+use Bivio::UI::ViewLanguageAUTOLOAD;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub initialize {
+    my($self) = @_;
+    $self->initialize_attr(source => [sub {shift}]);
+    $self->put_unless_exists(values => [
+	map(
+	    DIV_header(
+		Join([
+		    SPAN_label(vs_text_as_prose("MailHeader.$_")),
+		    SPAN_value(String(['->get_header', $_])),
+		]),
+		{
+		    control => [
+			sub {$_[1] =~ /\S/ ? 1 : 0},
+			['->get_header', $_],
+		    ],
+		},
+	    ),
+	    qw(from to cc date subject),
+	),
+    ]);
+    return shift->SUPER::initialize(@_);
+}
+
+sub internal_new_args {
+    my(undef, $source, $attributes) = @_;
+    return {
+	source => $source,
+	($attributes ? %$attributes : ()),
+    };
+}
+
+1;
