9e484e522b2b4169bb65565acd157a82fc869e81
Rob Nagler <github@q33.us>
2008-01-30 22:40:24 +0000

    rmpod

--- UI/HTML/Widget/MailTo.pm
+++ UI/HTML/Widget/MailTo.pm
@@ -1,121 +1,52 @@
-# Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::UI::HTML::Widget::MailTo;
 use strict;
-$Bivio::UI::HTML::Widget::MailTo::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::MailTo::VERSION;
-
-=head1 NAME
-
-Bivio::UI::HTML::Widget::MailTo - render an email address as a link
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::MailTo;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::HTML::Widget::MailTo::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::MailTo> displays an email as a
-C<mailto> link.
-
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item email : array_ref (required)
-
-Text to render for the mailto.
-
-=item email : string (required)
-
-Literal to render for the mailto.  The email address need not
-have the host suffix.  It will be appended.
-
-=item value : array_ref [I<email>]
-
-Text to render.  If same as I<email>, nothing will be rendered
-if email is invalid.
-
-=item value_invalid : string []
-
-What to display if the value is invalid.
-
-=item subject : array_ref []
-
-Text to use for subject of mailto.
-
-=item string_font : any [mailto]
-
-Used to render value.
-
-=item want_link : boolean [1]
-
-By default, render as a link.  Otherwise, just render the email address.
-
-=back
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'UI.Widget';
 use Bivio::Type::Email;
 
-#=VARIABLES
+# C<Bivio::UI::HTML::Widget::MailTo> displays an email as a
+# C<mailto> link.
+#
+#
+#
+#
+# email : array_ref (required)
+#
+# Text to render for the mailto.
+#
+# email : string (required)
+#
+# Literal to render for the mailto.  The email address need not
+# have the host suffix.  It will be appended.
+#
+# value : array_ref [I<email>]
+#
+# Text to render.  If same as I<email>, nothing will be rendered
+# if email is invalid.
+#
+# value_invalid : string []
+#
+# What to display if the value is invalid.
+#
+# subject : array_ref []
+#
+# Text to use for subject of mailto.
+#
+# string_font : any [mailto]
+#
+# Used to render value.
+#
+# want_link : boolean [1]
+#
+# By default, render as a link.  Otherwise, just render the email address.
 
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(any email, any value, any subject, hash_ref attrs) : Bivio::UI::HTML::Widget::MailTo
-
-Create an MailTo widget using I<email>, I<value>, and I<subject>.
-I<email> is the only required attribute.
-
-=head2 static new(hash_ref attributes) : Bivio::UI::HTML::Widget::MailTo
-
-Create an MailTo widget using I<attributes>.
-
-=cut
-
-sub new {
-    my($proto, @args) = _new_args(@_);
-    my($self) = $proto->SUPER::new(@args);
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes from configuration attributes.
-
-=cut
-
 sub initialize {
+    # (self) : undef
+    # Initializes from configuration attributes.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     return if $fields->{email};
@@ -163,15 +94,23 @@ sub initialize {
     return;
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Draws the file field on the specified buffer.
-
-=cut
+sub new {
+    # (proto, any, any, any, hash_ref) : Widget.MailTo
+    # (proto, hash_ref) : Widget.MailTo
+    # Create an MailTo widget using I<email>, I<value>, and I<subject>.
+    # I<email> is the only required attribute.
+    #
+    #
+    # Create an MailTo widget using I<attributes>.
+    my($proto, @args) = _new_args(@_);
+    my($self) = $proto->SUPER::new(@args);
+    $self->[$_IDI] = {};
+    return $self;
+}
 
 sub render {
+    # (self, any, string_ref) : undef
+    # Draws the file field on the specified buffer.
     my($self, $source, $buffer) = @_;
     my($fields) = $self->[$_IDI];
     my($req) = $source->get_request;
@@ -212,13 +151,9 @@ sub render {
     return;
 }
 
-#=PRIVATE METHODS
-
-# _new_args(proto, any arg, ...) : array
-#
-# Returns arguments to be passed to Attributes::new.
-#
 sub _new_args {
+    # (proto, any, ...) : array
+    # Returns arguments to be passed to Attributes::new.
     my($proto, $email, $value, $subject, $attrs) = @_;
     return ($proto, $email) if ref($email) eq 'HASH' || int(@_) == 1;
     return ($proto, {
@@ -230,15 +165,5 @@ sub _new_args {
     $proto->die(undef, undef, 'invalid arguments to new');
     # DOES NOT RETURN
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
