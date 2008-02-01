7121d835a19082a83f357a569de32afdaba7cf6a
Rob Nagler <github@q33.us>
2008-02-01 00:16:45 +0000

    use RFC822->format_mailbox

--- UI/Mail/Widget/Mailbox.pm
+++ UI/Mail/Widget/Mailbox.pm
@@ -4,23 +4,6 @@ package Bivio::UI::Mail::Widget::Mailbox;
 use strict;
 use Bivio::Base 'UI.Widget';
 
-# C<Bivio::UI::Mail::Widget::Mailbox> is a single RFC822 mailbox email
-# address.  Groups and multiple addresses are not supported.
-#
-#
-#
-# email : any (required)
-#
-# Email address to render.  See
-# L<Bivio::UI::Widget::render_attr|Bivio::UI::Widget/"render_attr">
-# for allowed attribute types.
-#
-# name : any []
-#
-# Email address to render.  See
-# L<Bivio::UI::Widget::render_attr|Bivio::UI::Widget/"render_attr">
-# for allowed attribute types.
-
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RFC) = __PACKAGE__->use('Mail.RFC822');
 
@@ -33,8 +16,6 @@ sub initialize {
 
 sub internal_new_args {
     my(undef, $email, $name, $attrs) = @_;
-    return '"email" attribute must be defined'
-	unless $email;
     return {
 	email => $email,
 	(defined($name) ? (name => $name) : ()),
@@ -44,12 +25,10 @@ sub internal_new_args {
 
 sub render {
     my($self, $source, $buffer) = @_;
-    my($b) = '';
-    my($e) = $self->get_request->format_email(
-	${$self->render_attr('email', $source)});
-    $$buffer .= $self->unsafe_render_attr('name',  $source, \$b)
-	    ? $_RFC->escape_header_phrase($b) . " <$e>"
-	    : $e;
+    $$buffer .= $_RFC->format_mailbox(
+	$self->req->format_email(${$self->render_attr('email', $source)}),
+	$self->render_simple_attr('name',  $source),
+    );
     return;
 }
 
