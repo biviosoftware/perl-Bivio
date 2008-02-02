9b3c7d99e214006fed0dd3d7f29c815d936cdc5d
Rob Nagler <github@q33.us>
2008-02-02 00:11:43 +0000

    rmpod

--- Type/TextArea.pm
+++ Type/TextArea.pm
@@ -1,75 +1,21 @@
-# Copyright (c) 2000 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2000-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::TextArea;
 use strict;
-$Bivio::Type::TextArea::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::TextArea::VERSION;
+use Bivio::Base 'Type.Text';
 
-=head1 NAME
+# C<Bivio::Type::TextArea> same as L<Bivio::Type::Text|Bivio::Type::Text>
+# except 64K characters. This is a typical limit for HTML TEXTAREAs.
 
-Bivio::Type::TextArea - very long version of Text used for HTML areas
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::TextArea;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Text>
-
-=cut
-
-use Bivio::Type::Text;
-@Bivio::Type::TextArea::ISA = ('Bivio::Type::Text');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::TextArea> same as L<Bivio::Type::Text|Bivio::Type::Text>
-except 64K characters. This is a typical limit for HTML TEXTAREAs.
-
-=cut
-
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="LINE_WIDTH"></a>
-
-=head2 LINE_WIDTH : string
-
-Return default line width
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub LINE_WIDTH {
     return 60;
 }
 
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value, int line_width) : any
-
-Wrap text lines at I<line_width> if desired by looking
-up the user's preference.
-
-=cut
-
 sub from_literal {
+    # Wrap text lines at I<line_width> if desired by looking
+    # up the user's preference.
     my($proto, $value, $line_width) = @_;
     $proto->internal_from_literal_warning
         unless wantarray;
@@ -86,28 +32,8 @@ sub from_literal {
     return $proto->SUPER::from_literal($value);
 }
 
-=for html <a name="get_width"></a>
-
-=head2 static get_width : int
-
-Returns 64K.
-
-=cut
-
 sub get_width {
     return 64*1024;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2000 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
