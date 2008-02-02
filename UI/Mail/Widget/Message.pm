620d475953fb63a5ad34803bed7e94170dcf5839
Rob Nagler <github@q33.us>
2008-02-02 00:12:11 +0000

    added headers_object to allow arbitrary addition of headers

--- UI/Mail/Widget/Message.pm
+++ UI/Mail/Widget/Message.pm
@@ -1,75 +1,26 @@
-# Copyright (c) 2001-2007 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2001-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::UI::Mail::Widget::Message;
 use strict;
-use Bivio::Base 'Bivio::UI::Widget';
-use Bivio::Die;
-use Bivio::IO::Log;
-use Bivio::Mail::Address;
-use Bivio::Mail::Outgoing;
+use Bivio::Base 'UI.Widget';
 
-# C<Bivio::UI::Mail::Widget::Message> creates and enqueues a plain text
-# mail message.  Eventually, this widget will be expanded to support
-# attachments and other content types.
-#
-# See L<Bivio::Mail::Outgoing|Bivio::Mail::Outgoing>.
-#
-#
-# All attributes are rendered identically.  They may be widget values,
-# constants, widgets, or widget values which return widgets.
-#
-#
-# body : any []
-#
-# The body of the message.
-#
-# cc : any []
-#
-# The Cc: address(es) in the header.  See I<recipients> for
-# the actual send-to addresses.
-#
-# from : any (required)
-#
-# The From: address in the header.
-#
-# headers : any []
-#
-# I<Deprecated>
-# Any additional headers.  Returns a string in RFC 822 header format.  Each
-# header appears on its own line.
-#
-# log_file : any []
-#
-# Where to log the message, if defined.  Calls
-# L<Bivio::IO::Log::write|Bivio::IO::Log/"write"> with the formatted
-# message.
-#
-# recipients : any (required)
-#
-# The recipients is a string of addresses separated by a comma.
-# Use a L<Bivio::UI::Widget::Join|Bivio::UI::Widget::Join> with
-# a comma separator if you have more than one address.
-#
-# subject : any []
-#
-# The Subject: address in the header.
-#
-# to : any []
-#
-# The To: address(es) in the header.  See I<recipients> for
-# the actual send-to addresses.
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_O) = __PACKAGE__->use('Mail.Outgoing');
+my($_A) = __PACKAGE__->use('Mail.Address');
+my($_L) = __PACKAGE__->use('IO.Log');
 
 sub execute {
     my($self, $req) = @_;
     $self->map_invoke(obsolete_attr => [qw(headers want_aol_munge)]);
-    my($msg) = Bivio::Mail::Outgoing->new;
+    my($msg) = $_O->new;
     my($from) = $self->render_simple_attr('from', $req);
     $msg->set_header('From', $from);
-    my($email) = (Bivio::Mail::Address->parse($from))[0]
-	|| $self->die('from', $req, 'no email in From: ', $from);
+    $self->die('from', $req, 'no email in From: ', $from)
+	unless my $email = ($_A->parse($from))[0];
     $msg->set_envelope_from($email);
     my($recips) = [];
+    $msg->set_header('X-Originating-IP', $req->get('client_addr'))
+	if $req->has_keys('client_addr');
     foreach my $header (qw(to cc subject)) {
 	my($value) = '';
 	if ($self->unsafe_render_attr($header, $req, \$value) && $value) {
@@ -79,19 +30,24 @@ sub execute {
 	}
     }
     $msg->set_recipients(
-	$self->render_simple_attr('recipients', $req) || $recips, $req);
-    $msg->set_header('X-Originating-IP', $req->get('client_addr'))
-	if $req->has_keys('client_addr');
+	$self->render_simple_attr('recipients', $req) || $recips,
+	$req,
+    );
     # Body must be rendered first in case the widget has mail headers.
-    my($body) = $self->render_attr('body', $req);
-    if (my $w = $self->unsafe_resolve_widget_value($self->get('body'), $req)) {
-	$msg->map_invoke(set_header => $w->mail_headers($req))
-	    if UNIVERSAL::can($w, 'mail_headers');
-    }
+    # However, must resolve to widget first, in case widget is dynamically
+    # created by body attribute.
+    my($b) = $self->unsafe_resolve_widget_value($self->get('body'), $req);
+    my($body) = $self->render_value(body => $b, $req);
+    $msg->map_invoke(set_header => $b->mail_headers($req))
+	if $b && UNIVERSAL::can($b, 'mail_headers');
+    # Allow headers_object to override any headers
+    my($o) = $self->unsafe_get('headers_object');
+    $msg->map_invoke(set_header => $o->mail_headers($req))
+	if $o and $o = $self->unsafe_resolve_widget_value($o, $req);
     $msg->set_body($$body);
     $msg->enqueue_send($req);
     my($lf);
-    Bivio::IO::Log->write($lf, $msg->as_string)
+    $_L->write($lf, $msg->as_string)
         if $self->unsafe_render_attr('log_file', $req, \$lf) && $lf;
     return;
 }
@@ -99,8 +55,14 @@ sub execute {
 sub initialize {
     my($self) = @_;
     $self->initialize_attr('from');
-    $self->map_invoke(unsafe_initialize_attr =>
-	[qw(recipients body cc to subject headers log_file)]);
+    $self->map_invoke(unsafe_initialize_attr => [qw(
+	body
+	cc
+	headers_object
+	recipients
+	subject
+	to
+    )]);
     return;
 }
 
