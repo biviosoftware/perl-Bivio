4796fa56e254a91d91cb2f20a2aed7225ddc2738
Rob Nagler <github@q33.us>
2008-02-07 18:12:19 +0000

    proper To/From

--- Biz/Model/MailForm.pm
+++ Biz/Model/MailForm.pm
@@ -5,37 +5,61 @@ use strict;
 use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_EA) = __PACKAGE__->use('Type.EmailArray');
+my($_I) = __PACKAGE__->use('Mail.Incoming');
 my($_O) = __PACKAGE__->use('Mail.Outgoing');
-my($_V) = __PACKAGE__->use('UI.View');
 my($_RFC) = __PACKAGE__->use('Mail.RFC822');
+my($_V) = __PACKAGE__->use('UI.View');
+my($_QUERY_WHO) = 'to';
+my($_MRW) = __PACKAGE__->use('Type.MailReplyWho');
+
+sub execute_cancel {
+    return {
+	task_id => 'next',
+	query => undef,
+    };
+}
 
 sub execute_empty {
     my($self) = @_;
-    if (my $s = $self->req->unsafe_get_nested(qw(Model.RealmMail subject))) {
-	$self->internal_put_field(
-	    subject => ($s =~ /\bRe:/ ? '' : 'Re: ') . $s);
-    }
+    my($m) = $self->req->unsafe_get_nested('Model.RealmMail');
+    my($in) = $m && $_I->new($m);
+    $self->internal_put_field(subject => $in->get_reply_subject)
+	if $in;
+    my($to, $cc) =  ($in || $_I)->get_reply_email_arrays(
+	($self->req('query') || {})->{$_QUERY_WHO},
+	$self->req,
+    );
+    $self->internal_put_field(to => $to);
+    $self->internal_put_field(cc => $cc);
     return;
 }
 
 sub execute_ok {
     my($self) = @_;
-    my($r) = $self->req(qw(auth_realm owner));
-    my($r_email) = $r->format_email;
     my($id) = $self->req->unsafe_get_nested(qw(Model.RealmMail message_id));
+    my($cc) = $self->get('cc')->as_literal;
+    my($to) = $self->get('to');
     $self->internal_put_field(headers => {
 	_from => $_RFC->format_mailbox(
 	    $self->new_other('Email')->load_for_auth_user->get('email'),
 	    $self->req(qw(auth_user display_name)),
 	),
-	_recipients => $r_email,
-	Sender => $r_email,
-	To => $_RFC->format_mailbox($r_email, $r->get('display_name')),
+	_recipients => $to->new([
+	    @{$to->as_array},
+	    @{$self->get('cc')->as_array},
+	])->as_literal,
+	Sender => $self->req(qw(auth_realm owner))->format_email,
+	To => $to->as_literal,
+	$cc ? (Cc => $cc) : (),
 	Subject => $self->get('subject'),
 	$id ? ('In-Reply-To' => $_RFC->format_angle_brackets($id)) : (),
     });
     $_V->execute('Mail->form_mail', $self->req);
-    return;
+    return {
+	task_id => 'next',
+	query => undef,
+    };
 }
 
 sub internal_initialize {
@@ -43,11 +67,16 @@ sub internal_initialize {
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
         visible => [
-# 	    {
-# 		name => 'to',
-# 		type => 'MailFormTo',
-# 		constraint => 'NOT_ZERO_ENUM',
-# 	    },
+ 	    {
+ 		name => 'to',
+		type => 'EmailArray',
+		constraint => 'NOT_NULL',
+ 	    },
+ 	    {
+ 		name => 'cc',
+		type => 'EmailArray',
+		constraint => 'NONE',
+ 	    },
 	    {
 		name => 'subject',
 		type => 'RealmMail.subject',
@@ -72,15 +101,29 @@ sub internal_initialize {
 		type => 'Hash',
 		constraint => 'NONE',
 	    },
+	    {
+		name => 'RealmMail.realm_file_id',
+		constraint => 'NONE',
+	    },
 	],
     });
 }
 
 sub internal_pre_execute {
     my($self) = @_;
-    $self->use('Type.FormMode')->setup_by_list_this(
-	$self->new_other('RealmMailList'), 'RealmMail');
+    $self->internal_put_field(
+	'RealmMail.realm_file_id' =>
+	    $self->use('Type.FormMode')->setup_by_list_this(
+		$self->new_other('RealmMailList'),
+		'RealmMail',
+	    )->eq_create ? undef
+	    : $self->req(qw(Model.RealmMail realm_file_id)),
+    );
     return shift->SUPER::internal_pre_execute(@_);
+}
+
+sub is_reply {
+    return shift->unsafe_get('RealmMail.realm_file_id') ? 1 : 0;
 }
 
 sub mail_header_from {
@@ -99,6 +142,14 @@ sub mail_envelope_recipients {
 sub map_attachments {
     my(undef, $op) = @_;
     return [map($op->("attachment$_"), 1..3)];
+}
+
+sub reply_query {
+    my(undef, $who) = @_;
+    return {
+	'ListQuery.this' => ['RealmMail.realm_file_id'],
+	$_QUERY_WHO => lc($_MRW->from_any($who)->as_uri),
+    };
 }
 
 1;
