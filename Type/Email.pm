b9f63e94bc7487a4803c6f3b5a9b751aeb9df403
Rob Nagler <github@q33.us>
2008-02-07 15:53:16 +0000

    rmpod

--- Type/Email.pm
+++ Type/Email.pm
@@ -1,147 +1,45 @@
-# Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::Email;
 use strict;
-$Bivio::Type::Email::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::Email::VERSION;
+use Bivio::Base 'Type.Line';
 
-=head1 NAME
-
-Bivio::Type::Email - email address
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::Email;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Line>
-
-=cut
-
-use Bivio::Type::Line;
-@Bivio::Type::Email::ISA = ('Bivio::Type::Line');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::Email> simple syntax checking on email addresses.
-
-=cut
-
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="IGNORE_PREFIX"></a>
-
-=head2 IGNORE_PREFIX : string
-
-Prefix we used to indicate addresses which go into a black hole.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_HTML) = __PACKAGE__->use('Bivio.HTML');
+my($_TE) = __PACKAGE__->use('Bivio.TypeError');
+my($_IGNORE) = __PACKAGE__->IGNORE_PREFIX;
+my($_INVALID) = __PACKAGE__->INVALID_PREFIX;
+my($_ATOM_ONLY_ADDR) = __PACKAGE__->use('Mail.RFC822')->ATOM_ONLY_ADDR;
 
 sub IGNORE_PREFIX {
     return 'ignore-';
 }
 
-=for html <a name="INVALID_PREFIX"></a>
-
-=head2 INVALID_PREFIX : string
-
-Prefix used to indicate addresses which are invalid.
-
-
-=cut
-
 sub INVALID_PREFIX {
     return 'invalid:';
 }
-
-#=IMPORTS
-use Bivio::HTML;
-use Bivio::TypeError;
-use Bivio::Mail::RFC822;
-
-#=VARIABLES
-my($_IGNORE) = IGNORE_PREFIX();
-my($_INVALID) = INVALID_PREFIX();
-my($_ATOM_ONLY_ADDR) = Bivio::Mail::RFC822->ATOM_ONLY_ADDR;
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="format_ignore"></a>
-
-=head2 static format_ignore(string base, Bivio::Agent::Request req) : string
-
-Formats an ignore email.
-
-=cut
 
 sub format_ignore {
     my($proto, $base, $req) = @_;
     return $req->format_email($proto->IGNORE_PREFIX . $base);
 }
 
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value) : array
-
-Returns C<undef> if the name is empty or zero length.
-Checks syntax and returns L<Bivio::TypeError|Bivio::TypeError>.
-
-=cut
-
 sub from_literal {
     my($proto, $value) = @_;
     $proto->internal_from_literal_warning
         unless wantarray;
-    return undef unless defined($value);
-    # Leave middle spaces, because user can't have them
+    return undef
+	unless defined($value);
     $value =~ s/^\s+|\s+$//g;
-    return undef unless length($value);
-    return (undef, Bivio::TypeError->TOO_LONG)
+    return undef
+	unless length($value);
+    return (undef, $_TE->TOO_LONG)
 	if length($value) > $proto->get_width;
-    # We always force to lower case to ensure values get into the
-    # database that can be searchable.
     $value = lc($value);
-
-    # Must match a simple dotted atom address and must contain at least one dot
-#TODO: parse out address and try to do a DNS resolution?
-#      I checked out Net::DNS, but it doesn't seem like it can
-#      handle timeouts properly.  (No way to stop the query once
-#      started(?)).  Anyway, we need a way to avoid entering bogus
-#      addresses.  The best way would be to mail the user the password.
-#      I don't know if we could sustain this in the beginning.
-    return $value if $value =~ /^$_ATOM_ONLY_ADDR$/os && $value =~ /.+\..*/;
-
-#TODO: This is weak.  Either make good, or just use general message
-#    # Give a reasonable error message
-#    # We don't accept domain literal addresses?
-#    return (undef, Bivio::TypeError::EMAIL_DOMAIN_LITERAL())
-#	    if /$_822_DOMAIN_LITERAL/os;
-#    # Must be qualified
-#    return (undef, Bivio::TypeError::EMAIL_UNQUALIFIED())
-#	    unless /\@/;
-    # Some other error
-    return (undef, Bivio::TypeError->EMAIL);
+    return $value
+	if $value =~ /^$_ATOM_ONLY_ADDR$/os && $value =~ /.+\..*/;
+    return (undef, $_TE->EMAIL);
 }
-
-=for html <a name="invalidate"></a>
-
-=head2 invalidate(string_ref email)
-
-Invalidate email address and truncate if necessary
-
-=cut
 
 sub invalidate {
     my($proto, $email) = @_;
@@ -149,62 +47,21 @@ sub invalidate {
     return;
 }
 
-=for html <a name="is_ignore"></a>
-
-=head2 static is_ignore(string email) : boolean
-
-Returns true if is not L<is_valid|"is_valid"> or
-begins with L<IGNORE_PREFIX|"IGNORE_PREFIX">.
-
-=cut
-
 sub is_ignore {
     my($proto, $email) = @_;
-    return 1 unless $proto->is_valid($email);
-    return $email =~ /^$_IGNORE/ios ? 1 : 0;
+    return !$proto->is_valid($email) ? 1
+	: $email =~ /^$_IGNORE/ios ? 1 : 0;
 }
-
-=for html <a name="is_valid"></a>
-
-=head2 is_valid(string email) : boolean
-
-Checks to see the email is a valid address.  Used to check
-values stored in the database which may be invalidated by support.
-
-=cut
 
 sub is_valid {
     my($proto, $email) = @_;
     return defined($email) && $email =~ /^$_ATOM_ONLY_ADDR$/os ? 1 : 0;
 }
 
-=for html <a name="to_xml"></a>
-
-=head2 to_xml(any value) : string
-
-Returns I<value> formatted properly for XML.
-
-HACK: if I<value> L<is_ignore|"is_ignore">, it rendered as the empty string.
-This is assumed by L<Bivio::UI::XML::ClubExport|Bivio::UI::XML::ClubExport>.
-
-=cut
-
 sub to_xml {
     my($proto, $value) = @_;
-    return '' unless defined($value) && !$proto->is_ignore($value);
-    return Bivio::HTML->escape($value);
+    return !defined($value) || $proto->is_ignore($value) ? ''
+	: $_HTML->escape($value);
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
