9d7a009c05d21cb16a7eb33e7fe46d6faf477b7b
Rob Nagler <github@q33.us>
2008-02-11 04:48:54 +0000

    get_local_part

--- Type/Email.pm
+++ Type/Email.pm
@@ -46,6 +46,11 @@ sub from_literal {
     return (undef, $_TE->EMAIL);
 }
 
+sub get_local_part {
+    my(undef, $value) = @_;
+    return $value && $value =~ /^(.+?)\@/ ? $1 : undef;
+}
+
 sub invalidate {
     my($proto, $email) = @_;
     $$email = substr($_INVALID . $$email, 0, $proto->get_width);
