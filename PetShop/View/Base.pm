c92332bf80485a3ea520187c1df60cbc33bcb27c
Rob Nagler <github@q33.us>
2008-02-11 04:42:59 +0000

    added menu

--- PetShop/View/Base.pm
+++ PetShop/View/Base.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2007-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::PetShop::View::Base;
 use strict;
@@ -8,9 +8,16 @@ use Bivio::UI::ViewLanguageAUTOLOAD;
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub internal_xhtml_adorned {
-    my(@res) = shift->SUPER::internal_xhtml_adorned(@_);
-    view_unsafe_put(xhtml_main_left => DIV_example('hello'));
-    return @res;
+    my($self) = @_;
+    return $self->call_super_before(\@_, sub {
+        view_unsafe_put(
+	    xhtml_header_middle => TaskMenu([
+		'FORUM_MAIL_THREAD_ROOT_LIST',
+		'FORUM_CRM_THREAD_ROOT_LIST',
+	    ]),
+	);
+        return;
+    });
 }
 
 1;
