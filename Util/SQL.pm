4a992f2bdb2970ed61c6e98792fa14e99869aa2e
Rob Nagler <github@q33.us>
2008-02-12 18:32:24 +0000

    fix bug in permissions51

--- Util/SQL.pm
+++ Util/SQL.pm
@@ -1145,6 +1145,7 @@ sub internal_upgrade_db_permissions51 {
 	    my($write);
 	    foreach my $bit (21 .. 69) {
 		next unless vec($ps, $bit, 1);
+		vec($ps, $bit, 1) = 0;
 		vec($ps, $bit + 30, 1) = 1;
 		$write++;
 	    }
