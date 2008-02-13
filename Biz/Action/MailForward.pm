72adc7feea61abc7b128acccc26dca1ae34ec35c
Alex Viggio <aviggio@bivio.biz>
2008-02-13 22:14:37 +0000

    Call Bivio::Mail::Outgoing->set_headers_for_forward

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -9,10 +9,11 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub execute {
     my(undef, $req) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
-    # Let sendmail handle mail loop detection (for now)
     Bivio::Mail::Outgoing->new(
 	Bivio::Mail::Incoming->new($mr->get('message')->{content}),
-    )->set_recipients($mr->get('recipient'), $req)->enqueue_send($req);
+    )->set_recipients($mr->get('recipient'), $req)
+	->set_headers_for_forward
+	    ->enqueue_send($req);
     return;
 }
 
