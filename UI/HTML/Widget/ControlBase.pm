20b9f3f162c0bfb627cc5571684b292e51a44e3d
Rob Nagler <github@q33.us>
2008-03-04 19:29:36 +0000

    rmpod

--- UI/HTML/Widget/ControlBase.pm
+++ UI/HTML/Widget/ControlBase.pm
@@ -2,101 +2,46 @@
 # $Id$
 package Bivio::UI::HTML::Widget::ControlBase;
 use strict;
-$Bivio::UI::HTML::Widget::ControlBase::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::ControlBase::VERSION;
+use Bivio::Base 'Bivio::UI::Widget::ControlBase';
 
-=head1 NAME
+# C<Bivio::UI::HTML::Widget::ControlBase> renders common html attributes.
+#
+#
+#
+# class : string []
+#
+# HTML class attribute.
+#
+# id : string []
+#
+# HTML id attribute.
+#
+# html_attrs : array_ref [[class id]]
+#
+# List of attributes to render.
 
-Bivio::UI::HTML::Widget::ControlBase - adds a class and id attributes
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::ControlBase;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget::ControlBase>
-
-=cut
-
-use Bivio::UI::Widget::ControlBase;
-@Bivio::UI::HTML::Widget::ControlBase::ISA = ('Bivio::UI::Widget::ControlBase');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::ControlBase> renders common html attributes.
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item class : string []
-
-HTML class attribute.
-
-=item id : string []
-
-HTML id attribute.
-
-=item html_attrs : array_ref [[class id]]
-
-List of attributes to render.
-
-=back
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_VS) = 'Bivio::UI::HTML::ViewShortcuts';
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="control_on_render"></a>
-
-=head2 control_on_render(any source, string_ref buffer)
-
-Render class and id.
-
-=cut
-
 sub control_on_render {
+    # (self, any, string_ref) : undef
+    # Render class and id.
     my($self, $source, $buffer) = @_;
     $$buffer .= $_VS->vs_html_attrs_render(
 	$self, $source, $self->unsafe_get('html_attrs'));
     return;
 }
 
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes class attribute.
-
-=cut
-
 sub initialize {
+    # (self) : undef
+    # Initializes class attribute.
     my($self) = @_;
     $_VS->vs_html_attrs_initialize($self, $self->unsafe_get('html_attrs'));
     return shift->SUPER::initialize(@_);
 }
 
-=for html <a name="internal_compute_new_args"></a>
-
-=head2 static internal_compute_new_args(array_ref required, array_ref args) : hash_ref
-
-=cut
-
 sub internal_compute_new_args {
+    # (proto, array_ref, array_ref) : hash_ref
     my($proto, $required, $args) = @_;
     return {
 	map({
@@ -114,27 +59,10 @@ sub internal_compute_new_args {
     };
 }
 
-=for html <a name="internal_new_args"></a>
-
-=head2 static internal_new_args(array_ref required, array_ref args) : hash_ref
-
-=cut
-
 sub internal_new_args {
+    # (proto, array_ref, array_ref) : hash_ref
     Bivio::IO::Alert->warn_deprecated('call internal_compute_new_args');
     return shift->internal_compute_new_args(@_);
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005-2006 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
