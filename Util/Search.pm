844d4603b1bc04d0225fe85d45428643f2543ab8
Rob Nagler <github@q33.us>
2008-03-04 22:39:47 +0000

    added rebuild_realm rebuild_db calls rebuild_realm in subprocess to avoid filling up memory

--- Util/Search.pm
+++ Util/Search.pm
@@ -1,16 +1,18 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Util::Search;
 use strict;
-use base 'Bivio::ShellUtil';
+use Bivio::Base 'Bivio::ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_X) = __PACKAGE__->use('Search.Xapian');
 
 sub USAGE {
     return <<'EOF';
 usage: b-search [options] command [args..]
 commands
   rebuild_db -- reload entire search database
+  rebuild_realm -- reindex the current realm (does not delete index)
 EOF
 }
 
@@ -18,24 +20,46 @@ sub rebuild_db {
     my($self) = @_;
     my($req) = $self->get_request;
     $self->are_you_sure('Destroy the ENTIRE Xapian database?');
+    $self->model('Lock')->acquire_general;
+    $_X->destroy_db($req);
+    my($realms) = $self->model('RealmFile')->map_iterate(
+	sub {shift->get('realm_id')},
+	'unauth_iterate_start',
+	'realm_id',
+	{path => '/'},
+    );
+    $self->commit_or_rollback;
+    foreach my $r (@$realms) {
+	system(
+	    $^X,
+	    '-w',
+	    $0,
+	    '-realm',
+	    $r,
+	    'rebuild_realm',
+	);
+    }
+    return;
+}
+
+sub rebuild_realm {
+    my($self) = @_;
     my($i) = 0;
-    Bivio::Biz::Model->new($req, 'RealmFile')->do_iterate(
+    Bivio::IO::Alert->info($self->req(qw(auth_realm owner name)));
+    $self->model('RealmFile')->do_iterate(
 	sub {
 	    my($it) = @_;
 	    if (0 == $i++ % 100) {
-		# Executes first time through
 		$self->commit_or_rollback;
 		Bivio::IO::Alert->reset_warn_counter;
-		$it->new_other('Lock')->execute_general($req);
-		$self->use('Bivio::Search::Xapian')->destroy_db($req)
-		    if $i == 1;
+		$it->new_other('Lock')->acquire_general;
 		Bivio::IO::Alert->info(
-		    'file#', $i, ': ', $it->get('realm_file_id'));
+		    'file#', $i, ': ', $it->get('realm_file_id'),
+		) if $i > 1;
 	    }
-	    Bivio::Search::Xapian->update_realm_file($it);
+	    $_X->update_realm_file($it);
 	    return 1;
         },
-	'unauth_iterate_start',
 	'realm_file_id asc',
 	{is_folder => 0},
     );
