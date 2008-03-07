3d01b6e80891d93faaa66af0ffb8878297851f2d
Alex Viggio <aviggio@bivio.biz>
2008-03-07 23:53:28 +0000

    Expose NAME_COLUMNS

--- Biz/Model/AdmUserList.pm
+++ Biz/Model/AdmUserList.pm
@@ -5,7 +5,8 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_NAME_COLS) = [map("User.${_}_name", qw(last first middle))];
+my($_NAME_SORT_COLS) = [map("User.${_}_name_sort", qw(last first middle))];
+my($_NAME_COLS) = [grep(s/_sort//, @$_NAME_SORT_COLS)];
 my($_U) = __PACKAGE__->use('Model.User');
 my($_L) = __PACKAGE__->use('Type.Line');
 
@@ -13,39 +14,54 @@ sub LOAD_ALL_SEARCH_STRING {
     return 'All';
 }
 
-sub NAME_SORT_COLUMNS {
+sub NAME_COLUMNS {
     return [@$_NAME_COLS];
 }
 
+sub NAME_SORT_COLUMNS {
+    return [@$_NAME_SORT_COLS];
+}
+
 sub internal_initialize {
-    my($self) = @_;
-    return {
-	version => 1,
-	can_iterate => 1,
-	order_by => $self->NAME_SORT_COLUMNS,
-        primary_key => ['User.user_id'],
-	other => [
-	    {
-		name => 'display_name',
-		type => 'Line',
-		constraint => 'NOT_NULL',
-	    },
-	],
-    };
+    my($delegator, $stmt, $info) = shift->delegated_args(@_);
+    return $delegator->merge_initialize_info(
+	$info || $delegator->SUPER::internal_initialize,
+	{
+	    version => 1,
+	    can_iterate => 1,
+	    order_by => [
+		@{$delegator->NAME_SORT_COLUMNS},
+		'Email.email',
+	    ],
+	    primary_key => [['User.user_id', 'Email.realm_id']],
+	    other => [
+		@{$delegator->NAME_COLUMNS},
+		[
+		    'Email.location',
+		    [$delegator->get_instance('Email')->DEFAULT_LOCATION],
+		],
+		{
+		    name => 'display_name',
+		    type => 'Line',
+		    constraint => 'NOT_NULL',
+		},
+	    ],
+	},
+    );
 }
 
 sub internal_post_load_row {
-    my($self, $row) = @_;
+    my($delegator, $row) = shift->delegated_args(@_);
     $row->{display_name} = $_U->concat_last_first_middle(@{$row}{@$_NAME_COLS});
     return 1;
 }
 
 sub internal_prepare_statement {
-    my($self, $stmt, $query) = @_;
+    my($delegator, $stmt, $query) = shift->delegated_args(@_);
     my($search) = $_L->from_literal($query->get('search'));
     return unless defined($search);
-    if ($search eq $self->LOAD_ALL_SEARCH_STRING) {
-	$query->put(count => $self->LOAD_ALL_SIZE);
+    if ($search eq $delegator->LOAD_ALL_SEARCH_STRING) {
+	$query->put(count => $delegator->LOAD_ALL_SIZE);
     }
     elsif ($search =~ /^\d+$/) {
 	$stmt->where(['User.user_id', [$search]]);
