05b1e6e376742250869bfe65e33f472c9275fca3
Rob Nagler <github@q33.us>
2008-03-08 19:47:13 +0000

    Bivio::Base

--- Type/BlogTitle.pm
+++ Type/BlogTitle.pm
@@ -2,10 +2,10 @@
 # $Id$
 package Bivio::Type::BlogTitle;
 use strict;
-use base 'Bivio::Type::Line';
+use Bivio::Base 'Type.Line';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_BC) = Bivio::Type->get_instance('BlogContent');
+my($_BC) = __PACKAGE__->use('Type.BlogContent');
 
 sub from_content {
     my($self, $content) = @_;
