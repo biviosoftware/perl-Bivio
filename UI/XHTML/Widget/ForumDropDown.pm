053ab1f4c16d6375b002ebe8ceb73d3accc61197
Rob Nagler <github@q33.us>
2008-03-14 02:52:36 +0000

    refactor _forums to internal_choices() so can be overriden by apps

--- UI/XHTML/Widget/ForumDropDown.pm
+++ UI/XHTML/Widget/ForumDropDown.pm
@@ -1,19 +1,19 @@
-# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2007-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::XHTML::Widget::ForumDropDown;
 use strict;
 use Bivio::Base 'Widget.If';
 use Bivio::UI::ViewLanguageAUTOLOAD;
-my($_RT) = __PACKAGE__->use('Auth.RealmType');
+my($_FORUM) = __PACKAGE__->use('Auth.RealmType')->FORUM;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub initialize {
     my($self) = @_;
     $self->put(
-	control => [sub {@{_forums(shift)} != 0}],
+	control => [sub {@{$self->internal_choices(shift)} != 0}],
 	control_on_value => If(
-	    [\&_one_forum],
+	    [sub {_one_choice($self, shift)}],
 	    SPAN(_curr_realm(), {class => 'dd_link'}),
 	    DropDown(
 		If([[qw(->req auth_realm type)], '->eq_forum'],
@@ -23,7 +23,7 @@ sub initialize {
 		DIV_dd_menu(
 		    [sub {
 			 my($source) = @_;
-			 my($forums) = _forums($source);
+			 my($forums) = $self->internal_choices($source);
 			 my($r) = $source->req('auth_realm');
 			 $r = $r->get('type')->eq_forum ? $r->get('owner_name')
 			     : '';
@@ -49,6 +49,14 @@ sub initialize {
     return shift->SUPER::initialize(@_);
 }
 
+sub internal_choices {
+    my(undef, $source) = @_;
+    return $source->req->map_user_realms(
+	sub {shift->{'RealmOwner.name'}},
+	{'RealmOwner.realm_type' => $_FORUM},
+    );
+}
+
 sub internal_new_args {
     my(undef, $attributes) = @_;
     return {
@@ -56,21 +64,14 @@ sub internal_new_args {
     };
 }
 
-sub _forums {
-    return shift->req->map_user_realms(
-	sub {shift->{'RealmOwner.name'}},
-	{'RealmOwner.realm_type' => $_RT->FORUM},
-    );
-}
-
-sub _one_forum {
-    my($source) = @_;
-    return $source->req(qw(auth_realm type))->eq_forum
-	&& @{_forums($source)} == 1;
-}
-
 sub _curr_realm {
     return String([qw(->req auth_realm owner_name)]);
+}
+
+sub _one_choice {
+    my($self, $source) = @_;
+    return $source->req(qw(auth_realm type))->eq_forum
+	&& @{$self->internal_choices($source)} == 1;
 }
 
 1;
