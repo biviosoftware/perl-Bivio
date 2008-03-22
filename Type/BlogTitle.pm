d6842ad4694d1a9bcd2a4d9ca3f9c1e4e571e161
Rob Nagler <github@q33.us>
2008-03-22 20:22:25 +0000

    added empty_value

--- Type/BlogTitle.pm
+++ Type/BlogTitle.pm
@@ -7,10 +7,14 @@ use Bivio::Base 'Type.Line';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_BC) = __PACKAGE__->use('Type.BlogContent');
 
+sub empty_value {
+    return '(No Title)'
+}
+
 sub from_content {
     my($self, $content) = @_;
     my($res) = $_BC->split($content);
-    return ref($res) ? '(No Title)' : $res;
+    return ref($res) ? $self->EMPTY_VALUE : $res;
 }
 
 1;
