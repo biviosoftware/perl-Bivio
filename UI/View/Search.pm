291dd9063fef30bfd20f9c7e065a0818813ca130
Rob Nagler <github@q33.us>
2008-03-23 22:17:16 +0000

    added result_who, title, and excerpt

--- UI/View/Search.pm
+++ UI/View/Search.pm
@@ -10,24 +10,22 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub list {
     return shift->internal_body(
 	Join([
-# 	    vs_simple_form(SearchForm => [
-# 		[FormField('SearchForm.search')],
-# 		'*ok_button',
-# 	    ]),
 	    vs_paged_list(SearchList => [
 		['RealmFile.modified_date_time', {
 		    column_data_class => 'last_update',
 		    column_heading_class => 'last_update',
 		}],
-		['RealmFile.path', {
-		    column_data_class => 'long_text item',
-		    column_heading_class => 'item',
-#TODO: Need to fix up page
+		'result_who',
+		['result_title', {
+		    column_data_class => 'long_text',
 		    column_widget => Link(
-#HTML?
-			String(['result_excerpt']),
+			String(['result_title']),
 			['result_uri'],
 		    ),
+		}],
+		['result_excerpt', {
+		    column_data_class => 'long_text item',
+		    column_heading_class => 'item',
 		}],
 	    ]),
 	]),
