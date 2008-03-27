17f8c0fe0810ff5c41830c9b8330c809e9844e33
Rob Nagler <github@q33.us>
2008-03-27 22:25:06 +0000

    *** empty log message ***

---
+++ PetShop/Model/FieldTestForm.pm
@@ -0,0 +1,24 @@
+# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::PetShop::Model::FieldTestForm;
+use strict;
+use Bivio::Base 'Biz.FormModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	@{$self->internal_initialize_local_fields(visible => [
+	    [qw(boolean Boolean)],
+	    [qw(date Date)],
+	    [qw(date_time DateTime)],
+	    [qw(realm_name RealmName)],
+	    [qw(line Line)],
+	    [qw(text Text)],
+	], undef, 'NONE')},
+    });
+}
+
+1;
