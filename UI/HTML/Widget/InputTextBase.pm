65d6696f44ccfd866e4312ff9a5aa9398518f194
Rob Nagler <github@q33.us>
2008-03-27 19:10:35 +0000

    generate type= dynamically for password or text

--- UI/HTML/Widget/InputTextBase.pm
+++ UI/HTML/Widget/InputTextBase.pm
@@ -20,13 +20,15 @@ sub initialize {
 sub internal_input_base_render_attrs {
     my($self, $form, $field, $source, $buffer) = @_;
     shift->SUPER::internal_input_base_render_attrs(@_);
+    my($t) = $form->get_field_type($field);
     my($req) = $source->get_request;
     my($size) = $self->render_simple_attr('size', $source);
     my($width) = $self->render_simple_attr('max_width', $source)
-	|| $form->get_field_type($field)->get_width;
+	|| $t->get_width;
     $size += 2
 	if $size == $width;
-    $$buffer .= qq{ size="$size" maxlength="$width"};
+    $$buffer .= qq{ type="@{[$t->is_password ? 'password' : 'text']}"}
+	. qq{ size="$size" maxlength="$width"};
     return;
 }
 1;
