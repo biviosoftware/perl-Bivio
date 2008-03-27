ed30fce3153d531006e01e1abde4ff848648d158
Rob Nagler <github@q33.us>
2008-03-27 00:00:58 +0000

    use InputTextBase

--- UI/HTML/Widget/DateField.pm
+++ UI/HTML/Widget/DateField.pm
@@ -1,200 +1,34 @@
-# Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::HTML::Widget::DateField;
 use strict;
-$Bivio::UI::HTML::Widget::DateField::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::DateField::VERSION;
+use Bivio::Base 'HTMLWidget.InputTextBase';
 
-=head1 NAME
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_D) = __PACKAGE__->use('Type.Date');
 
-Bivio::UI::HTML::Widget::DateField - a date field for forms
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::DateField;
-    Bivio::UI::HTML::Widget::DateField->new();
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::HTML::Widget::ControlBase>
-
-=cut
-
-use Bivio::UI::HTML::Widget::ControlBase;
-@Bivio::UI::HTML::Widget::DateField::ISA = ('Bivio::UI::HTML::Widget::ControlBase');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::DateField> is a date field for forms.
-
-B<Don't use this for DateTime values.>
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item allow_undef : boolean [false]
-
-Allow undef for field, i.e. don't fill in with now.
-
-=item event_handler : Bivio::UI::Widget []
-
-If set, this widget will be initialized as a child and must
-support a method C<get_html_field_attributes> which returns a
-string to be inserted in this fields declaration.
-I<event_handler> will be rendered before this field.
-
-=item field : string (required)
-
-Name of the form field.
-
-=item form_model : array_ref (required, inherited, get_request)
-
-Which form are we dealing with.
-
-=back
-
-=cut
-
-#=IMPORTS
-use Bivio::UI::DateTimeMode;
-use Bivio::UI::HTML::Format::DateTime;
-
-#=VARIABLES
-my($_D) = Bivio::Type->get_instance('Date');
-my(@_ATTRS) = qw(
-    allow_undef
-    event_handler
-    field
-    form_model
-);
-
-=head1 FACTORIES
-
-=cut
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="accepts_attribute"></a>
-
-=head2 static accepts_attribute(string attr) : boolean
-
-Does the widget accept this attribute?
-
-=cut
-
-sub accepts_attribute {
-    my(undef, $attr) = @_;
-    return grep($_ eq $attr, @_ATTRS) ? 1 : 0;
-}
-
-=for html <a name="control_on_render"></a>
-
-=head2 control_on_render(any source, string_ref buffer)
-
-Draws the date field on the specified buffer.
-
-=cut
-
-sub control_on_render {
-    my($self, $source, $buffer) = @_;
-    my($req) = $source->get_request;
-    my($form) = $req->get_widget_value(@{$self->ancestral_get('form_model')});
-    my($field) = ${$self->render_attr('field', $source)};
-
-#TODO: Merge with Text.  Too much duplicated code.
-    my($p, $s) = Bivio::UI::Font->format_html('input_field', $req);
-    my($width) = $_D->get_width;
-    $$buffer .= $p
-	. '<input name="'
-	. $form->get_field_name_for_html($field)
-	. '" type="text" size="'
-	. ($width  + 2)
-	. qq{" maxlength="$width"};
-    my($h) = $self->unsafe_get('event_handler');
-    $h = $self->unsafe_resolve_widget_value($h, $source)
-	if $h;
-    $$buffer .= ' ' . $h->get_html_field_attributes($field, $source)
-	if $h;
-    $$buffer .= ' disabled="1"'
-	unless $form->is_field_editable($field);
-    my($b) = '';
-    $$buffer .= ' value="'
-	. ($form->get_field_error($field) ? $form->get_field_as_html($field)
-	    : $_D->to_html(
-		$form->get($field)
-		    or $self->unsafe_render_attr('allow_undef', $source, \$b)
-			&& $b ? undef
-		    : $_D->local_today
-	)) . qq{" />$s};
-    # Handler is rendered after, because it probably needs to reference the
-    # field.
-    $h->render($source, $buffer)
-	if $h;
+sub internal_render_tag_attr_value {
+    my($self, $form, $field, $source, $buffer) = @_;
+    my($v) = $_D->to_html(
+       $form->get($field)
+	   || ($self->render_simple_attr('allow_undef', $source) ? undef
+	   : $_D->local_today),
+    );
+    $$buffer .= qq{ value="$v"};
     return;
 }
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes from configuration attributes.
-
-=cut
 
 sub initialize {
     my($self) = @_;
     $self->initialize_attr('field');
-    $self->map_invoke(
-	'unsafe_initialize_attr',
-	[@_ATTRS],
-    );
-    return;
+    $self->initialize_attr(size => $_D->get_width);
+    $self->initialize_attr(max_width => $_D->get_width);
+    $self->unsafe_initialize_attr('allow_undef');
+    return shift->SUPER::initialize(@_);
 }
 
-=for html <a name="internal_as_string"></a>
-
-=head2 internal_as_string() : array
-
-Returns this widget's config for
-L<Bivio::UI::Widget::as_string|Bivio::UI::Widget/"as_string">.
-
-=cut
-
-sub internal_as_string {
-    return shift->unsafe_get('field');
+sub internal_attributes {
+    return [@{shift->SUPER::internal_attributes(@_)}, 'allow_undef'];
 }
-
-=for html <a name="internal_new_args"></a>
-
-=head2 static internal_new_args() :  hash_ref
-
-Implements positional argument parsing for L<new|"new">.
-
-=cut
-
-sub internal_new_args {
-    return shift->internal_compute_new_args(['field'], \@_);
-}
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
--- UI/HTML/Widget/Text.pm
+++ UI/HTML/Widget/Text.pm
@@ -1,161 +1,37 @@
-# Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::UI::HTML::Widget::Text;
 use strict;
-use Bivio::Base 'Bivio::UI::Widget';
-use Bivio::HTML;
-use Bivio::UI::HTML::Format;
-
-# C<Bivio::UI::HTML::Widget::Text> draws a C<INPUT> tag with
-# attribute C<TYPE=TEXT>.  If I<field> I<isa>
-# L<Bivio::Type::Password|Bivio::Type::Password>,
-# will render as a C<TYPE=PASSWORD>.
-#
-#
-#
-# event_handler : Bivio::UI::Widget []
-#
-# If set, this widget will be initialized as a child and must
-# support a method C<get_html_field_attributes> which returns a
-# string to be inserted in this fields declaration.
-# I<event_handler> will be rendered before this field.
-#
-# field : string (required)
-#
-# Name of the form field.
-#
-# form_model : array_ref (required, inherited, get_request)
-#
-# Which form are we dealing with.
-#
-# format : string []
-#
-# format : array_ref []
-#
-# Widget value which returns the formatter to format the field
-# if it is not in error.  May return C<undef> iwc no formatting
-# will done.
-#
-# Only in the first case will the formatter be dymically loaded.
-# This is to prevent unnecessary transient state.
-#
-# The second form may be deprecated, so try to avoid it.
-#
-# is_read_only : boolean [!is_field_editable()]
-#
-# size : int (required)
-#
-# How wide is the field represented.  (maxlength comes from the
-# field's type.)
+use Bivio::Base 'HTMLWidget.InputTextBase';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_IDI) = __PACKAGE__->instance_data_index;
-my(@_ATTRS) = qw(
-    event_handler
-    field
-    form_model
-    format
-    size
-    class
-);
-
-sub accepts_attribute {
-    my(undef, $attr) = @_;
-    # Does the widget accept this attribute?
-    return grep($_ eq $attr, @_ATTRS);
-}
 
 sub initialize {
     my($self) = @_;
-    # Initializes static information.  In this case, prefix and suffix
-    # field values.
-    my($fields) = $self->[$_IDI];
-    return if $fields->{model};
-    $fields->{model} = $self->ancestral_get('form_model');
-    ($fields->{field}, $fields->{size}) = $self->get('field', 'size');
-    $self->unsafe_initialize_attr('max_width');
-    $self->initialize_attr(is_read_only => [
-	'!',
-	[['->get_request'],
-	@{$fields->{model}}],
-	'->is_field_editable',
-	$fields->{field},
-    ]);
-    # Initialize handler, if any
-    $fields->{handler} = $self->unsafe_get('event_handler');
-    if ($fields->{handler}) {
-	$fields->{handler}->put(parent => $self);
-	$fields->{handler}->initialize;
-    }
-
-    $fields->{format} = $self->unsafe_get('format');
-    $fields->{format}
-	    = Bivio::UI::HTML::Format->get_instance($fields->{format})
-		    if defined($fields->{format}) && !ref($fields->{format});
-    return;
+    $self->unsafe_initialize_attr('format');
+    return shift->SUPER::initialize(@_);
 }
 
-sub new {
-    my($self) = shift->SUPER::new(@_);
-    # Creates a new Text widget.
-    $self->[$_IDI] = {};
-    return $self;
+sub internal_attributes {
+    return [@{shift->SUPER::internal_attributes(@_)}, 'format'];
 }
 
-sub render {
-    my($self, $source, $buffer) = @_;
-    # Render the input field.  First render is special, because we need
-    # to extract the field's type and can only do that when we have a form.
-    my($fields) = $self->[$_IDI];
-    my($req) = $source->get_request;
-    my($form) = $req->get_widget_value(@{$fields->{model}});
-    my($field) = $fields->{field};
-    unless ($fields->{initialized}) {
-	my($type) = $fields->{type} = $form->get_field_type($field);
-
-	# It works better to have one extra space if the size == max.
-	my($s) = $fields->{size};
-	my($w) = $self->render_simple_attr('max_width') || $type->get_width();
-	$s++ if $s == $w;
-
-	$fields->{prefix} = '<input type="'
-		. ($type->is_password ? 'password' : 'text')
-		. qq{" size="$s" maxlength="$w"};
-	$fields->{initialized} = 1;
-    }
-    my($p, $s) = Bivio::UI::Font->format_html('input_field', $req);
-    $$buffer .= $p
-        . $fields->{prefix}
-	. ' name="'
-	. $form->get_field_name_for_html($field)
-	. '"';
-    if ($self->unsafe_get('class')) {
-	$$buffer .= ' class="';
-	$self->unsafe_render_attr('class', $source, $buffer);
-	$$buffer .= '"';
-    }
-    $$buffer .= ' '.$fields->{handler}->get_html_field_attributes(
-	$field, $source) if $fields->{handler};
-    $$buffer .= ' disabled="1"'
-	if $self->render_simple_attr('is_read_only', $source);
+sub internal_input_base_render_attrs {
+    my($self, $form, $field, $source, $buffer) = @_;
+    shift->SUPER::internal_input_base_render_attrs(@_);
     my($v);
-    if ($fields->{format} && !$form->get_field_error($field)) {
-	my($f) = ref($fields->{format}) eq 'ARRAY'
-		? $source->get_widget_value(@{$fields->{format}})
-		: $fields->{format};
-	if ($f) {
-	    $v = $f->get_widget_value($form->get($field));
-	    # Formatter must always return a defined value
-	    $v = Bivio::HTML->escape($v)
-		unless $f->result_is_html;
-	}
+    if (!$form->get_field_error($field)
+        and my $f = $self->unsafe_resolve_attr('format')
+    ) {
+	$f = Bivio::UI::HTML::Format->get_instance($f)
+	    unless ref($f);
+	$v = $f->get_widget_value($form->get($field));
+	$v = Bivio::HTML->escape($v)
+	    unless $f->result_is_html;
     }
     $v = $form->get_field_as_html($field)
 	unless defined($v);
-    $$buffer .= qq{ value="$v" />$s};
-    # Handler is rendered after, because it probably needs to reference the
-    # field.
-    $fields->{handler}->render($source, $buffer) if $fields->{handler};
+    $$buffer .= qq{ value="$v"};
     return;
 }
 
