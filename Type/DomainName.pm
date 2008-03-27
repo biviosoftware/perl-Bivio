d261e6b4c41b36cd9c7fcd242a898bb95f25f7d4
Rob Nagler <github@q33.us>
2008-03-27 03:43:36 +0000

    rmpod

--- Type/DomainName.pm
+++ Type/DomainName.pm
@@ -1,75 +1,21 @@
-# Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::DomainName;
 use strict;
-$Bivio::Type::DomainName::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::DomainName::VERSION;
+use Bivio::Base 'Bivio::Type::Name';
 
-=head1 NAME
-
-Bivio::Type::DomainName - internet domain names
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::DomainName;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Name>
-
-=cut
-
-use Bivio::Type::Name;
-@Bivio::Type::DomainName::ISA = ('Bivio::Type::Name');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::DomainName> desribes a domain name.  Allows dotted
-decimal names (ip numbers).
-
-=cut
-
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="REGEXP"></a>
-
-=head2 REGEXP : regexp_ref
-
-Returns regular expression used for validating.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_REGEXP) = __PACKAGE__->REGEXP;
 
 sub REGEXP {
+    # : regexp_ref
+    # Returns regular expression used for validating.
     return qr/^(?:[-a-z0-9]{1,63})(?:\.[-a-z0-9]{1,63})+$/is;
 }
 
-#=IMPORTS
-
-#=VARIABLES
-my($_REGEXP) = __PACKAGE__->REGEXP;
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value) : any
-
-Downcases result of super and validates against dotted decimal.
-
-=cut
-
 sub from_literal {
+    # (proto, string) : any
+    # Downcases result of super and validates against dotted decimal.
     my($value, $err) = shift->SUPER::from_literal(@_);
     return ($value, $err)
 	unless defined($value);
@@ -78,28 +24,10 @@ sub from_literal {
     return lc($value);
 }
 
-=for html <a name="get_width"></a>
-
-=head2 static get_width() : int
-
-Max host name is 255.
-
-=cut
-
 sub get_width {
+    # (proto) : int
+    # Max host name is 255.
     return 255.
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
