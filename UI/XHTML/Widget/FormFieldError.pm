9b44224cec347a7d4a76ad031d8239b1a3dcbf91
Rob Nagler <github@q33.us>
2008-04-05 15:05:52 +0000

    simplified

--- UI/XHTML/Widget/FormFieldError.pm
+++ UI/XHTML/Widget/FormFieldError.pm
@@ -2,54 +2,39 @@
 # $Id$
 package Bivio::UI::XHTML::Widget::FormFieldError;
 use strict;
-use base 'Bivio::UI::HTML::Widget::Tag';
-use Bivio::UI::HTML::Widget::ControlBase;
+use Bivio::Base 'XHTMLWidget.Tag';
+use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-
-sub internal_as_string {
-    my($self) = @_;
-    return $self->unsafe_get('field');
-}
-
-sub internal_new_args {
-    shift;
-    return Bivio::UI::HTML::Widget::ControlBase->internal_compute_new_args(
-	[qw(field)],
-	\@_,
-    );
-}
+my($_F) = __PACKAGE__->use('UI.Facade');
 
 sub initialize {
     my($self) = @_;
-    return if $self->unsafe_get('value');
-    $self->put(
+    $self->initialize_attr(label => '');
+    $self->put_unless_exists(
         tag => 'div',
 	class => 'field_err',
-	control => [
-	    ['->get_request'], $self->ancestral_get('form_class'),
-	    '->get_field_error', $self->get('field'),
-	],
-	value => [sub {
-	    my($source, $model, $field, $label) = @_;
-	    return Bivio::UI::Facade->get_from_request_or_self(
-		$source->get_request,
-	    )->get_or_default(
-		'FormError', 'Bivio::UI::HTML::FormErrors',
-	    )->to_html(
-		$source,
-		$model,
-		$field,
-		$label,
-		$model->get_field_error($field),
-	    );
-	}, [['->get_request'], $self->ancestral_get('form_class')],
-	   $self->get('field'),
-	   $self->get_or_default('label', ''),
-	],
+	control => IfFieldError($self->get('field')),
 	string_font => 0,
     );
     return shift->SUPER::initialize(@_);
+}
+
+sub render_tag_value {
+    my($self, $source, $buffer) = @_;
+    my($field) = $self->render_simple_attr(field => $source);
+    my($model) = $self->resolve_form_model($source);
+    $$buffer .= ($_F->get_from_request_or_self($source->req)
+	->unsafe_get('FormError')
+        || $self->use('Bivio::UI::HTML::FormErrors')
+    )->to_html(
+	$source,
+	$model,
+	$field,
+	$self->render_simple_attr(label => $source),
+	$model->get_field_error($field),
+    );
+    return;
 }
 
 1;
