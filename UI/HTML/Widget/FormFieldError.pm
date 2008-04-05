94ed564d85b4ffd56d23b6e1c072ec8a2e49b6ab
Rob Nagler <github@q33.us>
2008-04-05 15:05:39 +0000

    simplified and rmpod

--- UI/HTML/Widget/FormFieldError.pm
+++ UI/HTML/Widget/FormFieldError.pm
@@ -2,159 +2,35 @@
 # $Id$
 package Bivio::UI::HTML::Widget::FormFieldError;
 use strict;
-$Bivio::UI::HTML::Widget::FormFieldError::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::FormFieldError::VERSION;
+use Bivio::Base 'Widget.IfFieldError';
 
-=head1 NAME
-
-Bivio::UI::HTML::Widget::FormFieldError - an error message for a form field
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::FormFieldError;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::HTML::Widget::FormFieldError::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::FormFieldError> an error message for a form field
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item field : string (required)
-
-The model field name.
-
-=item form_model : array_ref (required, inherited, get_request)
-
-Which form are we dealing with.
-
-=item label: string
-
-=item label: array_ref
-
-The label string or widget value used in the error message.
-
-=back
-
-=cut
-
-#=IMPORTS
-use Bivio::UI::Font;
-use Bivio::UI::HTML::FormErrors;
-use Bivio::UI::FormError;
-
-#=VARIABLES
-
-my($_IDI) = __PACKAGE__->instance_data_index;
-
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new() : Bivio::UI::HTML::Widget::DescriptiveFormField
-
-Creates a new DescriptiveFormField widget.
-
-=cut
-
-sub new {
-    my($self) = shift->SUPER::new(@_);
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes the widgets internal structures.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_FCF) = __PACKAGE__->use('FacadeComponent.Font');
+my($_F) = __PACKAGE__->use('UI.Facade');
 
 sub initialize {
     my($self) = @_;
-    my($fields) = $self->[$_IDI];
-    $fields->{field} = $self->get('field');
-    $fields->{model} = $self->ancestral_get('form_model');
-    $fields->{label} = $self->unsafe_get('label') || '';
-    return;
+    $self->initialize_attr(label => '');
+    return shift->SUPER::initialize(@_);
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Appends the value of the widget to I<buffer>.
-
-=cut
-
-sub render {
+sub control_on_render {
     my($self, $source, $buffer) = @_;
-    my($fields) = $self->[$_IDI];
-    my($req) = $source->get_request;
-
-    # check for errors.  Multi-field fields (ref(field)) handle errors
-    # themselves.
-    my($model) = $req->get_widget_value(@{$fields->{model}});
-    if ($model->in_error && !ref($fields->{field})) {
-	my($errors) = $model->get_errors;
-
-	my($error) = $errors->{$fields->{field}};
-	if (defined($error)) {
-	    my($p, $s) = Bivio::UI::Font->format_html('form_field_error',
-		    $req);
-	    $$buffer .= $p
-		. Bivio::UI::Facade->get_from_request_or_self($req)
-		    ->get_or_default(
-			'FormError', 'Bivio::UI::HTML::FormErrors')
-		    ->to_html(
-			$source,
-			$model,
-			$fields->{field},
-			ref($fields->{label}) eq 'ARRAY'
-			    ? $source->get_widget_value(@{$fields->{label}})
-			    : $fields->{label},
-			$error,
-		    )
-		. $s
-		. "<br>\n";
-	}
-    }
+    my($req) = $source->req;
+    my($p, $s) = $_FCF->format_html('form_field_error', $req);
+    $$buffer .= $p
+	. ($_F->get_from_request_or_self($req)->unsafe_get('FormError')
+	    || $self->use('Bivio::UI::HTML::FormErrors')
+	)->to_html(
+	    $source,
+	    $self->resolve_form_model($source),
+	    $self->render_simple_attr(field => $source),
+	    $self->render_simple_attr(label => $source),
+	    $self->resolve_attr(control => $source),
+	)
+	. $s
+	. "<br />\n";
     return;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2000 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
