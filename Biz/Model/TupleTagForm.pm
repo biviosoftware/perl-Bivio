198f992a5ed754fec5613f6cc6a6e08a995b0c5f
Rob Nagler <github@q33.us>
2008-04-05 15:05:15 +0000

    if model doesn't load in execute_empty, just default values

--- Biz/Model/TupleTagForm.pm
+++ Biz/Model/TupleTagForm.pm
@@ -17,8 +17,10 @@ sub execute_empty {
 	$delegator,
 	sub {
 	    my($model) = @_;
+	    return 0
+		unless $delegator->unsafe_get_model($model)->is_loaded;
 	    $delegator->load_from_model_properties($model);
-	    return;
+	    return 1;
 	},
 	sub {
 	    my($model) = @_;
@@ -38,6 +40,7 @@ sub execute_ok {
     my($delegator) = shift->delegated_args(@_);
     _setup($delegator, sub {
 	$delegator->create_or_update_model_properties(shift);
+	return 1;
     });
     return;
 }
@@ -69,7 +72,6 @@ sub internal_initialize {
 		    name => $field,
 		    type => $_TST,
 		    $_FS->extract_column_from_classes($info, $id)->{in_list}
-
 			? (in_list => 1) : (),
 		};
 	    }),
@@ -87,6 +89,32 @@ sub tuple_tag_form_state {
     return shift->[$_IDI] ||= {};
 }
 
+sub tuple_tag_map_slots {
+    my($delegator, $id, $op) = shift->delegated_args(@_);
+    return _fields($delegator, sub {$op->(shift)}, $id);
+}
+
+sub tuple_tag_slot_choice_select_list {
+    my($delegator, $field) = shift->delegated_args(@_);
+    return $delegator->new_other('TupleSlotChoiceSelectList')
+	->load_all_from_slot_type(
+	    (_field_def_value($delegator, $field, 'tuple_slot_info') || {})
+		->{type},
+	);
+}
+
+sub tuple_tag_slot_has_choices {
+    my($delegator, $field) = shift->delegated_args(@_);
+    return 0
+	unless my $i = _field_def_value($delegator, $field, 'tuple_slot_info');
+    return $i->{type}->get('choices')->is_specified;
+}
+
+sub tuple_tag_slot_label {
+    my($delegator, $field) = shift->delegated_args(@_);
+    return _field_def_value($delegator, $field, 'TupleSlotDef.label') || '';
+}
+
 sub _defs {
     my($delegator, $field) = @_;
     my($prefix) = $_FS->extract_qualified_prefix($field);
@@ -100,22 +128,31 @@ sub _defs {
     return $field =~ /TupleTag$/ ? $d : $d->find_row_by_field_name($field);
 }
 
+sub _field_def_value {
+    my($delegator, $field, $which) = @_;
+    return undef
+	unless my $d = _defs($delegator, $field);
+    return undef
+	unless $d->find_row_by_field_name($field);
+    return $d->get($which);
+}
+
 sub _fields {
-    my($delegator, $op) = @_;
+    my($delegator, $op, $id) = @_;
     return _ids($delegator, sub {
 	my($id, $model) = @_;
         @{$_TSN->map_list(sub {
 	    my($slot) = @_;
 	    return $op->("$model.$slot", $id);
 	})};
-    });
+    }, $id);
 }
 
 sub _ids {
-    my($delegator, $op) = @_;
+    my($delegator, $op, $id) = @_;
     return [map(
 	$op->($_, $_FS->extract_qualified_prefix($_) . '.TupleTag'),
-	@{$delegator->TUPLE_TAG_IDS},
+	$id ? $id : @{$delegator->TUPLE_TAG_IDS},
     )];
 }
 
@@ -129,7 +166,7 @@ sub _setup {
 	return $else->($model)
 	    unless $id = $delegator->unsafe_get($id);
 	$delegator->internal_put_field("$model.primary_id" => $id);
-	$if->($model);
+	$if->($model) || $else->($model);
 	return;
     });
     return;
