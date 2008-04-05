59c7a8e9c45c493beb76b5ced3375d55056c7061
Rob Nagler <github@q33.us>
2008-04-05 07:47:25 +0000

    added tuple_slot_info field & MISSING_SLOT_INFO()

--- Biz/Model/TupleSlotDefList.pm
+++ Biz/Model/TupleSlotDefList.pm
@@ -2,23 +2,41 @@
 # $Id$
 package Bivio::Biz::Model::TupleSlotDefList;
 use strict;
-use base 'Bivio::Biz::ListModel';
+use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_TST) = __PACKAGE__->get_instance('TupleSlotType');
-my($_TSD) = __PACKAGE__->get_instance('TupleSlotDef');
-my($_TSN) = Bivio::Type->get_instance('TupleSlotNum');
-my($_EK) = __PACKAGE__->get_instance('TupleSlotChoiceSelectList')
-    ->EMPTY_KEY_VALUE;
+my($_EK) = __PACKAGE__->use('Model.TupleSlotChoiceSelectList')->EMPTY_KEY_VALUE;
+my($_TSD) = __PACKAGE__->use('Model.TupleSlotDef');
+my($_TSN) = __PACKAGE__->use('Type.TupleSlotNum');
+my($_TST) = __PACKAGE__->use('Model.TupleSlotType');
+my($_TTST) = __PACKAGE__->use('Type.TupleSlotType');
+my($_NOT_NULL) = __PACKAGE__->use('SQL.Constraint')->NOT_NULL;
+my($_NONE) = $_NOT_NULL->NONE;
+my($_MISSING) = {
+    type => $_TTST->new(
+	$_TTST->DEFAULT_CLASS,
+	__PACKAGE__->use('Type.TupleSlotArray')->new([]),
+    ),
+    constraint => $_NONE,
+};
 
 sub EMPTY_KEY_VALUE {
     #NOTE: You have to relax constraint on tuple_slot_num if you use this
     return -1;
 }
 
+sub MISSING_SLOT_INFO {
+    return $_MISSING;
+}
+
 sub field_from_num {
     my($self) = @_;
-    return  $_TSN->field_name($self->get('TupleSlotDef.tuple_slot_num'));
+    return $_TSN->field_name($self->get('TupleSlotDef.tuple_slot_num'));
+}
+
+sub find_row_by_field_name {
+    my($self, $field) = @_;
+    return $self->find_row_by_num($_TSN->field_name_to_num($field));
 }
 
 sub find_row_by_num {
@@ -39,8 +57,25 @@ sub internal_initialize {
 	other => [
 	    [qw(TupleSlotDef.tuple_slot_type_id TupleSlotType.tuple_slot_type_id)],
 	    @{$_TSD->LIST_FIELDS},
+	    {
+		name => 'tuple_slot_info',
+		type => 'Hash',
+		constraint => $_NONE,
+	    },
 	],
     });
+}
+
+sub internal_post_load_row {
+    my($self, $row) = @_;
+    $row->{tuple_slot_info} = {
+	type => $_TTST->new(
+	    $row->{'TupleSlotType.type_class'},
+	    $row->{'TupleSlotType.choices'},
+	),
+	constraint => $row->{'TupleSlotDef.is_required'} ? $_NOT_NULL : $_NONE,
+    };
+    return 1;
 }
 
 sub type_class_instance {
