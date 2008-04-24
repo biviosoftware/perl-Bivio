9aaa960761dace422c7640c9906938ba28cece2b
Paul Moeller <moeller@bivio.biz>
2008-04-24 19:20:16 +0000

    use dynamic superclass

--- Biz/Model/RealmUserAddForm.pm
+++ Biz/Model/RealmUserAddForm.pm
@@ -2,7 +2,7 @@
 # $Id$
 package Bivio::Biz::Model::RealmUserAddForm;
 use strict;
-use base 'Bivio::Biz::Model::UserRegisterForm';
+use Bivio::Base 'Model.UserRegisterForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
