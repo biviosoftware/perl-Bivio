4d30e0cd2044efe5b3e5463f4d828b2ac75da7b9
Paul Moeller <moeller@bivio.biz>
2008-04-28 16:10:28 +0000

    added can_check_in() can_checkout_out(), can_unlock()

--- Biz/Model/RealmFileTreeList.pm
+++ Biz/Model/RealmFileTreeList.pm
@@ -6,6 +6,8 @@ use Bivio::Base 'Model.TreeList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RF) = __PACKAGE__->use('Model.RealmFile');
+my($_RTK) = __PACKAGE__->use('Type.RowTagKey');
+my($_TLN) = __PACKAGE__->use('Type.TreeListNode');
 
 sub LOAD_ALL_SIZE {
     return 5000;
@@ -13,6 +15,25 @@ sub LOAD_ALL_SIZE {
 
 sub PARENT_NODE_ID_FIELD {
     return 'RealmFile.folder_id';
+}
+
+sub can_check_in {
+    my($self) = @_;
+    return 0 unless $self->is_file;
+    return $self->get('lock_user_id')
+	&& ($self->get('lock_user_id') eq $self->req('auth_user_id'))
+	? 1 : 0;
+}
+
+sub can_check_out {
+    my($self) = @_;
+    return 0 unless $self->is_file;
+    return $self->get('RealmFile.is_read_only') ? 0 : 1;
+}
+
+sub can_unlock {
+    my($self) = @_;
+    return $self->get('lock_user_id') && $self->can_check_in ? 1 : 0;
 }
 
 sub internal_default_expand {
@@ -31,17 +52,31 @@ sub internal_initialize {
 	    RealmFile.path_lc
 	    RealmFile.modified_date_time
             Email.email
+            RealmOwner_2.display_name
 	)],
 	other => [
 	    'RealmOwner.name',
 	    'RealmFile.path',
 	    'RealmFile.folder_id',
-            [qw(RealmFile.user_id Email.realm_id)],
+	    'RealmFile.is_read_only',
+            [qw(RealmFile.user_id Email.realm_id RealmOwner_2.realm_id)],
 	    'RealmFile.is_folder',
 	    {
 		name => 'base_name',
 		type => 'FilePath',
 		constraint => 'NONE',
+	    },
+	    ['Email.location', [$self->use('Model.Email')->DEFAULT_LOCATION]],
+	    {
+		name => 'lock_user_id',
+		type => 'User.user_id',
+		in_select => 1,
+		select_value => "(
+                    SELECT value
+                    FROM row_tag_t
+                    WHERE primary_id = realm_file_t.realm_file_id
+                    AND key = @{[$_RTK->REALM_FILE_LOCK->as_sql_param]}
+                 ) AS lock_user_id",
 	    },
 	],
     });
@@ -69,6 +104,8 @@ sub internal_parent_id {
 
 sub internal_post_load_row {
     my($self, $row) = @_;
+    $row->{node_state} = $_TLN->LOCKED_LEAF_NODE
+	if $row->{lock_user_id};
     $row->{base_name} = $row->{'RealmFile.path'} eq '/' ? '/'
 	: Bivio::Type::FileName->get_tail($row->{'RealmFile.path'});
     return 1;
@@ -76,7 +113,6 @@ sub internal_post_load_row {
 
 sub internal_prepare_statement {
     my($self, $stmt) = @_;
-    $stmt->EQ('Email.location', $self->get_instance('Email')->DEFAULT_LOCATION);
     # /Mail is probably large so we'll ignore it
     # dot-files are uninteresting, so we'll ignore them.
     # All are available via DAV
@@ -94,22 +130,26 @@ sub internal_root_parent_node_id {
 
 sub is_child_folder {
     my($self) = @_;
-    return is_folder($self)
-	&& $self->get_list_model->get('RealmFile.path_lc') ne '/';
+    return is_folder($self) && $self->get('RealmFile.path_lc') ne '/';
 }
 
 sub is_file {
     my($self) = @_;
-    return $self->get_list_model->get('RealmFile.is_folder') ? 0 : 1;
+    return $self->get('RealmFile.is_folder') ? 0 : 1;
 }
 
 sub is_folder {
     my($self) = @_;
-    return $self->get_list_model->get('RealmFile.is_folder');
+    return $self->get('RealmFile.is_folder');
+}
+
+sub is_root {
+    my($self) = @_;
+    return $self->get_cursor == 0;
 }
 
 sub is_text_content_type {
-    return $_RF->is_text_content_type(shift->get_list_model, 'RealmFile.');
+    return $_RF->is_text_content_type(shift, 'RealmFile.');
 }
 
 1;
