cd540cc6f2c3b1fe57348d5d532cc5afcc84e2d6
Rob Nagler <github@q33.us>
2008-04-30 05:07:22 +0000

    rmpod deprecated by View.Error

--- Biz/Action/Forbidden.pm
+++ Biz/Action/Forbidden.pm
@@ -1,57 +1,14 @@
-# Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2004-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Action::Forbidden;
 use strict;
-$Bivio::Biz::Action::Forbidden::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Biz::Action::Forbidden::VERSION;
-
-=head1 NAME
-
-Bivio::Biz::Action::Forbidden - handle forbidden redirects
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Biz::Action::Forbidden;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Biz::Action>
-
-=cut
-
-use Bivio::Biz::Action;
-@Bivio::Biz::Action::Forbidden::ISA = ('Bivio::Biz::Action');
-
-=head1 DESCRIPTION
-
-C<Bivio::Biz::Action::Forbidden>
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Biz.Action';
 use Bivio::Ext::ApacheConstants;
 
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="execute"></a>
-
-=head2 static execute(Bivio::Agent::Request req) : boolean
-
-Always returns true.  Sets forbidden output in text/html.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub execute {
+    # DEPRECATED: use View.Error
     my($proto, $req) = @_;
     my($reply) = $req->get('reply');
     $reply->set_http_status(Bivio::Ext::ApacheConstants->FORBIDDEN)
@@ -68,17 +25,5 @@ EOF
     $reply->set_output_type('text/html');
     return 1;
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
