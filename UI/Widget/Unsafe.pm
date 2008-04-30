4197cb00521f9f995fa2c426580a9dd6319aaf2b
Rob Nagler <github@q33.us>
2008-04-30 00:50:43 +0000

    *** empty log message ***

---
+++ UI/Widget/Unsafe.pm
@@ -0,0 +1,19 @@
+# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::Widget::Unsafe;
+use strict;
+use Bivio::Base 'Widget.Simple';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DIE) = __PACKAGE__->use('Bivio.Die');
+
+sub render {
+    my($self, $source, $buffer) = @_;
+    $_DIE->eval(sub {
+        $$buffer .= ${$self->render_attr(value => $source)};
+	return;
+    });
+    return;
+}
+
+1;
