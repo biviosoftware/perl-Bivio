35528bfb5b76ef4b06a997b7ff91ec1b861ecdde
Rob Nagler <github@q33.us>
2008-04-30 05:09:37 +0000

    rmpod & cruft

--- Biz/Model/ForbiddenForm.pm
+++ Biz/Model/ForbiddenForm.pm
@@ -2,66 +2,9 @@
 # $Id$
 package Bivio::Biz::Model::ForbiddenForm;
 use strict;
-$Bivio::Biz::Model::ForbiddenForm::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Biz::Model::ForbiddenForm::VERSION;
+use Bivio::Base 'Biz.FormModel';
 
-=head1 NAME
-
-Bivio::Biz::Model::ForbiddenForm - handles default forbidden
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Biz::Model::ForbiddenForm;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Biz::FormModel>
-
-=cut
-
-use Bivio::Biz::FormModel;
-@Bivio::Biz::Model::ForbiddenForm::ISA = ('Bivio::Biz::FormModel');
-
-=head1 DESCRIPTION
-
-C<Bivio::Biz::Model::ForbiddenForm>
-
-=cut
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item require_explicit_su : boolean
-
-If true for executing task, will not try to auto-logout super users.
-
-=back
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="execute_empty"></a>
-
-=head2 execute_empty()
-
-Called when a FORBIDDEN expection is thrown.  Context tells us about the
-calling realm.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub execute_empty {
     my($self) = @_;
@@ -94,30 +37,12 @@ sub execute_empty {
     return 'next';
 }
 
-=for html <a name="execute_unwind"></a>
-
-=head2 execute_unwind()
-
-Unwinds another level to the previous task.  Called after a successful login or
-su.
-
-=cut
-
 sub execute_unwind {
     my($self) = @_;
-    # Don't redirect too many times
     my($rc) = ($self->unsafe_get('redirect_count') || 0) + 1;
     $self->internal_put_field(redirect_count => $rc);
     return $rc < 2 ? $self->internal_redirect_next : 'forbidden_task';
 }
-
-=for html <a name="internal_initialize"></a>
-
-=head2 internal_initialize() : hash_ref
-
-Config
-
-=cut
 
 sub internal_initialize {
     my($self) = @_;
@@ -133,31 +58,5 @@ sub internal_initialize {
 	],
     });
 }
-
-#=PRIVATE SUBROUTINES
-
-# _really_forbidden(self) : any
-#
-# Returns forbidden_task or exists with forbidden status.
-#
-sub _really_forbidden {
-    my($self) = @_;
-    my($req) = $self->get_request;
-    return 'forbidden_task'
-	if $req->get('task')->unsafe_get('forbidden_task');
-    $req->get('reply')->set_http_status(
-	Bivio::Ext::ApacheConstants->FORBIDDEN
-    ) if $req->get('reply')->can('set_http_status');
-}
-
-=head1 COPYRIGHT
-
-Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
