fbd9f89ec1e4ef7abe4ee5b228b0810de6ff2e1f
Rob Nagler <github@q33.us>
2008-04-30 05:08:07 +0000

    support for any errors (driven by Action.Error)

--- UI/View/Error.pm
+++ UI/View/Error.pm
@@ -6,27 +6,21 @@ use Bivio::Base 'View.Base';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_AC) = __PACKAGE__->use('Ext.ApacheConstants');
 
-sub not_found {
-    view_pre_execute(sub {
-        my($req) = shift->get_request;
-	$req->put(go_back => undef);
-	$req->get('reply')->set_http_status($_AC->NOT_FOUND);
-	return unless my $r = $req->unsafe_get('r');
-	return unless $r = $r->header_in('Referer');
-	$req->put(go_back => $r);
-	return;
-    });
-    return shift->internal_body(DIV_not_found(Prose(<<'EOF')));
-The page requested is not a functioning page on our site.
-If(['go_back'],
-   Link('Go back to the previous page, and try again.', ['go_back']),
-   If(['auth_user'],
-      Link('Go back to your home page.', 'MY_SITE'),
-      Link('Go to the home page.', 'SITE_ROOT'),
-));
-EOF
+sub default {
+    return shift->internal_body(DIV_page_error(
+	Join([
+	    [sub {
+		 return vs_text_as_prose(
+		    'page_error.' . shift->req(qw(Action.Error status)));
+	    }],
+	    XLink(Cond(
+		['->req', 'Action.Error', 'uri'] => 'page_error_referer',
+		['auth_user'] => 'page_error_user',
+		1 => 'page_error_visitor',
+	    )),
+	]),
+    ));
 }
 
 1;
