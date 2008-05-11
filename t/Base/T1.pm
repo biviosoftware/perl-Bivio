b039d8e783f0084b5d35b09dec6a72e576d9b1f1
Rob Nagler <github@q33.us>
2008-05-11 13:26:51 +0000

    added test_b_die/info

--- t/Base/T1.pm
+++ t/Base/T1.pm
@@ -5,5 +5,19 @@ use strict;
 use Bivio::Base 'Type.String';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_A) = __PACKAGE__->use('IO.Alert');
+
+sub test_b_die {
+    b_die('here');
+    # DOES NOT RETURN
+}
+
+sub test_b_info {
+    my($msg) = @_;
+    $_A->set_printer(sub {$msg = shift});
+    b_info('xyz');
+    $_A->set_printer('STDERR');
+    return $msg;
+}
 
 1;
