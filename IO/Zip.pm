32c5965068c2da1b5811938caae692751f0d76c0
Paul Moeller <moeller@bivio.biz>
2008-06-07 22:31:24 +0000

    initial revision

---
+++ IO/Zip.pm
@@ -0,0 +1,103 @@
+# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::IO::Zip;
+use strict;
+use Bivio::Base 'Bivio::UNIVERSAL';
+use Archive::Zip ();
+use Bivio::IO::File;
+use Bivio::IO::Trace;
+use Bivio::MIME::Type;
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+$Bivio::IO::Zip::IN = undef;
+my($_IDI) = __PACKAGE__->instance_data_index;
+
+sub add_file {
+    # Adds the named file to the archive. This is the preferable way to add a
+    # file because it is not brought into memory.
+    my($self, $path, $name) = @_;
+    my($fields) = $self->[$_IDI];
+    _trace($path, ' ', $name) if $_TRACE;
+    Bivio::Die->die('file not found for zip: ', $path)
+        unless -e $path;
+    $fields->{zip}->addFile($path, $name);
+    return $self;
+}
+
+sub add_string {
+    # Adds the in-memory file contents to the archive.
+    my($self, $contents, $name) = @_;
+    my($fields) = $self->[$_IDI];
+    _trace($name) if $_TRACE;
+    $fields->{zip}->addString($contents, $name)
+        ->desiredCompressionMethod(Archive::Zip::COMPRESSION_DEFLATED);
+    return $self;
+}
+
+sub get_member_count {
+    my($self) = @_;
+    my($fields) = $self->[$_IDI];
+    return $fields->{zip}->numberOfMembers;
+}
+
+sub iterate_members {
+    # Iterates each member of the zip file, calling
+    # handler->(member, contents).
+    # Stops iteratation if handler returns false.
+    my($self, $handler) = @_;
+    my($fields) = $self->[$_IDI];
+
+    foreach my $member ($fields->{zip}->members) {
+        last unless $handler->($member->fileName,
+            $fields->{zip}->contents($member));
+    }
+    return $self;
+}
+
+sub new {
+    my($proto) = shift;
+    my($self) = $proto->SUPER::new(@_);
+    $self->[$_IDI] = {
+        zip => Archive::Zip->new,
+    };
+    Archive::Zip::setErrorHandler(sub {Bivio::Die->die(shift)});
+    return $self;
+}
+
+sub read_zip_from_string {
+    # Loads the instance from an in-memory value.
+    my($self, $zip_contents) = @_;
+    my($fields) = $self->[$_IDI];
+    my($file) = Bivio::IO::File->temp_file(Bivio::Agent::Request->get_current);
+    Bivio::IO::File->write($file, $zip_contents);
+    $fields->{zip} = Archive::Zip->new;
+    Bivio::Die->die('failed to read zip from string')
+	unless $fields->{zip}->read($file) == Archive::Zip::AZ_OK();
+    return $self;
+}
+
+sub send_zip_to_client {
+    # Saves the zip file to disk, sets the file handle for the request output.
+    my($self, $req) = @_;
+    my($file_name) = Bivio::IO::File->temp_file($req);
+    $self->write_to_file($file_name);
+    my($reply) = $req->get('reply');
+    $reply->set_output_type(Bivio::MIME::Type->from_extension($file_name));
+    my($fh) = \*Bivio::IO::Zip::IN;
+    open($fh, '< ' . $file_name)
+        || Bivio::Die->die('file open failed: ', $file_name);
+    $reply->set_output($fh);
+    return $self;
+}
+
+sub write_to_file {
+    my($self, $filename) = @_;
+    my($fields) = $self->[$_IDI];
+    Bivio::Die->die('failed to write zip file')
+        unless $fields->{zip}->writeToFileNamed($filename)
+            == Archive::Zip::AZ_OK();
+    return $self;
+}
+
+
+1;
