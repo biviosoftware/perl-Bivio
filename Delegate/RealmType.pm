fb82d299b4cd590ce59e0cb3907b08aca96e524b
Rob Nagler <github@q33.us>
2008-06-11 19:39:49 +0000

    rmpod & added "any" alias for UNKNOWN

--- Delegate/RealmType.pm
+++ Delegate/RealmType.pm
@@ -2,107 +2,37 @@
 # $Id$
 package Bivio::Delegate::RealmType;
 use strict;
-$Bivio::Delegate::RealmType::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Delegate::RealmType::VERSION;
+use Bivio::Base 'Bivio.Delegate';
 
-=head1 NAME
+# C<Bivio::Delegate::RealmType> implements the common RealmTypes in bOP.
+#
+# You should extend this class if you have new RealmTypes in your application.
+# The numbers 0-19 are reserved by this module so your first RealmType would
+# look like:
+#
+#     sub get_delegate_info {
+# 	my($proto) = @_;
+# 	return [
+#             @{$proto->SUPER::get_delegate_info},
+# 	    MY_NEW_TYPE => [
+# 	        20,
+# 		undef,
+# 		'access to some new type of realm',
+# 	    ],
+#         ];
+#     }
 
-Bivio::Delegate::RealmType - Realm types
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Delegate::RealmType;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Delegate>
-
-=cut
-
-use Bivio::Delegate;
-@Bivio::Delegate::RealmType::ISA = ('Bivio::Delegate');
-
-=head1 DESCRIPTION
-
-C<Bivio::Delegate::RealmType> implements the common RealmTypes in bOP.
-
-You should extend this class if you have new RealmTypes in your application.
-The numbers 0-19 are reserved by this module so your first RealmType would
-look like:
-
-    sub get_delegate_info {
-	my($proto) = @_;
-	return [
-            @{$proto->SUPER::get_delegate_info},
-	    MY_NEW_TYPE => [
-	        20,
-		undef,
-		'access to some new type of realm',
-	    ],
-        ];
-    }
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_delegate_info"></a>
-
-=head2 get_delegate_info() : array_ref
-
-Returns standard realm types.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_delegate_info {
     return [
-	UNKNOWN => [
-	    0,
-	    undef,
-	    'realm has yet to be established',
-	],
-	GENERAL => [
-	    1,
-	    undef,
-	    'access to general areas (not club or user specific)',
-	],
-	USER => [
-	    2,
-	    undef,
-	    'access to a particular user',
-	],
-	CLUB => [
-	    3,
-	    undef,
-	    'access to a particular club',
-	],
-	FORUM => [4],
-	CALENDAR_EVENT=> [5],
+	UNKNOWN => [0, 'any'],
+	GENERAL => 1,
+	USER => 2,
+	CLUB => 3,
+	FORUM => 4,
+	CALENDAR_EVENT=> 5,
     ];
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
