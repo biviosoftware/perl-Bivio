9f8bd4a64445308f832db52e161bd66af3712d63
Rob Nagler <github@q33.us>
2008-06-14 23:20:02 +0000

    moved delete_output up from AgentHTTP.Reply

--- Agent/Reply.pm
+++ Agent/Reply.pm
@@ -1,49 +1,21 @@
-# Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Agent::Reply;
 use strict;
-$Bivio::Agent::Reply::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Agent::Reply::VERSION;
+use Bivio::Base 'Collection.Attributes';
 
-=head1 NAME
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-Bivio::Agent::Reply - a user agent reply
+sub delete_output {
+    my($self) = @_;
+    my($output) = $self->unsafe_get('output');
+    $self->delete('output');
+    return $output;
+}
 
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-   use Bivio::Agent::Reply;
-
-=cut
-
-use Bivio::Collection::Attributes;
-@Bivio::Agent::Reply::ISA = qw(Bivio::Collection::Attributes);
-
-=head1 DESCRIPTION
-
-C<Bivio::Agent::Reply> is the complement to
-L<Bivio::Agent::Request>, it is the output channel.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new() : Bivio::Agent::Reply
-
-Creates a reply in an error state with the 'text/plain' output type.
-
-=cut
+sub get_output_type {
+    return shift->get('output_type');
+}
 
 sub new {
     return shift->SUPER::new({
@@ -51,41 +23,9 @@ sub new {
     });
 }
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_output_type"></a>
-
-=head2 get_ouput_type() : string
-
-Returns the reply format type.
-
-=cut
-
-sub get_output_type {
-    return shift->get('output_type');
-}
-
-=for html <a name="send"></a>
-
-=head2 send(Bivio::Agent::Request req)
-
-Sends the buffered reply data.
-
-=cut
-
 sub send {
     return;
 }
-
-=for html <a name="set_header"></a>
-
-=head2 set_header(string name, string value) : self
-
-Sets an arbitrary header value.
-
-=cut
 
 sub set_header {
     my($self, $name, $value) = @_;
@@ -93,43 +33,12 @@ sub set_header {
     return $self;
 }
 
-=for html <a name="set_output"></a>
-
-=head2 set_output(scalar_ref value) : self
-
-=head2 set_output(io_handle file) : self
-
-Sets the output to the file.  Output type must be set.
-I<file> or I<value> will be owned by this method.
-
-=cut
-
 sub set_output {
     return shift->put(output => shift);
 }
 
-=for html <a name="set_output_type"></a>
-
-=head2 set_output_type(string type) : self
-
-Sets the reply format type. For example this could be 'text/html'.
-
-=cut
-
 sub set_output_type {
     return shift->put(output_type => shift);
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
