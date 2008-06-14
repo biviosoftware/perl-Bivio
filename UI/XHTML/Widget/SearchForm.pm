59a7fa2d3068ba38e86d10a389381cf2779ed4f7
Rob Nagler <github@q33.us>
2008-06-14 22:55:46 +0000

    allow dynamic image_form_button and text_size

--- UI/XHTML/Widget/SearchForm.pm
+++ UI/XHTML/Widget/SearchForm.pm
@@ -6,18 +6,25 @@ use Bivio::Base 'XHTMLWidget.Form';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_TI) = __PACKAGE__->use('Agent.TaskId');
+my($_SEARCH_LIST) = b_use('Agent.TaskId')->SEARCH_LIST;
 
 sub initialize {
     my($self) = @_;
     $self->map_invoke(put_unless_exists => [
-	[action => $_TI->SEARCH_LIST],
+	[action => $_SEARCH_LIST],
 	[form_class => 'SearchForm'],
+	[want_hidden_fields => 0],
+	[form_method => 'GET'],
 	[value => sub {
-	    return Join([
-		Text({field => 'search', size => 30}),
-		ImageFormButton(qw(ok_button magnifier go)),
-	    ]);
+	     return Join([
+		 Text({
+		     field => 'search',
+		     size => $self->get_or_default(text_size => 30),
+		 }),
+		 $self->get_or_default(image_form_button =>
+		     ImageFormButton(qw(ok_button magnifier go)),
+		 ),
+	     ]);
 	}],
 	[class => 'search'],
     ]);
