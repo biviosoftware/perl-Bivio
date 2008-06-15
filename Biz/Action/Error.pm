a2f810e7f532bff36f18f5ffc694360e5bc17b29
Rob Nagler <github@q33.us>
2008-06-15 00:06:16 +0000

    simplified and made more robust to errors related to not having SiteForum or wikiview Changed name of text attributes to ActionError.wiki_name

--- Biz/Action/Error.pm
+++ Biz/Action/Error.pm
@@ -5,10 +5,13 @@ use strict;
 use Bivio::Base 'Action.EmptyReply';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_C) = b_use('IO.Config');
 my($_SF) = b_use('Action.SiteForum');
+my($_FC) = b_use('FacadeComponent.Constant');
 my($_T) = b_use('FacadeComponent.Text');
 my($_V) = b_use('UI.View');
 my($_WV) = b_use('Action.WikiView');
+my($_WARNINGS) = {};
 
 sub execute {
     my($proto, $req) = @_;
@@ -16,28 +19,38 @@ sub execute {
     $status =~ s/^default_error_redirect_?//s;
     $status ||= 'server_error';
     my($r);
-    $proto->new({
+    my($self) = $proto->new({
 	status => $status,
 	uri => ($r = $req->unsafe_get('r') and $r = $r->header_in('Referer')),
     })->put_on_request($req);
-    my($die) = Bivio::Die->catch(sub {
-        $proto->execute_wiki($req);
-    });
+    my($reply) = $req->get('reply');
+    $reply->delete_output;
+    my($die) = Bivio::Die->catch_quietly(sub {_wiki($self, $req)});
+    Bivio::IO::Alert->warn($status, ': wiki rendering error: ', $die)
+        if $die && !$_WARNINGS->{$status}++;
     $_V->execute('Error->default', $req)
-        if $die || ! $req->get('reply')->unsafe_get_output;
+        if $die || ! $reply->unsafe_get_output;
     return $proto->SUPER::execute($req, uc($status));
 }
 
-sub execute_wiki {
-    my($proto, $req) = @_;
-    return 0
-        unless $req->get('task_id')->is_component_included('wiki');
-    $_SF->execute($req);
-    $req->put(path_info => $_T->get_value(
-        'ErrorWiki.file_path', $req->get_nested(qw(Action.Error status))));
-    $_WV->execute_prepare_html($req);
-    $_V->execute('Wiki->site_view', $req);
-    return 0;
+sub _wiki {
+    my($self, $req) = @_;
+    return
+	unless $_C->if_version(6);
+    return $req->with_realm(
+	$_FC->get_value('site_realm_id', $req),
+	sub {
+	    my($wn) = $_T->get_from_source($req)
+		->unsafe_get_value(
+		    'ActionError.wiki_name', $self->get('status'));
+	    return
+		unless $wn;
+	    $req->put(path_info => $wn);
+	    $_WV->execute_prepare_html($req);
+	    $_V->execute('Wiki->site_view', $req);
+	    return;
+	},
+    );
 }
 
 1;
