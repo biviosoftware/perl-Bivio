851caa3c3ad6314764652cb5674112437eaae9e0
Rob Nagler <github@q33.us>
2008-06-17 04:14:30 +0000

    added rss support

--- Biz/Model/CalendarEventList.pm
+++ Biz/Model/CalendarEventList.pm
@@ -2,11 +2,26 @@
 # $Id$
 package Bivio::Biz::Model::CalendarEventList;
 use strict;
-use base 'Bivio::Biz::ListModel';
-use Bivio::MIME::Calendar;
+use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_CE) = Bivio::Biz::Model->get_instance('CalendarEvent');
+my($_CE) = b_use('Model.CalendarEvent');
+my($_T) = b_use('Type.Time');
+my($_D) = b_use('Type.Date');
+my($_UTC) = b_use('Type.TimeZone')->UTC;
+my($_DT) = b_use('Type.DateTime');
+my($_MC) = b_use('MIME.Calendar');
+
+sub get_modified_date_time {
+    return shift->get('CalendarEvent.modified_date_time');
+}
+
+sub get_rss_summary {
+    my($self) = @_;
+#TODO: Modularize as Search.Parser
+    return $self->get('CalendarEvent.location')
+	. ' - ' . $self->get('CalendarEvent.description');
+}
 
 sub internal_initialize {
     my($self) = @_;
@@ -42,6 +57,21 @@ sub internal_initialize {
 		name => 'dtend_in_tz',
 		type => 'DateTime',
 	    },
+	    {
+		name => 'title_',
+		type => 'Line',
+		constraint => 'NOT_NULL',
+	    },
+	    {
+		name => 'path_info',
+		type => 'FilePath',
+		constraint => 'NONE',
+	    },
+	    {
+		name => 'query',
+		type => 'Line',
+		constraint => 'NOT_NULL',
+	    },
 	],
     });
 }
@@ -49,11 +79,24 @@ sub internal_initialize {
 sub internal_post_load_row {
     my($self, $row) = @_;
     $row->{uid} = $_CE->id_to_uid($row->{'CalendarEvent.calendar_event_id'});
-    my($tz) = $row->{'CalendarEvent.time_zone'};
-    $row->{'dtstart_in_tz'} = $tz ? $tz->date_time_from_utc(
-	$row->{'CalendarEvent.dtstart'}) : undef;
-    $row->{'dtend_in_tz'} = $tz ? $tz->date_time_from_utc(
-	$row->{'CalendarEvent.dtend'}) : undef;
+    my($tz) = $row->{'CalendarEvent.time_zone'} || $_UTC;
+    $row->{'dtstart_in_tz'}
+	= $tz->date_time_from_utc($row->{'CalendarEvent.dtstart'});
+    $row->{'dtend_in_tz'}
+	= $tz->date_time_from_utc($row->{'CalendarEvent.dtend'});
+    $row->{path_info} = undef;
+    $row->{query} = $self->get_query->format_uri_for_this(
+	$self->internal_get_sql_support,
+	[$row->{'CalendarEvent.calendar_event_id'}],
+    );
+#TODO: Enscapsulate with T
+    $row->{title} = join(' - ',
+	$row->{'RealmOwner.display_name'},
+	grep($_ =~ s/GMT|UTC// || 1,
+	     $_DT->to_string($row->{'dtstart_in_tz'}),
+	     $_DT->to_string($row->{'dtend_in_tz'}),
+	),
+    );
     return 1;
 }
 
@@ -61,7 +104,7 @@ sub update_from_ics {
     my($self, $ics) = @_;
     my($old) = {map(($_->{uid} => $_), @{$self->map_iterate})};
     my($ce) = $self->new_other('CalendarEvent');
-    foreach my $v (@{Bivio::MIME::Calendar->from_ics($ics)}) {
+    foreach my $v (@{$_MC->from_ics($ics)}) {
         if (my $x = delete($old->{$v->{uid}})) {
 	    $ce->load({calendar_event_id => $x->{'CalendarEvent.calendar_event_id'}})
 		->update_from_vevent($v);
