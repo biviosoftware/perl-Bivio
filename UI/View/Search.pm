b82b1011ffcdbf83b920db4c4dafd3467f5594be
Rob Nagler <github@q33.us>
2008-06-17 02:09:53 +0000

    simplify results to be like google

--- UI/View/Search.pm
+++ UI/View/Search.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2007-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::View::Search;
 use strict;
@@ -7,29 +7,32 @@ use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub internal_byline_control {
+    return 1;
+}
+
 sub list {
-    return shift->internal_body(
-	Join([
-	    vs_paged_list(SearchList => [
-		['RealmFile.modified_date_time', {
-		    column_data_class => 'last_update',
-		    column_heading_class => 'last_update',
-		}],
-		'result_who',
-		['result_title', {
-		    column_data_class => 'long_text',
-		    column_widget => Link(
-			String(['result_title']),
-			['result_uri'],
-		    ),
-		}],
-		['result_excerpt', {
-		    column_data_class => 'long_text item',
-		    column_heading_class => 'item',
-		}],
-	    ]),
-	]),
-    );
+    my($self) = @_;
+    return $self->internal_body(vs_paged_list(SearchList => [
+	['result_title', {
+	    column_widget => Link(
+		Join([
+		    SPAN_title(String(['result_title'])),
+		    SPAN_excerpt(String(['result_excerpt'])),
+		    SPAN_byline(Join([
+			SPAN_author(String(['result_author'])),
+			SPAN_date(DateTime(['RealmFile.modified_date_time'])),
+		    ]), {
+			control => $self->internal_byline_control,
+		    }),
+		]),
+		['result_uri'],
+	    ),
+	}],
+    ], {
+	class => 'paged_list search_results',
+	show_headings => 0,
+    })),
 }
 
 1;
