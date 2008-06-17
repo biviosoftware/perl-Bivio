252d10dd84afc2834dc662b04b7515f4f81643c0
Rob Nagler <github@q33.us>
2008-06-17 02:07:09 +0000

    Use new_excerpt to generate excerpts

--- Biz/Model/SearchList.pm
+++ Biz/Model/SearchList.pm
@@ -2,17 +2,15 @@
 # $Id$
 package Bivio::Biz::Model::SearchList;
 use strict;
-use base 'Bivio::Biz::ListModel';
-use Bivio::Search::Xapian;
+use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_BFN) = __PACKAGE__->use('Type.BlogFileName');
-my($_MFN) = __PACKAGE__->use('Type.MailFileName');
-my($_P) = __PACKAGE__->use('Search.Parseable');
-my($_RF) = __PACKAGE__->use('SearchParser.RealmFile');
-my($_WN) = __PACKAGE__->use('Type.WikiName');
-my($_WDN) = __PACKAGE__->use('Type.WikiDataName');
-my($_FP) = __PACKAGE__->use('Type.FilePath');
+my($_BFN) = b_use('Type.BlogFileName');
+my($_MFN) = b_use('Type.MailFileName');
+my($_S) = b_use('Type.String');
+my($_WDN) = b_use('Type.WikiDataName');
+my($_WN) = b_use('Type.WikiName');
+my($_X) = b_use('Search.Xapian');
 my($_REALM_FILE_FIELDS) = [qw(
     realm_file_id
     path
@@ -26,35 +24,27 @@ my($_REALM_OWNER_FIELDS) = [qw(
     name
     display_name
 )];
-my($_A) = __PACKAGE__->use('Mail.Address');
-
-sub RESULT_EXCERPT_LENGTH {
-    return 250;
-}
-
-sub internal_realm_ids {
-    my($self, $query) = @_;
-    return (
-	$self->get_request->map_user_realms(
-	    sub {shift->{'RealmUser.realm_id'}}),
-        1,
-    );
-}
+my($_FP) = b_use('Type.FilePath');
 
 sub internal_initialize {
     my($self) = @_;
-    return Bivio::Search::Xapian->query_list_model_initialize(
+    return $_X->query_list_model_initialize(
 	$self,
 	$self->merge_initialize_info($self->SUPER::internal_initialize, {
 	    other => [
 		@{$self->internal_initialize_local_fields(
-		    [qw(result_uri result_title result_excerpt result_who)],
+		    [qw(result_uri result_title result_excerpt result_author)],
 		    'Text', 'NOT_NULL',
 		)},
 		map(+{name => $_, in_select => 0},
 		    map("RealmOwner.$_", @$_REALM_OWNER_FIELDS),
-	            map("RealmFile.$_", @$_REALM_FILE_FIELDS),
+#	            map("RealmFile.$_", @$_REALM_FILE_FIELDS),
 		),
+		{
+		    name => 'model',
+		    constraint => 'NOT_NULL',
+		    type => 'Biz.PropertyModel',
+		},
 	    ],
         }),
     );
@@ -64,11 +54,15 @@ sub internal_load_rows {
     my($self, $query) = @_;
     my($s, $pn, $c) = $query->unsafe_get(qw(search page_number count));
     return []
-	unless defined((Bivio::Type::String->from_literal($s))[0]);
-    my($rows) = Bivio::Search::Xapian->query(
-	$s, ($pn - 1) * $c, $c + 1,
-	$self->internal_realm_ids($query),
-    );
+	unless defined(($_S->from_literal($s))[0]);
+    my($rows) = $_X->query({
+	phrase => $s,
+	offset => ($pn - 1) * $c,
+	length => $c + 1,
+	private_realm_ids => $self->internal_private_realm_ids($query),
+	public_realm_ids => $self->internal_public_realm_ids($query),
+	want_all_public => $self->internal_want_all_public($query),
+    });
     if (@$rows > $c) {
 	$query->put(
 	    has_next => 1,
@@ -85,10 +79,10 @@ sub internal_load_rows {
 
 sub internal_post_load_row {
     my($self, $row) = @_;
-    my($rf) = $self->new_other('RealmFile');
+    my($m) = $self->new_other($row->{simple_class});
     # There's a possibility that the the search db is out of sync with db
-    return $rf->unauth_load({realm_file_id => $row->{primary_id}})
-	? $self->internal_post_load_row_with_model($row, $rf)
+    return $m->unauth_load({$m->get_primary_id_name => $row->{primary_id}})
+	? $self->internal_post_load_row_with_model($row, $m)
 	: 0;
 }
 
@@ -101,18 +95,22 @@ sub internal_post_load_row_with_model {
 #      verify they are here.  Can save the info above in
 #      internal_get_realm_ids
 #TODO: Optimize by caching realms
-    my($ro) = $model->new_other('RealmOwner')
-	->unauth_load_or_die({realm_id => $model->get('realm_id')});
+    my($ro) = $model->new_other('RealmOwner') ->unauth_load_or_die({
+	realm_id => $model->get_auth_id,
+    });
     foreach my $f (@$_REALM_OWNER_FIELDS) {
 	$row->{"RealmOwner.$f"} = $ro->get($f);
     }
-    $row->{result_who}
+    $row->{result_title} = $_FP->get_tail($model->unsafe_get('path') || '');
+    $row->{result_author}
 	= $ro->unauth_load_or_die({realm_id => $row->{'RealmFile.user_id'}})
 	->get('display_name');
     my($req) = $model->req;
-    my($other);
+    my($realm_mail);
     $row->{result_uri} = $req->format_uri(
+# Order needs to be clear.  Possibly by registration order or dependencies?
 	$_BFN->is_absolute($row->{'RealmFile.path'}) ? {
+# What if I removed realm from the task?  What would happen?
 	    task_id => 'FORUM_BLOG_DETAIL',
 	    realm => $row->{'RealmOwner.name'},
 	    query => undef,
@@ -134,13 +132,14 @@ sub internal_post_load_row_with_model {
 		        ? 'FORUM_CRM_THREAD_LIST' : 'FORUM_MAIL_THREAD_LIST',
 		    realm => $row->{'RealmOwner.name'},
 		    query => {
-			'ListQuery.parent_id' => ($other = $model
-			    ->new_other('RealmMail')
+			'ListQuery.parent_id' => (
+			    $realm_mail = $model->new_other('RealmMail')
 			    ->load({
 				realm_file_id =>
 				$row->{'RealmFile.realm_file_id'},
 			    }))->get('thread_root_id'),
 		    },
+#TODO: Integrate with View.Mail->internal_part_list (need <a name=>)
 		    anchor => $row->{'RealmFile.realm_file_id'},
 		};
 	    },
@@ -151,7 +150,21 @@ sub internal_post_load_row_with_model {
 	    path_info => $row->{'RealmFile.path'},
 	},
     );
-    _excerpt($proto, $row, $model, $other);
+    _excerpt($model, $realm_mail, $row);
+    return 1;
+}
+
+sub internal_private_realm_ids {
+    my($self, $query) = @_;
+    return $self->get_request->map_user_realms(
+	sub {shift->{'RealmUser.realm_id'}});
+}
+
+sub internal_public_realm_ids {
+    return [];
+}
+
+sub internal_want_all_public {
     return 1;
 }
 
@@ -169,49 +182,17 @@ sub parse_query_from_request {
 }
 
 sub _excerpt {
-    my($proto, $row, $model, $other_model) = @_;
-    my($x) = $_RF->parse($_P->new($model));
-    $row->{result_title} = $_FP->get_tail($model->get('path'));
-    return $row->{result_excerpt} = ''
-	unless $x;
-    $row->{result_title} = $x->{title}
-	if length($x->{title});
-    my($max) = $proto->RESULT_EXCERPT_LENGTH;
-    my($num_words) = $max/6;
-    my($words) = [grep(
-	length($_),
-	split(
-	    ' ',
-	    ref($other_model) =~ /RealmMail/
-		? _trim_mail_header($x->{text}, $row)
-		: ${$x->{text}},
-	    $num_words,
-	),
-    )];
-    pop(@$words)
-	if @$words >= $num_words;
-    $max *= 1.5;
-    my($trimmed_text) = 0;
-
-    while (1) {
-	last if length($row->{result_excerpt} = join(' ', @$words)) < $max;
-	pop(@$words);
-	$trimmed_text = 1;
+    my($model, $realm_mail, $row) = @_;
+    my($p) = $_X->excerpt_model($model);
+    foreach my $n (qw(excerpt title author)) {
+	my($v) = $p && $p->unsafe_get($n);
+	$row->{"result_$n"} = $v
+	    if defined($v) && length($v);
     }
-    $row->{result_excerpt} .= '...'
-	if $trimmed_text;
+    $row->{result_title} = $realm_mail->get('subject')
+	if $realm_mail;
     return;
-}
 
-sub _trim_mail_header {
-    my($v, $row) = @_;
-    my($h, $b) = split(/\n\n/, $$v, 2);
-    if ($h =~ /^from:\s+(.+)/mi) {
-	my($a, $n) = $_A->parse($1);
-	$row->{'result_who'} = $n
-	    if $n ||= ($a =~ /(.+?)\@/)[0];
-    }
-    return $b;
 }
 
 1;
