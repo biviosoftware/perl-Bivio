16a2285e661ddd8962b061b2a9f76735ea0551f9
Rob Nagler <github@q33.us>
2008-07-01 22:11:34 +0000

    can_iterate is false for all lists of this type

--- Biz/Model/RoleBaseList.pm
+++ Biz/Model/RoleBaseList.pm
@@ -9,6 +9,7 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+	can_iterate => 0,
 	other => [{
 	    name => 'roles',
 	    type => 'Array',
