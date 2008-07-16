dffc4b53d87ed9850992aef7dc816a0fe2c10891
Eric Dobbs <dobbs@bivio.biz>
2008-07-16 22:52:54 +0000

    USAGE now provides a default usage string.  Subclasses are no longer required to override it.  Commands for that string come from the new shell_commands() function which finds the methods in the $proto symbol table.  Does not yet find methods on parent ShellUtils.
    Subclasses can now specify command usage by creating foo_USAGE methods
    that return the usage details for that command.  For example:
    sub score_USAGE {'type year -- set the current scoring year'}

--- t/ShellUtil/T2.pm
+++ t/ShellUtil/T2.pm
@@ -2,12 +2,22 @@
 # $Id$
 package Bivio::t::ShellUtil::T2;
 use strict;
-use base 'Bivio::ShellUtil';
+use Bivio::Base 'Bivio.ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub my_package {
     return shift->simple_package_name . "\n";
+}
+
+sub f2_USAGE {'x -- something wonderful'}
+sub f2 {
+    my($x) = @_;
+    return $x x 2;
+}
+
+sub f1 {
+    return;
 }
 
 1;
