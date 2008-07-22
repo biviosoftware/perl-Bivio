9341da89a9467e303d25b34fc40bb7bd22faac35
Rob Nagler <github@q33.us>
2008-07-22 16:19:59 +0000

    added ability to register handlers

--- Test/Reload.pm
+++ Test/Reload.pm
@@ -2,8 +2,7 @@
 # $Id$
 package Bivio::Test::Reload;
 use strict;
-use base 'Bivio::UNIVERSAL';
-use Bivio::IO::ClassLoader;
+use Bivio::Base 'Bivio::UNIVERSAL';
 use File::Find ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
@@ -13,21 +12,31 @@ my($_WATCH) = [grep(s{/BConf.pm$}{}, @{[values(%INC)]})];
 #TODO: Use "relative_path", which I think File::Find uses
 my($_INC) = [map($_ eq '.' ? '' : "$_/", @INC)];
 Bivio::IO::Alert->info('Watching: ', $_WATCH);
+my($_HANDLERS) = b_use('Biz.Registrar')->new;
+my($_CL) = b_use('IO.ClassLoader');
 
 sub handler {
     if (my $modified = _modified()) {
+	$_HANDLERS->call_fifo(handle_unload_class => [$modified]);
 	_do(delete_require => $modified);
 	_do(simple_require => $modified);
+	$_HANDLERS->call_fifo(handle_reload_class => [$modified]);
 	$_LAST_TIME = time;
     }
     return 1;
+}
+
+sub register_handler {
+    shift;
+    $_HANDLERS->push_object(@_);
+    return;
 }
 
 sub _do {
     my($method, $modules) = @_;
     foreach my $m (@$modules) {
 	Bivio::IO::Alert->info($method, ': ', $m);
-	Bivio::IO::ClassLoader->$method($m);
+	$_CL->$method($m);
     }
     return;
 }
