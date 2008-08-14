328f43a656eb1fce4b3ad95733f09946957bf7be
Rob Nagler <github@q33.us>
2008-08-14 03:46:37 +0000

    use Bivio::Base

--- Type/ForumEmailMode.pm
+++ Type/ForumEmailMode.pm
@@ -2,16 +2,16 @@
 # $Id$
 package Bivio::Type::ForumEmailMode;
 use strict;
-use base 'Bivio::Type::Enum';
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 __PACKAGE__->compile([
-    UNKNOWN => [0, "Select Option"],
-    DEFAULT => [1, "Forum Members"],
-    ADMIN_ONLY_FORUM_EMAIL => [2, "Forum Administrators"],
-    SYSTEM_USER_FORUM_EMAIL => [3, "Any User"],
-    PUBLIC_FORUM_EMAIL => [4, "Anyone"],
+    UNKNOWN => [0, 'Select Option'],
+    DEFAULT => [1, 'Forum Members'],
+    ADMIN_ONLY_FORUM_EMAIL => [2, 'Forum Administrators'],
+    SYSTEM_USER_FORUM_EMAIL => [3, 'Any User'],
+    PUBLIC_FORUM_EMAIL => [4, 'Anyone'],
 ]);
 
 sub OPTIONAL_MODES {
