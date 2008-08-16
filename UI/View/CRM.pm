c5da38629c1defd3215018374ff925ec0c669fd3
Rob Nagler <github@q33.us>
2008-08-16 04:48:05 +0000

    don't render update_only for new tickets deprecate $buttons passed to send_form

--- UI/View/CRM.pm
+++ UI/View/CRM.pm
@@ -8,22 +8,34 @@ use Bivio::UI::ViewLanguageAUTOLOAD;
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub internal_crm_send_form_buttons {
-    return '*ok_button update_only cancel_button';
+    return StandardSubmit({
+	buttons => Join([
+	    'ok_button',
+ 	    If([qw(! ->req form_model is_new)],
+ 	       'update_only',
+ 	    ),
+	    'cancel_button',
+	], {join_separator => ' '}),
+    });
 }
 
 sub internal_crm_send_form_extra_fields {
-    my($self, $no_action) = @_;
+    my($self, $p) = shift->name_parameters([qw(action label field)], \@_);
     my($form) = b_use('Model', $self->internal_name . 'Form')->get_instance;
     return [
-	$no_action ? () : [$form->simple_package_name . '.action_id', {
+        [$form->simple_package_name . '.action_id', {
 	    wf_class => 'Select',
 	    list_display_field => 'name',
 	    choices => ['->req', 'Model.CRMActionList'],
 	    list_id_field => 'id',
+	    %{$p->{action} || {}},
 	}],
 	@{$form->tuple_tag_map_slots(sub {
 	    my($field) = @_;
-	    return [TupleTagSlotLabel($field), TupleTagSlotField($field)];
+	    return [
+		TupleTagSlotLabel($field, $p->{label}),
+		TupleTagSlotField($field, $p->{field}),
+	    ];
 	})},
     ];
 }
@@ -34,6 +46,8 @@ sub internal_reply_list {
 
 sub send_form {
     my($self, $extra_fields, $buttons) = @_;
+    Bivio::IO::Alert->warn_deprecated('use internal_crm_send_form_buttons')
+        if $buttons;
     $extra_fields ||= $self->internal_crm_send_form_extra_fields;
     $buttons ||= $self->internal_crm_send_form_buttons;
     return $self->SUPER::send_form(
