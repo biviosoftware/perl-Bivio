34c83a2b3706c68e40f691f4f4009deda6b51a6c
Rob Nagler <github@q33.us>
2008-09-04 22:23:31 +0000

    rmpod

--- Type/ECPaymentStatusSet.pm
+++ Type/ECPaymentStatusSet.pm
@@ -2,83 +2,18 @@
 # $Id$
 package Bivio::Type::ECPaymentStatusSet;
 use strict;
-$Bivio::Type::ECPaymentStatusSet::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::ECPaymentStatusSet::VERSION;
+use Bivio::Base 'Type.EnumSet';
 
-=head1 NAME
-
-Bivio::Type::ECPaymentStatusSet - set of ECPaymentStatus
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::ECPaymentStatusSet;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::EnumSet>
-
-=cut
-
-use Bivio::Type::EnumSet;
-@Bivio::Type::ECPaymentStatusSet::ISA = ('Bivio::Type::EnumSet');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::ECPaymentStatusSet> is a set of
-L<Bivio::Type::ECPaymentStatus|Bivio::Type::ECPaymentStatus>.
-
-=cut
-
-#=IMPORTS
-use Bivio::Type::ECPaymentStatus;
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->initialize;
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_enum_type"></a>
-
-=head2 get_enum_type() : Bivio::Type::Enum
-
-Returns L<Bivio::Auth::Permission|Bivio::Auth::Permission>.
-
-=cut
+my($_ECPS) = b_use('Type.ECPaymentStatus');
 
 sub get_enum_type {
-    return 'Bivio::Type::ECPaymentStatus';
+    return $_ECPS
 }
-
-=for html <a name="get_width"></a>
-
-=head2 get_width() : int
-
-Returns 4 (32 bits).
-
-=cut
 
 sub get_width {
     return 4;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All Rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
