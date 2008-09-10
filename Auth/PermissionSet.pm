05804396bf431cbcb617dae8c767ec2b36cd17b4
Rob Nagler <github@q33.us>
2008-09-10 21:47:41 +0000

    use super class code

--- Auth/PermissionSet.pm
+++ Auth/PermissionSet.pm
@@ -1,48 +1,19 @@
-# Copyright (c) 1999-2007 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2008 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Auth::PermissionSet;
 use strict;
 use Bivio::Base 'Type.EnumSet';
-use Bivio::Auth::Permission;
-
-# C<Bivio::Auth::PermissionSet> is the storage format for the
-# task permissions.  Each element is a
-# L<Bivio::Auth::Permission|Bivio::Auth::Permission>.
-# A task may require more than one permission.
-# See L<Bivio::Agent::Task|Bivio::Agent::Task> and
-# L<Bivio::Auth::Realm|Bivio::Auth::Realm>
-# for more details.
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_P) = b_use('Auth.Permission');
 __PACKAGE__->initialize();
 
-sub clear {
-    my($proto, $perm_set, @arg_bits) = @_;
-    # Return a permission set with the specified bits cleared
-    my(@bits) = map({ref($_)
-        ? $_ : Bivio::Auth::Permission->from_literal($_)} @arg_bits);
-    return ${Bivio::Type::EnumSet->clear(\$perm_set, @bits)};
-}
-
 sub get_enum_type {
-    # Returns L<Bivio::Auth::Permission|Bivio::Auth::Permission>.
-    return 'Bivio::Auth::Permission';
+    return $_P;
 }
 
 sub get_width {
-    # Returns 15.  That's 120 permissions.
     return 15;
-}
-
-sub includes {
-    my($proto, $perm_set, @perm_name) = @_;
-    # Returns true if this permission set includes perm_name.
-
-    foreach my $name (@perm_name) {
-        return 1
-            if $proto->is_set($perm_set, Bivio::Auth::Permission->$name());
-    }
-    return 0;
 }
 
 1;
