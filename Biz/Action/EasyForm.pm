47e6ffa68a99e10f62f303c4cd4167e373b4aeaa
Rob Nagler <github@q33.us>
2008-09-10 21:49:09 +0000

    Relaxed existence contraints: columns and files added automatically Sends email to EASY_FORM_UPDATE_MAIL_TO or site_contact

--- Biz/Action/EasyForm.pm
+++ Biz/Action/EasyForm.pm
@@ -2,139 +2,116 @@
 # $Id$
 package Bivio::Biz::Action::EasyForm;
 use strict;
-use base 'Bivio::Biz::Action';
-use Bivio::Util::CSV;
+use Bivio::Base 'Biz.Action';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_DT) = __PACKAGE__->use('Type.DateTime');
-my($_FP) = __PACKAGE__->use('Type.FilePath');
+my($_CSV) = b_use('ShellUtil.CSV');
+my($_M) = b_use('Biz.Model');
+my($_RI) = b_use('Agent.RequestId');
+my($_FCT) = b_use('FacadeComponent.Task');
+my($_FP) = b_use('Type.FilePath');
+my($_C) = b_use('FacadeComponent.Constant');
+my($_DT) = b_use('Type.DateTime');
+my($_MT) = b_use('MIME.Type');
+my($_V) = b_use('UI.View');
 
 sub execute {
     my($proto, $req) = @_;
-    my($m) = $req->get('r')->method;
-    $req->throw_die(CORRUPT_FORM => {
-	message => 'must be HTTP POST',
-        entity => $m,
-    }) unless lc($m) eq 'post';
-    my($form, $dir, $rf, $fields, $has_error, $error_msg);
-    if (my $die = Bivio::Die->catch(sub {
-	# Make sure we really do have a uri for this task, and only
-	# get the last part.  This is extra safety so we couldn't possibly
-	# write in the wrong folder.
-	$dir = ($req->format_uri({query => '', path_info => ''})
-		    =~ m{([^/]+)$})[0] . '/';
-	$form = _form_fields($req, $dir);
-	($rf, $fields) = _csv_fields($req, $dir);
-	my($f) = {%$form};
-	$rf->append_content(
-	    Bivio::Util::CSV->to_csv_text([
-		[map(delete($f->{$_}), @$fields)],
-	    ]),
-	);
-	Bivio::Die->die([sort(keys(%$f))], ': unexpected fields submitted')
-	    if grep(!/^\&/, keys(%$f));
-	return;
-    })) {
-	$rf ||= Bivio::Biz::Model->new($req, 'RealmFile');
-	Bivio::Die->die($dir, ': directory not found: ', $req)
-	    unless $dir && $rf->unsafe_load({
-		path => $dir,
-		is_folder => 1,
-	    });
-	my($p) = "${dir}Error.log";
-	$rf->unsafe_load({path => $p})
-	    || $rf->create_with_content({
-		path => $p,
-		user_id => Bivio::Biz::Model::RealmUser->new
-		    ->get_any_online_admin->get('realm_id'),
-	    }, \(''));
-	my $keys = [sort(keys(%$form))]
-	    if $form;
-	$error_msg = $dir . $req->get('path_info')
-	    . '.csv: failed with error: '
-	    . ($die->get('attrs')->{message}
-		   || (($die->get('code')->eq_model_not_found
-		       ? 'CSV file not found'
-		       : $die->get('code')->as_string)
-		       . "\n"))
-	    . (($form ? ${Bivio::Util::CSV->to_csv_text([
-		$keys,
-		[map($form->{$_}, @$keys)],
-	    ])} : '<no form data>') . "\n");
-	$rf->append_content(\($error_msg));
-	$has_error = 1;
+    $req->assert_http_method('post');
+    $_FCT->assert_uri($req->get('task_id'), $req);
+    my($dir) = $_C->get_value('easyform_dir', $req);
+    my($rf) = $_M->new($req, 'RealmFile');
+    b_die($req->get('path_info'), ': invalid Forms path')
+	unless my $path
+	= $_FP->get_clean_tail($rf->parse_path($req->get('path_info')));
+    $path = $_FP->join($dir, "$path.csv");
+    my($headings) = _headings($rf, $path);
+    my($form) = _form($rf);
+    my($d) = ${$rf->get_content};
+    my($new_headings) = 0;
+    foreach my $k (sort(map($_, keys(%$form)))) {
+	next
+	    if grep($_ eq $k, @$headings);
+	$new_headings = 1;
+	push(@$headings, $k);
     }
-    my($rn) = $req->get_nested(qw(auth_realm owner name));
-    my($fn) = $has_error ? '/Error.log' : $req->get('path_info') . '.csv';
-    $req->put(easy_form => Bivio::Collection::Attributes
-	->new({
-	    mail_to => $rn,
-	    file_path => '/' . $rn . '/file/' . (($dir =~ /(.*)\/$/)[0]) . $fn,
-	    error => $has_error,
-	    error_msg => $error_msg,
-	    labels => $fields,
-	    values => defined($form) ? {%$form} : {},
-	}));
+    $d =~ s{^.*?\n}{_headings_csv($headings)}es
+	if $new_headings;
+    $d .= ${$_CSV->to_csv_text([[map($form->{$_}, @$headings)]])};
+    $rf->update_with_content({user_id => $rf->get('user_id')}, \$d);
+    $proto->new({
+	file_path => $rf->get('path'),
+	to => $rf->new_other('RowTag')->get_value('EASY_FORM_UPDATE_MAIL_TO')
+	    || $rf->new_other('EmailAlias')->format_realm_as_incoming(
+		$rf->new_other('RealmOwner')->unauth_load_or_die({
+		    realm_id => $_C->get_value('site_contact_realm_id')})),
+	hash_list => $rf->new_other('HashList')->load_from_hash(
+	    $form, $headings),
+    })->put_on_request($req);
+    $_V->execute('EasyForm->update_mail', $req);
     return {
 	uri => $req->get('query')->{goto},
 	query => undef,
     };
 }
 
-sub _csv_fields {
-    my($req, $dir) = @_;
-    my($rf) = Bivio::Biz::Model->new($req, 'RealmFile');
-    return (
-	$rf->load({path => $dir . $req->get('path_info') . '.csv'}),
-	[map(lc($_),
-	     @{Bivio::Util::CSV->parse(
-		 (${$rf->get_content} =~ /^([^\n]+)/)[0])->[0]})],
-    );
-}
-
-sub _form_fields {
-    my($req, $dir) = @_;
-    my($form) = $req->get_form();
-    Bivio::Die->die('undefined form submitted')
-	unless defined($form);
-    $form = {
-	map((lc($_) => _to_string(lc($_), $form->{$_}, $dir, $req)),
-	    keys(%$form)),
+sub _form {
+    my($rf) = @_;
+    my($e) = $rf->new_other('Email');
+    my($uid) = $rf->req->get('auth_user_id');
+    my($form) = $rf->req->get_form || {};
+    return {
+	map(_form_value($form, $_, $rf), keys(%$form)),
+	'&date' => $_DT->now_as_string,
+	'&client_addr' => $rf->req->get('client_addr'),
+	'&email' => $e->unauth_load({realm_id => $uid})
+	    ? $e->get('email') : '',
     };
-    $form->{'&date'} = Bivio::Type::DateTime->now_as_string;
-    $form->{'&client_addr'} = $req->get('client_addr');
-    my($e) = Bivio::Biz::Model->new($req, 'Email');
-    $form->{'&email'} = $req->get('auth_user')
-	&& $e->unauth_load({realm_id => $req->get('auth_user_id')})
-	? $e->get('email') : '';
-    return $form;
 }
 
-sub _to_string {
-    my($name, $value, $dir, $req) = @_;
-    return $value
-	unless ref($value);
-    Bivio::Die->die($value, ': unknown reference format')
-	unless ref($value) eq 'HASH';
-    my($rf) = Bivio::Biz::Model->new($req, 'RealmFile')
-	->load({path => $dir . $req->get('path_info')});
-    Bivio::Die->die($rf->get('path'), ': must be a folder to receive files')
-	unless $rf->get('is_folder');
-    return ''
-	unless defined($_FP->get_clean_tail($value->{filename}));
-    return $rf->create_with_content({
-	path => $_FP->join(
-	    $rf->get('path'),
-	    join('-',
-		$_DT->now_as_file_name,
-	        $name,
-		$_FP->get_clean_tail($value->{filename}),
+sub _form_value {
+    my($form, $name, $rf) = @_;
+    my($value) = $form->{$name};
+    $name = lc($name);
+#TODO: Windows safety is an issue (exe, zip, gif, pif?)
+    return ($name => !ref($value) ? $value : $rf->req->format_http({
+	task_id => 'FORUM_FILE',
+	query => undef,
+	path_info => $rf->new->create_with_content({
+	    user_id => $rf->get('user_id'),
+	    path => $_FP->join(
+		$_FP->delete_suffix($rf->get('path')),
+		$_RI->current($rf->req)
+		    . '-'
+		    . $name
+		    . '.'
+		    . ($_MT->to_extension($value->{content_type})
+			   || $_MT->UNKNOWN_EXTENSION),
 	    ),
-	),
-	user_id => $req->get('auth_user_id')
-	    || Bivio::Biz::Model::RealmUser->new
-		    ->get_any_online_admin->get('realm_id'),
-    }, $value->{content})->get('path');
+	}, $value->{content})->get('path'),
+    }));
+}
+
+sub _headings {
+    my($rf, $path) = @_;
+    return [map(
+	lc($_),
+	@{$_CSV->parse((${$rf->get_content} =~ /^([^\n]+)/)[0])->[0]},
+    )] if $rf->unsafe_load({path => $path});
+    $rf->create_with_content(
+	{
+	    user_id => $rf->new_other('RealmUser')
+		->get_any_online_admin->get('realm_id'),
+	    path => $path,
+	},
+	\(''),
+    );
+    return [];
+}
+
+sub _headings_csv {
+    my($headings) = @_;
+    return ${$_CSV->to_csv_text([$headings])};
 }
 
 1;
