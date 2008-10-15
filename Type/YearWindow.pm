55c734fb638e1b3bb1ab81b9ac922b9f8e0610a2
Paul Moeller <moeller@bivio.biz>
2008-10-15 22:39:31 +0000

    removed is_continuous()

--- Type/YearWindow.pm
+++ Type/YearWindow.pm
@@ -2,7 +2,7 @@
 # $Id$
 package Bivio::Type::YearWindow;
 use strict;
-use Bivio::Base 'Bivio::Type::Enum';
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_D) = __PACKAGE__->use('Type.Date');
@@ -14,9 +14,5 @@ __PACKAGE__->compile([
 	("Y$_" => [$_, $_]),
     } ($_NOW .. $_NOW + 9)),
 ]);
-
-sub is_continuous {
-    return 0;
-}
 
 1;
