ab929beced8bdabaf9a826215e7406dbe9468d23
Paul Moeller <moeller@bivio.biz>
2008-10-15 20:17:28 +0000

    added UNKNOWN and get_two_digit_value()

--- Type/Month.pm
+++ Type/Month.pm
@@ -2,42 +2,11 @@
 # $Id$
 package Bivio::Type::Month;
 use strict;
-$Bivio::Type::Month::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::Month::VERSION;
+use Bivio::Base 'Type.Enum';
 
-=head1 NAME
-
-Bivio::Type::Month - enumerated months
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::Month;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Enum>
-
-=cut
-
-use Bivio::Type::Enum;
-@Bivio::Type::Month::ISA = ('Bivio::Type::Enum');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::Month>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile([
+    UNKNOWN => [0, 'Select Month'],
     JANUARY => [1, 'Jan', 'January', '01'],
     FEBRUARY => [2, 'Feb', 'February', '02'],
     MARCH => [3, 'Mar', 'March', '03'],
@@ -53,20 +22,9 @@ __PACKAGE__->compile([
 ]);
 
 
-=head1 METHODS
-
-=cut
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
+sub get_two_digit_value {
+    my($self) = @_;
+    return sprintf("%.2d", $self->as_int);
+}
 
 1;
