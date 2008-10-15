08c69dd0d519102135bf60678325130027f990c5
Paul Moeller <moeller@bivio.biz>
2008-10-15 22:39:46 +0000

    put implementation back - can't derive from an enum

--- Type/ECCreditCardExpYear.pm
+++ Type/ECCreditCardExpYear.pm
@@ -2,10 +2,19 @@
 # $Id$
 package Bivio::Type::ECCreditCardExpYear;
 use strict;
-use Bivio::Base 'Type.YearWindow';
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-#TODO: remove this class - use superclass instead
+#TODO: remove this class - use YearWindow instead
+my($_D) = __PACKAGE__->use('Type.Date');
+my($_NOW) = $_D->get_part($_D->now, 'year');
+
+# 10 year window from current year forward, ex. (Y2004 => [2004, 2004])
+__PACKAGE__->compile([
+    map({
+	("Y$_" => [$_, $_]),
+    } ($_NOW .. $_NOW + 9)),
+]);
 
 1;
