1068aa9be23f708862dd25b7954b9f2961d59800
Rob Nagler <github@q33.us>
2008-11-10 00:59:19 +0000

    use self_or_any_group and map_user_realms

--- Biz/Action/RealmlessRedirect.pm
+++ Biz/Action/RealmlessRedirect.pm
@@ -14,41 +14,29 @@ sub execute {
     return $us->equals_by_name('JUST_VISITOR')
 	? 'visitor_task'
 	: $req->get('auth_user')
-	? _set_realm($proto, $req, $t->get('home_task'))
+	? _set_realm($proto, $req, $t->get_attr_as_id('home_task'))
 	|| 'unauth_task'
 	: Bivio::Agent::TaskId->LOGIN;
 }
 
 sub _choose_realm {
-    my($proto, $user_realms, $task) = @_;
-    my($res);
-    my($rt) = $task->get('realm_type');
-    $user_realms->do_rows(sub {
-        my($row) = shift->get_shallow_copy;
-	$res = $row
-	    if $rt->equals($row->{'RealmOwner.realm_type'})
-	    && (!$res
-	    || $_DT->compare(
-		$res->{'RealmUser.creation_date_time'},
-		$row->{'RealmUser.creation_date_time'},
-	    ) < 0
-	    || ($res->{'RealmOwner.name'} cmp $row->{'RealmOwner.name'}) < 0);
-        return 1;
-    });
+    my($proto, $req, $task) = @_;
+    my($res) = sort {
+	$_DT->compare(
+	    $a->{'RealmUser.creation_date_time'},
+	    $b->{'RealmUser.creation_date_time'})
+	|| $a->{'RealmOwner.name'} cmp $b->{'RealmOwner.name'};
+    } @{$req->map_user_realms(
+	sub {shift},
+	{'RealmOwner.realm_type' => $task->get('realm_type')->self_or_any_group},
+    )};
     return $res && $res->{'RealmUser.realm_id'};
 }
 
 sub _set_realm {
     my($proto, $req, $task) = @_;
     $task = Bivio::Agent::Task->get_by_id($task);
-    $req->set_realm(
-	_choose_realm(
-	    $proto,
-	    Bivio::Biz::Model->new($req, 'UserRealmList')
-	        ->unauth_load_all({auth_id => $req->get('auth_user_id')}),
-	    $task,
-	) || return
-    );
+    $req->set_realm(_choose_realm($proto, $req, $task) || return);
     return $task->get('id');
 }
 
