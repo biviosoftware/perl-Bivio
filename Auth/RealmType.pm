d1ea053845f96d2a41eb43b7fdf1e1cd5118994b
Rob Nagler <github@q33.us>
2008-11-10 00:58:23 +0000

    added equals_or_any_group_check, get_any_group_list, and self_or_any_group

--- Auth/RealmType.pm
+++ Auth/RealmType.pm
@@ -21,11 +21,28 @@ sub as_property_model_class_name {
     return $_S->to_camel_case_identifier(shift->get_name);
 }
 
+sub equals_or_any_group_check {
+    my($self, $match) = @_;
+    $match ||= $self->ANY_GROUP;
+    return $self == $match
+	|| $match->eq_any_group && grep($self == $_, $self->get_any_group_list)
+	? 1 : 0;
+}
+
+sub get_any_group_list {
+    return grep(!$_->eq_user, shift->get_non_zero_list);
+}
+
 sub is_default_id {
     my($proto, $id) = @_;
     $id = $_I->from_literal_or_die($id);
     return grep($id == $_->as_default_owner_id, $proto->get_non_zero_list)
 	? 1 : 0;
+}
+
+sub self_or_any_group {
+    my($self) = @_;
+    return [$self->eq_any_group ? $self->get_any_group_list : $self];
 }
 
 1;
