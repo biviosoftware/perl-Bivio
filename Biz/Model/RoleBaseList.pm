8c30c02c1fc245175c673f445536249bcdf860c1
Rob Nagler <github@q33.us>
2008-11-11 23:03:25 +0000

    added LOAD_ALL_SIZE and PAGE_SIZE

--- Biz/Model/RoleBaseList.pm
+++ Biz/Model/RoleBaseList.pm
@@ -5,11 +5,24 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_PAGE_SIZE) = b_use('Type.PageSize')->get_default
+    * b_use('Auth.Role')->get_overlap_count;
+my($_LOAD_ALL_SIZE) = __PACKAGE__->SUPER::LOAD_ALL_SIZE
+    * b_use('Auth.Role')->get_overlap_count;
+
+sub LOAD_ALL_SIZE {
+    return $_LOAD_ALL_SIZE;
+}
+
+sub PAGE_SIZE {
+    return $_PAGE_SIZE;
+}
 
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
 	can_iterate => 0,
+	version => 1,
 	other => [{
 	    name => 'roles',
 	    type => 'Array',
