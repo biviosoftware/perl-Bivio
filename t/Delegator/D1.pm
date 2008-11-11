724599608301e4795967893b11a5f2227a2a736c
Rob Nagler <github@q33.us>
2008-11-11 16:33:59 +0000

    rmpod

--- t/Delegator/D1.pm
+++ t/Delegator/D1.pm
@@ -1,57 +1,9 @@
-# Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2004-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::t::Delegator::D1;
 use strict;
-$Bivio::t::Delegator::D1::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::t::Delegator::D1::VERSION;
+use Bivio::Base 'Bivio.Delegator';
 
-=head1 NAME
-
-Bivio::t::Delegator::D1 - test delegator
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::t::Delegator::D1;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Delegator>
-
-=cut
-
-use Bivio::Delegator;
-@Bivio::t::Delegator::D1::ISA = ('Bivio::Delegator');
-
-=head1 DESCRIPTION
-
-C<Bivio::t::Delegator::D1>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 1;
--- t/Delegator/I1.pm
+++ t/Delegator/I1.pm
@@ -2,57 +2,14 @@
 # $Id$
 package Bivio::t::Delegator::I1;
 use strict;
-$Bivio::t::Delegator::I1::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::t::Delegator::I1::VERSION;
+use Bivio::Base 'Bivio.Delegate';
 
-=head1 NAME
-
-Bivio::t::Delegator::I1 - test impl
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::t::Delegator::I1;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Delegate>
-
-=cut
-
-use Bivio::Delegate;
-@Bivio::t::Delegator::I1::ISA = ('Bivio::Delegate');
-
-=head1 DESCRIPTION
-
-C<Bivio::t::Delegator::I1>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 
-
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static_echo new() : Bivio::t::Delegator::I1
-
-Does the new thing.
-
-=cut
-
 sub new {
+    # (proto) : Delegator.I1
+    # Does the new thing.
     my($proto) = shift;
     my($self) = $proto->SUPER::new;
     $self->[$_IDI] = {
@@ -61,47 +18,19 @@ sub new {
     return $self;
 }
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="static_echo"></a>
-
-=head2 static_echo(any arg) : any
-
-Echo arg.
-
-=cut
-
 sub static_echo {
+    # (proto, any) : any
+    # Echo arg.
     shift;
     return @_;
 }
 
-=for html <a name="value"></a>
-
-=head2 value() : any
-
-Returns value passed in.
-
-=cut
-
 sub value {
+    # (self) : any
+    # Returns value passed in.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     return $fields->{value};
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
