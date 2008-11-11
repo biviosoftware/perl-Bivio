a5733550074d877dfb771cad54bda3b2ee7407c3
Rob Nagler <github@q33.us>
2008-11-11 23:06:00 +0000

    added get_overlap_count

--- Auth/Role.pm
+++ Auth/Role.pm
@@ -7,6 +7,10 @@ use Bivio::Base 'Type.EnumDelegator';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile;
 
+sub get_overlap_count {
+    return int(shift->get_non_zero_list / 2);
+}
+
 sub is_continuous {
     return 0;
 }
