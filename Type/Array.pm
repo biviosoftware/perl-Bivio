542925efd45dfe94ec9c16161214ec7d2e1dd3c1
Rob Nagler <github@q33.us>
2008-11-14 20:13:36 +0000

    rmpod

--- Type/Array.pm
+++ Type/Array.pm
@@ -2,58 +2,18 @@
 # $Id$
 package Bivio::Type::Array;
 use strict;
-$Bivio::Type::Array::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::Array::VERSION;
-
-=head1 NAME
-
-Bivio::Type::Array - array utilities, not a true type
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::Array;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type>
-
-=cut
-
-use Bivio::Type;
-@Bivio::Type::Array::ISA = ('Bivio::Type');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::Array> is a collection of array utilities and
-a string representable type.  Not all conversions are supported.
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::Type';
 use Bivio::Die;
 
-#=VARIABLES
+# C<Bivio::Type::Array> is a collection of array utilities and
+# a string representable type.  Not all conversions are supported.
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="bsearch_numeric"></a>
-
-=head2 static bsearch_numeric(array_ref values, int to_find) : array
-
-Searches for I<to_find> in I<values> and returns an array
-of the result and nearest key.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub bsearch_numeric {
+    # (proto, array_ref, int) : array
+    # Searches for I<to_find> in I<values> and returns an array
+    # of the result and nearest key.
     my(undef, $key, $array) = @_;
     my($upper) = $#$array;
     my($lower) = 0;
@@ -77,81 +37,45 @@ sub bsearch_numeric {
     return (0, $middle);
 }
 
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value)
-
-Splits on commas surround by any amount of whitespace.
-
-=cut
-
 sub from_literal {
+    # (proto, string) : undef
+    # Splits on commas surround by any amount of whitespace.
     my($proto, $value) = @_;
     return $value
 	? [split(/\s*,\s*/, $value)]
 	: undef;
 }
 
-=for html <a name="from_sql_column"></a>
-
-=head2 static from_sql_column(string param) : array_ref
-
-Splits on $; and returns an array_ref (sometimes empty).
-
-=cut
-
 sub from_sql_column {
+    # (proto, string) : array_ref
+    # Splits on $; and returns an array_ref (sometimes empty).
     my(undef, $param) = @_;
     return defined($param) ? [split(/$;/, $param)] : [];
 }
 
-=for html <a name="get_width"></a>
-
-=head2 static get_width() : int
-
-Returns 4000.
-
-=cut
-
 sub get_width {
+    # (proto) : int
+    # Returns 4000.
     return 4000;
 }
 
-=for html <a name="to_literal"></a>
-
-=head2 static to_literal(array_ref value) : string
-
-Returns printable string.
-
-=cut
-
 sub to_literal {
+    # (proto, array_ref) : string
+    # Returns printable string.
     my($proto, $value) = @_;
     return join(', ', $value ? @$value : ());
 }
 
-=for html <a name="to_query"></a>
-
-=head2 static static to_query(any value) : string
-
-B<NOT SUPPORTED>
-
-=cut
-
 sub to_query {
+    # (proto, any) : string
+    # B<NOT SUPPORTED>
     Bivio::Die->die('not supported');
 }
 
-=for html <a name="to_sql_param"></a>
-
-=head2 static to_sql_param(array_ref param_value) : string
-
-Returns a string from the array_ref.  Dies if the value
-contains $;.
-
-=cut
-
 sub to_sql_param {
+    # (proto, array_ref) : string
+    # Returns a string from the array_ref.  Dies if the value
+    # contains $;.
     my(undef, $param_value) = @_;
     # May be the empty string, which is same as C<undef>
     return $param_value ? join($;, map {
@@ -161,52 +85,22 @@ sub to_sql_param {
     } @$param_value) : undef;
 }
 
-=for html <a name="to_sql_param_list"></a>
-
-=head2 to_sql_param_list(array_ref values) : array_ref
-
-Not implemented.
-
-=cut
-
 sub to_sql_param_list {
+    # (self, array_ref) : array_ref
+    # Not implemented.
     die('not implemented');
 }
 
-=for html <a name="to_uri"></a>
-
-=head2 static to_uri(any value) : string
-
-B<NOT SUPPORTED>
-
-=cut
-
 sub to_uri {
+    # (proto, any) : string
+    # B<NOT SUPPORTED>
     Bivio::Die->die('not supported');
 }
-
-=for html <a name="to_xml"></a>
-
-=head2 static to_xml(any value) : string
-
-B<NOT SUPPORTED>
-
-=cut
 
 sub to_xml {
+    # (proto, any) : string
+    # B<NOT SUPPORTED>
     Bivio::Die->die('not supported');
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2000,2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
