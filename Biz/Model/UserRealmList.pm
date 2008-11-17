bd5882d59c1db031fa0f06cb6ffebe7d9d99ea84
Rob Nagler <github@q33.us>
2008-11-17 13:59:08 +0000

    RoleBaseList handles all RealmUser stuff

--- Biz/Model/UserRealmList.pm
+++ Biz/Model/UserRealmList.pm
@@ -11,22 +11,21 @@ sub find_row_by_type {
 }
 
 sub internal_initialize {
-    return {
-	version => 1,
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
 	order_by => [qw(
 	    RealmOwner.name
-	    RealmUser.role
 	)],
 	other => [qw(
 	    RealmOwner.realm_type
             RealmOwner.display_name
-	    RealmUser.creation_date_time
 	)],
 	primary_key => [
 	    [qw(RealmUser.realm_id RealmOwner.realm_id)],
 	],
 	auth_id => ['RealmUser.user_id'],
-    };
+    });
 }
 
 1;
