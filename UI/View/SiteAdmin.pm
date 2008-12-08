d9b2453a3d04f6fae4f9a3d87cea2b539b343951
Rob Nagler <github@q33.us>
2008-12-08 05:38:46 +0000

    unapproved_applicant_form; fix bug in user_list

--- UI/View/SiteAdmin.pm
+++ UI/View/SiteAdmin.pm
@@ -14,9 +14,40 @@ sub substitute_user_form {
     }]));
 }
 
+sub unapproved_applicant_form {
+    my($self, $extra_columns) = @_;
+    return shift->internal_body(vs_simple_form(UnapprovedApplicantForm => [
+	['UnapprovedApplicantForm.RealmUser.role', {
+	    choices => ['->req', 'Model.RoleSelectList'],
+	    list_display_field => 'display',
+	    list_id_field => 'RealmUser.role',
+	}],
+	@{$extra_columns || []},
+    ]));
+}
+
+sub unapproved_applicant_list {
+    my($self, $extra_columns) = @_;
+    vs_user_email_list(
+	'UnapprovedApplicantList',
+	[
+	    [privileges => {
+		wf_list_link => {
+		    query => 'THIS_DETAIL',
+		    task => 'SITE_ADMIN_UNAPPROVED_APPLICANT_FORM',
+		},
+	    }],
+	    @{$extra_columns || []},
+	],
+    );
+    return;
+}
+
 sub user_list {
     my($self, $extra_columns) = @_;
-    vs_user_email_list('AdmUserList', $extra_columns);
+    vs_user_email_list('SiteAdminUserList', [
+	@{$extra_columns || []},
+    ]);
     return;
 }
 
