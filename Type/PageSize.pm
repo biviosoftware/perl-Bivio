519cc695ffeed7819a637d1f73f525cab53c58a5
Rob Nagler <github@q33.us>
2008-12-19 01:15:10 +0000

    added ROW_TAG_KEY

--- Type/PageSize.pm
+++ Type/PageSize.pm
@@ -2,28 +2,23 @@
 # $Id$
 package Bivio::Type::PageSize;
 use strict;
-use Bivio::Base 'Bivio::Type::Integer';
-
-# C<Bivio::Type::PageSize> is the number of lines on a page for
-# ListModel queries.  It is a user preference.
+use Bivio::Base 'Type.Integer';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub ROW_TAG_KEY {
+    return 'PAGE_SIZE';
+}
+
 sub get_default {
-    # (self) : int
-    # Returns 15.
     return 15;
 }
 
 sub get_max {
-    # (self) : integer
-    # Returns 500.
     return 500;
 }
 
 sub get_min {
-    # (self) : int
-    # Returns 5.
     return 5;
 }
 
