e47411755ff1bf8a8684f599f74c94c923a01761
Eric Dobbs <dobbs@bivio.biz>
2008-12-19 23:04:22 +0000

    rmpod

--- Biz/Model/QuerySearchBaseForm.pm
+++ Biz/Model/QuerySearchBaseForm.pm
@@ -2,72 +2,20 @@
 # $Id$
 package Bivio::Biz::Model::QuerySearchBaseForm;
 use strict;
-$Bivio::Biz::Model::QuerySearchBaseForm::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Biz::Model::QuerySearchBaseForm::VERSION;
+use Bivio::Base 'Biz.FormModel';
 
-=head1 NAME
-
-Bivio::Biz::Model::QuerySearchBaseForm - manages query and form values for searching.
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Biz::Model::QuerySearchBaseForm;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Biz::FormModel>
-
-=cut
-
-use Bivio::Biz::FormModel;
-@Bivio::Biz::Model::QuerySearchBaseForm::ISA = ('Bivio::Biz::FormModel');
-
-=head1 DESCRIPTION
-
-C<Bivio::Biz::Model::QuerySearchBaseForm>
-
-=cut
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="OMIT_DEFAULT_VALUES_FROM_QUERY"></a>
-
-=head2 OMIT_DEFAULT_VALUES_FROM_QUERY : boolean
-
-Returns true if the query values are to be omitted if they match
-the default_value. Subclasses may override this to change the behavior.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub OMIT_DEFAULT_VALUES_FROM_QUERY {
+    # : boolean
+    # Returns true if the query values are to be omitted if they match
+    # the default_value. Subclasses may override this to change the behavior.
     return 1;
 }
 
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="execute_empty"></a>
-
-=head2 execute_empty()
-
-Fills in the default values.
-
-=cut
-
 sub execute_empty {
+    # (self) : undef
+    # Fills in the default values.
     my($self) = @_;
 
     foreach my $field (@{_get_visible_fields($self)}) {
@@ -76,15 +24,9 @@ sub execute_empty {
     return;
 }
 
-=for html <a name="execute_ok"></a>
-
-=head2 execute_ok()
-
-Sets the query.
-
-=cut
-
 sub execute_ok {
+    # (self) : undef
+    # Sets the query.
     my($self) = @_;
     # Build query hash from form data.
     _redirect($self, {
@@ -101,30 +43,18 @@ sub execute_ok {
     return;
 }
 
-=for html <a name="execute_other"></a>
-
-=head2 execute_other()
-
-Reset all form fields to their default value if the reset button was clicked.
-
-=cut
-
 sub execute_other {
+    # (self) : undef
+    # Reset all form fields to their default value if the reset button was clicked.
     my($self, $button_field) = @_;
     _redirect($self)
         if $button_field eq 'reset_button';
     return $self->SUPER::execute_other($button_field);
 }
 
-=for html <a name="internal_initialize"></a>
-
-=head2 internal_initialize() : hash_ref
-
-B<FOR INTERNAL USE ONLY>
-
-=cut
-
 sub internal_initialize {
+    # (self) : hash_ref
+    # B<FOR INTERNAL USE ONLY>
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
 	visible => [
@@ -137,15 +67,9 @@ sub internal_initialize {
     });
 }
 
-=for html <a name="internal_pre_execute"></a>
-
-=head2 internal_pre_execute()
-
-Load the default value of any fields that were not present on the form.
-
-=cut
-
 sub internal_pre_execute {
+    # (self) : undef
+    # Load the default value of any fields that were not present on the form.
     my($self) = @_;
     return unless $self->get_request->unsafe_get('form')
 	&& $self->equals($self->get_request->get('form'));
@@ -159,13 +83,9 @@ sub internal_pre_execute {
     return;
 }
 
-#=PRIVATE SUBROUTINES
-
-# _get_visible_fields(self) : array_ref
-#
-# Returns the visible non-button form field names.
-#
 sub _get_visible_fields {
+    # (self) : array_ref
+    # Returns the visible non-button form field names.
     my($self) = @_;
     return [
         grep(! ($self->get_field_info($_, 'type')
@@ -175,12 +95,10 @@ sub _get_visible_fields {
     ];
 }
 
-# _load_query_value(self, string field)
-#
-# Load query value into form model.  Load the default value if no value is
-# present on the query.
-#
 sub _load_query_value {
+    # (self, string) : undef
+    # Load query value into form model.  Load the default value if no value is
+    # present on the query.
     my($self, $field) = @_;
     my($query) = $self->get_request->get('query');
     my($v, $e);
@@ -200,14 +118,11 @@ sub _load_query_value {
     return;
 }
 
-# _redirect(self)
-#
-# _redirect(self, hash_ref query)
-#
-# Redirect to this task with new query, but must got through
-# CLIENT_REDIRECT to avoid browser seeing redirect loop.
-#
 sub _redirect {
+    # (self) : undef
+    # (self, hash_ref) : undef
+    # Redirect to this task with new query, but must got through
+    # CLIENT_REDIRECT to avoid browser seeing redirect loop.
     my($self, $query) = @_;
     my($req) = $self->get_request;
     $req->client_redirect(Bivio::Agent::TaskId->CLIENT_REDIRECT, undef, {
@@ -216,15 +131,5 @@ sub _redirect {
     });
     return;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 2003 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
