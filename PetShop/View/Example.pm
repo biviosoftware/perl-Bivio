320921287ffc175cf9d9079c10e5ee335d05d1e4
Rob Nagler <github@q33.us>
2009-01-06 20:55:22 +0000

    added handle_ping_reply

--- PetShop/View/Example.pm
+++ PetShop/View/Example.pm
@@ -6,9 +6,15 @@ use Bivio::Base 'View.Base';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+b_use('Action.PingReply')->register_handler(__PACKAGE__);
 
 sub eg1 {
     return shift->internal_body(Simple('hello, world!'));
+}
+
+sub handle_ping_reply {
+    my($self, $req) = @_;
+    return ($req->unsafe_get('query') || {})->{'View.Example'} ? 0 : 1;
 }
 
 1;
