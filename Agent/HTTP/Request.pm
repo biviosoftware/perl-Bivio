8c5b6834bb21f296cde74e93b3c993af9b8a5a62
Rob Nagler <github@q33.us>
2009-01-09 00:25:20 +0000

    Added put_client_redirect_state and internal_client_redirect Fixed up secure redirects b_use

--- Agent/HTTP/Request.pm
+++ Agent/HTTP/Request.pm
@@ -2,19 +2,8 @@
 # $Id$
 package Bivio::Agent::HTTP::Request;
 use strict;
-use base 'Bivio::Agent::Request';
-use Bivio::Agent::HTTP::Cookie;
-use Bivio::Agent::HTTP::Form;
-use Bivio::Agent::HTTP::Query;
-use Bivio::Agent::HTTP::Reply;
-use Bivio::Auth::RealmType;
-use Bivio::Auth::Support;
-use Bivio::Die;
-use Bivio::DieCode;
-use Bivio::HTML;
+use Bivio::Base 'Agent.Request';
 use Bivio::IO::Trace;
-use Bivio::Type::DateTime;
-use Bivio::Type::UserAgent;
 #TODO: Should be Socket ();  Fix unqualified symbols
 use Socket;
 
@@ -29,97 +18,25 @@ use Socket;
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 # needed for is_https_port()
 our($_TRACE);
+my($_C) = b_use('AgentHTTP.Cookie');
+my($_F) = b_use('AgentHTTP.Form');
+my($_Q) = b_use('AgentHTTP.Query');
+my($_R) = b_use('AgentHTTP.Reply');
+my($_D) = b_use('Bivio.Die');
+my($_DC) = b_use('Bivio.DieCode');
+my($_H) = b_use('Bivio.HTML');
+my($_DT) = b_use('Type.DateTime');
+my($_T) = b_use('Agent.Task');
+my($_TI) = b_use('Agent.TaskId');
 
 sub client_redirect {
-    # (self, hash_ref) : undef
-    # (self, any, any) : undef
-    # (self, any, any, any, string, boolean) : undef
-    # See L<Bivio::Agent::Request::format_uri|Bivio::Agent::Request/"format_uri">
-    #
-    #
-    #
-    # Client side redirect to the new task within the new realm.  If I<new_task>
-    # is the same as the current task, does an server_redirect.  Otherwise,
-    # tells client to come back in.
-    #
-    # B<DOES NOT RETURN.>
-    my($self, $named) =  shift->internal_get_named_args(
-	ref($_[0]) && !(ref($_[0]) eq 'HASH' && $_[0]->{uri})
-	    ? [qw(task_id realm query path_info no_context require_context no_form uri carry_query carry_path_info)]
-	    : [qw(uri query no_context task_id realm path_info require_context no_form carry_query carry_path_info)],
-	\@_,
-    );
-    if (defined($named->{uri})) {
-	# NOTE: This form never had implicit query/path_info copying
-	foreach my $a (qw(query path_info)) {
-	    $named->{$a} = undef
-		unless exists($named->{$a}) || exists($named->{"carry_$a"});
-	}
-	$self->internal_copy_implicit($named);
-	$named->{query} = Bivio::Agent::HTTP::Query->format($named->{query})
-	    if ref($named->{query});
-	$named->{uri} =~ s/\?/\?$named->{query}&/
-	    || ($named->{uri} .= '?'.$named->{query})
-	    if defined($named->{query}) && length($named->{query});
-    }
-    else {
-	# use previous query if not specified, maintains state across pages
-	$self->internal_copy_implicit($named);
-	$self->SUPER::server_redirect($named)
-	    unless Bivio::UI::Task->has_uri($named->{task_id}, $self);
-        _trace(
-	    'current: ', $self->get('task_id'), ', new: ', $named->{task_id}
-	) if $_TRACE && !$named->{realm};
-#TODO: Probably needs to be elsewhere
-	foreach my $k (keys(%$named)) {
-	    delete($named->{$k})
-		unless grep($k eq $_, @{$self->FORMAT_URI_PARAMETERS});
-	}
-	$named->{uri} = $self->format_uri($named);
-    }
-    $self->get('reply')->client_redirect($self, $named->{uri});
-    Bivio::Die->throw_quietly(Bivio::DieCode->CLIENT_REDIRECT_TASK);
-    # DOES NOT RETURN
-}
-
-sub client_redirect_if_not_secure {
-    # (self) : undef
-    # Causes the client to redirect back to this task in secure mode.
-    # If already in secure mode or can't secure (!I<can_secure>),
-    # returns (does nothing).
     my($self) = shift;
-    return if $self->get('is_secure') || !$self->get('can_secure');
-    $self->client_redirect({
-	uri => $self->format_http_toggling_secure(@_),
-    });
+    if (my $named = $self->internal_client_redirect(@_)) {
+	$self->SUPER::server_redirect($named);
+	# DOES NOT RETURN
+    }
+    $_D->throw_quietly($_DC->CLIENT_REDIRECT_TASK);
     # DOES NOT RETURN
-}
-
-sub format_http_toggling_secure {
-    # (self) : string
-    # Formats the uri for this request, but toggles secure mode.  This
-    # is a very special and only used in one location.
-    my($self, $host) = @_;
-    my($is_secure, $r, $redirect_count, $uri, $query) = $self->get(
-	    qw(is_secure r redirect_count uri query));
-    $host ||= Bivio::UI::Facade->get_value('http_host', $self);
-
-    # This is particularly strange.  FormModel deletes the incoming
-    # query context.   If we haven't internally redirected, we use
-    # the original query string so we get the format_context.  If
-    # we redirected, don't bother with the form_context.
-#TODO: This is screwed up.  Probably best to take the current
-#      form's context and shove it on the URL.  Wouldn't hurt if not
-#      really the form_model.
-#      RJN 12/13/00 For require_secure, shouldn't grab form context,
-#      because we don't even want to pretend to process it.
-    $query = $redirect_count ? Bivio::Agent::HTTP::Query->format($query)
-	    : $r->args;
-    $uri =~ s/\?/\?$query&/ || ($uri .= '?'.$query)
-	if $query;
-    $uri =~ s{https?://[^/]+/?}{/};
-    # Go into secure if not secure and vice-versa
-    return ($is_secure ? 'http://' : 'https://'). $host . $uri;
 }
 
 sub get_content {
@@ -157,8 +74,43 @@ sub get_form {
     # I<form_model> must be set.
     my($self) = @_;
     return $self->get_if_exists_else_put(
-	form => sub {Bivio::Agent::HTTP::Form->parse($self)},
+	form => sub {$_F->parse($self)},
     );
+}
+
+sub internal_client_redirect {
+    # NOTE: Use cient_redirect unless you know what you are doing
+    my($self, $named) = shift->internal_client_redirect_args(@_);
+    if (defined($named->{uri})) {
+	# NOTE: This form never had implicit query/path_info copying
+	foreach my $a (qw(query path_info)) {
+	    $named->{$a} = undef
+		unless exists($named->{$a}) || exists($named->{"carry_$a"});
+	}
+	$self->internal_copy_implicit($named);
+	$named->{query} = $_Q->format($named->{query})
+	    if ref($named->{query});
+	$named->{uri} =~ s/\?/\?$named->{query}&/
+	    || ($named->{uri} .= '?'.$named->{query})
+	    if defined($named->{query}) && length($named->{query});
+    }
+    else {
+	# use previous query if not specified, maintains state across pages
+	$self->internal_copy_implicit($named);
+	return $named
+	    unless Bivio::UI::Task->has_uri($named->{task_id}, $self);
+        _trace(
+	    'current: ', $self->get('task_id'), ', new: ', $named->{task_id}
+	) if $_TRACE && !$named->{realm};
+#TODO: Probably needs to be elsewhere
+	foreach my $k (keys(%$named)) {
+	    delete($named->{$k})
+		unless grep($k eq $_, @{$self->FORMAT_URI_PARAMETERS});
+	}
+	$named->{uri} = $self->format_uri($named);
+    }
+    $self->get('reply')->client_redirect($self, $named->{uri});
+    return;
 }
 
 sub new {
@@ -166,7 +118,7 @@ sub new {
     # Creates a Request from an apache request.  The target and path are
     # separated.
     my($proto, $r) = @_;
-    my($start_time) = Bivio::Type::DateTime->gettimeofday();
+    my($start_time) = $_DT->gettimeofday();
     # Set remote IP address if passed through by mod_proxy (RH6.2 and RH7.2)
     $r->connection->remote_ip($1)
 	if ($r->header_in('x-forwarded-for') || $r->header_in('via') || '')
@@ -174,7 +126,7 @@ sub new {
     # Sets Bivio::Agent::Request->get_current, so do the minimal thing
     my($self) = $proto->internal_new({
 	start_time => $start_time,
-	reply => Bivio::Agent::HTTP::Reply->new($r),
+	reply => $_R->new($r),
 	r => $r,
 	client_addr => $r->connection->remote_ip,
 	is_secure => $ENV{HTTPS} || _is_https_port($r) ? 1 : 0,
@@ -185,7 +137,7 @@ sub new {
     # Cookie parsed first, so log code works properly.
     # We must put the cookie now, because it may be used below.
     # auth_user (may) is set by cookie.
-    $self->put_durable(cookie => Bivio::Agent::HTTP::Cookie->new($self, $r));
+    $self->put_durable(cookie => $_C->new($self, $r));
     my($auth_user) = $self->unsafe_get('auth_user');
 
     # Task next, because may not be found or task may want
@@ -199,11 +151,11 @@ sub new {
     $self->internal_set_current();
 
     # Must re-escape the URI.
-    $uri = Bivio::HTML->escape_uri($uri) if $uri;
+    $uri = $_H->escape_uri($uri) if $uri;
 
     # NOTE: Syntax is weird to avoid passing $r->args in an array context
     # which avoids parsing $r->args.
-    my($query) = Bivio::Agent::HTTP::Query->parse(scalar($r->args));
+    my($query) = $_Q->parse(scalar($r->args));
 
     _trace($r->method, ': query=', $query, '; path_info=', $path_info)
 	    if $_TRACE;
@@ -224,11 +176,19 @@ sub new {
     return $self;
 }
 
+sub put_client_redirect_state {
+    my($self) = shift;
+    if (my $named = $self->internal_client_redirect(@_)) {
+	b_die($named, ': invalid redirect state');
+    }
+    return;
+}
+
 sub reset_reply {
     # (self) : undef
     # Clears the current reply and sets a new one on this request.
     my($self) = @_;
-    $self->put(reply => Bivio::Agent::HTTP::Reply->new($self->get('r')));
+    $self->put(reply => $_R->new($self->get('r')));
     return;
 }
 
