625fbd8c87138ca2383b871c4f27cfa4092ad6ce
Rob Nagler <github@q33.us>
2009-02-28 12:52:32 +0000

    *** empty log message ***

---
+++ Biz/Model/RealmFileMD5List.pm
@@ -0,0 +1,51 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::RealmFileMD5List;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+use Digest::MD5 ();
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_RF) = b_use('Model.RealmFile');
+my($_FP) = b_use('Type.FilePath');
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	can_iterate => 1,
+        auth_id => 'RealmFile.realm_id',
+        primary_key => ['RealmFile.realm_file_id'],
+	order_by => [qw(
+	    RealmFile.path
+	)],
+	$self->field_decl(other => [[qw(md5 Name)]]),
+	other_query_keys => [qw(path_info)],
+    });
+}
+
+sub internal_post_load_row {
+    my($self, $row) = @_;
+    return 0
+	unless shift->SUPER::internal_post_load_row(@_);
+    my($md5) = Digest::MD5->new;
+    $md5->addfile($_RF->get_handle($self, 'RealmFile.', $row));
+    $row->{md5} = $md5->b64digest;
+    return 1;
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt, $query) = @_;
+    my($p) = $_RF->parse_path($query->unsafe_get('path_info'), $self);
+    $stmt->where(
+	['RealmFile.is_folder', [0]],
+	$p eq '/' ? $stmt->NOT_LIKE(
+	    'RealmFile.path_lc', $_FP->join(lc($_FP->VERSIONS_FOLDER), '%'),
+	) : $stmt->LIKE(
+	    'RealmFile.path_lc', $_FP->join(lc($p), '%'),
+	),
+    );
+    return shift->SUPER::internal_prepare_statement(@_);
+}
+
+1;
