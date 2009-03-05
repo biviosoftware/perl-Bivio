b5bd34e23ef69b303727c13378b6a94419b74d59
Rob Nagler <github@q33.us>
2009-03-05 13:21:59 +0000

    added (partially complete and untested) remote_file_copy_form

--- UI/View/SiteAdmin.pm
+++ UI/View/SiteAdmin.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::View::SiteAdmin;
 use strict;
@@ -7,6 +7,29 @@ use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_AUL) = __PACKAGE__->use('Model.AdmUserList');
+
+sub remote_file_copy_form {
+    return shift->internal_body(vs_simple_form(RemoteFileCopyListForm => [
+	List(RemoteFileCopyListForm => [
+	    DIV(Join([
+		SPAN_field(
+		    vs_descriptive_field(
+			'RemoteFileCopyListForm.want_realm')->[1],
+		),
+		String([['->get_list_model'], 'realm']),
+		If(
+		    ['prepare_ok'],
+		    String('not yet'),
+		    UL_none(
+			With([['->get_list_model'], 'folder'], [
+			    LI(String(['value'])),
+			]),
+		    ),
+		),
+	    ])),
+	]),
+    ]));
+}
 
 sub substitute_user_form {
     return shift->internal_body(vs_simple_form(SiteAdminSubstituteUserForm => [qw{
