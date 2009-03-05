0fa6f161c4572d7d333de1e7d239856cd20481d7
Rob Nagler <github@q33.us>
2009-03-05 14:09:29 +0000

    don't need to read the file any more (handled by super)

--- Agent/Embed/Reply.pm
+++ Agent/Embed/Reply.pm
@@ -5,12 +5,9 @@ use strict;
 use Bivio::Base 'Agent.Reply';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_F) = b_use('IO.File');
 
 sub set_output {
     my($self, $value) = @_;
-    $value = $_F->read($value)
-	unless ref($value) eq 'SCALAR';
     $self->get('parent_request')->put(ref($self) => $self);
     return shift->SUPER::set_output(@_);
 }
