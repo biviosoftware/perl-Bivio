b303bd09aab46bba8aeee12bfcb39471a3d9b3a9
Rob Nagler <github@q33.us>
2009-03-10 13:26:57 +0000

    return the redirect so bunits work comments

--- Biz/Model/QuerySearchBaseForm.pm
+++ Biz/Model/QuerySearchBaseForm.pm
@@ -1,23 +1,20 @@
-# Copyright (c) 2003 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2003-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::QuerySearchBaseForm;
 use strict;
 use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_CR) = b_use('Action.ClientRedirect');
 
 sub OMIT_DEFAULT_VALUES_FROM_QUERY {
-    # : boolean
     # Returns true if the query values are to be omitted if they match
     # the default_value. Subclasses may override this to change the behavior.
     return 1;
 }
 
 sub execute_empty {
-    # (self) : undef
-    # Fills in the default values.
     my($self) = @_;
-
     foreach my $field (@{_get_visible_fields($self)}) {
 	_load_query_value($self, $field);
     }
@@ -25,36 +22,27 @@ sub execute_empty {
 }
 
 sub execute_ok {
-    # (self) : undef
-    # Sets the query.
     my($self) = @_;
-    # Build query hash from form data.
-    _redirect($self, {
+    return _redirect($self, {
         map({
             my($v) = $self->unsafe_get($_);
             my($t) = $self->get_field_info($_, 'type');
             my($dv) = $self->get_field_info($_, 'default_value');
 	    my($name) = $self->get_field_info($_, 'form_name');
-            $self->OMIT_DEFAULT_VALUES_FROM_QUERY
-                ? ($t->is_equal($dv, $v) ? () : ($name => $t->to_literal($v)))
-                : ($name => $t->to_literal($v));
+            !$self->OMIT_DEFAULT_VALUES_FROM_QUERY || !$t->is_equal($dv, $v)
+	        ? ($name => $t->to_literal($v)) : ();
         } @{_get_visible_fields($self)}),
     });
-    return;
 }
 
 sub execute_other {
-    # (self) : undef
-    # Reset all form fields to their default value if the reset button was clicked.
-    my($self, $button_field) = @_;
-    _redirect($self)
-        if $button_field eq 'reset_button';
-    return $self->SUPER::execute_other($button_field);
+    my($self) = shift;
+    return _redirect($self)
+        if $self->unsafe_get('reset_button');
+    return $self->SUPER::execute_other(@_);
 }
 
 sub internal_initialize {
-    # (self) : hash_ref
-    # B<FOR INTERNAL USE ONLY>
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
 	visible => [
@@ -68,8 +56,6 @@ sub internal_initialize {
 }
 
 sub internal_pre_execute {
-    # (self) : undef
-    # Load the default value of any fields that were not present on the form.
     my($self) = @_;
     return unless $self->get_request->unsafe_get('form')
 	&& $self->equals($self->get_request->get('form'));
@@ -84,8 +70,6 @@ sub internal_pre_execute {
 }
 
 sub _get_visible_fields {
-    # (self) : array_ref
-    # Returns the visible non-button form field names.
     my($self) = @_;
     return [
         grep(! ($self->get_field_info($_, 'type')
@@ -96,13 +80,9 @@ sub _get_visible_fields {
 }
 
 sub _load_query_value {
-    # (self, string) : undef
-    # Load query value into form model.  Load the default value if no value is
-    # present on the query.
     my($self, $field) = @_;
     my($query) = $self->get_request->get('query');
     my($v, $e);
-
     my($value) = $query->{$self->get_field_info($field, 'form_name')};
     if (defined($value)) {
 	($v, $e) = $self->get_field_type($field)->from_literal($value);
@@ -119,17 +99,17 @@ sub _load_query_value {
 }
 
 sub _redirect {
-    # (self) : undef
-    # (self, hash_ref) : undef
-    # Redirect to this task with new query, but must got through
     # CLIENT_REDIRECT to avoid browser seeing redirect loop.
     my($self, $query) = @_;
-    my($req) = $self->get_request;
-    $req->client_redirect(Bivio::Agent::TaskId->CLIENT_REDIRECT, undef, {
-        Bivio::Biz::Action::ClientRedirect->QUERY_TAG =>
-            $req->format_uri($req->get('task_id'), $query || {}),
-    });
-    return;
+    my($req) = $self->req;
+    return {
+	method => 'client_redirect',
+	task_id => 'CLIENT_REDIRECT',
+	query => {
+	    $_CR->QUERY_TAG => $req->format_uri(
+		$req->get('task_id'), $query || {}),
+	},
+    };
 }
 
 1;
