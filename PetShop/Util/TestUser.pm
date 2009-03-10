e12e09a5033903c8ca22680c2b9f6c1d7adf1cc2
Rob Nagler <github@q33.us>
2009-03-10 13:28:32 +0000

    root is created in TestUser->init_adm so SiteForum->init_files works

--- PetShop/Util/TestUser.pm
+++ PetShop/Util/TestUser.pm
@@ -7,11 +7,16 @@ use Bivio::Base 'ShellUtil';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub ADM {
-    return shift->new_other('SQL')->ROOT;
+    return 'root';
 }
 
 sub init_adm {
-    return;
+    my($self) = @_;
+    $self->initialize_fully->with_realm(undef, sub {
+	$self->new_other('SQL')->create_user_with_account($self->ADM)
+	    unless $self->model('RealmOwner')->unauth_load({name => $self->ADM});
+    });
+    return shift->SUPER::init_adm(@_);
 }
 
 1;
