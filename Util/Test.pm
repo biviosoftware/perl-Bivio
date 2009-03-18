9d533353fa56e6b63d57ed06d68565d2c92d7715
Rob Nagler <github@q33.us>
2009-03-18 20:38:27 +0000

    added Util.Test

---
+++ Util/Test.pm
@@ -0,0 +1,12 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Util::Test;
+use strict;
+use Bivio::Base 'Test.Util';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+#TODO: migrate BConf config
+#TODO: upgrade apps to use ShellUtil.Test
+
+1;
