6ad37776667d578bf4737a3f53219571550b3a9d
Rob Nagler <github@q33.us>
2009-03-18 20:02:35 +0000

    convert to a Collection.Attributes so can be reassigned

--- Test/Bean.pm
+++ Test/Bean.pm
@@ -1,48 +1,48 @@
-# Copyright (c) 2002-2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2008 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Test::Bean;
 use strict;
-use base 'Bivio::UNIVERSAL';
-use Bivio::IO::Ref;
+use Bivio::Base 'Collection.Attributes';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($AUTOLOAD);
 my($_IDI) = __PACKAGE__->instance_data_index;
+my($_R) = b_use('IO.Ref');
 
 sub AUTOLOAD {
     my($self, @args) = @_;
     my($method) = $AUTOLOAD;
     $method =~ s/.*:://;
     return if $method eq 'DESTROY';
-    my($fields) = $self->[$_IDI];
-    if ($fields->{_new_mode}) {
+    if ($self->unsafe_get('_new_mode')) {
 	my($orig_args) = [@args];
 	while(1) {
 	    my($sig) = "$method(" . join(',', @args) . ')';
-	    return ref($fields->{$sig}) eq 'CODE' ? $fields->{$sig}->($orig_args)
-		: wantarray ? @{$fields->{$sig}} : $fields->{$sig}->[0]
-		if exists($fields->{$sig});
+	    return ref($self->unsafe_get($sig)) eq 'CODE'
+		? $self->unsafe_get($sig)->($orig_args)
+		: wantarray
+		? @{$self->unsafe_get($sig)}
+		: $self->unsafe_get($sig)->[0]
+		if $self->has_keys($sig);
 	    if (@args) {
 		pop(@args);
 	    }
 	    else {
-		$fields->{$sig} = [];
+		$self->put($sig => []);
 		Bivio::IO::Alert->warn($sig, ': not found returning nothing');
 	    }
 	}
 	return;
     }
-#    Bivio::IO::Alert->warn_deprecated('set up in new()');
-    my($res) = $fields->{$method} || [];
-    $fields->{$method} = \@args if @args;
+    my($res) = $self->get_or_default($method => []);
+    $self->put($method => \@args)
+	if @args;
     return wantarray ? @$res : $res->[0];
 }
 
 sub new {
-    my($proto, $values) = @_;
-    my($self) = $proto->SUPER::new;
-    $self->[$_IDI] = $values ? _verify(Bivio::IO::Ref->nested_copy($values)) : {};
-    return $self;
+    my(undef, $values) = @_;
+    return shift->SUPER::new($values ? _verify($_R->nested_copy($values)) : {});
 }
 
 sub _verify {
