87eb7d3bda0c75371f46b3298ec38c1d30c0f3b0
Rob Nagler <github@q33.us>
2009-04-03 17:40:26 +0000

    use Registrar MailFileName no longer includes subject Use RealmMailReferenceList Removed create_from_file

--- Biz/Model/RealmMail.pm
+++ Biz/Model/RealmMail.pm
@@ -1,20 +1,20 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::RealmMail;
 use strict;
-use base 'Bivio::Biz::PropertyModel';
-use Bivio::Mail::Incoming;
+use Bivio::Base 'Model.RealmBase';
 use Bivio::IO::Trace;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($_TRACE);
-my($_MAX_LINE) = Bivio::Type->get_instance('Line')->get_width;
-my($_MAX_EMAIL) = Bivio::Type->get_instance('Email')->get_width;
-my($_MS) = Bivio::Type->get_instance('MailSubject');
-my($_MFN) = Bivio::Type->get_instance('MailFileName');
-my($_RF) = __PACKAGE__->use('Model.RealmFile');
-my($_HANDLERS) = [];
-my($_DT) = __PACKAGE__->use('Type.DateTime');
+my($_MAX_EMAIL) = b_use('Type.Email')->get_width;
+my($_MS) = b_use('Type.MailSubject');
+my($_MFN) = b_use('Type.MailFileName');
+my($_RF) = b_use('Model.RealmFile');
+my($_DT) = b_use('Type.DateTime');
+my($_I) = b_use('Mail.Incoming');
+my($_HANDLERS) = b_use('Biz.Registrar')->new;
+my($_MI) = b_use('Type.MessageId');
 
 sub cascade_delete {
     my($self, $query) = @_;
@@ -46,11 +46,6 @@ sub cascade_delete {
     return;
 }
 
-sub create_from_file {
-    my($self, $file) = @_;
-    return _create($self, Bivio::Mail::Incoming->new($file->get_content), $file);
-}
-
 sub create_from_rfc822 {
     my($self, $rfc822) = @_;
     return _create($self, _create_file($self, $rfc822));
@@ -76,8 +71,7 @@ sub internal_initialize {
         columns => {
             realm_file_id => ['RealmFile.realm_file_id', 'PRIMARY_KEY'],
 	    # Index on realm_id, message_id.  Does it need to be unique?
-            realm_id => ['RealmOwner.realm_id', 'NOT_NULL'],
-	    message_id => ['Line', 'NOT_NULL'],
+	    message_id => ['MessageId', 'NOT_NULL'],
 	    thread_root_id => ['RealmFile.realm_file_id', 'NOT_NULL'],
 	    thread_parent_id => ['RealmFile.realm_file_id', 'NONE'],
             from_email => ['Email', 'NOT_NULL'],
@@ -93,10 +87,8 @@ sub internal_initialize {
 }
 
 sub register {
-    my($proto, $handler) = @_;
-    $handler = $proto->use($handler);
-    push(@$_HANDLERS, $handler)
-	unless grep($_ eq $handler, @$_HANDLERS);
+    shift;
+    $_HANDLERS->push_object(@_);
     return;
 }
 
@@ -105,32 +97,26 @@ sub update {
     if (defined($values->{subject})) {
 	$values->{subject} = $_MS->trim_literal($values->{subject});
 	$values->{subject_lc} = $_MS->clean_and_trim($values->{subject});
-	# Used by Tuple, but makes sense to cut the thread if
-	# the subject is changed.
-#TODO: Update children of thread to have correct thread_root_id
-	$values->{thread_root_id} = $self->get('realm_file_id')
-	    unless $self->get('subject_lc') eq $values->{subject_lc};
     }
-    $values->{thread_parent_id} = undef
-	if $self->get('realm_file_id') eq ($values->{thread_root_id} || '');
+    $self->die(
+	$values,
+	': must set both thread_parent_id and thread_root_id or neither',
+    ) if exists($values->{thread_parent_id})
+        xor exists($values->{thread_root_id});
     return shift->SUPER::update(@_);
 }
 
 sub _call_handlers {
-    my($method) = shift;
-    foreach my $h (@$_HANDLERS) {
-	$h->$method(@{[@_]})
-	    if $h->can($method);
-    }
+    $_HANDLERS->call_fifo(shift, [@_]);
     return;
 }
 
 sub _create {
     my($self, $in, $file) = @_;
     $self->create(
-	_thread_values({
+	_thread_values($self, $in, {
 	    map(($_ => $file->get($_)), qw(realm_id realm_file_id)),
-	    message_id => substr($in->get_message_id, 0, $_MAX_LINE),
+	    message_id => $_MI->clean_and_trim($in->get_message_id),
 	    from_email => substr(lc(($in->get_from)[0]), 0, $_MAX_EMAIL),
 	    subject => $_MS->trim_literal($in->get_subject),
 	    subject_lc => $_MS->clean_and_trim($in->get_subject),
@@ -143,7 +129,7 @@ sub _create {
 sub _create_file {
     my($self, $rfc822) = @_;
     _call_handlers(handle_mail_pre_create_file => $self, $rfc822);
-    my($in) = Bivio::Mail::Incoming->new($rfc822);
+    my($in) = $_I->new($rfc822);
     my($date) = $in->get_date_time;
     $date = $date ? $_DT->from_unix($date) : $_DT->now;
     my($rf) = $self->new_other('RealmFile');
@@ -151,7 +137,7 @@ sub _create_file {
 	$in,
 	$rf->create_with_content({
 	    override_is_read_only => 1,
-	    path => $_MFN->to_unique_absolute($date, $in->get_subject),
+	    path => $_MFN->to_unique_absolute($date),
 	    user_id => _user_id($self, $in),
 	    modified_date_time => $date,
 	}, $rfc822),
@@ -159,22 +145,13 @@ sub _create_file {
 }
 
 sub _thread_values {
-    my($values, $in) = @_;
-    my($row);
-    foreach my $ref (@{$in->get_references}) {
-        last if $row = Bivio::SQL::Connection->execute_one_row(
-            'SELECT realm_file_id, thread_root_id, message_id
-            FROM realm_mail_t
-            WHERE realm_id = ?
-            AND realm_file_id < ?
-            AND message_id = ?',
-	    [@{$values}{qw(realm_id realm_file_id)}, $ref],
-	);
-    }
-    $row ||= [];
-    _trace($values->{realm_file_id}, ' ', $row) if $_TRACE;
-    $values->{thread_parent_id} = $row->[0];
-    $values->{thread_root_id} = $row->[1] || $values->{realm_file_id};
+    my($self, $in, $values) = @_;
+    my($l) = $self->new_other('RealmMailReferenceList')
+	->load_first_from_incoming($in);
+    $values->{thread_parent_id} = $l ? $l->get('RealmMail.realm_file_id')
+	: undef;
+    $values->{thread_root_id} = $l ? $l->get('RealmMail.thread_root_id')
+	: $values->{realm_file_id};
     return $values;
 }
 
