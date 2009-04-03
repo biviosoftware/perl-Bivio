4deeff91e930e1c246989610d79e459b70728fe1
Rob Nagler <github@q33.us>
2009-04-03 17:41:17 +0000

    no longer includes subject

--- Type/MailFileName.pm
+++ Type/MailFileName.pm
@@ -2,29 +2,28 @@
 # $Id$
 package Bivio::Type::MailFileName;
 use strict;
-use base 'Bivio::Type::DocletFileName';
+use Bivio::Base 'Type.DocletFileName';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_MS) = Bivio::Type->get_instance('MailSubject');
-my($_DT) = Bivio::Type->get_instance('DateTime');
+my($_DT) = b_use('Type.DateTime');
+my($_R) = b_use('Biz.Random');
 
 sub PRIVATE_FOLDER {
     return shift->MAIL_FOLDER;
 }
 
 sub REGEX {
-    return qr{(@{[shift->join('\d{4}-\d{2}', '[^/]+ \d{17}.eml')]})}i;
+    return qr{(@{[shift->join('\d{4}-\d{2}', '[^/]*\d{14}-\d{3}.eml')]})}i;
 }
 
 sub to_unique_absolute {
-    my($proto, $date, $subject, $is_public) = @_;
+    my($proto, $date, $is_public) = @_;
     return $proto->to_absolute(
 	$proto->join(
 	    sprintf('%04d-%02d', $_DT->get_parts($date, qw(year month))),
-	    $_MS->clean_and_trim($subject)
-		. ' '
-		. $_DT->to_file_name($date)
-		. sprintf('%03d', int(rand(1_000)))
+		$_DT->to_file_name($date)
+		. '-'
+		. sprintf('%03d', $_R->integer(1000))
 		. '.eml',
 	),
 	$is_public,
