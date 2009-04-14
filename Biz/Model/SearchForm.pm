6aa1d4b573ab6a19402333bc37bfcd7d9e01cd3a
Rob Nagler <github@q33.us>
2009-04-14 23:22:14 +0000

    Added get_search_value and CLEAR_ON_FOCUS_HINT

--- Biz/Model/SearchForm.pm
+++ Biz/Model/SearchForm.pm
@@ -1,14 +1,31 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::SearchForm;
 use strict;
-use base 'Bivio::Biz::FormModel';
+use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub CLEAR_ON_FOCUS_HINT {
+    return 'Search';
+}
+
+sub execute_empty {
+    my($self) = @_;
+    $self->internal_put_field(search => $self->CLEAR_ON_FOCUS_HINT)
+	unless defined($self->unsafe_get('search'));
+    return;
+}
 
 sub execute_ok {
     shift->internal_stay_on_page;
     return;
+}
+
+sub get_search_value {
+    my($self) = @_;
+    my($s) = self->unsafe_get('search');
+    return ($s || '') eq $self->CLEAR_ON_FOCUS_HINT ? undef : $s;
 }
 
 sub internal_initialize {
@@ -20,7 +37,7 @@ sub internal_initialize {
 		name => 'search',
 		type => 'Line',
 		constraint => 'NONE',
-		form_name => Bivio::SQL::ListQuery->to_char('search'),
+		form_name => b_use('SQL.ListQuery')->to_char('search'),
 	    },
 	],
     });
