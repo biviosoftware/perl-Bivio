5f6a1eeb201e0421846e5eef5928c5fb42f66d01
Rob Nagler <github@q33.us>
2009-04-24 10:20:45 +0000

    rmpod

--- Biz/Model/SummaryList.pm
+++ Biz/Model/SummaryList.pm
@@ -2,89 +2,26 @@
 # $Id$
 package Bivio::Biz::Model::SummaryList;
 use strict;
-$Bivio::Biz::Model::SummaryList::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Biz::Model::SummaryList::VERSION;
+use Bivio::Base 'Bivio::Collection::Attributes';
 
-=head1 NAME
+# C<Bivio::Biz::Model::SummaryList> summarizes columns from a list model
+# and implements a ListModel-like interface so it can be used in Tables.
+#
+# Note: SummaryList provides a summary only for the values currently loaded
+# by the source ListModel(s). If it has only loaded a page of data, then only
+# the page will be summarized.
+#
+# SummaryList L<get|"get"> will reset the cursor in the ListModel to the
+# beginning of the list each time a value is requested - don't call this
+# methods while iterating through the source(s).
 
-Bivio::Biz::Model::SummaryList - a list model summary
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Biz::Model::SummaryList;
-    Bivio::Biz::Model::SummaryList->new();
-
-=cut
-
-use Bivio::Collection::Attributes;
-@Bivio::Biz::Model::SummaryList::ISA = ('Bivio::Collection::Attributes');
-
-=head1 DESCRIPTION
-
-C<Bivio::Biz::Model::SummaryList> summarizes columns from a list model
-and implements a ListModel-like interface so it can be used in Tables.
-
-Note: SummaryList provides a summary only for the values currently loaded
-by the source ListModel(s). If it has only loaded a page of data, then only
-the page will be summarized.
-
-SummaryList L<get|"get"> will reset the cursor in the ListModel to the
-beginning of the list each time a value is requested - don't call this
-methods while iterating through the source(s).
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(array_ref source) : Bivio::Biz::Model::SummaryList
-
-=head2 static new(array_ref source, hash_ref static_properties) : Bivio::Biz::Model::SummaryList
-
-Creates a summary list which gets data from the specified source ListModel(s).
-Sets the static_properties to values supplied.
-
-=cut
-
-sub new {
-    my($proto, $source, $static_properties) = @_;
-    my($self) = $proto->SUPER::new($static_properties);
-    $self->[$_IDI] = {
-	source => $source,
-	request => $source->[0]->get_request,
-	loaded => 1,
-    };
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-#TODO: export type information from source ListModel(s)
-
-=for html <a name="get"></a>
-
-=head2 get(string key, ...) : (string, ...)
-
-Overrides Attributes get to dynamically create a summary value for the
-named column.
-
-=cut
-
 sub get {
+    # (self, string, ...) : (string, ...)
+    # Overrides Attributes get to dynamically create a summary value for the
+    # named column.
     my($self, @keys) = @_;
     my($fields) = $self->[$_IDI];
 
@@ -95,85 +32,49 @@ sub get {
     return $self->SUPER::get(@keys);
 }
 
-=for html <a name="get_cursor"></a>
-
-=head2 get_cursor() : int
-
-Returns the position.  Returns -1 before the list is read and
-undef after the list is read.
-
-=cut
-
 sub get_cursor {
+    # (self) : int
+    # Returns the position.  Returns -1 before the list is read and
+    # undef after the list is read.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     return $fields->{loaded} ? -1 : 0;
 }
 
-=for html <a name="get_list_model"></a>
-
-=head2 get_list_model() : self
-
-Returns itself, the list model.
-
-=cut
-
 sub get_list_model {
+    # (self) : self
+    # Returns itself, the list model.
     my($self) = @_;
     return $self;
 }
 
-=for html <a name="get_request"></a>
-
-=head2 get_request() : Bivio::Agent::Request
-
-Returns the request associated with this list.
-
-=cut
-
 sub get_request {
+    # (self) : Agent.Request
+    # Returns the request associated with this list.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     return $fields->{request};
 }
 
-=for html <a name="get_result_set_size"></a>
-
-=head2 get_result_set_size() : int
-
-Returns the number of rows loaded.
-
-=cut
-
 sub get_result_set_size {
+    # (self) : int
+    # Returns the number of rows loaded.
     return 1;
 }
 
-=for html <a name="get_source_list_model"></a>
-
-=head2 get_source_list_model() : Bivio::Biz::ListModel
-
-Returns the (first) source list model for this SummaryList.
-
-=cut
-
 sub get_source_list_model {
+    # (self) : Biz.ListModel
+    # Returns the (first) source list model for this SummaryList.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     return $fields->{source}->[0];
 }
 
-=for html <a name="get_widget_value"></a>
-
-=head2 get_widget_value(string param1, ...) : any
-
-Overrides Attributes get_widget_value to dynamically compute summaries.
-Generates and stores summary value and uses Attributes.get_widget_value
-to do the rest.
-
-=cut
-
 sub get_widget_value {
+    # (self, string, ...) : any
+    # Overrides Attributes get_widget_value to dynamically compute summaries.
+    # Generates and stores summary value and uses Attributes.get_widget_value
+    # to do the rest.
     my($self, $name, @params) = @_;
     my($fields) = $self->[$_IDI];
     # Only "get" if first source has this key.  "get" does a "put"
@@ -182,16 +83,10 @@ sub get_widget_value {
     return $self->SUPER::get_widget_value($name, @params);
 }
 
-=for html <a name="internal_sum"></a>
-
-=head2 internal_sum(array_ref source, string name) : string
-
-Returns the summed value for all the values for the specified column
-across all the source list models.
-
-=cut
-
 sub internal_sum {
+    # (self, array_ref, string) : string
+    # Returns the summed value for all the values for the specified column
+    # across all the source list models.
     my($self, $source, $name) = @_;
     my($result) = 0;
 
@@ -208,15 +103,24 @@ sub internal_sum {
     return $result;
 }
 
-=for html <a name="next_row"></a>
-
-=head2 next_row() : boolean
-
-Summary lists return only one row.
-
-=cut
+sub new {
+    # (proto, array_ref) : Model.SummaryList
+    # (proto, array_ref, hash_ref) : Model.SummaryList
+    # Creates a summary list which gets data from the specified source ListModel(s).
+    # Sets the static_properties to values supplied.
+    my($proto, $source, $static_properties) = @_;
+    my($self) = $proto->SUPER::new($static_properties);
+    $self->[$_IDI] = {
+	source => $source,
+	request => $source->[0]->get_request,
+	loaded => 1,
+    };
+    return $self;
+}
 
 sub next_row {
+    # (self) : boolean
+    # Summary lists return only one row.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     Bivio::Die->die('no cursor')unless defined($fields->{loaded});
@@ -229,31 +133,13 @@ sub next_row {
     return 0;
 }
 
-=for html <a name="reset_cursor"></a>
-
-=head2 reset_cursor()
-
-Places the cursor at the start of the list.
-
-=cut
-
 sub reset_cursor {
+    # (self) : undef
+    # Places the cursor at the start of the list.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     $fields->{loaded} = 1;
     return;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
