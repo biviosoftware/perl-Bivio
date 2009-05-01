7addd0f14900301286d51bcfa75ec6646e719cca
Rob Nagler <github@q33.us>
2009-05-01 17:17:04 +0000

    get handle_class_loader_require

--- IO/t/ClassLoader/Valid.pm
+++ IO/t/ClassLoader/Valid.pm
@@ -1,9 +1,20 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::IO::t::ClassLoader::Valid;
 use strict;
 use base 'Bivio::UNIVERSAL';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_IMPORTERS) = [];
+
+sub get_importers {
+    return $_IMPORTERS;
+}
+
+sub handle_class_loader_require {
+    my($proto, $importer) = @_;
+    push(@$_IMPORTERS, $importer);
+    return;
+}
 
 1;
