a7fce9dca8998f81026dcd1d1b287616add2fe5c
Rob Nagler <github@q33.us>
2009-05-01 17:20:34 +0000

    from_literal_validator isn't needed, because now calls UNDERLYING_TYPE->from_literal subclasses SemicolonStringArray

--- Type/FilePathArray.pm
+++ Type/FilePathArray.pm
@@ -2,26 +2,13 @@
 # $Id$
 package Bivio::Type::FilePathArray;
 use strict;
-use Bivio::Base 'Type.StringArray';
+use Bivio::Base 'Type.SemicolonStringArray';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_FP) = b_use('Type.FilePath');
 
-sub LITERAL_SEPARATOR {
-    return '; ';
-}
-
-sub LITERAL_SEPARATOR_REGEX {
-    return qr{\s*;\s*}s;
-}
-
 sub UNDERLYING_TYPE {
     return $_FP;
-}
-
-sub from_literal_validator {
-    shift;
-    return $_FP->from_literal(@_);
 }
 
 1;
