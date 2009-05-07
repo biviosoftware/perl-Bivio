2d15209cb4e647b78f48a69092efb5fe986f0bb9
Rob Nagler <github@q33.us>
2009-05-07 20:30:29 +0000

    allow @domain.com domain aliases

--- Type/EmailAliasOutgoing.pm
+++ Type/EmailAliasOutgoing.pm
@@ -2,17 +2,24 @@
 # $Id$
 package Bivio::Type::EmailAliasOutgoing;
 use strict;
-use base 'Bivio::Type::Email';
+use Bivio::Base 'Type.Email';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DN) = b_use('Type.DomainName');
+my($_ERR) = b_use('Bivio.TypeError')->EMAIL_ALIAS_OUTGOING;
 
 sub from_literal {
     my($proto, $value) = @_;
     my($v, $e) = shift->SUPER::from_literal(@_);
     return ($v, $e)
 	unless $e && $e == Bivio::TypeError->EMAIL;
-    return ($v = $proto->get_instance('RealmName')->unsafe_from_uri($value))
-	? $v : (undef, Bivio::TypeError->EMAIL_ALIAS_OUTGOING);
+    if ($value =~ s/^\@//) {
+	($v, $e) = $_DN->from_literal($value);
+	return $v ? ('@' . $v, undef) : ($v, $e);
+    }
+    return (undef, $_ERR)
+	unless $v = $proto->get_instance('RealmName')->unsafe_from_uri($value);
+    return ($v, undef);
 }
 
 1;
