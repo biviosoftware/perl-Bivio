27dda1847b8b2cfb7e18e336c4e2c807586db8e8
Rob Nagler <github@q33.us>
2009-05-15 15:06:35 +0000

    rmpod

--- Type/LongText.pm
+++ Type/LongText.pm
@@ -2,68 +2,17 @@
 # $Id$
 package Bivio::Type::LongText;
 use strict;
-$Bivio::Type::LongText::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+use Bivio::Base 'Bivio::Type::Text';
 
-=head1 NAME
+# C<Bivio::Type::LongText> same as L<Bivio::Type::Text|Bivio::Type::Text>
+# except 4000 characters.
 
-Bivio::Type::LongText - longer (4000 char) version of Text
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::LongText;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Text>
-
-=cut
-
-use Bivio::Type::Text;
-@Bivio::Type::LongText::ISA = ('Bivio::Type::Text');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::LongText> same as L<Bivio::Type::Text|Bivio::Type::Text>
-except 4000 characters.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_width"></a>
-
-=head2 static get_width : int
-
-Returns 4000.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_width {
+    # : int
+    # Returns 4000.
     return 4000;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2000 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
