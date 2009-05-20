3255b1dfd902d45c2103d0f71735ef86506e6092
Paul Moeller <moeller@bivio.biz>
2009-05-20 20:13:12 +0000

    calls b_use('UI.ViewLanguage')

--- UI/ViewLanguageAUTOLOAD.pm
+++ UI/ViewLanguageAUTOLOAD.pm
@@ -2,15 +2,14 @@
 # $Id$
 package Bivio::UI::ViewLanguageAUTOLOAD;
 use strict;
-use base 'Bivio::UNIVERSAL';
+use Bivio::Base 'Bivio::UNIVERSAL';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($AUTOLOAD);
+my($_VL) = b_use('UI.ViewLanguage');
 
 sub AUTOLOAD {
-    return Bivio::UI::ViewLanguage->call_method(
-	$AUTOLOAD, 'Bivio::UI::ViewLanguage', @_,
-    );
+    return $_VL->call_method($AUTOLOAD, 'Bivio::UI::ViewLanguage', @_);
 }
 
 sub import {
