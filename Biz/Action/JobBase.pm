6b8474dc51b7e2c317b46bb9dfa995397b73bab3
Rob Nagler <github@q33.us>
2009-05-25 03:17:03 +0000

    execute takes $proto, not $self

--- Biz/Action/JobBase.pm
+++ Biz/Action/JobBase.pm
@@ -8,10 +8,10 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_SENTINEL) = __PACKAGE__ . '.internal_execute';
 
 sub execute {
-    my($self, $req) = @_;
-    die($self, ': does not implement internal_execute')
-	unless $self->can('internal_execute');
-    return $self->internal_execute($req)
+    my($proto, $req) = @_;
+    die($proto, ': does not implement internal_execute')
+	unless $proto->can('internal_execute');
+    return $proto->internal_execute($req)
 	if $req->unsafe_get($_SENTINEL);
     b_use('AgentJob.Dispatcher')->enqueue(
 	$req, $req->get('task_id'), {$_SENTINEL => 1});
