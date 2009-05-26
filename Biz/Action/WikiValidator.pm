b31b3c8519dffb08a0d5f70c0d62ed6a8080f171
Rob Nagler <github@q33.us>
2009-05-26 17:35:55 +0000

    set message, if empty after perl junk stripping

--- Biz/Action/WikiValidator.pm
+++ Biz/Action/WikiValidator.pm
@@ -84,8 +84,10 @@ sub unsafe_load_error_list {
 
 sub validate_error {
     my($self, $entity, $message, $wiki_state) = @_;
-    b_warn($message, ': removed Perl junk: ', $1)
-	if $message =~ s/(\w+(?:\:\:|\-\>)\w+.*)//;
+    if ($message =~ s/(\w+(?:\:\:|\-\>)\w+.*)//) {
+	b_warn($message, ': removed Perl junk: ', $1);
+	$message ||= 'internal server error';
+    }
     my($err) = {
 	entity => $entity,
 	message => $message,
