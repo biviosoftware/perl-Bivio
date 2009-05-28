41dd2833d77199317f30d490f27999f3ab3d9bce
Paul Moeller <moeller@bivio.biz>
2009-05-28 16:28:38 +0000

    moved filter code into FilterQueryForm

--- Biz/Model/TaskLogList.pm
+++ Biz/Model/TaskLogList.pm
@@ -5,14 +5,6 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_DT) = b_use('Type.DateTime');
-my($_D) = b_use('Type.Date');
-my($_DATE_OP) = {qw(
-    < LTE
-    > GTE
-    = EQ
-)};
-my($_DATE_OP_RE) = join('|', sort(keys(%$_DATE_OP)));
 
 sub internal_initialize {
     my($self) = @_;
@@ -54,71 +46,17 @@ sub internal_prepare_statement {
 	]));
     }
 
-    if (my $qf = $self->ureq('Model.TaskLogQueryForm')) {
-	if (defined(my $filter = $qf->get_filter_value)) {
-	    $stmt->where(map(_filter($_, $stmt), split(' ', $filter)));
-	}
+    if (my $qf = $self->ureq('Model.FilterQueryForm')) {
+	$qf->filter_statement($stmt, {
+	    date_time => 'TaskLog.date_time',
+	    match_fields => [
+		qr/\// => 'TaskLog.uri',
+		qr/\@/ => 'Email.email',
+		qr/^\w/ => 'RealmOwner.display_name',
+	    ],
+	});
     }
     return shift->SUPER::internal_prepare_statement(@_);
-}
-
-sub _filter {
-    my($word, $stmt) = @_;
-    return _filter_date($stmt, $1, $word)
-	if $word =~ s/^($_DATE_OP_RE)//o;
-    my($method) = $word =~ s/^-// ? 'NOT_ILIKE' : 'ILIKE';
-    if ($word =~ m{[/\@]|^\w}) {
-	$word =~ s/\%/_/g;
-	return $stmt->$method(
-	    $word =~ m{/} ? 'TaskLog.uri'
-		: $word =~ m{\@} ? 'Email.email'
-		    : 'RealmOwner.display_name',
-	    '%' . lc($word) . '%',
-	);
-    }
-    return b_warn($word, ': invalid word');
-}
-
-sub _filter_date {
-    my($stmt) = shift;
-    my($ops, $dates) = _filter_date_parse(@_);
-    return
-	unless $ops;
-    my($method) = 'set_local_beginning_of_day';
-    return @{__PACKAGE__->map_together(
-	sub {
-	    my($op, $date) = @_;
-	    my($m) = $method;
-	    $method = 'set_local_end_of_day';
-	    return $op ? $stmt->$op('TaskLog.date_time', [$_DT->$m($date)])
-		: ();
-	},
-	$ops,
-	$dates,
-    )};
-}
-
-sub _filter_date_parse {
-    my($prefix, $word) = @_;
-    my($op) = $_DATE_OP->{$prefix};
-    my($parts) = [split(/\W+/, $word)];
-    return b_warn($word, ': too many date parts')
-	if @$parts > 3;
-    return b_warn($word, ': too few date parts')
-	if @$parts < 2;
-    if (@$parts == 2) {
-	my($begin) = $_DT->date_from_parts(
-	    1,
-	    length($parts->[0]) > 2 ? reverse(@$parts) : @$parts,
-	);
-	return ([qw(GTE LTE)], [$begin, $_DT->set_end_of_month($begin)]);
-    }
-    my($date, $e) = $_D->from_literal($word);
-    return b_warn($word, ': ', $e)
-	unless $date;
-    return $op eq 'EQ' ? ([qw(GTE LTE)], [$date, $date])
-	: $op eq 'LTE' ? ([undef, $op], [undef, $date])
-	: ([$op, undef], [$date, undef]);
 }
 
 1;
