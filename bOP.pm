1f5ec6392fc254cbdae2c1ff25dd4d6648fd12f2
Alex Viggio <aviggio@bivio.biz>
2009-06-04 20:25:53 +0000

    * Bivio::BConf   added JavaScript widget map * Bivio::UI::FacadeBase   rename SITE_REPORTS_REALM_NAME for consistency, update use * Bivio::UI::HTML::Widget::Script   added JAVASCRIPT_B_COMBO_BOX() * Bivio/UI/JavaScript   NEW * Bivio::UI::View::CSS   added div.cb_selected for ComboBox * Bivio::UI::XHTML::ViewShortcuts   added more options to vs_filter_query_form() * Bivio::UI::XHTML::Widget::ComboBox   use class for selected color,   don't eat enter if dropdown is hidden   add constants for keycodes,   when typing in field, save value, restore on escape,   move static javascript to Script,   put list values in window.bivio.combobox.list_<list_class>,   move quoted value to Bivio::UI::Javascript::Widget::QuotedValue,   add auto_submit,   added hint_name   cleaned up list value formatting

--- bOP.pm
+++ bOP.pm
@@ -26,11 +26,23 @@ Model-View-Controller (MVC) architecture.  At the lowest level, bOP provides a
 cohesive infrastructure for any Perl application.
 
 We'll be writing more here later.  Please visit
-http://www.bivio.biz for more info. 
+http://www.bivio.biz for more info.
 
 =head1 CHANGES
 
   $Log$
+  Revision 7.95  2009/06/03 23:23:17  aviggio
+  * Bivio::Type::ForumName
+    added split
+  * Bivio::UI::XHTML::Widget::ComboBox
+    NEW
+  * Bivio::UI::XHTML::Widget::WikiText::Menu
+    unsafe_load_realm_file => unsafe_load_wiki_data
+  * Bivio::UI::XHTML::Widget::WikiText
+    unsafe_load_realm_file => unsafe_load_wiki_data
+  * Bivio::Util::HTTPStats
+    simplify reports forum search, add SkipFiles directive
+
   Revision 7.94  2009/06/03 03:44:39  nagler
   * Bivio::Biz::Action::WikiView
     added unsafe_load_wiki_data
