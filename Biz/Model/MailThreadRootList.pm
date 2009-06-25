40d4110a0d6202e779998c34d99fa6ee27299fc0
Rob Nagler <github@q33.us>
2009-06-25 21:33:07 +0000

    added update_uri

--- Biz/Model/MailThreadRootList.pm
+++ Biz/Model/MailThreadRootList.pm
@@ -1,12 +1,13 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::MailThreadRootList;
 use strict;
 use Bivio::Base 'Model.MailThreadList';
-my($_A) = __PACKAGE__->use('Mail.Address');
-my($_P) = __PACKAGE__->use('Search.Parser');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_A) = b_use('Mail.Address');
+my($_P) = b_use('Search.Parser');
+my($_MF) = b_use('Model.MailForm');
 
 sub DATE_SORT_ORDER {
     return 0;
@@ -22,6 +23,15 @@ sub drilldown_uri {
     return $req->format_uri({
 	task_id => $req->get('task')->get_attr_as_id('thread_task'),
 	query => $self->format_query('THIS_CHILD_LIST'),
+    });
+}
+
+sub update_uri {
+    my($self) = @_;
+    my($req) = $self->req;
+    return $req->format_uri({
+	task_id => $req->get('task')->get_attr_as_id('update_task'),
+	query => $_MF->reply_query('realm', $self),
     });
 }
 
