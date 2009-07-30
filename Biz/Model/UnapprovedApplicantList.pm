3f4f4296e4af04f9e04aa1d53a9969e64a770798
Alex Viggio <aviggio@bivio.biz>
2009-07-30 22:30:46 +0000

    order by creation_date_time

--- Biz/Model/UnapprovedApplicantList.pm
+++ Biz/Model/UnapprovedApplicantList.pm
@@ -7,6 +7,17 @@ use Bivio::Base 'Model.GroupUserList';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_R) = b_use('Auth.Role');
 
+sub internal_initialize {
+    my($self) = @_;
+    my($info) = $self->SUPER::internal_initialize;
+    my(@order) = 'RealmOwner.creation_date_time';
+    push(@order, @{$info->{order_by}});
+    delete($info->{order_by});
+    return $self->merge_initialize_info($info, {
+ 	order_by => [@order],
+    });
+}
+
 sub internal_qualify_role {
     my($self, $stmt) = @_;
     $stmt->where($stmt->EQ('RealmUser.role', [$_R->UNAPPROVED_APPLICANT]));
