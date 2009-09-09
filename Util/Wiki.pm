88d66f124163ccc145fd736d201ff10f4b24be55
Alex Viggio <aviggio@bivio.biz>
2009-09-09 20:52:58 +0000

    add convert_links and convert_titles

--- Util/Wiki.pm
+++ Util/Wiki.pm
@@ -7,15 +7,83 @@ b_use('IO.Trace');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($_TRACE);
+my($_FP) = b_use('Type.FilePath');
+my($_BF) = $_FP->BLOG_FOLDER;
+my($_VF) = $_FP->VERSIONS_FOLDER;
+my($_WN) = b_use('Type.WikiName');
+my($_WF) = $_WN->WIKI_FOLDER;
+my($_CAMEL_CASE) = b_use('XHTMLWidget.WikiText')->CAMEL_CASE_REGEX;
+my($_EMAIL) =  b_use('XHTMLWidget.WikiText')->EMAIL_REGEX;
+my($_IMG) = b_use('XHTMLWidget.WikiText')->IMG_REGEX;
+my($_DOMAIN) = b_use('XHTMLWidget.WikiText')->DOMAIN_REGEX;
 
 sub USAGE {
     return <<'EOF';
 usage: b-wiki [options] command [args..]
 commands
+  convert_links -- converts WikiText to new format requiring explicit links
+  convert_titles -- converts H3 tag at the top of blog article to H1 tag
   from_xhtml file.html ... - converts file.html to file (wiki)
   validate_all_realms [email] - call validate_realm for all realms with wiki/blog files
   validate_realm [email] - call Action.WikiValidator; send email if $email
 EOF
+}
+
+sub convert_links {
+    my($self) = @_;
+    $self->assert_not_general;
+    $self->initialize_ui;
+    $self->model('RealmFile')->do_iterate(sub {
+        my($it) = @_;
+	return 1
+	    if $it->get('is_folder')
+		|| $it->get('path_lc') =~ /\Q$_VF\E/i
+			|| ($it->get('path_lc') !~ /\Q$_WF\E/i
+				&& $it->get('path_lc') !~ /\Q$_BF\E/i);
+	$self->print("***\nCHECKING: " . $it->get('path') . "\n");
+	my($content) = ${$it->get_content};
+	$content = _upgrade_content($self, $content);
+ 	b_debug($content);
+	if (${$it->get_content} ne $content) {
+	    my($die) = Bivio::Die->catch(
+		sub {
+		    $self->print("\nCONTENT MODIFIED:\n" . $content . "\n");
+		    $it->update_with_content({
+			override_is_read_only => 1,
+		    }, \$content);
+		    $self->commit_or_rollback;
+		}
+	    );
+	}
+        return 1;
+    });
+    return;
+}
+
+sub convert_titles {
+    my($self) = @_;
+    $self->assert_not_general;
+    $self->initialize_ui;
+    $self->model('RealmFile')->do_iterate(sub {
+        my($it) = @_;
+	return 1
+	    if $it->get('is_folder') || ($it->get('path_lc') =~ m/\/blog\// ? 0 : 1);
+	$self->print("***\nCHECKING: " . $it->get('path') . "\n");
+	my($content) = _convert_title($self, ${$it->get_content});
+	if (${$it->get_content} ne $content) {
+	    my($die) = Bivio::Die->catch(
+		sub {
+		    $self->print("CONTENT MODIFIED:\n" . $content . "\n");
+		    $it->update_with_content({
+			override_is_read_only => 1,
+		    }, \$content);
+		    $self->commit_or_rollback;
+		}
+	    );
+	}
+        return 1;
+    });
+    return;
 }
 
 sub from_xhtml {
@@ -37,6 +105,47 @@ sub from_xhtml {
 	Bivio::IO::File->write($out, \$wiki);
     }
     return;
+}
+
+sub _insert_carets {
+    my(undef, $content) = @_;
+    return $content
+	if $content =~ /[\^]/;
+    $content =~ s/($_IMG|$_EMAIL|((https?:\/\/)?$_DOMAIN(\/\w*)*))/^$1/mg;
+#TODO: Tries to handle @a/@img cases
+#    $content =~ s/(?<!=)($_IMG|((mailto:)$_EMAIL)|((https?:\/\/)$_DOMAIN(\/\w+)*))/^$1/mg;
+    $content =~ s/(?<![=a-z0-9])($_CAMEL_CASE)(?![=])/^$1/mg;
+    return $content;
+}
+
+sub _remove_equals_equals {
+    my(undef, $content) = @_;
+    $content =~ s/=($_CAMEL_CASE)=/$1/g;
+    return $content;
+}
+
+sub _remove_leading_caret {
+    my(undef, $content) = @_;
+    $content =~ s/^\s*\^\s+//;
+    return $content;
+}
+
+sub _update_b_tags {
+    my(undef, $content) = @_;
+    $content
+	=~ s/^\@(?:ins\-page|b\-embed)\s(?:.*=)*(.*)$/\@b-embed value=$1/gm;
+    $content
+	=~ s/^\@b\-([a-z-]*)\s(?:\S*=)?(\S*)$/\@b-$1 value=$2/gm;
+    return $content;
+}
+
+sub _upgrade_content {
+    my($self, $content) = @_;
+    $content = _insert_carets($self, $content);
+    $content = _remove_equals_equals($self, $content);
+    $content = _update_b_tags($self, $content);
+    $content = _remove_leading_caret($self, $content);
+    return $content;
 }
 
 sub validate_all_realms {
@@ -97,6 +206,12 @@ sub validate_realm {
 	$wv->error_txt,
 	scalar(@$errors) . ' errors',
     ];
+}
+
+sub _convert_title {
+    my($self, $content) = @_;
+    $content =~ s/^\s*\@h3/\@h1/;
+    return $content;
 }
 
 sub _extract_content {
