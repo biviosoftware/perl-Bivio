1bef71ce481b97f9109e7404224fccb5cb305fb8
Alex Viggio <aviggio@bivio.biz>
2009-09-21 22:54:06 +0000

    rename convert_links to upgrade_content rename convert_titles to upgrade_blog_titles extract app-specific content upgrades

--- Util/Wiki.pm
+++ Util/Wiki.pm
@@ -7,79 +7,20 @@ b_use('IO.Trace');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($_TRACE);
-my($_FP) = b_use('Type.FilePath');
-my($_BF) = $_FP->BLOG_FOLDER;
-my($_VF) = $_FP->VERSIONS_FOLDER;
-my($_WN) = b_use('Type.WikiName');
-my($_WF) = $_WN->WIKI_FOLDER;
-my($_CAMEL_CASE) = b_use('XHTMLWidget.WikiText')->CAMEL_CASE_REGEX;
-my($_EMAIL) =  b_use('XHTMLWidget.WikiText')->EMAIL_REGEX;
-my($_IMG) = b_use('XHTMLWidget.WikiText')->IMG_REGEX;
-my($_DOMAIN) = b_use('XHTMLWidget.WikiText')->DOMAIN_REGEX;
+my($_BF) = b_use('Type.FilePath')->BLOG_FOLDER;
+my($_VF) = b_use('Type.FilePath')->VERSIONS_FOLDER;
+my($_WF) = b_use('Type.WikiName')->WIKI_FOLDER;
 
 sub USAGE {
     return <<'EOF';
 usage: b-wiki [options] command [args..]
 commands
-  convert_links -- converts WikiText to new format requiring explicit links
-  convert_titles -- converts H3 tag at the top of blog article to H1 tag
   from_xhtml file.html ... - converts file.html to file (wiki)
+  upgrade_blog_titles -- converts H3 tag at the top of blog article to H1 tag
+  upgrade_content -- upgrades WikiText to new format requiring explicit links
   validate_all_realms [email] - call validate_realm for all realms with wiki/blog files
   validate_realm [email] - call Action.WikiValidator; send email if $email
 EOF
-}
-
-sub convert_links {
-    my($self) = @_;
-    $self->assert_not_general;
-    $self->initialize_ui;
-    $self->model('RealmFile')->do_iterate(sub {
-        my($it) = @_;
-	return 1
-	    unless _is_mutable_wiki_file($it);
-	$self->print("***\nCHECKING: " . $it->get('path') . "\n");
-	my($content) = ${$it->get_content};
-	$content = _upgrade_content($self, $content);
-	if (${$it->get_content} ne $content) {
-	    my($die) = Bivio::Die->catch(
-		sub {
-		    $self->print("\nCONTENT MODIFIED:\n" . $content . "\n");
-		    $it->update_with_content({
-			override_is_read_only => 1,
-		    }, \$content);
-		    $self->commit_or_rollback;
-		}
-	    );
-	}
-        return 1;
-    });
-    return;
-}
-
-sub convert_titles {
-    my($self) = @_;
-    $self->assert_not_general;
-    $self->initialize_ui;
-    $self->model('RealmFile')->do_iterate(sub {
-        my($it) = @_;
-	return 1
-	    if $it->get('is_folder') || ($it->get('path_lc') =~ m/\/blog\// ? 0 : 1);
-	$self->print("***\nCHECKING: " . $it->get('path') . "\n");
-	my($content) = _convert_title($self, ${$it->get_content});
-	if (${$it->get_content} ne $content) {
-	    my($die) = Bivio::Die->catch(
-		sub {
-		    $self->print("CONTENT MODIFIED:\n" . $content . "\n");
-		    $it->update_with_content({
-			override_is_read_only => 1,
-		    }, \$content);
-		    $self->commit_or_rollback;
-		}
-	    );
-	}
-        return 1;
-    });
-    return;
 }
 
 sub from_xhtml {
@@ -103,57 +44,65 @@ sub from_xhtml {
     return;
 }
 
-sub _insert_carets {
-    my(undef, $content) = @_;
-    return $content
-	if $content =~ /[\^]/;
-    open(CONTENT, "<", \$content);
-    my($res) = '';
-    while (my $line = <CONTENT>) {
-	unless ($line =~ /^\@/) {
-	    $line =~ s/($_IMG|$_EMAIL|((https?:\/\/)?$_DOMAIN(\/\w*)*))/^$1/g;
-	    $line =~ s/(?<![=a-z0-9])($_CAMEL_CASE)(?![=])/^$1/g;
-	}
-	$res .= $line;
-    }
-    return $res;
-}
-
-sub _remove_equals_equals {
-    my(undef, $content) = @_;
-    $content =~ s/=($_CAMEL_CASE)=/$1/g;
-    return $content;
-}
-
-sub _remove_leading_caret {
-    my(undef, $content) = @_;
-    $content =~ s/^\s*\^\s+//;
-    return $content;
-}
-
-sub _update_caret_ampersand {
-    my(undef, $content) = @_;
-    $content =~ s/\^\&/\@\&/g;
-    return $content;
-}
-
-sub _update_b_tags {
-    my(undef, $content) = @_;
-    $content
-	=~ s/^\@(?:ins\-page|b\-embed)\s(?:.*=)*(.*)$/\@b-embed value=$1/gm;
-    $content
-	=~ s/^\@b\-([a-z-]*)\s(?:\S*=)?(\S*)$/\@b-$1 value=$2/gm;
-    return $content;
-}
-
-sub _upgrade_content {
-    my($self, $content) = @_;
-    $content = _insert_carets($self, $content);
-    $content = _remove_equals_equals($self, $content);
+sub internal_upgrade_content {
+    my($self, $content, $path_lc) = @_;
     $content = _update_caret_ampersand($self, $content);
     $content = _update_b_tags($self, $content);
-    $content = _remove_leading_caret($self, $content);
     return $content;
+}
+
+sub upgrade_blog_titles {
+    my($self) = @_;
+    $self->assert_not_general;
+    $self->initialize_ui;
+    $self->model('RealmFile')->do_iterate(sub {
+        my($it) = @_;
+	return 1
+	    unless _mutable_wikitext($it, 1);
+	$self->print("***\nCHECKING: " . $it->get('path') . "\n");
+	my($content) = _upgrade_title($self, ${$it->get_content});
+	if (${$it->get_content} ne $content) {
+	    my($die) = Bivio::Die->catch(
+		sub {
+		    $self->print("CONTENT MODIFIED:\n" . $content . "\n");
+		    $it->update_with_content({
+			override_is_read_only => 1,
+		    }, \$content);
+		    $self->commit_or_rollback;
+		}
+	    );
+	}
+        return 1;
+    });
+    return;
+}
+
+sub upgrade_content {
+    my($self) = @_;
+    $self->assert_not_general;
+    $self->initialize_ui;
+    $self->model('RealmFile')->do_iterate(sub {
+        my($it) = @_;
+	return 1
+	    unless _mutable_wikitext($it);
+	$self->print("***\nCHECKING: " . $it->get('path') . "\n");
+	my($content) = ${$it->get_content};
+	$content = $self->internal_upgrade_content($content,
+						   $it->get('path_lc'));
+	if (${$it->get_content} ne $content) {
+	    my($die) = Bivio::Die->catch(
+		sub {
+		    $self->print("\nCONTENT MODIFIED:\n" . $content . "\n");
+		    $it->update_with_content({
+			override_is_read_only => 1,
+		    }, \$content);
+		    $self->commit_or_rollback;
+		}
+	    );
+	}
+        return 1;
+    });
+    return;
 }
 
 sub validate_all_realms {
@@ -216,12 +165,6 @@ sub validate_realm {
     ];
 }
 
-sub _convert_title {
-    my($self, $content) = @_;
-    $content =~ s/^\s*\@h3/\@h1/;
-    return $content;
-}
-
 sub _extract_content {
     my($self, $tag, $children) = @_;
     return
@@ -256,16 +199,42 @@ sub _from_xhtml {
     );
 }
 
-sub _is_mutable_wiki_file {
-    my($rf) = @_;
-    my($p) = $rf->get('path_lc');
+sub _mutable_wikitext {
+    my($self, $rf, $blog_only) = @_;
     return !$rf->get('is_folder') && $rf->get_content_type =~ /wiki/i
-	&& $p !~ /\Q$_VF\/\E/i && ($p =~ /\Q$_WF\/\E/i || $p =~ /\Q$_BF\E\//i);
+	&& _check_path($self, $rf->get('path_lc'), $blog_only);
+}
+
+sub _check_path {
+    my(undef, $path, $blog_only) = @_;
+    return $path !~ /\Q$_VF\/\E/i
+	&& ($path =~ /\Q$_BF\E\//i || ($blog_only xor $path =~ /\Q$_WF\/\E/i));
 }
 
 sub _recurse {
     my($self, $op, $children) = @_;
     return join('', @{$self->map_by_two(sub {$op->($self, @_)}, $children)});
+}
+
+sub _update_caret_ampersand {
+    my(undef, $content) = @_;
+    $content =~ s/\^\&/\@\&/g;
+    return $content;
+}
+
+sub _update_b_tags {
+    my(undef, $content) = @_;
+    $content
+	=~ s/^\@(?:ins\-page|b\-embed)\s(?:.*=)*(.*)$/\@b-embed value=$1/gm;
+    $content
+	=~ s/^\@b\-([a-z-]*)\s(?:\S*=)?(\S*)$/\@b-$1 value=$2/gm;
+    return $content;
+}
+
+sub _upgrade_title {
+    my($self, $content) = @_;
+    $content =~ s/^\s*\@h3/\@h1/;
+    return $content;
 }
 
 1;
