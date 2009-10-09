c9ac597f3002e37ad41e9585660fa20d73865d92
Rob Nagler <github@q33.us>
2009-10-09 04:58:43 +0000

    addec new_from_file_line

--- IO/CallingContext.pm
+++ IO/CallingContext.pm
@@ -9,6 +9,30 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 my($_A) = 'Bivio::IO::Alert';
 
+sub calling_context_get {
+    $_A->warn_deprecated('use get');
+    return shift->get(@_);
+}
+
+sub get {
+    my($self) = shift;
+    my($fields) = $self->[$_IDI]->[0];
+    return $self->return_scalar_or_array(
+	map(exists($fields->{$_}) ? $fields->{$_}
+	    : $_A->bootstrap_die($_, ': not a calling_context field'),
+	    @_),
+    );
+}
+
+sub get_top_package_file_line_sub {
+    return @{shift->[$_IDI]->[0]}{qw(package file line sub)};
+}
+
+sub internal_as_string {
+    my($self) = @_;
+    return [$self->get(qw(file line))];
+}
+
 sub new_from_caller {
     my($proto, $skip_packages) = @_;
     my($frame) = 0;
@@ -33,28 +57,14 @@ sub new_from_caller {
 
 }
 
-sub calling_context_get {
-    $_A->warn_deprecated('use get');
-    return shift->get(@_);
-}
-
-sub get {
-    my($self) = shift;
-    my($fields) = $self->[$_IDI]->[0];
-    return $self->return_scalar_or_array(
-	map(exists($fields->{$_}) ? $fields->{$_}
-	    : $_A->bootstrap_die($_, ': not a calling_context field'),
-	    @_),
-    );
-}
-
-sub get_top_package_file_line_sub {
-    return @{shift->[$_IDI]->[0]}{qw(package file line sub)};
-}
-
-sub internal_as_string {
-    my($self) = @_;
-    return [$self->get(qw(file line))];
+sub new_from_file_line {
+    my($proto, $file, $line) = @_;
+    my($self) = $proto->SUPER::new;
+    $self->[$_IDI] = [{
+	file => $file,
+	line => $line,
+    }];
+    return $self;
 }
 
 1;
