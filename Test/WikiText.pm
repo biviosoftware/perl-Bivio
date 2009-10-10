8d857f33127454e7d7aca91ae6c4377fae1038ca
Rob Nagler <github@q33.us>
2009-10-10 22:46:30 +0000

    Added ability to set is_public in init args Added wiki_data_create & wiki_data_delete_all

--- Test/WikiText.pm
+++ Test/WikiText.pm
@@ -8,14 +8,22 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub new_unit {
     my($proto, $class_name, $args) = @_;
+    my($default_wiki_args) = {
+	is_public => 0,
+	map(
+	    exists($args->{$_}) ? (die_on_validate_error => delete($args->{$_}))
+		: (),
+	    qw(die_on_validate_error is_public),
+	),
+    };
     my($create_params);
     $class_name = b_use('XHTMLWidget.WikiText');
     return $proto->SUPER::new_unit(
 	$class_name,
 	{
 	    task_id => 'FORUM_WIKI_VIEW',
-	    realm => 'fourem',
-	    user => 'root',
+	    realm => 'wiki_bunit',
+	    user => b_use('ShellUtil.SQL')->SITE_ADM,
 	    class_name => $class_name,
 	    create_object => sub {
 		my($case, $params) = @_;
@@ -28,6 +36,7 @@ sub new_unit {
 		my($p) = $create_params->[0];
 		my($req) = b_use('Test.Request')->get_instance;
 		return [{
+		    %$default_wiki_args,
 		    ref($p) eq 'HASH' ? %$p : (value => $create_params->[0]),
 		    req => $req,
 		}];
@@ -46,10 +55,23 @@ sub new_unit {
 sub wiki_uri_to_req {
     my($self, $name) = @_;
     return $self->builtin_req->put(uri => $self->builtin_req->format_uri({
-	realm => 'fourem',
+	realm => $self->builtin_req(qw(auth_realm owner_name)),
 	task_id => 'FORUM_WIKI_VIEW',
 	path_info => $name,
     }));
+}
+
+sub wiki_data_create {
+    my($self, $name, $is_public, $content) = @_;
+    return $self->builtin_model('RealmFile')->create_with_content({
+	path => b_use('Type.WikiDataName')->to_absolute($name, $is_public),
+    }, ref($content) ? $content : \$content);
+}
+
+sub wiki_data_delete_all {
+    my($self) = @_;
+    $self->builtin_model('RealmFile')->delete_all;
+    return;
 }
 
 1;
