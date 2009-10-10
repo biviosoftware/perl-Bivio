8ab714422d1b97894dc4d2189b93562ba9b60196
Rob Nagler <github@q33.us>
2009-10-10 23:21:25 +0000

    fpc

--- TypeError.pm
+++ TypeError.pm
@@ -1,18 +1,15 @@
-# Copyright (c) 1999,2000 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2009 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::TypeError;
 use strict;
-use Bivio::Base 'Bivio::Type::Enum';
-
-# C<Bivio::TypeError>
+# Do not use Bivio::Base
+use base 'Bivio::Type::Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile(
-	Bivio::IO::ClassLoader->delegate_require_info(__PACKAGE__));
+    Bivio::IO::ClassLoader->delegate_require_info(__PACKAGE__));
 
 sub is_continuous {
-    # (proto) : false
-    # Task Ids aren't continuous.  Tasks can go away.
     return 0;
 }
 
