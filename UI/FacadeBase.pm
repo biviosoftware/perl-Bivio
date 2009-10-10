1303e2476aaeff379d562c3a2186abd5a549c285
Rob Nagler <github@q33.us>
2009-10-10 00:28:35 +0000

    Added ActionError.wiki_name.default

--- UI/FacadeBase.pm
+++ UI/FacadeBase.pm
@@ -1521,7 +1521,7 @@ sub _cfg_wiki {
             ['ActionError.wiki_name' => [
                 FORBIDDEN => 'ForbiddenError',
                 [qw(MODEL_NOT_FOUND NOT_FOUND)] => 'NotFoundError',
-                SERVER_ERROR => 'ServerError',
+                [qw(default SERVER_ERROR)] => 'ServerError',
             ]],
 	],
     };
