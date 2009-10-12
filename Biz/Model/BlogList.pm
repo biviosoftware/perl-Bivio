92375d1a49d4133e5db203de410413437140b3f6
Rob Nagler <github@q33.us>
2009-10-12 17:10:50 +0000

    Set is_inline_text explicitly in render_html

--- Biz/Model/BlogList.pm
+++ Biz/Model/BlogList.pm
@@ -165,10 +165,10 @@ sub render_html {
 	name => $self->get('path_info'),
 	req => $self->get_request,
 	task_id => undef,
+	is_inline_text => 0,
 	calling_context =>
 	    $_CC->new_from_file_line($self->get('RealmFile.path'), 1),
 	map(($_ => $self->get("RealmFile.$_")), qw(is_public realm_id path)),
-	no_auto_links => 1,
     });
 }
 
