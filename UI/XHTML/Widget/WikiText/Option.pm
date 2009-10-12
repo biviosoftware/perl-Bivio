ff95aaec19e8d33c23dc1ce9af91c71f38833e7d
Rob Nagler <github@q33.us>
2009-10-12 04:40:05 +0000

    *** empty log message ***

---
+++ UI/XHTML/Widget/WikiText/Option.pm
@@ -0,0 +1,29 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::XHTML::Widget::WikiText::Option;
+use strict;
+use Bivio::Base 'XHTMLWidget.WikiTextTag';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub handle_register {
+    return [qw(b-option)];
+}
+
+sub parse_tag_start {
+    sub PARSE_TAG_START {[
+	[qw(paragraphing Boolean), undef],
+    ]};
+    my($proto, $args, $attrs) = shift->parameters(@_);
+    return
+	unless $proto;
+    $args->{state}->{option}->{paragraphing} = $attrs->{paragraphing}
+	if defined($attrs->{paragraphing});
+    return;
+}
+
+sub render_html {
+    return '';
+}
+
+1;
