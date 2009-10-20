8de6c5087f34a7c1c3137e2cff1056e170103d6a
Rob Nagler <github@q33.us>
2009-10-20 22:23:24 +0000

    remove all *.bs, .packlist, perllocal.pod from top level build dir, because /usr/lib64 is not necessarily where perl gets installed

--- Util/Release.pm
+++ Util/Release.pm
@@ -862,6 +862,11 @@ sub _link_base_version {
     unlink($base);
     _output($output, "LINKING $version AS $base\n");
     _system("ln -s '$version' '$base'", $output);
+    return
+	if $base =~ /-HEAD\./;
+    unlink($base);
+    _output($output, "LINKING $version AS $base\n");
+    _system("ln -s '$version' '$base'", $output);
     return;
 }
 
@@ -890,7 +895,7 @@ sub _perl_make {
 	    && $Config::Config{$_} && $Config::Config{$_} =~ m!^/!,
 	    sort(keys(%Config::Config))))
 	.  ' POD2MAN=true pure_install && '
-        . ' find $RPM_BUILD_ROOT%{_libdir}/perl? -name "*.bs" '
+        . ' find $RPM_BUILD_ROOT -name "*.bs" '
 	. " -o -name .packlist -o -name perllocal.pod | xargs rm -f\n";
 }
 
