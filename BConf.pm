1ed46e7890edbe33c6aa75df47fc75e875de4081
Paul Moeller <moeller@bivio.biz>
2009-11-14 00:45:30 +0000

    ignore JOB_START/END changed

--- BConf.pm
+++ BConf.pm
@@ -320,7 +320,7 @@ sub merge_http_log {
 		    '\] \[(?:info|debug)\] ',
 		    '\[notice\] Apache/\S+ configured -- resuming normal operations',
 		    '\[notice\] Accept mutex',
-		    'Dispatcher::execute_queue:.*JOB_(?:START|END):',
+		    'Dispatcher::.* JOB_(?:START|END):',
 		    # Virii and such
 		    '(?:File does not exist:|DieCode::NOT_FOUND:).*(?:robots.txt|system32|\.asp|_vti|default\.ida|/sumthin|/scripts|/cgi|root.exe|/instmsg|/favicon2|site_root/default.bview|\.php$)',
 		    '::NOT_FOUND:.*view..site_root/(\w+.html|robots.txt).bview',
