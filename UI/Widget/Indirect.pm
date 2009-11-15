48da09e81c80cc6675aedeb6be020a6db869dfa5
Rob Nagler <github@q33.us>
2009-11-15 16:26:21 +0000

    rmpod

--- UI/Widget/Indirect.pm
+++ UI/Widget/Indirect.pm
@@ -2,99 +2,29 @@
 # $Id$
 package Bivio::UI::Widget::Indirect;
 use strict;
-$Bivio::UI::Widget::Indirect::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::Widget::Indirect::VERSION;
+use Bivio::Base 'Bivio::UI::Widget';
 
-=head1 NAME
+# C<Bivio::UI::Widget::Indirect> adds a level of indirection to
+# the rendering of widgets.  The widget which is this widget's I<value>
+# is rendered dynamically by accessing this widget's attributes dynamically.
+#
+#
+#
+# value : Bivio::UI::Widget (required, dynamic)
+#
+# Accessed dynamically.  If the dynamic value is false, nothing is rendered.
+# B<NOTE: the widget is not initialized.  You must do this yourself.>
+#
+# value : array_ref (required,dynamic)
+#
+# Accessed dynamically.  Widget value must be a widget or false.
 
-Bivio::UI::Widget::Indirect - renders an arbitrary widget dynamically
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::Widget::Indirect;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::Widget::Indirect::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::Widget::Indirect> adds a level of indirection to
-the rendering of widgets.  The widget which is this widget's I<value>
-is rendered dynamically by accessing this widget's attributes dynamically.
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item value : Bivio::UI::Widget (required, dynamic)
-
-Accessed dynamically.  If the dynamic value is false, nothing is rendered.
-B<NOTE: the widget is not initialized.  You must do this yourself.>
-
-=item value : array_ref (required,dynamic)
-
-Accessed dynamically.  Widget value must be a widget or false.
-
-=back
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(hash_ref attributes) : Bivio::UI::Widget::Indirect
-
-=head2 static new(array_ref value) : Bivio::UI::Widget::Indirect
-
-=head2 static new(Bivio::UI::Widget value) : Bivio::UI::Widget::Indirect
-
-=head2 static new(boolean value) : Bivio::UI::Widget::Indirect
-
-Creates a new Indirect widget.  I<value> may be anything but a hash_ref,
-really.  If it is a hash_ref, it must contain a I<value> attribute.
-
-=cut
-
-sub new {
-    my($proto, @args) = _new_args(@_);
-    my($self) = $proto->SUPER::new(@args);
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="execute"></a>
-
-=head2 execute(Bivio::Agent::Request req)
-
-Executes the child widget as selected from I<req> (as source).
-
-=cut
-
 sub execute {
+    # (self, Agent.Request) : undef
+    # Executes the child widget as selected from I<req> (as source).
     my($self, $req) = @_;
     my($w) = _select($self, $req);
     Bivio::Die->die('Indirect did not select a widget; no content type')
@@ -102,30 +32,33 @@ sub execute {
     return $w->execute($req);
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Render the indirect.  If there the widget value is false, then
-nothing is rendered.  If the widget value is an array_ref,
-it first calls get_widget_value to get the actual value.
-
-=cut
+sub new {
+    # (proto, hash_ref) : Widget.Indirect
+    # (proto, array_ref) : Widget.Indirect
+    # (proto, UI.Widget) : Widget.Indirect
+    # (proto, boolean) : Widget.Indirect
+    # Creates a new Indirect widget.  I<value> may be anything but a hash_ref,
+    # really.  If it is a hash_ref, it must contain a I<value> attribute.
+    my($proto, @args) = _new_args(@_);
+    my($self) = $proto->SUPER::new(@args);
+    $self->[$_IDI] = {};
+    return $self;
+}
 
 sub render {
+    # (self, any, string_ref) : undef
+    # Render the indirect.  If there the widget value is false, then
+    # nothing is rendered.  If the widget value is an array_ref,
+    # it first calls get_widget_value to get the actual value.
     my($self, $source, $buffer) = @_;
     my($w) = _select($self, $source);
     $w->render($source, $buffer) if ref($w);
     return;
 }
 
-#=PRIVATE METHODS
-
-# _new_args(proto, any value, ...) : array
-#
-# Returns arguments to be passed to Attributes::new.
-#
 sub _new_args {
+    # (proto, any, ...) : array
+    # Returns arguments to be passed to Attributes::new.
     my($proto, $value) = @_;
     return ($proto, $value) if ref($value) eq 'HASH';
     # We accept any value
@@ -134,26 +67,14 @@ sub _new_args {
     });
 }
 
-# _select(self, any source) : Bivio::UI::Widget
-#
-# Returns the widget as directed by the source
-#
 sub _select {
+    # (self, any) : UI.Widget
+    # Returns the widget as directed by the source
     my($self, $source) = @_;
     my($v) = $self->get('value');
     return unless ref($v);
     return $source->get_widget_value(@$v) if ref($v) eq 'ARRAY';
     return $v;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
