43d1ec76135ceb22b3378d241ba45db5d86084b8
Rob Nagler <github@q33.us>
2009-11-18 19:16:13 +0000

    was not managing references correctly

--- Biz/Action/EasyForm.pm
+++ Biz/Action/EasyForm.pm
@@ -38,9 +38,9 @@ sub execute {
     }
     $d =~ s{^.*?\n}{_headings_csv($headings)}es
 	if $new_headings;
-    $d = $_TA->canonicalize_newlines(\$d);
-    $$d .= ${$_CSV->to_csv_text([[map($form->{$_}, @$headings)]])};
-    $rf->update_with_content({user_id => $rf->get('user_id')}, $d);
+    my($ref) = $_TA->canonicalize_newlines(\$d);
+    $$ref .= ${$_CSV->to_csv_text([[map($form->{$_}, @$headings)]])};
+    $rf->update_with_content({user_id => $rf->get('user_id')}, $ref);
     my($email) = _email($rf, $base);
     $proto->new({
 	file_path => $rf->get('path'),
