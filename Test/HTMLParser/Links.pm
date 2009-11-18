b37e2a11e3dacf1786ec16b4c8df2dbf4c6e3fcc
Rob Nagler <github@q33.us>
2009-11-18 20:46:47 +0000

    rmpod

--- Test/HTMLParser/Links.pm
+++ Test/HTMLParser/Links.pm
@@ -2,112 +2,27 @@
 # $Id$
 package Bivio::Test::HTMLParser::Links;
 use strict;
-$Bivio::Test::HTMLParser::Links::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Test::HTMLParser::Links::VERSION;
-
-=head1 NAME
-
-Bivio::Test::HTMLParser::Links - models links on the page
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Test::HTMLParser::Links;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Test::HTMLParser>
-
-=cut
-
-use Bivio::Test::HTMLParser;
-@Bivio::Test::HTMLParser::Links::ISA = ('Bivio::Test::HTMLParser');
-
-=head1 DESCRIPTION
-
-C<Bivio::Test::HTMLParser::Links> models the links on a page.
-
-=cut
-
-=head1 ATTRIBUTES
-
-Links are stored as attributes indexed by their labels.
-
-=over 4
-
-=item E<lt>labelE<gt> => {
-
-            label =>  <label of the hyperlink>,
-            href  =>  <contents of href attribute>,
-            alt   =>  <contents of alt attribute>
-       }
-
-=item usage:  $links->get('label')->{href}
-
-=back
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::Test::HTMLParser';
 use Bivio::IO::Trace;
 
-#=VARIABLES
-use vars ('$_TRACE');
-Bivio::IO::Trace->register;
+# C<Bivio::Test::HTMLParser::Links> models the links on a page.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($_TRACE);
 my($_IDI) = __PACKAGE__->instance_data_index;
 __PACKAGE__->register(['Cleaner']);
 
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(Bivio::Test::HTMLParser parser) : Bivio::Test::HTMLParser::Links
-
-Parses cleaned html for links.
-
-=cut
-
-sub new {
-    my($proto, $parser) = @_;
-    my($self) = $proto->SUPER::new;
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="html_parser_end"></a>
-
-=head2 html_parser_end(string tag, string origtext)
-
-Dispatch to the _end_XXX routines.
-
-=cut
-
 sub html_parser_end {
+    # (self, string, string) : undef
+    # Dispatch to the _end_XXX routines.
     my($self, $tag) = @_;
     return _end_a($self) if $tag eq 'a';
     return;
 }
 
-=for html <a name="html_parser_start"></a>
-
-=head2 html_parser_start(string tag, hash_ref attr, array_ref attrseq, string origtext)
-
-Dispatches to the _start_XXX routines.
-
-=cut
-
 sub html_parser_start {
+    # (self, string, hash_ref, array_ref, string) : undef
+    # Dispatches to the _start_XXX routines.
     my($self, $tag, $attr) = @_;
     my($fields) = $self->[$_IDI];
     return _start_a($fields, $attr) if $tag eq 'a';
@@ -115,18 +30,12 @@ sub html_parser_start {
     return;
 }
 
-=for html <a name="html_parser_text"></a>
-
-=head2 html_parser_text(string text)
-
-Text is applied to the current link, if any.
-
-For links, we can't assume that we are called with an entire sequence
-of text (like Forms), so we append until the end_a.
-
-=cut
-
 sub html_parser_text {
+    # (self, string) : undef
+    # Text is applied to the current link, if any.
+    #
+    # For links, we can't assume that we are called with an entire sequence
+    # of text (like Forms), so we append until the end_a.
     my($self, $text) = @_;
     my($fields) = $self->[$_IDI];
     $text = $self->get('cleaner')->text($text);
@@ -134,13 +43,18 @@ sub html_parser_text {
     return;
 }
 
-#=PRIVATE METHODS
+sub new {
+    # (proto, Test.HTMLParser) : HTMLParser.Links
+    # Parses cleaned html for links.
+    my($proto, $parser) = @_;
+    my($self) = $proto->SUPER::new;
+    $self->[$_IDI] = {};
+    return $self;
+}
 
-# _end_a(self)
-#
-# No longer in a link.
-#
 sub _end_a {
+    # (self) : undef
+    # No longer in a link.
     my($self) = @_;
     my($fields) = $self->[$_IDI];
     _link($self, $fields->{text})
@@ -149,11 +63,9 @@ sub _end_a {
     return;
 }
 
-# _link(self, string label, string alt)
-#
-# Adds the link.  Creates unique name ($label_$i) if not unique.
-#
 sub _link {
+    # (self, string, string) : undef
+    # Adds the link.  Creates unique name ($label_$i) if not unique.
     my($self, $label, $alt) = @_;
     my($fields) = $self->[$_IDI];
     my($base, $i) = $label;
@@ -171,11 +83,9 @@ sub _link {
     return;
 }
 
-# _start_a(hash_ref fields, hash_ref attr)
-#
-# Stores the href.
-#
 sub _start_a {
+    # (hash_ref, hash_ref) : undef
+    # Stores the href.
     my($fields, $attr) = @_;
     Bivio::Die->die(
 	'already have an href (missing </a>). current=', $fields->{href},
@@ -191,11 +101,9 @@ sub _start_a {
     return;
 }
 
-# _start_img(self, hash_ref attr)
-#
-# Adds a new link.
-#
 sub _start_img {
+    # (self, hash_ref) : undef
+    # Adds a new link.
     my($self, $attr) = @_;
     my($fields) = $self->[$_IDI];
     return unless $fields->{href};
@@ -206,15 +114,5 @@ sub _start_img {
     _link($self, $attr->{src}, $attr->{alt});
     return;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002-2006 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
