2e9e68fdcce5ccc806e38da1a5a369c281165b47
Rob Nagler <github@q33.us>
2009-11-20 17:48:56 +0000

    check_value takes type added IS_SPECIFIED

--- SQL/Constraint.pm
+++ SQL/Constraint.pm
@@ -1,33 +1,30 @@
-# Copyright (c) 1999-2007 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2009 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::SQL::Constraint;
 use strict;
 use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_TE) = __PACKAGE__->use('Bivio::TypeError');
-my($_E) = __PACKAGE__->use('Type.Enum');
+my($_TE) = b_use('Bivio::TypeError');
+my($_E) = b_use('Type.Enum');
 __PACKAGE__->compile([
     NONE => [0],
     PRIMARY_KEY => [1],
     NOT_NULL => [2],
     NOT_NULL_UNIQUE => [3],
     NOT_ZERO_ENUM => [4],
-    NOT_NULL_SET => [5],
+    IS_SPECIFIED => [5],
 ]);
 
 sub check_value {
-    my($self, $value) = @_;
+    my($self, $type, $value) = @_;
     return
 	if $self->eq_none;
     return $_TE->NULL
 	unless defined($value);
-    if ($self->eq_not_zero_enum) {
-	return $_TE->NOT_ZERO
-	    unless $value->as_int != 0;
-    }
-    elsif ($self->eq_not_null_set) {
-	die($self, ': check_value not supported');
+    if ($self->equals_by_name(qw(NOT_ZERO_ENUM IS_SPECIFIED))) {
+	return $_TE->UNSPECIFIED
+	    unless $type->is_specified($value);
     }
     return;
 }
