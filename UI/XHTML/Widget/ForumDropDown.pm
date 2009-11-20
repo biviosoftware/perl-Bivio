3a65d4b0cb321dc0060d54c87141daaf615d2bea
Rob Nagler <github@q33.us>
2009-11-20 17:57:15 +0000

    use DEFAULT_REALM_TYPE

--- UI/XHTML/Widget/ForumDropDown.pm
+++ UI/XHTML/Widget/ForumDropDown.pm
@@ -1,21 +1,13 @@
-# Copyright (c) 2007-2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2007-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::XHTML::Widget::ForumDropDown;
 use strict;
 use Bivio::Base 'XHTMLWidget.RealmDropDown';
-use Bivio::UI::ViewLanguageAUTOLOAD;
-my($_FORUM) = __PACKAGE__->use('Auth.RealmType')->FORUM;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-sub NEW_ARGS {
-    return [];
-}
-
-sub initialize {
-    my($self) = @_;
-    $self->put_unless_exists(realm_type => $_FORUM);
-    return shift->SUPER::initialize(@_);
+sub DEFAULT_REALM_TYPE {
+    return 'FORUM';
 }
 
 1;
