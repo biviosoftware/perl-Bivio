2d4ec6d29f9e942b5f65e515945834819a6de1b2
Rob Nagler <github@q33.us>
2009-11-27 18:07:55 +0000

    rmpod

--- PetShop/Widget/Search.pm
+++ PetShop/Widget/Search.pm
@@ -1,70 +1,24 @@
-# Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2001-2009 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::PetShop::Widget::Search;
 use strict;
-$Bivio::PetShop::Widget::Search::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::PetShop::Widget::Search::VERSION;
-
-=head1 NAME
-
-Bivio::PetShop::Widget::Search - search box and button
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::PetShop::Widget::Search;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::PetShop::Widget::Search::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::PetShop::Widget::Search> simple search form.
-
-=cut
-
-#=IMPORTS
 use Bivio::Agent::TaskId;
+use Bivio::Base 'Bivio::UI::Widget';
 use Bivio::UI::Font;
 
-#=VARIABLES
+# C<Bivio::PetShop::Widget::Search> simple search form.
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Does nothing.  Widget is entirely dynamic.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub initialize {
+    # (self) : undef
+    # Does nothing.  Widget is entirely dynamic.
     return;
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Render the input field.
-
-=cut
-
 sub render {
+    # (self, any, string_ref) : undef
+    # Render the input field.
     my($self, $source, $buffer) = @_;
     my($req) = $source->get_request;
 
@@ -89,17 +43,5 @@ sub render {
 	    .'</form>';
     return;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
