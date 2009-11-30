787afe6d3a905917c20bda04441bc8f8e1f1b37c
Rob Nagler <github@q33.us>
2009-11-30 21:18:28 +0000

    multiple DropDowns now work

--- UI/XHTML/Widget/DropDown.pm
+++ UI/XHTML/Widget/DropDown.pm
@@ -13,11 +13,11 @@ sub initialize {
     my($id) = $self->get_nested(qw(widget id));
     $self->die($id, undef, 'widget.id is not JS identifier')
 	unless $id =~ /^[a-z]\w+$/s;
-    my($var) = "window.bivio.drop_down_$id";
+    my($local) = JavaScript()->var_name("drop_down_$id");
     $self->put_unless_exists(values => [
 	$self->get('widget'),
-	_js($var, $id),
-	_link($self, $var),
+	[sub {_js(@_)}, JavaScript()->var_name('drop_down'), $local, $id],
+	_link($self, $local),
     ]);
     return shift->SUPER::initialize(@_);
 }
@@ -32,38 +32,61 @@ sub internal_new_args {
 }
 
 sub _js {
-    my($var, $id) = @_;
-    my($x) = JavaScript()->strip(<<"EOF");
-<script type="text/javascript">
-window.bivio = window.bivio || {};
+    my($source, $global, $local, $id) = @_;
+    my($b) = '';
+    my($js) = JavaScript();
+    $js->render(
+	$source,
+	\$b,
+	__PACKAGE__,
+	$js->strip(<<"EOF"),
 (function (){
-    $var = $var || {};
-    var b = $var;
-    b.close = function (e) {
-        b.s.visibility == 'visible' && b.toggle(e);
-    };
-    b.toggle = function (e) {
-	if (!b.s) {
-	    b.s = document.getElementById('$id').style;
-	    document.onclick = b.close;
+    $global = $global || {};
+    var dd = $global;
+    dd.curr = null;
+    dd.toggle = function (e, b2) {
+	if (dd.curr) {
+	    var c = dd.curr;
+	    dd.curr = null;
+	    c.toggle(e);
 	}
-	b.s.visibility = b.s.visibility == 'visible' ? 'hidden' : 'visible';
-	(e||window.event).cancelBubble = true;
+	(e || window.event).cancelBubble = true;
+	if (!b2)
+	    return;
+	if (b2.style.visibility == 'visible') {
+	    b2.style.visibility = 'hidden';
+	}
+	else {
+	    b2.style.visibility = 'visible';
+	    dd.curr = b2;
+	}
+    };
+    document.onclick = function(e) {
+	dd.toggle(e, null);
     };
 })();
-</script>
 EOF
-    chomp($x);
-    return $x;
+	JavaScript()->strip(<<"EOF"),
+(function (){
+    $local = $local || {};
+    var b = $local;
+    b.toggle = function (e) {
+        b.style = b.style || document.getElementById('$id').style;
+        $global.toggle(e, $local);
+    };
+})();
+EOF
+    );
+    return $b;
 }
 
 sub _link {
-    my($self, $var) = @_;
+    my($self, $local) = @_;
     return A(
 	Join([$self->get('label'), vs_text_as_prose('drop_down_arrow')]),
 	{
-	    HREF => '/',
-	    ONCLICK => "this.blur(); $var.toggle(event); return false",
+	    HREF => '#',
+	    ONCLICK => "this.blur(); $local.toggle(event); return false",
 	    class => $self->get('link_class'),
 	},
     );
