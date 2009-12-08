2caf002261767bfbe5ec115ecf4df996b4aa49ef
Rob Nagler <github@q33.us>
2009-12-08 17:50:15 +0000

    *** empty log message ***

---
+++ Type/MACAddress.pm
@@ -0,0 +1,21 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Type::MACAddress;
+use strict;
+use Bivio::Base 'Type.SyntacticString';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub REGEX {
+    return qr{(?:[0-9a-f]{2}\:){5}[0-9a-f]{2}}i;
+}
+
+sub get_width {
+    return 17;
+}
+
+sub internal_post_from_literal {
+    return lc($_[1]);
+}
+
+1;
