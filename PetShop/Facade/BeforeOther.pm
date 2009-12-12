996219a772f9298a60f96a02b32416cb337cafcd
Rob Nagler <github@q33.us>
2009-12-12 20:44:29 +0000

    *** empty log message ***

---
+++ PetShop/Facade/BeforeOther.pm
@@ -0,0 +1,24 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::PetShop::Facade::BeforeOther;
+use strict;
+use Bivio::Base 'Facade.Other';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+__PACKAGE__->new({
+    uri => 'beforeother',
+    http_host => 'beforeother.bivio.biz',
+    mail_host => 'beforeother.bivio.biz',
+    clone => 'Other',
+    Constant => __PACKAGE__->make_groups([
+	@{__PACKAGE__->bunit_shared_values},
+	shared_value2 => 'BeforeOther',
+    ]),
+    Text => __PACKAGE__->make_groups([
+	@{__PACKAGE__->bunit_shared_values},
+	shared_value1 => 'BeforeOther',
+    ]),
+});
+
+1;
