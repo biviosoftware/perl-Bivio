79f5493020f23aa5e7f317c1e82da5c2641d4b26
Rob Nagler <github@q33.us>
2009-12-13 19:22:28 +0000

    fmt

--- UI/TableRowClass.pm
+++ UI/TableRowClass.pm
@@ -1,25 +1,14 @@
-# Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2009 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::TableRowClass;
 use strict;
-use Bivio::Base 'Bivio::Type::Enum';
-
-# C<Bivio::UI::TableRowClass> controls rendering of widget table rows.
-# This is used internally by
-# L<Bivio::UI::HTML::Widget::Table|Bivio::UI::HTML::Widget::Table>.
-#
-#
-# HEADING
-#
-# DATA
-#
-# FOOTER
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile([
-    HEADING => [1],
-    DATA => [2],
-    FOOTER => [3],
+    HEADING => 1,
+    DATA => 2,
+    FOOTER => 3,
 ]);
 
 1;
