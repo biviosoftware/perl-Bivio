8af773ae5b4af93804cdf57ce707acdf5aa40899
Rob Nagler <github@q33.us>
2009-12-14 01:30:24 +0000

    rmpod

--- UI/ViewShortcutsBase.pm
+++ UI/ViewShortcutsBase.pm
@@ -2,93 +2,36 @@
 # $Id$
 package Bivio::UI::ViewShortcutsBase;
 use strict;
-$Bivio::UI::ViewShortcutsBase::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::ViewShortcutsBase::VERSION;
+use Bivio::Base 'Bivio::UNIVERSAL';
 
-=head1 NAME
+# C<Bivio::UI::ViewShortcutsBase> is subclassed by classes which implement view
+# helper methods called from L<Bivio::UI::ViewLanguage|Bivio::UI::ViewLanguage>.
+#
+# The methods are available from views and
+# L<Bivio::UI::Widget::Prose|Bivio::UI::Widget::Prose>.  All methods defined here
+# must begin with C<vs_> (view shortcut) and be static.  This is enforced by this
+# module.
+#
+# You may specify the shortcuts by
+# L<Bivio::UI::ViewLanguage::view_shortcuts|Bivio::UI::ViewLanguage/"view_shortcuts">.
 
-Bivio::UI::ViewShortcutsBase - site specific helper methods for templates and views
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::ViewShortcutsBase;
-
-=cut
-
-use Bivio::UNIVERSAL;
-@Bivio::UI::ViewShortcutsBase::ISA = ('Bivio::UNIVERSAL');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::ViewShortcutsBase> is subclassed by classes which implement view
-helper methods called from L<Bivio::UI::ViewLanguage|Bivio::UI::ViewLanguage>.
-
-The methods are available from views and
-L<Bivio::UI::Widget::Prose|Bivio::UI::Widget::Prose>.  All methods defined here
-must begin with C<vs_> (view shortcut) and be static.  This is enforced by this
-module.
-
-You may specify the shortcuts by
-L<Bivio::UI::ViewLanguage::view_shortcuts|Bivio::UI::ViewLanguage/"view_shortcuts">.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new()
-
-Dies.  You can't instantiate a ViewShortcut.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub new {
+    # (proto) : undef
+    # Dies.  You can't instantiate a ViewShortcut.
     Bivio::Die->die(
 	"you can't instantiate a ViewShortcut; perhaps you meant vs_new()?");
     # DOES NOT RETURN
 }
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="view_autoload"></a>
-
-=head2 view_autoload(string method, array_ref args) : any
-
-Called as last resort when the method doesn't match the
-standard ViewLanguage patterns (vs_*, view_*, SomeWidget).
-
-=cut
-
 sub view_autoload {
+    # (self, string, array_ref) : any
+    # Called as last resort when the method doesn't match the
+    # standard ViewLanguage patterns (vs_*, view_*, SomeWidget).
     my(undef, $method, $args) = @_;
     Bivio::Die->die("$method invalid view function, widget, or shortcut.");
     # DOES NOT RETURN
 }
 
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
-
 1;
-
