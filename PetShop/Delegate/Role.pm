4a3236297e5ae50d0fc2f0673576bd335b374829
Rob Nagler <github@q33.us>
2009-12-26 01:11:16 +0000

    rmpod

--- PetShop/Delegate/Role.pm
+++ PetShop/Delegate/Role.pm
@@ -2,72 +2,20 @@
 # $Id$
 package Bivio::PetShop::Delegate::Role;
 use strict;
-$Bivio::PetShop::Delegate::Role::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::PetShop::Delegate::Role::VERSION;
+use Bivio::Base 'Bivio::Delegate::Role';
 
-=head1 NAME
+# C<Bivio::PetShop::Delegate::Role>
 
-Bivio::PetShop::Delegate::Role - roles for the PetShop application
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::PetShop::Delegate::Role;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Delegate::Role>
-
-=cut
-
-use Bivio::Delegate::Role;
-@Bivio::PetShop::Delegate::Role::ISA = ('Bivio::Delegate::Role');
-
-=head1 DESCRIPTION
-
-C<Bivio::PetShop::Delegate::Role>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_delegate_info"></a>
-
-=head2 get_delegate_info() : array_ref
-
-Return test roles.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_delegate_info {
+    # (self) : array_ref
+    # Return test roles.
     return [
 	@{shift->SUPER::get_delegate_info(@_)},
 	TEST_ROLE1 => [21],
 	TEST_ROLE2 => [22],
     ];
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
