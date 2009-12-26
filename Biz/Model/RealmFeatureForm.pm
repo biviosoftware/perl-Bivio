75c5710d1780db4c64fbf5111165e501849d2ef9
Rob Nagler <github@q33.us>
2009-12-26 22:53:05 +0000

    generalized interface so can be subclassed easily execute_empty and execute_ok behave properly in all cases introduced row_tag_replace_value & row_tag_get_value APIs introduced as_realm_role_category

--- Biz/Model/RealmFeatureForm.pm
+++ Biz/Model/RealmFeatureForm.pm
@@ -5,72 +5,56 @@ use strict;
 use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_F) = b_use('UI.Facade');
 my($_RR) = b_use('ShellUtil.RealmRole');
-my($_EMAIL_MODES) = [map(lc($_), b_use('Type.ForumEmailMode')->OPTIONAL_MODES)];
-my($_CATEGORY_DEFAULTS) = {
-#TODO: Define list here, but might be worth confirming against features
-# supported in BConf category map
-    feature_blog => 1,
-    feature_calendar => 1,
-    feature_file => 1,
-    feature_mail => 1,
-    feature_motion => 1,
-    feature_tuple => 1,
-    map(($_ => 0), @$_EMAIL_MODES),
+my($_MSA) = b_use('Type.MailSendAccess');
+my($_FEATURE_TYPE_MAP) = {
+    map(($_ => b_use('Type.RealmFeature')), qw(
+	feature_blog
+	feature_calendar
+	feature_file
+	feature_mail
+	feature_motion
+	feature_tuple
+    )),
+    mail_want_reply_to => b_use('Type.MailWantReplyTo'),
+    mail_send_access => b_use('Type.MailSendAccess'),
 };
-my($_CATEGORIES) = [sort(keys(%$_CATEGORY_DEFAULTS))];
-my($_ENABLED_CATEGORIES) = [grep($_CATEGORY_DEFAULTS->{$_}, @$_CATEGORIES)];
-my($_FEM) = b_use('Type.ForumEmailMode');
 
-sub CATEGORY_LIST {
-    return @$_CATEGORIES;
-}
-
-sub FEATURE_LIST {
-    return grep(/^feature/, @$_CATEGORIES);
+sub FEATURE_TYPE_MAP {
+    return {%$_FEATURE_TYPE_MAP};
 }
 
 sub execute_empty {
     my($self) = @_;
-    $self->internal_use_general_realm_for_site_admin(sub {
-        $self->load_from_model_properties('RealmOwner')
-            unless $self->req('auth_realm')->is_general;
-        $self->internal_put_categories(1);
-    });
+    $self->internal_put_field(%{_empty_values($self)});
     return;
 }
 
 sub execute_ok {
     my($self) = @_;
-    $self->internal_use_general_realm_for_site_admin(sub {
-        my($x) = $self->get_model_properties('RealmOwner');
-        foreach my $k (keys(%$x)) {
-            delete($x->{$k})
-                unless defined($x->{$k});
-        }
-        if ($self->req('auth_realm')->is_general) {
-            my($forum, $ro) = $self->new_other('Forum')
-                ->create_realm({}, $x, $self->req('auth_user_id'));
-            $self->req->with_realm($ro, sub {
-                $self->internal_edit_categories;
-            })
-        }
-        else {
-            $self->get_model('RealmOwner')->update($x)
-                if keys(%$x);
-            $self->internal_edit_categories;
-        }
-        return;
-    });
-    return;
-}
-
-sub internal_edit_categories {
-    my($self) = @_;
-    $_RR->edit_categories({
-        feature_wiki => 1,
-        map(($_ => $self->get($_)), @$_CATEGORIES),
+    b_die('cannot be executed in the general realm')
+	if $self->req('auth_realm')->is_general;
+    my($ev) = _empty_values($self);
+    $self->map_feature_type(sub {
+        my($field, $type) = @_;
+	my($v);
+	if ($self->unsafe_get('force_default_values')) {
+	    $v = $ev->{$field};
+	}
+	else {
+	    return
+		unless defined($v = $self->unsafe_get($field));
+	    return
+		if $type->is_equal($v, $ev->{$field});
+	}
+	return $type->row_tag_replace_value($v)
+	    if $type->can('row_tag_replace_value');
+	$_RR->edit_categories(
+	    $type->can('as_realm_role_category')
+		? {$v->as_realm_role_category => 1}
+		: {$field => $v},
+	);
+	return;
     });
     return;
 }
@@ -79,109 +63,39 @@ sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
-        visible => [
-            $self->field_decl([
-                [('RealmOwner.display_name')x2],
-                [('RealmOwner.name')x2],
-            ], undef, 'NONE'),
-	    $self->field_decl(
-		[
-                    @$_CATEGORIES,
-                    [email_mode => 'ForumEmailMode', 'NONE'],
-                ],
-		'NullBoolean',
-	    ),
-        ],
-        require_context => 1,
+	$self->field_decl(
+	    visible => $self->map_feature_type(sub {[@_]}),
+	    other => [[qw(force_default_values Boolean)]],
+	),
 	auth_id => ['RealmOwner.realm_id'],
     });
 }
 
-sub internal_put_categories {
-    my($self, $overwrite) = @_;
-    my($cats) = $self->req('auth_realm')->is_general
-	? [@$_ENABLED_CATEGORIES]
-	: $_RR->list_enabled_categories;
-    foreach my $c (@$_CATEGORIES) {
-	$self->internal_put_field($c => grep($_ eq $c, @$cats) ? 1 : 0)
-	    if $overwrite || !defined($self->unsafe_get($c));
-    }
-    _put_email_mode($self);
-    return;
-}
-
-sub internal_put_field_category_defaults {
-    my($self) = @_;
-    _put_default_email_mode($self);
-    return shift->internal_put_field(%$_CATEGORY_DEFAULTS)
-}
-
-sub internal_use_general_realm_for_site_admin {
+sub map_feature_type {
     my($self, $op) = @_;
-    return $self->req->with_realm(undef, $op)
-        if $_F->get_from_source($self)->auth_realm_is_site_admin($self->req);
-    return $op->();
+    my($m) = $self->FEATURE_TYPE_MAP;
+    return [map($op ? $op->($_, $m->{$_}) : $_, sort(keys(%$m)))];
 }
 
-sub internal_validate_email_modes {
+sub _empty_values {
     my($self) = @_;
-    if (my $mode = $self->unsafe_get('email_mode')) {
-        $self->internal_put_field(
-            map(($_ => $mode->equals_by_name($_) ? 1 : 0), @$_EMAIL_MODES),
-        );
-    }
-    my($x) = [grep($self->unsafe_get($_), @$_EMAIL_MODES)];
-    $self->internal_put_error($x->[1], 'MUTUALLY_EXCLUSIVE')
-        if @$x > 1;
-    return;
-}
-
-sub validate {
-    my($self) = @_;
-    $self->internal_use_general_realm_for_site_admin(sub {
-        $self->internal_validate_email_modes;
-        $self->validate_not_null('RealmOwner.name')
-            if $self->req('auth_realm')->is_general;
-    });
-    return;
-}
-
-sub _is_auth_relam_general_or_site_admin {
-    my($self) = @_;
-    return $_F->get_from_source($self)->auth_realm_is_site_admin($self->req)
-        || $self->req('auth_realm')->is_general;
-}
-
-sub _put_default_email_mode {
-    my($self) = @_;
-    my(@x) = $self->unsafe_get(qw(
-        email_mode
-        admin_only_forum_email
-        system_user_forum_email
-        public_forum_email
-    ));
-    return if @x;
-    $self->internal_put_field(
-        email_mode => $_FEM->DEFAULT);
-    return;
-}
-
-sub _put_email_mode {
-    my($self) = @_;
-    my($email, $admin, $system, $public) = $self->unsafe_get(qw(
-        email_mode
-        admin_only_forum_email
-        system_user_forum_email
-        public_forum_email
-    ));
-    $self->internal_put_field(
-        email_mode =>
-            $admin && !$system && !$public ? $_FEM->ADMIN_ONLY_FORUM_EMAIL
-            : !$admin && $system && !$public ? $_FEM->SYSTEM_USER_FORUM_EMAIL
-            : !$admin && !$system && $public ? $_FEM->PUBLIC_FORUM_EMAIL
-            : $_FEM->DEFAULT,
-    );
-    return;
+    return {@{$self->map_feature_type(sub {
+        my($field, $type) = @_;
+	return ($field => $type->get_default);
+    })}} if $self->req('auth_realm')->is_general
+        || $self->unsafe_get('force_default_values');
+    my($cats) = $_RR->list_enabled_categories;
+    return {@{$self->map_feature_type(sub {
+        my($field, $type) = @_;
+	return (
+	    $field,
+	    $type->can('from_realm_role_enabled_categories')
+		? $type->from_realm_role_enabled_categories($cats)
+		: $type->can('row_tag_get_value')
+		? $type->row_tag_get_value($self->req)
+		: grep($field eq $_, @$cats) ? 1 : 0,
+	);
+    })}};
 }
 
 1;
