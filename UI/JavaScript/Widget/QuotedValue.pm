2b99045fc8571900fba3d1931de16319e6b7f977
Rob Nagler <github@q33.us>
2009-12-28 19:26:44 +0000

    do the work in render

--- UI/JavaScript/Widget/QuotedValue.pm
+++ UI/JavaScript/Widget/QuotedValue.pm
@@ -2,20 +2,26 @@
 # $Id$
 package Bivio::UI::JavaScript::Widget::QuotedValue;
 use strict;
-use Bivio::Base 'Widget.Simple';
+use Bivio::Base 'UI.Widget';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub NEW_ARGS {
+    return [qw(value)];
+}
+
 sub initialize {
     my($self) = @_;
-    return if $self->unsafe_get('is_initialized');
-    $self->put(is_initialized => 1);
-    $self->put(value => [sub {
-        my(undef, $value) = @_;
- 	$value =~ s/"/\\"/g;
- 	return "\"$value\"";
-    }, $self->get('value')]);
+    $self->initialize_attr('value');
     return shift->SUPER::initialize(@_);
+}
+
+sub render {
+    my($self, $source, $buffer) = @_;
+    my($v) = $self->render_simple_attr(value => $source);
+    $v =~ s/"/\\"/g;
+    $$buffer .= qq{"$v"};
+    return;
 }
 
 1;
