eda09f25f56b55f52016e1d07a4bbe234e338c2e
Rob Nagler <github@q33.us>
2009-12-28 19:18:52 +0000

    *** empty log message ***

---
+++ Biz/Model/AdmRealmRoleList.pm
@@ -0,0 +1,26 @@
+# Copyright (c) 2009 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::AdmRealmRoleList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	can_iterate => 1,
+	primary_key => [[qw(RealmRole.realm_id RealmOwner.realm_id)]],
+	order_by => [qw(RealmRole.realm_id)],
+	other => [qw(
+	    RealmRole.role
+	    RealmRole.permission_set
+	    RealmOwner.realm_type
+	    RealmOwner.name
+	    RealmOwner.display_name
+	)],
+    });
+}
+
+1;
