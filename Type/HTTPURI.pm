4057fca2b8e9587ca0ab8224772c874803313d26
Rob Nagler <github@q33.us>
2010-01-11 00:42:57 +0000

    rmpod

--- Type/HTTPURI.pm
+++ Type/HTTPURI.pm
@@ -2,58 +2,18 @@
 # $Id$
 package Bivio::Type::HTTPURI;
 use strict;
-$Bivio::Type::HTTPURI::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::HTTPURI::VERSION;
-
-=head1 NAME
-
-Bivio::Type::HTTPURI - uniform resource locator
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::HTTPURI;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::String>
-
-=cut
-
-use Bivio::Type::String;
-@Bivio::Type::HTTPURI::ISA = ('Bivio::Type::String');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::HTTPURI>
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::Type::String';
 use URI ();
 
-#=VARIABLES
+# C<Bivio::Type::HTTPURI>
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value) : any
-
-Returns C<undef> if the line is empty.
-Leading and trailing blanks are trimmed.
-Length is checked.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub from_literal {
+    # (proto, string) : any
+    # Returns C<undef> if the line is empty.
+    # Leading and trailing blanks are trimmed.
+    # Length is checked.
     my($proto, $value) = @_;
     $value =~ s/^\s+|\s+$//g
 	if defined($value);
@@ -65,28 +25,10 @@ sub from_literal {
 	: (undef, Bivio::TypeError->HTTP_URI);
 }
 
-=for html <a name="get_width"></a>
-
-=head2 static get_width : int
-
-Returns 255.
-
-=cut
-
 sub get_width {
+    # : int
+    # Returns 255.
     return 255;
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
