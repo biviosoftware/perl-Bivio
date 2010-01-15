f794c0214627066d688e5e5573417b7e08bc6094
Rob Nagler <github@q33.us>
2010-01-15 06:25:42 +0000

    load the CalendarEvent from the request

--- Biz/Model/CalendarEventDeleteForm.pm
+++ Biz/Model/CalendarEventDeleteForm.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::CalendarEventDeleteForm;
 use strict;
@@ -9,6 +9,7 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub execute_ok {
     my($self) = @_;
     $self->req('Model.CalendarEvent')->cascade_delete;
+#TODO: Return to view which had the event
     return;
 }
 
@@ -17,6 +18,12 @@ sub internal_initialize {
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
     });
+}
+
+sub internal_pre_execute {
+    my($self) = @_;
+    $self->new_other('CalendarEvent')->load_this_from_request;
+    return shift->SUPER::internal_pre_execute(@_);
 }
 
 1;
