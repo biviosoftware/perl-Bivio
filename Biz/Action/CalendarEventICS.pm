be61eaff8e829360ff506aa4de1474f9000eacd4
Rob Nagler <github@q33.us>
2010-01-15 06:23:42 +0000

    simplify

--- Biz/Action/CalendarEventICS.pm
+++ Biz/Action/CalendarEventICS.pm
@@ -1,16 +1,19 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Action::CalendarEventICS;
 use strict;
-use base 'Bivio::Biz::Action';
+use Bivio::Base 'Biz.Action';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub execute {
     my($proto, $req) = @_;
-    $req->get('reply')->set_output_type('text/calendar')
-	->set_output($req->use('Model.CalendarEventDAVList')->new($req)
-	    ->vcalendar_list($req->req('Model.CalendarEventList')));
+    $req->get('reply')
+	->set_output_type('text/calendar')
+	->set_output(
+	    b_use('Model.CalendarEventDAVList')->new($req)
+	    ->vcalendar_list($req->get('Model.CalendarEventList')),
+	);
     return;
 }
 
