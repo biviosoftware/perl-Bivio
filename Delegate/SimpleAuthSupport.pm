b4112f8e3905e9686a2b2ed57e5834a789b27abd
Rob Nagler <github@q33.us>
2010-01-21 20:17:28 +0000

    cache permissions for realms and load permissions for entire realm at once

--- Delegate/SimpleAuthSupport.pm
+++ Delegate/SimpleAuthSupport.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2001-2008 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2001-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Delegate::SimpleAuthSupport;
 use strict;
@@ -6,26 +6,31 @@ use Bivio::Base 'Collection.Attributes';
 use Bivio::IO::Trace;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_RT) = b_use('Model.RowTag');
 my($_DEFAULT_PERMISSIONS) = {};
+my($_RT) = b_use('Model.RowTag');
+my($_RO) = b_use('Model.RealmOwner');
+my($_RR) = b_use('Model.RealmRole');
+my($_PS) = b_use('Auth.PermissionSet');
+my($_P) = b_use('Auth.Permission');
+my($_USER) = b_use('Auth.RealmType')->USER;
 
 sub get_auth_user {
     # (proto, Agent.Request) : Biz.Model
     # Expects I<Request.auth_user_id> to be set.  If it isn't set, returns
     # C<undef>.  LoginForm or some other cookie handler should set this.
     my(undef, $req) = @_;
-
     # This special field is set by one of the handlers (LoginForm).
     my($auth_user_id) = $req->unsafe_get('auth_user_id');
     _trace('auth_user_id=', $auth_user_id) if $_TRACE;
-    return undef unless $auth_user_id;
-
+    return
+	undef unless $auth_user_id;
     # Make sure user loads and has a valid password (could login)
-    my($auth_user) = Bivio::Biz::Model->new($req, 'RealmOwner');
+    my($auth_user) = $_RO->new($req);
     return $auth_user
-	    if $auth_user->unauth_load(realm_id => $auth_user_id,
-		    realm_type => Bivio::Auth::RealmType->USER)
-	    && $auth_user->has_valid_password;
+	if $auth_user->unauth_load({
+	    realm_id => $auth_user_id,
+	    realm_type => $_USER,
+	}) && $auth_user->has_valid_password;
     return undef;
 }
 
@@ -36,20 +41,18 @@ sub load_permissions {
     my($proto, $realm, $role, $req) = @_;
     my($owner) = $realm->unsafe_get('owner');
     if ($owner) {
-	my($rr) = Bivio::Biz::Model->new($req, 'RealmRole');
-	# Try to load for just this role explicitly and cache.
-	return $rr->get('permission_set')
-	    if _load_realm($proto, $rr, $realm->get('id'), $role);
+	my($map) = $req->get_if_exists_else_put(
+	    __PACKAGE__ . '.' . $realm->get('id'),
+	    sub {_map_permissions($_RR->new($req), $realm->get('id'))},
+	);
+	return $map->{$role}
+	    if defined($map->{$role});
     }
-
     my($rti) = $realm->get('type')->as_int;
     _load_default_permissions($rti, $req)
 	unless $_DEFAULT_PERMISSIONS->{$rti};
-
-    # Copy just this role's permission if there is an owner
     my($res) = $_DEFAULT_PERMISSIONS->{$rti}->{$role};
-    # Return the permission, but make sure it exists
-    Bivio::Die->die($realm, ': unable to load default permissions for ', $role)
+    b_die($realm, ': unable to load default permissions for ', $role)
 	unless defined($res);
     return $res;
 }
@@ -62,10 +65,11 @@ sub task_permission_ok {
     foreach my $op ('', qw(super_user substitute_user test)) {
 	if ($op) {
 	    my($method) = 'is_' . $op;
-	    next unless $req->$method();
-	    Bivio::Auth::PermissionSet->set(
+	    next
+		unless $req->$method();
+	    $_PS->set(
 		\$user,
-		Bivio::Auth::Permission->from_name($op . '_transient'));
+		$_P->from_name($op . '_transient'));
 	    _trace($op, ' user: ', $user) if $_TRACE;
 	}
 	# Does this role have all the required permission?
@@ -84,26 +88,26 @@ sub unsafe_get_user_pref {
 
 sub _load_default_permissions {
     my($rti, $req) = @_;
-    my($rr) = Bivio::Biz::Model->new($req, 'RealmRole');
+    my($rr) = $_RR->new($req);
 #TODO: Use RealmRole->get_permission_map
     $_DEFAULT_PERMISSIONS->{$rti} = {
 	%{$rr->EMPTY_PERMISSION_MAP},
-	@{$rr->map_iterate(
-	    sub {
-		my($it) = @_;
-		return ($it->get('role') => $it->get('permission_set'));
-	    },
-	    'unauth_iterate_start',
-	    'role',
-	    {realm_id => $rti},
-	)},
+	%{_map_permissions($rr, $rti)},
     };
     return;
 }
 
-sub _load_realm {
-    my($proto, $rr, $realm_id, $role) = @_;
-    return $rr->unauth_load(realm_id => $realm_id, role => $role);
+sub _map_permissions {
+    my($rr, $realm_id) = @_;
+    return {@{$rr->map_iterate(
+	sub {
+	    my($it) = @_;
+	    return ($it->get('role') => $it->get('permission_set'));
+	},
+	'unauth_iterate_start',
+	'role',
+	{realm_id => $realm_id},
+    )}};
 }
 
 1;
