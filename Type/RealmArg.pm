dde7eec77f5d213da5d7f24c3efa8130f4855af7
Rob Nagler <github@q33.us>
2010-01-22 22:14:58 +0000

    check email, too

--- Type/RealmArg.pm
+++ Type/RealmArg.pm
@@ -6,14 +6,23 @@ use Bivio::Base 'Type.String';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RN) = b_use('Type.RealmName');
+my($_E) = b_use('Type.Email');
 my($_PI) = b_use('Type.PrimaryId');
 my($_SYNTAX_ERROR) = b_use('Bivio.TypeError')->SYNTAX_ERROR;
+my($_NOT_FOUND) = b_use('Bivio.TypeError')->NOT_FOUND;
 
 sub from_literal {
     my($proto, $value) = @_;
     my($v, $e) = $_PI->from_literal($value);
     return $_PI->is_valid($v) ? ($v, undef) : (undef, $_SYNTAX_ERROR)
 	if defined($v);
+    ($v, $e) = $_E->from_literal($value);
+    if (defined($v) and my $req = b_use('Agent.Request')->get_current) {
+	my($email) = b_use('Model.Email')->new($req);
+	return $email->unauth_load({email => $v})
+	    ? ($email->get('realm_id'), undef)
+	    : (undef, $_NOT_FOUND);
+    }
     return $_RN->from_literal($value);
 }
 
