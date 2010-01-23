0ff3fa76cccf8850f66318b3f0ac3108efbce3cc
Rob Nagler <github@q33.us>
2010-01-23 23:52:17 +0000

    look up names in facade allow caller to set date

--- Biz/Model/MonthList.pm
+++ Biz/Model/MonthList.pm
@@ -1,37 +1,51 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::MonthList;
 use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_D) = __PACKAGE__->use('Type.Date');
+my($_D) = b_use('Type.Date');
+my($_T) = b_use('FacadeComponent.Text');
+
 
 sub internal_initialize {
     my($self) = @_;
     return {
 	version => 1,
 	$self->field_decl(
-	    other => [
+	    primary_key => [
                 [qw(date Date)],
-		[qw(month String)],
 	    ],
-	    undef, 'NONE',
+	    order_by => [
+		[qw(display_name String)],
+	    ],
 	),
+	other_query_keys => ['b_month'],
     };
 }
 
 sub internal_load_rows {
-    my($self) = @_;
-    my($year) = $_D->get_part($_D->now, 'year');
+    my($self, $query) = @_;
+    my($date) = $_D->now;
+    if (my $bm = $query->unsafe_get('b_month')) {
+	$bm = ($_D->from_literal($bm))[0];
+	$date = $bm
+	    if $bm;
+    }
+    my($year) = $_D->get_part($date, 'year');
+    my($t) = $_T->get_from_source($self->req);
     return [
         reverse(map(+{
             date => $_,
-            month => $_D->english_month3($_D->get_part($_, 'month'))
-                . ' ' . $_D->get_part($_, 'year'),
+            display_name => $t->get_value(
+		$self->simple_package_name,
+		$_D->english_month3($_D->get_part($_, 'month')),
+	    ) . ' '
+	    . $_D->get_part($_, 'year'),
         }, map({
             my($year) = $_;
-            map($_D->date_from_parts(1, $_, $year), (1 .. 12));
+            map($_D->date_from_parts(1, $_, $year), 1 .. 12);
         } ($year - 1 .. $year + 1)))),
     ];
 }
