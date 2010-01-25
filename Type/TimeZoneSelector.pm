4cc4e2e7d66a967639dd260aa773e2dd178c6451
Rob Nagler <github@q33.us>
2010-01-25 03:09:53 +0000

    *** empty log message ***

---
+++ Type/TimeZoneSelector.pm
@@ -0,0 +1,26 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Type::TimeZoneSelector;
+use strict;
+use Bivio::Base 'Type.DisplayName';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_TZ) = b_use('Type.TimeZone');
+
+sub row_tag_get {
+    my(undef, $req) = @_;
+#TODO: Only supports implicit realm_id format
+    return $req->get('Model.TimeZoneList')
+	->display_name_for_enum($_TZ->row_tag_get($req));
+}
+
+sub row_tag_replace {
+    my(undef, $value, $req) = @_;
+#TODO: Only supports implicit realm_id format
+    return $_TZ->row_tag_replace(
+	$req->get('Model.TimeZoneList')->enum_for_display_name($value),
+	$req,
+    );
+}
+
+1;
