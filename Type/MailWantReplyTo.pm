c3140438de7d697383e73f6011a20f2882518ae4
Rob Nagler <github@q33.us>
2010-01-25 03:14:45 +0000

    removed row_tag_get/replace_value.  Use Type->row_tag_*

--- Type/MailWantReplyTo.pm
+++ Type/MailWantReplyTo.pm
@@ -7,23 +7,16 @@ use Bivio::Base 'Type.Boolean';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RT) = b_use('Model.RowTag');
 
+sub ROW_TAG_KEY {
+    return 'MAIL_WANT_REPLY_TO';
+}
+
 sub get_default {
     return 1;
 }
 
 sub is_set_for_realm {
-    return shift->row_tag_get_value(@_);
-}
-
-sub row_tag_get_value {
-    my($self, $req) = @_;
-    return $_RT->new($req)->get_value('MAIL_WANT_REPLY_TO') ? 1 : 0;
-}
-
-sub row_tag_replace_value {
-    my($self, $value, $req) = @_;
-    return $_RT->new($req)->replace_value(
-	MAIL_WANT_REPLY_TO => $value ? 1 : undef);
+    return shift->row_tag_get(@_);
 }
 
 1;
