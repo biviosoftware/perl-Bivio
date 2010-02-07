77a2d93673f47d0b9ecca1703fbc18897784feda
Rob Nagler <github@q33.us>
2010-02-07 19:10:40 +0000

    cleaned up permissions some formatting

--- Biz/Model/RealmFileTreeList.pm
+++ Biz/Model/RealmFileTreeList.pm
@@ -1,15 +1,18 @@
-# Copyright (c) 2006-2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::RealmFileTreeList;
 use strict;
 use Bivio::Base 'Model.TreeList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_FP) = __PACKAGE__->use('Type.FilePath');
 my($_IDI) = __PACKAGE__->instance_data_index;
-my($_RF) = __PACKAGE__->use('Model.RealmFile');
-my($_RTK) = __PACKAGE__->use('Type.RowTagKey');
-my($_TLN) = __PACKAGE__->use('Type.TreeListNode');
+my($_FP) = b_use('Type.FilePath');
+my($_RF) = b_use('Model.RealmFile');
+my($_RTK) = b_use('Type.RowTagKey');
+my($_TLN) = b_use('Type.TreeListNode');
+my($_DEFAULT_LOCATION) = b_use('Model.Email')->DEFAULT_LOCATION;
+my($_MAIL_FOLDER) = lc($_FP->MAIL_FOLDER);
+my($_VERSIONS_FOLDER_RE) =  qr{^$_FP->VERSIONS_FOLDER(?:/:$)}ios;
 
 sub LOAD_ALL_SIZE {
     return 5000;
@@ -20,13 +23,11 @@ sub PARENT_NODE_ID_FIELD {
 }
 
 sub can_write {
-    my($self) = @_;
-    return $self->[$_IDI]->{can_write};
+    return shift->[$_IDI]->{can_write};
 }
 
 sub internal_default_expand {
-    my($self) = @_;
-    return [$self->new_other('RealmFile')->path_info_to_id('/')];
+    return [shift->new_other('RealmFile')->path_info_to_id('/')];
 }
 
 sub internal_initialize {
@@ -67,8 +68,7 @@ sub internal_initialize {
 		type => 'String',
 		constraint => 'NONE',
 	    },
-	    ['Email_2.location',
-		[$self->use('Model.Email')->DEFAULT_LOCATION]],
+	    ['Email_2.location', [$_DEFAULT_LOCATION]],
 	],
 	other_query_keys => [qw(path_info)],
     });
@@ -101,7 +101,8 @@ sub internal_leaf_node_uri {
 
 sub internal_parent_id {
     my($self, $id) = @_;
-    return $self->new_other('RealmFile')->load({realm_file_id => $id})
+    return $self->new_other('RealmFile')
+	->load({realm_file_id => $id})
 	->get('folder_id');
 }
 
@@ -132,8 +133,8 @@ sub internal_post_load_row {
 sub internal_prepare_statement {
     my($self, $stmt) = @_;
     $self->[$_IDI] = {
-	can_write => grep($_ eq $self->use('Auth.Role')->FILE_WRITER,
-	    @{$self->req->get_auth_roles}) ? 1 : 0,
+	can_write => $self->req->can_user_execute_task(
+	    $self->req('task')->get_attr_as_task('write_task')),
     };
     $stmt->from($stmt->LEFT_JOIN_ON(qw(RealmFile RealmFileLock), [
 	[qw(RealmFile.realm_file_id RealmFileLock.realm_file_id)],
@@ -144,14 +145,13 @@ sub internal_prepare_statement {
     ]));
     $stmt->from($stmt->LEFT_JOIN_ON(qw(RealmFileLock Email_3), [
 	[qw(RealmFileLock.user_id Email_3.realm_id)],
-	['Email_3.location', [$self->use('Model.Email')->DEFAULT_LOCATION]],
+	['Email_3.location', [$_DEFAULT_LOCATION]],
     ]));
     # /Mail is probably large so we'll ignore it
     # dot-files are uninteresting, so we'll ignore them.
     # All are available via DAV
-    my($mf) = lc($self->get_instance('RealmFile')->MAIL_FOLDER);
     $stmt->where(@{$stmt->map_invoke(
-	NOT_LIKE => ['/.%', '%/.%', $mf . '/%', $mf],
+	NOT_LIKE => ['/.%', '%/.%', $_MAIL_FOLDER . '/%', $_MAIL_FOLDER],
 	['RealmFile.path_lc'],
     )});
     return shift->SUPER::internal_prepare_statement(@_);
@@ -167,18 +167,15 @@ sub internal_root_parent_node_id {
 
 sub is_archive {
     my($self) = @_;
-    my($archive_path) = $_FP->VERSIONS_FOLDER;
-    return $self->get('RealmFile.path') =~ /^$archive_path/ ? 1 : 0;
+    return $self->get('RealmFile.path') =~ $_VERSIONS_FOLDER_RE ? 1 : 0;
 }
 
 sub is_file {
-    my($self) = @_;
-    return $self->get('RealmFile.is_folder') ? 0 : 1;
+    return shift->get('RealmFile.is_folder') ? 0 : 1;
 }
 
 sub is_locked {
-    my($self) = @_;
-    return $self->get('RealmFileLock.modified_date_time') ? 1 : 0;
+    return shift->get('RealmFileLock.modified_date_time') ? 1 : 0;
 }
 
 sub parse_query_from_request {
