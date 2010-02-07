75c9b595defb7a9301fa3078a15eab88330d7799
Rob Nagler <github@q33.us>
2010-02-07 23:55:11 +0000

    move all text except unapproved_applicant_mail to Facade

--- UI/View/UserAuth.pm
+++ UI/View/UserAuth.pm
@@ -20,7 +20,7 @@ sub general_contact_mail {
 	    vs_text('support_email'),
 	    vs_text_as_prose('support_name'),
 	),
-	subject => Join([vs_site_name(), ' Web Contact']),
+	subject => _prose('general_contact_mail.subject'),
 	body => ['Model.ContactForm', 'text'],
     );
 }
@@ -40,54 +40,7 @@ sub login {
 }
 
 sub missing_cookies {
-    return shift->internal_body(DIV_prose(Join([
-	P('It seems that your browser does not support cookies, or cookies have been disabled. Cookies are required for you to sign-in.'),
-	H3('Enabling Cookies in your Browser'),
-	P(q{The members area application requires the use of Cookies. By default, cookies are enabled in your browser. If you were directed to this page by our software, you or someone else has disable cookies in your browser. The following instructions are meant as a guide only. Please consult your browser's help system for a complete description. Scroll down this page until you find your browser. We apologize if your browser isn't in our list yet.}),
-	map((
-	    H4(shift(@$_)),
-	    OL(Join([map(LI($_), @$_)])),
-	), [
-	    'Internet Explorer 6.0',
-	    'Click on the Tools menu (at the very top of your window)',
-	    'Select Internet Options',
-	    'Switch to the Privacy tab',
-	    'Slide the vertical slider to Medium',
-	], [
-	    'AOL 6.0 and above',
-	    'Click on My AOL at the top of the AOL window',
-	    'Select Preferences from the menu',
-	    'Click on the WWW icon',
-	    'Switch to the Privacy tab',
-	    'Slide the vertical slider to Medium',
-	], [
-	    'Older Internet Explorer Versions',
-	    'Click on the Tools menu (at the very top of your window)',
-	    'Select Internet Options',
-	    'Switch to the Security tab',
-	    'Click on Internet in the Select a Web content zone',
-	    'Further down, press the Custom Level button',
-	    'Scroll down to the Cookies section in the Settings box',
-	    'Click on Enable for Allow cookies that are stored option',
-	    'Click on Enable for Allow per-session cookies option',
-	], [
-	    'Older AOL Versions',
-	    'Click on My AOL at the top of the AOL window',
-	    'Select Preferences from the menu',
-	    'Click on the WWW icon',
-	    'Click on Internet in the Select a Web content zone',
-	    'Further down, press the Custom Level button',
-	    'Scroll down to the Cookies section in the Settings box',
-	    'Click on Enable for Allow cookies that are stored option',
-	    'Click on Enable for Allow per-session cookies option',
-	], [
-	    'Netscape Communicator',
-	    'Click on the Edit menu (at the very top of your window)',
-	    'Select Preferences',
-	    'Click on Advanced in the Category box',
-	    'Click on Accept all cookies in the Cookies box',
-	]),
-    ])));
+    return shift->internal_body(DIV_prose(_prose('missing_cookies.body')));
 }
 
 sub password {
@@ -107,19 +60,7 @@ sub password_query_ack {
 }
 
 sub password_query_mail {
-    return _mail(shift(@_), 'UserPasswordQueryForm', <<'EOF');
-Please follow the link to reset your password:
-
-Join([['Model.UserPasswordQueryForm', 'uri']]);
-
-For your security, this link may be used one time only to set your
-password.
-
-You may contact customer support by replying to this message.
-
-Thank you,
-vs_site_name(); Support
-EOF
+    return _mail(shift(@_), 'UserPasswordQueryForm');
 }
 
 sub create {
@@ -129,30 +70,11 @@ sub create {
 }
 
 sub create_done {
-    return shift->internal_body(DIV_prose(Prose(<<'EOF')));
-We have sent a confirmation email to
-String(['Model.UserRegisterForm', 'Email.email']);.
-Please follow the instructions in this email message to complete
-your registration with vs_site_name();.
-EOF
+    return shift->internal_body(DIV_prose(_prose('create_done.body')));
 }
 
 sub create_mail {
-    return _mail(shift(@_), 'UserRegisterForm', <<'EOF');
-Thank you for registering with vs_site_name();.
-In order to complete your registration, please click on the
-following link:
-
-String(['Model.UserRegisterForm', 'uri']);
-
-For your security, this link may be used one time only to set your
-password.
-
-You may contact customer support by replying to this message.
-
-Thank you,
-vs_site_name(); Support
-EOF
+    return _mail(shift(@_), 'UserRegisterForm');
 }
 
 sub internal_settings_form_extra_fields {
@@ -194,6 +116,7 @@ sub settings_form {
 
 sub unapproved_applicant_mail {
     return shift->internal_put_base_attr(
+#TODO: use _prose() for this
 	from => Mailbox(
 	    ['Model.UserRegisterForm', 'Email.email'],
 	    ['Model.UserRegisterForm', 'RealmOwner.display_name'],
@@ -222,13 +145,13 @@ EOF
 }
 
 sub _mail {
-    my($self, $form, $body) = @_;
+    my($self, $form) = @_;
     my($n) = $self->my_caller;
     view_put(
 	mail_to => Mailbox(["Model.$form", 'Email.email']),
-	mail_subject => vs_text_as_prose($n . '_subject'),
+	mail_subject => _prose($n, 'subject'),
     );
-    return $self->internal_body_prose($body);
+    return $self->internal_body_prose(_prose($n, 'body'));
 }
 
 sub _password_fields {
@@ -240,6 +163,10 @@ sub _password_fields {
 	"$m.new_password",
 	"$m.confirm_new_password",
     );
+}
+
+sub _prose {
+    return vs_text_as_prose('UserAuth', @_);
 }
 
 1;
