0bb55d5ab501176fc39f99133e5f28c1b6548404
Eric Dobbs <dobbs@bivio.biz>
2010-02-09 00:19:09 +0000

    added RealmDropDownList

---
+++ Biz/Model/RealmDropDownList.pm
@@ -0,0 +1,49 @@
+# Copyright (c) 2010 bivio Software Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::RealmDropDownList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_RT) = b_use('Auth.RealmType');
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	can_iterate => 0,
+	$self->field_decl(
+	    primary_key => [
+		[qw(RealmOwner.name)],
+	    ],
+            other => [
+                [qw(link Text)],
+            ],
+	    undef, 'NOT_NULL',
+	),
+        other_query_keys => ['realm_types', 'task_id'],
+    });
+}
+
+sub internal_load_rows {
+    my($self, $query, $stmt, $where, $params, $sql_support) = @_;
+    my($req) = $self->req;
+    my($realm_types) = [map($_RT->from_any($_), @{$query->get('realm_types')})];
+    my($task_id) = $query->get_or_default(
+        task_id => $realm_types->[0]->get_name . '_HOME'
+    );
+    return [map(+{
+        'RealmOwner.name' => $_,
+        link => $req->format_stateless_uri({
+            realm => $_,
+            task_id => $task_id,
+        }),
+    }, @{$self->req->map_user_realms(
+	sub {shift->{'RealmOwner.name'}},
+        @$realm_types ? {
+            'RealmOwner.realm_type' => $realm_types
+        } : (),
+    )})];
+}
+
+1;
