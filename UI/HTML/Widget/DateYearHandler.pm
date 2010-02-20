81a58926f5b1cebd528b241e9af1a020356ac82b
Rob Nagler <github@q33.us>
2010-02-20 20:09:24 +0000

    rmpod

--- UI/HTML/Widget/DateYearHandler.pm
+++ UI/HTML/Widget/DateYearHandler.pm
@@ -2,73 +2,23 @@
 # $Id$
 package Bivio::UI::HTML::Widget::DateYearHandler;
 use strict;
-$Bivio::UI::HTML::Widget::DateYearHandler::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::DateYearHandler::VERSION;
-
-=head1 NAME
-
-Bivio::UI::HTML::Widget::DateYearHandler - txn to val date handler
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::DateYearHandler;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::HTML::Widget::DateYearHandler::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::DateYearHandler>
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item form_name : string (inherited)
-
-Used to access the form within JavaScript.
-
-=item target_field : string
-
-The date field to receive the new value. This value only get assigned
-if the source date is less than the current value.
-
-=back
-
-=cut
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="JAVASCRIPT_FUNCTION_NAME"></a>
-
-=head2 JAVASCRIPT_FUNCTION_NAME : string
-
-Return the tag used by this class to prefix javascript functions.
-
-=cut
-
-sub JAVASCRIPT_FUNCTION_NAME {
-    return 'dy';
-}
-
-#=IMPORTS
+use Bivio::Base 'Bivio::UI::Widget';
 use Bivio::UI::HTML::Widget::JavaScript;
 
-#=VARIABLES
+# C<Bivio::UI::HTML::Widget::DateYearHandler>
+#
+#
+#
+# form_name : string (inherited)
+#
+# Used to access the form within JavaScript.
+#
+# target_field : string
+#
+# The date field to receive the new value. This value only get assigned
+# if the source date is less than the current value.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_FUNCS) = Bivio::UI::HTML::Widget::JavaScript->strip(<<"EOF");
 
 // Adds the current four digit year to the source date if
@@ -121,61 +71,33 @@ function dy_complete_date(s) {
 }
 EOF
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_html_field_attributes"></a>
-
-=head2 get_html_field_attributes(string field_name, ref source) : string
-
-Returns the inlined source for this method.
-
-=cut
+sub JAVASCRIPT_FUNCTION_NAME {
+    # : string
+    # Return the tag used by this class to prefix javascript functions.
+    return 'dy';
+}
 
 sub get_html_field_attributes {
+    # (self, string, ref) : string
+    # Returns the inlined source for this method.
     my($self, $field_name, $source) = @_;
     # This has to be onBlur, because onChange doesn't work quite right in IE.
     return ' onblur="dy_complete_date(this)"';
 }
 
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-NOP
-
-=cut
-
 sub initialize {
+    # (self) : undef
+    # NOP
     return;
 }
 
-=for html <a name="render"></a>
-
-=head2 render(any source, string_ref buffer)
-
-Renders the javascript.
-
-=cut
-
 sub render {
+    # (self, any, string_ref) : undef
+    # Renders the javascript.
     my($self, $source, $buffer) = @_;
     Bivio::UI::HTML::Widget::JavaScript->render($source, $buffer,
 	    JAVASCRIPT_FUNCTION_NAME(), $_FUNCS);
     return;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
