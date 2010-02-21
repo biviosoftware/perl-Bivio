3041b50c2191b2fd07dea065c815d9725e07b98b
Rob Nagler <github@q33.us>
2010-02-21 16:45:49 +0000

    add is_today_*

--- Biz/Model/CalendarEventWeekList.pm
+++ Biz/Model/CalendarEventWeekList.pm
@@ -25,6 +25,7 @@ sub internal_initialize {
 		    ["create_date_$_", 'Date'],
 		    ["in_this_month_$_", 'Boolean'],
 		    ["day_of_month_$_", 'Integer'],
+		    ["is_today_$_", 'Boolean'],
 		    ["event_list_$_", 'Model.CalendarEventDayList'],
 		), $self->day_of_week_suffix_list),
 	    ],
@@ -39,6 +40,8 @@ sub internal_load_rows {
     my($this_month) = $month_list->this_month;
     my($months) = _months($month_list);
     my($rows) = [];
+    my($today) = $_D->from_datetime(
+	$month_list->auth_user_time_zone->date_time_to_utc($_DT->now));
     $_DT->do_iterate(
 	sub {
 	    my($dt) = @_;
@@ -46,11 +49,12 @@ sub internal_load_rows {
 	    push(@$rows, {})
 		if $dow eq 'sunday';
 	    my($month, $day) = $_DT->get_parts($dt, 'month', 'day');
+	    my($date) = $_D->from_datetime($dt);
 	    $rows->[$#$rows] = {
 		%{$rows->[$#$rows]},
-		"create_date_$dow" => $_D->from_datetime($dt),
+		"create_date_$dow" => $date,
 		"in_this_month_$dow" => $month eq $this_month ? 1 : 0,
-#TODO: 		"is_today_$dow" => $dt ? 1 : 0,
+		"is_today_$dow" => $_D->is_equal($date, $today),
 		"day_of_month_$dow" => $day,
 		"day_list_$dow" => $self->new_other('CalendarEventDayList')
 		    ->load_all({b_rows => [sort({
