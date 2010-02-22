1fb7fb82ca5d131cf44bf3ac9cdea3877c1343b5
Rob Nagler <github@q33.us>
2010-02-22 04:29:06 +0000

    b_use

--- Util/RealmMail.pm
+++ Util/RealmMail.pm
@@ -2,17 +2,17 @@
 # $Id$
 package Bivio::Util::RealmMail;
 use strict;
-use base 'Bivio::ShellUtil';
-use Bivio::Mail::Incoming;
+use Bivio::Base 'Bivio.ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_FCT) = __PACKAGE__->use('FacadeComponent.Text');
-my($_F) = __PACKAGE__->use('FacadeComponent.Facade');
-my($_O) = __PACKAGE__->use('Mail.Outgoing');
-my($_IOF) = __PACKAGE__->use('IO.File');
-my($_FP) = __PACKAGE__->use('Type.FilePath');
-my($_DT) = __PACKAGE__->use('Type.DateTime');
-my($_E) = __PACKAGE__->use('Type.Email');
+my($_FCT) = b_use('FacadeComponent.Text');
+my($_F) = b_use('FacadeComponent.Facade');
+my($_O) = b_use('Mail.Outgoing');
+my($_IOF) = b_use('IO.File');
+my($_FP) = b_use('Type.FilePath');
+my($_DT) = b_use('Type.DateTime');
+my($_E) = b_use('Type.Email');
+my($_I) = b_use('Mail.Incoming');
 
 sub USAGE {
     return <<'EOF';
@@ -29,7 +29,7 @@ sub delete_message_id {
     my($self, @message_id) = @_;
     my($req) = $self->get_request;
     foreach my $id (@message_id) {
-	Bivio::Biz::Model->new($req, 'RealmMail')->cascade_delete({
+	$self->model('RealmMail')->cascade_delete({
 	    message_id => $id,
 	});
     }
@@ -78,7 +78,7 @@ sub import_bulletins {
 
 sub import_mbox {
     my($self) = @_;
-    my($rm) = Bivio::Biz::Model->new($self->get_request, 'RealmMail');
+    my($rm) = $self->model('RealmMail');
     my($t) = 0;
     my($n) = 0;
     foreach my $m (split(/(?<=\n)From [^\n]+\n/, ${$self->read_input})) {
@@ -97,7 +97,7 @@ sub import_rfc822 {
 	    sort {$_DT->compare($a->[1], $b->[1])}
 	        map(_import_rfc822_validate($self, $_), glob("$dir/*")))
     ) {
-	Bivio::Biz::Model->new($req, 'RealmMail')->create_from_rfc822($f);
+	$self->model('RealmMail')->create_from_rfc822($f);
 	$self->commit_or_rollback
 	    if ++$i % 100 == 0;
     }
@@ -110,10 +110,10 @@ sub _create_from_rfc822 {
         sub {
             $$t++;
             $rm->create_from_rfc822(\$m);
-            Bivio::IO::Alert->info("imported $$t");
+            b_info("imported $$t");
             $$n++;
         });
-    Bivio::IO::Alert->info("skipped $$t\n", $die, \$m)
+    b_info("skipped $$t\n", $die, \$m)
             if $die;
     $self->commit_or_rollback;
     return;
@@ -123,9 +123,9 @@ sub _import_rfc822_validate {
     my($self, $name) = @_;
     return
 	unless -f $name;
-    my($in) = Bivio::Mail::Incoming->new(my $d = Bivio::IO::File->read($name));
+    my($in) = $_I->new(my $d = $_IOF->read($name));
     return
-	if Bivio::Biz::Model->new($self->get_request, 'RealmMail')
+	if $self->model('RealmMail')
 	    ->unsafe_load({message_id => my $id = $in->get_message_id});
     return [$d, $in->get_date_time];
 }
