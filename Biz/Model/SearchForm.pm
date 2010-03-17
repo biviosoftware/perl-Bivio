5a2436bf8ce475abc0ea2db11c307be5dd2fda64
Rob Nagler <github@q33.us>
2010-03-17 22:30:48 +0000

    CLEAR_ON_FOCUS_HINT management is handled by ClearOnFocus widget in JavaScript

--- Biz/Model/SearchForm.pm
+++ Biz/Model/SearchForm.pm
@@ -2,7 +2,7 @@
 # $Id$
 package Bivio::Biz::Model::SearchForm;
 use strict;
-use Bivio::Base 'Biz.FormModel';
+use Bivio::Base 'Model.ListQueryForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
@@ -10,43 +10,14 @@ sub CLEAR_ON_FOCUS_HINT {
     return 'Search';
 }
 
-sub execute_empty {
+sub internal_query_fields {
     my($self) = @_;
-    $self->internal_put_field(search => $self->CLEAR_ON_FOCUS_HINT)
-	unless defined($self->unsafe_get('search'));
-    return;
-}
-
-sub execute_ok {
-    shift->internal_stay_on_page;
-    return;
-}
-
-sub get_search_value {
-    my($self) = @_;
-    my($s) = $self->unsafe_get('search');
-    return ($s || '') eq $self->CLEAR_ON_FOCUS_HINT ? undef : $s;
-}
-
-sub internal_initialize {
-    my($self) = @_;
-    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
-        version => 1,
-        visible => [
-	    {
-		name => 'search',
-		type => 'Line',
-		constraint => 'NONE',
-		form_name => b_use('SQL.ListQuery')->to_char('search'),
-	    },
-	],
-    });
-}
-
-sub put_search_value {
-    my($self, $value) = @_;
-    $self->internal_put_field(search => $value);
-    return;
+    return [
+	[qw(search Line), {
+	    form_name => b_use('SQL.ListQuery')->to_char('search'),
+	}],
+	[qw(b_realm_only NullBoolean)],
+    ];
 }
 
 1;
