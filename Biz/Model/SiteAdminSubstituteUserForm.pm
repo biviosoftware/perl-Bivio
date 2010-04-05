8263bd5e478db6603a1a1b07522a4ba040d564c3
Rob Nagler <github@q33.us>
2010-04-05 23:40:46 +0000

    can_substitute_user now effective

--- Biz/Model/SiteAdminSubstituteUserForm.pm
+++ Biz/Model/SiteAdminSubstituteUserForm.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::SiteAdminSubstituteUserForm;
 use strict;
@@ -6,8 +6,29 @@ use Bivio::Base 'Model.AdmSubstituteUserForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-sub assert_can_substitute_user {
-    return;
+sub can_substitute_user {
+    my($self, $user_id) = @_;
+    my($req) = $self->req;
+    return 0
+	unless my $super_users = $req->cache_for_auth_user(
+	    $req->get('auth_id'),
+	    sub {
+		return undef
+		    if $req->is_substitute_user
+		    || !b_use('UI.Facade')
+		    ->get_from_source($req)
+		    ->auth_realm_is_site_admin($req);
+		return {@{$self
+		    ->new_other('SiteAdminSuperUserList')
+		    ->map_iterate(
+			sub {shift->get('RealmUser.user_id') => 1},
+		    )
+		}};
+	    },
+	);
+    return 0
+	unless $super_users->{$req->get('auth_user_id')};
+    return $super_users->{$user_id} ? 0 : 1;
 }
 
 1;
