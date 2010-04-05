423c06b5abf88a409ee4b6fec787a660bfb8b1be
Rob Nagler <github@q33.us>
2010-04-05 23:40:55 +0000

    added SUBSTITUTE_USER_FORM

--- Biz/Model/SiteAdminUserList.pm
+++ Biz/Model/SiteAdminUserList.pm
@@ -1,9 +1,13 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::SiteAdminUserList;
 use strict;
 use Bivio::Base 'Model.AdmUserList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub SUBSTITUTE_USER_FORM {
+    return 'SiteAdminSubstituteUserForm';
+}
 
 1;
