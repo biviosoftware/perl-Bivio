47ed423d393aa865c35290fb31cb39cf6b5d144d
Paul Moeller <moeller@bivio.biz>
2010-04-06 21:40:02 +0000

    rm pod

--- Biz/Model/Phone.pm
+++ Biz/Model/Phone.pm
@@ -2,55 +2,9 @@
 # $Id$
 package Bivio::Biz::Model::Phone;
 use strict;
-$Bivio::Biz::Model::Phone::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Biz::Model::Phone::VERSION;
+use Bivio::Base 'Model.LocationBase';
 
-=head1 NAME
-
-Bivio::Biz::Model::Phone - interface to phone_t SQL table
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Biz::Model::Phone;
-    Bivio::Biz::Model::Phone->new($req);
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Biz::Model::LocationBase>
-
-=cut
-
-use Bivio::Biz::Model::LocationBase;
-@Bivio::Biz::Model::Phone::ISA = qw(Bivio::Biz::Model::LocationBase);
-
-=head1 DESCRIPTION
-
-C<Bivio::Biz::Model::Phone> is the create, read, update,
-and delete interface to the C<phone_t> table.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="internal_initialize"></a>
-
-=head2 internal_initialize() : hash_ref
-
-B<FOR INTERNAL USE ONLY>
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub internal_initialize {
     return {
@@ -64,17 +18,5 @@ sub internal_initialize {
 	auth_id => 'realm_id',
     };
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
