52698bafc93e668e00cd5f59444473b06e0b8bf6
Rob Nagler <github@q33.us>
2010-04-07 21:24:02 +0000

    can su from any realm, just make sure user is an admin of that realm

--- Biz/Model/SiteAdminSubstituteUserForm.pm
+++ Biz/Model/SiteAdminSubstituteUserForm.pm
@@ -5,23 +5,36 @@ use strict;
 use Bivio::Base 'Model.AdmSubstituteUserForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_C) = b_use('FacadeComponent.Constant');
 
 sub can_substitute_user {
     my($self, $user_id) = @_;
     my($req) = $self->req;
     return 0
 	unless my $users = $req->cache_for_auth_user(
-	    $req->get('auth_id'),
+	    [],
 	    sub {
 		return undef
-		    if $req->is_substitute_user
-		    || !b_use('UI.Facade')
-		    ->get_from_source($req)
-		    ->auth_realm_is_site_admin($req);
+		    if $req->is_substitute_user;
+		my($auid) = $req->get('auth_user_id');
+		my($found);
 		my($res) = {@{$self->new_other('GroupUserList')
 		    ->map_iterate(
-		        sub {shift->get('RealmUser.user_id') => 1},
-		)}};
+		        sub {
+			    my($uid, $role) = shift->get(qw(RealmUser.user_id RealmUser.role));
+			    $found++
+				if $uid eq $auid && $role->eq_administrator;
+			    return ($uid => 1);
+			},
+			'unauth_iterate_start',
+			{
+			    auth_id =>
+			    $_C->get_value('site_admin_realm_id', $req),
+			},
+		       ),
+		}};
+		return undef
+		    unless $found || $req->is_super_user;
 		$self->new_other('AdmSuperUserList')
 		    ->do_iterate(sub {
 		        delete($res->{shift->get('RealmUser.user_id')});
