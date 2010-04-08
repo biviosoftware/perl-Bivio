1807a1f7c56fe2663d32a299c3ec95ade6e39861
Rob Nagler <github@q33.us>
2010-04-08 15:58:23 +0000

    oc

--- Biz/Action/UserLogout.pm
+++ Biz/Action/UserLogout.pm
@@ -1,17 +1,12 @@
-# Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Biz::Action::UserLogout;
 use strict;
-use Bivio::Base 'Bivio::Biz::Action';
-
-# C<Bivio::Biz::Action::UserLogout> clears the user on the request
-# and in the cookie.
+use Bivio::Base 'Biz.Action';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub execute {
-    # (proto, Agent.Request) : Agent.TaskId
-    # Calls the I<Model.LoginForm> to clear the user.
     my(undef, $req) = @_;
     return Bivio::Biz::Model->get_instance('UserLoginForm')->execute(
 	$req, {realm_owner => undef});
