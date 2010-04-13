392181cc334504954b0d3515a41ee6a88eb4822d
Rob Nagler <github@q33.us>
2010-04-13 00:22:58 +0000

    * Bivio::PetShop::Util::SQL   create ROOT user   always init root * Bivio::Util::SiteForum   moved site_admin_forum_users2 to SiteForum   Call at initialization * Bivio::Util::SQL   moved site_admin_forum_users2 to SiteForum   Call at initialization   don't init site_admin_forum_users2 if not v10

--- bOP.pm
+++ bOP.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2001-2010 bivio Software, Inc.  All Rights reserved.
+# Copyright (c) 2001-2010 bivio Software, Inc.  All Rights reserved
 # $Id$
 package Bivio::bOP;
 use strict;
@@ -31,6 +31,12 @@ http://www.bivio.biz for more info.
 =head1 CHANGES
 
   $Log$
+  Revision 9.17  2010/04/12 21:28:49  nagler
+  * Bivio::Biz::Model::RealmUserAddForm
+    don't delete userse before adding
+  * Bivio::Type::ArrayBase
+    exclude algorithm would explode memory
+
   Revision 9.16  2010/04/12 02:46:06  nagler
   * Bivio::Biz::Model::RealmUserAddForm
     normalize some code
