f70947273a67034df29198fe4779caf6871f5682
Rob Nagler <github@q33.us>
2010-04-18 19:58:45 +0000

    *** empty log message ***

---
+++ Biz/Action/DevRestart.pm
@@ -0,0 +1,30 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::DevRestart;
+use strict;
+use Bivio::Base 'Action.JobBase';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_F) = b_use('IO.File');
+my($_SENTINEL) = 'restart';
+
+sub internal_execute {
+    my($proto, $req) = @_;
+    $req->assert_test;
+    b_use('ShellUtil.HTTPD')->assert_in_exec_dir;
+    $_F->write($_SENTINEL, time);
+    CORE::exit(0);
+    # DOES NOT RETURN
+}
+
+sub restart_requested {
+    return 0
+	unless -r $_SENTINEL;
+    my($rs) = ${$_F->read($_SENTINEL)};
+    unlink($_SENTINEL);
+    b_die('not restarting, sentinel too old')
+	if $rs + 20 < time;
+    return 1;
+}
+
+1;
