75ad5c15a11936c913d8f656c858e9d403f56b91
Rob Nagler <github@q33.us>
2010-04-22 20:05:40 +0000

    factor out enqueue_task

--- Biz/Action/JobBase.pm
+++ Biz/Action/JobBase.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2002-2009 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Action::JobBase;
 use strict;
@@ -7,14 +7,26 @@ use Bivio::Base 'Biz.Action';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_SENTINEL) = __PACKAGE__ . '.internal_execute';
 
+sub enqueue_task {
+    my($proto, $task_id, $attrs, $req) = @_;
+    b_use('AgentJob.Dispatcher')->enqueue(
+	$req,
+	$task_id,
+	{
+	    %{$attrs || {}},
+	    $_SENTINEL => 1,
+	},
+    );
+    return;
+}
+
 sub execute {
     my($proto, $req) = @_;
     die($proto, ': does not implement internal_execute')
 	unless $proto->can('internal_execute');
     return $proto->internal_execute($req)
 	if $req->unsafe_get($_SENTINEL);
-    b_use('AgentJob.Dispatcher')->enqueue(
-	$req, $req->get('task_id'), {$_SENTINEL => 1});
+    $proto->enqueue_task($req->get('task_id'), undef, $req);
     my($buffer) = '';
     $req->get('reply')->set_output(\$buffer);
     return 0;
