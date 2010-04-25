c8f03e00d27f69817a7a761d3a9c6693e7f7a82a
Rob Nagler <github@q33.us>
2010-04-25 17:46:06 +0000

    pushed up code into Cache

--- Cache/RealmRole.pm
+++ Cache/RealmRole.pm
@@ -3,33 +3,17 @@
 package Bivio::Cache::RealmRole;
 use strict;
 use Bivio::Base 'Bivio.Cache';
-use Storable ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RR) = b_use('Model.RealmRole');
-my($_FILE);
-my($_F) = b_use('IO.File');
 b_use('Biz.PropertyModel')->register_handler(__PACKAGE__);
 
-sub handle_commit {
-    unlink(_file());
-    return;
-}
-
-sub permission_set_for_realm_role {
-    my($self, $realm_id, $role, $req) = @_;
-    return (
-	_map($req)->{$realm_id}
-	|| return undef
-     )->{$role->as_int};
-}
-
 sub handle_property_model_modification {
-    my($self, $model, $op, $query) = @_;
+    my($proto, $model, $op, $query) = @_;
     return
 	unless $model->simple_package_name eq 'RealmRole';
     my($req) = $model->req;
-    my($map) = _map($model->req);
+    my($map) = $proto->internal_retrieve($model->req);
     if ($op eq 'delete') {
 	b_die($query, 'must supply a realm_id to delete')
 	    unless $query->{realm_id};
@@ -47,14 +31,8 @@ sub handle_property_model_modification {
     return;
 }
 
-sub handle_rollback {
-    my(undef, $req) = @_;
-    $req->delete(__PACKAGE__);
-    return;
-}
-
-sub _compute {
-    my($req) = @_;
+sub internal_compute {
+    my($self, $req) = @_;
     my($map) = {};
     $_RR->new($req)->set_ephemeral->do_iterate(
 	sub {
@@ -63,25 +41,15 @@ sub _compute {
 	},
 	'unauth_iterate_start',
     );
-    $_F->mkdir_parent_only(_file(), 0750);
-    Storable::lock_store($map, $_FILE);
     return $map;
 }
 
-sub _file {
-    return $_FILE ||= b_use('Biz.File')->absolute_path('Cache/RealmRole.storable');
-}
-
-sub _map {
-    my($req) = @_;
-    return $req->get_if_exists_else_put(
-	__PACKAGE__,
-	sub {_read($req) || _compute($req)},
-    );
-}
-
-sub _read {
-    return -r _file() && Storable::lock_retrieve($_FILE);
+sub permission_set_for_realm_role {
+    my($proto, $realm_id, $role, $req) = @_;
+    return (
+	$proto->internal_retrieve($req)->{$realm_id}
+	|| return undef
+     )->{$role->as_int};
 }
 
 sub _update {
