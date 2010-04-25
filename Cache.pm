8c904f785c9c5a76c00adf5f97dbe2295472713b
Rob Nagler <github@q33.us>
2010-04-25 17:45:55 +0000

    support subclasses better

--- Cache.pm
+++ Cache.pm
@@ -2,13 +2,63 @@
 # $Id$
 package Bivio::Cache;
 use strict;
-use Bivio::Base 'Bivio.UNIVERSAL';
+use Bivio::Base 'Collection.Attributes';
+use Storable ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_IOF) = b_use('IO.File');
+my($_FP) = b_use('Type.FilePath');
+my($_BF) = b_use('Biz.File');
+my($_REALM_ID) = b_use('Auth.Realm')->get_general->get_default_id;
 
 sub init {
     b_use('IO.ClassLoader')->map_require_all('Cache');
     return;
+}
+
+sub handle_commit {
+    my($proto, $req) = @_;
+    unlink(_file($proto, $req));
+    shift->handle_rollback(@_);
+    return;
+}
+
+sub handle_rollback {
+    my($proto, $req) = @_;
+    $req->delete(_file($proto, $req));
+    return;
+}
+
+sub internal_realm_id {
+    return $_REALM_ID;
+}
+
+sub internal_retrieve {
+    my($proto, $req) = @_;
+    my($fp) = _file($proto, $req);
+    return $req->get_if_exists_else_put(
+	$fp,
+	sub {
+	    my($res);
+	    return $res
+		if $res = -r $fp && Storable::lock_retrieve($fp);
+	    $res = $proto->internal_compute($req);
+	    $_IOF->mkdir_parent_only($fp, 0750);
+	    Storable::lock_store($res, $fp);
+	    return $res;
+	},
+    );
+}
+
+sub _file {
+    my($proto, $req) = @_;
+    return $_BF->absolute_path(
+	$_FP->join(
+	    'Cache',
+	    $proto->simple_package_name,
+	    $proto->internal_realm_id($req),
+	),
+    );
 }
 
 1;
