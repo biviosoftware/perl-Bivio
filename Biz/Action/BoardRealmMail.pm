a605b7c58ada81ccfc9c0d8ce467ba3985517e35
Rob Nagler <github@q33.us>
2010-04-28 01:41:43 +0000

    *** empty log message ***

---
+++ Biz/Action/BoardRealmMail.pm
@@ -0,0 +1,24 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::BoardRealmMail;
+use strict;
+use Bivio::Base 'Biz.Action';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_RM) = b_use('Model.RealmMail');
+
+sub TASK_URI {
+    return 'board';
+}
+
+sub execute_receive {
+    my($self, $req, $rfc822) = @_;
+    $_RM->new($req)->create_from_rfc822(
+	$rfc822
+	    || $req->get('Model.MailReceiveDispatchForm')
+	    ->get('message')->{content},
+    );
+    return;
+}
+
+1;
