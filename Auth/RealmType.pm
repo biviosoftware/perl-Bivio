b88bfa59acad20c4eb41d37b66f500e1fa294af0
Rob Nagler <github@q33.us>
2010-04-28 22:46:14 +0000

    is_default_id should just use unsafe_from_int, because doesn't work with large realm_ids to use Type.Integer

--- Auth/RealmType.pm
+++ Auth/RealmType.pm
@@ -7,7 +7,6 @@ use Bivio::Base 'Type.EnumDelegator';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile;
 my($_S) = b_use('Type.String');
-my($_I) = b_use('Type.Integer');
 
 sub as_default_owner_id {
     return shift->as_int;
@@ -39,9 +38,7 @@ sub get_any_owner_list {
 
 sub is_default_id {
     my($proto, $id) = @_;
-    $id = $_I->from_literal_or_die($id);
-    return grep($id == $_->as_default_owner_id, $proto->get_non_zero_list)
-	? 1 : 0;
+    return $id && ($proto->unsafe_from_int($id) || return 0)->as_int ? 1 : 0;
 }
 
 sub is_group {
