13c7e0d526b4e77ee2d19c1a3e2affb333278763
Rob Nagler <github@q33.us>
2010-04-28 02:50:51 +0000

    Added 'enable' configuration to allow apps to turn off caching. Important if RealmRole table is large

--- Cache/RealmRole.pm
+++ Cache/RealmRole.pm
@@ -6,7 +6,19 @@ use Bivio::Base 'Bivio.Cache';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RR) = b_use('Model.RealmRole');
-b_use('Biz.PropertyModel')->register_handler(__PACKAGE__);
+my($_RT) = b_use('Auth.RealmType');
+my($_DEFAULT_PERMISSIONS) = {};
+b_use('IO.Config')->register(my $_CFG = {
+    enable => 1,
+});
+b_use('Biz.PropertyModel')->register_handler(__PACKAGE__)
+    if $_CFG->{enable};
+
+sub handle_config {
+    my(undef, $cfg) = @_;
+    $_CFG = $cfg;
+    return;
+}
 
 sub handle_property_model_modification {
     my($proto, $model, $op, $query) = @_;
@@ -45,11 +57,35 @@ sub internal_compute {
 }
 
 sub permission_set_for_realm_role {
+    return _not_enabled(@_)
+	unless $_CFG->{enable};
     my($proto, $realm_id, $role, $req) = @_;
     return (
 	$proto->internal_retrieve($req)->{$realm_id}
 	|| return undef
      )->{$role->as_int};
+}
+
+sub _not_enabled {
+    my($proto, $realm_id, $role, $req) = @_;
+    my($rr) = $_RR->new($req);
+    unless ($_RT->is_default_id($realm_id)) {
+	return	$rr->get('permission_set')
+	    if $rr->unauth_load({realm_id => $realm_id, role => $role});
+	return undef;
+    }
+    return ($_DEFAULT_PERMISSIONS->{$realm_id} ||= {
+	%{$rr->EMPTY_PERMISSION_MAP},
+	@{$rr->map_iterate(
+	    sub {
+		my($it) = @_;
+		return ($it->get('role') => $it->get('permission_set'));
+	    },
+	    'unauth_iterate_start',
+	    'role',
+	    {realm_id => $realm_id},
+	)},
+    })->{$role};
 }
 
 sub _update {
