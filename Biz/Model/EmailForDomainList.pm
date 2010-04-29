8df22460fe69030b9cd1459d7d35355c365666cd
Rob Nagler <github@q33.us>
2010-04-29 18:41:38 +0000

    *** empty log message ***

---
+++ Biz/Model/EmailForDomainList.pm
@@ -0,0 +1,27 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::EmailForDomainList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+        version => 1,
+	can_iterate => 1,
+	primary_key => [qw(Email.realm_id Email.location)],
+        order_by => ['Email.email'],
+	other_query_keys => ['b_domain_name'],
+    });
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt, $query) = @_;
+    $stmt->where(
+	$stmt->LIKE('Email.email', '%@' . $query->get('b_domain_name')));
+    return shift->SUPER::internal_prepare_statement(@_);
+}
+
+1;
---
+++ Util/Email.pm
@@ -0,0 +1,35 @@
+# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Util::Email;
+use strict;
+use Bivio::Base 'Bivio.ShellUtil';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_E) = b_use('Type.Email');
+
+sub USAGE {
+    return <<'EOF';
+usage: bivio Email [options] command [args..]
+commands
+  replace_email_domain from_domain to_domain
+EOF
+}
+
+sub replace_email_domain {
+    sub REPLACE_EMAIL_DOMAIN {[[qw(from_domain DomainName)], [qw(to_domain DomainName)]]}
+    my($self, $bp) = shift->parameters(\@_);
+    $self->are_you_sure("Update all emails ending in $bp->{from_domain} to $bp->{to_domain}?");
+    $self->model('EmailForDomainList')->do_iterate(
+	sub {
+	    my($e) = shift->get_model('Email');
+	    $e->update({
+		email => $_E->replace_domain($e->get('email'), $bp->{to_domain}),
+	    });
+	    return 1;
+	},
+	{b_domain_name => $bp->{from_domain}},
+    );
+    return;
+}
+
+1;
