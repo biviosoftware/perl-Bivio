1de90c358386fc919afd8c516bf6473049ed00d4
Paul Moeller <moeller@bivio.biz>
2010-05-10 14:56:22 +0000

    fixed unauth_iterate_start removed call to check_transaction_batch() - not implemented

--- Biz/Action/ECPaymentProcessAll.pm
+++ Biz/Action/ECPaymentProcessAll.pm
@@ -9,7 +9,6 @@ use Bivio::Base 'Action.JobBase';
 # one payment at a time.
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-our($_TRACE);
 my($_PROCESSOR);
 Bivio::IO::Config->register({
     processor => 'Bivio::Biz::Action::ECCreditCardProcessor',
@@ -17,8 +16,7 @@ Bivio::IO::Config->register({
 
 sub handle_config {
     my(undef, $cfg) = @_;
-    $_PROCESSOR = $cfg->{processor};
-    b_use($_PROCESSOR);
+    $_PROCESSOR = b_use($cfg->{processor});
     return;
 }
 
@@ -27,9 +25,6 @@ sub internal_execute {
     # For each payment, setup user and realm, then call ECCreditCardProcessor
     # to handle it.
     my($self, $req) = @_;
-    # check batch before.  Sometimes there is an error downloading
-    # the status, and we have accidentally resubmitted a payments.
-    $_PROCESSOR->check_transaction_batch($req);
     b_use('Model.ECPayment')->new($req)->do_iterate(sub {
         my($ecp) = @_;
 	$ecp->put_on_request;
@@ -37,7 +32,7 @@ sub internal_execute {
         $req->set_realm($ecp->get('realm_id'));
         $_PROCESSOR->execute_process($req);
 	return 1;
-    }, 'creation_date_time asc', {
+    }, 'unauth_iterate_start', 'creation_date_time asc', {
 	status => b_use('Type.ECPaymentStatus')->needs_processing_list,
     });
     return 0;
