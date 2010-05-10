09c70ad5f793183ce77f095b388ac55dd3b1742d
Paul Moeller <moeller@bivio.biz>
2010-05-10 15:25:42 +0000

    refactored, removed unimplemented check_transaction_batch()

--- Biz/Action/ECCreditCardProcessor.pm
+++ Biz/Action/ECCreditCardProcessor.pm
@@ -2,20 +2,8 @@
 # $Id$
 package Bivio::Biz::Action::ECCreditCardProcessor;
 use strict;
-use Bivio::Base 'Bivio::Biz::Action';
-use Bivio::Die;
-use Bivio::Ext::LWPUserAgent;
-use Bivio::HTML;
-use Bivio::IO::Alert;
-use Bivio::IO::Config;
+use Bivio::Base 'Biz.Action';
 use Bivio::IO::Trace;
-use Bivio::Type::Amount;
-use Bivio::Type::Date;
-use Bivio::Type::DateTime;
-use Bivio::Type::ECPaymentMethod;
-use Bivio::Type::ECPaymentStatus;
-use Bivio::Type::PrimaryId;
-use Bivio::UI::Text;
 use HTTP::Request ();
 
 # C<Bivio::Biz::Action::ECCreditCardProcessor> manages e-commerce payments.
@@ -26,74 +14,16 @@ use HTTP::Request ();
 #   http://www.authorize.net/support/AIM_guide.pdf
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-our($_TRACE);
+my($_ECPS) = b_use('Type.ECPaymentStatus');
 my($_GW_LOGIN);
 my($_GW_PASSWORD);
 my($_GW_TEST_MODE);
+our($_TRACE);
 Bivio::IO::Config->register({
     login => undef,
     password => undef,
     test_mode => 1,
 });
-
-sub check_transaction_batch {
-    # (self, Agent.Request) : undef
-    # Download current batch from Authorize.Net and double-check
-    # settled transactions. Ignores errors as those may be retried.
-    # Format is one transaction per line (CR-LF), fields are TAB-separated.
-    my($proto, $req) = @_;
-#TODO: Recode this to new interface
-    return;
-    my($hreq) = HTTP::Request->new(
-	POST => 'https://secure.authorize.net/Interface/minterface.dll?batchreport');
-    $hreq->content_type('application/x-www-form-urlencoded');
-    $hreq->content('x_Login='.$_GW_LOGIN.'&x_Password='.$_GW_PASSWORD.
-            '&Action=DOWNLOAD&BATCHID=NULL');
-    $hreq->referer(
-	'https://' . Bivio::UI::Facade->get_value('http_host', $req));
-    my($response) = Bivio::Ext::LWPUserAgent->new->request($hreq);
-    my($payment) = Bivio::Biz::Model->new($req, 'ECPayment');
-    foreach my $transaction (split(/\n/, $response->content)) {
-        my(@fields) = split(/\t/, $transaction);
-        my($status, $payment_id, $realm_id) = (@fields)[0,7,12];
-#TODO: RJN: Shouldn't realm_id be required?
-	unless (defined($status) && defined($payment_id)) {
-	    Bivio::IO::Alert->warn('missing field values in: ',
-		    \@fields);
-	    next;
-	}
-	($payment_id) = Bivio::Type::PrimaryId->from_literal($payment_id);
-	unless ($payment_id) {
-	    Bivio::IO::Alert->warn('ignoring bad payment_id: ', \@fields);
-	    next;
-	}
-        _trace('transaction data: ', \@fields) if $_TRACE;
-
-        # Ignore payments we don't know about
-        next unless $payment->unauth_load(ec_payment_id => $payment_id);
-
-#TODO: Should the payment gateway status codes (1,2,3) be encapsulated?
-        if ($status eq '1') {
-            next if $payment->get('status')->is_approved;
-            Bivio::IO::Alert->warn('fixing status of approved payment: ',
-                    $payment_id);
-            _update_status($proto, $payment, $status);
-        }
-	elsif ($status eq '2') {
-            next if $payment->get('status')
-                    == Bivio::Type::ECPaymentStatus->DECLINED;
-            Bivio::IO::Alert->warn('fixing status of declined payment: ',
-                    $payment_id);
-            _update_status($proto, $payment, $status);
-        }
-	else {
-	    # Ignore errors as they will be retried
-            Bivio::IO::Alert->warn(
-		$payment_id, ': failed request; transaction data: ', \@fields);
-	}
-    }
-    return;
-}
 
 sub execute_process {
     # (self, Agent.Request) : boolean
@@ -105,12 +35,6 @@ sub execute_process {
 }
 
 sub handle_config {
-    # (proto, hash) : undef
-    # login : string [undef]
-    #
-    # password : string [undef]
-    #
-    # test_mode : boolean [1]
     my(undef, $cfg) = @_;
     $_GW_LOGIN = $cfg->{login};
     $_GW_PASSWORD = $cfg->{password};
@@ -120,10 +44,11 @@ sub handle_config {
 
 sub internal_get_additional_form_data {
     # (self, proto, Model.ECPayment) : string
-    # Allow subclasses to provide additional form data for the payment processor.
+    # Allow subclasses to provide additional form data for the payment
+    # processor.
     # Used by ECSecureSourceProcessor.
     my($proto, $payment) = @_;
-    return '';
+    return [];
 }
 
 sub _process_payment {
@@ -132,29 +57,28 @@ sub _process_payment {
     # See http://secure.authorize.net/docs/developersguide.pml for
     # details of required field names and values.
     my($proto, $payment) = @_;
-    return unless
-	$payment->get('method') == Bivio::Type::ECPaymentMethod->CREDIT_CARD;
+    return
+	unless $payment->get('method')->eq_credit_card;
 
     unless ($_GW_LOGIN && $_GW_PASSWORD) {
-	Bivio::IO::Alert->warn('Missing payment gateway login configuration');
+	b_warn('Missing payment gateway login configuration');
 	return;
     }
     my($hreq) = HTTP::Request->new(
-	    POST => 'https://secure.authorize.net/gateway/transact.dll'
-	   );
+	POST => 'https://secure.authorize.net/gateway/transact.dll');
     $hreq->content_type('application/x-www-form-urlencoded');
     $hreq->content(_transact_form_data($proto, $payment));
     _trace($hreq) if $_TRACE;
-    my($response) = Bivio::Ext::LWPUserAgent->new->request($hreq);
+    my($response) = b_use('Bivio::Ext::LWPUserAgent')->new->request($hreq);
     my($response_string) = $response->as_string;
     _trace($response_string) if $_TRACE;
-    Bivio::Die->die('request failed: ', $response_string)
-	    unless $response->is_success;
+    b_die('request failed: ', $response_string)
+	unless $response->is_success;
 #TODO: RJN: Need more error checking on responses from external sites.
 #      @details is just assumed to be correct later on.
     my($result_code, @details) = split(',', $response->content);
-    Bivio::Die->die('cannot parse result string: ', $response->content)
-		unless defined($result_code);
+    b_die('cannot parse result string: ', $response->content)
+	unless defined($result_code);
     _update_status($proto, $payment, $result_code, \@details);
     return;
 }
@@ -165,14 +89,13 @@ sub _transact_form_data {
     # Will add x_Test_Request=TRUE if in test mode.
     my($proto, $payment) = @_;
     my($cc_payment) = $payment->get_model('ECCreditCardPayment');
-    my(undef, undef, undef, undef, $m, $y) = Bivio::Type::Date->to_parts(
-	$cc_payment->get('card_expiration_date'));
+    my(undef, undef, undef, undef, $m, $y) = b_use('Type.Date')
+	->to_parts($cc_payment->get('card_expiration_date'));
     my($exp_date) = sprintf('%02d/%04d', $m, $y);
-    my($test_request) = '';
     my($card_number);
     my($amount);
+
     if ($_GW_TEST_MODE) {
-        $test_request = '&x_Test_Request=TRUE';
         $card_number = '4222222222222';
         # Amount field used to trigger response:
         # 1=Approved, 2=Declined, 3=Error
@@ -182,30 +105,33 @@ sub _transact_form_data {
     } else {
         $card_number = $cc_payment->get('card_number');
 	# Amounts are always positiv.
-        $amount = Bivio::Type::Amount->abs($payment->get('amount'));
+        $amount = b_use('Type.Amount')->abs($payment->get('amount'));
     }
-    return 'x_ADC_Delim_Data=TRUE'.
-            '&x_ADC_URL=FALSE'.
-            '&x_Version=3.0'.
-            '&x_Login='.$_GW_LOGIN.
-            '&x_Password='.$_GW_PASSWORD.
-            '&x_Type='.$payment->get('status')->get_authorize_net_type.
-	    (defined($cc_payment->get('processor_transaction_number'))
-		? '&x_Trans_ID='
-		    .$cc_payment->get('processor_transaction_number')
-		: '').
-            '&x_Amount='.$amount.
-            '&x_Card_Num='.$card_number.
-            '&x_Description='.$payment->get('description').
-            '&x_Exp_Date='.$exp_date.
-            '&x_Cust_ID='.$payment->get('realm_id').
-            '&x_Invoice_Num='.$payment->get('ec_payment_id').
-	    ($cc_payment->get('card_zip') =~ /\S/
-		? ('&x_Zip='.Bivio::HTML->escape_uri(
-		    $cc_payment->get('card_zip')))
-		: '') .
-            $proto->internal_get_additional_form_data($payment).
-            $test_request;
+    return join('&', map(join('=', @$_),
+        [x_ADC_Delim_Data => 'TRUE'],
+        [x_ADC_URL => 'FALSE'],
+	[x_Version => '3.0'],
+	[x_Login => $_GW_LOGIN],
+        [x_Password => $_GW_PASSWORD],
+	[x_Type => $payment->get('status')->get_authorize_net_type],
+	defined($cc_payment->get('processor_transaction_number'))
+	    ? [x_Trans_ID => $cc_payment->get('processor_transaction_number')]
+	    : (),
+	[x_Amount => $amount],
+	[x_Card_Num => $card_number],
+	[x_Description => $payment->get('description')],
+	[x_Exp_Date => $exp_date],
+	[x_Cust_ID => $payment->get('realm_id')],
+	[x_Invoice_Num => $payment->get('ec_payment_id')],
+	$cc_payment->get('card_zip') =~ /\S/
+	    ? [x_Zip => b_use('Bivio::HTML')
+	        ->escape_uri($cc_payment->get('card_zip'))]
+	    : (),
+        $_GW_TEST_MODE
+	    ? [x_Test_Request => 'TRUE']
+	    : (),
+	@{$proto->internal_get_additional_form_data($payment)},
+    ));
 }
 
 sub _update_status {
@@ -220,17 +146,17 @@ sub _update_status {
 	$status = $payment->get('status')->get_success_state;
     }
     elsif ($result_code eq '2') {
-	$status = Bivio::Type::ECPaymentStatus->DECLINED;
+	$status = $_ECPS->DECLINED;
     }
     elsif ($result_code eq '3') {
         # Error. Keep existing status except for the following fatal cases
         $status = $error_code =~ /^([5-9]|1[079]|2[01235678]|3[5])$/
-	    ? Bivio::Type::ECPaymentStatus->FAILED
+	    ? $_ECPS->FAILED
 	    : $payment->get('status');
-	Bivio::IO::Alert->warn($details, ': failed request: ', $payment);
+	b_warn($details, ': failed request: ', $payment);
     }
     else {
-        Bivio::Die->throw_die('DIE', {
+        b_die({
 	    message => "unknown processor result code: $result_code",
 	    entity => $payment->unsafe_get('ec_payment_id'),
 	    details => $details,
@@ -241,25 +167,13 @@ sub _update_status {
 	status => $status,
     });
     $payment->get_model('ECCreditCardPayment')->update({
-	processed_date_time => Bivio::Type::DateTime->now,
+	processed_date_time => b_use('Type.DateTime')->now,
 	processor_response => $msg,
 	processor_transaction_number => $processor_transaction_number,
     });
-    _warn_declined($proto, $payment)
-	if $status->is_bad;
-    return;
-}
-
-sub _warn_declined {
-    # (proto, Model.ECPayment) : undef
-    # Writes a warning about a declined or failed payment.
-    my($proto, $payment) = @_;
-    my($req) = $payment->get_request;
-    $req->warn(
-	$payment->get('status')->get_name,
-	' payment for ',
-	$req->get('auth_realm')->get('owner_name'),
-    );
+    b_warn($payment->get('status')->get_name,
+	' payment for ', $payment->req(qw(auth_realm owner_name)),
+    ) if $status->is_bad;
     return;
 }
 
