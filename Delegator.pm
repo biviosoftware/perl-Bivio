fcf776fa3e282b703571ae3036ba49a6cf6b8374
Rob Nagler <github@q33.us>
2010-05-29 22:09:20 +0000

    allow subclasses to specify delegate explicitly (Bivio::Search needs this)

--- Delegator.pm
+++ Delegator.pm
@@ -1,46 +1,45 @@
-# Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2001-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Delegator;
 use strict;
 use Bivio::Base 'Bivio::UNIVERSAL';
-use Bivio::IO::ClassLoader;
 
 # C<Bivio::Delegator> delegates implementation to another class. Subclasses
 # must have an entry in ClassLoader.delegates.
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($AUTOLOAD);
+my($_CL) = b_use('IO.ClassLoader');
 my($_IDI) = __PACKAGE__->instance_data_index;
 my($_MAP) = {};
+our($last) = '';
 
 sub AUTOLOAD {
-    # (self) : undef
-    # Handles method calls by invoking the delegate. This is only called if the
-    # subclass doesn't implement the method.
     my($proto) = shift;
     my($method) = $AUTOLOAD =~ /([^:]+)$/;
-    return if $method eq 'DESTROY';
-    return (ref($proto) ? $proto->[$_IDI]->{delegate} : _map($proto))
-	->$method(@_);
+    return
+	if $method eq 'DESTROY';
+    die($AUTOLOAD)
+	if $AUTOLOAD eq $last;
+    local($last) = $AUTOLOAD;
+    return (
+	ref($proto) ? $proto->[$_IDI]->{delegate}
+	    : $proto->internal_delegate_package
+    )->$method(@_);
+}
+
+sub internal_delegate_package {
+    my($proto) = @_;
+    return $_MAP->{$proto} ||= $_CL->delegate_require($proto);
 }
 
 sub new {
-    # (proto, ...) : Bivio.Delegator
-    # Creates a new instance of the delegator and the delegate.
     my($proto, @args) = @_;
-    my($self) = $proto->SUPER::new($proto);
+    my($self) = $proto->SUPER::new;
     $self->[$_IDI] = {
-	delegate => _map(ref($self))->new(@args),
+	delegate => ref($self)->internal_delegate_package->new(@args),
     };
     return $self;
-}
-
-sub _map {
-    # (proto) : string
-    # Returns the delegate class for the current class/instance.
-    my($proto) = @_;
-    return $_MAP->{$proto}
-	||= Bivio::IO::ClassLoader->delegate_require($proto);
 }
 
 1;
