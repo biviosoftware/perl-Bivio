45c72f2f09d8db239cbe25cd7a14ad62b32b3b9d
Rob Nagler <github@q33.us>
2010-06-13 21:35:01 +0000

    Always send to board (broken when internal_send_to_board_maybe removed) to/cc switched around when new Grab first From: in the list of all emails so MailReplyWho->ALL does the right thing

--- Biz/Model/CRMForm.pm
+++ Biz/Model/CRMForm.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2008-2009 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::CRMForm;
 use strict;
@@ -9,6 +9,7 @@ my($_RFC) = b_use('Mail.RFC822');
 my($_CTS) = b_use('Type.CRMThreadStatus');
 my($_CT) = b_use('Model.CRMThread');
 my($_BRM) = b_use('Action.BoardRealmMail');
+my($_I) = b_use('Mail.Incoming');
 my($_TAG_ID) = 'CRMThread.thread_root_id';
 b_use('ClassWrapper.TupleTag')->wrap_methods(
     __PACKAGE__, __PACKAGE__->TUPLE_TAG_INFO);
@@ -25,27 +26,36 @@ sub TUPLE_TAG_INFO {
 
 sub execute_cancel {
     my($self) = @_;
-    _ifelse_req_has_crmthread($self, sub {_release_lock(@_)});
+    _if_crm_thread($self, sub {_release_lock(@_)});
     return shift->SUPER::execute_cancel(@_);
 }
 
 sub execute_empty {
     my($self) = @_;
     shift->SUPER::execute_empty(@_);
-    _ifelse_req_has_crmthread($self, sub {
-        my($ct, $cal) = @_;
-	my($discuss) = $self->internal_query_who->eq_realm;
-	_acquire_lock($ct)
-	    unless $discuss;
-	$self->internal_put_field(
-	    subject => $ct->clean_subject($self->get('subject')),
-	    action_id => _action_id_for_owner_and_status(
-                $self, $ct, $cal, $discuss));
-	return;
-    }, sub {
-	$self->internal_put_field(action_id =>
-            shift->status_to_id_in_list($self->internal_empty_status_when_new));
-    });
+    $self->internal_put_field(
+	_if_crm_thread(
+	    $self,
+	    sub {
+		my($ct, $cal) = @_;
+		_acquire_lock($ct)
+		    unless my $discuss = $self->internal_query_who->eq_realm;
+		return (
+		    action_id => _action_id_for_owner_and_status(
+			$self, $ct, $cal, $discuss),
+		    subject => $ct->clean_subject($self->get('subject')),
+		);
+	    },
+	    sub {
+		return (
+		    to => undef,
+		    cc => $self->get('to'),
+		    action_id => shift->status_to_id_in_list(
+			$self->internal_empty_status_when_new),
+		);
+	    },
+	),
+    );
     return;
 }
 
@@ -54,10 +64,10 @@ sub execute_ok {
     my($res) = $self->unsafe_get('update_only') ? $self->internal_return_value
 	: shift->SUPER::execute_ok(@_);
     my($ct) = $self->req('Model.CRMThread');
-    my($cal) = $self->req('Model.CRMActionList');
+    my($cal, $cid) = $self->unsafe_get(
+	qw(crm_action_list CRMThread.customer_realm_id));
     my($id) = $self->get('action_id');
     my($status) = $cal->id_to_status($id);
-    my($cid) = $self->unsafe_get('CRMThread.customer_realm_id');
     $ct->update({
 	crm_thread_status => $status,
 	owner_user_id => $cal->id_to_owner($id),
@@ -82,7 +92,7 @@ sub internal_empty_status_when_new {
 
 sub internal_format_from {
     my($self, @args) = @_;
-    return _ifelse_req_has_crmthread(
+    return _if_crm_thread(
 	$self,
 	sub {
 	    return $_RFC->format_mailbox(
@@ -90,32 +100,45 @@ sub internal_format_from {
 		$self->req(qw(auth_realm owner display_name)),
 	    );
 	},
-	sub {
-	    return $self->SUPER::internal_format_from(@args);
-	},
+	sub {$self->SUPER::internal_format_from(@args)},
     );
 }
 
 sub internal_format_subject {
-    my($self) = @_;
-    return shift->SUPER::internal_format_subject(@_)
-	unless my $ct = $self->req->unsafe_get('Model.CRMThread');
-    return $ct->make_subject($self->get('subject'));
+    my($self, @args) = @_;
+    return _if_crm_thread(
+	$self,
+	sub {shift->make_subject($self->get('subject'))},
+	sub {$self->SUPER::internal_format_subject(@args)},
+    );
+}
+
+sub internal_get_reply_incoming {
+    my($self, $in) = @_;
+    return shift->SUPER::internal_get_reply_incoming(@_)
+	unless $in;
+    my($trid) = $self->get('RealmMail.thread_root_id');
+    return shift->SUPER::internal_get_reply_incoming(@_)
+	if $trid eq $self->get('RealmMail.realm_file_id');
+    return $_I->new(
+	$self->new_other('Model.RealmMail')->load({realm_file_id => $trid}),
+    );
 }
 
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
 	version => 1,
-	visible => [
-	    $self->field_decl([
+	$self->field_decl(
+	    visible => [
 		[qw(action_id CRMActionId)],
 		[qw(update_only OKButton)],
-	    ]),
-	],
-	other => [
-	    $_TAG_ID,
-	],
+	    ],
+	    other => [
+		$_TAG_ID,
+		[qw(crm_action_list Model.CRMActionList)],
+	    ],
+	),
     });
 }
 
@@ -123,10 +146,13 @@ sub internal_pre_execute {
     my($self) = @_;
     my(@res) = shift->SUPER::internal_pre_execute(@_);
     if (my $trid = $self->get('RealmMail.thread_root_id')) {
-	$self->new_other('CRMThread')->load({thread_root_id => $trid});
 	$self->internal_put_field($_TAG_ID => $trid);
+	$self->new_other('CRMThread')->load({thread_root_id => $trid});
     }
-    $self->new_other('CRMActionList')->load_all;
+    $self->internal_put_field(
+	crm_action_list => $self->new_other('CRMActionList')->load_all,
+	board_always => 1,
+    );
     return @res;
 }
 
@@ -142,14 +168,13 @@ sub validate {
     else {
 	shift->SUPER::validate(@_);
     }
-    _ifelse_req_has_crmthread($self, sub {
+    return _if_crm_thread($self, sub {
         my(undef, $cal) = @_;
 	$self->internal_put_error(action_id => 'SYNTAX_ERROR')
 	    unless $self->get_field_error('action_id')
 	    || $cal->validate_id($self->get('action_id'));
 	return;
     });
-    return;
 }
 
 sub _acquire_lock {
@@ -168,11 +193,11 @@ sub _action_id_for_owner_and_status {
                 $self->internal_empty_status_when_exists);
 }
 
-sub _ifelse_req_has_crmthread {
+sub _if_crm_thread {
     my($self, $true, $false) = @_;
-    my($cal) = $self->req('Model.CRMActionList');
+    my($cal) = $self->get('crm_action_list');
     return $false && $false->($cal)
-	unless my $ct = $self->req->unsafe_get('Model.CRMThread');
+	unless my $ct = $self->ureq('Model.CRMThread');
     return $true->($ct, $cal);
 }
 
