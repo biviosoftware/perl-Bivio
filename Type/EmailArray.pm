651c1528629607d22946fbcf2347fe88a72a2598
Rob Nagler <github@q33.us>
2010-06-15 21:29:25 +0000

    b_use

--- Type/EmailArray.pm
+++ Type/EmailArray.pm
@@ -1,11 +1,11 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::EmailArray;
 use strict;
 use Bivio::Base 'Type.StringArray';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_E) = __PACKAGE__->use('Type.Email');
+my($_E) = b_use('Type.Email');
 
 sub UNDERLYING_TYPE {
     return $_E;
