2838d39b4273282fc5644b29c65c1f39acf2ea0e
Rob Nagler <github@q33.us>
2010-07-27 00:33:18 +0000

    rmpod

--- Type/Dollar.pm
+++ Type/Dollar.pm
@@ -2,92 +2,28 @@
 # $Id$
 package Bivio::Type::Dollar;
 use strict;
-$Bivio::Type::Dollar::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::Dollar::VERSION;
+use Bivio::Base 'Bivio::Type::Amount';
 
-=head1 NAME
+# C<Bivio::Type::Dollar>
 
-Bivio::Type::Dollar - Amount rounded to cents
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::Dollar;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Amount>
-
-=cut
-
-use Bivio::Type::Amount;
-@Bivio::Type::Dollar::ISA = ('Bivio::Type::Amount');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::Dollar>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_decimals"></a>
-
-=head2 static get_decimals : int
-
-Returns 2.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_decimals {
+    # : int
+    # Returns 2.
     return 2;
 }
 
-=for html <a name="get_max"></a>
-
-=head2 static get_max : string
-
-Returns '9999999999999.99'.
-
-=cut
-
 sub get_max {
+    # : string
+    # Returns '9999999999999.99'.
     return '9999999999999.99';
 }
 
-=for html <a name="get_min"></a>
-
-=head2 static get_min : string
-
-Returns '-9999999999999.99'.
-
-=cut
-
 sub get_min {
+    # : string
+    # Returns '-9999999999999.99'.
     return '-9999999999999.99';
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
