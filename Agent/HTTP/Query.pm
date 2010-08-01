71715b1abeac15c25192feb687edd1d665552492
Rob Nagler <github@q33.us>
2010-08-01 15:54:35 +0000

    rmpod

--- Agent/HTTP/Query.pm
+++ Agent/HTTP/Query.pm
@@ -2,53 +2,18 @@
 # $Id$
 package Bivio::Agent::HTTP::Query;
 use strict;
-$Bivio::Agent::HTTP::Query::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Agent::HTTP::Query::VERSION;
-
-=head1 NAME
-
-Bivio::Agent::HTTP::Query - formats query strings
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Agent::HTTP::Query;
-    Bivio::Agent::HTTP::Query->format($query);
-
-=cut
-
-use Bivio::UNIVERSAL;
-@Bivio::Agent::HTTP::Query::ISA = ('Bivio::UNIVERSAL');
-
-=head1 DESCRIPTION
-
-C<Bivio::Agent::HTTP::Query> formats a hash_ref into a query string
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::UNIVERSAL';
 use Bivio::HTML;
 
-#=VARIABLES
+# C<Bivio::Agent::HTTP::Query> formats a hash_ref into a query string
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="format"></a>
-
-=head2 static format(hash_ref query) : string
-
-Returns the string version of the query.  Returns C<undef> if I<query> is
-C<undef>.  Attributes of the form C<ListQuery.>I<name> will be looked up
-with L<Bivio::ListQuery::to_char|Bivio::ListQuery/"to_char">.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub format {
+    # (proto, hash_ref) : string
+    # Returns the string version of the query.  Returns C<undef> if I<query> is
+    # C<undef>.  Attributes of the form C<ListQuery.>I<name> will be looked up
+    # with L<Bivio::ListQuery::to_char|Bivio::ListQuery/"to_char">.
     my(undef, $query) = @_;
     return undef unless $query;
     my($res) = '';
@@ -65,16 +30,10 @@ sub format {
     return $res;
 }
 
-=for html <a name="parse"></a>
-
-=head2 static parse(string query) : hash_ref
-
-Returns a hash_ref for the query string.  Returns C<undef> if
-string not defined.
-
-=cut
-
 sub parse {
+    # (proto, string) : hash_ref
+    # Returns a hash_ref for the query string.  Returns C<undef> if
+    # string not defined.
     my(undef, $string) = @_;
 
     # Empty?
@@ -113,13 +72,9 @@ sub parse {
     return {@v};
 }
 
-#=PRIVATE METHODS
-
-# _correct(string method, string literal) : string
-#
-# Corrects the URI using specified unescape method
-#
 sub _correct {
+    # (string, string) : string
+    # Corrects the URI using specified unescape method
     my($method, $literal) = @_;
     my(@msg) = ('correcting query=', $literal);
     my($req) = Bivio::Agent::Request->get_current;
@@ -135,15 +90,5 @@ sub _correct {
     Bivio::IO::Alert->warn(@msg);
     return Bivio::HTML->$method($literal);
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
