a3d3ab7e05f553f45aa1de3b4f6752a624874719
Rob Nagler <github@q33.us>
2010-08-02 17:27:51 +0000

    bunit; fmt

--- Type/ECCreditCardNumber.pm
+++ Type/ECCreditCardNumber.pm
@@ -1,49 +1,40 @@
-# Copyright (c) 2000 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2000-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::ECCreditCardNumber;
 use strict;
-use Bivio::Base 'Bivio::Type::Secret';
-
-# C<Bivio::Type::ECCreditCardNumber> interprets a string as a credit card number.
+use Bivio::Base 'Type.Secret';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-our($_TRACE);
+my($_TE) = b_use('Bivio.TypeError');
 
 sub TEST_NUMBER {
-    # : string
-    # Returns a valid test credit card number.
     return '4222 2222 2222 2';
 }
 
 sub from_literal {
-    # (proto, string) : array
-    # Returns C<undef> if the value is empty or does not pass the Luhn test
     my($proto, $value) = @_;
     my($err);
     ($value, $err) = $proto->SUPER::from_literal($value);
-    return ($value, $err) if $err;
-    return undef unless defined($value);
-
-    # Remove dashes and spaces to be friendly
+    return ($value, $err)
+	if $err;
+    return undef
+	unless defined($value);
     $value =~ s/[-\s]+//g;
     return $proto->luhn_mod10($value) ? $value :
-            (undef, Bivio::TypeError::CREDITCARD_INVALID_NUMBER());
+            (undef, $_TE->CREDITCARD_INVALID_NUMBER);
 }
 
 sub get_width {
-    # (self) : int
-    # Returns the maximum width of a credit card.
     return 19;
 }
 
 sub luhn_mod10 {
-    # (self, string) : boolean
-    # Returns TRUE if I<number> passes the Luhn Mod-10 test
     my(undef, $number) = @_;
-    return 0 unless defined($number);
+    return 0
+	unless defined($number);
     my($len) = length($number);
-    return 0 if $len < 12 || $len > 19 || $number =~ /\D/;
-
+    return 0
+	if $len < 12 || $len > 19 || $number =~ /\D/;
     my($sum) = 0;
     my($mul) = 1;
     my(@digits) = split('', $number);
