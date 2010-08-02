2d8e3fc78870de7f85977ded055bf022e8b975a6
Rob Nagler <github@q33.us>
2010-08-02 17:23:23 +0000

    rmpod

--- Type/ECCreditCardNumber.pm
+++ Type/ECCreditCardNumber.pm
@@ -2,73 +2,22 @@
 # $Id$
 package Bivio::Type::ECCreditCardNumber;
 use strict;
-$Bivio::Type::ECCreditCardNumber::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::ECCreditCardNumber::VERSION;
+use Bivio::Base 'Bivio::Type::Secret';
 
-=head1 NAME
+# C<Bivio::Type::ECCreditCardNumber> interprets a string as a credit card number.
 
-Bivio::Type::ECCreditCardNumber - a credit card number, TEST MODULE ONLY!
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::ECCreditCardNumber;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Secret>
-
-=cut
-
-use Bivio::Type::Secret;
-@Bivio::Type::ECCreditCardNumber::ISA = ('Bivio::Type::Secret');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::ECCreditCardNumber> interprets a string as a credit card number.
-
-=cut
-
-=head1 CONSTANTS
-
-=cut
-
-=for html <a name="TEST_NUMBER"></a>
-
-=head2 TEST_NUMBER : string
-
-Returns a valid test credit card number.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+our($_TRACE);
 
 sub TEST_NUMBER {
+    # : string
+    # Returns a valid test credit card number.
     return '4222 2222 2222 2';
 }
 
-#=IMPORTS
-
-#=VARIABLES
-use vars qw($_TRACE);
-Bivio::IO::Trace->register;
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="from_literal"></a>
-
-=head2 static from_literal(string value) : array
-
-Returns C<undef> if the value is empty or does not pass the Luhn test
-
-=cut
-
 sub from_literal {
+    # (proto, string) : array
+    # Returns C<undef> if the value is empty or does not pass the Luhn test
     my($proto, $value) = @_;
     my($err);
     ($value, $err) = $proto->SUPER::from_literal($value);
@@ -81,27 +30,15 @@ sub from_literal {
             (undef, Bivio::TypeError::CREDITCARD_INVALID_NUMBER());
 }
 
-=for html <a name="get_width"></a>
-
-=head2 get_width() : int
-
-Returns the maximum width of a credit card.
-
-=cut
-
 sub get_width {
+    # (self) : int
+    # Returns the maximum width of a credit card.
     return 19;
 }
 
-=for html <a name="luhn_mod10"></a>
-
-=head2 luhn_mod10(string number) : boolean
-
-Returns TRUE if I<number> passes the Luhn Mod-10 test
-
-=cut
-
 sub luhn_mod10 {
+    # (self, string) : boolean
+    # Returns TRUE if I<number> passes the Luhn Mod-10 test
     my(undef, $number) = @_;
     return 0 unless defined($number);
     my($len) = length($number);
@@ -117,17 +54,5 @@ sub luhn_mod10 {
     }
     return ($sum % 10) == 0 ? 1 : 0;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2000 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
