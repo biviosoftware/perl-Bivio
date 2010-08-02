620b3331778edae2affbf9b043ca9a3f48b888c6
Rob Nagler <github@q33.us>
2010-08-02 17:05:00 +0000

    fmt

--- Type/Appellation.pm
+++ Type/Appellation.pm
@@ -1,10 +1,8 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::Appellation;
 use strict;
-use Bivio::Base 'Bivio::Type::Enum';
-
-# C<Bivio::Type::Appellation>
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile([
