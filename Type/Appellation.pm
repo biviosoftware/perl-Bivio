7e5c9365e51d79b94c171947b91c66fbe7a5ba8d
Rob Nagler <github@q33.us>
2010-08-02 17:03:53 +0000

    rmpod

--- Type/Appellation.pm
+++ Type/Appellation.pm
@@ -2,41 +2,11 @@
 # $Id$
 package Bivio::Type::Appellation;
 use strict;
-$Bivio::Type::Appellation::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::Appellation::VERSION;
+use Bivio::Base 'Bivio::Type::Enum';
 
-=head1 NAME
+# C<Bivio::Type::Appellation>
 
-Bivio::Type::Appellation - titles
-
-=head1 RELEASE SCOPE
-
-Bivio
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::Appellation;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Enum>
-
-=cut
-
-use Bivio::Type::Enum;
-@Bivio::Type::Appellation::ISA = ('Bivio::Type::Enum');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::Appellation>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile([
     UNKNOWN => [0, 'None'],
     DR => [1, 'Dr.'],
@@ -44,21 +14,5 @@ __PACKAGE__->compile([
     MRS => [3, 'Mrs.'],
     MS => [4, 'Ms.'],
 ]);
-
-=head1 METHODS
-
-=cut
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
