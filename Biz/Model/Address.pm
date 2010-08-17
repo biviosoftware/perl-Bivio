44a3e64c4e71115e10c943656eaa4ec336ab33ca
Rob Nagler <github@q33.us>
2010-08-17 11:51:09 +0000

    remove comments

--- Biz/Model/Address.pm
+++ Biz/Model/Address.pm
@@ -1,17 +1,12 @@
-# Copyright (c) 1999 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Biz::Model::Address;
 use strict;
-use Bivio::Base 'Bivio::Biz::Model::LocationBase';
-
-# C<Bivio::Biz::Model::Address> is the create, read, update,
-# and delete interface to the C<address_t> table.
+use Bivio::Base 'Model.LocationBase';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub execute_load_home {
-    # (proto, Agent.Request) : boolean
-    # Loads the home address for the current realm.
     my($proto, $req) = @_;
     $proto->new($req)->load({
 	location => Bivio::Type::Location->HOME,
@@ -20,20 +15,6 @@ sub execute_load_home {
 }
 
 sub format {
-    # (self) : string
-    # (proto, Biz.Model, string) : string
-    # Returns the street1, street2, city, state, zip, and country as
-    # a single string (with embedded newlines).
-    #
-    # In the second form, I<list_model> is used to get the values, not I<self>.
-    # List Models can declare a method of the form:
-    #
-    #     sub format_address {
-    # 	my($self) = shift;
-    # 	Bivio::Biz::Model::Address->format($self, 'Address.');
-    #     }
-    #
-    # Always returns a valid (defined) string, but may be zero length.
     my($self, $model, $model_prefix) = shift->internal_get_target(@_);
     my($m, $p) = ($model, $model_prefix);
     my($sep) = ', ';
@@ -54,8 +35,6 @@ sub format {
 }
 
 sub internal_initialize {
-    # (self) : hash_ref
-    # B<FOR INTERNAL USE ONLY>
     return {
 	version => 1,
 	table_name => 'address_t',
