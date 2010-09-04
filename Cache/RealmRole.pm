62a1372d5d5aa1553d2006225f2394d682e93d05
Rob Nagler <github@q33.us>
2010-09-04 20:50:51 +0000

    moved DEFAULT_PERMISSIONS cache to SimpleAuthSupport, because it is more than a simple cache. Need to use as_int, because values are persisted

--- Cache/RealmRole.pm
+++ Cache/RealmRole.pm
@@ -7,7 +7,6 @@ use Bivio::Base 'Bivio.Cache';
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_RR) = b_use('Model.RealmRole');
 my($_RT) = b_use('Auth.RealmType');
-my($_DEFAULT_PERMISSIONS) = {};
 b_use('IO.Config')->register(my $_CFG = {
     enable => 1,
 });
@@ -31,7 +30,7 @@ sub handle_property_model_modification {
 	b_die($query, 'must supply a realm_id to delete')
 	    unless $query->{realm_id};
 	if ($query->{role}) {
-	    delete($map->{$query->{realm_id}}->{$query->{role}});
+	    delete($map->{$query->{realm_id}}->{$query->{role}->as_int});
 	}
 	else {
 	    delete($map->{$query->{realm_id}});
@@ -60,24 +59,9 @@ sub internal_compute {
 sub internal_compute_no_cache {
     my($self, $req, $realm_id, $role) = @_;
     my($rr) = $_RR->new($req);
-    unless ($_RT->is_default_id($realm_id)) {
-	return {$realm_id => {$role => $rr->get('permission_set')}}
-	    if $rr->unauth_load({realm_id => $realm_id, role => $role});
-	return undef;
-    }
-    $_DEFAULT_PERMISSIONS->{$realm_id} ||= {
-	%{$rr->EMPTY_PERMISSION_MAP},
-	@{$rr->map_iterate(
-	    sub {
-		my($it) = @_;
-		return ($it->get('role') => $it->get('permission_set'));
-	    },
-	    'unauth_iterate_start',
-	    'role',
-	    {realm_id => $realm_id},
-	)},
-    };
-    return $_DEFAULT_PERMISSIONS;
+    return {$realm_id => {$role->as_int => $rr->get('permission_set')}}
+	if $rr->unauth_load({realm_id => $realm_id, role => $role});
+    return undef;
 }
 
 sub permission_set_for_realm_role {
@@ -87,20 +71,20 @@ sub permission_set_for_realm_role {
     my($res) = $proto->internal_retrieve($req, $realm_id, $role);
     return $res
 	unless ref($res) eq 'HASH';
-    return $res->{$realm_id}->{$role};
+    return $res->{$realm_id}->{$role->as_int};
 }
 
 sub _not_enabled {
     my($proto, $realm_id, $role, $req) = @_;
     return undef
 	unless my $res = $proto->internal_compute_no_cache($req, $realm_id, $role);
-    return $res->{$realm_id}->{$role};
+    return $res->{$realm_id}->{$role->as_int};
 }
 
 sub _update {
     my($model, $map) = @_;
     my($rid, $role, $ps) = $model->get(qw(realm_id role permission_set));
-    $map->{$rid}->{$role} = $ps;
+    $map->{$rid}->{$role->as_int} = $ps;
     return;
 }
 
