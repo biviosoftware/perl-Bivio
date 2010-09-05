08f93af1552eaab4a573bbe454491b0f91270f32
Rob Nagler <github@q33.us>
2010-09-05 18:02:42 +0000

    rmpod

--- Delegate/NoCookie.pm
+++ Delegate/NoCookie.pm
@@ -2,75 +2,23 @@
 # $Id$
 package Bivio::Delegate::NoCookie;
 use strict;
-$Bivio::Delegate::NoCookie::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Delegate::NoCookie::VERSION;
+use Bivio::Base 'Bivio::Collection::Attributes';
 
-=head1 NAME
+# C<Bivio::Delegate::NoCookie> is a placeholder for
+# L<Bivio::Agent::HTTP::Cookie|Bivio::Agent::HTTP::Cookie>.
 
-Bivio::Delegate::NoCookie - no cookie management
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Delegate::NoCookie;
-
-=cut
-
-use Bivio::Collection::Attributes;
-@Bivio::Delegate::NoCookie::ISA = ('Bivio::Collection::Attributes');
-
-=head1 DESCRIPTION
-
-C<Bivio::Delegate::NoCookie> is a placeholder for
-L<Bivio::Agent::HTTP::Cookie|Bivio::Agent::HTTP::Cookie>.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="assert_is_ok"></a>
-
-=head2 assert_is_ok(Bivio::Agent::Request req) : boolean
-
-This cookie is always OK.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub assert_is_ok {
+    # (self, Agent.Request) : boolean
+    # This cookie is always OK.
     return 1;
 }
 
-=for html <a name="header_out"></a>
-
-=head2 header_out(Bivio::Agent::Request req, Apache::Request r) : boolean
-
-Does nothing.
-
-=cut
-
 sub header_out {
+    # (self, Agent.Request, Apache.Request) : boolean
+    # Does nothing.
     return;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
