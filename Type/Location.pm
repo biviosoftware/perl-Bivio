146e8256fe45bfa425ffe79462cb9aa956c8471c
Rob Nagler <github@q33.us>
2010-09-08 21:48:40 +0000

    b_use and fmt

--- Type/Location.pm
+++ Type/Location.pm
@@ -1,22 +1,15 @@
-# Copyright (c) 1999,2000 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::Location;
 use strict;
-use Bivio::Base 'Bivio::Type::Enum';
-
-# C<Bivio::Type::Location> describes the physical location where an
-# address, phone, or email resides.
-#
-# It can be assumed C<HOME> is defined.
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile(
-    Bivio::IO::ClassLoader->delegate_require_info(__PACKAGE__));
+    b_use('IO.ClassLoader')->delegate_require_info(__PACKAGE__));
 
 sub get_default {
     return shift->from_int(1);
 }
-
-#=PRIVATE METHODS
 
 1;
