e6ec7084b564c3bd276fcae57a015b34e4fe161c
Rob Nagler <github@q33.us>
2010-09-08 21:40:43 +0000

    removed comments

--- Delegate/NoCookie.pm
+++ Delegate/NoCookie.pm
@@ -1,23 +1,16 @@
-# Copyright (c) 2001 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 2001-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Delegate::NoCookie;
 use strict;
 use Bivio::Base 'Bivio::Collection::Attributes';
 
-# C<Bivio::Delegate::NoCookie> is a placeholder for
-# L<Bivio::Agent::HTTP::Cookie|Bivio::Agent::HTTP::Cookie>.
-
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub assert_is_ok {
-    # (self, Agent.Request) : boolean
-    # This cookie is always OK.
     return 1;
 }
 
 sub header_out {
-    # (self, Agent.Request, Apache.Request) : boolean
-    # Does nothing.
     return;
 }
 
