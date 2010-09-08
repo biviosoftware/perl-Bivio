34bba514d06f236c12b75b341a99926b70f15715
Rob Nagler <github@q33.us>
2010-09-08 21:48:26 +0000

    fmt

--- Type/EnumDelegator.pm
+++ Type/EnumDelegator.pm
@@ -2,7 +2,7 @@
 # $Id$
 package Bivio::Type::EnumDelegator;
 use strict;
-use Bivio::Base 'Bivio::Type::Enum';
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($AUTOLOAD);
@@ -11,13 +11,14 @@ my($_MAP) = {};
 sub AUTOLOAD {
     my($proto) = shift;
     my($method) = $AUTOLOAD =~ /([^:]+)$/;
-    return if $method eq 'DESTROY';
+    return
+	if $method eq 'DESTROY';
     my($c) = ref($proto) || $proto;
     # can() returns a reference to the method to invoke
     # use this so delegates can be subclassed
     $_MAP->{$c} ||= Bivio::IO::ClassLoader->delegate_require($c);
     my($dispatch) = $_MAP->{$c}->can($method);
-    Bivio::Die->die('method not found: ', $c, '->', $method)
+    b_die('method not found: ', $c, '->', $method)
         unless $dispatch;
     return ref($proto) ? $dispatch->($proto, @_) : $_MAP->{$c}->$method(@_);
 }
@@ -25,7 +26,7 @@ sub AUTOLOAD {
 sub compile {
     my($proto, $values) = @_;
     return $proto->SUPER::compile(
-	$values || Bivio::IO::ClassLoader->delegate_require_info($proto),
+	$values || b_use('IO.ClassLoader')->delegate_require_info($proto),
     );
 }
 
