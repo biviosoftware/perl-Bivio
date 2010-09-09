07b791190b65f92bb4eb44a42ab89f6c708d3d51
Rob Nagler <github@q33.us>
2010-09-09 02:57:16 +0000

    rmpod

--- UI/HTML/Widget/Grid.pm
+++ UI/HTML/Widget/Grid.pm
@@ -2,183 +2,113 @@
 # $Id$
 package Bivio::UI::HTML::Widget::Grid;
 use strict;
-$Bivio::UI::HTML::Widget::Grid::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::Grid::VERSION;
-
-=head1 NAME
-
-Bivio::UI::HTML::Widget::Grid - lays out widgets in a grid (html table)
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::Grid;
-    Bivio::UI::HTML::Widget::Grid->new($attrs);
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::HTML::Widget::TableBase>
-
-=cut
-
-use Bivio::UI::HTML::Widget::TableBase;
-@Bivio::UI::HTML::Widget::Grid::ISA = ('Bivio::UI::HTML::Widget::TableBase');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::Grid> lays out widgets in an html table.
-There are two types of attributes: table and cell.
-
-=head1 TABLE ATTRIBUTES
-
-=over 4
-
-=item hide_empty_cells : boolean [false]
-
-If true, empty cells will not be rendered.
-
-=item values : array_ref (required)
-
-An array_ref of rows of array_ref of columns (cells).  A cell may
-be C<undef>.  A cell may be a widget_value which returns a widget
-or a string or it may be a widget or a string.
-
-=back
-
-=head1 CELL ATTRIBUTES
-
-=over 4
-
-=item cell_align : string []
-
-How to align the value within the cell.  The allowed (case
-insensitive) values are defined in
-L<Bivio::UI::Align|Bivio::UI::Align>.
-The value affects the C<ALIGN> and C<VALIGN> attributes of the C<TD> tag.
-
-=item cell_bgcolor : string [] (dynamic)
-
-=item cell_bgcolor : array_ref [] (dynamic)
-
-The value to be passed to the C<BGCOLOR> attribute of the C<TD> tag.
-See L<Bivio::UI::Color|Bivio::UI::Color>.
-
-=item cell_class : any [] (dynamic)
-
-=item cell_colspan : int [1]
-
-The value passed to C<COLSPAN> attribute of the C<TD> tag.
-
-=item cell_compact : boolean [false]
-
-If true, the cell will be C<WIDTH=1%>.
-
-=item cell_end : boolean [true]
-
-If false, will not put cell end tag C<E<lt>/TD E<gt>>
-
-=item cell_end_form : boolean [false]
-
-End the form after the cell is closed.  This is a netscape/IE hack
-which generates invalid html.
-
-=item cell_expand : boolean [false]
-
-If true, the cell will consume any excess columns in its row.
-Excess columns are not the same as C<undef> columns which are
-blank place holders.
-
-=item cell_height : int []
-
-Sets the cell height explicitly.
-
-=item cell_height_as_html : array_ref []
-
-Sets the cell height explicitly from a widget value.  The
-widget value must return the full attribute, e.g. use
-L<Bivio::UI::Icon::get_height_as_html|Bivio::UI::Icon/"get_height_as_html">.
-
-=item cell_nowrap : boolean [false]
-
-If true, the cell will not be wrapped.
-
-=item cell_rowspan : int [1]
-
-The value passed to C<ROWSPAN> attribute of the C<TD> tag.
-
-=item cell_width : int []
-
-Sets the cell width explicitly.
-
-=item cell_width_as_html : array_ref []
-
-Sets the cell width explicitly from a widget value.  The
-widget value must return the full attribute, e.g. use
-L<Bivio::UI::Icon::get_width_as_html|Bivio::UI::Icon/"get_width_as_html">.
-
-=item row_control : array_ref []
-
-If set, controls the rendering of an entire row.  Can be set
-on any cell in the row.
-
-=item row_class : any [] (dynamic)
-
-If set, controls the class of the entire row.  Can be set
-on any cell in the row.
-
-=back
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::UI::HTML::Widget::TableBase';
 use Bivio::UI::Align;
 
-#=VARIABLES
+# C<Bivio::UI::HTML::Widget::Grid> lays out widgets in an html table.
+# There are two types of attributes: table and cell.
+#
+#
+#
+# hide_empty_cells : boolean [false]
+#
+# If true, empty cells will not be rendered.
+#
+# values : array_ref (required)
+#
+# An array_ref of rows of array_ref of columns (cells).  A cell may
+# be C<undef>.  A cell may be a widget_value which returns a widget
+# or a string or it may be a widget or a string.
+#
+#
+#
+#
+# cell_align : string []
+#
+# How to align the value within the cell.  The allowed (case
+# insensitive) values are defined in
+# L<Bivio::UI::Align|Bivio::UI::Align>.
+# The value affects the C<ALIGN> and C<VALIGN> attributes of the C<TD> tag.
+#
+# cell_bgcolor : string [] (dynamic)
+#
+# cell_bgcolor : array_ref [] (dynamic)
+#
+# The value to be passed to the C<BGCOLOR> attribute of the C<TD> tag.
+# See L<Bivio::UI::Color|Bivio::UI::Color>.
+#
+# cell_class : any [] (dynamic)
+#
+# cell_colspan : int [1]
+#
+# The value passed to C<COLSPAN> attribute of the C<TD> tag.
+#
+# cell_compact : boolean [false]
+#
+# If true, the cell will be C<WIDTH=1%>.
+#
+# cell_end : boolean [true]
+#
+# If false, will not put cell end tag C<E<lt>/TD E<gt>>
+#
+# cell_end_form : boolean [false]
+#
+# End the form after the cell is closed.  This is a netscape/IE hack
+# which generates invalid html.
+#
+# cell_expand : boolean [false]
+#
+# If true, the cell will consume any excess columns in its row.
+# Excess columns are not the same as C<undef> columns which are
+# blank place holders.
+#
+# cell_height : int []
+#
+# Sets the cell height explicitly.
+#
+# cell_height_as_html : array_ref []
+#
+# Sets the cell height explicitly from a widget value.  The
+# widget value must return the full attribute, e.g. use
+# L<Bivio::UI::Icon::get_height_as_html|Bivio::UI::Icon/"get_height_as_html">.
+#
+# cell_nowrap : boolean [false]
+#
+# If true, the cell will not be wrapped.
+#
+# cell_rowspan : int [1]
+#
+# The value passed to C<ROWSPAN> attribute of the C<TD> tag.
+#
+# cell_width : int []
+#
+# Sets the cell width explicitly.
+#
+# cell_width_as_html : array_ref []
+#
+# Sets the cell width explicitly from a widget value.  The
+# widget value must return the full attribute, e.g. use
+# L<Bivio::UI::Icon::get_width_as_html|Bivio::UI::Icon/"get_width_as_html">.
+#
+# row_control : array_ref []
+#
+# If set, controls the rendering of an entire row.  Can be set
+# on any cell in the row.
+#
+# row_class : any [] (dynamic)
+#
+# If set, controls the class of the entire row.  Can be set
+# on any cell in the row.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 my($_SPACER) = '&nbsp;' x 3;
 my($_VS) = __PACKAGE__->use('Bivio::UI::HTML::ViewShortcuts');
 my($_END_COL) = "</td>\n";
 
-=head1 FACTORIES
-
-=cut
-
-=for html <a name="new"></a>
-
-=head2 static new(array_ref values, hash_ref attributes) : Bivio::UI::HTML::Widget::Grid
-
-Creates a new Grid widget with I<values> and optional I<attributes>.
-
-=head2 static new(hash_ref attributes) : Bivio::UI::HTML::Widget::Grid
-
-Creates a new Grid widget with I<attributes>.
-
-=cut
-
-sub new {
-    my($self) = shift->SUPER::new(@_);
-    $self->[$_IDI] = {};
-    return $self;
-}
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes static information.
-
-=cut
-
 sub initialize {
+    # (self) : undef
+    # Initializes static information.
     my($self, $source) = @_;
     my($fields) = $self->[$_IDI];
     return if exists($fields->{rows});
@@ -263,15 +193,9 @@ sub initialize {
     return;
 }
 
-=for html <a name="internal_new_args"></a>
-
-=head2 static internal_new_args(any arg, ...) : any
-
-Implements positional argument parsing for L<new|"new">.
-
-=cut
-
 sub internal_new_args {
+    # (proto, any, ...) : any
+    # Implements positional argument parsing for L<new|"new">.
     my($proto, $values, $attributes) = @_;
     return "'values' must be an array_ref (rows) of array_refs (cells)"
 	unless ref($values) eq 'ARRAY';
@@ -283,18 +207,11 @@ sub internal_new_args {
     };
 }
 
-
-=for html <a name="layout_buttons"></a>
-
-=head2 layout_buttons(array_ref buttons, int max_width)
-
-Sets I<values> to I<buttons> laid out appropriately for I<max_width>,
-i.e. the maximum width of the buttons in characters.  See
-I<RadioGrid> and I<CheckboxGrid> for examples.
-
-=cut
-
 sub layout_buttons {
+    # (self, array_ref, int) : undef
+    # Sets I<values> to I<buttons> laid out appropriately for I<max_width>,
+    # i.e. the maximum width of the buttons in characters.  See
+    # I<RadioGrid> and I<CheckboxGrid> for examples.
     my($self, $buttons, $max_width) = @_;
     my(@rows) = ();
     my($s) = '&nbsp;' x 3;
@@ -327,23 +244,17 @@ sub layout_buttons {
     return;
 }
 
-=for html <a name="layout_buttons_row_major"></a>
-
-=head2 layout_buttons(array_ref buttons, int column_count)
-
-Lays out I<column_count> in row major (across the rows) format.
-
-Here's a three column example:
-
-    [
-        button1, button2, button3,
-        button4, button5, button6,
-        ...
-    ]
-
-=cut
-
 sub layout_buttons_row_major {
+    # (self, array_ref, int) : undef
+    # Lays out I<column_count> in row major (across the rows) format.
+    #
+    # Here's a three column example:
+    #
+    #     [
+    #         button1, button2, button3,
+    #         button4, button5, button6,
+    #         ...
+    #     ]
     my($self, $buttons, $column_count) = @_;
     my(@buttons) = @$buttons;
     $column_count--;
@@ -361,13 +272,20 @@ sub layout_buttons_row_major {
     return;
 }
 
-=for html <a name="render"></a>
-
-=head2 render(string_ref buffer)
-
-=cut
+sub new {
+    # (proto, array_ref, hash_ref) : Widget.Grid
+    # (proto, hash_ref) : Widget.Grid
+    # Creates a new Grid widget with I<values> and optional I<attributes>.
+    #
+    #
+    # Creates a new Grid widget with I<attributes>.
+    my($self) = shift->SUPER::new(@_);
+    $self->[$_IDI] = {};
+    return $self;
+}
 
 sub render {
+    # (self, string_ref) : undef
     my($self, $source, $buffer) = @_;
     my($fields) = $self->[$_IDI];
     my($req) = $source->get_request;
@@ -415,15 +333,11 @@ sub render {
     return;
 }
 
-#=PRIVATE METHODS
-
-# _append(array_ref list, string element)
-# _append(array_ref list, ref element)
-#
-# Appends element literally to $list->[$#list] both parts are a string,
-# else pushes on a new element.
-#
 sub _append {
+    # (array_ref, string) : undef
+    # (array_ref, ref) : undef
+    # Appends element literally to $list->[$#list] both parts are a string,
+    # else pushes on a new element.
     my($list, $element) = @_;
     if (ref($list->[$#$list]).ref($element) eq '') {
 	# both are strings
@@ -435,15 +349,5 @@ sub _append {
     }
     return;
 }
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2005 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
--- UI/HTML/Widget/TableBase.pm
+++ UI/HTML/Widget/TableBase.pm
@@ -2,138 +2,94 @@
 # $Id$
 package Bivio::UI::HTML::Widget::TableBase;
 use strict;
-$Bivio::UI::HTML::Widget::TableBase::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::TableBase::VERSION;
-
-=head1 NAME
-
-Bivio::UI::HTML::Widget::TableBase - common attrs between Grid and Table
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::TableBase;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::Widget>
-
-=cut
-
-use Bivio::UI::Widget;
-@Bivio::UI::HTML::Widget::TableBase::ISA = ('Bivio::UI::Widget');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::TableBase>
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item align : any [Table => Facade.HTML.table_default_align]
-
-How to align the table.  The allowed (case
-insensitive) values are defined in
-L<Bivio::UI::Align|Bivio::UI::Align>.
-The value affects the C<ALIGN> and C<VALIGN> attributes of the C<TD> tag.
-No default if I<class> is set.
-
-=item background : any
-
-Widget which returns image to render for background.
-
-=item bgcolor : any [] (dynamic)
-
-The value to be passed to the C<BGCOLOR> attribute of the C<TABLE> tag.
-See L<Bivio::UI::Color|Bivio::UI::Color>.
-
-=item border : any [0]
-
-Width of border surrounding the table and its cells.
-No default if I<class> is set.
-
-=item cellpadding : any [Table => 5, Grid => 0]
-
-Padding inside each cell in pixels.
-No default if I<class> is set.
-
-=item cellspacing : any [0]
-
-Spacing around each cell in pixels.
-No default if I<class> is set.
-
-=item class : any
-
-The html CLASS for the table.  If exists, then pad, space, cellspacing,
-cellpadding, border, and align default to "undef".
-
-=item end_tag : any [true]
-
-If false, this widget won't render the start tag.
-
-=item expand : boolean [false]
-
-If true, the table C<WIDTH> will be C<95%> or C<100%> depending
-on Bivio::UI::HTML.page_left_margin.
-
-=item height : any []
-
-Dynamic height (only IE and Netscape support this attribute).
-
-=item id : any
-
-The html ID for the table.
-
-=item pad : any [0]
-
-B<DEPRECATED>
-
-=item space : any [0]
-
-B<DEPRECATED>
-
-=item style : string
-
-Style attribute for the table.  No elements of the string are interpolated.
-
-=item start_tag : any [true]
-
-If false, this widget won't render the end tag.
-
-=item width : any []
-
-Set the width of the table explicitly.
-
-=back
-
-=cut
-
-#=IMPORTS
+use Bivio::Base 'Bivio::UI::Widget';
 use Bivio::UI::HTML::ViewShortcuts;
 
-#=VARIABLES
+# C<Bivio::UI::HTML::Widget::TableBase>
+#
+#
+#
+# align : any [Table => Facade.HTML.table_default_align]
+#
+# How to align the table.  The allowed (case
+# insensitive) values are defined in
+# L<Bivio::UI::Align|Bivio::UI::Align>.
+# The value affects the C<ALIGN> and C<VALIGN> attributes of the C<TD> tag.
+# No default if I<class> is set.
+#
+# background : any
+#
+# Widget which returns image to render for background.
+#
+# bgcolor : any [] (dynamic)
+#
+# The value to be passed to the C<BGCOLOR> attribute of the C<TABLE> tag.
+# See L<Bivio::UI::Color|Bivio::UI::Color>.
+#
+# border : any [0]
+#
+# Width of border surrounding the table and its cells.
+# No default if I<class> is set.
+#
+# cellpadding : any [Table => 5, Grid => 0]
+#
+# Padding inside each cell in pixels.
+# No default if I<class> is set.
+#
+# cellspacing : any [0]
+#
+# Spacing around each cell in pixels.
+# No default if I<class> is set.
+#
+# class : any
+#
+# The html CLASS for the table.  If exists, then pad, space, cellspacing,
+# cellpadding, border, and align default to "undef".
+#
+# end_tag : any [true]
+#
+# If false, this widget won't render the start tag.
+#
+# expand : boolean [false]
+#
+# If true, the table C<WIDTH> will be C<95%> or C<100%> depending
+# on Bivio::UI::HTML.page_left_margin.
+#
+# height : any []
+#
+# Dynamic height (only IE and Netscape support this attribute).
+#
+# id : any
+#
+# The html ID for the table.
+#
+# pad : any [0]
+#
+# B<DEPRECATED>
+#
+# space : any [0]
+#
+# B<DEPRECATED>
+#
+# style : string
+#
+# Style attribute for the table.  No elements of the string are interpolated.
+#
+# start_tag : any [true]
+#
+# If false, this widget won't render the end tag.
+#
+# width : any []
+#
+# Set the width of the table explicitly.
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_HTML_ATTRS) = [qw(border cellpadding cellspacing width height style class id)];
 my($_VS) = 'Bivio::UI::HTML::ViewShortcuts';
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize_html_attrs"></a>
-
-=head2 initialize_html_attrs()
-
-Initializes above attributes.
-
-=cut
-
 sub initialize_html_attrs {
+    # (self) : undef
+    # Initializes above attributes.
     my($self, $source) = @_;
     # Grid used to use pad and space; Can't warn, because too many uses.
     foreach my $x ([pad => 'cellpadding'], [space => 'cellspacing']) {
@@ -173,30 +129,18 @@ sub initialize_html_attrs {
     return;
 }
 
-=for html <a name="render_end_tag"></a>
-
-=head2 render_end_tag(any source, string prefix) : string
-
-Renders end table tag.
-
-=cut
-
 sub render_end_tag {
+    # (self, any, string) : string
+    # Renders end table tag.
     my($self, $source) = @_;
     return ${$self->render_attr('end_tag', $source)}
 	? ($self->subclass_is_table ? "\n" : '') . "</table>"
 	: '';
 }
 
-=for html <a name="render_start_tag"></a>
-
-=head2 render_start_tag(any source) : string
-
-Returns <table tag with common attributes.
-
-=cut
-
 sub render_start_tag {
+    # (self, any) : string
+    # Returns <table tag with common attributes.
     my($self, $source) = @_;
     my($req) = $source->get_request;
     return ${$self->render_attr('start_tag', $source)}
@@ -219,28 +163,10 @@ sub render_start_tag {
 	) : '';
 }
 
-=for html <a name="subclass_is_table"></a>
-
-=head2 subclass_is_table() : boolean
-
-Returns true if subclass is table.
-
-=cut
-
 sub subclass_is_table {
+    # (self) : boolean
+    # Returns true if subclass is table.
     return ref(shift) =~ /Table$/ ? 1 : 0;
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
