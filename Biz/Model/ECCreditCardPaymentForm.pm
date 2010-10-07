aa470a853a2474610b6eb9783471fb6e4ea7e204
Paul Moeller <moeller@bivio.biz>
2010-10-07 21:17:30 +0000

    return 0 from process_payment() if in_error() minor refactoring

--- Biz/Model/ECCreditCardPaymentForm.pm
+++ Biz/Model/ECCreditCardPaymentForm.pm
@@ -5,13 +5,15 @@ use strict;
 use Bivio::Base 'Model.ConfirmableForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_D) = __PACKAGE__->use('Type.Date');
-my($_DT) = __PACKAGE__->use('Type.DateTime');
+my($_A) = b_use('Type.Amount');
+my($_D) = b_use('Type.Date');
+my($_DT) = b_use('Type.DateTime');
+my($_PM) = b_use('Type.ECPaymentMethod');
 
 sub execute_empty {
     my($self) = @_;
     $self->internal_put_field('ECCreditCardPayment.card_number' =>
-	$self->use('Type.ECCreditCardNumber')->TEST_NUMBER)
+	b_use('Type.ECCreditCardNumber')->TEST_NUMBER)
         unless $self->req->is_production;
     return unless $self->req('auth_user_id');
     $self->internal_put_field('ECCreditCardPayment.card_name' =>
@@ -61,16 +63,16 @@ sub internal_initialize {
 
 sub process_payment {
     my($proto, $form, $payment) = @_;
-    # returns 1 on success, 0 if double clicked
+    # returns 1 on success, 0 if double clicked or error
     # <payment> should contain ECPayment values (service, amount, ...)
     return 0 if _possible_double_click($proto, $form, $payment->{amount});
     $form->new_other('ECCreditCardPayment')->create({
         %{$form->get_model_properties('ECCreditCardPayment')},
         ec_payment_id => $form->new_other('ECPayment')->create({
-            point_of_sale => $proto->use('Type.ECPointOfSale')->INTERNET,
+            point_of_sale => b_use('Type.ECPointOfSale')->INTERNET,
 	    %$payment,
-            method => $proto->use('Type.ECPaymentMethod')->CREDIT_CARD,
-            status => $proto->use('Type.ECPaymentStatus')->TRY_CAPTURE,
+            method => $_PM->CREDIT_CARD,
+            status => b_use('Type.ECPaymentStatus')->TRY_CAPTURE,
         })->get('ec_payment_id'),
     });
 
@@ -78,24 +80,23 @@ sub process_payment {
     my($req) = $form->req;
     $req->with_realm($req->get('auth_user'), sub {
         Bivio::Die->catch(sub {
-            $req->use('Action.ECCreditCardProcessor')->execute_process($req);
+            b_use('Action.ECCreditCardProcessor')->execute_process($req);
             my($payment) = $req->get('Model.ECPayment');
 
-            if ($payment->get('status')->equals_by_name('DECLINED')) {
+            if ($payment->get('status')->eq_declined) {
                 $form->internal_put_error(processor_error => 'NULL');
                 $form->internal_put_field(processor_error =>
                     $payment->get_model('ECCreditCardPayment')
                         ->get('processor_response') || 'Card declined');
             }
 	    else {
-		Bivio::IO::Alert->info('credit card processed: ',
+		b_info('credit card processed: ',
 		    join(' ', $payment->get(qw(realm_id user_id)),
-			'$' . $req->use('Type.Amount')
-			    ->to_literal($payment->get('amount'))));
+			'$' . $_A->to_literal($payment->get('amount'))));
 	    }
         });
     });
-    return 1;
+    return $form->in_error ? 0 : 1;
 }
 
 sub validate {
@@ -107,27 +108,23 @@ sub validate {
 
 sub _possible_double_click {
     my($proto, $form, $amount) = @_;
-    # Returns true if the form was most likely double clicked.
-    #
     # A double click occurs if the creation_date_time of the most recent
     # payment is very new.
-    my($result) = 0;
-    $form->new_other('ECPayment')->do_iterate(sub {
+    return @{$form->new_other('ECPayment')->map_iterate(sub {
 	my($payment) = @_;
-	return 1 unless $payment->get('method')->eq_credit_card;
 
 	if ($_DT->compare($payment->get('creation_date_time'),
 	    # one minute ago
-	    $_DT->add_seconds($_DT->now, -1 * 60)) > 0
-	    && $payment->get('amount') == $amount) {
+	    $_DT->add_seconds($_DT->now, -1 * 60)) > 0) {
 	    $form->req->warn('ignoring OK, possible double-click');
-	    $result = 1;
+	    return 1;
 	}
-	return 0;
+	return ();
     }, 'iterate_start', 'creation_date_time DESC', {
+	amount => $amount,
 	user_id => $form->req('auth_user_id'),
-    });
-    return $result;
+	method => $_PM->CREDIT_CARD,
+    })} ? 1 : 0;
 }
 
 sub _validate_credit_card_expiration {
@@ -139,17 +136,12 @@ sub _validate_credit_card_expiration {
     my($month) = $self->get('card_exp_month')->as_int;
     my($year) = $self->get('card_exp_year')->as_int;
     my($day) = $_D->get_last_day_in_month($month, $year);
-
     my($exp_date, $err) = $_D->date_from_parts($day, $month, $year);
-
-    Bivio::Die->throw_die('DIE', {
-	message => 'unable to convert expiration date: '.$err->get_short_desc,
-	entity => [$day, $month, $year],
-    }) unless $exp_date;
-
+    b_die('unable to convert expiration date: ' . $err->get_short_desc)
+	unless $exp_date;
     $self->internal_put_field('ECCreditCardPayment.card_expiration_date' =>
 	$exp_date);
-    $self->internal_put_error('ECCreditCardPayment.card_expiration_date',
+    $self->internal_put_error('ECCreditCardPayment.card_expiration_date' =>
 	'CREDITCARD_EXPIRED')
 	if $_D->compare($exp_date, $_D->local_today) < 0;
     return;
@@ -160,7 +152,7 @@ sub _validate_credit_card_type {
     return if $self->get_field_error('ECCreditCardPayment.card_number');
     $self->internal_put_error('ECCreditCardPayment.card_number' =>
         'CREDITCARD_UNSUPPORTED_TYPE')
-	unless $self->use('Type.ECCreditCardType')->is_supported_by_number(
+	unless b_use('Type.ECCreditCardType')->is_supported_by_number(
 	    $self->get('ECCreditCardPayment.card_number'));
     return;
 }
