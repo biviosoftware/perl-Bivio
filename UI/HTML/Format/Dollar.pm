da1b29351887cc27d0d614789b7abfe689c23bf2
Rob Nagler <github@q33.us>
2010-10-14 23:07:56 +0000

    fmt

--- UI/HTML/Format/Dollar.pm
+++ UI/HTML/Format/Dollar.pm
@@ -1,18 +1,12 @@
-# Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2004-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::HTML::Format::Dollar;
 use strict;
-use Bivio::Base 'Bivio::UI::HTML::Format::Amount';
-
-# C<Bivio::UI::HTML::Format::Dollar>
+use Bivio::Base 'HTMLFormat.Amount';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_widget_value {
-    # (proto, string, int, boolean, boolean) : string
-    # Formats like Amount but with leading $.  See
-    # L<Bivio::UI::HTML::Format::Amount|Bivio::UI::HTML::Format::Amount>
-    # for arguments.
     my($self, $amount) = @_;
     return '$' . shift->SUPER::get_widget_value(@_);
 }
