22c26979ef92fb037b24280ab44c0b4bbd632d98
Rob Nagler <github@q33.us>
2010-10-14 23:08:19 +0000

    rmpod

--- UI/HTML/Widget/DollarCell.pm
+++ UI/HTML/Widget/DollarCell.pm
@@ -2,69 +2,25 @@
 # $Id$
 package Bivio::UI::HTML::Widget::DollarCell;
 use strict;
-$Bivio::UI::HTML::Widget::DollarCell::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::UI::HTML::Widget::DollarCell::VERSION;
+use Bivio::Base 'Bivio::UI::HTML::Widget::String';
 
-=head1 NAME
+# C<Bivio::UI::HTML::Widget::DollarCell> formats a cell with a dollar amount preceded by a dollar sign.  Sets the font to C<NUMBER_CELL>, alignment is C<RIGHT>.
+#
+#
+#
+# field : string (required)
+#
+# Name of the field to render.
+#
+# pad_left : int [1]
+#
+# Number of spaces to pad to left (same as String's pad_left).
 
-Bivio::UI::HTML::Widget::DollarCell - formats with Dollar formatter
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::UI::HTML::Widget::DollarCell;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::UI::HTML::Widget::String>
-
-=cut
-
-use Bivio::UI::HTML::Widget::String;
-@Bivio::UI::HTML::Widget::DollarCell::ISA = ('Bivio::UI::HTML::Widget::String');
-
-=head1 DESCRIPTION
-
-C<Bivio::UI::HTML::Widget::DollarCell> formats a cell with a dollar amount preceded by a dollar sign.  Sets the font to C<NUMBER_CELL>, alignment is C<RIGHT>.
-
-=head1 ATTRIBUTES
-
-=over 4
-
-=item field : string (required)
-
-Name of the field to render.
-
-=item pad_left : int [1]
-
-Number of spaces to pad to left (same as String's pad_left).
-
-=back
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="initialize"></a>
-
-=head2 initialize()
-
-Initializes String attributes.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub initialize {
+    # (self) : undef
+    # Initializes String attributes.
     my($self) = shift;
     return if $self->unsafe_get('value');
     $self->put(
@@ -80,15 +36,9 @@ sub initialize {
     return $self->SUPER::initialize(@_);
 }
 
-=for html <a name="internal_new_args"></a>
-
-=head2 internal_new_args(any arg, ...) : hash_ref
-
-Converts positional to hash notation.
-
-=cut
-
 sub internal_new_args {
+    # (self, any, ...) : hash_ref
+    # Converts positional to hash notation.
     my(undef, $field, $attributes) = @_;
     return '"field" attribute must be defined'
 	unless defined($field);
@@ -97,17 +47,5 @@ sub internal_new_args {
 	($attributes ? %$attributes : ()),
     };
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2004 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
