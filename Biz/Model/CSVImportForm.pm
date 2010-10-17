fe7d100cad93a12e592959e3bbcbd908f26ceff6
Rob Nagler <github@q33.us>
2010-10-17 22:11:50 +0000

    internal_source_error takes @args look up some of the text (detail_prefix) in Facade internal_put_error_and_detail call_super_before If enum is empty, return NULL, not not found, only if constraint is NOT_NULL

--- Biz/Model/CSVImportForm.pm
+++ Biz/Model/CSVImportForm.pm
@@ -2,14 +2,14 @@
 # $Id$
 package Bivio::Biz::Model::CSVImportForm;
 use strict;
-use Bivio::Base 'Bivio::Biz::FormModel';
-use Bivio::Util::CSV;
+use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_C) = __PACKAGE__->use('SQL.Constraint');
-my($_T) = __PACKAGE__->use('Bivio::Type');
-my($_FF) = __PACKAGE__->use('Type.FileField');
+my($_C) = b_use('SQL.Constraint');
+my($_T) = b_use('Bivio::Type');
+my($_FF) = b_use('Type.FileField');
 my($_CONFIG) = {};
+my($_A) = b_use('IO.Alert');
 
 # subclasses must define COLUMNS with the expected format:
 #
@@ -39,18 +39,23 @@ sub execute_ok {
 	unless my $rows = _parse_rows($self);
     return $self->internal_source_error(undef, 'No data rows found')
         unless @$rows;
-    return unless _validate_columns($self, $rows->[0], $columns);
+    return
+	unless _validate_columns($self, $rows->[0], $columns);
     my($count) = 1;
     foreach my $row (@$rows) {
 	_validate_record($self, $row, $columns, $count++);
-	next if $self->CONTINUE_VALIDATION_ON_ERROR;
-	return if $self->in_error;
+	next
+	    if $self->CONTINUE_VALIDATION_ON_ERROR;
+	return
+	    if $self->in_error;
     }
-    return if $self->in_error;
+    return
+	if $self->in_error;
     $count = 1;
     foreach my $row (@$rows) {
 	$self->process_record($row, $count++);
-	return if $self->in_error;
+	return
+	    if $self->in_error;
     }
     return;
 }
@@ -61,9 +66,11 @@ sub internal_import_error {
 }
 
 sub internal_source_error {
-    my($self, $row_count, $text) = @_;
-    my($ed) = (defined($row_count) ? ('Record ' . $row_count . ': ') : '')
-	. $text;
+    my($self, $row_count, @args) = @_;
+    my($ed) = $_A->format_args(
+	defined($row_count) ? ('Record ', $row_count, ': ') : (),
+	@args,
+    );
     my($oed) = $self->get_field_error_detail('source');
     $self->internal_put_error_and_detail(
 	source => 'SYNTAX_ERROR',
@@ -95,8 +102,13 @@ sub internal_initialize {
 
 sub internal_pre_execute {
     my($self, $method) = @_;
-    $self->internal_put_field(import_errors => '');
-    return;
+    return shift->call_super_before(
+	\@_,
+	sub {
+	    $self->internal_put_field(import_errors => '');
+	    return;
+	},
+    );
 }
 
 sub process_content {
@@ -110,7 +122,7 @@ sub record_to_model_properties {
     my($self, $row, $model) = @_;
     return {map({
 	my($i) = $self->column_info($_);
-	$i && $i->{field} =~ /^$model\.(\w+)$/ ? ($1 => $row->{$_}) : ();
+	$i && $i->{field} =~ /^$model\.(\w.*)/ ? ($1 => $row->{$_}) : ();
     }
 	keys(%$row),
     )};
@@ -129,7 +141,7 @@ sub _config_init {
 	Bivio::Die->die($name, ': duplicate name')
 	    if $seen->{$name}++;
 	$name = lc($name);
-	if (my($model, $property) = $type =~ /(\w+)\.(\w+)/) {
+	if (my($model, $property) = $type =~ /(\w+)\.(\w.*)/) {
 	    $field ||= $type;
 	    $model = $self->get_instance($model);
 	    $type = $model->get_field_type($property);
@@ -153,14 +165,15 @@ sub _parse_rows {
     my($self) = @_;
     my($rows);
     my($die) = Bivio::Die->catch(sub {
-        $rows = Bivio::Util::CSV->parse(
+        $rows = b_use('ShellUtil.CSV')->parse(
 	    $self->get('source')->{content},
 	);
 	return;
     });
-    return $self->internal_source_error(undef, 'Invalid CSV file'
-	. ($die->get('attrs')->{message}
-	    ? (': ' . $die->get('attrs')->{message}) : ''),
+    return $self->internal_source_error(
+	undef,
+	'Invalid CSV file',
+	$die->get('attrs')->{message} ? (': ', $die->get('attrs')->{message}) : (),
     ) if $die;
     return $rows
 	unless $rows && @$rows;
@@ -180,35 +193,50 @@ sub _parse_rows {
 sub _validate_columns {
     my($self, $row, $columns) = @_;
     my($headings) = [keys(%$columns)];
-    return grep(exists($row->{$_}), @$headings) == @$headings ? 1
+    return grep(exists($row->{$_}), @$headings) == @$headings
+	? 1
 	: $self->internal_source_error(
 	    undef,
-	    'Missing column(s): '
-		. join(',', grep(!exists($row->{$_}), @$headings)),
+	    'Missing column(s): ',
+	        [grep(!exists($row->{$_}), @$headings)],
 	);
 }
 
 sub _validate_record {
     my($self, $row, $columns, $count) = @_;
     foreach my $name (keys(%$columns)) {
-	my($type) = Bivio::Type->get_instance($columns->{$name}->{type});
+	my($type) = $columns->{$name}->{type};
 	my($v, $err);
-	if ($type->isa('Bivio::Type::Enum') && $row->{$name}) {
+	if ($type->isa('Bivio::Type::Enum') && defined($row->{$name})) {
 	    $row->{$name} =~ s/^\s+|\s+$//g;
-	    $v = $type->unsafe_from_any($row->{$name});
-	    $err = Bivio::TypeError->NOT_FOUND unless $v;
+	    if (length($row->{$name})) {
+		$v = $type->unsafe_from_any($row->{$name});
+		$err = Bivio::TypeError->NOT_FOUND
+		    unless $v;
+	    }
 	}
 	else {
 	    ($v, $err) = $type->from_literal($row->{$name});
 	}
 	if ($err) {
-	    $self->internal_source_error($count,
-		$name . ': ' . $row->{$name} . ', ' . $err->get_short_desc)
+	    $self->internal_source_error(
+		$count,
+		$name,
+		': ',
+		defined($row->{$name}) && length($row->{$name})
+		    ? ($row->{$name}, ', ')
+		    : (),
+		$err->get_short_desc,
+	    );
 	}
 	# is the field required?
 	elsif (my $e = $columns->{$name}->{constraint}->check_value($type, $v)) {
 	    $self->internal_source_error(
-		$count, "${name}: " . $e->get_long_desc);
+		$count,
+		$name,
+		': ',
+		$e->get_long_desc,
+	    );
 	}
 	$row->{$name} = $v;
     }
