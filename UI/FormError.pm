2e78020af26f7c0f34adb76f5ae38dd70113f626
Rob Nagler <github@q33.us>
2010-10-17 22:17:19 +0000

    detail can be a widget (FormModel doesn't care) FormError returns a widget to_html => to_widget_value

--- UI/FormError.pm
+++ UI/FormError.pm
@@ -3,27 +3,35 @@
 package Bivio::UI::FormError;
 use strict;
 use Bivio::Base 'FacadeComponent.Text';
+use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($_IN_EVAL);
+my($_D) = b_use('Bivio.Die');
+my($_T) = b_use('FacadeComponent.Text');
+my($_W) = b_use('UI.Widget');
 
 sub field_value {
     my(undef, $item) = @_;
-    Bivio::Die->die($item, ': FormError not evaluating')
+    $_D->die($item, ': FormError not evaluating')
         unless $_IN_EVAL;
-    Bivio::Die->die($item, ': no such FormError attribute')
+    $_D->die($item, ': no such FormError attribute')
 	unless exists($_IN_EVAL->{$item});
     return $_IN_EVAL->{$item};
 }
 
-sub to_html {
+sub to_widget_value {
     my($proto, $source, $form, $field, $label, $error) = @_;
     my($self) = $proto->internal_get_self($source->get_request);
-    my($form_class) = ref($form) || $form;
     $error ||= $form->get_field_error($field);
     my($detail) = $form->get_field_error_detail($field);
+    $detail = String($detail, {hard_newlines => 1})
+	if $detail && !$_W->is_blessed($detail);
     my($v) = $self->unsafe_get_value(
-	$form_class->simple_package_name, $field, $error->get_name);
+	$form->simple_package_name,
+	$field,
+	$error->get_name,
+    );
     if (defined($v)) {
 	my($buf) = '';
 	local($_IN_EVAL) = {
@@ -31,24 +39,25 @@ sub to_html {
 	    error => $error,
 	    detail => $detail,
 	};
-	my($die) = Bivio::Die->catch(
+	my($die) = $_D->catch(
 	    sub {
-	        $proto->use('Bivio::UI::ViewShortcuts')
-		    ->vs_call('Prose', $v)
+		Prose($v)
 		    ->initialize_with_parent(undef)
 		    ->render($source, \$buf);
 		return;
             },
 	);
-	return $buf
+	return Simple($buf)
 	    unless $die;
-	Bivio::IO::Alert->warn(
-	    'Error interpolating: ', $v, ': ', $die);
+	b_warn('Error interpolating: ', $v, ': ', $die);
     }
-    my($str) = Bivio::HTML->escape($error->get_long_desc
-	. ($detail ? "; additional info: $detail" : ''));
-    $str =~ s{\n}{<br />}mg;
-    return $str;
+    return Join([
+#TODO: use Enum widget (can't now, b/c defaults to short_desc)
+	String($error->get_long_desc),
+	$detail
+	    ? ($_T->get_widget_value('FormError.prose.detail_prefix'), $detail)
+	    : (),
+    ]);
 }
 
 1;
