72fc593b59ac74488b6cc1dde6c57a68ac51c134
Paul Moeller <moeller@bivio.biz>
2010-10-22 22:51:08 +0000

    initial revision

---
+++ UI/HTML/Widget/CopyListValueHandler.pm
@@ -0,0 +1,31 @@
+# Copyright (c) 2010 bivio Software Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::HTML::Widget::CopyListValueHandler;
+use strict;
+use Bivio::Base 'UI.Widget';
+my($_JS) = b_use('HTMLWidget.JavaScript');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub get_html_field_attributes {
+    my($self, $field_name, $source) = @_;
+    return ' onfocus="cr_focus(this)"';
+}
+
+sub render {
+    my($self, $source, $buffer) = @_;
+    $_JS->render($source, $buffer, $self->package_name, <<'EOF');
+function cr_focus(c) {
+  if (c.value)
+    return;
+  var match = ner_match_name(c.name);
+  var prev = document.getElementsByName(match[1] + (parseInt(match[2]) - 1));
+  if (prev.length)
+    c.value = prev[0].value;
+  return;
+}
+EOF
+    return;
+}
+
+1;
