df99446bc5c30c10197e04613451760125ec47a1
Rob Nagler <github@q33.us>
2010-10-28 18:07:18 +0000

    rmpod

--- Auth/Permission.pm
+++ Auth/Permission.pm
@@ -2,91 +2,39 @@
 # $Id$
 package Bivio::Auth::Permission;
 use strict;
-$Bivio::Auth::Permission::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Auth::Permission::VERSION;
+use Bivio::Base 'Bivio::Type::Enum';
 
-=head1 NAME
+# C<Bivio::Auth::Permission> is used to specify a task's access
+# permissions.  See L<Bivio::Agent::Task|Bivio::Agent::Task> for
+# how the permissions are used.
+#
+#
+# See L<Bivio::Agent::TaskId|Bivio::Agent::TaskId> and
+# L<Bivio::Delegate::SimpleTaskId|Bivio::Delegate::SimpleTaskId>
+# for how you define permissions on tasks.
+#
+# See L<Bivio::Agent::Task|Bivio::Agent::Task> and
+# L<Bivio::Delegate::SimpleAuthSupport|Bivio::Delegate::SimpleAuthSupport>
+# for how permissions are checked.
+#
+# See L<Bivio::Biz::Model::RealmRole|Bivio::Biz::Model::RealmRole>
+# and L<Bivio::Agent::Request|Bivio::Agent::Request> for how users
+# are assigned roles in realms.  This table must be configured if
+# you want to use permissions.
+#
+# You can avoid the use of permissions by delegating
+# L<Bivio::Auth::Support|Bivio::Auth::Support> to
+# L<Bivio::Delegate::NoDbAuthSupport|Bivio::Delegate::NoDbAuthSupport>.
 
-Bivio::Auth::Permission - used to define access requirements for tasks
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Auth::Permission;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::Enum>
-
-=cut
-
-use Bivio::Type::Enum;
-@Bivio::Auth::Permission::ISA = ('Bivio::Type::Enum');
-
-=head1 DESCRIPTION
-
-C<Bivio::Auth::Permission> is used to specify a task's access
-permissions.  See L<Bivio::Agent::Task|Bivio::Agent::Task> for
-how the permissions are used.
-
-
-See L<Bivio::Agent::TaskId|Bivio::Agent::TaskId> and
-L<Bivio::Delegate::SimpleTaskId|Bivio::Delegate::SimpleTaskId>
-for how you define permissions on tasks.
-
-See L<Bivio::Agent::Task|Bivio::Agent::Task> and
-L<Bivio::Delegate::SimpleAuthSupport|Bivio::Delegate::SimpleAuthSupport>
-for how permissions are checked.
-
-See L<Bivio::Biz::Model::RealmRole|Bivio::Biz::Model::RealmRole>
-and L<Bivio::Agent::Request|Bivio::Agent::Request> for how users
-are assigned roles in realms.  This table must be configured if
-you want to use permissions.
-
-You can avoid the use of permissions by delegating
-L<Bivio::Auth::Support|Bivio::Auth::Support> to
-L<Bivio::Delegate::NoDbAuthSupport|Bivio::Delegate::NoDbAuthSupport>.
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile(
 	Bivio::IO::ClassLoader->delegate_require_info(__PACKAGE__));
 
-=head1 METHODS
-
-=cut
-
-=for html <a name="is_continuous"></a>
-
-=head2 static is_continuous() : false
-
-Permissions aren't continuous, because they may go away or have
-gaps across delegate aggregations.
-
-=cut
-
 sub is_continuous {
+    # (proto) : false
+    # Permissions aren't continuous, because they may go away or have
+    # gaps across delegate aggregations.
     return 0;
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 1999-2001 bivio Software, Inc.  All rights reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
