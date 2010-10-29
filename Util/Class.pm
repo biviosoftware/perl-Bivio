3099cff7ce00a5009fc2855e5be1b86c27efb855
Rob Nagler <github@q33.us>
2010-10-29 17:39:30 +0000

    removed tasks_for_label and tasks_for_view: not in use, and too tightly coupled implementation with TaskId which is changing

--- Util/Class.pm
+++ Util/Class.pm
@@ -16,8 +16,6 @@ commands
   info class -- return information about the class
   qualified_name class -- return fully qualified name for class
   super package -- return the list of superclasses for given package
-  tasks_for_label text -- find TaskIds from text in Facade.Text map
-  tasks_for_view view -- find TaskIds from view name (View.<view name>)
 EOF
 }
 
@@ -31,24 +29,6 @@ sub info {
     return ${\${$pkg . '::VERSION'}} . ' ' . $INC{$file};
 }
 
-sub internal_initialize_index {
-    my($self) = @_;
-    #TODO: BAD PROGRAMMER!  TaskId docs say only Tasks can call get_cfg_list
-    my @tasks = @{Bivio::Agent::TaskId->get_cfg_list};
-    my($fields) = $self->[$_IDI] ||= {
-	view => {},
-	task => {},
-    };
-    foreach my $task (@tasks) {
-	$fields->{task}->{$task->[0]} = $task;
-	my($view) = grep(/^View\./, @$task)
-	    or next;
-	$view =~ s/^View\.//;
-	push(@{($fields->{view}->{$view} ||= [])}, $task);
-    }
-    return;
-}
-
 sub qualified_name {
     my($self, $name) = @_;
     return $self->use($name);
@@ -57,32 +37,6 @@ sub qualified_name {
 sub super {
     my($self, $package) = @_;
     return $self->use($package)->inheritance_ancestors;
-}
-
-sub tasks_for_label {
-    my($self, $text) = @_;
-    my($req) = $self->get_request;
-    $req->initialize_fully;
-    $self->internal_initialize_index;
-    my($fields) = $self->[$_IDI];
-    #TODO: BAD PROGRAMMER!  No hacking the internal data structures!
-    #  does this use case justify exposing a reverse lookup on Facade?
-    my($task_text) = $req->get_nested(qw(Bivio::UI::Facade Text))
-	->[1]->{map};
-    return [map($fields->{task}->{$_},
-		map(@{$task_text->{$_}->{names}},
-		grep({
-		    defined($task_text->{$_})
-			&& lc($task_text->{$_}->{value}) eq lc($text)
-		} keys(%$task_text))))];
-}
-
-sub tasks_for_view {
-    my($self, $view) = @_;
-    $self->internal_initialize_index;
-    my($fields) = $self->[$_IDI];
-#    return [keys(%{$fields->{view}})];
-    return $fields->{view}->{$view};
 }
 
 1;
