d07832599d2769aa86dbe0a5c60a68c300d6bc66
Rob Nagler <github@q33.us>
2010-11-17 02:30:37 +0000

    fmt

--- Type/Text.pm
+++ Type/Text.pm
@@ -1,19 +1,12 @@
-# Copyright (c) 1999-2009 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::Text;
 use strict;
 use Bivio::Base 'Type.Line';
-use Bivio::TypeError;
-
-# C<Bivio::Type::Text> defines a complex text string to be stored
-# in the database, e.g. a remark or a URL.  This is the "maximum"
-# size string we allow in the database for our purposes.
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_width {
-    # : int
-    # Returns 500.
     return 500;
 }
 
