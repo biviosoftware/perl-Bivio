93fc66d8eea0c03e2b3261212fc21e458454a2f6
Rob Nagler <github@q33.us>
2010-11-17 02:29:48 +0000

    from_literal calls canonicalize_charset

--- Type/Line.pm
+++ Type/Line.pm
@@ -1,35 +1,19 @@
-# Copyright (c) 1999-2009 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Type::Line;
 use strict;
 use Bivio::Base 'Type.String';
 
-# C<Bivio::Type::Line> defines a compound name or long line of text, e.g.
-# a person's full name, an e-mail address, and an account name.
-# If you want
-# a simple name, e.g.
-# first name, use L<Bivio::Type::Name|Bivio::Type::Name>.
-#
-# Note: leading and trailing spaces are trimmed in
-# L<from_literal|"from_literal">.
-
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub from_literal {
-    # (proto, string) : any
-    # Returns C<undef> if the line is empty.
-    # Leading and trailing blanks are trimmed.
-    # Length is checked.
     my($proto, $value) = @_;
-    # Leave middle spaces in case a "display" name.
-    $value =~ s/^\s+|\s+$//g
+    $value = ${$proto->canonicalize_charset($value)}
 	if defined($value);
     return $proto->SUPER::from_literal($value);
 }
 
 sub get_width {
-    # : int
-    # Returns 100.
     return 100;
 }
 
