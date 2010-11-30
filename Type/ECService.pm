00d5ad31ab0552e1916e3e0d503e3ed7b57166e8
Rob Nagler <github@q33.us>
2010-11-30 18:11:15 +0000

    rmpod

--- Type/ECService.pm
+++ Type/ECService.pm
@@ -2,61 +2,15 @@
 # $Id$
 package Bivio::Type::ECService;
 use strict;
-$Bivio::Type::ECService::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Type::ECService::VERSION;
+use Bivio::Base 'Bivio::Type::EnumDelegator';
 
-=head1 NAME
+# C<Bivio::Type::ECService>
 
-Bivio::Type::ECService - enum of EC services
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Type::ECService;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Type::EnumDelegator>
-
-=cut
-
-use Bivio::Type::EnumDelegator;
-@Bivio::Type::ECService::ISA = ('Bivio::Type::EnumDelegator');
-
-=head1 DESCRIPTION
-
-C<Bivio::Type::ECService>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile;
-
-=head1 METHODS
-
-=cut
 
 sub is_continuous {
     return 0;
 }
-
-#=PRIVATE SUBROUTINES
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
