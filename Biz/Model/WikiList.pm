c401c78a16862a4b61498fbee757f835f064e174
Rob Nagler <github@q33.us>
2010-12-03 17:41:07 +0000

    call prepare_statement_for_access_mode

--- Biz/Model/WikiList.pm
+++ Biz/Model/WikiList.pm
@@ -6,26 +6,14 @@ use Bivio::Base 'Model.RealmFileList';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_WN) = b_use('Type.WikiName');
-my($_UNSPECIFIED) = b_use('Type.PrimaryId')->UNSPECIFIED_VALUE;
 
 sub internal_pre_load {
     return '';
 }
 
 sub internal_prepare_statement {
-    my($self, $stmt, $query) = @_;
-    # {path_info => $type->to_absolute(undef, $public)},
-    my($rf) = $self->new_other('RealmFile');
-    my($in) = [map(
-	$rf->unauth_load({
-	    path => $_WN->to_absolute(undef, $_),
-	    realm_id => $query->unsafe_get('auth_id'),
-	}) ? $rf->get('realm_file_id') : (),
-	0, 1,
-    )];
-    $stmt->where(
-	$stmt->IN('RealmFile.folder_id', @$in ? $in : [$_UNSPECIFIED]),
-    );
+    my($self, $stmt) = @_;
+    $self->prepare_statement_for_access_mode($stmt, $_WN);
     return shift->SUPER::internal_prepare_statement(@_);
 }
 
