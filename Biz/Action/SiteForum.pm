eb6f40f7d4530754c79ef2823c737bda7155efb1
Rob Nagler <github@q33.us>
2010-12-14 20:45:08 +0000

    b_use

--- Biz/Action/SiteForum.pm
+++ Biz/Action/SiteForum.pm
@@ -1,11 +1,11 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Action::SiteForum;
 use strict;
 use Bivio::Base 'Biz.Action';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_C) = __PACKAGE__->use('FacadeComponent.Constant');
+my($_C) = b_use('FacadeComponent.Constant');
 
 sub execute {
     my($proto, $req) = @_;
