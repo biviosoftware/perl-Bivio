73674aee8b4a29c24ed1be4fab9047be05bffa26
Rob Nagler <github@q33.us>
2010-12-25 19:29:28 +0000

    doc

--- Delegate/RealmType.pm
+++ Delegate/RealmType.pm
@@ -1,26 +1,8 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Delegate::RealmType;
 use strict;
 use Bivio::Base 'Bivio.Delegate';
-
-# C<Bivio::Delegate::RealmType> implements the common RealmTypes in bOP.
-#
-# You should extend this class if you have new RealmTypes in your application.
-# The numbers 0-19 are reserved by this module so your first RealmType would
-# look like:
-#
-#     sub get_delegate_info {
-# 	my($proto) = @_;
-# 	return [
-#             @{$proto->SUPER::get_delegate_info},
-# 	    MY_NEW_TYPE => [
-# 	        20,
-# 		undef,
-# 		'access to some new type of realm',
-# 	    ],
-#         ];
-#     }
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
--- Delegate/SimpleLocation.pm
+++ Delegate/SimpleLocation.pm
@@ -1,17 +1,17 @@
-# Copyright (c) 2002-2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Delegate::SimpleLocation;
 use strict;
-use Bivio::Base 'Bivio::Delegate';
+use Bivio::Base 'Bivio.Delegate';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_delegate_info {
-    # Returns HOME.
     return [
 	HOME => [1],
 	WORK => [2],
 	MOBILE => [3],
+	# START at 21
     ];
 }
 
