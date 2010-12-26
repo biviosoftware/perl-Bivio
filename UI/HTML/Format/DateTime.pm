96916edb3f4c150789376b2f052bc9b4cf911816
Rob Nagler <github@q33.us>
2010-12-26 00:33:47 +0000

    added HOUR_MINUTE_AM_PM_LC and FULL_MONTH_DAY_AND_YEAR refactored to be clearer what's going on

--- UI/HTML/Format/DateTime.pm
+++ UI/HTML/Format/DateTime.pm
@@ -1,10 +1,8 @@
-# Copyright (c) 1999-2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::HTML::Format::DateTime;
 use strict;
-use base 'Bivio::UI::HTML::Format';
-use Bivio::Type::DateTime;
-use Bivio::UI::DateTimeMode;
+use Bivio::Base 'UIHTML.Format';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_MONTHS) = [qw(
@@ -22,55 +20,112 @@ my($_MONTHS) = [qw(
     November
     December
 )];
-my($_DT) = Bivio::Type->get_instance('DateTime');
+my($_DT) = b_use('Type.DateTime');
+my($_DTM) = b_use('UI.DateTimeMode');
+my($_DTWTZ) = b_use('Type.DateTimeWithTimeZone');
 
 sub get_widget_value {
-    my(undef, $time, $mode, $no_timezone) = @_;
+    my(undef, $dt, $mode, $no_timezone) = @_;
     return ''
-	unless defined($time);
-    $mode = defined($mode) ? Bivio::UI::DateTimeMode->from_any($mode)
-	: Bivio::UI::DateTimeMode->get_default;
-    return $_DT->rfc822($time)
-	if $mode->eq_rfc822;
-    my($sec, $min, $hour, $mday, $mon, $year) = $_DT->to_parts($time);
-    my($m) = $mode->as_int;
-    # ASSUMES: Bivio::UI::DateTimeMode is DATE=1, TIME=2 & DATE_TIME=3
-    return (
-	$m <= 3 ? (
-	    (($m & 1) ? sprintf('%02d/%02d/%04d', $mon, $mday, $year) : '')
-	    . ($m == 3 ? ' ' : '')
-	    . (($m & 2) ? sprintf('%02d:%02d:%02d', $hour, $min, $sec) : '')
-        ) : $mode->eq_month_name_and_day_number ? "$_MONTHS->[$mon] $mday"
-	: $mode->eq_full_month_day_and_year_uc
-	    ? uc($_MONTHS->[$mon]) . " $mday, $year"
-	: $mode->eq_full_month_and_year_uc ? uc($_MONTHS->[$mon]) . ", $year"
-	: $mode->eq_full_month ? $_MONTHS->[$mon]
-	: $mode->eq_month_and_day ? sprintf('%02d/%02d', $mon, $mday)
-	: $mode->eq_day_month3_year
-	    ? sprintf('%02d-%.3s-%04d', $mday, $_MONTHS->[$mon], $year)
-	: $mode->eq_day_month3_year_time
-	    ? sprintf('%02d-%.3s-%04d %02d:%02d:%02d',
-		      $mday, $_MONTHS->[$mon], $year, $hour, $min, $sec)
-	: $mode->eq_day_month3_year_time_period
-	    ? sprintf('%02d-%.3s-%04d %02d:%02d:%02d %.2s',
-		      $mday, $_MONTHS->[$mon], $year,
-		      _to_twelve_hour($hour), $min, $sec, _period($hour))
-        : Bivio::Die->throw_die('DIE', {
-	    message => 'unhandled DateTimeMode',
-	    entity => $mode
-	})) . ($no_timezone ? '': ' GMT');
+	unless defined($dt);
+    $dt = $_DTWTZ->as_date_time($dt)
+	if $_DTWTZ->is_blessed($dt);
+    $mode = defined($mode) ? $_DTM->from_any($mode) : $_DTM->get_default;
+    my($op) = '_to_' . lc($mode->get_name);
+    return (\&{$op})->($_DT->to_parts($dt), $dt) . ($no_timezone ? '': ' GMT');
 }
 
-sub _period {
+sub _am_pm {
     my($hour) = @_;
     return $hour > 11 && $hour < 24 ? 'PM' : 'AM';
 }
 
-sub _to_twelve_hour {
+sub _to_date {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf('%02d/%02d/%04d', $mon, $mday, $year);
+}
+
+sub _to_date_time {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf('%02d/%02d/%04d %02d:%02d:%02d', $mon, $mday, $year, $hour, $min, $sec);
+}
+
+sub _to_day_month3_year {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf('%02d-%.3s-%04d', $mday, $_MONTHS->[$mon], $year);
+}
+
+sub _to_day_month3_year_time {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf(
+	'%02d-%.3s-%04d %02d:%02d:%02d',
+	$mday, $_MONTHS->[$mon], $year, $hour, $min, $sec,
+    );
+}
+
+sub _to_day_month3_year_time_period {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf(
+	'%02d-%.3s-%04d %02d:%02d:%02d %.2s',
+	$mday,
+	$_MONTHS->[$mon],
+	$year,
+	_twelve_hour($hour),
+	$min,
+	$sec,
+	_am_pm($hour),
+    );
+}
+
+sub _to_full_month {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return $_MONTHS->[$mon];
+}
+
+sub _to_full_month_and_year_uc {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return uc($_MONTHS->[$mon]) . ", $year";
+}
+
+sub _to_full_month_day_and_year {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return $_MONTHS->[$mon] . " $mday, $year";
+}
+
+sub _to_full_month_day_and_year_uc {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return uc($_MONTHS->[$mon]) . " $mday, $year";
+}
+
+sub _to_hour_minute_am_pm_lc {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    ($min, $hour) = $_DT->get_parts($_DT->add_seconds($dt, 30), 'minute', 'hour');
+    return sprintf('%02d:%02d %s', _twelve_hour($hour), $min, lc(_am_pm($hour)));
+}
+
+sub _to_month_and_day {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf('%02d/%02d', $mon, $mday);
+}
+
+sub _to_month_name_and_day_number {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return "$_MONTHS->[$mon] $mday";
+}
+
+sub _to_rfc822 {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return $_DT->rfc822($dt);
+}
+
+sub _to_time {
+    my($sec, $min, $hour, $mday, $mon, $year, $dt) = @_;
+    return sprintf('%02d:%02d:%02d', $hour, $min, $sec);
+}
+
+sub _twelve_hour {
     my($hour) = @_;
-    return $hour > 0 && $hour < 13 ? $hour
-	: $hour == 0 ? 12
-	    : $hour - 12;
+    return $hour == 0 ? 12 : $hour > 12 ? $hour - 12 : $hour;
 }
 
 1;
