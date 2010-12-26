7099010b5078299162299bdae0add318c0286133
Rob Nagler <github@q33.us>
2010-12-26 00:34:05 +0000

    added HOUR_MINUTE_AM_PM_LC and FULL_MONTH_DAY_AND_YEAR simplified compile() statement

--- UI/DateTimeMode.pm
+++ UI/DateTimeMode.pm
@@ -1,25 +1,27 @@
-# Copyright (c) 1999-2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 1999-2010 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::DateTimeMode;
 use strict;
-use base 'Bivio::Type::Enum';
+use Bivio::Base 'Type.Enum';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 __PACKAGE__->compile([
     # DO NOT CHANGE THESE NUMBERS
     # unless you modify UI::HTML::Widget::DateTime
-    DATE => [1],
-    TIME => [2],
-    DATE_TIME => [3],
-    MONTH_NAME_AND_DAY_NUMBER => [4],
-    MONTH_AND_DAY => [5],
-    FULL_MONTH_DAY_AND_YEAR_UC => [6],
-    FULL_MONTH_AND_YEAR_UC => [7],
-    FULL_MONTH => [8],
-    DAY_MONTH3_YEAR => [9],
-    DAY_MONTH3_YEAR_TIME => [10],
-    RFC822 => [11],
-    DAY_MONTH3_YEAR_TIME_PERIOD => [12],
+    DATE => 1,
+    TIME => 2,
+    DATE_TIME => 3,
+    MONTH_NAME_AND_DAY_NUMBER => 4,
+    MONTH_AND_DAY => 5,
+    FULL_MONTH_DAY_AND_YEAR_UC => 6,
+    FULL_MONTH_AND_YEAR_UC => 7,
+    FULL_MONTH => 8,
+    DAY_MONTH3_YEAR => 9,
+    DAY_MONTH3_YEAR_TIME => 10,
+    RFC822 => 11,
+    DAY_MONTH3_YEAR_TIME_PERIOD => 12,
+    FULL_MONTH_DAY_AND_YEAR => 13,
+    HOUR_MINUTE_AM_PM_LC => 14,
 ]);
 Bivio::IO::Config->register(my $_CFG = {
     default => __PACKAGE__->DATE_TIME,
