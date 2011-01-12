4f0e978b49d2f3a7b91382ee5172814845685183
Rob Nagler <github@q33.us>
2011-01-12 03:15:51 +0000

    added realms_for_rebuild_db and do_iterate_realm_models

--- Search/Parser/RealmFile.pm
+++ Search/Parser/RealmFile.pm
@@ -1,12 +1,25 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2011 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Search::Parser::RealmFile;
 use strict;
 use Bivio::Base 'Search.Parser';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_M) = b_use('Biz.Model');
 my($_FP) = b_use('Type.FilePath');
+my($_S) = b_use('Type.String');
 my($_HANDLER) = _handler_map();
+
+sub do_iterate_realm_models {
+    my($self, $op, $req) = @_;
+    $_M->new($req, 'RealmFile')
+	->do_iterate(
+	    $op,
+	    'realm_file_id asc',
+	    {is_folder => 0},
+	);
+    return;
+}
 
 sub handle_new_text {
     return _do(@_);
@@ -17,7 +30,22 @@ sub handle_new_excerpt {
 }
 
 sub handle_realm_file_new_excerpt {
-    return shift->SUPER::handle_new_excerpt(@_);
+    my($proto, $parseable, @rest) = @_;
+    return undef
+	unless my $self = ref($proto) ? $proto
+	: $proto->new_text($parseable, @rest);
+    return $self->put(excerpt => ${$_S->canonicalize_and_excerpt($self->get('text'))});
+}
+
+sub realms_for_rebuild_db {
+    my($self, $req) = @_;
+    return $_M->new($req, 'RealmFile')
+	->map_iterate(
+	    sub {shift->get('realm_id')},
+	    'unauth_iterate_start',
+	    'realm_id',
+	    {path => '/'},
+	); 
 }
 
 sub _do {
