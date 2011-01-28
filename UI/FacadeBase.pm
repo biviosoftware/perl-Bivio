7f059fb1232dd446479258a94bf5020cd6c2db3a
Rob Nagler <github@q33.us>
2011-01-28 18:28:27 +0000

    mail_visibility_mode

--- UI/FacadeBase.pm
+++ UI/FacadeBase.pm
@@ -964,6 +964,7 @@ sub _cfg_group_admin {
 	    ]],
  	    ['Forum.require_otp' => 'Require OTP?'],
 	    [mail_send_access => 'Mail Sending Mode'],
+	    [mail_visibility_mode => 'Mail Visibility'],
 	    [feature_blog => 'Blog'],
 	    [feature_motion => 'Poll'],
 	    [feature_mail => 'Mail'],
