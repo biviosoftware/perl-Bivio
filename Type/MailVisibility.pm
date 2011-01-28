930210c46b130c54f0ec920cff2560fc24e72ef8
Rob Nagler <github@q33.us>
2011-01-28 18:24:26 +0000

    *** empty log message ***

---
+++ Type/MailVisibility.pm
@@ -0,0 +1,23 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Type::MailVisibility;
+use strict;
+use Bivio::Base 'Type.Enum';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+__PACKAGE__->compile([
+    UNKNOWN => [0, 'Select who can see mail'],
+    ALWAYS_IS_PRIVATE => [1, 'Only group members can see mail'],
+    ALWAYS_IS_PUBLIC => [2, 'Anybody (even non-users) can see mail'],
+    ALLOW_IS_PUBLIC => [3, 'Adminstrators can set visibility on each message'],
+]);
+
+sub ROW_TAG_KEY {
+    return 'MAIL_VISIBILITY';
+}
+
+sub get_default {
+    return shift->ALWAYS_IS_PRIVATE;
+}
+
+1;
