447ac8162a9a8fc6e944cf4748f155b5947bd26c
Rob Nagler <github@q33.us>
2011-02-02 16:42:50 +0000

    Bivio::Base

--- Biz/Model/TupleUseForm.pm
+++ Biz/Model/TupleUseForm.pm
@@ -2,7 +2,7 @@
 # $Id$
 package Bivio::Biz::Model::TupleUseForm;
 use strict;
-use base 'Bivio::Biz::FormModel';
+use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
