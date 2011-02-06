1cc2a5acadef576332ac183df5d82c7007607983
Rob Nagler <github@q33.us>
2011-02-06 01:46:22 +0000

    leave_and_delete takes a pattern create uses parameters()

--- Util/TestUser.pm
+++ Util/TestUser.pm
@@ -19,27 +19,29 @@ sub USAGE {
     return <<'EOF';
 usage: b-test-user [options] command [args..]
 commands
-  create user_or_email [[password] name]-- RealmAdmin->create_user
+  create name_or_email [[password] name]-- RealmAdmin->create_user
   format_email base [domain] -- HTTP->generate_local_email if not already an email
   init -- test users (adm, etc.)
-  leave_and_delete -- remove user from all realms and delete
+  leave_and_delete [email_re] -- remove user from all realms and delete [
 EOF
 }
 
 sub create {
-    my($self, $user_or_email, $password, $name) = shift->name_args([
+    sub CREATE {[
 	[qw(user_or_email String)],
-	[Password => sub {shift->DEFAULT_PASSWORD}],
-	[name => RealmName => undef],
-    ], \@_);
+	[qw(password Password), sub {shift->DEFAULT_PASSWORD}],
+	[qw(name RealmName), undef],
+    ]}
+    my($self, $bp) = shift->parameters(\@_);
     $self->initialize_fully;
-    my($display_name) = $_E->is_valid($user_or_email)
-	? $_E->get_local_part($user_or_email) : $user_or_email;
+    my($display_name) = $_E->is_valid($bp->{user_or_email})
+	? $_E->get_local_part($bp->{user_or_email})
+	: $bp->{user_or_email};
     my($uid) = $self->new_other('RealmAdmin')->create_user(
-	$self->format_email($user_or_email),
+	$self->format_email($bp->{user_or_email}),
 	$display_name,
-	$password,
-	$name || b_use('Type.RealmName')->clean_and_trim($display_name),
+	$bp->{password},
+	$bp->{name} || b_use('Type.RealmName')->clean_and_trim($display_name),
     );
     b_use('Type.PageSize')->row_tag_replace($uid, 100, $self->req);
     return $uid;
@@ -76,21 +78,50 @@ sub init_adm {
 }
 
 sub leave_and_delete {
-    my($self) = @_;
+    sub LEAVE_AND_DELETE {[[qw(name_re Regexp), undef]]}
+    my($self, $bp) = shift->parameters(\@_);
     $self->req->assert_test;
-    my($uid) = $self->req('auth_user_id');
+    my($uids) = $bp->{name_re} ? _match_users($self, $bp->{name_re})
+	: [$self->req('auth_user_id')];
+    return []
+	unless @$uids;
     $self->model('RealmUser')->do_iterate(
 	sub {
 	    my($it) = @_;
-	    $it->unauth_delete
-		unless $it->get('realm_id') eq $uid;
+	    $it->unauth_delete;
 	    return 1;
 	},
 	'unauth_iterate_start',
 	'realm_id',
-	{user_id => $uid},
+	{user_id => $uids},
     );
-    return $self->new_other('RealmAdmin')->put(force => 1)->delete_user
+    foreach my $uid (@$uids) {
+	$self->req->with_user(
+	    $uid,
+	    sub {$self->new_other('RealmAdmin')->put(force => 1)->delete_auth_user},
+	);
+    }
+    return $uids;
+}
+
+sub _match_users {
+    my($self, $re) = @_;
+    return [sort(
+	keys(
+	    %{{
+		@{$self->model('AdmUserList')
+		    ->map_iterate(
+			sub {
+			    my($it) = @_;
+			    return $it->get('Email.email') =~ $re
+				? ($it->get('User.user_id') => 1)
+				: ();
+			},
+		    ),
+		},
+	    }},
+	),
+    )];
 }
 
 1;
