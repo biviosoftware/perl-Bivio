71d97de4966bb2d4d66764250b964360839ee596
Rob Nagler <github@q33.us>
2011-02-18 17:54:18 +0000

    *** empty log message ***

---
+++ PetShop/Delegate/Support.pm
@@ -0,0 +1,17 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::PetShop::Delegate::Support;
+use strict;
+use Bivio::Base 'Delegate.SimpleAuthSupport';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub extra_auth_realm_bunit {
+    my($proto, $realm, $task, $req) = @_;
+    return 1
+	unless defined(my $x = $req->unsafe_get('Realm.bunit'));
+    $req->put('Realm.bunit' => [$realm->unsafe_get('owner_name'), $task->get('id')->get_name]);
+    return $x;
+}
+
+1;
