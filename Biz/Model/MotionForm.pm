87f7b1c8a7cb22f005fab1d74a98b75c36749f77
Paul Moeller <moeller@bivio.biz>
2011-03-14 23:13:34 +0000

    now derived from FormModeBaseForm added motion file refactored

--- Biz/Model/MotionForm.pm
+++ Biz/Model/MotionForm.pm
@@ -1,34 +1,60 @@
-# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2007-2011 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::MotionForm;
 use strict;
-use base 'Bivio::Biz::FormModel';
+use Bivio::Base 'Model.FormModeBaseForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_MS) = Bivio::Type->get_instance('MotionStatus');
-my($_MT) = Bivio::Type->get_instance('MotionType');
+my($_DT) = b_use('Type.DateTime');
+my($_MS) = b_use('Type.MotionStatus');
+my($_MT) = b_use('Type.MotionType');
 
-sub execute_empty {
+sub LIST_MODEL {
+    return 'MotionList';
+}
+
+sub MOTIONS_FOLDER {
+    return '/Polls/';
+}
+
+sub PROPERTY_MODEL {
+    return 'Motion';
+}
+
+sub execute_empty_create {
     my($self) = @_;
-    if (_is_create($self)) {
-	$self->internal_put_field('Motion.status' => $_MS->from_name('OPEN'));
-	$self->internal_put_field('Motion.type' =>
-				      $_MT->from_name('VOTE_PER_USER'));
-    }
-    else {
-	$self->load_from_model_properties('Motion');
-    }
+    $self->internal_put_field('Motion.status' => $_MS->OPEN);
+    $self->internal_put_field('Motion.type' => $_MT->VOTE_PER_USER);
     return;
 }
 
-sub execute_ok {
+sub execute_empty_edit {
     my($self) = @_;
-    shift->SUPER::execute_ok(@_);
-    return if $self->in_error;
-    $self->new_other('Motion')->create_or_update({
-	%{$self->get_model_properties('Motion')},
-    });
+    $self->load_from_model_properties('Motion');
     return;
+}
+
+sub execute_ok_create {
+    my($self) = @_;
+    _add_file($self);
+    $self->new_other('Motion')->create($self->get_model_properties('Motion'));
+    return;
+}
+
+sub execute_ok_edit {
+    my($self) = @_;
+    _add_file($self);
+    $self->update_model_properties('Motion');
+    return;
+}
+
+sub get_motion_document {
+    my($self) = @_;
+    return $self->get('Motion.motion_file_id')
+	? $self->new_other('RealmFile')->load({
+	    realm_file_id => $self->get('Motion.motion_file_id'),
+	})
+	: undef;
 }
 
 sub internal_initialize {
@@ -37,48 +63,38 @@ sub internal_initialize {
         version => 1,
 	require_context => 1,
 	visible => [qw(
-		       Motion.name
-		       Motion.question
-		       Motion.status
-		       Motion.type
-		   )],
+	    Motion.name
+	    Motion.question
+	    Motion.status
+	    Motion.type
+	),
+	    {
+		name => 'file',
+		type => 'FileField',
+		constraint => 'NONE',
+	    },
+	],
+	hidden => [qw(
+	    Motion.motion_file_id
+	)],
 	other => [qw(
-		     Motion.motion_id
-		 )],
+	    Motion.motion_id
+	    Motion.name_lc
+	)],
     });
 }
 
-sub validate {
+sub _add_file {
     my($self) = @_;
-    shift->SUPER::validate(@_);
-    $self->new_other('MotionList')->do_iterate(
-	sub {
-	    my($it) = @_;
-	    if (lc($self->get('Motion.name')) eq lc($it->get('Motion.name'))) {
-		$self->internal_put_error('Motion.name' => 'EXISTS')
-		    unless _is_create($self) ||
-			$self->get('Motion.motion_id')
-			    eq $it->get('Motion.motion_id');
-	    }
-	    return 1;
-	});
-    return;
-}
-
-sub _is_create {
-    my($self) = @_;
-    my($fm) = $self->get_request->unsafe_get('Type.FormMode');
-    return !$fm || $fm->eq_create ? 1 : 0;
-}
-
-sub internal_pre_execute {
-    my($self) = @_;
-    unless (_is_create($self)) {
-	my($l) = $self->get_request->unsafe_get('Model.MotionList');
-	$self->internal_put_field('Motion.motion_id' =>
-				      $l->get('Motion.motion_id'))
-	    if $l && $l->unsafe_get('Motion.motion_id');
-    }
+    return unless $self->get('file');
+    my($name) = b_use('Model.FileChangeForm')
+	->validate_file_name($self, 'file');
+    return if $self->in_error;
+    $self->internal_put_field('Motion.motion_file_id' =>
+        $self->new_other('RealmFile')->create_with_content({
+	    path => $self->MOTIONS_FOLDER . $name,
+	    is_read_only => 1,
+	}, $self->get('file')->{content})->get('realm_file_id'));
     return;
 }
 
