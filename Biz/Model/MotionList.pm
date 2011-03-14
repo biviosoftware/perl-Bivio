afc59b8504d207026ca0e1e7f1ee554cda83eabd
Paul Moeller <moeller@bivio.biz>
2011-03-14 23:14:04 +0000

    removed execute_load_parent() added can_vote() added motion file fields

--- Biz/Model/MotionList.pm
+++ Biz/Model/MotionList.pm
@@ -1,17 +1,20 @@
-# Copyright (c) 2007 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2007-2011 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::MotionList;
 use strict;
-use base 'Bivio::Biz::ListModel';
+use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_FP) = b_use('Type.FilePath');
 
-sub execute_load_parent {
-    my($proto, $req) = @_;
-    $proto->new($req)->load_this({
-	this => $req->unsafe_get('query')->{'p'},
-    });
-    return;
+sub can_vote {
+    my($self) = @_;
+    return 0
+	if $self->get('Motion.status')->eq_closed;
+    return $self->new_other('MotionVote')->unsafe_load({
+	motion_id => $self->get('Motion.motion_id'),
+	user_id => $self->req('auth_user_id'),
+    }) ? 0 : 1;
 }
 
 sub internal_initialize {
@@ -22,14 +25,41 @@ sub internal_initialize {
 	auth_id => ['Motion.realm_id'],
 	primary_key => ['Motion.motion_id'],
 	order_by => [qw(
-	    Motion.name
+	    Motion.name_lc
+	    Motion.start_date_time
+	    Motion.end_date_time
 	    Motion.status
+	    RealmFile.path_lc
 	)],
         other => [qw(
+	    Motion.name
 	    Motion.question
 	    Motion.type
-	)],
+	    Motion.motion_file_id
+	    RealmFile.path
+	),
+	    {
+		name => 'file_name',
+		type => 'FileName',
+		constraint => 'NONE',
+	    }],
     });
+}
+
+sub internal_post_load_row {
+    my($self, $row) = @_;
+    $row->{file_name} = defined($row->{'RealmFile.path'})
+	? $_FP->get_tail($row->{'RealmFile.path'})
+	: undef;
+    return shift->SUPER::internal_post_load_row(@_);
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt) = @_;
+    $stmt->from($stmt->LEFT_JOIN_ON('Motion', 'RealmFile', [
+	[qw(Motion.motion_file_id RealmFile.realm_file_id)],
+    ]));
+    return;
 }
 
 1;
