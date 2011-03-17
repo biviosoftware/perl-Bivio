dfd202705613c4f5c7890c262b9b53d4a62bc72d
Rob Nagler <github@q33.us>
2011-03-17 20:57:10 +0000

    don't parse files without content types

--- Search/Parser/RealmFile/Unknown.pm
+++ Search/Parser/RealmFile/Unknown.pm
@@ -12,9 +12,8 @@ sub CONTENT_TYPE_LIST {
 
 sub handle_realm_file_new_text {
     my($proto, $parseable) = @_;
-    return
-	if -B $parseable->get_os_path;
-    return $proto->new_text($parseable->put(content_type => 'text/plain'));
+#TODO: Test for binary, but has to be on get_content
+    return;
 }
 
 1;
