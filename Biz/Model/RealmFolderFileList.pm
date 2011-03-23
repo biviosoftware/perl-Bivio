808e8cef889b2a51d2d5ac533ece3af8c7c4e665
Paul Moeller <moeller@bivio.biz>
2011-03-23 19:18:20 +0000

    initial revision

---
+++ Biz/Model/RealmFolderFileList.pm
@@ -0,0 +1,44 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::RealmFolderFileList;
+use strict;
+use Bivio::Base 'Model.RealmFileTreeList';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub PAGE_SIZE {
+    return 200;
+}
+
+sub get_folder_path {
+    my($self) = @_;
+    return $self->new_other('RealmFile')->load({
+	realm_file_id => $self->internal_root_parent_node_id,
+    })->get('path');
+}
+
+sub internal_initialize {
+    my($self) = @_;
+    my($ii) = $self->merge_initialize_info($self->SUPER::internal_initialize, {
+	parent_id => 'RealmFile.folder_id',
+	other => [
+	    ['RealmFile.is_folder', [0]],
+	],
+    });
+    delete($ii->{other_query_keys});
+    return $ii;
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt) = @_;
+    $self->new_other('RealmFileList')->prepare_statement_for_access_mode($stmt,
+        b_use('Type.DocletFileName'));
+    return shift->SUPER::internal_prepare_statement(@_);
+}
+
+sub internal_root_parent_node_id {
+    my($self) = @_;
+    return $self->get_query->get('parent_id');
+}
+
+1;
