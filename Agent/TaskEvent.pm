184929f889b479138311af05615e66a81f8dd161
Rob Nagler <github@q33.us>
2011-03-27 18:08:29 +0000

    Need require_absolute for Mobile task events

--- Agent/TaskEvent.pm
+++ Agent/TaskEvent.pm
@@ -24,9 +24,11 @@ my($_EXPECTED) = {
     put_durable_server_redirect_state => [@$_PARAMS, qw(form form_model)],
     server_redirect => [qw(
         task_id realm query form path_info no_context require_context no_form
+	carry_path_info carry_query
     )],
     client_redirect => [qw(
         task_id realm query path_info no_context require_context no_form uri http_status_code
+	require_absolute carry_path_info carry_query
     )],
 };
 my($_IMPLICIT_OVERRIDE) = {
