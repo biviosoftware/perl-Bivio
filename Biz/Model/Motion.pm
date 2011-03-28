9fdc1f38279858431307ce4da343863bcdc86446
Andrew Schofield <andrews@bivio.biz>
2011-03-28 13:20:09 +0000

    IEEE WG ballots, vote counts

--- Biz/Model/Motion.pm
+++ Biz/Model/Motion.pm
@@ -6,32 +6,25 @@ use Bivio::Base 'Model.RealmBase';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_DT) = b_use('Type.DateTime');
+my($_MT) = b_use('Type.MotionType');
+my($_MS) = b_use('Type.MotionStatus');
+my($_VT) = b_use('Type.MotionVote');
 
 sub create {
     my($self, $values) = @_;
     $values->{name_lc} = lc($values->{name});
-
-    if ($values->{status}->eq_open) {
-	$values->{start_date_time} ||= $_DT->now;
-    }
+    $values->{type} = $_MT->VOTE_PER_USER;
+    $values->{status} = $_MS->OPEN;
+    $values->{start_date_time} ||= $_DT->now;
+    $values->{end_date_time} = undef;
     return shift->SUPER::create(@_);
 }
 
 sub update {
     my($self, $values) = @_;
     $values->{name_lc} = lc($values->{name});
-
-    if ($values->{status}
-	&& $values->{status} != $self->get('status')) {
-
-	if ($values->{status}->eq_open) {
-	    $values->{start_date_time} ||= $_DT->now;
-	    $values->{end_date_time} = undef;
-	}
-	else {
-	    $values->{end_date_time} ||= $_DT->now;
-	}
-    }
+    $values->{type} = $_MT->VOTE_PER_USER;
+    $values->{status} = $_MS->OPEN;
     return shift->SUPER::update(@_);
 }
 
@@ -49,14 +42,39 @@ sub internal_initialize {
 	    type => ['MotionType', 'NOT_NULL'],
 	    start_date_time => ['DateTime', 'NONE'],
 	    end_date_time => ['DateTime', 'NONE'],
-	    motion_file_id => ['RealmFile.realm_file_id', 'NONE'],
+      	    motion_file_id => ['RealmFile.realm_file_id', 'NONE'],
 	    moniker => ['TupleMoniker', 'NONE'],
 	},
 	other => [
 	    [qw(realm_id RealmOwner.realm_id)],
-	    [qw(motion_file_id RealmFile.realm_file_id)],
+	    [qw(motion_file_id RealmFile.realm_file_id)], 
 	],
     });
+}
+
+
+
+sub vote_count {
+    my($self,  $vote_type) = @_;
+    my($clause) = $vote_type ? ' AND mv.vote = ' . $vote_type->as_sql_param : '';
+
+      my($results) = Bivio::SQL::Connection->execute_one_row('
+            SELECT COUNT(*)
+            FROM motion_vote_t mv
+            WHERE mv.motion_id = ?'
+	    . $clause,
+	    [ $self->get('motion_id') ]);
+    return $results->[0];
+}
+
+sub vote_count_abstain {
+    return shift->vote_count($_VT->ABSTAIN);
+}
+sub vote_count_no {
+    return shift->vote_count($_VT->NO);
+}
+sub vote_count_yes {
+    return shift->vote_count($_VT->YES);
 }
 
 1;
