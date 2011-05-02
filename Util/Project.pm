4823cea6999e50a3588e33a0c490ca10de818d5b
Rob Nagler <github@q33.us>
2011-05-02 16:41:56 +0000

    was including the default facade directory as one of the dirs to link

--- Util/Project.pm
+++ Util/Project.pm
@@ -6,6 +6,7 @@ use Bivio::Base 'Bivio.ShellUtil';
 use File::Find ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_F) = b_use('IO.File');
 
 sub USAGE {
     return <<'EOF';
@@ -19,45 +20,51 @@ sub link_facade_files {
     my($self) = @_;
     $self->initialize_fully;
     my($default) = b_use('UI.Facade')->get_instance;
-    my($default_prefix) = $default->get('local_file_prefix');
-    b_die($default_prefix, ': local_file_prefix not found')
-	unless -d $default_prefix;
-    my($prefixes) = [
-	grep(
-	    $_ ne $default_prefix,
-	    map(
-		$default->get_instance($_)->get('local_file_prefix'),
-		@{$default->get_all_classes},
+    $_F->do_in_dir(
+	$default->get_local_file_root,
+	sub {
+	my($default_prefix) = $default->get('local_file_prefix');
+	b_die($default_prefix, ': local_file_prefix not found')
+	    unless -d $default_prefix;
+	my($prefixes) = [
+	    grep(
+		$_ ne $default_prefix,
+		map(
+		    $default->get_instance($_)->get('local_file_prefix'),
+		    @{$default->get_all_classes},
+		),
 	    ),
-	),
-    ];
-    File::Find::find(
-	{
-	    no_chdir => 1,
-	    follow => 0,
-	    wanted => sub {
-		my($file) = $File::Find::name;
-		return
-		    if $file =~ m{/CVS(?:/|$)} || $file =~ /\~$/;
-		$file =~ s,^$default_prefix,,;
-		foreach my $prefix (@$prefixes) {
-		    my($destination) = $prefix . $file;
-		    next
-			if -e $destination;
-		    if (-d $File::Find::name) {
-			Bivio::IO::File->mkdir_p($destination);
-			next;
+	];
+	File::Find::find(
+	    {
+		no_chdir => 1,
+		follow => 0,
+		wanted => sub {
+		    my($file) = $File::Find::name;
+		    return
+			if $file =~ m{/CVS(?:/|$)} || $file =~ /\~$/;
+		    return
+			unless $file =~ s,^$default_prefix,,;
+		    foreach my $prefix (@$prefixes) {
+			my($destination) = $prefix . $file;
+			next
+			    if -e $destination;
+			if (-d $File::Find::name) {
+			    Bivio::IO::File->mkdir_p($destination);
+			    next;
+			}
+			my($up) = $File::Find::dir;
+			$up =~ s,[^/]+,..,g;
+			b_die($!)
+			    unless symlink("$up/$File::Find::name", $destination);
 		    }
-		    my($up) = $File::Find::dir;
-		    $up =~ s,[^/]+,..,g;
-		    b_die($!)
-			unless symlink("$up/$File::Find::name", $destination);
-		}
-		return;
+		    return;
+		},
 	    },
-	},
-	$default_prefix,
-    );
+	    $default_prefix,
+	);
+	return;
+    });
     return;
 }
 
