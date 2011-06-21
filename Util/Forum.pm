7720cb93e70aa065047fa1f1b1fb11f3f1283d80
Justin Schell <schellj@bivio.biz>
2011-06-21 20:50:34 +0000

    add forum_activity, cascade_forum_activity

--- Util/Forum.pm
+++ Util/Forum.pm
@@ -5,6 +5,7 @@ use strict;
 use Bivio::Base 'Bivio.ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DT) = b_use('Type.DateTime');
 my($_FN) = b_use('Type.ForumName');
 my($_FP) = b_use('Type.FilePath');
 my($_FM) = b_use('Biz.FormModel');
@@ -15,10 +16,16 @@ sub USAGE {
     return <<'EOF';
 usage: bivio Forum [options] command [args..]
 commands
+   cascade_forum_activity -- most recent forum and subforum activity
    delete_forum -- deletes the forum
+   forum_activity -- most recent forum activity
    make_admin_of_forum -- make user admin of forum (and subforums)
    reparent child-forum new-parent -- updates child-forum to point at new-parent
 EOF
+}
+
+sub cascade_forum_activity {
+    return _activity(shift, '_cascade');
 }
 
 sub delete_forum {
@@ -32,6 +39,10 @@ sub delete_forum {
 	realm_id => $id,
     });
     return;
+}
+
+sub forum_activity {
+    return _activity(shift);
 }
 
 sub make_admin_of_forum {
@@ -80,6 +91,71 @@ sub reparent {
 sub tree_paths {
     my($self, $top) = shift->name_args([[qw(child ForumName)]], \@_);
     return [_tree_paths($self, $self->unauth_realm_id($top), '/')];
+}
+
+sub _activity {
+    my($self, $prefix) = @_;
+    $prefix ||= '';
+    my($method) = $prefix . '_forum_activity';
+    my($activity) = {};
+    $self->$method($activity);
+    my($buf) = "- Most recent activity -\n";
+    foreach my $f (keys(%$activity)) {
+	$buf .= "$f: ";
+	$buf .= $_DT->to_local_string(
+	    $activity->{$f}->{'modified_date_time'});
+	$buf .= ' in ' . $activity->{$f}->{'forum_name'};
+	$buf .= "\n";
+    }
+    return $buf;
+}
+
+sub _cascade_forum_activity {
+    my($self, $activity) = @_;
+    my($req) = $self->req;
+    $_M->new_other('Forum')->set_ephemeral->do_iterate(sub {
+        my($forum) = @_;
+	my($fid) = $forum->get('forum_id');
+	$req->with_realm(
+	    $fid,
+	    sub {
+		_forum_activity($self, $activity);
+		_cascade_forum_activity($self, $activity)
+		    unless $forum->is_leaf;
+	    },
+	);
+	return 1;
+    }, 'unauth_iterate_start', {
+	parent_realm_id => $req->get('auth_id'),
+    });
+    _forum_activity($self, $activity);
+    return;
+}
+
+sub _forum_activity {
+    my($self, $activity) = @_;
+    my($fn) = $self->req->get_nested(qw(auth_realm owner_name));
+    foreach my $m (qw(RealmOwner Forum)) {
+	foreach my $r (@{$_M->new_other($m)->set_ephemeral
+			     ->internal_get_sql_support->get_children}) {
+	    my($spn) = $r->[0]->simple_package_name;
+	    next
+		unless grep(/modified_date_time/,
+			@{$_M->new_other($spn)->set_ephemeral->get_keys});
+	    $_M->new($self->req, $spn)->set_ephemeral->do_iterate(
+		sub {
+		    my($it) = @_;
+		    $activity->{$spn} = {
+			forum_name => $fn,
+			modified_date_time => $it->get('modified_date_time'),
+		    } if $_DT->is_greater_than($it->get('modified_date_time'),
+			$activity->{$spn}->{modified_date_time});
+		    return 0;
+		}, 'iterate_start', 'modified_date_time desc',
+	    );
+	}
+    }
+    return;
 }
 
 sub _tree_paths {
