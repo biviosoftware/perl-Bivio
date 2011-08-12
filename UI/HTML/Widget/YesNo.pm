44c5784dd15a8de44dc3d26f2f64e7cd158de5b5
Paul Moeller <moeller@bivio.biz>
2011-08-12 15:57:29 +0000

    refactored, now uses Radio widgets

--- UI/HTML/Widget/YesNo.pm
+++ UI/HTML/Widget/YesNo.pm
@@ -2,46 +2,22 @@
 # $Id$
 package Bivio::UI::HTML::Widget::YesNo;
 use strict;
-use Bivio::Base 'UI.Widget';
+use Bivio::Base 'Widget.Join';
+use Bivio::UI::ViewLanguageAUTOLOAD;
 
-# C<Bivio::UI::HTML::Widget::YesNo> displays a Boolean field as Yes/No
-# radios.
+# Displays a Boolean field as Yes/No radios.
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_IDI) = __PACKAGE__->instance_data_index;
 
 sub initialize {
     my($self) = @_;
-    my($fields) = $self->[$_IDI] ||= {};
-    return if $fields->{yes_widget};
-    foreach my $name (qw(yes no)) {
-	$fields->{$name.'_widget'} = Bivio::UI::HTML::Widget::String->new(
-	    ucfirst($name), 'radio'
-	)->initialize_with_parent($self);
-    }
-    return;
-}
-
-sub render {
-    my($self, $source, $buffer) = @_;
-    my($fields) = $self->[$_IDI];
-    my($form) = $source->get_request->get_widget_value(
-	$self->ancestral_get('form_model'));
-    foreach my $name (qw(yes no)) {
-	my($value) = $name eq 'yes' ? 1 : 0;
-	$$buffer .= '<input name="'
-	    . $form->get_field_name_for_html($self->get('field'))
-	    . qq{" type=radio value="$value"};
-
-	if (($form->get($self->get('field')) || 0) eq $value) {
-	    $$buffer .= ' checked="checked"';
-	}
-
-	$$buffer .= ' />&nbsp;';
-	$fields->{$name . '_widget'}->render($source, $buffer);
-	$$buffer .= ' ';
-    }
-    return;
+    return if $self->unsafe_get('values');
+    $self->put(values => [
+	Radio($self->get('field'), 1, 'Yes'),
+	vs_blank_cell(3),
+	Radio($self->get('field'), 0, 'No'),
+    ]);
+    return shift->SUPER::initialize(@_);
 }
 
 1;
