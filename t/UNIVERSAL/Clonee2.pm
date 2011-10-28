913ebbccf470b25786d53f4d3f71d7cb4ea26838
Rob Nagler <github@q33.us>
2011-10-28 00:31:14 +0000

    *** empty log message ***

---
+++ t/UNIVERSAL/Clonee2.pm
@@ -0,0 +1,13 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::t::UNIVERSAL::Clonee2;
+use strict;
+use Bivio::Base 'Bivio::t::UNIVERSAL::Clonee';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub clone_return_is_self {
+    return 1;
+}
+
+1;
