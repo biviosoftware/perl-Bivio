e8fe1ae75976bd7ba1af7a5b1336d5de90b7067e
Rob Nagler <github@q33.us>
2011-11-05 23:44:48 +0000

    fixed clone to really delete temp files.  HTTP::Coookies will save to the file always if there's a $self->{file}

--- Ext/HTTPCookies.pm
+++ Ext/HTTPCookies.pm
@@ -8,20 +8,29 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_F) = Bivio::UNIVERSAL->use('IO.File');
 my($_D) = Bivio::UNIVERSAL->use('Bivio.Die');
 
-# sub clone {
-#     my($self) = @_;
-#     my($tmp) = $_F->temp_file;
-#     my($res) = return $_D->eval_or_die(sub {
-#         $self->save($tmp);
-# 	my($clone) = ref($self)->new(
-# 	    map(
-# 		($_ => $self->{$_}),
-# 		grep($_ ne 'COOKIES', keys(%$self)),
-# 	    ),
-# 	);
-# 	$clone->load($tmp);
-# 	return $clone;
-#     });
-#     unlink($tmp);
-#     return $res;
-# }
+sub clone {
+    my($self) = @_;
+    my($tmp) = $_F->temp_file;
+    my($clone);
+    my($die) = $_D->catch(
+	sub {
+	    $self->save($tmp);
+	    delete($self->{file});
+	    $clone = ref($self)->new(
+		map(
+		    ($_ => $self->{$_}),
+		    grep($_ ne 'COOKIES', keys(%$self)),
+		),
+	    );
+	    $clone->load($tmp);
+	    delete($clone->{file});
+	    return;
+	},
+    );
+    unlink($tmp);
+    $die->throw
+	if $die;
+    return $clone;
+}
+
+1;
