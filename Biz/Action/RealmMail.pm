02ea9978de60860fde2f1f9bde8374a45482d731
Rob Nagler <github@q33.us>
2011-12-06 02:51:28 +0000

    Don't queue a job if the user agent is_mail_agent so that we lock up an apache process

--- Biz/Action/RealmMail.pm
+++ Biz/Action/RealmMail.pm
@@ -14,6 +14,7 @@ my($_I) = b_use('Mail.Incoming');
 my($_M) = b_use('Biz.Model');
 my($_BMM) = b_use('Type.BulletinMailMode');
 my($_BBT) = b_use('Type.BulletinBodyTemplate');
+my($_UA) = b_use('Type.UserAgent');
 
 sub EMAIL_LIST {
     return 'RealmEmailList';
@@ -31,6 +32,8 @@ sub execute_receive {
     my($proto, $req, $rfc822, $reflector_task) = @_;
     $rfc822 ||= $req->get('Model.MailReceiveDispatchForm')
 	->get('message')->{content};
+    $reflector_task
+	||= $req->get('task')->get_attr_as_id('mail_reflector_task');
     my($rm) = Bivio::Biz::Model->new($req, 'RealmMail');
     my($in) = $proto->want_realm_mail_created($req)
 	? $rm->create_from_rfc822($rfc822)
@@ -44,17 +47,20 @@ sub execute_receive {
 	    && $_MWRT->is_set_for_realm($req),
 	subject_prefix => $proto->internal_subject_prefix($rm),
     });
-    b_use('AgentJob.Dispatcher')->enqueue(
-	$req,
-	$reflector_task
-	    || $req->get('task')->get_attr_as_id('mail_reflector_task'),
-	{
-	    $proto->package_name => $proto->new({
-		outgoing => $out,
-		realm_file_id => $rm->unsafe_get('realm_file_id'),
-	    }),
-	},
-    );
+    my($attrs) = {
+	$proto->package_name => $proto->new({
+	    outgoing => $out,
+	    realm_file_id => $rm->unsafe_get('realm_file_id'),
+	}),
+    };
+    if ($_UA->is_mail_agent($req)) {
+	$req->put_durable(%$attrs);
+	return {
+	    method => 'server_redirect',
+	    task_id => $reflector_task,
+	};
+    }
+    b_use('AgentJob.Dispatcher')->enqueue($req, $reflector_task, $attrs);
     return;
 }
 
