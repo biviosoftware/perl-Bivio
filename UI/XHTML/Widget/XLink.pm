476794964474bde0565bf424fb78470c4ce0f84e
Rob Nagler <github@q33.us>
2011-12-20 01:33:26 +0000

    factored out XLinkURI

--- UI/XHTML/Widget/XLink.pm
+++ UI/XHTML/Widget/XLink.pm
@@ -6,8 +6,6 @@ use Bivio::Base 'XHTMLWidget.Tag';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_TI) = b_use('Agent.TaskId');
-my($_C) = b_use('IO.Config');
 
 sub NEW_ARGS {
     return [qw(facade_label ?class)];
@@ -19,25 +17,7 @@ sub initialize {
     $self->put_unless_exists(
 	tag => 'a',
 	value => XLinkLabel($l),
-	href => $_TI->is_valid_name($l) ? URI({
-	    task_id => $_TI->from_name($l),
-	    $_C->if_version(8 => sub {
-	        return (
-		    query => undef,
-		    path_info => undef,
-		);
-	    }),
-	}) : [sub {
-		   my($source) = @_;
-		   my($req) = $self->req;
-		   return URI(
-		       vs_constant(
-			   $req,
-			   'xlink_'
-			   . $self->render_simple_attr(facade_label => $req),
-		       ),
-		   );
-	       }],
+	href => XLinkURI($l),
 	html_attrs => vs_html_attrs_merge([qw(href name link_target)]),
     );
     return shift->SUPER::initialize(@_);
