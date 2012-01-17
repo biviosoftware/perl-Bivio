58743534c03831f2f4aa1507674786608ef464de
Rob Nagler <github@q33.us>
2012-01-17 00:11:37 +0000

    creates "/b" link in files/<default>/plain for CKEditor if it doesn't exist -- assumes files structure is $ENV{HOME}/src/perl/Bivio Symlinks of directories stay symlinks (not directories, because "/b" wasn't working)

--- Util/Project.pm
+++ Util/Project.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2011-2012 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Util::Project;
 use strict;
@@ -7,6 +7,7 @@ use File::Find ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_F) = b_use('IO.File');
+my($_C) = b_use('IO.Config');
 
 sub USAGE {
     return <<'EOF';
@@ -23,49 +24,61 @@ sub link_facade_files {
     $_F->do_in_dir(
 	$default->get_local_file_root,
 	sub {
-	my($default_prefix) = $default->get('local_file_prefix');
-	b_die($default_prefix, ': local_file_prefix not found')
-	    unless -d $default_prefix;
-	my($prefixes) = [
-	    grep(
-		$_ ne $default_prefix,
-		map(
-		    $default->get_instance($_)->get('local_file_prefix'),
-		    @{$default->get_all_classes},
+	    my($default_prefix) = $default->get('local_file_prefix');
+	    b_die($default_prefix, ': local_file_prefix not found')
+		unless -d $default_prefix;
+	    if ($_C->is_dev) {
+		my($common) = "$ENV{HOME}/src/perl/Bivio/files";
+		my($common_b) = $default->get_local_file_name(
+		    'PLAIN',
+		    $default->get_local_file_plain_common_uri,
+		);
+		unless (-l $common_b) {
+		    b_die("symlink($common_b): $!")
+			unless symlink($common, $common_b);
+		}
+	    }
+	    my($prefixes) = [
+		grep(
+		    $_ ne $default_prefix,
+		    map(
+			$default->get_instance($_)->get('local_file_prefix'),
+			@{$default->get_all_classes},
+		    ),
 		),
-	    ),
-	];
-	File::Find::find(
-	    {
-		no_chdir => 1,
-		follow => 0,
-		wanted => sub {
-		    my($file) = $File::Find::name;
-		    return
-			if $file =~ m{/CVS(?:/|$)} || $file =~ /\~$/;
-		    return
-			unless $file =~ s,^$default_prefix,,;
-		    foreach my $prefix (@$prefixes) {
-			my($destination) = $prefix . $file;
-			next
-			    if -e $destination;
-			if (-d $File::Find::name && ! -l $File::Find::name) {
-			    Bivio::IO::File->mkdir_p($destination);
-			    next;
+	    ];
+	    File::Find::find(
+		{
+		    no_chdir => 1,
+		    follow => 0,
+		    wanted => sub {
+			my($file) = $File::Find::name;
+			return
+			    if $file =~ m{/CVS(?:/|$)} || $file =~ /\~$/;
+			return
+			    unless $file =~ s,^$default_prefix,,;
+			foreach my $prefix (@$prefixes) {
+			    my($destination) = $prefix . $file;
+			    next
+				if -e $destination;
+			    if (-d $File::Find::name && ! -l $File::Find::name) {
+				Bivio::IO::File->mkdir_p($destination);
+				next;
+			    }
+			    my($up) = $File::Find::dir;
+			    $up =~ s,[^/]+,..,g;
+			    next if $File::Find::name =~ /\.cvsignore/;
+			    b_die($!)
+				unless symlink("$up/$File::Find::name", $destination);
 			}
-			my($up) = $File::Find::dir;
-			$up =~ s,[^/]+,..,g;
-			next if $File::Find::name =~ /\.cvsignore/;
-			b_die($!)
-			    unless symlink("$up/$File::Find::name", $destination);
-		    }
-		    return;
+			return;
+		    },
 		},
-	    },
-	    $default_prefix,
-	);
-	return;
-    });
+		$default_prefix,
+	    );
+	    return;
+        },
+    );
     return;
 }
 
