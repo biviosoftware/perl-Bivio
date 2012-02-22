a3cfdb8a06907f697880960201bc14d1cd76fa41
Rob Nagler <github@q33.us>
2012-02-22 03:04:02 +0000

    refactored to be in a cleaner style

--- Biz/Model/DBAccessModelForm.pm
+++ Biz/Model/DBAccessModelForm.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2007-2011 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::DBAccessModelForm;
 use strict;
@@ -6,32 +6,48 @@ use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
-
+my($_E) = b_use('Type.Enum');
+my($_T) = b_use('Bivio.Type');
+my($_DT) = b_use('Type.DateTime');
+my($_CL) = b_use('IO.ClassLoader');
+my($_ES) = b_use('Type.EnumSet');
+my($_PN) = b_use('Type.PerlName');
+my($_PM) = b_use('Biz.PropertyModel');
+my($_SEARCH) = b_use('SQL.ListQuery')->to_char('search');
+my($_PAGE) = b_use('SQL.ListQuery')->to_char('page_number');
+my($_COUNT) = b_use('SQL.ListQuery')->to_char('count');
 my($_A) = b_use('Action.Acknowledgement');
-
-my(@_CANCEL_BUTTONS) = map($_ . '_button',
-		    qw(clear_form first prev next last  delete));
-my(@_OK_BUTTONS) = map($_ . '_button',
-		    qw(search update create));
-
-my($_all_fields);
+my($_E) = b_use('Type.Enum');
+my($_CANCEL_BUTTONS) = [map(
+    $_ . '_button',
+    qw(clear_form first prev next last delete),
+)];
+my($_OK_BUTTONS) = [map(
+    $_ . '_button',
+    qw(search update create),
+)];
+my($_ALL_FIELDS);
 
 sub execute_empty {
     my($self) = @_;
-    my($model_name) = $self->req('path_info') =~ qr{^/(.*)};
+    my($model_name) = b_debug _model_from_path_info($self);
     my($query) = $self->req('query');
-    my($status) = $query->{s};
-    if (defined($query->{n})) {       
-	$query->{c} ||= _get_row_count($self, $query, $model_name);
-	if ($query->{n} <= $query->{c}) {
-	    if (my $row = _get_nth_row($self, $query, $model_name, $query->{n})) {	       
+#TODO: User data needs to be type-checked
+    my($status) = $query->{$_SEARCH};
+    if (defined($query->{$_PAGE})) {
+	$query->{$_COUNT} ||= _get_row_count($self, $query, $model_name);
+	if ($query->{$_PAGE} <= $query->{$_COUNT}) {
+	    if (my $row = _get_nth_row($self, $query, $model_name)) {
 		foreach my $field (@{$row->get_keys}) {
-		    $self->internal_put_field($model_name . '.' . $field,
-					      _value_db_to_form($model_name, $field, $row->get($field)));
+		    $self->internal_put_field(
+			"$model_name.$field",
+			_value_db_to_form($self, $model_name, $field, $row->get($field)),
+		    );
 		}
 	    }
 	}
-	$status ||=  $query->{c} > 0 ? "Displaying row $query->{n} of $query->{c}"
+	$status ||= $query->{$_COUNT} > 0
+	    ? "Displaying row $query->{$_PAGE} of $query->{$_COUNT}"
 	    : "No rows found";
     }
     $self->internal_put_field('status', $status);
@@ -47,112 +63,113 @@ sub execute_ok {
 }
 
 sub get_all_fields {
-    my($proto) = @_;
-    unless (defined($_all_fields)) {
-	$_all_fields = {};
-	foreach my $class (@{Bivio::IO::ClassLoader->list_simple_packages_in_map('Model')}) {
-	    next if $class =~ /Base$/;	
-	    #TODO: investigate FormUserAddForm
-	    next if $class eq 'FormUserAddForm';
-	    if (b_use('Model.' . $class)->isa('Bivio::Biz::PropertyModel')) {
-		my($m) = b_use('Model.' . $class)->new;
-		foreach my $field (@{$m->get_info('column_names')}) {
-		    $_all_fields->{$class}->{$field} = {
-			map(($_ => $m->get_field_info($field, $_)), qw(type constraint)),
-			related => [],
-		    };
-		}
+    my($self) = @_;
+    return $_ALL_FIELDS
+	if $_ALL_FIELDS;
+    $_ALL_FIELDS = {};
+    $_PM->do_iterate_model_subclasses(sub {
+	my($class) = @_;
+	my($m) = $class->get_instance;
+	foreach my $field (@{$m->get_info('column_names')}) {
+	    $_ALL_FIELDS->{$m->simple_package_name}->{$field} = {
+		map(($_ => $m->get_field_info($field, $_)), qw(type constraint)),
+		related => [],
+	    };
+	}
+	return 1;
+    });
+    foreach my $class (keys(%$_ALL_FIELDS)) {
+	my($m) = b_use('Model.' . $class)->new;
+	while (my($model, $pv) = each(%{$m->get_info('parents')})) {
+	    while (my($fk, $fn) = each(%$pv)) {
+		push(@{$_ALL_FIELDS->{$class}->{$fk}->{related}}, {
+		    model => $model,
+		    field => $fn,
+		});
 	    }
 	}
-	foreach my $class (keys(%$_all_fields)) {
-	    my($m) = b_use('Model.' . $class)->new;
-	    while (my($pk, $pv) = each(%{$m->get_info('parents')})) {
-		my($model) = split(qr{\s+}, $pk);
-		while (my($fk, $fn) = each(%$pv)) {
-		    push(@{$_all_fields->{$class}->{$fk}->{related}}, {
-		    	model => $model,
-		    	field => $fn,
-		    });
-		}
-	    }
-	    foreach my $child (@{$m->get_info('children')}) {	    
-		while (my($fk, $fn) = each(%{$child->[1]})) {
-		    my($model) =  $child->[0] =~ /([^:]*)$/;
-		    next unless defined $_all_fields->{$model};
-		    next unless defined $_all_fields->{$model}->{$fn};
-		    next unless defined $_all_fields->{$model}->{$fn}->{related};
-		    push(@{$_all_fields->{$class}->{$fn}->{related}}, {
-		         model => $model,
-		         field => $fk,
-		    });
-		}
+	foreach my $child (@{$m->get_info('children')}) {
+	    while (my($fk, $fn) = each(%{$child->[1]})) {
+		my($model) =  $child->[0]->simple_package_name;
+		push(
+		    @{$_ALL_FIELDS->{$class}->{$fn}->{related}},
+		    {
+			model => $model,
+			field => $fk,
+		    },
+		) if $_ALL_FIELDS->{$model}
+		    && $_ALL_FIELDS->{$model}->{$fn}
+		    && $_ALL_FIELDS->{$model}->{$fn}->{related};
 	    }
 	}
     }
-    return $_all_fields;        
+    return $_ALL_FIELDS;
 }
 
 sub get_all_rows {
-    my($proto, $req) = @_;
+    my($self, $req) = @_;
     my($query) = $req->get('query');
-    my($model_name) = $req->get('path_info') =~ qr{^/(.*)};
+    my($model_name) = _model_from_path_info($self);
     my($res) = [];
     my($index) = 0;
-    b_use("Model.$model_name")->new()->do_iterate(
+    _do_iterate(
+	$self,
+	$query,
+	$model_name,
 	sub {
-	    my($model) = @_;
+	    my($it) = @_;
 	    my($row) = {
 		index => ++$index,
 	    };
-	    foreach my $field (@{$model->get_keys}) {
+	    foreach my $field (@{$it->get_keys}) {
 		my($f) = $field =~ qr{([^\.]*)$};
-		my($t) = get_all_fields()->{$model_name}->{$field}->{type};
-		my($dbv) = $model->get($field);
+		my($t) = _field_type($self, $model_name, $field);
+		my($dbv) = $it->get($field);
 		my($fv);
-		$fv = $t->to_xml($dbv) if $t eq 'Bivio::Type::DateTime';
-		$fv = $t->to_literal($dbv) if $t->isa('Bivio::Type::EnumSet');
-		$fv = $dbv unless defined($fv);
+		$fv = $t->to_xml($dbv)
+		    if $_DT->is_super_of($t);
+		$fv = $t->to_literal($dbv)
+		    if $_ES->is_super_of($t);
+		$fv = $dbv
+		    unless defined($fv);
 		$row->{"$model_name.$field"} = $fv;
 	    }
 	    push(@$res, $row);
 	    return 1;
-	}, 'unauth_iterate_start', undef, _get_iterator_params($query, $model_name)
+	},
     );
     return $res;
 }
 
 
 sub get_property_model_names {
-    return sort(keys(%{get_all_fields()}));
+    return [sort(keys(%{shift->get_all_fields}))];
 }
 
 sub get_qualified_fields {
-    my($proto) = @_;
-    my($model_name) = $proto->req('path_info') =~ qr{^/(.*)};
-    return map("$model_name.$_", sort(keys(%{get_all_fields()->{$model_name}})));
+    my($self) = @_;
+    my($model_name) = _model_from_path_info($self);
+    return [map(
+	"$model_name.$_",
+	sort(keys(%{_model_fields($self, $model_name)})),
+    )];
 }
 
 sub get_related {
-    my($proto, $model_name, $field) = @_;
-    return get_all_fields()->{$model_name}->{$field}->{related};
+    my($self, $model_name, $field) = @_;
+    return _field_attr($self, $model_name, $field, 'related');
 }
 
 sub internal_initialize {
     my($self) = @_;
     my($visible) = [];
-    while (my($model_name, $fields) = each(%{get_all_fields()})) {
+    while (my($model_name, $fields) = each(%{$self->get_all_fields})) {
     	foreach my $field (sort(keys(%$fields))) {
     	    my($e) = {
     		name => "$model_name.$field",
-    		type => get_all_fields()->{$model_name}->{$field}->{type},
+    		type => _type_map(_field_type($self, $model_name, $field)),
     		constraint => 'NONE',
     	    };
-	    $e->{type} = 'Integer' if  $e->{type}->isa('Bivio::Type::PrimaryId');
-	    $e->{type} = 'Line' if  $e->{type}->isa('Bivio::Type::Number')
-		&& !$e->{type}->isa('Bivio::Type::Enum');
-    	    $e->{type} = 'Line' if $e->{type}->isa('Bivio::Type::EnumSet');
-    	    $e->{type} = 'Line' if $e->{type}->isa('Bivio::Type::EmailVerifyKey');
-	    $e->{type} = 'Line' if $e->{type}->isa('Bivio::Type::DateTime');
     	    push(@$visible, $e);
     	}
     }
@@ -160,92 +177,88 @@ sub internal_initialize {
         version => 1, 
 	visible => [
 	    @$visible,
-	    {
-		name => 'status',
-		type => 'String',
-		constraint => 'NONE',
-	    },
-	    map({
-                name => $_,
-                type => 'CancelButton',
-                constraint => 'NONE',
-            }, @_CANCEL_BUTTONS),
-	    map({
-                name => $_,
-                type => 'OKButton',
-                constraint => 'NONE',
-            }, @_OK_BUTTONS),
+	    $self->field_decl(['status'], 'String'),
+	    $self->field_decl($_CANCEL_BUTTONS, 'CancelButton'),
+	    $self->field_decl($_OK_BUTTONS, 'OKButton'),
 	],
     });
 }
 
 sub relation_exists {
     my($self, $qualified_field_name, $related_model, $related_field) = @_;
-    my($exists) = 0;
-    $self->new_other($related_model)->do_iterate(
-	sub {
-	    $exists = 1;
-	    return 0;
-	}, 'unauth_iterate_start', undef, {
-	    $related_field => $self->get($qualified_field_name)
-	}
-    );    
-    return $exists;
+    return $self->has_fields($qualified_field_name)
+	&& $self->new_other($related_model)->unauth_rows_exist(
+	    {$related_field => $self->get($qualified_field_name)},
+	);
+}
+
+sub _do_iterate {
+    my($self, $query, $model_name, $op) = @_;
+    my($res) = $self->new_other($model_name);
+    $res->do_iterate(
+	$op,
+	'unauth_iterate_start',
+	undef,
+	{map(
+	    $_ =~ /^($_PAGE$|$_SEARCH$|$_COUNT$|_)/o ? ()
+		: ($_ => _value_query_to_db($self, $model_name, $_, $query->{$_})),
+	    keys(%$query),
+	)},
+    );
+    return $res;
 }
 
 sub _execute_ok_cancel {
-    my($self) = @_;
+    my($self, $button) = @_;
     my($query) = $self->req('query');
-    my($button) = grep(defined($self->get($_)), (@_CANCEL_BUTTONS, @_OK_BUTTONS));
-    my($method) = '_handle_' . $button;
-    my($model_name) = $self->req('path_info') =~ qr{^/(.*)};
-    delete($query->{s});
-    $query = $self->$method($query, $model_name);
+    delete($query->{$_SEARCH});
+    my($sub) = \&{'_handle_' . $button};
     return {
-	    task_id => 'DEV_DBACCESS_MODEL_FORM',
-	    path_info => $self->req('path_info'),
-	    query => $query,
+	task_id => 'DEV_DBACCESS_MODEL_FORM',
+	carry_path_info => 1,
+	query => $sub->($self, $query, _model_from_path_info($self)),
     };
 }
 
 
-sub _get_iterator_params {
-    my($query, $model_name) = @_;
-    my($res) = {};
-    foreach my $field (keys(%$query)) {	
-	next if $field =~ /^(n$|s$|c$|_)/;
-	$res->{$field} = _value_query_to_db($model_name, $field, $query->{$field});
-    }
-    return $res;
+sub _field_attr {
+    my($self, $model, $field, $attr) = @_;
+    return _model_fields($self, $model)->{$field}->{$attr};
 }
 
+sub _field_type {
+    return _field_attr(@_, 'type');
+}
 
 sub _get_nth_row {
-    my($self, $query, $model_name) = @_;
+    my($self, $query) = @_;
     my($i) = 0;
-    my($res) = $self->new_other($model_name);
-    $res->do_iterate(
+    my($res) = _do_iterate(
+	@_,
 	sub {
-	    return (++$i < $query->{n}) ? 1 : 0;
-	}, 'unauth_iterate_start', undef, _get_iterator_params($query, $model_name)
+	    return ++$i < $query->{$_PAGE} ? 1 : 0;
+	},
     );
-    return $i == $query->{n} ? $res : undef;
+    return $i == $query->{$_PAGE} ? $res : undef;
 }
 
 sub _get_row_count {
-    my($self, $query, $model_name) = @_;
+    my($self) = @_;
     my($c) = 0;
-    $self->new_other($model_name)->do_iterate(
+    _do_iterate(
+	@_,
 	sub {
 	    $c++;
 	    return 1;
-	}, 'unauth_iterate_start', undef, _get_iterator_params($query, $model_name)
+	},
     );
     return $c;
 }
 
 sub _handle_clear_form_button {
-    return {};
+    return {
+	carry_path_info => 1,
+    };
 }
 
 sub _handle_create_button {
@@ -253,32 +266,38 @@ sub _handle_create_button {
     my($model) = $self->new_other($model_name)->create({
 	map(
 	    {
-		my($v) = _value_form_to_db($model_name, $_,
-				           $self->unsafe_get("$model_name.$_"));
-		defined($v) && (($query || {})->{'_' . $_} == 1)
+		my($v) = _value_form_to_db(
+		    $self,
+		    $model_name,
+		    $_,
+		    $self->unsafe_get("$model_name.$_"),
+		);
+		defined($v) && (($query || {})->{'_' . $_} || 0) == 1
 		    ? ($_ => $v) : ();
 	    }
-		keys(%{get_all_fields()->{$model_name}}),
-	)});
-    my($res) =  {
-	s => 'Created',
-	n => 1,
+	    keys(%{_model_fields($self, $model_name)}),
+	),
+    });
+    my($res) = {
+	$_SEARCH => 'Created',
+	$_PAGE => 1,
 	map(
-	  ($_ => _value_db_to_query($model_name, $_, $model->unsafe_get($_))),
-	  @{$model->get_keys},
+	    ($_ => _value_db_to_query($self, $model_name, $_, $model->unsafe_get($_))),
+	    @{$model->get_keys},
 	),
     };
-    $res->{c} =  _get_row_count($self, $res, $model_name);
+    $res->{$_COUNT} = _get_row_count($self, $res, $model_name);
     return $res;
 }
 
 sub _handle_delete_button {
-    my($self, $query, $model_name) = @_;
-    return unless $query;
+    my($self, $query) = @_;
+    return
+	unless $query;
     if (my $row = _get_nth_row(@_)) {
 	$row->delete;
     }
-    return {s => 'Deleted'};
+    return {$_SEARCH => 'Deleted'};
 }
 
 sub _handle_first_button {
@@ -286,7 +305,7 @@ sub _handle_first_button {
     return unless $query;
     return {
 	%$query,
-        n => 1,
+        $_PAGE => 1,
     };
 }
 
@@ -295,8 +314,8 @@ sub _handle_next_button {
     return unless $query;
     return {
 	%$query,
-        n => $query->{n} + 1 <= $query->{c} ? $query->{n} + 1
-	    : $query->{c} > 0 ? $query->{c}
+        $_PAGE => $query->{$_PAGE} + 1 <= $query->{$_COUNT} ? $query->{$_PAGE} + 1
+	    : $query->{$_COUNT} > 0 ? $query->{$_COUNT}
 	    : 1,
     };
 }
@@ -306,7 +325,7 @@ sub _handle_last_button {
     return unless $query;
     return {
 	%$query,
-        n =>  $query->{c} > 0 ? $query->{c} : 1,
+        $_PAGE =>  $query->{$_COUNT} > 0 ? $query->{$_COUNT} : 1,
     };
 }
 
@@ -315,21 +334,27 @@ sub _handle_prev_button {
     return unless $query;
     return {
 	%$query,
-        n =>  $query->{n} > 1 ? $query->{n} - 1 : 1,
+        $_PAGE =>  $query->{$_PAGE} > 1 ? $query->{$_PAGE} - 1 : 1,
     };
 }
 
 sub _handle_search_button {
     my($self, $query, $model_name) = @_;
-    my($res) = {n => 1};
-    foreach my $field (keys(%{get_all_fields()->{$model_name}})) {
-	my($value) = _value_form_to_query($model_name, $field,
-					  $self->unsafe_get("$model_name.$field"));
-	next unless defined($value);
-	next unless (($query || {})->{'_' . $field} || 0) == 1; 
-	$res->{$field} = $value; 
+    my($res) = {$_PAGE => 1};
+    $query ||= {};
+    foreach my $field (keys(%{_model_fields($self, $model_name)})) {
+	my($value) = _value_form_to_query(
+	    $self,
+	    $model_name,
+	    $field,
+	    $self->unsafe_get("$model_name.$field"),
+	);
+	next
+	    unless defined($value)
+	    && ($query->{'_' . $field} || 0) == 1;
+	$res->{$field} = $value;
     }
-    $res->{c} =  _get_row_count($self, $res, $model_name);
+    $res->{$_COUNT} =  _get_row_count($self, $res, $model_name);
     return $res;
 }
 
@@ -338,66 +363,112 @@ sub _handle_update_button {
     if (my $row = _get_nth_row(@_)) {
 	$row->update({map(
 	    {
-		my($v) = _value_form_to_db($model_name, $_,
-					   $self->unsafe_get("$model_name.$_"));
+		my($v) = _value_form_to_db(
+		    $self,
+		    $model_name,
+		    $_,
+		    $self->unsafe_get("$model_name.$_"),
+		);
 		defined($v) ? ($_ => $v) : ();
 	    }
 	    @{$row->get_keys}
 	)});
 	my($res) = {
-	    s => 'Updated',
-	    n => 1,
+	    $_SEARCH => 'Updated',
+	    $_PAGE => 1,
 	    map(
 		{
-		    my($v) = _value_db_to_query($model_name, $_, $row->get($_));
+		    my($v) = _value_db_to_query($self, $model_name, $_, $row->get($_));
 		    defined($v) ? ($_ => $v) : ();
 		}
 		@{$row->get_keys}
 	    ),
 	};
-	$res->{c} =  _get_row_count($self, $res, $model_name);
+	$res->{$_COUNT} =  _get_row_count($self, $res, $model_name);
 	return $res;
     }
     return;
 }
 
+sub _model_fields {
+    my($self, $model) = @_;
+    return $self->get_all_fields->{$model};
+}
+
+sub _model_from_path_info {
+    my($self) = @_;
+    return undef
+	unless $self->ureq('path_info');
+    return $_PN->unsafe_from_path_info($self->req)
+	|| b_die($self->req('path_info'), ': invalid path_info');
+}
+
+sub _type_map {
+    my($type) = @_;
+    foreach my $map (
+	[qw(PrimaryId Integer)],
+	[Enum => $type],
+	[Number => 'Line'],
+	[EmailVerifyKey => 'Line'],
+	[EnumSet => 'Line'],
+	[DateTime => 'Line'],
+    ) {
+	return $map->[1]
+	    if $_T->get_instance($map->[0])->is_super_of($type);
+    }
+    return $type;
+}
+
 sub _value_db_to_query {
-    my($model_name, $field, $value) = @_;
-    my($t) = get_all_fields()->{$model_name}->{$field}->{type};
+    my($self, $model_name, $field, $value) = @_;
+    return
+	unless defined($value);
+    my($t) = _field_type($self, $model_name, $field);
     $value = $t->to_sql_param($value)
-	if (b_use($t)->isa('Bivio::Type::Enum'));
+	if $_E->is_super_of($t);
     return $value;
 }
 
 sub _value_db_to_form {
-    my($model_name, $field, $value) = @_;
-    return unless defined($value);
-    my($t) = get_all_fields()->{$model_name}->{$field}->{type};
-    $value = $t->to_xml($value) if $t eq 'Bivio::Type::DateTime';
-    $value = $t->to_literal($value) if $t->isa('Bivio::Type::EnumSet');
+    my($self, $model_name, $field, $value) = @_;
+    return
+	unless defined($value);
+    my($t) = _field_type($self, $model_name, $field);
+    return $t->to_xml($value)
+	if $_DT->is_super_of($t);
+    return $t->to_literal($value)
+	if $_ES->is_super_of($t);
     return $value;
 }
 
 sub _value_form_to_db {
-    my($model_name, $field, $value) = @_;
-    return unless defined($value);
-    my($t) = get_all_fields()->{$model_name}->{$field}->{type};
-    return $t->from_literal($value) if $t eq 'Bivio::Type::DateTime';
-    return $t->from_literal($value) if $t->isa('Bivio::Type::EnumSet');
+    my($self, $model_name, $field, $value) = @_;
+    return
+	unless defined($value);
+    my($t) = _field_type($self, $model_name, $field);
+    return $t->from_literal($value)
+	if $_DT->is_super_of($t);
+    return $t->from_literal($value)
+	if $_ES->is_super_of($t);
     return $value;
 }
 
 sub _value_form_to_query {
-    my($model_name, $field, $value) = @_;
-    return _value_db_to_query($model_name, $field,
-			      _value_form_to_db($model_name, $field, $value));
+    my($self, $model_name, $field, $value) = @_;
+    return _value_db_to_query(
+	$self,
+	$model_name,
+	$field,
+	_value_form_to_db($self, $model_name, $field, $value),
+    );
 }
 sub _value_query_to_db {
-    my($model_name, $field, $value) = @_;
-    return unless defined($value);
-    my($t) =  get_all_fields()->{$model_name}->{$field}->{type};
-    return  b_use($t)->from_int($value)
-	if b_use($t)->isa('Bivio::Type::Enum');
+    my($self, $model_name, $field, $value) = @_;
+    return
+	unless defined($value);
+    my($t) = _field_type($self, $model_name, $field);
+    return $t->from_int($value)
+	if $_E->is_super_of($t);
     return $value;
 }
 
--- Biz/Model/DBAccessModelList.pm
+++ Biz/Model/DBAccessModelList.pm
@@ -5,30 +5,23 @@ use strict;
 use Bivio::Base 'Biz.ListModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_DT) = b_use('Type.DateTime');
 my($_DBAMF) = b_use('Model.DBAccessModelForm');
 
 sub internal_initialize {
     my($self) = @_;
-    b_use('IO.Config')->assert_test;  
-    return $self->merge_initialize_info($self->SUPER::internal_initialize,
+    b_use('IO.Config')->assert_test;
+    return $self->merge_initialize_info(
+	$self->SUPER::internal_initialize,
         {
 	    version => '1',
 	    primary_key => [
-                {
-                    name => 'name',
-                    type => 'String',
-                    constraint => 'NONE',
-                },
+		$self->field_decl([[qw(name String)]]),
 	    ],
         });
-    return;
 }
 
 sub internal_load_rows {
-    my($self) = @_;
-    my(@result) = map(({name => $_}),  $_DBAMF->get_property_model_names);
-    return \@result;
+    return [map(({name => $_}), @{$_DBAMF->get_property_model_names})];
 }
 
 1;
