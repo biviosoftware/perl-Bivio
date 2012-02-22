8d0e983e0936213d5585b25920ff5b073cb4c89d
Rob Nagler <github@q33.us>
2012-02-22 03:07:03 +0000

    push up get_basic_authorization_realm, internal_validate_login_value, and validate_login from UserLoginForm so can be used by other apps

--- Biz/Model/UserLoginBaseForm.pm
+++ Biz/Model/UserLoginBaseForm.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2011-2012 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::UserLoginBaseForm;
 use strict;
@@ -13,6 +13,15 @@ sub PASSWORD_FIELD {
 sub USER_FIELD {
     # Returns the cookie key for the super user value.
     return 'u';
+}
+
+sub get_basic_authorization_realm {
+    my($self) = shift;
+    my($ro) = $self->unsafe_get('realm_owner');
+    return $ro && $ro->require_otp
+	# Extra space helps out on Mac, which puts a '.' right after realm
+	? 'Challenge: ' . $self->req('Model.OTP')->get_challenge . ' '
+	: '*';
 }
 
 sub internal_initialize {
@@ -79,6 +88,69 @@ sub internal_initialize {
         $field->{form_name} = $field->{name};
     }
     return $info;
+}
+
+sub internal_validate_login_value {
+    my($self, $value) = @_;
+    my($owner) = $self->new_other('RealmOwner');
+    my($err) = $owner->validate_login($value);
+    return $err ? (undef, $err) : ($owner, undef);
+}
+
+sub validate {
+    my($self, $login, $password) = @_;
+    # Checks the form property values.  Puts errors on the fields
+    # if there are any.
+    if (@_ == 3) {
+	$self->internal_put_field(login => $login);
+	$self->internal_put_field('RealmOwner.password' => $password);
+    }
+    _validate($self);
+    # don't send password back to client in error case
+    if ($self->in_error) {
+	$self->internal_put_field('RealmOwner.password' => undef);
+	$self->internal_clear_literal('RealmOwner.password');
+    }
+    return;
+}
+
+sub validate_login {
+    my($self, $model_or_login, $field) = @_;
+    $field ||= 'login';
+    my($model) = ref($model_or_login) ? $model_or_login : $self;
+    $model->internal_put_field($field => $model_or_login)
+	if defined($model_or_login) && !ref($model_or_login);
+    $model->internal_put_field(validate_called => 1);
+    my($login) = $model->get($field);
+    return undef
+	unless defined($login);
+    my($realm, $err) = $self->internal_validate_login_value($login);
+    $model->internal_put_error($field => $err)
+	if $err;
+    $model->internal_put_field(realm_owner => $realm);
+    return $realm;
+}
+
+sub _validate {
+    my($self) = @_;
+    my($owner) = $self->validate_login;
+    return
+	if !$owner || ($self->in_error && !$owner->require_otp);
+    unless ($owner->get_field_type('password')->is_equal(
+	$owner->get('password'),
+	$self->get('RealmOwner.password'),
+    )) {
+	return $self->internal_put_error(
+	    'RealmOwner.password', 'PASSWORD_MISMATCH',
+	) unless $owner->require_otp;
+	return $self->internal_put_error(
+	    'RealmOwner.password' => 'OTP_PASSWORD_MISMATCH'
+	) unless $self->new_other('OTP')->unauth_load_or_die({
+	    user_id => $owner->get('realm_id')
+	})->verify($self->get('RealmOwner.password'));
+    }
+    $self->internal_put_field(validate_called => 1);
+    return;
 }
 
 1;
