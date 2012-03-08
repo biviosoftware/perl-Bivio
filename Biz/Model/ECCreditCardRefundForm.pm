fbb8e88d9731b2fcd75df3fe78df14887d465132
Paul Moeller <moeller@bivio.biz>
2012-03-08 20:17:19 +0000

    now calls process_payment() directly

--- Biz/Model/ECCreditCardRefundForm.pm
+++ Biz/Model/ECCreditCardRefundForm.pm
@@ -1,12 +1,13 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2012 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Model::ECCreditCardRefundForm;
 use strict;
 use Bivio::Base 'Biz.FormModel';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_A) = __PACKAGE__->use('Type.Amount');
-my($_D) = __PACKAGE__->use('Type.Date');
+my($_A) = b_use('Type.Amount');
+my($_D) = b_use('Type.Date');
+my($_ECPS) = b_use('Type.ECPaymentStatus');
 
 sub execute_empty {
     my($self) = @_;
@@ -22,7 +23,7 @@ sub execute_ok {
     my($values) = $payment->get_shallow_copy;
     $values->{amount} = $_A->neg($self->get('ECPayment.amount'));
     $values->{description} .= ' Refund';
-    $values->{status} = Bivio::Type::ECPaymentStatus->TRY_CREDIT;
+    $values->{status} = $_ECPS->TRY_CREDIT;
     $values->{remark} = "Refund for payment on "
 	. $_D->to_string($payment->get('creation_date_time'));
 
@@ -34,7 +35,7 @@ sub execute_ok {
 	%{$cc_payment->get_shallow_copy},
 	ec_payment_id => $payment->get('ec_payment_id'),
 	processor_response => '',
-    });
+    })->process_payment($self);
     return;
 }
 
