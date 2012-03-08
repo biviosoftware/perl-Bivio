b6932a203b7ba88ad67f3044162d91f5411763d1
Justin Schell <schellj@bivio.biz>
2012-03-08 23:40:06 +0000

    add detach_user

--- Util/User.pm
+++ Util/User.pm
@@ -8,11 +8,22 @@ use Bivio::IO::TTY;
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_DT) = Bivio::Type->get_instance('DateTime');
 
+sub DETACH_MODELS {
+    return (
+	['Address', 'realm_id',
+	     [qw(street1 street2 city state zip country)]],
+	['Phone', 'realm_id', [qw(phone)]],
+	['User', 'user_id', [qw(birth_date gender)]],
+	['Website', 'realm_id', [qw(url)]],
+    );
+}
+
 sub USAGE {
     return <<'EOF';
 usage: b-user [options] command [args..]
 commands
     create_from_email email -- creates a new user, prompts for password
+    detach_user -- remove personal user data, invalidate email and remove from non-user realms
     merge_users source_user_id target_user_id -- merge two users
     realms -- returns realms of the current user
     unsubscribe_bulletin [user_id...] -- clears want_bulletin for auth_user_id or user_ids
@@ -32,6 +43,54 @@ sub create_from_email {
     }
     $self->initialize_ui;
     $self->new_other('RealmAdmin')->create_user($email, $u, $password, $u);
+    return;
+}
+
+sub detach_user {
+    my($self) = @_;
+    my($u) = $self->ureq('auth_user_id');
+    $self->usage_error('must provide user')
+	unless $u;
+    my($ro) = $self->model('RealmOwner')->unauth_load_or_die({
+	realm_id => $u,
+    });
+    $self->are_you_sure("Are you sure you want to remove personal data, invalidate email and remove from non-user realms for user @{[$ro->get('display_name')]}\n?");
+    foreach my $x ($self->DETACH_USER_MODELS) {
+	my($m) = $self->model($x->[0]);
+	$m->unauth_load({
+	    $x->[1] => $u,
+	});
+	$m->update({
+	    map({
+		$_ => $_ eq 'gender'
+		    ? b_use('Type.Gender')->UNKNOWN
+		    : undef,
+	    } @{$x->[2]}),
+	}) if $m->is_loaded;
+    }
+    $self->model('EmailVerify')->do_iterate(sub {
+	shift->delete;
+    }, 'unauth_iterate_start', {
+	realm_id => $u,
+    });
+    my($e) = $self->model('Email');
+    $e->unauth_load({
+	realm_id => $u,
+    });
+    $e->invalidate
+	if $e->is_loaded;
+    $self->model('RealmFileLock')->do_iterate(sub {
+	shift->delete;
+    }, 'unauth_iterate_start', {
+	user_id => $u,
+    });
+    $self->model('RealmUser')->do_iterate(sub {
+	my($ru) = @_;
+	$ru->delete
+	    unless $ru->get('realm_id') eq $u;
+    }, 'unauth_iterate_start', {
+	user_id => $u,
+    });
     return;
 }
 
