ce8701c6bebcb21bf8eee03226efccbbdc017c08
Andrew Schofield <andrews@bivio.biz>
2012-03-17 09:08:45 +0000

    Allow event_handler to be specified

--- UI/HTML/Widget/YesNo.pm
+++ UI/HTML/Widget/YesNo.pm
@@ -12,10 +12,16 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub initialize {
     my($self) = @_;
     return if $self->unsafe_get('values');
+    my($attrs) = {
+        map($self->unsafe_get($_)
+                ? ($_ => $self->get($_))
+                : (),
+                qw(event_handler)),
+    };    
     $self->put(values => [
-	Radio($self->get('field'), 1, 'Yes'),
+	Radio($self->get('field'), 1, 'Yes', $attrs),
 	vs_blank_cell(3),
-	Radio($self->get('field'), 0, 'No'),
+	Radio($self->get('field'), 0, 'No', $attrs),
     ]);
     return shift->SUPER::initialize(@_);
 }
