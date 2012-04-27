ca81bfac552128215fc73e25bb10c0ab6b60e502
Justin Schell <schellj@bivio.biz>
2012-04-27 16:29:48 +0000

    base

--- Biz/Model/EmailAliasList.pm
+++ Biz/Model/EmailAliasList.pm
@@ -2,7 +2,7 @@
 # $Id$
 package Bivio::Biz::Model::EmailAliasList;
 use strict;
-use base 'Bivio::Biz::ListModel';
+use Bivio::Base 'Biz.ListModel';
 use Digest::MD5 ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
