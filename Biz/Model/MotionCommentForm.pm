09cde5b22a95add0622304f6d8a0070966e9331c
Paul Moeller <moeller@bivio.biz>
2012-06-07 00:25:58 +0000

    now derives from FormModeBaseForm

--- Biz/Model/MotionCommentForm.pm
+++ Biz/Model/MotionCommentForm.pm
@@ -2,13 +2,21 @@
 # $Id$
 package Bivio::Biz::Model::MotionCommentForm;
 use strict;
-use Bivio::Base 'Biz.FormModel';
+use Bivio::Base 'Model.FormModeBaseForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_MC) = b_use('Model.MotionComment');
 my($_TAG_ID) = 'MotionComment.motion_comment_id';
 b_use('ClassWrapper.TupleTag')->wrap_methods(
     __PACKAGE__, __PACKAGE__->TUPLE_TAG_INFO);
+
+sub LIST_MODEL {
+    return 'MotionCommentList';
+}
+
+sub PROPERTY_MODEL {
+    return 'MotionComment';
+}
 
 sub TUPLE_TAG_INFO {
     return {
@@ -17,19 +25,30 @@ sub TUPLE_TAG_INFO {
     };
 }
 
-sub execute_empty {
-    my($self) = @_;
-    $self->load_from_model_properties('Motion');
+sub execute_empty_create {
     return;
 }
 
-sub execute_ok {
+sub execute_empty_edit {
+    my($self) = @_;
+    $self->load_from_model_properties('MotionComment');
+    return;
+}
+
+sub execute_ok_create {
     my($self) = @_;
     return unless $self->req('Model.MotionList')->can_comment;
     $self->internal_put_field('MotionComment.motion_comment_id' =>
         $self->new_other('MotionComment')
 	    ->create($self->get_model_properties('MotionComment'))
 		->get('motion_comment_id'));
+    return;
+}
+
+sub execute_ok_edit {
+    my($self) = @_;
+    $self->req('Model.MotionComment')->update(
+	$self->get_model_properties('MotionComment'));
     return;
 }
 
@@ -41,7 +60,6 @@ sub get_tuple_use_moniker {
 sub internal_initialize {
     my($self) = @_;
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
-        version => 1,
 	require_context => 1,
 	visible => [qw(
 	    MotionComment.comment
@@ -62,7 +80,8 @@ sub internal_pre_execute {
     my($self) = @_;
     $self->internal_put_field('Motion.motion_id' =>
         $self->req(qw(Model.MotionList Motion.motion_id)));
-    return;
+    $self->load_from_model_properties($self->get_model('Motion'));
+    return shift->SUPER::internal_pre_execute(@_);
 }
 
 1;
