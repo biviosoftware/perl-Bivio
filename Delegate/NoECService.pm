1e166257951bfb3b60a15bbdb0afc45bd41a99e4
Rob Nagler <github@q33.us>
2012-09-09 20:10:02 +0000

    rmpod

--- Delegate/NoECService.pm
+++ Delegate/NoECService.pm
@@ -2,70 +2,18 @@
 # $Id$
 package Bivio::Delegate::NoECService;
 use strict;
-$Bivio::Delegate::NoECService::VERSION = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-$_ = $Bivio::Delegate::NoECService::VERSION;
+use Bivio::Base 'Bivio::Delegate';
 
-=head1 NAME
+# C<Bivio::Delegate::NoECService>
 
-Bivio::Delegate::NoECService - Empty enumerated type.
-
-=head1 RELEASE SCOPE
-
-bOP
-
-=head1 SYNOPSIS
-
-    use Bivio::Delegate::NoECService;
-
-=cut
-
-=head1 EXTENDS
-
-L<Bivio::Delegate>
-
-=cut
-
-use Bivio::Delegate;
-@Bivio::Delegate::NoECService::ISA = ('Bivio::Delegate');
-
-=head1 DESCRIPTION
-
-C<Bivio::Delegate::NoECService>
-
-=cut
-
-#=IMPORTS
-
-#=VARIABLES
-
-=head1 METHODS
-
-=cut
-
-=for html <a name="get_delegate_info"></a>
-
-=head2 get_delegate_info() : array_ref
-
-Returns an enumerated type with one value, UNKNOWN.
-
-=cut
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub get_delegate_info {
+    # (self) : array_ref
+    # Returns an enumerated type with one value, UNKNOWN.
     return [
 	UNKNOWN => [0],
     ];
 }
-
-#=PRIVATE METHODS
-
-=head1 COPYRIGHT
-
-Copyright (c) 2002 bivio Software, Inc.  All Rights Reserved.
-
-=head1 VERSION
-
-$Id$
-
-=cut
 
 1;
