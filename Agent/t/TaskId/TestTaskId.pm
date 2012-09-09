7760d0e4f6272877ba3459a0b823850cbf9b0e80
Rob Nagler <github@q33.us>
2012-09-09 21:53:29 +0000

    *** empty log message ***

---
+++ Agent/t/TaskId/TestTaskId.pm
@@ -0,0 +1,25 @@
+# Copyright (c) 2012 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Agent::t::TaskId::TestTaskId;
+use strict;
+use Bivio::Base 'Delegate.TaskId';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub get_delegate_info {
+    my($proto) = @_;
+    return $proto->merge_task_info(
+	'blog',
+	[
+	    [qw(
+	        TEST_TASK_ID_1
+		500
+		ANY_OWNER
+		ANYBODY
+		Action.EmptyReply
+	    )],
+	],
+    );
+}
+
+1;
