2f3d110558f8e67d405cd46d0027f8ce7938154f
Rob Nagler <github@q33.us>
2012-09-09 21:51:53 +0000

    Took business logic from Delegate.TaskId and moved here.  This was allowed by having Delegate.TaskId subclass Type.EnumDelegate so that methods could be called from the enum directly. merge_task_info was computing $_INCLUDED_COMPONENT (was $_INCLUDED), but this was wrong since merge_task_info is simply stateless business logic.  Rather have _compile() set that.  In order to make that work, needed to create dummy config _TASK_COMPONENT_<name> which gets set by merge_task_info when components are referenced.  These are then stripped by _compile()

--- Agent/TaskId.pm
+++ Agent/TaskId.pm
@@ -1,47 +1,175 @@
-# Copyright (c) 1999-2008 bivio Software, Inc.  All rights reserved.
+# Copyright (c) 1999-2012 bivio Software, Inc.  All rights reserved.
 # $Id$
 package Bivio::Agent::TaskId;
 use strict;
 use Bivio::Base 'Type.EnumDelegator';
 
-# C<Bivio::Agent::TaskId> defines all possible "tasks" within bOP site.  A
-# structure of a task is defined in L<Bivio::Agent::TaskBivio::Agent::Task>.
-#
-# The syntax of the configuration table is defined as follows:
-#
-#     NAME_OF_TASK
-#     <unique number for enumerated type>
-#     <Bivio::Auth::RealmType>
-#     <Bivio::Auth::PermissionSet>
-#     <executable object1>
-#     <executable object2>
-#     ...
-#     attribute1=value1
-#     attribute2=value2
-#     ...
-#
-# The first two entries are what defines the TaskId enumerated type.
-# The subsequent entries are configuration for the Task instance itself.
-# See L<Bivio::Agent::Task|Bivio::Agent::Task> for a description.
-
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_CFG) = b_use('IO.ClassLoader')->delegate_require_info(__PACKAGE__);
-__PACKAGE__->compile([
-    map(
-	(
-	    $_->{name},
-	    [$_->{int} || b_die($_, ': missing int')],
-	),
-	@{__PACKAGE__->get_cfg_list},
-    ),
-]);
+my($_INFO_RE) = qr{^info_(.*)};
+my($_INCLUDED_COMPONENT) = {};
+my($_PS) = b_use('Auth.PermissionSet');
+my($_C) = b_use('IO.Config');
+my($_A) = b_use('IO.Alert');
+my($_CFG);
+_compile(__PACKAGE__);
+
+sub bunit_validate_all {
+    # Sanity check to make sure the the list of info_ methods don't collide
+    my($proto) = @_;
+    my($seen) = {};
+    foreach my $c (@{$proto->standard_components}) {
+	foreach my $t (@{_component_info($proto, $c) || []}) {
+	    my($n) = $t->[0];
+	    Bivio::Die->die($c, ' and ', $seen->{$n}, ': both define ', $n)
+	        if $seen->{$n};
+	    $seen->{$n} = $c;
+	}
+    }
+    return;
+}
+
+sub canonicalize_task_info {
+    my($proto, $info) = @_;
+    my($canonicalize) = sub {
+	my($cfg) = @_;
+	if (ref($cfg) eq 'HASH') {
+	    if ($cfg->{permissions}) {
+		$_A->warn_deprecated(
+		    $cfg->{name},
+		    ': permissions deprecated, use permission_set',
+		);
+		$cfg->{permission_set} = delete($cfg->{permissions});
+	    }
+	    return $cfg;
+	}
+	if (ref($cfg) eq 'ARRAY') {
+	    return {
+		name => shift(@$cfg),
+		int => shift(@$cfg),
+		realm_type => shift(@$cfg),
+		permission_set => shift(@$cfg),
+		items => [grep(!/=/, @$cfg)],
+		map(split(/=/, $_, 2), grep(/=/, @$cfg)),
+	    };
+	}
+	b_die($cfg, ': invalid config format');
+        # DOES NOT RETURN
+    };
+    my($seen) = {};
+    my($validate) = sub {
+	my($cfg) = @_;
+	b_die($cfg, 'name: missing from: ', $cfg)
+	    unless $cfg->{name};
+	b_die($cfg->{name}, ': duplicate name')
+	    if $seen->{$cfg->{name}}++;
+	return $cfg;
+    };
+    return [map($validate->($canonicalize->($_)), @$info)];
+}
 
 sub get_cfg_list {
     return $_CFG;
 }
 
+sub included_components {
+    return [sort _sort keys(%$_INCLUDED_COMPONENT)];
+}
+
+sub is_component_included {
+    my(undef, $component) = @_;
+    return $_INCLUDED_COMPONENT->{$component} || 0;
+}
+
 sub is_continuous {
     return 0;
+}
+
+sub merge_task_info {
+    my($proto, @info) = @_;
+    my($merge) = sub {
+	my($info) = @_;
+	my($map) = {};
+	foreach my $cfg (@$info) {
+	    $map->{$cfg->{name}} = {
+		%{$map->{$cfg->{name}} || {}},
+		%$cfg,
+	    };
+	}
+	return [sort(
+	    {$a->{int} <=> $b->{int}}
+	    values(%$map),
+	)];
+    };
+    my($info) = sub {
+	my($component) = @_;
+	return $component
+	    if ref($component);
+	return []
+	    unless my $tasks = _component_info($proto, $component);
+	return [
+	    {
+		name => "_TASK_COMPONENT_$component",
+		int => 0,
+	    },
+	    @$tasks,
+	];
+    };
+    return $merge->([
+	map(@{$proto->canonicalize_task_info($info->($_))}, @info),
+    ]);
+}
+
+sub standard_components {
+    my($proto) = @_;
+    return [sort(
+        _sort
+	grep(
+	    $_ ne 'otp'
+		&& $_C->if_version(10, 1, sub {$_ ne 'task_log'}),
+	    @{$proto->internal_delegate_package->grep_methods($_INFO_RE)},
+	),
+    )];
+}
+
+sub _compile {
+    my($proto) = @_;
+    return
+	if $_CFG;
+    $_CFG = [];
+    foreach my $cfg (@{$proto->internal_delegate_package->get_delegate_info}) {
+	if ($cfg->{name} =~ /_TASK_COMPONENT_(\w+)/) {
+	    $_INCLUDED_COMPONENT->{lc($1)}++;
+	}
+	else {
+	    push(@$_CFG, $cfg);
+	}
+    }
+    $proto->compile([
+	map(
+	    (
+		$_->{name},
+		[$_->{int} || b_die($_, ': missing int')],
+	    ),
+	    @{$proto->get_cfg_list},
+	)
+    ]);
+    return;
+}
+
+sub _component_info {
+    my($proto, $component) = @_;
+    my($method) = "info_$component";
+    my($delegate) = $proto->internal_delegate_package;
+    b_die($component, ': no such info_* component in ', $delegate)
+        unless $delegate->can($method);
+    return $delegate->$method;
+}
+
+sub _sort {
+    return $a eq $b ? 0
+	: $a eq 'base' ? -1
+	: $b eq 'base' ? +1
+	: $a cmp $b;
 }
 
 1;
