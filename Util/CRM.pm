51432ad2b96efe9a49577a1c756111f8c44caccb
Rob Nagler <github@q33.us>
2012-10-15 17:13:07 +0000

    removed setup_realm_with_priority setup_realm takes a priority and CRM_SUBJECT_PREFIX but defaults to priority 1-3.

--- Util/CRM.pm
+++ Util/CRM.pm
@@ -10,31 +10,46 @@ sub USAGE {
     return <<'EOF';
 usage: b-crm [options] command [args..]
 commands
-  setup_realm -- sets +feature_crm and EMPTY_SUBJECT_PREFIX on realm
-  setup_realm_with_priority [max_priority] -- set_realm and then create b_ticket with Priority
+  setup_realm [prefix] [priority]-- sets up tickets for realm
 EOF
 }
 
 sub setup_realm {
-    my($self) = @_;
+    sub SETUP_REALM {[
+	[qw(?prefix Line)],
+	[qw(?max_priority NonNegativeInteger 3)],
+    ]}
+    my($self, $bp) = shift->parameters(\@_);
     $self->initialize_fully;
     $self->new_other('RealmRole')->edit_categories('+feature_crm');
     $self->model('RealmFeatureForm', {mail_want_reply_to => 1});
     $self->model('RowTag')->replace_value(
 	MAIL_SUBJECT_PREFIX => b_use('Action.RealmMail')->EMPTY_SUBJECT_PREFIX,
     );
+    _prefix($self, $bp->{prefix});
+    _priority($self, $bp->{priority});
     return;
 }
 
-sub setup_realm_with_priority {
-    sub SETUP_REALM_WITH_PRIORITY {[[qw(max_priority PositiveInteger 3)]]}
-    my($self, $bp) = shift->parameters(\@_);
-    $self->setup_realm;
+sub _prefix {
+    my($self, $prefix) = @_;
+    return
+	unless $prefix;
+    $self->model('RowTag')->replace_value(
+	CRM_SUBJECT_PREFIX => $prefix,
+    );
+    return;
+}
+
+sub _priority {
+    my($self, $max_priority) = @_;
+    return
+	unless $max_priority;
     $self->model('TupleSlotType')->create_from_hash({
 	Priority => {
 	    type_class => 'TupleSlot',
-	    choices => [1 .. $bp->{max_priority}],
-	    default_value => $bp->{max_priority},
+	    choices => [1 .. $max_priority],
+	    default_value => $max_priority,
 	},
     });
     $self->model('TupleDef')->create_from_hash({
