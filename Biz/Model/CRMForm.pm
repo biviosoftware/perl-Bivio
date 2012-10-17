7aaa048377baf42ab6a919d8f50fcd8ca00aa413
Justin Schell <schellj@bivio.biz>
2012-10-17 23:22:02 +0000

    - change from_display_name to be user, not realm. - send message to board or realm with current status and updated fields if any changed.

--- Biz/Model/CRMForm.pm
+++ Biz/Model/CRMForm.pm
@@ -16,6 +16,7 @@ b_use('ClassWrapper.TupleTag')->wrap_methods(
 #TODO: Locked needs to limit users from acting (are you sure?)
 #TODO: Verify that auth_realm is in the list of emails????
 #TODO: Bounce handling
+my($_TS) = b_use('Type.TupleSlot');
 
 sub TUPLE_TAG_INFO {
     return {
@@ -33,11 +34,13 @@ sub execute_cancel {
 sub execute_empty {
     my($self) = @_;
     shift->SUPER::execute_empty(@_);
+    my($status);
     $self->internal_put_field(
 	_if_crm_thread(
 	    $self,
 	    sub {
 		my($ct, $cal) = @_;
+		$status = $ct->get('crm_thread_status');
 		_acquire_lock($ct)
 		    unless my $discuss = $self->internal_query_who->eq_realm;
 		return (
@@ -50,6 +53,7 @@ sub execute_empty {
 	    },
 	    sub {
 		my($cal) = @_;
+		$status = $_CTS->UNKNOWN;
 		return (
 		    to => undef,
 		    cc => $self->get('to'),
@@ -58,6 +62,9 @@ sub execute_empty {
 		);
 	    },
 	),
+    );
+    $self->internal_put_field(
+	old_status => $status,
     );
     return;
 }
@@ -68,22 +75,68 @@ sub execute_ok {
 	: shift->SUPER::execute_ok(@_);
     return if $self->in_error;
     my($ct) = $self->req('Model.CRMThread');
+    $self->internal_put_field(old_fields => {
+	crm_thread_status => $self->unsafe_get('old_status')
+	    || $ct->get('crm_thread_status'),
+	owner_user_id => $ct->get('owner_user_id'),
+    });
     my($cal, $cid) = $self->unsafe_get(
 	qw(crm_action_list CRMThread.customer_realm_id));
     my($id) = $self->get('action_id');
     my($status) = $cal->id_to_status($id);
+    my($owner) = $cal->id_to_owner($id);
     $ct->update({
 	crm_thread_status => $status,
-	owner_user_id => $cal->id_to_owner($id),
+	owner_user_id => $owner,
 	modified_by_user_id => $self->req('auth_user_id'),
 	lock_user_id => $status->eq_locked ? $self->req('auth_user_id')
 	    : undef,
 	subject => $self->get('subject'),
 	$cid ? (customer_realm_id => $cid) : (),
     });
+    $self->internal_put_field(new_fields => {
+	crm_thread_status => $status,
+	owner_user_id => $owner,
+    });
     $self->internal_put_field(
 	$_TAG_ID => $ct->get('thread_root_id'));
     return $res;
+}
+
+sub handle_tuple_tag_update_properties {
+    my($self, $old, $new, $info) = @_;
+    return
+	if $self->in_error;
+    $self->internal_put_field(
+	map({
+	    my($which, $v) = @$_;
+	    ($which => {
+		%{$self->get($which)},
+		map((
+		    $info->{$_}->{tuple_tag_label}
+			=> [$v->{$_}, $info->{$_}->{type}],
+		), keys(%$info)),
+	    });
+	} (['old_fields', $old], ['new_fields', $new])),
+    );
+    if (_fields_changed($self->get('old_fields'), $self->get('new_fields'))) {
+	my($board_only) = $self->internal_set_headers;
+	return
+	    if $self->in_error;
+	my($headers) = $self->get('headers');
+	delete($headers->{_recipients});
+	delete($headers->{Cc});
+	$headers->{To} = $board_only
+	    ? $_BRM->format_email_for_realm($self->req)
+	    : $self->get('realm_email');
+	$self->internal_put_field(headers => $headers);
+	$board_only
+	    ? $self->internal_send_to_board(
+		$self->internal_format_field_updates)
+	    : $self->internal_send_to_realm(
+		$self->internal_format_field_updates);
+    }
+    return;
 }
 
 sub internal_empty_status_when_exists {
@@ -94,6 +147,13 @@ sub internal_empty_status_when_new {
     return $_CTS->OPEN;
 }
 
+sub internal_format_field_updates {
+    my($self) = @_;
+    return b_use('UI.View')->render(
+	$self->VIEW_CLASS . '->field_updates_imail', $self->req);
+    return;
+}
+
 sub internal_format_from {
     my($self, @args) = @_;
     return _if_crm_thread(
@@ -101,7 +161,7 @@ sub internal_format_from {
 	sub {
 	    return $_RFC->format_mailbox(
 		$self->new_other('EmailAlias')->format_realm_as_incoming,
-		$self->req(qw(auth_realm owner display_name)),
+		$self->req(qw(auth_user display_name)),
 	    );
 	},
 	sub {$self->SUPER::internal_format_from(@args)},
@@ -138,9 +198,14 @@ sub internal_initialize {
 		[qw(action_id Line)],
 		[qw(update_only OKButton)],
 	    ],
+	    hidden => [
+		[qw(old_status CRMThreadStatus)],
+	    ],
 	    other => [
 		$_TAG_ID,
 		[qw(crm_action_list Model.CRMActionList)],
+		[qw(old_fields Hash)],
+		[qw(new_fields Hash)],
 	    ],
 	),
     });
@@ -197,6 +262,17 @@ sub _action_id_for_owner_and_status {
         || $cal->status_to_id_in_list($ct->get('crm_thread_status'))
             : $cal->status_to_id_in_list(
                 $self->internal_empty_status_when_exists);
+}
+
+sub _fields_changed {
+    my($old, $new) = @_;
+    b_die('field mismatch')
+	unless scalar(keys(%$old)) == scalar(keys(%$new));
+    for (keys(%$new)) {
+	return 1
+	    if $_TS->compare($old->{$_}, $new->{$_});
+    }
+    return 0;
 }
 
 sub _if_crm_thread {
