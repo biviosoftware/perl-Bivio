1545fe2c3e92025d3f312b13ece31fd436477d33
Rob Nagler <github@q33.us>
2012-10-27 19:52:51 +0000

    is_production needs to be true

--- PetShop/Facade/RequireSecure.pm
+++ PetShop/Facade/RequireSecure.pm
@@ -10,6 +10,7 @@ __PACKAGE__->new({
     uri => 'requiresecure',
     http_host => 'requiresecure.bivio.biz',
     mail_host => 'requiresecure.bivio.biz',
+    is_production => 1,
     clone => 'PetShop',
     Constant => [
 	[require_secure => 1],
