21c3e0950da37c8d3887c2eb79a2a77fbb921b21
Andrew Schofield <andrews@bivio.biz>
2012-11-16 16:43:29 +0000

    Move NormForge MIME::Calendar extensions to bOP

--- MIME/Calendar.pm
+++ MIME/Calendar.pm
@@ -3,15 +3,81 @@
 package Bivio::MIME::Calendar;
 use strict;
 use Bivio::Base 'Collection.Attributes';
+use POSIX ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_D) = b_use('Type.Date');
 my($_DT) = b_use('Type.DateTime');
 my($_HTML) = b_use('Bivio::HTML');
 my($_S) = b_use('Type.String');
+my($_SECONDS_IN_WEEK) = $_DT->SECONDS_IN_DAY * 7;
 my($_TZ) = b_use('Type.TimeZone');
 
+sub COMMON_TIME_ZONES {
+    return [
+        $_TZ->PACIFIC_HONOLULU,
+        $_TZ->AMERICA_ANCHORAGE,
+        $_TZ->AMERICA_ADAK,
+        $_TZ->AMERICA_LOS_ANGELES,
+        $_TZ->AMERICA_DENVER,
+        $_TZ->AMERICA_CHICAGO,
+        $_TZ->AMERICA_NEW_YORK,
+        $_TZ->UTC,
+        $_TZ->EUROPE_BERLIN,
+        $_TZ->EUROPE_ATHENS,
+        $_TZ->EUROPE_MOSCOW,
+        $_TZ->ASIA_KARACHI,
+        $_TZ->ASIA_SHANGHAI,
+        $_TZ->ASIA_TOKYO,
+        $_TZ->AUSTRALIA_SYDNEY,
+        $_TZ->PACIFIC_AUCKLAND,
+    ];
+}
+
 sub from_ics {
+    my($proto, $ics) = @_;
+    return _split(
+        $proto->new({
+            ics => $ics,
+            row_num => 0,
+            vevents => [],
+        }));
+}
+
+sub guess_time_zone {
+    my($self, $time) = @_;
+    if (my $tz = $_TZ->unsafe_from_any($self->unsafe_get('time_zone_id'))) {
+        return $tz;
+    }
+    $time ||= $_DT->now;
+    my($utc) = $self->to_utc($time);
+    my(@tzs) = $_TZ->get_list;
+    foreach my $tz (@{$self->COMMON_TIME_ZONES}) {
+        return $tz
+            unless $_DT->compare_defined($utc, $tz->date_time_to_utc($time));
+    }
+    return $_TZ->UTC;
+}
+
+sub to_utc {
+    my($self, $time) = @_;
+    $time ||= $_DT->now;
+    my($transitions) = _tz_transitions_for_year($self, $time);
+    my($offset);
+    foreach my $transition (@$transitions) {
+        if ($_DT->compare_defined($time, $transition->{start}) < 0) {
+            $offset ||=  $transition->{offset_from};
+            last;
+        } else {
+            $offset = $transition->{offset_to};
+        }
+    }
+    return $_DT->add_seconds($time, - $offset)
+        if defined($offset);
+    return $time;
+}
+
+sub vevents_from_ics {
     my($proto, $ics) = @_;
     return [
 	sort({
@@ -26,15 +92,10 @@ sub from_ics {
 		return $a->{rrule} ? 1 : -1;
 	    }
 	    return 0;
-	} @{_split(
-	    $proto->new({
-		ics => $ics,
-		row_num => 0,
-		vevents => [],
-	    }),
-	)->get('vevents')}),
+	} @{$proto->from_ics($ics)->get('vevents')}),
     ];
 }
+
 
 sub _assert {
     my($self, $expect) = @_;
@@ -61,7 +122,6 @@ sub _die {
 sub _event {
     my($self) = @_;
     my($ve) = {};
-    my($default_tz) = $self->unsafe_get('time_zone');
     _do_until($self, 'end', sub {
         my($k, $v) = @_;
         return 1
@@ -75,23 +135,28 @@ sub _event {
 		|organizer
 		|priority
 		|transp
+                |x-alt-[^;]*
+                |x-entourage[^;]*
 	        |x-lic-error
+                |x-microsoft-[^;]*
+                |x-ms-[^;]*
 		|contact
 	    )(?:$|;)}x;
 	if ($k =~ /^(dtstart|dtend|recurrence-id|exdate)(;value=date(?:-time)?)?(;tzid=(.*))?$/) {
 	    my($w) = $1;
 	    my($is_date) = $2 && $2 =~ /date$/ ? 1 : 0;
-	    my($tz) = $3 ? $4 : $default_tz;
+	    my($tz) = $3 ? $4 : $self->unsafe_get('time_zone_id');
 	    my($is_gmt) = $v =~ /Z$/;
 	    my($t, $e) = ($is_date ? $_D : $_DT)->from_literal(
 		$v . ($is_date || $is_gmt ? '' : 'Z'));
 	    _die($self, $v, ": failed to parse $k: ", $e)
 		unless $t;
-	    $ve->{time_zone} = $tz ? $_TZ->from_any($tz) : $_TZ->UTC;
+	    $ve->{time_zone} = $_TZ->unsafe_from_any($tz)
+                || $self->guess_time_zone($t);
 	    $k = $w;
 	    $v = $is_date || $is_gmt
 		? $t
-		: $ve->{time_zone}->date_time_to_utc($t);
+		: $self->to_utc($t);
 	}
 	elsif ($k eq 'tzid') {
 	    $ve->{time_zone} = $_TZ->from_any($v);
@@ -111,10 +176,13 @@ sub _event {
 	elsif ($k =~ /^(url)(;value=uri)?$/) {
 	    $k = $1;
 	}
+	elsif ($k =~ /^(summary)(;language=(.*))?$/) {
+	    $k = $1;
+	}
 	elsif ($k !~ m{^(?:
-	    summary
-	    |description
+	    description
 	    |location
+            |method
 	    |class
 	    |url
 	    |uid
@@ -153,8 +221,12 @@ sub _header {
 	    $end_vcalendar = 1;
 	    return 0;
 	}
+        if ($k eq 'method') {
+            $self->put($k => $v);
+            return 1;
+        }
 	_die($self, 'unknown element')
-	    unless $k =~ /^(version|prodid|method|calscale|(x-wr-.*))$/;
+	    unless $k =~ /^(version|prodid|calscale|(x-wr-.*)|(x-ms-.*))$/;
 	return 1;
     });
     return $self
@@ -166,7 +238,7 @@ sub _header {
 	my($type) = lc($v);
 
 	if ($type eq 'vtimezone') {
-	    _timezone($self);
+	    $self->put(time_zone_periods => _timezone($self));
 	}
 	elsif ($type eq 'vevent') {
 	    _event($self);
@@ -199,6 +271,23 @@ sub _next_row {
     return $r;
 }
 
+sub _parse_value_params {
+    my($self, $value, $sep) = @_;
+    $sep ||= ';';
+    my($res) = {};
+    foreach my $part (split($sep, $value)) {
+        my($n, $v) = $part =~ /(^[^=]+)=(.*)$/;
+        $res->{lc($n)} = $v;
+    }
+    return $res;
+}
+
+sub _seconds_from_hhmmss {
+    my($self, $value) = @_;
+    my($hours, $minutes, $seconds) = ($value || '') =~ /^([+\-]\d\d)(\d\d)?(\d\d)?/;
+    return (($hours * 60) + $minutes) * 60 + $seconds;
+}
+
 sub _split {
     my($self) = @_;
     (my $ics = ${$self->get('ics')}) =~ s/\r?\n( |\t)//g;
@@ -221,16 +310,93 @@ sub _split {
 
 sub _timezone {
     my($self) = @_;
+    my($res) = {};
     _do_until($self, 'end', sub {
         my($k, $v) = @_;
-	$self->put(time_zone => $v)
+        _die($self, ['repeated timezone'])
+            if $self->unsafe_get('time_zone_periods');
+	$self->put(time_zone_id => $v)
 	    if $k eq 'tzid';
 	return 1
 	    unless $k eq 'begin';
-	_ignore_subentry($self, $v);
+        $res->{$v} = _timezone_period($self, $v);
 	return 1;
     });
-    return;
+    return $res;
+}
+
+sub _timezone_period {
+    my($self, $type) = @_;
+    $type = lc($type);
+    my($res) = {};
+    _do_until($self, 'end', sub {
+        my($k, $v) = @_;
+        $res->{$k} = $v;
+        return 1;
+     });
+    _assert($self, [end => $type]);
+    return $res;
+}
+
+sub _tz_transition_for_year {
+    my($self, $time, $period) = @_;
+    my($dtstart) = $period->{dtstart};
+    return
+        unless defined($dtstart)
+            && defined($period->{tzoffsetfrom})
+            && defined($period->{tzoffsetto});
+    $dtstart .= 'Z'
+        unless $dtstart =~ /Z$/;
+    $dtstart =~ s/^16\d\d/1970/;
+    return {
+        start => $_DT->from_literal($dtstart),
+        offset_from => _seconds_from_hhmmss($self, $period->{tzoffsetfrom}),
+        offset_to => _seconds_from_hhmmss($self, $period->{tzoffsetto}),
+    } unless defined($period->{rrule});
+    # nearly all the world's timezone transitions are
+    # on the nth Sunday of a given month see
+    # http://www.webexhibits.org/daylightsaving/g.html
+    my($sec, $min, $hour, $day, $mon) = $_DT->to_parts($_DT->from_literal($dtstart));
+    my($dt) = $_DT->from_parts_or_die($sec, $min, $hour, 1, $mon,
+                                      $_DT->get_parts($time, 'year'));
+    my($unix) = $_DT->to_unix($dt);
+    $unix = (POSIX::floor($unix / $_SECONDS_IN_WEEK) * $_SECONDS_IN_WEEK)
+        + ($unix % $_DT->SECONDS_IN_DAY);
+    $unix -= 4 * $_DT->SECONDS_IN_DAY;
+    my($sundays) = [];
+    foreach my $i (1 .. 6) {
+        push(@$sundays, $_DT->from_unix($unix))
+            if (gmtime($unix))[4] + 1 == $mon;
+        $unix += $_SECONDS_IN_WEEK;
+    }
+    my($rule) = _parse_value_params($self, $period->{rrule});
+    return
+        unless defined($rule->{freq})
+            && defined($rule->{bymonth})
+            && defined($rule->{byday})
+            && $rule->{freq} eq 'YEARLY'
+            && $rule->{byday} =~ /^-?\dSU$/;
+    my($n) = $rule->{byday} =~ /^(-?\d)/;
+    $n -= 1
+        if $n > 0;
+    return {
+        start => $sundays->[$n],
+        offset_from => _seconds_from_hhmmss($self, $period->{tzoffsetfrom}),
+        offset_to => _seconds_from_hhmmss($self, $period->{tzoffsetto}),
+    };
+}
+
+sub _tz_transitions_for_year {
+    my($self, $time) = @_;
+    my($tzp) = $self->unsafe_get('time_zone_periods');
+    my ($res) = [];
+    foreach my $k (keys(%$tzp)) {
+        my($period) = $tzp->{$k};
+        if (my($transition) = _tz_transition_for_year($self, $time, $tzp->{$k})) {
+            push(@$res, $transition);
+        }
+    }
+    return [sort({$_DT->compare_defined($a->{start}, $b->{start})} @$res)];
 }
 
 1;
