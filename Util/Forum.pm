ba2b59be911b0aac9e4866f54b24649418cad154
Rob Nagler <github@q33.us>
2012-11-30 20:39:07 +0000

    cascade_delete_forum_and_users: must initialize_fully

--- Util/Forum.pm
+++ Util/Forum.pm
@@ -32,7 +32,7 @@ EOF
 sub cascade_delete_forum_and_users {
     my($self) = @_;
     IO_Config()->assert_test;
-    my($req) = $self->req;
+    my($req) = $self->initialize_fully;
     $req->get('auth_realm')->assert_type('forum');
     $self->are_you_sure('Delete forum, users, and children of ' . $self->req(qw(auth_realm owner_name)));
     $self->put(force => 1);
