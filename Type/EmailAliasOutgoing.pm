af574b5e3bc96c1e976f76a5d4ea9ab35ae2f2ba
Rob Nagler <github@q33.us>
2012-12-03 21:31:22 +0000

    added format_domain

--- Type/EmailAliasOutgoing.pm
+++ Type/EmailAliasOutgoing.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2012 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Type::EmailAliasOutgoing;
 use strict;
@@ -8,18 +8,29 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_DN) = b_use('Type.DomainName');
 my($_ERR) = b_use('Bivio.TypeError')->EMAIL_ALIAS_OUTGOING;
 
+sub format_domain {
+    my(undef, $value) = @_;
+    return '@' . $value;
+}
+
 sub from_literal {
     my($proto, $value) = @_;
     my($v, $e) = shift->SUPER::from_literal(@_);
     return ($v, $e)
-	unless $e && $e == Bivio::TypeError->EMAIL;
+	unless $e && $e->eq_email;
     if ($value =~ s/^\@//) {
 	($v, $e) = $_DN->from_literal($value);
-	return $v ? ('@' . $v, undef) : ($v, $e);
+	return $v ? ($proto->format_domain($v), undef) : ($v, $e);
     }
     return (undef, $_ERR)
 	unless $v = $proto->get_instance('RealmName')->unsafe_from_uri($value);
     return ($v, undef);
+}
+
+sub get_domain_part {
+    my(undef, $value) = @_;
+    # may be of the form '@domain' so can't use Type.Email to parse
+    return $value && ($value =~ /\@(.+)$/)[0] || undef;
 }
 
 1;
