ccd166d4834d3495d8c10e424f01369a9a5f3a0a
Andrew Schofield <andrews@bivio.biz>
2012-12-30 16:29:24 +0000

    Subclass of CalendarEventList with 'series_count' column that is set equal to the number of future events in the same series. Allow 'uid' to be specified in query.

---
+++ Biz/Model/CalendarEventSeriesList.pm
@@ -0,0 +1,37 @@
+# Copyright (c) 2012 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Model::CalendarEventSeriesList;
+use strict;
+use Bivio::Base 'Model.CalendarEventList';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    my($self) = @_;
+    return $self->merge_initialize_info($self->SUPER::internal_initialize, {
+	other => [
+            {
+		name => 'series_count',
+		type => 'Integer',
+		constraint => 'NONE',
+		in_select => 1,
+		select_value => "(
+                    SELECT COUNT(*)
+                    FROM calendar_event_t ce
+                    WHERE ce.uid = calendar_event_t.uid
+                          AND ce.dtstart > calendar_event_t.dtstart
+                ) AS series_count",
+	    },
+	],
+    });
+}
+
+sub internal_prepare_statement {
+    my($self, $stmt, $query) = @_;
+    if (my $uid = $query->unsafe_get('uid')) {
+        $stmt->where($stmt->EQ('CalendarEvent.uid', [$uid]));
+    }
+    return shift->SUPER::internal_prepare_statement(@_);
+}
+
+1;
