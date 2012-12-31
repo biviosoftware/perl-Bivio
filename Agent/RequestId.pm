33016a695e6b12927be6789666a53c29dd34acdd
Rob Nagler <github@q33.us>
2012-12-31 17:17:02 +0000

    don't truncate the md5, because needs to be globally unique, and better for it to be longer (using for TaskRateLimit user id when there is no cookie)

--- Agent/RequestId.pm
+++ Agent/RequestId.pm
@@ -1,26 +1,24 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2008-2012 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Agent::RequestId;
 use strict;
-use Bivio::Base 'Bivio::UNIVERSAL';
+use Bivio::Base 'Bivio.UNIVERSAL';
 use Digest::MD5 ();
 use Sys::Hostname ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_COUNT) = 0;
 my($_BASE);
-my($_DT) = b_use('Type.DateTime');
 
 sub current {
     my($proto, $req) = @_;
     return $req->get_if_exists_else_put($proto->package_name => sub {
 	return (
-	    $_BASE ||= substr(
-		Digest::MD5::md5_hex(
-		    Sys::Hostname::hostname() . $_DT->now_as_file_name . $$,
-		),
-		-16,
-	    )
+	    $_BASE ||= Digest::MD5::md5_hex(
+		Sys::Hostname::hostname()
+	            . b_use('Type.DateTime')->now_as_file_name
+		    . $$,
+	    ),
 	) . sprintf('%08x', ++$_COUNT);
     });
 }
