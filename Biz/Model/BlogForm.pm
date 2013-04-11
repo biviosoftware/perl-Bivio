f927c0573a98d15eb7ad23a3f285d168d792c6aa
Paul Moeller <moeller@bivio.biz>
2013-04-11 15:11:35 +0000

    use Bivio::Base replaced call to private superclass method _authorized_name()

--- Biz/Model/BlogForm.pm
+++ Biz/Model/BlogForm.pm
@@ -2,23 +2,24 @@
 # $Id$
 package Bivio::Biz::Model::BlogForm;
 use strict;
-use base 'Bivio::Biz::Model::WikiForm';
+use Bivio::Base 'Model.WikiForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_BN) = b_use('Type.BlogName');
 
 sub execute_ok {
     my($self) = @_;
     shift->SUPER::execute_ok(@_);
-    $self->get_request()->delete('path_info');
+    $self->req->delete('path_info');
     return;
 }
 
 sub internal_pre_execute {
     my($self) = @_;
     shift->SUPER::internal_pre_execute(@_);
-    $self->internal_put_field('RealmFile.path_lc'
-        => Bivio::Biz::Model::WikiForm::_authorized_name($self))
-	    if $self->get('file_exists');
+    $self->internal_put_field(
+	'RealmFile.path_lc' => $self->internal_authorized_name)
+	if $self->get('file_exists');
     $self->internal_clear_error('RealmFile.path_lc');
     return;
 }
@@ -31,7 +32,7 @@ sub is_field_editable {
 }
 
 sub name_type {
-    return Bivio::Type->get_instance('BlogName');
+    return $_BN;
 }
 
 1;
