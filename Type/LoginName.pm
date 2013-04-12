058b7fd73aa7d1104d44657bb63205e557520c77
Justin Schell <schellj@bivio.biz>
2013-04-12 23:15:14 +0000

    add get_width

--- Type/LoginName.pm
+++ Type/LoginName.pm
@@ -17,4 +17,13 @@ sub from_literal {
     return (undef, Bivio_TypeError()->SYNTAX_ERROR);
 }
 
+sub get_width {
+    my($w) = 'get_width';
+    return Bivio_Type()->max(
+	Type_Email()->$w,
+	Type_RealmName()->$w,
+	Type_PrimaryId()->$w,
+    );
+}
+
 1;
