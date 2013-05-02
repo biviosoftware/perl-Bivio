abcbe8f3bb98540c85ce9f7d97299d798b5560f5
Rob Nagler <github@q33.us>
2013-05-02 12:19:22 +0000

    pass req to set_headers_for_forward

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -1,19 +1,20 @@
-# Copyright (c) 2006 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2013 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Action::MailForward;
 use strict;
-use base 'Bivio::Biz::Action';
+use Bivio::Base 'Biz.Action';
+b_use('IO.ClassLoaderAUTOLOAD');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
 sub execute {
     my(undef, $req) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
-    Bivio::Mail::Outgoing->new(
-	Bivio::Mail::Incoming->new($mr->get('message')->{content}),
+    Mail_Outgoing()->new(
+	Mail_Incoming()->new($mr->get('message')->{content}),
     )->set_recipients($mr->get('recipient'), $req)
-	->set_headers_for_forward
-	    ->enqueue_send($req);
+	->set_headers_for_forward($req)
+        ->enqueue_send($req);
     return;
 }
 
