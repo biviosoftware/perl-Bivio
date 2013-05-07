95b8beaf05b6fd8a2577ad1c2d1b50b2f42388fe
Paul Moeller <moeller@bivio.biz>
2013-05-07 21:11:42 +0000

    refactored added method anchors for Views

--- UI/HTML/Widget/TaskInfo.pm
+++ UI/HTML/Widget/TaskInfo.pm
@@ -2,49 +2,79 @@
 # $Id$
 package Bivio::UI::HTML::Widget::TaskInfo;
 use strict;
-use Bivio::Base 'UI.Widget';
+use Bivio::Base 'Widget.Simple';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_SC);
 
-sub render {
-    my($self, $source, $buffer) = @_;
-    my($req) = $source->get_request;
-    my($task) = $req->get('task');
-    $$buffer .= <<"EOF"
-<pre>
-<b>Control Logic for This Page</b>
-(click on link to see the source code)
-Name:         @{[$task->get('id')->get_name]}
-Realm:        @{[$task->get('realm_type')->get_name]}
-Permissions:  @{[join('&amp;',
-    map($_->get_name, @{Bivio::Auth::PermissionSet->to_array(
-        $task->get('permission_set'))}))]}
-EOF
-        . join("\n",
-	    map({
-		my($object, $method, $args) = @$_;
-		$object = ref($object)
-		    if ref($object);
-    #TODO: Does not handle inline subs
-		my($b) = '';
-		($_SC ||= SourceCode({})->package_name)->render_source_link(
-		    $req,
-		    $object->isa('Bivio::UI::View::LocalFile')
-			? 'View.' . $args->[0]
-			: $object,
-		    $object . '->'
-			. $method
-			. (@$args ? '(' . join(', ', @$args) . ')' : ''),
-		    \$b,
+sub initialize {
+    my($self) = @_;
+    $self->put(value => PRE(Join([
+	B('Control Logic for This Page'),
+	'(click on link to see the source code)',
+	Join([
+	    'Name:         ',
+	    ['task_id', '->get_name'],
+	]),
+	Join([
+	    'Realm:        ',
+	    ['task', 'realm_type', '->get_name'],
+	]),
+	Join([
+	    'Permissions:  ',
+	    [
+		sub {
+		    my(undef, $set) = @_;
+		    return join(
+			'&amp;',
+			map(
+			    $_->get_name,
+			    @{b_use('Auth.PermissionSet')->to_array($set)},
+			),
+		    );
+		},
+		['task', 'permission_set'],
+	    ],
+	]),
+	[
+	    sub {
+		my($req, $task) = @_;
+		return join(
+		    "\n",
+		    map({
+			_render_source_link($self, $req, $_);
+		    } @{$task->get('items')}),
 		);
-		$b;
-	    } @{$task->get('items')}),
-	    "</pre>\n",
-	 );
-#TODO: render attributes
-    return;
+	    },
+	    ['task'],
+	],
+    ], "\n")));
+    return shift->SUPER::initialize(@_);
+}
+
+sub _render_source_link {
+    my($self, $req, $item) = @_;
+    my($object, $method, $args) = @$item;
+    $object = ref($object)
+	if ref($object);
+#TODO: Does not handle inline subs
+    my($res) = '';
+    ($_SC ||= SourceCode({})->package_name)->render_source_link(
+	$req,
+	$object->isa('Bivio::UI::View::LocalFile')
+	    ? 'View.' . $args->[0]
+	    : $object,
+	$object . '->'
+	    . $method
+		. (@$args ? '(' . join(', ', @$args) . ')' : ''),
+	\$res,
+	# View method name as anchor
+	$object->isa('Bivio::UI::View::Method')
+	    ? $args->[0]
+	    : (),
+    );
+    return $res;
 }
 
 1;
