ee64081076304a50158f2f3456d89ead7aa949e0
Paul Moeller <moeller@bivio.biz>
2013-05-07 21:13:05 +0000

    added error bubble for fancy_input

--- UI/XHTML/Widget/FormFieldLabel.pm
+++ UI/XHTML/Widget/FormFieldLabel.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2005 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2005-2013 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::UI::XHTML::Widget::FormFieldLabel;
 use strict;
@@ -6,7 +6,8 @@ use Bivio::Base 'XHTMLWidget.Simple';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_CB) = __PACKAGE__->use('XHTMLWidget.ControlBase');
+my($_CB) = b_use('XHTMLWidget.ControlBase');
+my($_IS_FANCY) = b_use('XHTMLWidget.FormField')->is_fancy_input;
 
 sub internal_as_string {
     my($self) = @_;
@@ -22,29 +23,63 @@ sub initialize {
     my($self) = @_;
     $self->initialize_attr('field');
     $self->put_unless_exists(
-	value => Join([
-	    IfFieldError(
-		$self->get('field'),
-		[sub {
-		     my($source) = @_;
-		     return vs_text(
-			 $source->req,
-			 $self->resolve_form_model($source)
-			     ->simple_package_name,
-			 'prose',
-			 'error_indicator',
-		     );
-		}],
-	    ),
-	    $self->get('label'),
-	]),
-	cell_class => IfFieldError(
-	    $self->get('field'),
-	    'label label_err',
-	    'label label_ok',
-	),
-    );
+	value => $_IS_FANCY
+	    ? _error_bubble($self)
+	    : _error_indicator($self)
+	);
     return shift->SUPER::initialize(@_);
+}
+
+sub _error_bubble {
+    my($self) = @_;
+    $self->put(cell_class => '');
+    return Grid([[
+    	IfFieldError(
+    	    $self->get('field'),
+    	    Join([
+    		DIV_b_error_bubble(Join([
+    		    FormFieldError({
+    			field => $self->get('field'),
+    			label => $self->get('label')->get('value'),
+    		    }),
+    		])),
+    	    ]),
+    	)->put(cell_class => 'b_error_bubble'),
+    	IfFieldError(
+    	    $self->get('field'),
+    	    DIV_b_error_arrow_holder(Join([
+    		SPAN_b_error_arrow_border(),
+    		SPAN_b_error_arrow(),
+    	    ])),
+    	)->put(cell_class => 'b_error_arrow'),
+	$self->get('label')->put(cell_class => 'label label_ok'),
+    ]], {
+    	class => 'b_label_group',
+    });
+}
+
+sub _error_indicator {
+    my($self) = @_;
+    $self->put(cell_class => IfFieldError(
+	$self->get('field'),
+	'label label_err',
+	'label label_ok',
+    ));
+    return Join([
+	IfFieldError(
+	    $self->get('field'),
+	    [sub {
+		 my($source) = @_;
+		 return vs_text(
+		     $source->req,
+		     $self->resolve_form_model($source)->simple_package_name,
+		     'prose',
+		     'error_indicator',
+		 );
+	     }],
+	),
+	$self->get('label'),
+    ]);
 }
 
 1;
