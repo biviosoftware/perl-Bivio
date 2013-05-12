6029969a3d201d5ad47a175516df44d6af53feff
Rob Nagler <github@q33.us>
2013-05-12 16:01:23 +0000

    added bivio_ssl_no_check_certificate and bivio_redirect_automatically new: want_redirects param is deprecated.  Default is still redirects off. Added POST to valid redirects, because you probably redirect after a form POST. LWP 6.x switched to ssl cert checking by default so need to set bivio_ssl_no_check_certificate explicitly in a few cases

--- Ext/LWPUserAgent.pm
+++ Ext/LWPUserAgent.pm
@@ -14,9 +14,7 @@ use LWP::Debug ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($_TRACE);
-my($_PKG) = __PACKAGE__;
 Bivio::IO::Trace->register;
-my($_HTTP_PROXY);
 Bivio::IO::Config->register(my $_CFG = {
     http_proxy => undef,
     timeout => 60,
@@ -24,13 +22,29 @@ Bivio::IO::Config->register(my $_CFG = {
 
 sub bivio_http_get {
     my($proto, $uri) = @_;
-    my($response) = $proto->new(1)
+    my($response) = $proto->new
+	->bivio_redirect_automatically
 	->request(
 	    HTTP::Request->new('GET', $uri),
 	);
     Bivio::Die->die($response)
 	unless $response->is_success;
     return \($response->content);
+}
+
+sub bivio_ssl_no_check_certificate {
+    my($self) = @_;
+    return $self
+	unless $self->can('ssl_opts');
+    $self->ssl_opts(verify_hostname => 0);
+    return $self;
+}
+
+sub bivio_redirect_automatically {
+    my($self) = @_;
+    $self->max_redirect(10);
+    $self->requests_redirectable([qw(GET HEAD POST)]);
+    return $self;
 }
 
 sub handle_config {
@@ -42,18 +56,8 @@ sub handle_config {
 }
 
 sub new {
-    # (proto, boolean) : Ext.LWPUserAgent
-    # Calls SUPER::new and sets timeout and proxy.
-    #
-    # If I<want_redirects> is true, L<redirect_ok|"redirect_ok"> will return true.
-    #
-    # Turns on LWP::Debug if $_TRACE is true for this class.
     my($proto, $want_redirects) = @_;
     my($self) = $proto->SUPER::new;
-    my($fields) = $self->{$_PKG} = {
-	want_redirects => $want_redirects ? 1 : 0,
-    };
-    # Relatively short timeout, so we don't get stuck in remote services.
     $self->timeout($_CFG->{timeout});
     # Use a proxy if configured
     if (defined($_CFG->{http_proxy})) {
@@ -62,15 +66,16 @@ sub new {
     elsif ($ENV{http_proxy}) {
         $self->proxy(['http', 'https'], $ENV{http_proxy});
     }
+    if ($want_redirects) {
+	Bivio::IO::Alert->warn_deprecated('use bivio_redirect_automatically, instead of passing param to new');
+	$self->bivio_redirect_automatically;
+    }
+    else {
+	$self->max_redirect(0);
+	$self->requests_redirectable([]);
+    }
     LWP::Debug::level("+debug") if $_TRACE;
     return $self;
-}
-
-sub redirect_ok {
-    # (self) : boolean
-    # Always returns false.  Redirects need to be handled at higher level for cookies
-    # and logging.
-    return shift->{$_PKG}->{want_redirects};
 }
 
 1;
