5439b7d4c44de1778acdc0814fdbf526ba0f7e05
Rob Nagler <github@q33.us>
2013-05-16 03:54:12 +0000

    String->wrap_lines became TextArea::_wrap_lines, because wasn't used anywhere. _wrap_lines calls canonicalize_charset from_literal only appends newline if there isn't already a newline

--- Type/TextArea.pm
+++ Type/TextArea.pm
@@ -16,22 +16,74 @@ sub from_literal {
         unless wantarray;
     return (undef, undef)
  	unless defined($value) && length($value);
+    return (undef, Bivio::TypeError->TOO_LONG)
+ 	if length($value) > $proto->get_width;
     my($pref, $req);
-    return $proto->wrap_lines($value, $line_width || $proto->LINE_WIDTH)
+    my($v);
+    $v = _wrap_lines($proto, $value, $line_width || $proto->LINE_WIDTH)
 	if defined($value)
         and $req = Bivio::Agent::Request->get_current
 	and $pref = b_use('Model.RowTag')->new($req)
 	    ->row_tag_get_for_auth_user('TEXTAREA_WRAP_LINES')
         and $pref;
-    return (undef, Bivio::TypeError->TOO_LONG)
- 	if length($value) > $proto->get_width;
-    my($v) = $proto->canonicalize_charset(\$value);
-    return length($$v) ? $$v . "\n" : (undef, undef);
+    $v = $proto->canonicalize_charset(\$value)
+	unless $v;
+    return (undef, undef)
+	unless $$v;
+    $$v .= "\n"
+	unless $$v =~ /\n$/;
+    return $$v;
 }
 
 sub get_width {
     # Max size in browsers
     return 0xffff;
+}
+
+sub _wrap_lines {
+    my($proto, $value, $width) = @_;
+    $width = 72
+	unless $width;
+    my($lines) = [];
+    my($formatted) = [];
+    my($indent) = 0;
+    foreach my $line (
+	Text::Tabs::expand(
+	    @{[split(
+		/\n/,
+		${$proto->canonicalize_charset($value)},
+	    )]},
+	),
+    ) {
+        $line =~ s/\s+$//;
+        while (defined($line) && length($line) > $width) {
+            _wrap_line($formatted, \$line, $indent, $width);
+        }
+        push(@$formatted, $line)
+	    if defined($line);
+    }
+    return \(join("\n", @$formatted, ''));
+}
+
+sub _wrap_line {
+    my($formatted, $line, $indent, $width) = @_;
+    $$line =~ /(^\s*(|[\-\*])\s+)/;
+    $indent = defined($1) ? substr($1, 0, $width) : '';
+    my($white_pos) = rindex($$line, ' ', $width);
+    $white_pos = index($$line, ' ', $width)
+	if $white_pos < length($indent);
+    # Line cannot be broken if no white-space found or quoted
+    if ($white_pos == -1 || $$line =~ /^\s*[>]/) {
+        push(@$formatted, $$line);
+        undef($$line);
+    }
+    else {
+        my($wrapped) = substr($$line, 0, $white_pos);
+        push(@$formatted, $wrapped);
+        $$line = substr($$line, $white_pos);
+        $$line =~ s/^\s+/' ' x length($indent)/e;
+    }
+    return;
 }
 
 1;
