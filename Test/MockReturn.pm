0aa4ebe06f484b1d4f327a1da857f9e95f8a5836
Rob Nagler <github@q33.us>
2013-05-27 18:51:54 +0000

    *** empty log message ***

---
+++ Test/MockReturn.pm
@@ -0,0 +1,10 @@
+# Copyright (c) 2013 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Test::MockReturn;
+use strict;
+use Bivio::Base 'Collection.Attributes';
+b_use('IO.ClassLoaderAUTOLOAD');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+1;
