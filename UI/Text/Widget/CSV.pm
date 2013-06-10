bb7529df9c25fdc2aade1a0f0e766a914bfa87c4
Paul Moeller <moeller@bivio.biz>
2013-06-10 17:31:39 +0000

    iterate with request query

--- UI/Text/Widget/CSV.pm
+++ UI/Text/Widget/CSV.pm
@@ -98,7 +98,7 @@ sub render {
 
     if ($self->unsafe_get('want_iterate_start')) {
 	$list = _list_class($self)->new($self->req);
-	$list->iterate_start;
+	$list->iterate_start($list->parse_query_from_request);
     }
     else {
 	$list = $source->get_widget_value(ref(_list_class($self)));
