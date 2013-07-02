a9f8223e69440faf2b1cc28da7706b0bd93c154b
Paul Moeller <moeller@bivio.biz>
2013-07-02 20:58:15 +0000

    don't render unless is_production use newer javascript

--- UI/HTML/Widget/GoogleAnalytics.pm
+++ UI/HTML/Widget/GoogleAnalytics.pm
@@ -5,29 +5,33 @@ use strict;
 use Bivio::Base 'Widget.Simple';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_C) = b_use('IO.Config');
 
 sub initialize {
     my($self) = @_;
-    my($tracking_id) = $self->get('tracking_id');
-    $self->put(value => <<"EOF");
+    my($tracking_id, $site) = $self->get(qw(tracking_id site));
+    $self->put(
+	value => $_C->is_production
+	    ? <<"EOF"
 <script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '$tracking_id']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', '$tracking_id', '$site');
+  ga('send', 'pageview');
 </script>
 EOF
+	    : ''
+    );
     return shift->SUPER::initialize(@_);
 }
 
 sub internal_new_args {
-    my($self, $tracking_id) = @_;
+    my($self, $tracking_id, $site) = @_;
     return {
 	tracking_id => $tracking_id,
+	site => $site,
     };
 }
 
