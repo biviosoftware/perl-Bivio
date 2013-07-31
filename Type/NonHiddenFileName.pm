41787f17b2c801747ae2ce2b08ade26d6d06cfa2
Paul Moeller <moeller@bivio.biz>
2013-07-31 23:05:42 +0000

    initial revision

---
+++ Type/NonHiddenFileName.pm
@@ -0,0 +1,19 @@
+# Copyright (c) 2013 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Type::NonHiddenFileName;
+use strict;
+use Bivio::Base 'Type.FileName';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub from_literal {
+    my($proto, $value) = @_;
+    my($v, $e) = $proto->SUPER::from_literal($value);
+    return ($v, $e)
+	unless defined($v);
+    return (undef, Bivio::TypeError->FILE_NAME_LEADING_DOT)
+	if $v =~ /^\./;
+    return $v;
+}
+
+1;
