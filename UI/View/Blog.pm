d4345936cc57621413b2d6cf376fdcb7737fa5dc
Paul Moeller <moeller@bivio.biz>
2013-10-28 20:11:40 +0000

    new layout for blog list and detail pages removed old v3 config

--- UI/View/Blog.pm
+++ UI/View/Blog.pm
@@ -6,86 +6,44 @@ use Bivio::Base 'View.Base';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_C) = b_use('IO.Config');
-my($_FP) = b_use('Type.FilePath');
-
-sub TEXT_AREA_COLS {
-    return 80;
-}
-
-sub TEXT_AREA_ROWS {
-    return 30;
-}
+my($_DT) = b_use('Type.DateTime');
 
 sub edit {
-    my($self) = @_;
-    return shift->edit_wysiwyg(@_)
-	if b_use('View.Wiki')->use_wysiwyg;
-    return $self->internal_body(vs_simple_form(BlogEditForm => [
-	['BlogEditForm.title', {
+    my($self, $form) = @_;
+    $form ||= 'BlogEditForm';
+    my($editor) = b_use('View.Wiki')->use_wysiwyg
+	? \&CKEditor
+	: \&TextArea;
+    return $self->internal_body(vs_simple_form($form => [
+	["$form.title", {
 	    size => 57,
 	}],
-	'BlogEditForm.RealmFile.is_public',
-	_edit($self, 'BlogEditForm', {editor => \&TextArea}),
-    ]));
-}
-
-sub edit_wysiwyg {
-    my($self) = @_;
-    return $self->internal_body(vs_simple_form(BlogEditForm => [
-	['BlogEditForm.title', {
-	    size => 57,
-	}],
-	'BlogEditForm.RealmFile.is_public',
-	_edit($self, 'BlogEditForm', {editor => \&CKEditor}),
+	"$form.RealmFile.is_public",
+	Join([
+	    FormFieldError({
+		field => 'body',
+		label => 'text',
+	    }),
+	    $editor->({
+		field => 'body',
+		rows => 30,
+		cols => 80,
+		%{b_use('View.Wiki')->get_image_folders},
+		use_public_image_folder =>
+		    ["Model.$form", 'RealmFile.is_public'],
+	    }),
+	], {
+	    cell_class => 'blog_textarea',
+	}),
     ]));
 }
 
 sub create {
-    my($self) = @_;
-    return shift->create_wysiwyg(@_)
-	if b_use('View.Wiki')->use_wysiwyg;
-    return $self->internal_body(vs_simple_form(BlogCreateForm => [
-	'BlogCreateForm.title',
-	'BlogCreateForm.RealmFile.is_public',
-	_edit($self, 'BlogCreateForm', {editor => \&TextArea}),
-    ]));
-}
-
-sub create_wysiwyg {
-    my($self) = @_;
-    return $self->internal_body(vs_simple_form(BlogCreateForm => [
-	'BlogCreateForm.title',
-	'BlogCreateForm.RealmFile.is_public',
-	_edit($self, 'BlogCreateForm', {editor => \&CKEditor}),
-    ]));
+    return shift->edit('BlogCreateForm');
 }
 
 sub detail {
     return shift->internal_put_base_attr(
-	menu => Join([
-	    DIV_heading('Recent Entries'),
-	    UL(List('BlogRecentList', [
-		LI(vs_link(['title'], URI({
-		    task_id => _access_mode('FORUM_BLOG_DETAIL'),
-		    path_info => ['path_info'],
-		}))),
-	    ])),
-	]),
-	topic => String([qw(Model.BlogList title)]),
-	byline => Join([
-	    'posted on ',
-	    DateTime([qw(Model.BlogList ->get_creation_date_time)]),
-	    ' (',
-	    'last edited by ',
-	    MailTo(
-		[qw(Model.BlogList Email.email)],
-		[qw(Model.BlogList RealmOwner.display_name)],
-	    ),
-	    ' on ',
-	    DateTime(['Model.BlogList', 'RealmFile.modified_date_time']),
-	    ')',
-	]),
 	tools => TaskMenu([
 	    {
 		task_id => 'FORUM_BLOG_EDIT',
@@ -95,102 +53,120 @@ sub detail {
 	]),
 	body => vs_paged_detail(
 	    'BlogList',
-	    [THIS_LIST => _access_mode('FORUM_BLOG_LIST')],
-#TODO: Replace with proper blog widget that *partially* reuses wiki markup
- 	    DIV_blog(
-		DIV(DIV_text(['Model.BlogList', '->render_html', undef, 'FORUM_WIKI_VIEW']), 
-#TODO: multiple classes
- 		{class => If(
- 		    ['Model.BlogList', 'RealmFile.is_public'],
- 		    'public', 'private')},
- 	    )),
+	    [THIS_LIST => 'FORUM_BLOG_LIST'],
+ 	    DIV_blog(Join([
+		WithModel('BlogList', _blog_title(0)),
+		WithModel('BlogList', _blog_byline()),
+		DIV_text(
+		    WikiText({
+			value => ['Model.BlogList', 'body'],
+			name => ['Model.BlogList', 'path_info'],
+		    }),
+		    {
+			ITEMSCOPE => 'itemscope',
+			ITEMPROP => 'text',
+		    },
+		),
+	    ]), {
+		ITEMPROP => 'blogPost',
+		ITEMSCOPE => 'itemscope',
+		ITEMTYPE => 'http://schema.org/BlogPosting',
+	    }),
 	),
     );
 }
 
 sub list {
     return shift->internal_put_base_attr(
-#TODO: Fix this
-	rss_task => _access_mode('FORUM_BLOG_RSS'),
+	rss_task => 'FORUM_BLOG_RSS',
 	tools => TaskMenu(['FORUM_BLOG_CREATE']),
-	menu => Join([
-#TODO: Move to FacadeBase
-	    DIV_heading('Recent Entries'),
-	    UL(List('BlogRecentList', [
-		LI(vs_link(['title'], URI({
-		    task_id => _access_mode('FORUM_BLOG_DETAIL'),
-		    path_info => ['path_info'],
-		}))),
-	    ])),
-	]),
-	body => DIV_blog(Join([
-	    DIV_list(vs_paged_list(BlogList => List(BlogList => [
+	body => DIV_blog(DIV_list(
+	    vs_paged_list(BlogList => List(BlogList => [
 		DIV(Join([
-		    DIV_heading(Join([
-			vs_link(['title'], URI({
-			    task_id => _access_mode('FORUM_BLOG_DETAIL'),
-			    path_info => ['path_info'],
-			})),
-		    ])),
+		    _blog_title(1),
+		    _blog_byline(),
 		    DIV_text(Join([
 			DIV_excerpt(String(['->get_rss_summary'])),
-			Link('[more]', URI({
-			    task_id => _access_mode('FORUM_BLOG_DETAIL'),
-			    path_info => ['path_info'],
-			}), 'more'),
-		    ])),
-		    DIV_menu(Link(vs_text('title.FORUM_BLOG_EDIT'), URI({
-			task_id => 'FORUM_BLOG_EDIT',
-			path_info => ['path_info'],
-		    }), {
-			control => Bivio::Agent::TaskId->FORUM_BLOG_EDIT,
-		    })),
-		]),
-		{class => If(['RealmFile.is_public'], 'public', 'private')},
-	    ),
-	]))),
-    ])));
+			BR(),
+			DIV(
+			    A('Read More', {
+				HREF => URI({
+				    task_id => 'FORUM_BLOG_DETAIL',
+				    path_info => ['path_info'],
+				})
+			    })->put(class => 'b_button_link b_ok_button_link'),
+			)->put(class => 'b_align_e'),
+			BR(),
+		    ]), {
+			ITEMSCOPE => 'itemscope',
+			ITEMPROP => 'text',
+		    }),
+		]), {
+		    ITEMPROP => 'blogPost',
+		    ITEMSCOPE => 'itemscope',
+		    ITEMTYPE => 'http://schema.org/BlogPosting',
+		}),
+	    ])),
+	    {
+		ITEMPROP => 'blogPosts',
+		ITEMSCOPE => 'itemscope',
+		ITEMTYPE => 'http://schema.org/BlogPosting',
+	    }), {
+		ITEMSCOPE => 'itemscope',
+		ITEMTYPE => 'http://schema.org/Blog',
+	    }),
+    );
 }
 
 sub list_rss {
     return shift->internal_body(AtomFeed('BlogList'));
 }
 
-sub _access_mode {
-    my($task) = @_;
-    return $_C->if_version(3,
-	$task,
-	sub {
-	    (my $p = $task) =~ s/(?<=^FORUM_)/PUBLIC_/;
-	    return [
-		sub {
-		    my($source, $private, $public) = @_;
-		    shift->req('Type.AccessMode')->eq_private
-			? $private : $public;
+sub _blog_byline {
+    return DIV_blog_byline(Join([
+	If(['->unsafe_get_author_image_uri'],
+	   SPAN_blog_img(Image(
+	       ['->unsafe_get_author_image_uri'],
+	   ),
+	)),
+	SPAN_blog_author(Join([
+	    SPAN(
+		String(['RealmOwner.display_name']),
+		{
+		    ITEMPROP => 'author',
+		    ITEMSCOPE => 'itemscope',
 		},
-		$task, $p,
-	    ];
-	},
-    );
+	    ),
+	    BR(),
+	    SPAN(Join([
+		String([$_DT, '->english_day_of_week',
+			['->get_creation_date_time']]),
+		', ',
+		DateTime(['->get_creation_date_time'],
+			 'FULL_MONTH_DAY_AND_YEAR'),
+	    ]), {
+		ITEMPROP => 'dateCreated',
+		ITEMSCOPE => 'itemscope',
+	    }),
+	])),
+	DIV_clear(vs_blank_cell()),
+    ]));
 }
 
-sub _edit {
-    my($self, $form, $options) = @_;
-    return Join([
-	FormFieldError({
-	    field => 'body',
-	    label => 'text',
-	}),
-	$options->{editor}->({
-	    field => 'body',
-	    rows => $self->TEXT_AREA_ROWS,
-	    cols => $self->TEXT_AREA_COLS,
-	    %{b_use('View.Wiki')->get_image_folders},
-	    use_public_image_folder => ["Model.$form", 'RealmFile.is_public'],
-	}),
-    ], {
-	cell_class => 'blog_textarea',
+sub _blog_title {
+    my($title_as_link) = @_;
+    my($title) = SPAN(String(['title']), {
+	ITEMPROP => 'headline',
+	ITEMSCOPE => 'itemscope',
     });
+    return DIV_blog_title(
+	$title_as_link
+	    ? vs_link($title, URI({
+		task_id => 'FORUM_BLOG_DETAIL',
+		path_info => ['path_info'],
+	    }))
+	    : $title,
+    );
 }
 
 1;
