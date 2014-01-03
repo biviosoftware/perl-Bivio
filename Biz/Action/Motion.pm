04282d01015f34a07914c33a92063d534a39b5ac
Justin Schell <schellj@bivio.biz>
2014-01-03 02:19:27 +0000

    *** empty log message ***

---
+++ Biz/Action/Motion.pm
@@ -0,0 +1,14 @@
+# Copyright (c) 2014 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::Motion;
+use strict;
+use Bivio::Base 'Action.MotionBase';
+b_use('IO.ClassLoaderAUTOLOAD');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub execute_redirect_if_closed {
+    return shift->internal_redirect_if_closed(shift, 'Model.Motion');
+}
+
+1;
---
+++ Biz/Action/MotionBase.pm
@@ -0,0 +1,20 @@
+# Copyright (c) 2013 IEEE SA, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::Biz::Action::MotionBase;
+use strict;
+use Bivio::Base 'Biz.Action';
+b_use('IO.ClassLoaderAUTOLOAD');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_redirect_if_closed {
+    my($self, $req, $model) = @_;
+    my($m) = $req->ureq($model);
+    return
+	if !$m || !$m->is_loaded;
+    return 'closed_task'
+	unless $m->is_open;
+    return;
+}
+
+1;
