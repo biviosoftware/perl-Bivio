2a3cc5a75d990167e94a0115c210cc044eb8284e
Rob Nagler <github@q33.us>
2014-01-27 17:55:51 +0000

    to allow dynamic substitution of widgets from the facade

---
+++ UI/Widget/WidgetSubstitute.pm
@@ -0,0 +1,27 @@
+# Copyright (c) 2013 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::Widget::WidgetSubstitute;
+use strict;
+use Bivio::Base 'Widget.Simple';
+b_use('UI.ViewLanguageAUTOLOAD');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_WS) = b_use('FacadeComponent.WidgetSubstitute');
+
+sub control_on_render {
+    my($self, $source, $wo) = shift->widget_render_args(@_);
+    if (defined(my $ws = $_WS->get_widget_substitute_value($self->get('value'), $source))) {
+	$wo->append_buffer($ws);
+	return;
+    }
+    return $self->SUPER::control_on_render(@_);
+}
+
+sub initialize {
+    my($self) = @_;
+    b_die($self->get('value'), ': value must be a widget')
+	unless UI_Widget()->is_super_of($self->get('value'));
+    return shift->SUPER::initialize(@_);
+}
+
+1;
