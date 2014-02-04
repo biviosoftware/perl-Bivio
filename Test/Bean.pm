70354d869a939231c6c941583ffff5ee14b5e2f6
Rob Nagler <github@q33.us>
2014-02-04 23:58:46 +0000

    the old mode of return values passed as first argument is no longer valid.  Added test_bean_register_callback to allow dynamic additions to the interface without having to know the argument syntax.

--- Test/Bean.pm
+++ Test/Bean.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2002-2008 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2002-2014 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Test::Bean;
 use strict;
@@ -14,37 +14,55 @@ sub AUTOLOAD {
     my($method) = $AUTOLOAD;
     $method =~ s/.*:://;
     return if $method eq 'DESTROY';
-    if ($self->unsafe_get('_new_mode')) {
-	my($orig_args) = [@args];
-	while(1) {
-	    my($sig) = "$method(" . join(',', @args) . ')';
-	    return ref($self->unsafe_get($sig)) eq 'CODE'
-		? $self->unsafe_get($sig)->($orig_args)
-		: wantarray
-		? @{$self->unsafe_get($sig)}
-		: $self->unsafe_get($sig)->[0]
-		if $self->has_keys($sig);
-	    if (@args) {
-		pop(@args);
-	    }
-	    else {
-		$self->put($sig => []);
-		Bivio::IO::Alert->warn($sig, ': not found returning nothing');
-	    }
-	}
-	return;
-    }
-    my($res) = $self->get_or_default($method => []);
-    $self->put($method => \@args)
-	if @args;
-    return wantarray ? @$res : $res->[0];
+    return _callback($self, $method, \@args);
 }
 
 sub new {
     my(undef, $values) = @_;
-    return shift->SUPER::new($values ? _verify($_R->nested_copy($values)) : {});
+    b_die('must supply $values')
+	unless $values;
+    return shift->SUPER::new(_verify($_R->nested_copy($values)));
 }
 
+sub test_bean_register_callback {
+    my($self, $method, $args, $callback) = @_;
+    $self->put(_sig($method, $args) => $callback);
+    return $self;
+}
+
+sub _callback {
+    my($self, $method, $args) = @_;
+    my($orig_args) = [@$args];
+    while(1) {
+	my($sig) = _sig($method, $args);
+	return ref($self->unsafe_get($sig)) eq 'CODE'
+	    ? $self->unsafe_get($sig)->($orig_args)
+	    : wantarray
+	    ? @{$self->unsafe_get($sig)}
+	    : $self->unsafe_get($sig)->[0]
+	    if $self->has_keys($sig);
+	if (@$args) {
+	    pop(@$args);
+	}
+	else {
+	    $self->put_unless_exists(
+		"_warn_$sig",
+		sub {
+		    # Don't use b_warn, because in AUTOLOAD
+		    Bivio::IO::Alert->warn($sig, ': not found returning nothing');
+		    return 1;
+		},
+	    );
+	    return wantarray ? () : undef;
+	}
+    }
+    # DOES NOT RETURN
+}
+
+sub _sig {
+    my($method, $args) = @_;
+    return "$method(" . join(',', @$args) . ')';
+}
 sub _verify {
     my($values) = @_;
     while (my($k, $v) = each(%$values)) {
@@ -53,7 +71,6 @@ sub _verify {
 	Bivio::Die->die($v, ': value for ', $k, ' must be array_ref or code_ref')
             unless ref($v) =~ /^(?:ARRAY|CODE)$/s;
     }
-    $values->{_new_mode} = 1;
     return $values;
 }
 
