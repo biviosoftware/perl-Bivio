e04c6c68d684604756f53d7fdff241f1615aeb0a
Justin Schell <schellj@bivio.biz>
2014-02-16 06:22:26 +0000

    add generate_bootstrap_css

--- Util/Project.pm
+++ Util/Project.pm
@@ -4,6 +4,7 @@ package Bivio::Util::Project;
 use strict;
 use Bivio::Base 'Bivio.ShellUtil';
 use File::Find ();
+b_use('IO.ClassLoaderAUTOLOAD');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_F) = b_use('IO.File');
@@ -15,6 +16,34 @@ usage: bivio Project [options] command [args..]
 commands
   link_facade_files
 EOF
+}
+
+sub generate_bootstrap_css {
+    my($self, $facade) = @_;
+    $self->initialize_fully;
+    my($f) = UI_Facade()->get_instance($facade);
+    IO_File()->do_in_dir(
+	_join_with_plain($f, $f->get_local_file_plain_app_uri()),
+	sub {
+	    my($out) = _join_with_plain(
+		$f, $f->get_local_file_plain_app_uri('/css/bootstrap.min.css'));
+	    $self->are_you_sure("overwrite $out?")
+		if -f $out;
+	    my($b) = '';
+	    _add_import_line('bootstrap.less', \$b);
+	    foreach my $l (glob('less/*.less')) {
+		_add_import_line($l, \$b);
+	    }
+	    _add_import_line('utilities.less', \$b);
+	    my($include) = _join_with_plain(
+		$f, $f->get_local_file_plain_common_uri('/bootstrap/less'));
+	    my($tmp) = IO_File()->tmp_path($self->req);
+	    IO_File()->write($tmp, \$b);
+	    $self->piped_exec("lessc -x --include-path=$include $tmp $out");
+	    $self->print_line("generation successful");
+	},
+    );
+    return;
 }
 
 sub link_facade_files {
@@ -36,7 +65,11 @@ sub link_facade_files {
 		    : "$ENV{HOME}/src";
 		my($common) = "$src/perl/Bivio/files";
 		$_F->mkdir_p($common);
-		foreach my $j (grep($_ !~ /CVS$/ && -d $_, glob("$src/javascript/*"))) {
+		foreach my $j (grep(
+		    $_ !~ /CVS$/ && -d $_,
+		    glob("$src/javascript/*"),
+		    "$src/external/bootstrap",
+		)) {
 		    my($dest) = "$common/" . ($j =~ m{([^/]+)$})[0];
 		    $_F->do_in_dir(
 			$j,
@@ -98,6 +131,17 @@ sub link_facade_files {
         },
     );
     return;
+}
+
+sub _add_import_line {
+    my($file, $buffer) = @_;
+    $$buffer .= '@import "' . $file . "\";\n";
+    return;
+}
+
+sub _join_with_plain {
+    my($facade, $path) = @_;
+    return Type_FilePath()->join($facade->get_local_file_name('PLAIN'), $path);
 }
 
 1;
