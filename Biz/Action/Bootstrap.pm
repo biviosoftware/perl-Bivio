6613754f3672d9eb7ae6af9bc1e2bab9d9a785db
Justin Schell <schellj@bivio.biz>
2014-02-21 00:32:06 +0000

    check when less files were modified, only generate bootstrap css if necessary

--- Biz/Action/Bootstrap.pm
+++ Biz/Action/Bootstrap.pm
@@ -6,11 +6,36 @@ use Bivio::Base 'Biz.Action';
 b_use('IO.ClassLoaderAUTOLOAD');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_TIME_SIG);
 
 sub execute_generate_css {
     my($proto, $req) = @_;
+    $req->with_realm(
+	undef,
+	sub {
+	    my($old_sig) = $_TIME_SIG || '';
+	    my($new_sig) = _time_sig($req);
+	    if ($old_sig ne $new_sig) {
+		ShellUtil_Project()->generate_bootstrap_css;
+		$_TIME_SIG = $new_sig;
+	    }
+	},
+    );
     return $proto->get_instance('LocalFilePlain')
-	->execute($req, ShellUtil_Project()->generate_bootstrap_css);
+	->execute($req, ShellUtil_Project()->bootstrap_css_path);
+}
+
+sub _time_sig {
+    my($req) = @_;
+    return join(
+	' ',
+	map({
+	    IO_File()->get_modified_date_time($_) => $_;
+	} glob(Type_FilePath()->join(
+	    $req->req('UI.Facade')->get_local_file_name(UI_LocalFileType()->PLAIN),
+	    ShellUtil_Project()->bootstrap_less_path,
+	))),
+    );
 }
 
 1;
