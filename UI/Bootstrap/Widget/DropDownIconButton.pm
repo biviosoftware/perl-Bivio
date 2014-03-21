084b1f89d45721c00ff78916a9129cd0f5b63502
Justin Schell <schellj@bivio.biz>
2014-03-21 03:27:44 +0000

    *** empty log message ***

---
+++ UI/Bootstrap/Widget/DropDownIconButton.pm
@@ -0,0 +1,51 @@
+# Copyright (c) 2014 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::Bootstrap::Widget::DropDownIconButton;
+use strict;
+use Bivio::Base 'XHTMLWidget.Join';
+b_use('UI.ViewLanguageAUTOLOAD');
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub initialize {
+    my($self) = @_;
+    my($items) = $self->get('items');
+    return shift->put_unless_exists(values => [
+	IconButton(
+	    $self->get('icon'),
+	    $self->get('title'),
+	    {
+		$self->unsafe_get('data_edit')
+		    ? (data_edit => $self->get('data_edit')) : (),
+		additional_value => SPAN_caret(),
+		additional_classes => 'btn-dropdown dropdown-toggle',
+		'DATA-TOGGLE' => 'dropdown',
+	    },
+	),
+	ref($items) eq 'ARRAY'
+	    ? UL(
+		Join([
+		    map(
+			LI($_),
+			@$items,
+		    ),
+		]),
+		{
+		    class => 'dropdown-menu',
+		},
+	    )
+	    : $items,
+    ])->SUPER::initialize(@_);
+}
+
+sub internal_new_args {
+    my(undef, $icon, $title, $items, $attributes) = @_;
+    return {
+	icon => $icon,
+	title => $title,
+	items => $items,
+	($attributes ? %$attributes : ()),
+    };
+}
+
+1;
