110fd59c2981a5021186584e1a30e362ad9ca599
Justin Schell <schellj@bivio.biz>
2014-04-25 02:58:54 +0000

    support altering NAV and container div classes

--- UI/XHTML/Widget/NavContainer.pm
+++ UI/XHTML/Widget/NavContainer.pm
@@ -14,25 +14,28 @@ sub NEW_ARGS {
 sub initialize {
     my($self) = @_;
     my($id) = JavaScript()->unique_html_id;
-    return shift->put_unless_exists(value => NAV(DIV_container(Join([
+    return shift->put_unless_exists(value => NAV(
 	DIV(Join([
-	    BUTTON(Join([
-		SPAN('Toggle navigation', 'sr-only'),
-		SPAN('', 'icon-bar'),
-		SPAN('', 'icon-bar'),
-		SPAN('', 'icon-bar'),
-	    ]), {
-		class => 'navbar-toggle',
-		TYPE => 'button',
-		'DATA-TOGGLE' => 'collapse',
-		'DATA-TARGET' => '#' . $id,
+	    DIV(Join([
+		BUTTON(Join([
+		    SPAN('Toggle navigation', 'sr-only'),
+		    SPAN('', 'icon-bar'),
+		    SPAN('', 'icon-bar'),
+		    SPAN('', 'icon-bar'),
+		]), {
+		    class => 'navbar-toggle',
+		    TYPE => 'button',
+		    'DATA-TOGGLE' => 'collapse',
+		    'DATA-TARGET' => '#' . $id,
+		}),
+		$self->get('header')->put(class => 'navbar-brand'),
+	    ]), 'navbar-header'),
+	    DIV($self->get('collapse_items'), 'collapse navbar-collapse', {
+		ID => $id,
 	    }),
-	    $self->get('header')->put(class => 'navbar-brand'),
-	]), 'navbar-header'),
-	DIV($self->get('collapse_items'), 'collapse navbar-collapse', {
-	    ID => $id,
-	}),
-    ])), 'navbar navbar-default navbar-fixed-top'))->SUPER::initialize(@_);
+	]), $self->unsafe_get('container_class') || 'container'),
+	join(' ', 'navbar navbar-default navbar-fixed-top', ($self->unsafe_get('additional_navbar_classes') || '')),
+    ))->SUPER::initialize(@_);
 }
 
 1;
