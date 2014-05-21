6d29db1259359ee3082390e9b4727307c1f00c74
Paul Moeller <moeller@bivio.biz>
2014-05-21 00:39:29 +0000

    don't die if the acknowldgement label is invalid, could be a bad uri. warn instead

--- UI/XHTML/Widget/Acknowledgement.pm
+++ UI/XHTML/Widget/Acknowledgement.pm
@@ -6,8 +6,8 @@ use Bivio::Base 'XHTMLWidget.Tag';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_A) = __PACKAGE__->use('Action.Acknowledgement');
-my($_T) = __PACKAGE__->use('FacadeComponent.Text');
+my($_A) = b_use('Action.Acknowledgement');
+my($_T) = b_use('FacadeComponent.Text');
 
 sub new {
     return shift->SUPER::new(@_)->put_unless_exists(
@@ -19,6 +19,10 @@ sub new {
 	         my($req) = shift->req;
 	         return ''
 		     unless my $label = $_A->extract_and_delete_label($req);
+		 unless ($_A->exists_in_facade($req, $label)) {
+		     b_warn('invalid acknowledgement label: ', $label);
+		     return '';
+		 }
                  return vs_text($req, 'acknowledgement', $label);
 	    }],
         )),
