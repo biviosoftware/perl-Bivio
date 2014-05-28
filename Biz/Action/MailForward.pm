f742020373b682ab3959056bf5b69f8df82318ed
Rob Nagler <github@q33.us>
2014-05-28 23:59:12 +0000

    added support for REWRITE_FROM_DOMAIN_REFLECTOR task

--- Biz/Action/MailForward.pm
+++ Biz/Action/MailForward.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2006-2013 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2006-2014 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Bivio::Biz::Action::MailForward;
 use strict;
@@ -7,13 +7,26 @@ b_use('IO.ClassLoaderAUTOLOAD');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub REWRITE_FROM_DOMAIN_URI {
+    return 'user';
+}
+
 sub execute {
-    my(undef, $req) = @_;
+    return _execute(shift, shift);
+}
+
+sub execute_rewrite_from_domain {
+    my($proto, $req) = @_;
+    return _execute($proto, $req, Model_Email()->new($req)->load->get('email'));
+}
+
+sub _execute {
+    my(undef, $req, $recipient) = @_;
     my($mr) = $req->get('Model.MailReceiveDispatchForm');
     Mail_Outgoing()->new(
 	Mail_Incoming()->new($mr->get('message')->{content}),
     )->set_recipients(
-	$mr->get('recipient'),
+	$recipient || $mr->get('recipient'),
 	$req,
     )->set_headers_for_forward(
 	$mr->unsafe_get('email_alias_incoming'),
