deb80a00917d713c8793cf62e486be5260fab3e6
Rob Nagler <github@q33.us>
2014-10-16 00:26:21 +0000

    added primitive git support

--- Util/VC.pm
+++ Util/VC.pm
@@ -6,6 +6,11 @@ use Bivio::Base 'Bivio.ShellUtil';
 b_use('IO.ClassLoaderAUTOLOAD');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+b_use('IO.Config')->register(my $_CFG = {
+    repo_map => {
+	'perl/Cal54' => 'https://github.com/biviosoftware/perl-Cal54.git',
+    },
+});
 
 sub CONTROL_DIR_GLOB {
     return '{CVS,.git}';
@@ -23,22 +28,42 @@ commands
 EOF
 }
 
+sub handle_config {
+    my(undef, $cfg) = @_;
+    $_CFG = $cfg;
+    return;
+}
+
 sub u_checkout {
    my($self) = shift;
    # 'perl-IEEE' where
    my($version) = @_ > 1 ? shift(@_) : undef;
    my($module) = @_;
+   my($repo) = $_CFG->{module_map}->{$module} || $module;
+   my($git_dir) = $repo =~ m{([^/]+)\.git$};
+   # 'git clone -b 2.4 --single-branch https://github.com/Itseez/opencv.git opencv-2.4'
    if (-d $module) {
        if ($version) {
 	   $self->usage_error($module,': module exists and version supplied');
        }
-       $self->piped_exec([qw(cvs -Q update -d), $module]);
+       if ($git_dir) {
+	   IO_File()->do_in_dir(
+	       $module,
+	       sub {$self->piped_exec(['git pull'])},
+	   );
+       }
+       else {
+	   $self->piped_exec([qw(cvs -Q update -d), $repo]);
+       }
    }
-   $self->piped_exec([qw(cvs -Q checkout -f -r), $version || 'HEAD', $module]);
-#git clone -n git://path/to/the_repo.git --depth 1
-#    Then check out just the file you want like so:
-#    cd the_repo
-#	git checkout HEAD name_of_file
+   $version ||= 'HEAD';
+   if ($git_dir) {
+       $self->piped_exec([qw(git clone), $version eq 'HEAD' ? () : (-b => $version), $repo]);
+       IO_File->mkdir_parent_only($module);
+       IO_File->rename($git_dir, $module);
+       return;
+   }
+   $self->piped_exec([qw(cvs -Q checkout -f -r), $version, $repo]);
    return;
 }
 
