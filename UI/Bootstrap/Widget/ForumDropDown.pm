6ceca0e9f615bb6a7865fcf3d741f2f16977e6ab
Paul Moeller <moeller@bivio.biz>
2014-11-04 17:04:23 +0000

    Initial checkin

---
+++ UI/Bootstrap/Widget/ForumDropDown.pm
@@ -0,0 +1,11 @@
+# Copyright (c) 2014 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Bivio::UI::Bootstrap::Widget::ForumDropDown;
+use strict;
+use Bivio::Base 'Bivio::UI::XHTML::Widget::ForumDropDown';
+
+sub DISPLAY_NAME_FIELD {
+    return 'display_name';
+}
+
+1;
