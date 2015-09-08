f44a6cfc0c475781d32d54d2284763e537a58dab
Paul Moeller <moeller@bivio.biz>
2015-09-08 14:34:48 -0600

    detect a rewritten email domain in unsafe_user_id_from_email()

--- Biz/Model/Email.pm
+++ Biz/Model/Email.pm
@@ -67,6 +67,13 @@ sub is_ignore {
 
 sub unsafe_user_id_from_email {
     my($self, $email) = @_;
+
+    # detect a rewritted email domain
+    if ($email && $email =~ /(\w+)\*(\d+)@/) {
+        my($tag, $realm_id) = ($1, $2);
+        return $realm_id
+            if $tag eq  b_use('Action.MailForward')->REWRITE_FROM_DOMAIN_URI;
+    }
     # guard against duplicate email, use user with role, or oldest id
     my($user_ids) = $self->map_iterate(
 	'realm_id', 'unauth_iterate_start', 'realm_id ASC', {
