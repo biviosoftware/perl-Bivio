29ab34c71cb33547edcf53b8c85cd0923df3975b
robnagler <github@q33.us>
2021-03-02 17:47:05 +0000

    dump payments

--- Util/ECPayment.pm
+++ Util/ECPayment.pm
@@ -14,6 +14,48 @@ commands
 EOF
 }
 
+sub info {
+    # Return subscription info for the current club.
+    #
+    # type, start - end
+    #     pay date, method, amount [CC #, CC exp date]
+    #     ...
+    # ...
+    my($self) = @_;
+    my($payments) = $self->model('ECPaymentList')->load_all;
+    my($info) = join(', ', $self->req(qw(auth_realm owner))
+        ->get(qw(name display_name))) . "\n";
+
+    while ($payments->next_row) {
+	$info .= $payments->get('ECPayment.ec_payment_id')
+	    . ' ' . $payments->get('ECPayment.service')->get_short_desc.', ';
+
+	if ($payments->get('ECSubscription.start_date')) {
+	    $info .= Type_Date()->to_literal(
+		$payments->get('ECSubscription.start_date')) . ' - '
+		. Type_Date()->to_literal($payments->get('ECSubscription.end_date'))
+		. "\n";
+	}
+	else {
+	    $info .= "no subscription\n";
+	}
+	$info .= "\t" . Type_Date()->to_literal(
+	    $payments->get('ECPayment.creation_date_time'))
+	    . ' ' . $payments->get('ECPayment.method')->get_short_desc
+	    . ' $' . $payments->get('ECPayment.amount')
+	    . ' ' . $payments->get('ECPayment.status')->get_short_desc
+	    . "\n";
+
+	if ($payments->get('ECPayment.method')->eq_credit_card) {
+	    $info .= "\t\tCC# " . $payments->get_model('ECCreditCardPayment')
+		->get('card_number')
+		. ' expires: ' . Type_Date()->to_literal($payments->get(
+		    'ECCreditCardPayment.card_expiration_date')) . "\n";
+	}
+    }
+    return $info;
+}
+
 sub process_all {
     my($self) = @_;
     $self->initialize_fully;
