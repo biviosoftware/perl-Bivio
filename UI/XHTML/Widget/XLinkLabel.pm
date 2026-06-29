a067c626b761612794d51ac74f7a10f23b7ffbe8
Justin Schell <jschell@bivio.biz>
2026-06-29 18:00:05 +0000

    fix memory leaks caused by widget reference cycles

--- UI/XHTML/Widget/XLinkLabel.pm
+++ UI/XHTML/Widget/XLinkLabel.pm
@@ -1,10 +1,9 @@
-# Copyright (c) 2008 bivio Software, Inc.  All Rights Reserved.
-# $Id$
+# Copyright (c) 2008-2026 bivio Software, Inc.  All Rights Reserved.
 package Bivio::UI::XHTML::Widget::XLinkLabel;
 use strict;
 use Bivio::Base 'XHTMLWidget.Simple';
 use Bivio::UI::ViewLanguageAUTOLOAD;
-
+use Scalar::Util ();
 
 sub qualify_label {
     my(undef, $label) = @_;
@@ -13,12 +12,14 @@ sub qualify_label {
 
 sub initialize {
     my($self) = @_;
+    # Weakly capture $self so the stored Prose's code_ref does not form a reference-cycle
+    Scalar::Util::weaken(my $w = $self);
     $self->initialize_attr(_prose => Prose([sub {
         my($source) = @_;
         return vs_text(
             $source->req,
-            $self->qualify_label(
-                $self->render_simple_attr('value', $source),
+            $w->qualify_label(
+                $w->render_simple_attr('value', $source),
             ),
         );
     }]));
